996552c1456dc97a7c96fce29a15f30df676256d
radu <radu.cx+linuxmce@gmail.com>
2004-11-04 10:23:33 +0000

    Remove leading and trailing spaces in parameters Replace multiple space runs with a single space in parameters

--- ConfirmDependencies_Script/ConfirmDependencies_Debian.sh
+++ ConfirmDependencies_Script/ConfirmDependencies_Debian.sh
@@ -7,16 +7,18 @@ ERR_APT=3
 ERR_WGET=4
 ERR_DPKG_INSTALL=5
 
-PKG_NAME="$1" # Package Name
-REPOS_SRC="$2" # repository source (URL, freeform)
-REPOS="$3" # repository (woody, sarge, ...)
-REPOS_TYPE="$4" # repository type (numeric: 1 - package, 2 - link to source, 3 - archive)
-MIN_VER="$5" # minimum version (text)
-BIN_EXEC="$6" # binary executables
-SRC_INCLUDES="$7" # source includes
-SRC_IMPL="$8" # source implementation
-BIN_LIB="$9" # binary library
-CONFIG="${10}" # configuration
+SPACE_SED='s/  */ /g; s/^ *//g; s/ *$//g'
+
+PKG_NAME=$(echo "$1" | sed "$SPACE_SED") # Package Name
+REPOS_SRC=$(echo "$2" | sed "$SPACE_SED") # repository source (URL, freeform)
+REPOS=$(echo "$3" | sed "$SPACE_SED") # repository (woody, sarge, ...)
+REPOS_TYPE=$(echo "$4" | sed "$SPACE_SED") # repository type (numeric: 1 - package, 2 - link to source, 3 - archive)
+MIN_VER=$(echo "$5" | sed "$SPACE_SED") # minimum version (text)
+BIN_EXEC=$(echo "$6" | sed "$SPACE_SED") # binary executables
+SRC_INCLUDES=$(echo "$7" | sed "$SPACE_SED") # source includes
+SRC_IMPL=$(echo "$8" | sed "$SPACE_SED") # source implementation
+BIN_LIB=$(echo "$9" | sed "$SPACE_SED") # binary library
+CONFIG=$(echo "${10}" | sed "$SPACE_SED") # configuration
 
 SECTIONS="main non-free contrib"
 
@@ -33,8 +35,6 @@ case "$REPOS_TYPE" in
 		exit $ERR_UNKNOWN_REP_TYPE
 	;;
 esac
-
-SPACE_SED='s/  */ /g; s/^ *//g; s/ *$//g'
 
 case "$REPOS_SRC" in
 	deb*) URL_TYPE=apt; REPOS_SRC="$(echo $REPOS_SRC | sed "$SPACE_SED")" ;;
