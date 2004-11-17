084e0695978542c215d1e6933804129f241bd0ea
mtoader <mtoader>
2004-11-17 17:04:02 +0000

    added Connect() declaration

--- MythTV_Player/MythTV_Player.h
+++ MythTV_Player/MythTV_Player.h
@@ -2,7 +2,7 @@
 #ifndef MythTV_Player_h
 #define MythTV_Player_h
 
-//	DCE Implemenation for #35 MythTV Player
+//  DCE Implemenation for #35 MythTV Player
 
 #include "Gen_Devices/MythTV_PlayerBase.h"
 //<-dceag-d-e->
@@ -17,8 +17,8 @@
 //<-dceag-decl-b->
 namespace DCE
 {
-	class MythTV_Player : public MythTV_Player_Command
-	{
+    class MythTV_Player : public MythTV_Player_Command
+    {
 //<-dceag-decl-e->
     // Private member variables
     QApplication    *m_pQApplication;
@@ -34,52 +34,50 @@ public:
 
 //<-dceag-const-b->
 public:
-		// Constructors/Destructor
-		MythTV_Player(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL);
-		virtual ~MythTV_Player();
-		virtual bool Register();
+        // Constructors/Destructor
+        MythTV_Player(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL);
+        virtual ~MythTV_Player();
+        virtual bool Register();
 //<-dceag-const-e->
 
+        virtual bool Connect();
 //<-dceag-h-b->
-	/*
-				AUTO-GENERATED SECTION
-				Do not change the declarations
-	*/
+    /*
+                AUTO-GENERATED SECTION
+                Do not change the declarations
+    */
 
-	/*
-			*****DATA***** accessors inherited from base class
+    /*
+            *****DATA***** accessors inherited from base class
 
-			*****EVENT***** accessors inherited from base class
-	void EVENT_Error_Occured(string sError_Message);
+            *****EVENT***** accessors inherited from base class
+    void EVENT_Error_Occured(string sError_Message);
 
-			*****COMMANDS***** we need to implement
-	*/
+            *****COMMANDS***** we need to implement
+    */
 
-/* 
-	COMMAND: #75 - Start TV
-	COMMENTS: Start TV playback on this device.
-	PARAMETERS:
-*/
-	virtual void CMD_Start_TV() { string sCMD_Result; CMD_Start_TV(sCMD_Result,NULL);};
-	virtual void CMD_Start_TV(string &sCMD_Result,Message *pMessage);
 
-/* 
-	COMMAND: #76 - Stop TV
-	COMMENTS: Stop TV playback on this device.
-	PARAMETERS:
-*/
-	virtual void CMD_Stop_TV() { string sCMD_Result; CMD_Stop_TV(sCMD_Result,NULL);};
-	virtual void CMD_Stop_TV(string &sCMD_Result,Message *pMessage);
+    /** @brief COMMAND: #75 - Start TV */
+    /** Start TV playback on this device. */
 
-/* 
-	COMMAND: #187 - Tune to channel
-	COMMENTS: This will make the device to tune to a specific channel.
-	PARAMETERS:
-		#48 Value
-			The Program ID that we need to tune to.
-*/
-	virtual void CMD_Tune_to_channel(int iValue) { string sCMD_Result; CMD_Tune_to_channel(iValue,sCMD_Result,NULL);};
-	virtual void CMD_Tune_to_channel(int iValue,string &sCMD_Result,Message *pMessage);
+    virtual void CMD_Start_TV() { string sCMD_Result; CMD_Start_TV(sCMD_Result,NULL);};
+    virtual void CMD_Start_TV(string &sCMD_Result,Message *pMessage);
+
+
+    /** @brief COMMAND: #76 - Stop TV */
+    /** Stop TV playback on this device. */
+
+    virtual void CMD_Stop_TV() { string sCMD_Result; CMD_Stop_TV(sCMD_Result,NULL);};
+    virtual void CMD_Stop_TV(string &sCMD_Result,Message *pMessage);
+
+
+    /** @brief COMMAND: #187 - Tune to channel */
+    /** This will make the device to tune to a specific channel. */
+        /** @param #68 ProgramID */
+            /** The Program ID that we need to tune to. */
+
+    virtual void CMD_Tune_to_channel(string sProgramID) { string sCMD_Result; CMD_Tune_to_channel(sProgramID.c_str(),sCMD_Result,NULL);};
+    virtual void CMD_Tune_to_channel(string sProgramID,string &sCMD_Result,Message *pMessage);
 
 //<-dceag-h-e->
 };
