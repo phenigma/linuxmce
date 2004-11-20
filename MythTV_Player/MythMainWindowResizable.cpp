5047d47dc3d393d90526854c27f4a03885a5d9da
mihaid <mihaid>
2004-11-20 12:09:44 +0000

    I put the comments about the license in the files below

--- MythTV_Player/MythMainWindowResizable.cpp
+++ MythTV_Player/MythMainWindowResizable.cpp
@@ -1,3 +1,20 @@
+/*
+	MythMainWindowResizable
+	
+	Copyright (C) 2004 Pluto, Inc., a Florida Corporation
+	
+	www.plutohome.com		
+	
+	Phone: +1 (877) 758-8648
+	
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+	of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	
+	See the GNU General Public License for more details.
+*/
+
+
 #include "MythMainWindowResizable.h"
 #include "DCE/Logger.h"
 
