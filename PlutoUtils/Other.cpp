4804d2619b84587a6ffcd8fe133173e9e42e2d41
mihaid <mihaid>
2004-11-21 09:39:13 +0000

    I put the informations about the license in the files below.

--- PlutoUtils/Other.cpp
+++ PlutoUtils/Other.cpp
@@ -1,3 +1,19 @@
+/* 
+	Other
+	
+	Copyright (C) 2004 Pluto, Inc., a Florida Corporation
+	
+	www.plutohome.com		
+	
+	Phone: +1 (877) 758-8648
+	
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+	of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	
+	See the GNU General Public License for more details.
+*/
+
 /**
  *
  * @file Other.cpp
@@ -7,12 +23,7 @@
  *
  */
 
- /**
-  *
-  * Copyright Notice goes here
-  *
-  */
-
+ 
 #include "Other.h"
 
 #ifndef SYMBIAN
