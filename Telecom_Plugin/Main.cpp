acbf9ad8df26a868c1abb51f12d073d2eb5b047e
mihaid <mihaid>
2004-11-21 12:27:03 +0000

    I put the informations about the license in teh files below.

--- Telecom_Plugin/Main.cpp
+++ Telecom_Plugin/Main.cpp
@@ -1,3 +1,20 @@
+/*
+ Orbiter
+ 
+ Copyright (C) 2004 Pluto, Inc., a Florida Corporation
+ 
+ www.plutohome.com		
+ 
+ Phone: +1 (877) 758-8648
+ 
+ This program is distributed according to the terms of the Pluto Public License, available at: 
+ http://plutohome.com/index.php?section=public_license 
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ or FITNESS FOR A PARTICULAR PURPOSE. See the Pluto Public License for more details.
+ 
+ */
+
 //<-dceag-incl-b->
 #include "Telecom_Plugin.h"
 #include "DCE/Logger.h"
@@ -8,7 +25,7 @@
 #include "PlutoUtils/Other.h"
 #include "PlutoUtils/Other.h"
 
-#define  Telecom_Plugin_VERSION "testing"
+#define  VERSION "testing"
 
 namespace DCE
 {
@@ -55,6 +72,18 @@ extern "C" {
 //<-dceag-main-b->
 int main(int argc, char* argv[]) 
 {
+	
+//                123456789012345678901234567890123456789012345678901234567890		
+	cout<<"	Copyright (C) 2004 Pluto, Inc., a Florida Corporation"<<endl
+		    <<"	www.plutohome.com"<<endl
+		    <<"	Phone: +1 (877) 758-8648"<<endl
+		    <<"	This program is distributed according to " << endl
+		    <<" the terms of the Pluto Public License, available at: "<<endl
+		    <<"	http://plutohome.com/index.php?section=public_license "<<endl
+		    <<"	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; "<<endl
+		    <<"	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. "<<endl
+		    <<"	See the Pluto Public License for more details."<<endl;    
+		    	
 	string sRouter_IP="dce_router";
 	int PK_Device=0;
 	string sLogger="stdout";
@@ -89,12 +118,18 @@ int main(int argc, char* argv[])
 
 	if (bError)
 	{
-		cout << "Telecom_Plugin, v." << Telecom_Plugin_VERSION << endl
-			<< "A Pluto DCE Device.  See www.plutohome.com/dce for details." << endl
-			<< "Usage: Telecom_Plugin [-r Router's IP] [-d My Device ID] [-l dce_router|stdout|null|filename]" << endl
-			<< "-r -- the IP address of the DCE Router  Defaults to 'dce_router'." << endl
-			<< "-d -- This device's ID number.  If not specified, it will be requested from the router based on our IP address." << endl
-			<< "-l -- Where to save the log files.  Specify 'dce_router' to have the messages logged to the DCE Router.  Defaults to stdout." << endl;
+//                       123456789012345678901234567890123456789012345678901234567890			
+		cout << "Telecom_Plugin, v." << VERSION << endl
+			<< "A Pluto DCE Device. See www.plutohome.com/dce for details." << endl
+			<< "Usage: Telecom_Plugin [-r Router's IP] [-d My Device ID] " << endl
+			<< "[-l dce_router|stdout|null|filename]" << endl
+			<< "-r router's IP	-- the IP address of the DCE Router " << endl
+			<< " 				Defaults to 'dce_router'." << endl
+			<< "-d my device ID	-- This device's ID number. " << endl
+			<< "	If not specified, it will be requested from the router based on our IP address." << endl
+			<< "-l dce_router|stdout|null|filename   -- Where to save the log files." << endl
+			<< " Specify 'dce_router' to have the messages logged to the DCE Router. Defaults to stdout." << endl;
+					
 		exit(0);
 	}
 
