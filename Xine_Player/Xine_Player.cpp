733704baafed2d15d763987ed389049c7b7a4b45
mihaid <mihaid>
2004-11-21 10:32:41 +0000

    I put the informations about the license in the files below.

--- Xine_Player/Xine_Player.cpp
+++ Xine_Player/Xine_Player.cpp
@@ -1,3 +1,20 @@
+/*
+	Xine_Player
+	
+	Copyright (C) 2004 Pluto, Inc., a Florida Corporation
+	
+	www.plutohome.com		
+	
+	Phone: +1 (877) 758-8648
+	
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+	of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	
+	See the GNU General Public License for more details.
+*/
+
+
 /**
  *
  * @file Xine_Player.cpp
@@ -6,11 +23,6 @@
  *
  */
 
- /**
-  *
-  * Copyright Notice goes here
-  *
-  */
  //<-dceag-d-b->
 #include "Xine_Player.h"
 #include "DCE/Logger.h"
