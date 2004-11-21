a01057d5c58135f8a21eb906d72b88a41f349322
mihaid <mihaid>
2004-11-21 11:24:36 +0000

    I put the informations about the license in the files below.

--- PlutoMO/src/PlutoMOGridEngine.cpp
+++ PlutoMO/src/PlutoMOGridEngine.cpp
@@ -1,3 +1,20 @@
+/*
+ PlutoMOGridEngine
+ 
+ Copyright (C) 2004 Pluto, Inc., a Florida Corporation
+ 
+ www.plutohome.com		
+ 
+ Phone: +1 (877) 758-8648
+ 
+ This program is distributed according to the terms of the Pluto Public License, available at: 
+ http://plutohome.com/index.php?section=public_license 
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ or FITNESS FOR A PARTICULAR PURPOSE. See the Pluto Public License for more details.
+ 
+ */
+
 #include "PlutoMOGridEngine.h"
 
 #include <akniconarray.h> // CAknIconArray
