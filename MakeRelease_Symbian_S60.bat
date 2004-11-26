19584313611256023e3c66373061d4fbdedd0f58
cmiron <cmiron>
2004-11-26 11:28:18 +0000

    Minor changes in MakeRelease_Windows.bat and MakeRelease_Symbian*.bat-

--- MakeRelease_Symbian_S60.bat
+++ MakeRelease_Symbian_S60.bat
@@ -1,8 +1,7 @@
-del %LOG_PATH%\Build_Symbian_S60.log
-
 CALL SET INPUT_PATH=C:\pluto\trunk
+CALL SET MAKERELEASE_PATH=C:\pluto\trunk
 
-%INPUT_PATH%\bin\MakeRelease.exe -h 10.0.0.150 -o 8 -s %INPUT_PATH% -n / -v 2
+%MAKERELEASE_PATH%\bin\MakeRelease.exe -h 10.0.0.150 -o 8 -s %INPUT_PATH% -n / -v 2
 
 
 
--- MakeRelease_Windows.bat
+++ MakeRelease_Windows.bat
@@ -1,15 +1,14 @@
 SET PATH="c:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE"
 
-del %LOG_PATH%\Build_Windows.log
-
 CALL SET CONFIGURATION=debug
 CALL SET INPUT_PATH=c:\pluto\trunk
+CALL SET MAKERELEASE_PATH=C:\pluto\trunk
 CALL SET LOG_PATH=c:
 
+del %LOG_PATH%\Build_Windows.log
+
 ren %INPUT_PATH%\pluto.sln pluto.sln.temp
-
-%INPUT_PATH%\bin\MakeRelease.exe -h 10.0.0.150 -o 7 -s %INPUT_PATH% -n / -v 2
-
+%MAKERELEASE_PATH%\bin\MakeRelease.exe -h 10.0.0.150 -o 7 -s %INPUT_PATH% -n / -v 2 -S
 ren %INPUT_PATH%\pluto.sln.temp pluto.sln
 
 
