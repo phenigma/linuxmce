dfda8bbffeb4b438037336deeed521dae68b1d9e
radu <radu.cx+linuxmce@gmail.com>
2004-11-28 14:19:00 +0000

    oops

--- database/mkr_postinst_local.sh
+++ database/mkr_postinst_local.sh
@@ -5,8 +5,12 @@ if ! echo 'SELECT PK_Users FROM Users limit 1;' | mysql pluto_main; then
 	cd /usr/pluto/database/
 	/usr/pluto/bin/sqlCVS -D pluto_main -r local import
 else
+	#This is a temporary patch to chagne the schema until sqlCVS can propagate schema changes
 	#if the table was already modified, this won't hurt anything
-	$(echo 'alter table `pluto_main`.`Users` ,add column `Password_Unix` varchar (100)   NULL  after `Staff`, add column `Password_Samba` varchar (100)   NULL  after `Password_Unix`;' | mysql pluto_main)
+	if ! $(echo 'alter table `pluto_main`.`Users` ,add column `Password_Unix` varchar (100)   NULL  after `Staff`, add column `Password_Samba` varchar (100)   NULL  after `Password_Unix`;' | mysql -f pluto_main); then
+		echo 'Database already updated'
+	fi
+	echo 'A mysql error above this line is okay'
 	# Don't want an error code
 	exit 0
 fi
