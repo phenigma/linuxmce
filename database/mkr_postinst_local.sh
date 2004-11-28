baa7a9bbb8c1570f2409c7d77bcae48715a5b875
radu <radu.cx+linuxmce@gmail.com>
2004-11-28 11:57:16 +0000

    try again

--- database/mkr_postinst_local.sh
+++ database/mkr_postinst_local.sh
@@ -6,7 +6,7 @@ if ! echo 'SELECT PK_Users FROM Users limit 1;' | mysql pluto_main; then
 	/usr/pluto/bin/sqlCVS -D pluto_main -r local import
 else
 	#if the table was already modified, this won't hurt anything
-	echo 'alter table `pluto_main`.`Users` ,add column `Password_Unix` varchar (100)   NULL  after `Staff`, add column `Password_Samba` varchar (100)   NULL  after `Password_Unix`;' | mysql pluto_main
+	$(echo 'alter table `pluto_main`.`Users` ,add column `Password_Unix` varchar (100)   NULL  after `Staff`, add column `Password_Samba` varchar (100)   NULL  after `Password_Unix`;' | mysql pluto_main)
 	# Don't want an error code
 	exit 0
 fi
