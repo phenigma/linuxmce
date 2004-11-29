9f07e3647d6b3ed4d343823377a23173c7ce9cb4
radu <radu.cx+linuxmce@gmail.com>
2004-11-29 17:06:27 +0000

    extra comments

--- database/mkr_postinst_system.sh
+++ database/mkr_postinst_system.sh
@@ -1,2 +1,9 @@
+echo 
+echo 
+echo Setting up system database.
+echo ****This may take a long time****
+echo This involves merging all the changes from the
+echo central database back into your local copy.
+echo Please be patient.......
 (cd /usr/pluto/database; /usr/pluto/bin/sqlCVS -D pluto_main -r constants,dce,designer,document,ir,website import)
 
