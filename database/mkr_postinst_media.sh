11472bbf4f7d8cbc5745bec82d78111ecdf998c2
radu <radu.cx+linuxmce@gmail.com>
2004-12-10 15:59:06 +0000

    adding user for pluto_media

--- database/mkr_postinst_media.sh
+++ database/mkr_postinst_media.sh
@@ -1,1 +1,4 @@
 (cd /usr/pluto/database; /usr/pluto/bin/sqlCVS -D pluto_media -r media import) || exit $?
+
+Q="GRANT ALL PRIVILEGES ON pluto_media.* TO 'plutomedia'@'localhost'; FLUSH PRIVILEGES;"
+echo "$Q" | /usr/bin/mysql
