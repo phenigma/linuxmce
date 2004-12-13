31e4f6c6dfe0a333657e7954a6a3b586aded7d11
radu <radu.cx+linuxmce@gmail.com>
2004-12-13 11:01:28 +0000

    changed a little

--- Orbiter/mkr_postinst.sh
+++ Orbiter/mkr_postinst.sh
@@ -2,26 +2,26 @@
 
 PrevVer="$2"
 
-if [ ! -z "$PrevVer" ]; then
-	echo "Upgrading from version '$PrevVer'. Not setting up X twice"
+if [ -n "$PrevVer" ]; then
+	echo "Upgrading from version '$PrevVer'. Not setting up X again"
 	exit 0
-fi
+else
+	echo "Configuring X"
+	config=$(/usr/bin/X11/X -configure 2>&1 | grep 'Your XF86Config file' | cut -d" " -f5-)
+	retcode=$?
+	if [ "$retcode" -ne 0 -o -z "$config" -o ! -e "$config" ]; then
+		echo "Something went wrong while configuring X."
+		exit 1
+	fi
 
-echo "Configuring X"
-config=$(/usr/bin/X11/X -configure 2>&1 | grep 'Your XF86Config file' | cut -d" " -f5-)
-retcode=$?
-if [ "$retcode" -ne 0 -o -z "$config" -o ! -e "$config" ]; then
-	echo "Something went wrong while configuring X."
-	exit 1
-fi
-#ConfSet X_Was_Set true
-
-[ -e /etc/X11/XF86Config-4 ] && mv /etc/X11/XF86Config-4 /etc/X11/XF86Config-4.orig
-awk '
+	[ -e /etc/X11/XF86Config-4 ] && mv /etc/X11/XF86Config-4 /etc/X11/XF86Config-4.orig
+	awk '
 { print }
 /Monitor.*Monitor0/ { print("\tDefaultDepth\t16") }
 /Depth.*16/ { print("\t\tModes\t\"800x600\"") }
 ' "$config" >/etc/X11/XF86Config-4
-rm -f "$config"
+	rm -f "$config"
 
-[ -e /dev/mouse ] || ln -s /dev/psaux /dev/mouse
+	# TODO: correct link for 2.4 too
+	ln -sf /dev/input/mice /dev/mouse # for 2.6
+fi
