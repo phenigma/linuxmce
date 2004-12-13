bb051151197c1ca7a81dd941a7c21f0e6d6245a7
radu <radu.cx+linuxmce@gmail.com>
2004-12-13 12:22:20 +0000

    open firewall

--- database/mkr_postinst_local.sh
+++ database/mkr_postinst_local.sh
@@ -2,3 +2,8 @@
 
 cd /usr/pluto/database/
 /usr/pluto/bin/sqlCVS -D pluto_main -r local import
+
+echo "insert into Firewall ( PK_Firewall, Protocol, SourcePort, SourcePortEnd, DestinationPort, DestinationIP, RuleType, psc_id, psc_batch, psc_user, psc_frozen, psc_mod ) values (  NULL,  'tcp',  '22',  '0',  '0',  '',  'core_input',  NULL,  NULL,  NULL,  '0',  NULL );" | mysql pluto_main
+echo "insert into Firewall ( PK_Firewall, Protocol, SourcePort, SourcePortEnd, DestinationPort, DestinationIP, RuleType, psc_id, psc_batch, psc_user, psc_frozen, psc_mod ) values (  NULL,  'tcp',  '80',  '0',  '0',  '',  'core_input',  NULL,  NULL,  NULL,  '0',  NULL );" | mysql pluto_main
+
+
