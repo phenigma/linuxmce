13e7852b4f27873f400cbf4474468f30251bbee8
radu <radu.cx+linuxmce@gmail.com>
2004-12-15 15:06:10 +0000

    NoSound

--- Xine_Player/mkr_postinst.sh
+++ Xine_Player/mkr_postinst.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-alsaconf
-amixer sset Master 74% unmute || /bin/true
-amixer sset PCM 74% unmute || /bin/true
+NoSound()
+{
+	[ "$1" == "0" ] && return 0
+	echo "It seems you don't have a (auto-configurable) sound card. You won't be able to play any sound."
+	return 0
+}
+
+amixer sset Master 74% unmute || NoSound
+amixer sset PCM 74% unmute || NoSound 0
