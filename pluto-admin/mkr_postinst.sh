4ecbaef93057893261ea383f85b48ce46411c39e
radu <radu.cx+linuxmce@gmail.com>
2004-12-15 10:39:06 +0000

    removed leftovers

--- pluto-admin/mkr_postinst.sh
+++ pluto-admin/mkr_postinst.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir -p $(/usr/bin/dirname "$ConfFile")
-echo "$Conf" >"$ConfFile"
-mkdir -p /usr/pluto/orbiter/floorplans
+mkdir -p -m 0777 /usr/pluto/orbiter/floorplans
+ln -sf /usr/pluto/orbiter/floorplans/ /var/www/pluto-admin/
 mkdir -p /usr/pluto/orbiter/users
 ln -sf /usr/pluto/orbiter/floorplans/ /var/www/floorplans
 ln -sf /usr/pluto/orbiter/users/ /var/www/users
@@ -17,5 +16,3 @@ done
 Redirect="<?php header('Location: support/index.php?section=pluto'); ?>"
 [ -e /var/www/index.php ] || echo "$Redirect" >/var/www/index.php
 
-mkdir -p -m 0777 /usr/pluto/orbiter/floorplans
-ln -sf /usr/pluto/orbiter/floorplans /var/www/pluto-admin/
