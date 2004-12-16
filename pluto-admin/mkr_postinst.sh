fc9f05a8deedc3e43e0618ad147a45b153104937
radu <radu.cx+linuxmce@gmail.com>
2004-12-16 13:51:09 +0000

    Made to replace default debian page if that's the only index page on the system

--- pluto-admin/mkr_postinst.sh
+++ pluto-admin/mkr_postinst.sh
@@ -6,13 +6,14 @@ mkdir -p /usr/pluto/orbiter/users
 ln -sf /usr/pluto/orbiter/floorplans/ /var/www/floorplans
 ln -sf /usr/pluto/orbiter/users/ /var/www/users
 
-Index=$(grep DirectoryIndex /etc/apache/httpd.conf | sed 's/DirectoryIndex//g; s/^ *//g; s/ *$//g')
+Index=$(grep DirectoryIndex /etc/apache/httpd.conf | sed 's/DirectoryIndex//g; s/^ *//g; s/ *$//g' | grep -v '^#')
 [ -n "$Index" ] || exit 0
 
 for File in $Index; do
-	[ -e "$File" ] && exit 0
+	[ -e "/var/www/$File" -a "$File" != "index.html" ] && exit 0
 done
+[ -e "/var/www/index.html" ] && ! diff /usr/share/apache/default-configs/apache/intro.html /var/www/index.html &>/dev/null && exit 0
+rm -f /var/www/index.html
 
-Redirect="<?php header('Location: support/index.php?section=pluto'); ?>"
+Redirect="<?php header('Location: support/index.php?section=home&package=0'); ?>"
 [ -e /var/www/index.php ] || echo "$Redirect" >/var/www/index.php
-
