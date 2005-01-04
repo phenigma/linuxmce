b77e10bee448973ddb8508c7bc3943f91cb7b571
radu <radu.cx+linuxmce@gmail.com>
2005-01-04 12:32:12 +0000

    added text to speech

---
+++ Gen_Devices/Text_To_SpeechBase.h
@@ -0,0 +1,156 @@
+#ifndef Text_To_SpeechBase_h
+#define Text_To_SpeechBase_h
+#include "DeviceData_Impl.h"
+#include "Message.h"
+#include "Command_Impl.h"
+
+
+namespace DCE
+{
+//   OUR EVENT CLASS 
+
+class Text_To_Speech_Event : public Event_Impl
+{
+public:
+	Text_To_Speech_Event(int DeviceID, string ServerAddress, bool bConnectEventHandler=true) : Event_Impl(DeviceID, ServerAddress, bConnectEventHandler) {};
+	Text_To_Speech_Event(class ClientSocket *pOCClientSocket, int DeviceID) : Event_Impl(pOCClientSocket, DeviceID) {};
+	//Events
+	class Event_Impl *CreateEvent(int PK_DeviceTemplate, ClientSocket *pOCClientSocket, int DeviceID);
+};
+
+
+//   OUR DATA CLASS 
+
+class Text_To_Speech_Data : public DeviceData_Impl
+{
+public:
+	virtual ~Text_To_Speech_Data() {};
+	class DeviceData_Impl *CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition);
+	virtual int GetPK_DeviceList() { return 57; } ;
+	virtual const char *GetDeviceDescription() { return "Text_To_Speech"; } ;
+	string Get_Name() { return m_mapParameters[40];}
+};
+
+
+
+//   OUR COMMAND CLASS 
+
+class Text_To_Speech_Command : public Command_Impl
+{
+public:
+	Text_To_Speech_Command(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL)
+	: Command_Impl(DeviceID, ServerAddress, bLocalMode, pRouter)
+	{
+		if( m_bLocalMode )
+			return;
+		m_pData=NULL;
+		m_pEvent = new Text_To_Speech_Event(DeviceID, ServerAddress);
+		int Size; char *pConfig = m_pEvent->GetConfig(Size);
+		if( !pConfig )
+			throw "Cannot get configuration data";
+		m_pData = new Text_To_Speech_Data();
+		m_pData->SerializeRead(Size,pConfig);
+		delete pConfig;
+		pConfig = m_pEvent->GetDeviceList(Size);
+		m_pData->m_AllDevices.SerializeRead(Size,pConfig);
+		delete pConfig;
+		m_pData->m_pEvent_Impl = m_pEvent;
+		m_pcRequestSocket = new Event_Impl(DeviceID, ServerAddress);
+	};
+	Text_To_Speech_Command(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter) : Command_Impl(pPrimaryDeviceCommand, pData, pEvent, pRouter) {};
+	virtual ~Text_To_Speech_Command() {};
+	Text_To_Speech_Event *GetEvents() { return (Text_To_Speech_Event *) m_pEvent; };
+	Text_To_Speech_Data *GetData() { return (Text_To_Speech_Data *) m_pData; };
+	const char *GetClassName() { return "Text_To_Speech_Command"; };
+	int PK_DeviceTemplate_get() { return 57; };
+	virtual void ReceivedCommandForChild(DeviceData_Base *pDeviceData_Base,string &sCMD_Result,Message *pMessage) { };
+	virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage) { };
+	Command_Impl *CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent);
+	//Data accessors
+	string DATA_Get_Name() { return GetData()->Get_Name(); }
+	//Event accessors
+	//Commands - Override these to handle commands from the server
+	virtual void CMD_Send_Audio_To_Device(string sText,string sPK_Device_List,string &sCMD_Result,class Message *pMessage) {};
+	virtual void CMD_Text_To_Wave(string sText,char **pData,int *iData_Size,string &sCMD_Result,class Message *pMessage) {};
+
+	//This distributes a received message to your handler.
+	virtual bool ReceivedMessage(class Message *pMessageOriginal)
+	{
+		int iHandled=0;
+		for(int s=-1;s<(int) pMessageOriginal->m_vectExtraMessages.size(); ++s)
+		{
+			Message *pMessage = s>=0 ? pMessageOriginal->m_vectExtraMessages[s] : pMessageOriginal;
+			if (pMessage->m_dwPK_Device_To==m_dwPK_Device && pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND)
+			{
+				switch(pMessage->m_dwID)
+				{
+				case 253:
+					{
+						string sCMD_Result="OK";
+					string sText=pMessage->m_mapParameters[9];
+					string sPK_Device_List=pMessage->m_mapParameters[103];
+						CMD_Send_Audio_To_Device(sText.c_str(),sPK_Device_List.c_str(),sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+						{
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							SendMessage(pMessageOut);
+						}
+						else if( pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString )
+							SendString(sCMD_Result);
+					};
+					iHandled++;
+					continue;
+				case 256:
+					{
+						string sCMD_Result="OK";
+					string sText=pMessage->m_mapParameters[9];
+					char *pData=pMessage->m_mapData_Parameters[19];
+					int iData_Size=pMessage->m_mapData_Lengths[19];
+						CMD_Text_To_Wave(sText.c_str(),&pData,&iData_Size,sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+						{
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+						pMessageOut->m_mapData_Parameters[19]=pData; pMessageOut->m_mapData_Lengths[19]=iData_Size;
+							SendMessage(pMessageOut);
+						}
+						else if( pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString )
+							SendString(sCMD_Result);
+					};
+					iHandled++;
+					continue;
+				}
+				iHandled += Command_Impl::ReceivedMessage(pMessage);
+			}
+			else if( pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND )
+			{
+				MapCommand_Impl::iterator it = m_mapCommandImpl_Children.find(pMessage->m_dwPK_Device_To);
+				if( it!=m_mapCommandImpl_Children.end() && !(*it).second->m_bGeneric )
+				{
+					Command_Impl *pCommand_Impl = (*it).second;
+					iHandled += pCommand_Impl->ReceivedMessage(pMessage);
+			}
+			else
+			{
+				DeviceData_Base *pDeviceData_Base = m_pData->m_AllDevices.m_mapDeviceData_Base_Find(pMessage->m_dwPK_Device_To);
+				string sCMD_Result="UNHANDLED";
+				if( pDeviceData_Base->IsChildOf(m_pData) )
+					ReceivedCommandForChild(pDeviceData_Base,sCMD_Result,pMessage);
+				else
+					ReceivedUnknownCommand(sCMD_Result,pMessage);
+					if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+					{
+						Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+						SendMessage(pMessageOut);
+					}
+					else if( pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString )
+						SendString(sCMD_Result);
+				}
+			}
+		}
+		return iHandled!=0;
+	}
+}; // end class
+
+
+}
+#endif
