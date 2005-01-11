847785a35a209174c68f4de8a4dee244541651c9
radu <radu.cx+linuxmce@gmail.com>
2005-01-11 12:57:41 +0000

    added password retreive from database

--- BootScripts/LDAP_Setup.sh
+++ BootScripts/LDAP_Setup.sh
@@ -3,25 +3,35 @@
 . /usr/pluto/bin/SQL_Ops.sh
 
 SlapdConf="/etc/ldap/slapd.conf"
-SlapdInc="/etc/ldap/pluto.databases"
+#SlapdInc="/etc/ldap/pluto.databases"
 
 mkdir -p /usr/share/ldap/data
 
-if ! grep -F "# Pluto" $SlapdConf &>/dev/null; then
-	Template='# Pluto
+echo "
+# Pluto
 database bdb
-suffix "dc=plutohome, dc=org"
-rootdn "cn=admin, dc=plutohome, dc=org"
+suffix \"dc=plutohome, dc=org\"
+rootdn \"cn=admin, dc=plutohome, dc=org\"
 rootpw secret
 directory /usr/share/ldap/data
 schemacheck on
 lastmod on
 index cn,sn,st eq,pres,sub
 
-include $SlapdInc
-'
-	eval "echo \"$Template\"" >>"$SlapdConf"
-fi
+
+access to dn=\".*dc=([^,]+),dc=plutohome,dc=org\"
+       by dn=\"uid=\$1,ou=users,dc=plutohome,dc=org\" write
+       by * read
+
+access to *
+       by * read
+
+access to *
+       by dn=\"cn=admin,dc=plutohome,dc=org\" write
+       by dn=\"cn=admin,dc=plutohome,dc=org\" read
+
+" >> "$SlapdConf"
+
 
 TemplateDB='database bdb
 suffix "dc=$UserName, dc=plutohome, dc=org"
@@ -31,13 +41,15 @@ directory /usr/share/ldap/data
 schemacheck on
 lastmod on
 index cn,sn,st eq,pres,sub
+
 '
 
 TemplateTemp='
 dn: uid=$UserName, ou=users, dc=plutohome, dc=org
 uid: $UserName
 sn: $UserName
-userPassword: secret
+cn: $UserName
+userPassword: {MD5}$UserPassword
 objectClass: top
 objectClass: person
 objectClass: organizationalPerson
@@ -50,15 +62,45 @@ objectClass: domain
 description: $UserName
 '
 
-Q="SELECT UserName from Users"
+Q="SELECT UserName, Password from Users"
 R=$(RunSQL "$Q")
 
-: >"$SlapdInc"
+#: >"$SlapdInc"
 : >/tmp/data.ldap
+
+echo "dn: dc=plutohome, dc=org
+dc: plutohome
+objectClass: top
+objectClass: domain
+
+dn: ou=users, dc=plutohome, dc=org
+ou: users
+description: These are privileged users
+objectClass: top
+objectClass: organizationalUnit
+
+dn: cn=admin, dc=plutohome, dc=org
+objectClass: organizationalRole
+cn: admin
+description: LDAP Directory Administrator
+" >> /tmp/data.ldap
+
 
 for User in $R; do
 	UserName=$(Field 1 "$User")
-	eval "echo \"$TemplateDB\"" >>"$SlapdInc"
+	UserPassword=$(Field 2 "$User")
+#	eval "echo \"$TemplateDB\"" >>"$SlapdInc"
 	eval "echo \"$TemplateTemp\"" >>/tmp/data.ldap
 done
+
+/etc/init.d/slapd restart
+
+timeout=60
+waited=0
+while ! nc -z localhost 389 &>/dev/null && [ "$waited" -lt "$timeout" ]; do
+	sleep 1
+	waited=$((waited + 1))
+done
+		
+ldapadd -f /tmp/data.ldap -xvc -D "cn=admin,dc=plutohome,dc=org" -w secret
 
