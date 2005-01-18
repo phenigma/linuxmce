7948506d429022fe9e93d4d643d46d5cbc03b6c1
radu <radu.cx+linuxmce@gmail.com>
2005-01-18 14:55:09 +0000

    Logout cookie repair

--- plutohome-com/client/login/clientLoggedOut.php
+++ plutohome-com/client/login/clientLoggedOut.php
@@ -1,10 +1,10 @@
 <?php
 function clientLoggedOut($output) {
 	
-	$_SESSION['userIsLogged']='';
-	session_unset();	
-	session_destroy();	
-	setcookie("PlutoHomeAutoLogin",'',time()-3600,"/", "plutohome.com");
+	$_SESSION = array();
+	session_destroy();
+
+	setcookie("PlutoHomeAutoLogin",'',time()-315360000,"/", false);
 	$out = '<div class="text" align="center" style="height:200px;vertical-align:middle;"><br><br><br><h3>Thank you for your visit</h3></div>';
 			
 	$output->setBody($out);
