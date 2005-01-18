b702b7648c8262e9004db049f9024cd9ba8035b1
radu <radu.cx+linuxmce@gmail.com>
2005-01-18 14:56:13 +0000

    Logout cookie repair

--- pluto-support/operations/logout.php
+++ pluto-support/operations/logout.php
@@ -5,7 +5,7 @@ $out='';
 
 $_SESSION['userIsLogged']='';
 session_destroy();
-setcookie("PlutoHomeAutoLogin",'',time()-3600,"/", "plutohome.com");
+setcookie("PlutoHomeAutoLogin",'',time()-315360000,"/", false);
 
 $out = '<div class="text" align="center" style="height:100px;vertical-align:middle;"><h3>Thank you for your visit</h3></div>';
 
