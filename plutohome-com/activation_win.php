8fcb6a7f196310050924bc47537830bc7e36302b
radu <radu.cx+linuxmce@gmail.com>
2005-01-19 09:16:11 +0000

    activation_win changed to listall

--- plutohome-com/activation_win.php
+++ plutohome-com/activation_win.php
@@ -20,7 +20,7 @@ if (! isset($code) || $code === "" || preg_match("/^\d+-\d+$/", $code) != 1 || !
 }
 else // return
 {
-	$message = "OK\n" . implode("\n", (array)GetActivationSh($code,'list'));
+	$message = "OK\n" . implode("\n", (array)GetActivationSh($code,'listall'));
 }
 
 echo "$message";
