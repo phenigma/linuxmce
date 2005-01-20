902081d8bf7fa3a8495d9ccfcfe14c6d23c77850
radu <radu.cx+linuxmce@gmail.com>
2005-01-20 13:49:46 +0000

    anchor fixes

--- plutohome-com/operations/maps.php
+++ plutohome-com/operations/maps.php
@@ -81,31 +81,31 @@ $out.=<<<TEXT
       &bull; <a href="#climate">Climate Map</a><br>
       &bull; <a href="#telecomunications">Telecommunications Map </a><br>
       <strong>&bull; <a href="#security">Security Map</a></strong></strong></p>
-    <p><strong>&bull;<a href="#notification"><strong></strong></a> <a name="lighting"></a></a>Lighting Map</strong><br>
+    <p><strong>&bull;<a href="notification"><strong></strong></a> <a name="lighting"></a></a>Lighting Map</strong><br>
         Here are all your lights, drapes & blinds. You can have multiple views that zoom in on different parts of the house (Main, North Wing, etc.). Touch any light to control it individually. You can also group lights so that you can control a block of devices at once. </p>
     <p><center><a href="javascript:open_popup('operations/poza19.html','532','465')"><img src="images/img%20how%20to/09/09_light_map_m.jpg" width="141" height="112" border="0"></a>
     </center></p>
     <p>
-      <strong>&bull;<a href="#notification"><strong></strong></a> <a name="media" id="media"></a>Media Map</strong><br>
+      <strong>&bull;<a href="notification"><strong></strong></a> <a name="media" id="media"></a>Media Map</strong><br>
 Here are all your entertainment areas.  You can have multiple views that zoom in on different parts of the house (Main, North Wing, etc.).
 You'll see there are 2 areas playing media now. One a playlist, the other live tv. Touch any area to control it, using the small buttons at the bottom. The 'remote' button shows you the full remote control for that media. You can also touch an entertainment area, and then touch a piece of media (the red or green boxes) to move the media to that new area.
 </p>
     <p><center><a href="javascript:open_popup('operations/poza20.html','532','465')"><img src="images/img%20how%20to/09/09_media_map_m.jpg" width="141" height="112" border="0"></a>
     </center></p>
     <p>
-      <strong>&bull;<a href="#notification"><strong></strong></a> <a name="climate" id="climate"></a>Climate Map</strong><br>
+      <strong>&bull;<a href="notification"><strong></strong></a> <a name="climate" id="climate"></a>Climate Map</strong><br>
 Here are all your climate devices. You can have multiple views that zoom in on different parts of the house (Main, North Wing, etc.).
 Every device reports it's setting. Touch any device to control it (pool, thermostat, etc.). Here we touched a thermostat and can adjust it on-screen.
     <p>  <center><a href="javascript:open_popup('operations/poza21.html','532','465')"><img src="images/img%20how%20to/09/09_climate_map_m.jpg" width="141" height="112" border="0"></a>
     </center>        
     <p>
-      <strong>&bull;<a href="#notification"><strong></strong></a> <a name="telecomunications" id="telecomunications"></a>Telecommunications Map</strong><br>
+      <strong>&bull;<a href="notification"><strong></strong></a> <a name="telecomunications" id="telecomunications"></a>Telecommunications Map</strong><br>
 Here are all your telephones and Pluto video phones. You can have multiple views that zoom in on different parts of the house (Main, North Wing, etc.).
 You'll see there are 2 phone calls going on now, marked by the blue and green dots, and the details are in the lower right.
 </p>
     <p><center><a href="javascript:open_popup('operations/poza22.html','532','465')"><img src="images/img%20how%20to/09/09_telecom_map_m.jpg" width="141" height="112" border="0"></a>
     </center></p>
-    <p><strong>&bull;<a href="#notification"><strong></strong></a> <a name="security" id="security"></a>Security Map</strong><br>
+    <p><strong>&bull;<a href="notification"><strong></strong></a> <a name="security" id="security"></a>Security Map</strong><br>
   Here are all your security zones. You can have multiple views that zoom in on different parts of the house (Main, North Wing, etc.).
   Here you see 2 motion detectors and 2 doors. The front door sensor is tripped--that's why it appears in red.</p>
     <p><center><a href="javascript:open_popup('operations/poza23.html','532','465')"><img src="images/img%20how%20to/09/09_security_map_m.jpg" width="141" height="112" border="0"></a>
