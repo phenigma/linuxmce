e198b9ae9e61d1e30ac6a211a8d7dfa32fb90530
radu <radu.cx+linuxmce@gmail.com>
2005-01-21 20:13:08 +0000

    mediapics

--- pluto-admin/mkr_postinst.sh
+++ pluto-admin/mkr_postinst.sh
@@ -6,6 +6,9 @@ mkdir -p /usr/pluto/orbiter/users
 ln -sf /usr/pluto/orbiter/floorplans/ /var/www/floorplans
 ln -sf /usr/pluto/orbiter/users/ /var/www/users
 
+mkdir -p -m 0777 /home/mediapics
+ln -sf /home/mediapics /var/www/pluto-admin/mediapics
+
 Index=$(grep DirectoryIndex /etc/apache/httpd.conf | sed 's/DirectoryIndex//g; s/^ *//g; s/ *$//g' | grep -v '^#')
 [ -n "$Index" ] || exit 0
 
