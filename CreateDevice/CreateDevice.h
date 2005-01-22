5226e89376590cc4a4927ac83ee782a82cb234b1
aaron <aaron.b@plutohome.com>
2005-01-22 19:11:11 +0000

    forgot to add a header

---
+++ CreateDevice/CreateDevice.h
@@ -0,0 +1,18 @@
+#ifndef CreateDevice_h
+#define CreateDevice_h
+
+#include "PlutoUtils/MySQLHelper.h"
+
+class CreateDevice : public MySqlHelper
+{
+	int m_iPK_Installation;
+
+public:
+	CreateDevice(int PK_Installation,string host, string user, string pass, string db_name, int port=3306) 
+		: MySqlHelper(host, user, pass, db_name, port) { m_iPK_Installation=PK_Installation; }
+
+	int DoIt(int iPK_DHCPDevice,int iPK_DeviceTemplate,string sIPAddress,string sMacAddress,int PK_Device_ControlledVia=0);
+	void CreateChildrenByCategory(int iPK_Device,int iPK_DeviceCategory);
+	void CreateChildrenByTemplate(int iPK_Device,int iPK_DeviceTemplate);
+};
+#endif
---
+++ CreateDevice/Main.cpp
@@ -0,0 +1,112 @@
+#include "PlutoUtils/FileUtils.h"
+#include "PlutoUtils/StringUtils.h"
+#include "PlutoUtils/Other.h"
+#include "DCEConfig.h"
+#include "Logger.h"
+#include "CreateDevice.h"
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+
+#ifdef WIN32
+#include <direct.h>
+#include <conio.h>
+#define chdir _chdir  // Why, Microsoft, why?
+#define mkdir _mkdir  // Why, Microsoft, why?
+#else
+
+#endif
+
+#define  VERSION "<=version=>"
+
+using namespace std;
+using namespace DCE;
+DCEConfig dceConfig;
+
+namespace DCE
+{
+	Logger *g_pPlutoLogger;
+}
+
+int main(int argc, char *argv[])
+{
+	g_pPlutoLogger = new FileLogger(stdout);
+
+	int iPK_DeviceTemplate=0,iPK_DHCPDevice=0;
+	string sIPAddress,sMacAddress;
+
+	bool bError=false;
+	char c;
+	for(int optnum=1;optnum<argc;++optnum)
+	{
+		c=argv[optnum][1];
+		switch (c)
+		{
+		case 'h':
+			dceConfig.m_sDBHost = argv[++optnum];
+			break;
+		case 'u':
+			dceConfig.m_sDBUser = argv[++optnum];
+			break;
+		case 'p':
+			dceConfig.m_sDBPassword = argv[++optnum];
+			break;
+		case 'D':
+			dceConfig.m_sDBName = argv[++optnum];
+			break;
+		case 'P':
+			dceConfig.m_iDBPort = atoi(argv[++optnum]);
+			break;
+		case 'c':
+			iPK_DHCPDevice = atoi(argv[++optnum]);
+			break;
+		case 'd':
+			iPK_DeviceTemplate = atoi(argv[++optnum]);
+			break;
+		case 'i':
+			dceConfig.m_iPK_Installation = atoi(argv[++optnum]);
+			break;
+		case 'I':
+			sIPAddress = argv[++optnum];
+			break;
+		case 'M':
+			sMacAddress = argv[++optnum];
+			break;
+		default:
+			cout << "Unknown: " << argv[optnum] << endl;
+			bError=true;
+			break;
+		};
+	}
+
+	if ( bError || (!iPK_DHCPDevice && !iPK_DeviceTemplate) )
+	{
+		cout << "CreateDevice, v." << VERSION << endl
+			<< "Usage: CreateDevice [-h hostname] [-u username] [-p password] [-D database] [-P mysql port] [-c PK_DHCPDevice] [-d PK_DeviceTemplate] [-i PK_Installation] [-I IPAddress] [-M MacAddress]" << endl
+			<< "hostname    -- address or DNS of database host, default is `dce_router`" << endl
+			<< "username    -- username for database connection" << endl
+			<< "password    -- password for database connection, default is `` (empty)" << endl
+			<< "database    -- database name.  default is pluto_main" << endl;
+
+		exit(0);
+	}
+
+	CreateDevice createDevice(dceConfig.m_iPK_Installation,dceConfig.m_sDBHost,dceConfig.m_sDBUser,dceConfig.m_sDBPassword,dceConfig.m_sDBName,dceConfig.m_iDBPort);
+
+	int PK_Device=createDevice.DoIt(iPK_DHCPDevice,iPK_DeviceTemplate,sIPAddress,sMacAddress);
+	if( PK_Device==0 )
+	{
+		cerr << "CreateDevice failed" << endl;
+		exit(1);
+	}
+	
+	cout << PK_Device << endl;
+	return 0;
+}
+
