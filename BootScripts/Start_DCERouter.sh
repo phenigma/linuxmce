9c915a53fe820d96ba077f0dc4b67fe1d6eae5ed
aaron <aaron.b@plutohome.com>
2005-01-24 20:09:24 +0000

    put DCERouter in a screen session and logged

--- BootScripts/Start_DCERouter.sh
+++ BootScripts/Start_DCERouter.sh
@@ -12,47 +12,4 @@ real_log="$log_file.log"
 module=DCERouter
 device_name="$module"
 
-i=1
-while [ "$i" -le 10 ]; do
-	Logging $TYPE $SEVERITY_NORMAL "$module" "Appending log..."
-	cat "$new_log" >> "$real_log"
-	Logging $TYPE $SEVERITY_NORMAL "$module" "Starting... $i"
-	echo $(date) Starting > "$new_log"
-
-	screen -d -m -S DCERouter /usr/pluto/bin/Spawn_Wrapper.sh $VGcmd/usr/pluto/bin/DCERouter -h localhost
-
-	if [ "$?" -eq 3 ]; then
-		# Abort
-		Logging $TYPE $SEVERITY_NORMAL "$module" "Shutting down... $i $device_name"
-		echo $(date) Shutdown >> "$new_log"
-		exit
-	elif [ "$?" -eq 2 ]; then
-		Logging $TYPE $SEVERITY_NORMAL "$module" "Device requests restart... $i $device_name"
-		echo $(date) Shutdown >> "$new_log"
-	else
-		Logging $TYPE $SEVERITY_CRITICAL "$module" "Device died... $i $device_name"
-		echo $(date) died >> "$new_log"
-		echo $(date) $device_name died >> /var/log/pluto/deaths
-		echo $(date) died >> /var/log/pluto/died_${device_id}_$device_name
-		sleep 5
-		i=$((i+1))
-	fi
-	echo out
-done
-
-# old
-#screen -d -m -S DCERouter bash -c "(echo \$(date) Starting; $VGcmd/usr/pluto/bin/DCERouter -h localhost) | tee $new_log"
-#timeout=60
-#waited=0
-#while ! nc -z localhost 3450 &>/dev/null && [ "$waited" -lt "$timeout" ]; do
-#	sleep 1
-#	waited=$((waited + 1))
-#done
-#if [ "$waited" -eq "$timeout" ]; then
-#	Logging "$TYPE" "$SEVERITY_CRITICAL" "$0" "--> DCERouter failed to start"
-#	exit 1
-#else
-#	Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "DCERouter started ok"
-#	PID=$(ps ax | grep /usr/pluto/bin/DCERouter | egrep -v 'SCREEN|grep|bash' | awk '{ print $1 }')
-#	echo "$PID DCERouter (by $0)" >>/var/log/pluto/running.pids
-#fi
+screen -d -m -S DCERouter /usr/pluto/bin/Spawn_DCERouter.sh
