f3acbbdd11e4f8dceb52cf4d502112d19cdbf635
cmiron <cmiron>
2005-01-26 09:24:48 +0000

    Modified windows postinstall batch files

--- database/mkr_wpostinst_system.bat
+++ database/mkr_wpostinst_system.bat
@@ -1,2 +1,7 @@
 cd /pluto/database 
-/pluto/bin/sqlCVS -D pluto_system -r system import
+/pluto/bin/sqlCVS -D pluto_system -r constants import
+/pluto/bin/sqlCVS -D pluto_system -r dce import
+/pluto/bin/sqlCVS -D pluto_system -r designer import
+/pluto/bin/sqlCVS -D pluto_system -r document import
+/pluto/bin/sqlCVS -D pluto_system -r ir import
+/pluto/bin/sqlCVS -D pluto_system -r website import
