482549cd63a4d30f891131797d8d75fa0e2b03b1
aaron <aaron.b@plutohome.com>
2005-01-27 19:24:13 +0000

    added share_IR Codes

---
+++ BootScripts/Share_IRCodes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#do a sqlCVS ci on the dce and designer and ir, all should be frozen so they are saved to a separate file
+#create a 'used codes' file that contains that table from the local and ftp that over
+
