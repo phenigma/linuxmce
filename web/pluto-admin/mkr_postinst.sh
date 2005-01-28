cd2588cea461778d912d12a7330f64e99a5e4e67
aaron <aaron.b@plutohome.com>
2005-01-28 11:17:55 +0000

    updated to use apache2

--- web/pluto-admin/mkr_postinst.sh
+++ web/pluto-admin/mkr_postinst.sh
@@ -9,14 +9,77 @@ ln -sf /usr/pluto/orbiter/users/ /var/www/users
 mkdir -p -m 0777 /home/mediapics
 ln -sf /home/mediapics /var/www/pluto-admin/mediapics
 
-Index=$(grep DirectoryIndex /etc/apache/httpd.conf | sed 's/DirectoryIndex//g; s/^ *//g; s/ *$//g' | grep -v '^#')
-[ -n "$Index" ] || exit 0
+a2dissite default || /bin/true
 
-for File in $Index; do
-	[ -e "/var/www/$File" -a "$File" != "index.html" ] && exit 0
-done
-[ -e "/var/www/index.html" ] && ! diff /usr/share/apache/default-configs/apache/intro.html /var/www/index.html &>/dev/null && exit 0
-rm -f /var/www/index.html
+#Index=$(grep DirectoryIndex /etc/apache/httpd.conf | sed 's/DirectoryIndex//g; s/^ *//g; s/ *$//g' | grep -v '^#')
+#[ -n "$Index" ] || exit 0
 
-Redirect="<?php header('Location: support/index.php?section=home&package=0'); ?>"
-[ -e /var/www/index.php ] || echo "$Redirect" >/var/www/index.php
+#for File in $Index; do
+#	[ -e "/var/www/$File" -a "$File" != "index.html" ] && exit 0
+#done
+#[ -e "/var/www/index.html" ] && ! diff /usr/share/apache/default-configs/apache/intro.html /var/www/index.html &>/dev/null && exit 0
+#rm -f /var/www/index.html
+
+#Redirect="<?php header('Location: support/index.php?section=home&package=0'); ?>"
+#[ -e /var/www/index.php ] || echo "$Redirect" >/var/www/index.php
+
+Site="
+NameVirtualHost *
+<VirtualHost *>
+	ServerAdmin webmaster@localhost
+	
+	DocumentRoot /var/www/
+	<Directory />
+		Options FollowSymLinks
+		AllowOverride None
+	</Directory>
+	<Directory /var/www/>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride None
+		Order allow,deny
+		allow from all
+		# This directive allows us to have apache2's default start page
+                # in /apache2-default/, but still have / go to the right place
+		RedirectMatch ^/$ /pluto-admin/
+	</Directory>
+
+	ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
+	<Directory \"/usr/lib/cgi-bin\">
+		AllowOverride None
+		Options ExecCGI -MultiViews +SymLinksIfOwnerMatch
+		Order allow,deny
+		Allow from all
+	</Directory>
+
+	ErrorLog /var/log/apache2/error.log
+
+	# Possible values include: debug, info, notice, warn, error, crit,
+	# alert, emerg.
+	LogLevel warn
+
+	CustomLog /var/log/apache2/access.log combined
+	ServerSignature On
+	
+	Alias /icons/ \"/usr/share/apache2/icons/\"
+	<Directory \"/usr/share/apache2/icons\">
+	    Options Indexes MultiViews
+	    AllowOverride None
+	    Order allow,deny
+	    Allow from all
+	</Directory>
+
+    Alias /doc/ \"/usr/share/doc/\"
+    <Directory \"/usr/share/doc/\">
+        Options Indexes MultiViews FollowSymLinks
+        AllowOverride None
+        Order deny,allow
+        Deny from all
+        Allow from 127.0.0.0/255.0.0.0 ::1/128
+    </Directory>
+
+</VirtualHost>
+"
+
+echo "$Site" >/etc/apache2/sites-available/pluto
+a2ensite pluto
+/etc/init.d/apache2 reload
