b0bf096c5918ed9345502af57c862af8da6eed1f
mtoader <mtoader>
2005-02-01 08:46:46 +0000

    :updated

--- src/Dhcpd-Plugin/Dhcpd-Plugin.pl
+++ src/Dhcpd-Plugin/Dhcpd-Plugin.pl
@@ -23,7 +23,7 @@ while (1 eq 1) {
   $found = 0;
 
   @data = split(/ /, $inline);
-  if($data[4] eq "localhost") {
+  if($data[4] ne "localhost") {
     $tag = $data[4];
     $op = $data[5];
     $mac_found = $data[9];
@@ -34,7 +34,7 @@ while (1 eq 1) {
     $mac_found = $data[10];
     $ip_sent = $data[8];
   }
-
+  print "we have tag=$tag $op=$op mac=$mac_found ip=$ip_sent\n";
   if($tag eq "dhcpd:") {
     if($op eq "DHCPOFFER") {
 
@@ -51,61 +51,61 @@ while (1 eq 1) {
       $st = $db_handle->prepare($sql);
       $st->execute();
       if($row = $st->fetchrow_hashref()) {
-      
+	  print "allready\n";
       } else {
-      $sql = "select PK_DHCPDevice,FK_DeviceTemplate,ConfigureScript,Package_Source.Name FROM DHCPDevice JOIN DeviceTemplate ON DHCPDevice.FK_DeviceTemplate=DeviceTemplate.PK_DeviceTemplate LEFT JOIN Package ON DeviceTemplate.FK_Package=PK_Package LEFT JOIN Package_Source ON Package_Source.FK_Package=Package.PK_Package WHERE Mac_Range_Low<=$mac_2_nr AND Mac_Range_High>=$mac_2_nr";
-      $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
-      $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
-      while($row_ref = $statement->fetchrow_hashref())
-      {
-          $found = 1;
-          $dhcpd_device = $row_ref->{PK_DHCPDevice};
-          $dev_template = $row_ref->{FK_DeviceTemplate};
-          $configure_script = $row_ref->{ConfigureScript};
-          $package_name = $row_ref->{Name};
-          $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
+          $sql = "select PK_DHCPDevice,FK_DeviceTemplate,ConfigureScript,Package_Source.Name FROM DHCPDevice JOIN DeviceTemplate ON DHCPDevice.FK_DeviceTemplate=DeviceTemplate.PK_DeviceTemplate LEFT JOIN Package ON DeviceTemplate.FK_Package=PK_Package LEFT JOIN Package_Source ON Package_Source.FK_Package=Package.PK_Package WHERE Mac_Range_Low<=$mac_2_nr AND Mac_Range_High>=$mac_2_nr";
+	  $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
           $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
-          $tmp = "";
-          $row_ref = $statement->fetchrow_hashref();
-          $tmp = $row_ref->{PK_Device};
-          if($tmp eq "") {
-            $install = get_install();
-            system("/usr/pluto/bin/CreateDevice -i $install -c $dhcpd_device -I $ip_sent -M $mac_found > dhcpd_temp.file\n");
-            open(FILE, "dhcpd_temp.file");
-            @data = <FILE>;
-            $Device_ID = $data[0];
-            chomp($Device_ID);
-            close(FILE);
-            system("/usr/pluto/bin/MessageSend localhost 0 0 2 24 26 $Device_ID");
-            system("rm -f dhcpd_temp.file");
-            if($package_name ne "") {
-              system("apt-get install $package_name");
-            }
-          } else {
-            $Device_ID = $tmp;
+          while($row_ref = $statement->fetchrow_hashref())
+	  {
+    	      $found = 1;
+	      $dhcpd_device = $row_ref->{PK_DHCPDevice};
+              $dev_template = $row_ref->{FK_DeviceTemplate};
+              $configure_script = $row_ref->{ConfigureScript};
+	      $package_name = $row_ref->{Name};
+	      $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
+              $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
+	      $tmp = "";
+    	      $row_ref = $statement->fetchrow_hashref();
+              $tmp = $row_ref->{PK_Device};
+              if($tmp eq "") {
+	        $install = get_install();
+                system("/usr/pluto/bin/CreateDevice -i $install -c $dhcpd_device -I $ip_sent -M $mac_found > dhcpd_temp.file\n");
+	        open(FILE, "dhcpd_temp.file");
+	        @data = <FILE>;
+                $Device_ID = $data[0];
+	        chomp($Device_ID);
+                close(FILE);
+	        system("/usr/pluto/bin/MessageSend localhost 0 0 2 24 26 $Device_ID");
+                system("rm -f dhcpd_temp.file");
+	        if($package_name ne "") {
+                  system("apt-get install $package_name");
+	        }
+              } else {
+	        $Device_ID = $tmp;
+              }
+	      chomp($Device_ID);
+              system("/usr/pluto/bin/$configure_script -d $Device_ID -i $ip_sent -m $mac_found");
           }
-          chomp($Device_ID);
-          system("/usr/pluto/bin/$configure_script -d $Device_ID -i $ip_sent -m $mac_found");
-      }
-      if($found == 0) {
-        $sql = "select PK_UnknownDevices FROM UnknownDevices WHERE MacAddress='$mac_found'";
-        $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
-        $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
-        $row_ref = $statement->fetchrow_hashref();
-        if($row_ref eq "")
-        {
-          $sql = "insert into UnknownDevices(MacAddress,IPAddress) VALUES('$mac_found','$ip_sent')";
-          $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
-          $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
-        } else {
-          $sql = "update UnknownDevices SET IPAddress='$ip_sent' WHERE MacAddress='$mac_found'";
-          $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
-          $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
+          if($found == 0) {
+	    $sql = "select PK_UnknownDevices FROM UnknownDevices WHERE MacAddress='$mac_found'";
+            $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
+            $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
+	    $row_ref = $statement->fetchrow_hashref();
+            if($row_ref eq "")
+	    {
+              $sql = "insert into UnknownDevices(MacAddress,IPAddress) VALUES('$mac_found','$ip_sent')";
+	      $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
+              $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
+	    } else {
+              $sql = "update UnknownDevices SET IPAddress='$ip_sent' WHERE MacAddress='$mac_found'";
+	      $statement = $db_handle->prepare($sql) or die "Couldn't prepare query '$sql': $DBI::errstr\n";
+              $statement->execute() or die "Couldn't execute query '$sql': $DBI::errstr\n";
+            }
+          }
+          $db_handle->disconnect();
         }
-      }
-      $db_handle->disconnect();
-    }
-    }
+     }
   }
 }
 
