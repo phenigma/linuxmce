63b3cd7651c53e3c6d25d142c32f19f83447452a
mtoader <mtoader>
2005-02-02 13:55:20 +0000

    tweak to share IR codes

--- src/BootScripts/Share_IRCodes.sh
+++ src/BootScripts/Share_IRCodes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . /usr/pluto/bin/Config_Ops.sh
 
-/usr/pluto/bin/sqlCVS -H sqlcvs.plutohome.com -h localhost -a -n -r local -t InfraredGroup_Command_Preferred -d "$PK_Users" -U "$PK_Users~nopass" -e checkin >> /var/log/pluto/Share_IR.newlog
+# Check in dce,ir first since that may change the primary key in InfraredGroup_Command_Preferred
 /usr/pluto/bin/sqlCVS -H sqlcvs.plutohome.com -h localhost -a -n -r dce,ir -d "$PK_Users" -U "$PK_Users~nopass" -e checkin >> /var/log/pluto/Share_IR.newlog
+/usr/pluto/bin/sqlCVS -H sqlcvs.plutohome.com -h localhost -a -n -r local -t InfraredGroup_Command_Preferred -d "$PK_Users" -U "$PK_Users~nopass" -e checkin >> /var/log/pluto/Share_IR.newlog
