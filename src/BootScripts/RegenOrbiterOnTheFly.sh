adc724793cc39670c4ccb700735c939519473dcc
mtoader <mtoader>
2005-02-07 20:17:09 +0000

    tweaks to regen orbiter on the fly to do all orbiters at once

--- src/BootScripts/RegenOrbiterOnTheFly.sh
+++ src/BootScripts/RegenOrbiterOnTheFly.sh
@@ -10,10 +10,26 @@ OutDir=/usr/pluto/orbiter
 
 export SDL_VIDEODEVICE=dummy
 
-Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Generating Orbiter on the fly nr. $1"
-echo "on the fly regen of $1 $2 $3" >> /var/log/pluto/orbitergen.newlog
-/usr/pluto/bin/OrbiterGen -d "$1" $3 -g "$SkinDir" -f "$FontDir" -o "$OutDir" -h "$MySqlHost"  >> /var/log/pluto/orbitergen.newlog || Logging "$TYPE" "$SEVERITY_CRITICAL" "$0" "Failed to generate Orbiter nr. $1"
+if [ "$1" = "0" ]; then
+	Q="SELECT Device.PK_Device FROM Device
+	JOIN DeviceTemplate ON Device.FK_DeviceTemplate=PK_DeviceTemplate
+	JOIN DeviceCategory ON FK_DeviceCategory=PK_DeviceCategory
+	WHERE (FK_DeviceCategory=5 OR FK_DeviceCategory_Parent=5)
+	AND Device.FK_Installation=$PK_Installation"
+else
+	Q="SELECT Device.PK_Device FROM Device
+	WHERE PK_Device=$1"
+fi
 
-# Notify the Orbiter Plugin that we finished
-/usr/pluto/bin/MessageSend localhost 0 $2 1 267 2 $1
+Orbiters=$(echo "$Q;" | /usr/bin/mysql -h $MySqlHost pluto_main | tail +2 | tr '\n' ' ')
 
+export SDL_VIDEODEVICE=dummy
+
+for OrbiterDev in $Orbiters; do
+	Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Generating Orbiter on the fly nr. $OrbiterDev"
+	echo "on the fly regen of $OrbiterDev $2 $3" >> /var/log/pluto/orbitergen.newlog
+	/usr/pluto/bin/OrbiterGen -d "$OrbiterDev" $3 -g "$SkinDir" -f "$FontDir" -o "$OutDir" -h "$MySqlHost"  >> /var/log/pluto/orbitergen.newlog || Logging "$TYPE" "$SEVERITY_CRITICAL" "$0" "Failed to generate Orbiter nr. $OrbiterDev"
+
+	# Notify the Orbiter Plugin that we finished
+	/usr/pluto/bin/MessageSend localhost 0 $2 1 267 2 $OrbiterDev
+done
