a2a609e22968cce796f08cc2131b8ef457a927f8
mtoader <mtoader>
2005-02-10 15:18:24 +0000

    Kill all stray setups

--- src/MythTV_Player/launchMythSetup.sh
+++ src/MythTV_Player/launchMythSetup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+MYTH_SETUP_PIDS=`pidof mythtv-setup`;
+
+if [ "$MYTH_SETUP_PIDS" != "" ]; then 
+	kill -9 $MYTH_SETUP_PIDS;
+fi;
+
 echo -e "\n\n" | mythtv-setup 
 
 /etc/init.d/mythtv-backend force-reload &
