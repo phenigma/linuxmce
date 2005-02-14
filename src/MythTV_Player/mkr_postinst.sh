250752c7f1ab5755c23673dbfe7dfa3a0d04f51a
mtoader <mtoader>
2005-02-14 23:28:13 +0000

    Added more hack's to work around bugs in the mythtv backend startup and initialization process.

--- src/MythTV_Player/mkr_postinst.sh
+++ src/MythTV_Player/mkr_postinst.sh
@@ -15,10 +15,30 @@ function addEntries
 }
 
 #Force the backend to make the database structure
-/etc/init.d/mythtv-backend force-reload
+/etc/init.d/mythtv-backend force-reload || /bin/true
 
 echo "Waiting 3 seconds so that myth backed is able to actually create the schema"
-sleep 3
+
+FOUND=0;
+TRIES=20;
+SLEEP=0.5;
+while [ $TRIES != 0 ]; do
+    RESULT=`echo show tables | $mysql_command | grep settings | wc -l`;
+    if [ "$RESULT" == "1" ]; then
+        TRIES=0;
+        FOUND=1;
+    else
+        sleep $SLEEP;
+        TRIES=$[$TRIES - 1];
+    fi;
+done;
+
+if [ "$FOUND" == "0" ]; then 
+	echo "********";
+	echo "There is an error in the mythtv backend setup. It looks like the database schema wasn't created properly";
+	echo "********";
+	exit 0;
+fi
 
 isSetup=`echo "$query" | $mysql_command`;
 
@@ -42,4 +62,9 @@ addEntries RecordFilePrefix /var/lib/mythtv/ $hostname;
 addEntries MasterServerIP	$routerip;
 addEntries MasterServerPort	6543;
 
-
+PID=`pidof /usr/bin/mythbackend`;
+if [ "$PID" != "" ]; then
+	# It seems that in certain circumstances the backed refuses to notice the -9 signal
+	kill -15 $PID;
+	/etc/init.d/mythtv-backend start || /bin/true
+fi
