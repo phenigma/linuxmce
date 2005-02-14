10331932fc4ccb740caa0453fdb86c662c20858a
mtoader <mtoader>
2005-02-14 10:22:53 +0000

    Add a waiting loop at the postinstall stage to make sure we have a proper schema before trying to update it.

--- src/MythTV_Backend_Proxy/mkr_postinst.sh
+++ src/MythTV_Backend_Proxy/mkr_postinst.sh
@@ -15,16 +15,31 @@ function addEntries
 }
 
 #Force the backend to make the database structure
-/etc/init.d/mythtv-backend force-reload
+/etc/init.d/mythtv-backend force-reload 2>/dev/null
 
-echo "Waiting 3 seconds so that myth backed is able to actually create the schema"
-sleep 3
+echo "Waiting until the schema is actually created or no more than 10 seconds"
+
+FOUND=0;
+TRIES=20;
+SLEEP=0.5;
+while [ $TRIES != 0 ]; do
+	RESULT=`echo show tables | $mysql_command | grep settings | wc -l`;
+    if [ "$RESULT" == "1" ]; then
+        TRIES=0;
+		FOUND=1;
+    else
+        sleep $SLEEP;
+        TRIES=$[$TRIES - 1];
+    fi;
+done;
+
+[ ! $FOUND ] && echo "The Backend server didn't created the schema in a timely manner. Trying again at boot time." && exit 0;
 
 isSetup=`echo "$query" | $mysql_command`;
 
 if [ "$isSetup" != 0  ]; then
 	echo "Removing old mythtv configuration for this machine";
-	echo "delete from settings where hostname='`hostname`' AND value LIkE 'BackendServer%'" | $mysql_command;
+	echo "delete from settings where hostname='`hostname`' AND value LIkE 'Backend%'" | $mysql_command;
 	echo "delete from settings where value LIkE 'MasterServer%'" | $mysql_command;
 	echo "delete from settings where hostname='`hostname`' AND value LIkE 'RecordFilePrefix'" | $mysql_command;
 fi;
@@ -36,8 +51,13 @@ routerip=`gethostip dcerouter | cut -f 2 -d ' '`;
 
 addEntries BackendServerIP $hostip $hostname;
 addEntries BackendServerPort 6143 $hostname;
+#
+# The is for Myth 0.17 * They changed the names
+#
+# addEntries BackendStatusPort 6143 $hostname;
 addEntries BackendServerStatus 6544 $hostname;
 addEntries RecordFilePrefix /var/lib/mythtv/ $hostname;
+# addEntries LiveBufferDir /var/cache/mythtv/ $hostname;
 
 addEntries MasterServerIP	$routerip;
 addEntries MasterServerPort	6543;
