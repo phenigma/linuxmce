0178a5eb5f1bc7d1cab7dbd26fbb64e45cfb9ec5
mtoader <mtoader>
2005-02-16 19:01:56 +0000

    Fixes to allow a better project presentation

--- web/pluto-support/operations/home.php
+++ web/pluto-support/operations/home.php
@@ -1,34 +1,47 @@
 <?
 $package=(isset($_SESSION['package']))?$_SESSION['package']:0;
 if($package!=0){
-	$out='
-	<p>This is the main home page for the software module <b>**package**</b>.  The above menu has links to documentation, downloads, source code, forums, mailing lists and FAQ\'s specific to <b>**package**</b>.</p>
-
-	<p>Many of Pluto\'s <a href=index.php?section=document&docID=7>plug-ins</a> and <a href=index.php?section=document&docID=6>DCE devices</a> have their own home pages.  
-	For general help, visit the <a href=index.php?section=home&package=0>general support</a> page.</p>
-
-	<p><b><u>**package**</b></u></p>';
-	$queryDevices = 'SELECT PK_DeviceTemplate,Description FROM DeviceTemplate WHERE FK_Package=?';
-	$resDevices = $dbADO->Execute($queryDevices,$package);
-	$found = 0;
-	$out1='This module includes the following DCE Devices: ';
-	$out2 = '';
-	while ($rowDevices = $resDevices->FetchRow()) {
-		$found = 1;
-		$out2.=' "'.$rowDevices['Description'].'" ';
-	}
-	if ($found == 0) {
-		$out.='<center><b>**No DCE Devices**</b></center>';
-	}
-	else {
-		$out.=$out1.'<b>'.$out2.'</b>';
-	}
+	
 	$queryDocs = 'SELECT Contents FROM Package
 					INNER JOIN Document ON FK_Document=PK_Document
 					where PK_Package=?';
 	$resDocs = $dbADO ->Execute($queryDocs,$package);
-	$rowDocs = $resDocs->FetchRow();
-	$out.='<br><br>Documentation:<br><br>'.$rowDocs['Contents'];
+	$rowDocs = $resDocs->FetchRow();                             	
+	
+	$documentation = "";
+	if ( $rowDocs['Contents'] != "" )
+	{
+		$documentation = $rowDocs['Contents'];
+	}
+	else
+	{
+		$documentation ='
+			<p>This is the main home page for the software module <b>**package**</b>. 
+			The above menu has links to documentation, downloads, source code, forums, 
+			mailing lists and FAQ\'s specific to <b>**package**</b>.</p>
+
+			<p>Many of Pluto\'s <a href=index.php?section=document&docID=7>plug-ins</a> and 
+			<a href=index.php?section=document&docID=6>DCE devices</a> have their own home pages.  
+			For general help, visit the <a href=index.php?section=home&package=0>general support</a> page.</p>';
+	}
+
+	
+	$queryDevices = 'SELECT PK_DeviceTemplate,Description FROM DeviceTemplate WHERE FK_Package=?';
+	$resDevices = $dbADO->Execute($queryDevices,$package);
+
+	$devices = "";
+	$found = 0;
+	while ($rowDevices = $resDevices->FetchRow()) 
+	{
+		$found = 1;
+		$devices .= ' <b>'.$rowDevices['Description'].'</b>';
+	}
+
+	$out = $documentation;
+	if ( $found == 1 )
+	{
+		$out .= '<br>This module includes the following DCE Devices: ' . $devices . '.'; 
+	}
 }
 else {
 	$out='<p>This is the main support page for Pluto.  Here you will find documentation, downloads, source code, forums, mailing lists and FAQ\'s for Pluto.</p>
--- web/pluto-support/operations/modules.php
+++ web/pluto-support/operations/modules.php
@@ -8,22 +8,35 @@
 					order by Type';
 	$resModules = $dbADO->Execute($queryModules);
 	$ind = 0;
-	$out.='<center><table><tr bgcolor="lightblue">
-			<td><center>Package</center></td>
-			<td><center>    Description   </center></td>
-			<td><center>Package Type</center></td>
-		</tr>';
+//	$out.='<center><table><tr bgcolor="lightblue">
+//			<td><center>Package</center></td>
+//			<td><center>    Description   </center></td>
+//			<td><center>Package Type</center></td>
+//		</tr>';
 	while ($rowModules = $resModules->FetchRow()) {
 		if ($ind%2 == 1) $col = ' bgcolor="#C0C0C0"'; else $col=' bgcolor="white"';
-		$out.='<tr'.$col.'>';
-			$out.='<td><center><a href="index.php?section=home&package='.$rowModules['PK_Package'].'">'.$rowModules['Pack'].'</a></center></td>';
-			$out.='<td><center>'.$rowModules['Comments'].'</center></td>';
-			$out.='<td><center>'.$rowModules['Type'].'</center></td>';
-		$out.='</tr>';
+		// $out.='<tr'.$col.'>';
+		$out .= '<p><a class="package-name" href="index.php?section=home&package='.$rowModules['PK_Package'].'">' . $rowModules['Pack'] . '</a></p>';
+			// $out.='<td><center><a href="'.$rowModules['Pack'].'</a></center></td>';
+		if ( trim($rowModules['Comments']) == "" ) 
+		{
+			$out .= '<p class="package-comments">A description is not yet available for this package.</p>';
+		}
+		else
+		{
+			$out.='<p class="package-comments">'.$rowModules['Comments'].'</p>';
+		}
+	
+		$out .= '<p class="package-comments">This package is in the ' . $rowModules['Type'] . ' group.</p>';
+	//	$out .= '<p class="package-subtitle">Package type: ' . $rowModules['Type'] . '</p>';
+		//$out.='<td><center>'.$rowModules['Comments'].'</center></td>';
+		//$out.='<td><center>'.$rowModules['Type'].'</center></td>';
+			//
+		//$out.='</tr>';
 		$ind++;
 	}
-	$out.='</table></center>';
-	
+//	$out.='</table></center>';
+
 $output->setBody($out);
 $output->setTitle(APPLICATION_NAME);			
 $output->output();  	
