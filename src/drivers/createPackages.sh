e8a2a5f49e3c6d4ad6ed2f63179cc067b573b901
mtoader <mtoader>
2005-02-18 18:50:19 +0000

    Actually build the packages

--- src/drivers/createPackages.sh
+++ src/drivers/createPackages.sh
@@ -2,11 +2,11 @@
 
 REPO_HOME=/home/samba/repositories/pluto/replacements/main/binary-i386
 
-# make -C ivtv/ clean 
-# make -C ivtv/ deb
+make -C ivtv/ clean 
+make -C ivtv/ deb
 
-# make -f Makefile.pluto -C video4linux clean 
-# make -f Makefile.pluto -C video4linux deb
+make -f Makefile.pluto -C video4linux clean 
+make -f Makefile.pluto -C video4linux deb
 
 
 if [ -d "$REPO_HOME" ]; then
