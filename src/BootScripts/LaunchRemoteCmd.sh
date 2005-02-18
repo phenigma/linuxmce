f15b283c5b11e5a6799ddb801b0be1100c8e0ead
radu <radu.cx+linuxmce@gmail.com>
2005-02-18 13:35:43 +0000

    fix

--- src/BootScripts/LaunchRemoteCmd.sh
+++ src/BootScripts/LaunchRemoteCmd.sh
@@ -6,7 +6,7 @@ if [ -n "$IP" -a "$#" -ne 0 ]; then
 	if [ "$IP" != "127.0.0.1" ]; then
 		ssh -l root -i /usr/pluto/keys/id_dsa_pluto_apache "$IP" "$*" || echo "Failed to contact destination computer (IP: $IP)"
 	else
-		"$@"
+		eval "$*"
 	fi
 else
 	Msg=""
