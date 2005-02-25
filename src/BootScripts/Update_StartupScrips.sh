2bac6850edbd0d623b101b0ee3971f89787c7ac0
mtoader <mtoader>
2005-02-25 16:30:12 +0000

    script change because of databsae schema change

--- src/BootScripts/Update_StartupScrips.sh
+++ src/BootScripts/Update_StartupScrips.sh
@@ -5,7 +5,7 @@ Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Updating startup scripts"
 
 Q="INSERT INTO Device_StartupScript(FK_Device,FK_StartupScript,Boot_Order,Background,Enabled,Parameter)
 SELECT FK_Device_ControlledVia, PK_StartupScript,Hybrid_Boot_Order,Hybrid_Background,Hybrid_Enabled,Hybrid_Parameter FROM Device
-JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate 
+JOIN DeviceTemplate ON Device.FK_DeviceTemplate=PK_DeviceTemplate 
 JOIN StartupScript ON 1=1
 LEFT JOIN Device_StartupScript ON Device_StartupScript.FK_Device=PK_Device AND Device_StartupScript.FK_StartupScript=PK_StartupScript
 WHERE FK_DeviceCategory=8 AND FK_Device_ControlledVia IS NOT NULL
@@ -15,7 +15,7 @@ echo "$Q;" | /usr/bin/mysql pluto_main
 
 Q="INSERT INTO Device_StartupScript(FK_Device,FK_StartupScript,Boot_Order,Background,Enabled,Parameter)
 SELECT PK_Device, PK_StartupScript,MD_Boot_Order,MD_Background,MD_Enabled,MD_Parameter FROM Device
-JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate 
+JOIN DeviceTemplate ON Device.FK_DeviceTemplate=PK_DeviceTemplate 
 JOIN StartupScript ON 1=1
 LEFT JOIN Device_StartupScript ON Device_StartupScript.FK_Device=PK_Device AND Device_StartupScript.FK_StartupScript=PK_StartupScript
 WHERE FK_DeviceCategory=8 AND FK_Device_ControlledVia IS NULL
@@ -25,7 +25,7 @@ echo "$Q;" | /usr/bin/mysql pluto_main
 
 Q="INSERT INTO Device_StartupScript(FK_Device,FK_StartupScript,Boot_Order,Background,Enabled,Parameter)
 SELECT PK_Device, PK_StartupScript,Core_Boot_Order,Core_Background,Core_Enabled,Core_Parameter FROM Device
-JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate 
+JOIN DeviceTemplate ON Device.FK_DeviceTemplate=PK_DeviceTemplate 
 JOIN StartupScript ON 1=1
 LEFT JOIN Device_StartupScript ON Device_StartupScript.FK_Device=PK_Device AND Device_StartupScript.FK_StartupScript=PK_StartupScript
 WHERE FK_DeviceCategory=7
