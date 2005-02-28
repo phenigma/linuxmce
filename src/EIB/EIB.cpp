f36d7fb5c46a0f1c1dc8bb380b7b8fb9409ff718
igor <igor>
2005-02-28 10:57:56 +0000

    changed serial port handling

--- src/EIB/EIB.cpp
+++ src/EIB/EIB.cpp
@@ -60,6 +60,17 @@ bool EIB::Connect(int iPK_DeviceTemplate) {
 	}
 
 	m_msgPool.regInterceptor(this);
+	
+	string sPort = m_pData->mapParameters_Find(DEVICEDATA_Port_CONST);
+	if(sPort.find("/dev") != 0) {
+		sPort = "/dev" + sPort;
+	}
+	
+	if(sPort.length() > 0) {
+		g_pPlutoLogger->Write(LV_STATUS, "Using serial port: %s.", sPort.c_str());
+		m_msgPool.setSerialPort(sPort.c_str());
+	}
+	
 	m_msgPool.Run(false);
 	
 /*	
