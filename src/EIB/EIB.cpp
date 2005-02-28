6690806b6d965aac08d6ef82b9ce4d64e18245cb
igor <igor>
2005-02-28 11:12:27 +0000

    removed /dev

--- src/EIB/EIB.cpp
+++ src/EIB/EIB.cpp
@@ -63,7 +63,7 @@ bool EIB::Connect(int iPK_DeviceTemplate) {
 	
 	string sPort = m_pData->mapParameters_Find(DEVICEDATA_Port_CONST);
 	if(sPort.find("/dev") != 0) {
-		sPort = "/dev" + sPort;
+		sPort.erase(0, strlen("/dev"));
 	}
 	
 	if(sPort.length() > 0) {
