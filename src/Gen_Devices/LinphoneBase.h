4e5e1345662d50bf41f222afeb2f0eeade74bef1
igor <igor>
2005-03-02 16:02:45 +0000

    Linphone wrapper

---
+++ src/Gen_Devices/LinphoneBase.h
@@ -0,0 +1,176 @@
+#ifndef LinphoneBase_h
+#define LinphoneBase_h
+#include "DeviceData_Impl.h"
+#include "Message.h"
+#include "Command_Impl.h"
+
+
+namespace DCE
+{
+//   OUR EVENT CLASS 
+
+class Linphone_Event : public Event_Impl
+{
+public:
+	Linphone_Event(int DeviceID, string ServerAddress, bool bConnectEventHandler=true) : Event_Impl(DeviceID,1602, ServerAddress, bConnectEventHandler) {};
+	Linphone_Event(class ClientSocket *pOCClientSocket, int DeviceID) : Event_Impl(pOCClientSocket, DeviceID) {};
+	//Events
+	class Event_Impl *CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice );
+};
+
+
+//   OUR DATA CLASS 
+
+class Linphone_Data : public DeviceData_Impl
+{
+public:
+	virtual ~Linphone_Data() {};
+	class DeviceData_Impl *CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition);
+	virtual int GetPK_DeviceList() { return 1602; } ;
+	virtual const char *GetDeviceDescription() { return "Linphone"; } ;
+	string Get_PhoneType() { return m_mapParameters[29];}
+	string Get_PhoneNumber() { return m_mapParameters[31];}
+};
+
+
+
+//   OUR COMMAND CLASS 
+
+class Linphone_Command : public Command_Impl
+{
+public:
+	Linphone_Command(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL)
+	: Command_Impl(DeviceID, ServerAddress, bLocalMode, pRouter)
+	{
+		if( m_bLocalMode )
+			return;
+		m_pData=NULL;
+		m_pEvent = new Linphone_Event(DeviceID, ServerAddress);
+		if( m_pEvent->m_dwPK_Device )
+			m_dwPK_Device = m_pEvent->m_dwPK_Device;
+		int Size; char *pConfig = m_pEvent->GetConfig(Size);
+		if( !pConfig )
+			throw "Cannot get configuration data";
+		m_pData = new Linphone_Data();
+		if( Size )
+			m_pData->SerializeRead(Size,pConfig);
+		delete pConfig;
+		pConfig = m_pEvent->GetDeviceList(Size);
+		m_pData->m_AllDevices.SerializeRead(Size,pConfig);
+		delete pConfig;
+		m_pData->m_pEvent_Impl = m_pEvent;
+		m_pcRequestSocket = new Event_Impl(DeviceID, 1602,ServerAddress);
+	};
+	Linphone_Command(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter) : Command_Impl(pPrimaryDeviceCommand, pData, pEvent, pRouter) {};
+	virtual ~Linphone_Command() {};
+	Linphone_Event *GetEvents() { return (Linphone_Event *) m_pEvent; };
+	Linphone_Data *GetData() { return (Linphone_Data *) m_pData; };
+	const char *GetClassName() { return "Linphone_Command"; };
+	virtual int PK_DeviceTemplate_get() { return 1602; };
+	static int PK_DeviceTemplate_get_static() { return 1602; };
+	virtual void ReceivedCommandForChild(DeviceData_Base *pDeviceData_Base,string &sCMD_Result,Message *pMessage) { };
+	virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage) { };
+	Command_Impl *CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent);
+	//Data accessors
+	string DATA_Get_PhoneType() { return GetData()->Get_PhoneType(); }
+	string DATA_Get_PhoneNumber() { return GetData()->Get_PhoneNumber(); }
+	//Event accessors
+	//Commands - Override these to handle commands from the server
+	virtual void CMD_Phone_Initiate(string sPhoneExtension,string &sCMD_Result,class Message *pMessage) {};
+	virtual void CMD_Phone_Answer(string &sCMD_Result,class Message *pMessage) {};
+	virtual void CMD_Phone_Drop(string &sCMD_Result,class Message *pMessage) {};
+
+	//This distributes a received message to your handler.
+	virtual bool ReceivedMessage(class Message *pMessageOriginal)
+	{
+		if( Command_Impl::ReceivedMessage(pMessageOriginal) )
+			return true;
+		int iHandled=0;
+		for(int s=-1;s<(int) pMessageOriginal->m_vectExtraMessages.size(); ++s)
+		{
+			Message *pMessage = s>=0 ? pMessageOriginal->m_vectExtraMessages[s] : pMessageOriginal;
+			if (pMessage->m_dwPK_Device_To==m_dwPK_Device && pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND)
+			{
+				switch(pMessage->m_dwID)
+				{
+				case 334:
+					{
+						string sCMD_Result="OK";
+					string sPhoneExtension=pMessage->m_mapParameters[83];
+						CMD_Phone_Initiate(sPhoneExtension.c_str(),sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+						{
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							SendMessage(pMessageOut);
+						}
+						else if( pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString )
+							SendString(sCMD_Result);
+					};
+					iHandled++;
+					continue;
+				case 335:
+					{
+						string sCMD_Result="OK";
+						CMD_Phone_Answer(sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+						{
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							SendMessage(pMessageOut);
+						}
+						else if( pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString )
+							SendString(sCMD_Result);
+					};
+					iHandled++;
+					continue;
+				case 336:
+					{
+						string sCMD_Result="OK";
+						CMD_Phone_Drop(sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+						{
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							SendMessage(pMessageOut);
+						}
+						else if( pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString )
+							SendString(sCMD_Result);
+					};
+					iHandled++;
+					continue;
+				}
+				iHandled += Command_Impl::ReceivedMessage(pMessage);
+			}
+			else if( pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND )
+			{
+				MapCommand_Impl::iterator it = m_mapCommandImpl_Children.find(pMessage->m_dwPK_Device_To);
+				if( it!=m_mapCommandImpl_Children.end() && !(*it).second->m_bGeneric )
+				{
+					Command_Impl *pCommand_Impl = (*it).second;
+					iHandled += pCommand_Impl->ReceivedMessage(pMessage);
+			}
+			else
+			{
+				DeviceData_Base *pDeviceData_Base = m_pData->m_AllDevices.m_mapDeviceData_Base_Find(pMessage->m_dwPK_Device_To);
+				string sCMD_Result="UNHANDLED";
+				if( pDeviceData_Base && pDeviceData_Base->IsChildOf(m_pData) )
+					ReceivedCommandForChild(pDeviceData_Base,sCMD_Result,pMessage);
+				else
+					ReceivedUnknownCommand(sCMD_Result,pMessage);
+					if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+					{
+						Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+						SendMessage(pMessageOut);
+					}
+					else if( pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString )
+						SendString(sCMD_Result);
+					if( sCMD_Result!="UNHANDLED" )
+						iHandled++;
+				}
+			}
+		}
+		return iHandled!=0;
+	}
+}; // end class
+
+
+}
+#endif
