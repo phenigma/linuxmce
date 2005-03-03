9678f4764b16f72ba2b9fd64fd77f8e340dc6cc2
radu <radu.cx+linuxmce@gmail.com>
2005-03-03 18:57:45 +0000

    Copy kernel after upgrading

--- src/kernel-upgrade/mkr_preinst.sh
+++ src/kernel-upgrade/mkr_preinst.sh
@@ -4,3 +4,6 @@ if ! grep -q "silent_modules" /etc/kernel-img.conf; then
 	# should we write the entire file just to be sure?
 	echo "silent_modules = yes" >>/etc/kernel-img.conf
 fi
+
+echo "Copying kernel package(s) for later use"
+cp -v /var/cache/apt/archives/kernel-image* /usr/pluto/deb-cache/dists/sarge/main/binary-i386/ 2>/dev/null || /bin/true
