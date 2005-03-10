25660b2213251cc7a0a747379a0142d01a3de74f
vali <vali>
2005-03-10 14:47:27 +0000

    minor change to Flash intro

--- web/plutohome-com/operations/home.php
+++ web/plutohome-com/operations/home.php
@@ -1,10 +1,10 @@
 <?
 function home($output,$conn){
 		
-$out='<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="553" height="353">
+$out='<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="555" height="353">
           <param name="movie" value="images/intro.swf" />
           <param name="quality" value="high" />
-          <embed src="images/intro.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="553" height="353"></embed>
+          <embed src="images/intro.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="555" height="353"></embed>
         </object><br><br><img src="images/main_text.gif">';
 
 
