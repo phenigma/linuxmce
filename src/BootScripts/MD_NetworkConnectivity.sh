905eac1f23afa3fb6631151fae2270b3e2ebda70
radu <radu.cx+linuxmce@gmail.com>
2005-03-15 01:42:40 +0000

    Merged Connectivity into RecoveryConsole Fixed RecoveryConsole

--- src/BootScripts/MD_NetworkConnectivity.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-trap './bin/chvt 11' EXIT
-
-while ./bin/ping -c 1 dcerouter &>/dev/null; do
-	./bin/sleep 5
-done
--- src/BootScripts/RecoveryConsole.sh
+++ src/BootScripts/RecoveryConsole.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-exec </dev/tty11 >/dev/tty11
+Connectivity()
+{
+	while ./bin/ping -c 1 dcerouter &>/dev/null; do
+		./bin/sleep 5
+	done
+}
+
+ShellLoop()
+{
+	exec </dev/tty11 &>/dev/tty11
+	while :; do
+		echo "-- Diskless MD analysis and recovery console --"
+		echo
+		bin/sh
+		bin/sleep 1
+	done
+}
+
+Emergency()
+{
+	Connectivity
+	./bin/chvt-11
+}
 
 mke2fs -m 0 /dev/ram0
 mkdir -p /mnt/recovery-rd
 mount /dev/ram0 /mnt/recovery-rd
 cd /mnt/recovery-rd
 tar -xjf /usr/pluto/install/ramdisk.tar.bz2
-screen -d -m -S MD_NetworkConnectivity /usr/pluto/bin/MD_NetworkConnectivity.sh
 mount -t devfs devfs dev
-while :; do
-	echo "-- Diskless MD analysis and recovery console --"
-	echo
-	bin/sh
-	bin/sleep 1
-done
+mount -t proc proc proc
+
+Emergency &
+ShellLoop &
