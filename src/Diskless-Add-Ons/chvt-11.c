57225f0d30e343a5bddf7f2c7acadbff85311051
radu <radu.cx+linuxmce@gmail.com>
2005-03-15 00:22:46 +0000

    Add-on for Recovery Console

---
+++ src/Diskless-Add-Ons/chvt-11.c
@@ -0,0 +1,16 @@
+/* extremely light program for switching to tty11 */
+/* inspired by chvt.c */
+#include <fcntl.h>
+
+#define VT_ACTIVATE 0x5606
+#define VT_WAITACTIVE 0x5607
+
+int main()
+{
+	int fd;
+
+	fd = open("./dev/tty", O_RDWR);
+	ioctl(fd, VT_ACTIVATE, 11);
+	ioctl(fd, VT_WAITACTIVE, 11);
+	return 0;
+}
