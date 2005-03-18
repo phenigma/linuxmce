7c634f9067d15a37f38b36c980ac6a7b3136c708
vali <vali>
2005-03-18 15:41:52 +0000

    changed login to redirect to initial mantis page, if available

--- web/plutohome-com/client/login/login.php
+++ web/plutohome-com/client/login/login.php
@@ -3,6 +3,7 @@ function login($output,$dbADO,$conn) {
 	global $forumHost;
 /* @var $dbADO ADOConnection */
 global $checkMasterUserUrl;
+$return=@$_REQUEST['return'];
 
 if(isset($_SESSION['userIsLogged']) && $_SESSION['userIsLogged']=="yes"){
 	// the user is already logged
@@ -52,6 +53,10 @@ if (isset($_POST['username']) && $_POST['username']!='') {
 				$_SESSION['setAutoCookie']=1;
 				$_SESSION['password']=$password;
 			}
+			if($return!=''){
+				header("Location: support/mantis/login.php?username=".$_SESSION['username']."&password=".$_SESSION['extPassword']."&login=1&return=".$return);
+				exit();
+			}
 			header("Location: index.php?section=myPluto");
 			exit();
 		} else {
@@ -67,6 +72,7 @@ if (isset($_POST['username']) && $_POST['username']!='') {
 	</script>
 	<form name="login" id="login" method="post" action="index.php">
 	<input type="hidden" name="section" value="login">
+	<input type="hidden" name="return" value="'.$return.'">
 		<table width="100%">
      		<tr>
 				<td align="left" colspan="2" class="insidetable"><img src="images/titles/login.gif" border="0"> <span class="err">'.(isset($_GET['error'])?strip_tags($_GET['error']):'').'</span></td>
