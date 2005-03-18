2bf12d02e4cf8e9243356a16b160606508321e2c
radu <radu.cx+linuxmce@gmail.com>
2005-03-18 15:10:34 +0000

    Left screen value to Dan's Designer

--- src/BootScripts/Mozilla.sh
+++ src/BootScripts/Mozilla.sh
@@ -4,12 +4,13 @@ Moz=/usr/bin/mozilla
 Rat=/usr/pluto/bin/ratpoison
 export DISPLAY=:0
 Orbiter="$1"
-URL="$2"
+Screen="$2"
+URL="$3"
 
 MozId="$($Rat -c 'windows %n %c' | grep -F Firefox-bin | head -1 | cut -d' ' -f1)"
 if [ -z "$MozId" ]; then
 	$Moz "$URL"
-	/usr/pluto/bin/MessageSend 0 "$Orbiter" 1 4 16 1927
+	/usr/pluto/bin/MessageSend 0 "$Orbiter" 1 4 16 "$Screen"
 else
 	$Rat -c "select $MozId"
 	$Moz -remote "openurl($URL)"
