27faf54beddf835186b6362913ad6c759d8c0ed4
aaron <aaron.b@plutohome.com>
2005-03-27 17:13:36 +0000

    logic flaw in determine deletions

--- src/sqlCVS/Table.cpp
+++ src/sqlCVS/Table.cpp
@@ -787,8 +787,8 @@ cout << endl;
 				cerr << "Found NULL in query: " << sSQL.str() << endl;
 				throw "Database error";
 			}
-			/** If the value of our local row[] is > than the server's vect, then we deleted some records locally */
-			while( pos<r_GetAll_psc_id.m_vectAll_psc_id.size( ) && atoi( row[0] )>r_GetAll_psc_id.m_vectAll_psc_id[pos] )
+			/** If the value of our local row[] is > than the server's vect (and not > than what we've already synced), then we deleted some records locally */
+			while( pos<r_GetAll_psc_id.m_vectAll_psc_id.size( ) && atoi( row[0] )>r_GetAll_psc_id.m_vectAll_psc_id[pos] && atoi(row[0])<=m_psc_id_last_sync )
 			{
 cout << "Deleted a local row - pos: " << pos << " size: " << r_GetAll_psc_id.m_vectAll_psc_id.size( ) << 
 	" atoi: " << atoi( row[0] ) << " comp: " << r_GetAll_psc_id.m_vectAll_psc_id[pos] << endl;
