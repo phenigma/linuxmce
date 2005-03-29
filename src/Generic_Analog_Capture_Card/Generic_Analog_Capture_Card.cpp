03dcb7ba3b9a57d41843b7daaec151ac92fa7a6f
mtoader <mtoader>
2005-03-29 12:27:17 +0000

    Readed the old revbision

---
+++ src/Generic_Analog_Capture_Card/Generic_Analog_Capture_Card.cpp
@@ -0,0 +1,450 @@
+//<-dceag-d-b->
+#include "Generic_Analog_Capture_Card.h"
+#include "DCE/Logger.h"
+#include "PlutoUtils/FileUtils.h"
+#include "PlutoUtils/StringUtils.h"
+#include "PlutoUtils/Other.h"
+
+#include <iostream>
+using namespace std;
+using namespace DCE;
+
+#include "Gen_Devices/AllCommandsRequests.h"
+//<-dceag-d-e->
+
+#include "DCERouter.h"
+#include "pluto_main/Define_DeviceData.h"
+#include "pluto_main/Define_Command.h"
+#include "pluto_main/Define_CommandParameter.h"
+#ifndef WIN32
+#include <unistd.h>
+#include <string.h>
+#else
+#include <process.h>
+#include <string.h>
+#endif
+
+//<-dceag-const-b->
+// The primary constructor when the class is created as a stand-alone device
+Generic_Analog_Capture_Card::Generic_Analog_Capture_Card(int DeviceID, string ServerAddress,bool bConnectEventHandler,bool bLocalMode,class Router *pRouter)
+	: Generic_Analog_Capture_Card_Command(DeviceID, ServerAddress,bConnectEventHandler,bLocalMode,pRouter)
+//<-dceag-const-e->
+{
+	int i;
+	FILE *fp;
+	string sLine, sLine2;
+	size_t size;
+	string sPortNumber, sPath;
+	string FilePath, sData, sRep;
+	unsigned long int isize, iPortNumber, pid;
+	
+    
+	int iVideoStandard = DATA_Get_Video_Standard();
+	int iNumberOfPorts = DATA_Get_Number_of_ports();
+	int iVideoInputType = DATA_Get_Video_Input_Type();
+
+	g_pPlutoLogger->Write(LV_STATUS, "Using Generic Analog Capture Card with parameters: VideoStandard=%d Number_of_Ports=%d Video_Input_Type=%d",iVideoStandard,iNumberOfPorts,iVideoInputType);
+	
+	g_pPlutoLogger->Write(LV_STATUS, "Writing configuration to motion.conf");
+	system("rm -f /etc/motion/*");
+
+	g_pPlutoLogger->Write(LV_STATUS, "Checking Config File");
+	if(FileUtils::FileExists("/etc/motion/motion.conf") == true) {
+		g_pPlutoLogger->Write(LV_STATUS, "Config file found. Deleting");
+		system("rm -f /etc/motion/motion.conf");
+	}
+	
+	g_pPlutoLogger->Write(LV_STATUS, "Creting new config file");
+	sLine = "/etc/motion";
+	if(FileUtils::DirExists(sLine) == false) {
+		g_pPlutoLogger->Write(LV_STATUS, "Motion package not install. Please install using \"apt-get install motion\"");
+		exit(1);
+	}
+	fp = fopen("/etc/motion/motion.conf","wt+");
+	//main config
+//	fprintf(fp,"videodevice /dev/video0\n");
+//	fprintf(fp,"input %d\n",iVideoInputType);	//0 for video/tv and 8 for usb cammera
+	fprintf(fp,"norm %d\n",iVideoStandard);	//pal/secam
+	fprintf(fp,"frequency 0\n");
+	fprintf(fp,"rotate 0\n");
+	fprintf(fp,"width 320\n");
+	fprintf(fp,"height 240\n");
+	fprintf(fp,"framerate 2\n");
+	fprintf(fp,"auto_brightness off\n");
+	fprintf(fp,"brightness 0\n");
+	fprintf(fp,"contrast 0\n");
+	fprintf(fp,"saturation 0\n");
+	fprintf(fp,"hue 0\n");
+// filters
+	fprintf(fp,"\ndespeckle EedDl \n");
+	fprintf(fp,"lightswitch 50 \n");
+	fprintf(fp,"minimum_motion_frames 5 \n");
+	fprintf(fp,"night_compensate on \n");
+	fprintf(fp,"smart_mask_speed 5\n");
+// output
+	fprintf(fp,"\nalways_changes on\n");
+	fprintf(fp,"locate on\n");
+	fprintf(fp,"post_capture 5 \n");
+	fprintf(fp,"webcam_quality 50 \n");
+	fprintf(fp,"webcam_maxrate 5\n");
+	fprintf(fp,"text_changes on\n");
+	sLine = "text_right %d-%m-%Y\\n%T";
+	fprintf(fp,"%s\n",sLine.c_str());
+//	fprintf(fp,"mail_address ...\n");
+
+
+// movies
+
+	fprintf(fp,"\nffmpeg_cap_new on\n");
+	fprintf(fp,"ffmpeg_timelapse 60\n");
+	fprintf(fp,"ffmpeg_timelapse_mode daily\n");
+	fprintf(fp,"ffmpeg_video_codec mpeg1\n");
+
+
+
+
+//snapshot config
+	fprintf(fp,"\nsnapshot_interval 60\n");
+	sLine = "snapshot_filename %Y/%m/%d/%H/%M_%S";
+	fprintf(fp,"%s\n",sLine.c_str());
+	sLine = "jpeg_filename %Y/%m/%d/%H/%M_%S";
+	fprintf(fp,"%s\n",sLine.c_str());
+	sLine = "ffmpeg_filename %Y/%m/%d/%H/%M_%S";
+	fprintf(fp,"%s\n",sLine.c_str());
+	sLine = "timelapse_filename %Y/%m/%d-timelapse";
+	fprintf(fp,"%s\n",sLine.c_str());
+
+	fprintf(fp,"\n\n");
+	//Output Directory
+	g_pPlutoLogger->Write(LV_STATUS, "Wrote default config");
+
+	for(i = 0 ; i < iNumberOfPorts+1 ; i++) {
+		fprintf(fp,"#thread /etc/motion/thread%d.conf\n",i);
+	}
+	fprintf(fp,"\n\n");
+
+	fclose(fp);
+//---------------------------------
+	for(size_t i=0;i<m_pData->m_vectDeviceData_Impl_Children.size();++i) {
+                DeviceData_Impl *pDeviceData_Impl = m_pData->m_vectDeviceData_Impl_Children[i];
+                int PK_Device = pDeviceData_Impl->m_dwPK_Device;
+                string sPort = pDeviceData_Impl->mapParameters_Find(DEVICEDATA_Port_Number_CONST);
+                string sMotion = pDeviceData_Impl->mapParameters_Find(DEVICEDATA_Motion_Option_CONST);
+       	  string sSensitivity = pDeviceData_Impl->mapParameters_Find(DEVICEDATA_Sensitivity_CONST);
+		  string sDevice =  pDeviceData_Impl->mapParameters_Find(DEVICEDATA_Device_CONST);
+		  string sDescription =  pDeviceData_Impl->m_sDescription ;
+                g_pPlutoLogger->Write(LV_STATUS, "Using child device %d from Generic Analog Capture Card ",i);
+		  
+		iPortNumber = i;
+		sPortNumber = StringUtils::itos(iPortNumber);
+		
+		sPath = "/etc/motion/thread" + StringUtils::itos(iPortNumber) + ".conf";
+		FilePath = sPath;
+		g_pPlutoLogger->Write(LV_STATUS, "Looking for camera config file");
+		if(FileUtils::FileExists(sPath) == true) {
+			g_pPlutoLogger->Write(LV_STATUS, "File found, modifing...");
+			sPath = "rm -f " + sPath;
+ 			system(sPath.c_str());
+		} else {
+			g_pPlutoLogger->Write(LV_STATUS, "Camera config file not found, writing new one");
+		}
+		fp = fopen(FilePath.c_str(),"wt+");
+		//main config
+		sLine = "videodevice /dev/video" + sDevice+ "\n";  //ADDED device parameter ...
+		fprintf(fp,sLine.c_str());
+		sLine = "input " + sPort + "\n";
+		fprintf(fp,sLine.c_str());
+
+		sLine = "800" + StringUtils::itos(iPortNumber);
+		fprintf(fp,"webcam_port %s\n",sLine.c_str());
+		sData = " ";
+		sRep = "_";
+ 		sLine = StringUtils::Replace(sDescription,sData,sRep);
+		fprintf(fp,"text_left %s\\nRoomName\n\n\n",sLine.c_str()); // Real Room Name could be added !!!!!!
+
+		//Output Directory
+		sLine = "/home/cameras/" + StringUtils::itos(PK_Device);
+		g_pPlutoLogger->Write(LV_STATUS, "Looking for camera output directory");
+		if(FileUtils::DirExists(sLine) == false) {
+			g_pPlutoLogger->Write(LV_STATUS, "Camera output directory not found, creating it");
+			FileUtils::MakeDir(sLine);
+		} else {
+			g_pPlutoLogger->Write(LV_STATUS, "Camera output found");
+		}
+		fprintf(fp,"target_dir %s\n",sLine.c_str());
+
+		//snapshot config
+		fprintf(fp,"\nsnapshot_interval 60\n");
+		sLine = "snapshot_filename %Y/%m/%d/%H/%M_%S";
+		fprintf(fp,"%s\n",sLine.c_str());
+		sLine = "jpeg_filename %Y/%m/%d/%H/%M_%S";
+		fprintf(fp,"%s\n",sLine.c_str());
+		sLine = "ffmpeg_filename %Y/%m/%d/%H/%M_%S";
+		fprintf(fp,"%s\n",sLine.c_str());
+		sLine = "timelapse_filename %Y/%m/%d-timelapse";
+		fprintf(fp,"%s\n",sLine.c_str());
+
+		fclose(fp);
+
+		g_pPlutoLogger->Write(LV_STATUS, "Reading Main configuration file of the motion server");
+		sPath = "/etc/motion/motion.conf";
+		isize = FileUtils::FileSize(sPath);
+		char *pData = new char(isize+1);
+		pData[isize] = '\0';
+		pData = FileUtils::ReadFileIntoBuffer(sPath,(size_t &)isize);
+		sLine = "#thread /etc/motion/thread" + sPortNumber + ".conf";
+		sRep = "thread /etc/motion/thread" + sPortNumber + ".conf";
+	
+		std::string std = pData;
+		std = StringUtils::Replace(std,sLine,sRep);
+
+		g_pPlutoLogger->Write(LV_STATUS, "Adding child camera %d to main config file",i);
+		strcpy(pData,std.c_str());
+		FileUtils::WriteBufferIntoFile(sPath,pData,isize-1);
+
+
+
+       }
+//---------------------------------	
+	system("rm -f motion.temp");
+	g_pPlutoLogger->Write(LV_STATUS, "Starting motion server");
+	system("/usr/bin/motion | grep Failed > motion.temp");
+	const char *ptr = FileUtils::ReadFileIntoBuffer("motion.temp",size);
+	if(strstr(ptr,"Failed") != NULL ) {
+		g_pPlutoLogger->Write(LV_STATUS, "Motion Server failed to start, exiting");
+		exit(1);
+	}
+}
+
+//<-dceag-const2-b->
+// The constructor when the class is created as an embedded instance within another stand-alone device
+Generic_Analog_Capture_Card::Generic_Analog_Capture_Card(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter)
+	: Generic_Analog_Capture_Card_Command(pPrimaryDeviceCommand, pData, pEvent, pRouter)
+//<-dceag-const2-e->
+{
+}
+
+//<-dceag-dest-b->
+Generic_Analog_Capture_Card::~Generic_Analog_Capture_Card()
+//<-dceag-dest-e->
+{
+	
+}
+
+//<-dceag-reg-b->
+// This function will only be used if this device is loaded into the DCE Router's memory space as a plug-in.  Otherwise Connect() will be called from the main()
+bool Generic_Analog_Capture_Card::Register()
+//<-dceag-reg-e->
+{
+	return Connect(PK_DeviceTemplate_get()); 
+}
+
+/*  Since several parents can share the same child class, and each has it's own implementation, the base class in Gen_Devices
+	cannot include the actual implementation.  Instead there's an extern function declared, and the actual new exists here.  You 
+	can safely remove this block (put a ! after the dceag-createinst-b block) if this device is not embedded within other devices. */
+//<-dceag-createinst-b->
+Generic_Analog_Capture_Card_Command *Create_Generic_Analog_Capture_Card(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter)
+{
+	return new Generic_Analog_Capture_Card(pPrimaryDeviceCommand, pData, pEvent, pRouter);
+}
+//<-dceag-createinst-e->
+
+/*
+	When you receive commands that are destined to one of your children,
+	then if that child implements DCE then there will already be a separate class
+	created for the child that will get the message.  If the child does not, then you will 
+	get all	commands for your children in ReceivedCommandForChild, where 
+	pDeviceData_Base is the child device.  If you handle the message, you 
+	should change the sCMD_Result to OK
+*/
+//<-dceag-cmdch-b->
+void Generic_Analog_Capture_Card::ReceivedCommandForChild(DeviceData_Base *pDeviceData_Base,string &sCMD_Result,Message *pMessage)
+//<-dceag-cmdch-e->
+{
+	size_t s;
+	string Command;
+	string FilePath;
+	unsigned long int iPID;
+
+	g_pPlutoLogger->Write(LV_STATUS, "Command %d received for child.", pMessage->m_dwID);
+	
+	
+	// find child device
+	DeviceData_Impl* pDeviceData_Impl = NULL;
+	
+	g_pPlutoLogger->Write(LV_STATUS, "Finding child device...");
+
+	VectDeviceData_Impl& vDeviceData = m_pData->m_vectDeviceData_Impl_Children;
+	for(VectDeviceData_Impl::size_type i = 0; i < vDeviceData.size(); i++) {
+		if(vDeviceData[i]->m_dwPK_Device == pMessage->m_dwPK_Device_To) {
+			pDeviceData_Impl = vDeviceData[i];
+			break;
+		}
+	}
+	
+	if(!pDeviceData_Impl) {
+		g_pPlutoLogger->Write(LV_CRITICAL, "Child device %d not found.", pMessage->m_dwPK_Device_To);
+		return;
+	}
+	
+	g_pPlutoLogger->Write(LV_STATUS, "Child device %d found.", pMessage->m_dwPK_Device_To);
+
+	switch(pMessage->m_dwID) {
+		case COMMAND_Get_Video_Frame_CONST: {
+			DeviceData_Router *pDeviceData = find_Device(pMessage->m_dwPK_Device_To);
+ 			if(!pDeviceData) {
+				g_pPlutoLogger->Write(LV_CRITICAL, "No device found with id: %d", pMessage->m_dwPK_Device_To);
+				return;
+			}
+			string sPortNumber = pDeviceData->mapParameters_Find(DEVICEDATA_Port_Number_CONST);
+			char *pData = pMessage->m_mapData_Parameters[COMMANDPARAMETER_Data_CONST];
+
+			g_pPlutoLogger->Write(LV_STATUS, "Geting PID of motion server");
+			Command = "ps -e | grep motion | awk '{print $1}' > camera_card.temp";
+			system(Command.c_str());
+			const char *ptr = FileUtils::ReadFileIntoBuffer("camera_card.temp",s);
+			
+			g_pPlutoLogger->Write(LV_STATUS, "Taking Snapshot");
+
+			iPID = atoi(ptr);
+			Command = "kill -s SIGALRM " + StringUtils::itos(iPID);		
+			system(Command.c_str());
+
+			g_pPlutoLogger->Write(LV_STATUS, "Reading and sending snapshot picture");
+
+			FilePath = "/home/cameras/" + StringUtils::itos(pMessage->m_dwPK_Device_To) + "/lastsnap.jpg";
+			pData = FileUtils::ReadFileIntoBuffer(FilePath, s);
+			}
+			break;
+		default:
+			g_pPlutoLogger->Write(LV_CRITICAL, "Unknown command %d received.", pMessage->m_dwID);
+	}
+	sCMD_Result = "OK";
+}
+
+/*
+	When you received a valid command, but it wasn't for one of your children,
+	then ReceivedUnknownCommand gets called.  If you handle the message, you 
+	should change the sCMD_Result to OK
+*/
+//<-dceag-cmduk-b->
+void Generic_Analog_Capture_Card::ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage)
+//<-dceag-cmduk-e->
+{
+	sCMD_Result = "UNKNOWN DEVICE";
+}
+
+//<-dceag-sample-b->
+/*		**** SAMPLE ILLUSTRATING HOW TO USE THE BASE CLASSES ****
+
+**** IF YOU DON'T WANT DCEGENERATOR TO KEEP PUTTING THIS AUTO-GENERATED SECTION ****
+**** ADD AN ! AFTER THE BEGINNING OF THE AUTO-GENERATE TAG, LIKE //<=dceag-sample-b->! ****
+Without the !, everything between <=dceag-sometag-b-> and <=dceag-sometag-e->
+will be replaced by DCEGenerator each time it is run with the normal merge selection.
+The above blocks are actually <- not <=.  We don't want a substitution here
+
+void Generic_Analog_Capture_Card::SomeFunction()
+{
+	// If this is going to be loaded into the router as a plug-in, you can implement: 	virtual bool Register();
+	// to do all your registration, such as creating message interceptors
+
+	// If you use an IDE with auto-complete, after you type DCE:: it should give you a list of all
+	// commands and requests, including the parameters.  See "AllCommandsRequests.h"
+
+	// Examples:
+	
+	// Send a specific the "CMD_Simulate_Mouse_Click" command, which takes an X and Y parameter.  We'll use 55,77 for X and Y.
+	DCE::CMD_Simulate_Mouse_Click CMD_Simulate_Mouse_Click(m_dwPK_Device,OrbiterID,55,77);
+	SendCommand(CMD_Simulate_Mouse_Click);
+
+	// Send the message to orbiters 32898 and 27283 (ie a device list, hence the _DL)
+	// And we want a response, which will be "OK" if the command was successfull
+	string sResponse;
+	DCE::CMD_Simulate_Mouse_Click_DL CMD_Simulate_Mouse_Click_DL(m_dwPK_Device,"32898,27283",55,77)
+	SendCommand(CMD_Simulate_Mouse_Click_DL,&sResponse);
+
+	// Send the message to all orbiters within the house, which is all devices with the category DEVICECATEGORY_Orbiter_CONST (see pluto_main/Define_DeviceCategory.h)
+	// Note the _Cat for category
+	DCE::CMD_Simulate_Mouse_Click_Cat CMD_Simulate_Mouse_Click_Cat(m_dwPK_Device,DEVICECATEGORY_Orbiter_CONST,true,BL_SameHouse,55,77)
+    SendCommand(CMD_Simulate_Mouse_Click_Cat);
+
+	// Send the message to all "DeviceTemplate_Orbiter_CONST" devices within the room (see pluto_main/Define_DeviceTemplate.h)
+	// Note the _DT.
+	DCE::CMD_Simulate_Mouse_Click_DT CMD_Simulate_Mouse_Click_DT(m_dwPK_Device,DeviceTemplate_Orbiter_CONST,true,BL_SameRoom,55,77);
+	SendCommand(CMD_Simulate_Mouse_Click_DT);
+
+	// This command has a normal string parameter, but also an int as an out parameter
+	int iValue;
+	DCE::CMD_Get_Signal_Strength CMD_Get_Signal_Strength(m_dwDeviceID, DestDevice, sMac_address,&iValue);
+	// This send command will wait for the destination device to respond since there is
+	// an out parameter
+	SendCommand(CMD_Get_Signal_Strength);  
+
+	// This time we don't care about the out parameter.  We just want the command to 
+	// get through, and don't want to wait for the round trip.  The out parameter, iValue,
+	// will not get set
+	SendCommandNoResponse(CMD_Get_Signal_Strength);  
+
+	// This command has an out parameter of a data block.  Any parameter that is a binary
+	// data block is a pair of int and char *
+	// We'll also want to see the response, so we'll pass a string for that too
+
+	int iFileSize;
+	char *pFileContents
+	string sResponse;
+	DCE::CMD_Request_File CMD_Request_File(m_dwDeviceID, DestDevice, "filename",&pFileContents,&iFileSize,&sResponse);
+	SendCommand(CMD_Request_File);
+
+	// If the device processed the command (in this case retrieved the file),
+	// sResponse will be "OK", and iFileSize will be the size of the file
+	// and pFileContents will be the file contents.  **NOTE**  We are responsible
+	// free deleting pFileContents.
+
+
+	// To access our data and events below, you can type this-> if your IDE supports auto complete to see all the data and events you can access
+
+	// Get our IP address from our data
+	string sIP = DATA_Get_IP_Address();
+
+	// Set our data "Filename" to "myfile"
+	DATA_Set_Filename("myfile");
+
+	// Fire the "Finished with file" event, which takes no parameters
+	EVENT_Finished_with_file();
+	// Fire the "Touch or click" which takes an X and Y parameter
+	EVENT_Touch_or_click(10,150);
+}
+*/
+//<-dceag-sample-e->
+
+/*
+
+	COMMANDS TO IMPLEMENT
+
+*/
+
+DeviceData_Router* Generic_Analog_Capture_Card::find_Device(int iPK_Device) {
+    /*search device by id*/
+	return m_pRouter->m_mapDeviceData_Router_Find(iPK_Device);
+}
+//<-dceag-c84-b->
+
+	/** @brief COMMAND: #84 - Get Video Frame */
+	/** Get's a picture from a specified surveilance camera */
+		/** @param #19 Data */
+			/** The video frame */
+		/** @param #20 Format */
+			/** Format of the frame */
+		/** @param #23 Disable Aspect Lock */
+			/** Disable Aspect Ratio */
+		/** @param #41 StreamID */
+			/** The ID of the stream */
+		/** @param #60 Width */
+			/** Frame width */
+		/** @param #61 Height */
+			/** Frame height */
+
+void Generic_Analog_Capture_Card::CMD_Get_Video_Frame(string sDisable_Aspect_Lock,int iStreamID,int iWidth,int iHeight,char **pData,int *iData_Size,string *sFormat,string &sCMD_Result,Message *pMessage)
+//<-dceag-c84-e->
+{
+}
