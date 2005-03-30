c8c91493a0910588bb3c126a2d8934bba2fba167
radu <radu.cx+linuxmce@gmail.com>
2005-03-30 12:14:36 +0000

    postinstall: removed unused legacy function alsaconf-noninteractive: if asound.state exists, restore settings from there instead of setting any default mixer volumes

--- src/Xine_Player/mkr_postinst.sh
+++ src/Xine_Player/mkr_postinst.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-NoSound()
-{
-	[ "$1" == "0" ] && return 0
-	echo "It seems you don't have a (auto-configurable) sound card. You won't be able to play any sound."
-	return 0
-}
-
 /usr/pluto/bin/alsaconf-noninteractive
 
 video_driver=xv;
