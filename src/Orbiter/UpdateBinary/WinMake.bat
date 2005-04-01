e7643afb0d978afa507d4efe946cc4bcba2c02af
cmiron <cmiron>
2005-04-01 11:05:45 +0000

    removed a hack

--- src/Orbiter/UpdateBinary/WinMake.bat
+++ src/Orbiter/UpdateBinary/WinMake.bat
@@ -1,3 +1,1 @@
-rem HACK!!! unable to build release version
-devenv.exe /build debug UpdateBinary.vcproj /out %LOG_PATH%\Build.log 
-rem devenv.exe /build %CONFIGURATION% UpdateBinary.vcproj /out %LOG_PATH%\Build.log 
+devenv.exe /build %CONFIGURATION% UpdateBinary.vcproj /out %LOG_PATH%\Build.log 
