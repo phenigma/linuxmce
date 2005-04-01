9a39c639c1b84e1d7dba706c85d4e5fbacdcef4b
cmiron <cmiron>
2005-04-01 12:21:49 +0000

    Modified windows makerelease scripts

--- src/MakeRelease_All.bat
+++ src/MakeRelease_All.bat
@@ -9,24 +9,23 @@ svn co http://10.0.0.170/pluto/trunk/src /pluto.makerelease/trunk/src
 svn co http://10.0.0.170/pluto/trunk/libs /pluto.makerelease/trunk/libs
 svn co http://10.0.0.170/pluto/trunk/installers/Installers /pluto.makerelease/trunk/installers/Installers
 
-cd "c:\pluto.makerelease\trunk\src"
-
 echo Building pluto(windows ce) sources...
+cd "c:\pluto.makerelease\trunk\src"
 call "MakeRelease_Symbian_S60.bat"
 
 echo Building pluto(windows) sources...
+cd "c:\pluto.makerelease\trunk\src"
 call "MakeRelease_Windows.bat"
 
 echo Building pluto(symbian) sources...
+cd "c:\pluto.makerelease\trunk\src"
 call "MakeRelease_WindowsCE.bat"
-
-rem echo Building pluto(windows) sources, release version... (for now, only Orbiter)
-rem call "MakeRelease_Windows_Release.bat"
 
 echo Misc actions...
 cd "c:\pluto.makerelease\trunk\src\bin"
 copy OrbiterCE.exe "c:\pluto.makerelease\trunk\src\release"
 copy OrbiterCE_SDL.exe "c:\pluto.makerelease\trunk\src\release"
+copy UpdateBinaryCE.exe "c:\pluto.makerelease\trunk\src\release"
 copy *.sis "c:\pluto.makerelease\trunk\src\release"
 
 cd "c:\pluto.makerelease\trunk\src\release"
@@ -34,13 +33,13 @@ copy Orbiter.exe Orbiter_Win32.dat
 copy OrbiterCE.exe Orbiter_WinCE.dat
 
 echo Copying EXE files...
-copy *.exe "\\10.0.0.150\builds\Windows_Output\src\release"
+copy *.exe "\\10.0.0.150\builds\Windows_Output\src\bin"
 echo Copying DLL files...
-copy *.dll "\\10.0.0.150\builds\Windows_Output\src\release"
+copy *.dll "\\10.0.0.150\builds\Windows_Output\src\bin"
 echo Copying DAT files...
-copy *.dat "\\10.0.0.150\builds\Windows_Output\src\release"
+copy *.dat "\\10.0.0.150\builds\Windows_Output\src\bin"
 echo Copying SIS files...
-copy *.sis "\\10.0.0.150\builds\Windows_Output\src\release"
+copy *.sis "\\10.0.0.150\builds\Windows_Output\src\bin"
 
 echo Copying LIB files...
 cd "c:\pluto.makerelease\trunk\src\lib"
@@ -48,6 +47,6 @@ copy *.lib "\\10.0.0.150\builds\Windows_Output\src\lib"
 
 echo Copying MSI files...
 cd C:\pluto.makerelease\trunk\installers\Installers\Orbiter\Release\
-copy *.msi "\\10.0.0.150\builds\Windows_Output\src\release"
+copy *.msi "\\10.0.0.150\builds\Windows_Output\src\bin"
 
 
