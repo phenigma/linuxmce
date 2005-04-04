297f21910ad8722df25743e731e8c66a8f3de95b
vali <vali>
2005-04-04 11:06:01 +0000

    fixed database configuration file to create new connections instead of using existing one

--- web/pluto-support/include/config/database.inc.php
+++ web/pluto-support/include/config/database.inc.php
@@ -7,11 +7,17 @@
  
   $ADODB_COUNTRECS=true; //for speed up, set it to false, but it will return -1 for RecordCount()
   
-  $dsn = $dbPlutoMainType.'://'.urlencode($dbPlutoMainUser).':'.urlencode($dbPlutoMainPass).'@'.$dbPlutoMainServer.'/'.urlencode($dbPlutoMainDatabase); 
-  $dbADO = &ADONewConnection($dsn);
+  // $dsn = $dbPlutoMainType.'://'.urlencode($dbPlutoMainUser).':'.urlencode($dbPlutoMainPass).'@'.$dbPlutoMainServer.'/'.urlencode($dbPlutoMainDatabase); 
+  // $dbADO = &ADONewConnection($dsn);
 
-  $webdsn = $dbType.'://'.urlencode($dbUser).':'.urlencode($dbPass).'@'.$dbServer.'/'.urlencode($dbDatabase); 
-  $webADO = &ADONewConnection($webdsn);
+  // $webdsn = $dbType.'://'.urlencode($dbUser).':'.urlencode($dbPass).'@'.$dbServer.'/'.urlencode($dbDatabase); 
+  // $webADO = &ADONewConnection($webdsn);
+
+  $dbADO = &ADONewConnection('mysql');
+  $dbADO->NConnect($dbPlutoMainServer,urlencode($dbPlutoMainUser),urlencode($dbPlutoMainPass),urlencode($dbPlutoMainDatabase));
+  
+  $webADO = &ADONewConnection('mysql'); 
+  $webADO->NConnect($dbServer,urlencode($dbUser),urlencode($dbPass),urlencode($dbDatabase));
 
   if (!$dbADO) {
 	die("Connection failed");
