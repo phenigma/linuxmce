f2a9b389ff6510e010971860ab082a8ff58aff4c
aaron <aaron.b@plutohome.com>
2005-04-05 12:45:46 +0000

    cl:pluto:Updated the message send utility to allow for out parameters and delivery confirmation

--- src/MessageSend/MessageSend.cpp
+++ src/MessageSend/MessageSend.cpp
@@ -33,42 +33,55 @@ int main(int argc, char *argv[])
 
     if (argc<6)
     {
-		cout << "Usage: MessageSendTest server [-targetType [device|category|template]] DeviceFrom DeviceTo MsgType(1=Command, 2=Event) MsgID [parm1id param1value] [parm2id parm2value] ..." << endl;
-		cout << "\t Important:" << endl;
-		cout << "\t\t- you can prefix parameter value with \"@filename.ext\" to insert a data parameter from a file" << endl;
-		cout << "\t\t- the default target type is the device." << endl;
-        return 1;
-    }
-    if (((argc) % 2) != 0)
-    {
-        printf("Parameters must be paired with an ID\n");
-        return 1;
-    }
+		cout << "Usage: MessageSendTest server [-targetType [device|category|template]] [-r | -o] DeviceFrom DeviceTo MsgType(1=Command, 2=Event) MsgID [parm1id param1value] [parm2id parm2value] ..." << endl
+			<< "\tthe default target type is the device." << endl
+			<< "\t-r means the message will be sent with a response request" << endl
+			<< "\t-o means the message will expect out parameters" << endl
+			<< "\t\twhich will be echoed to the screen as resp: [response]" << endl
+			<< "\tthe parm ID can be prefixed with a letter:" << endl
+			<< "\t\tD send as a data paramter, rather than text" << endl
+			<< "\t\tB the value is a filename, the contents sent as a data parameter" << endl
+			<< "\t\tT the value is a filename, the contents sent as a text parameter" << endl;
 
-    string ServerAddress=argv[1];
+        return 1;
+    }
 
     g_pPlutoLogger = new FileLogger(stdout);
     if (g_pPlutoLogger == NULL)
         fprintf(stderr,"Problem creating logger.  Check params.\n");
 
-    Event_Impl *pEvent = new Event_Impl(DEVICEID_MESSAGESEND, 0, ServerAddress);
-
-	int baseMessageSpecPos = 2;
+	int baseMessageSpecPos = 1;
 	int targetType = 0; // Device;
-	if ( strcmp(argv[2], "-targetType") == 0 )
+	bool bResponseRequired=false,bOutParams=false;
+	while( argv[baseMessageSpecPos][0]=='-' )
 	{
-		baseMessageSpecPos += 2;
-		if ( strcmp(argv[3], "category") == 0 )
-			targetType = 1;
-		else if ( strcmp(argv[3], "template") == 0 )
-			targetType = 2;
+		if ( strcmp(argv[baseMessageSpecPos], "-targetType") == 0 )
+		{
+			if ( strcmp(argv[baseMessageSpecPos+1], "category") == 0 )
+				targetType = 1;
+			else if ( strcmp(argv[baseMessageSpecPos+1], "template") == 0 )
+				targetType = 2;
+			baseMessageSpecPos += 2;
+		}
+		else if ( strcmp(argv[baseMessageSpecPos], "-r") == 0 )
+		{
+			bResponseRequired=true;
+			baseMessageSpecPos++;
+		}
+		else if ( strcmp(argv[baseMessageSpecPos], "-o") == 0 )
+		{
+			bOutParams=true;
+			baseMessageSpecPos++;
+		}
 	}
 
-    int msgFrom=atoi(argv[baseMessageSpecPos + 0]);
-	int msgTo=atoi(argv[baseMessageSpecPos + 1]);
-	int msgType=atoi(argv[baseMessageSpecPos + 2]);
-	int msgID=atoi(argv[baseMessageSpecPos + 3]);
+    string ServerAddress=argv[baseMessageSpecPos + 0];
+    int msgFrom=atoi(argv[baseMessageSpecPos + 1]);
+	int msgTo=atoi(argv[baseMessageSpecPos + 2]);
+	int msgType=atoi(argv[baseMessageSpecPos + 3]);
+	int msgID=atoi(argv[baseMessageSpecPos + 4]);
 
+	Event_Impl *pEvent = new Event_Impl(DEVICEID_MESSAGESEND, 0, ServerAddress);
     Message *pMsg;
 
 	switch ( targetType )
@@ -90,38 +103,106 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-    for(int i=baseMessageSpecPos + 4; i<argc; i+=2)
+    for(int i=baseMessageSpecPos + 5; i<argc; i+=2)
     {
-        char *pParam = argv[i + 1];
-        int ParamNum = atoi(argv[i]);
-        if (pParam[0]=='@')
-        {
-            if (ParamNum < 0)
-                ParamNum = -ParamNum;
+		enum { ptNormal, ptData, ptBinary, ptText, ptOut } eType = ptNormal;
 
-            FILE *f = fopen(pParam+1, "rb");
-            if (f)
-            {
-                fseek(f, 0, SEEK_END);
-                size_t length = ftell(f);
-                fseek(f, 0, SEEK_SET);
-                char *pData = new char[length+1];
-                fread(pData, length, 1, f);
-                fclose(f);
-                pMsg->m_mapData_Lengths[ParamNum]=(int)length;
-                pMsg->m_mapData_Parameters[ParamNum]=pData;
-            }
-            else
-            {
-                printf("\nWarning: Could not open file %s.\n", pParam+1);
-            }
-        }
-        else
-        {
-            pMsg->m_mapParameters[ParamNum] = string(pParam);
-        }
+        char *pParamID = argv[i];
+		if( pParamID[0]=='D' )
+		{
+			pParamID++;
+			eType=ptData;
+		}
+		else if( pParamID[0]=='B' )
+		{
+			pParamID++;
+			eType=ptBinary;
+		}
+		else if( pParamID[0]=='T' )
+		{
+			pParamID++;
+			eType=ptText;
+		}
+		int ParamNum = atoi(pParamID);
+
+		size_t tSizeParmValue = 0;
+		char *pParmValue = argv[i+1];
+		if( eType==ptBinary || eType==ptText )
+		{
+			pParmValue = FileUtils::ReadFileIntoBuffer( pParmValue, tSizeParmValue );
+			if( !pParmValue )
+			{
+				cerr << "Cannot read file: " << pParmValue << endl;
+				exit(1);
+			}
+		}
+		else if( eType==ptData )
+			tSizeParmValue = strlen( pParmValue );
+
+		if( !pParmValue )
+		{
+			cerr << "Bad value for parameter ID: " << ParamNum << endl;
+			return -1;
+		}
+		if( eType==ptData || eType==ptBinary )
+		{
+			pMsg->m_mapData_Parameters[ParamNum] = pParmValue;
+			pMsg->m_mapData_Lengths[ParamNum] = tSizeParmValue;
+		}
+		else
+			pMsg->m_mapParameters[ParamNum] = pParmValue;
     }
-    pEvent->SendMessage(pMsg);
+
+	// We need a response.  It will be a string if there are no out parameters
+	if( !bOutParams )
+	{
+		if( bResponseRequired )
+			pMsg->m_eExpectedResponse = ER_DeliveryConfirmation;  // i.e. just an "OK"
+		string sResponse; // We'll use this only if a response wasn't passed in
+		bool bResult;
+		
+		if( bResponseRequired )
+			bResult = pEvent->SendMessage( pMsg, sResponse );
+		else
+			bResult = pEvent->SendMessage( pMsg );
+
+		if( !bResponseRequired )
+		{
+			if( !bResult )
+				cerr << "Failed to send" << endl;
+			return bResult ? 0 : -1;
+		}
+		cout << "RESP: " << sResponse << endl;
+		return -1;
+	}
+
+	// There are out parameters, we need to get a message back in return
+	pMsg->m_eExpectedResponse = ER_ReplyMessage;
+	Message *pResponse = pEvent->SendReceiveMessage( pMsg );
+	if( !pResponse || pResponse->m_dwID != 0 )
+	{
+		if(pResponse)
+			delete pResponse;
+
+		cerr << "Failed to send message" << endl;
+		return -1;
+	}
+
+	for( map<long, string>::iterator it=pResponse->m_mapParameters.begin();it!=pResponse->m_mapParameters.end();++it)
+	{
+		cout << (*it).first << ":" << (*it).second << endl;
+	}
+
+	for( map<long, char *>::iterator it=pResponse->m_mapData_Parameters.begin();it!=pResponse->m_mapData_Parameters.end();++it)
+	{
+		cout << "D" << (*it).first << ":" << (*it).second << ":" << (*it).first << ".out" << endl;
+		FILE *file = fopen( (StringUtils::itos((*it).first) + ".out").c_str(),"wb");
+		if( file )
+		{
+			fwrite( (*it).second,pResponse->m_mapData_Lengths[(*it).first],1,file );
+			fclose(file);
+		}
+	}
 
     Sleep(50);
 #ifdef _WIN32
