89020849663d36235184ce57e34e1103578c06f6
aaron <aaron.b@plutohome.com>
2005-04-09 17:25:02 +0000

    Tweaks to website

--- web/plutohome-com/dce.php
+++ web/plutohome-com/dce.php
@@ -1,5 +1,5 @@
 <?
-Header("Location: support/index.php?section=document&docID=193");
+Header("Location: support/index.php?section=document&docID=51");
 
 print 'You should be redirected to support site. <a href="support/index.php?section=document&docID=193">Click here</a> if this doesnt\' happen.';
 ?>
