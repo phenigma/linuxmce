e2c293bddcd2a734915284c6a1175d7d43bdde78
radu <radu.cx+linuxmce@gmail.com>
2005-04-11 11:39:45 +0000

    silenced a grep

--- src/DCERouter/mkr_postinst.sh
+++ src/DCERouter/mkr_postinst.sh
@@ -3,7 +3,7 @@
 MyCnf=/etc/mysql/my.cnf
 DefTableType=innodb
 sed -i "s/^skip-networking/#skip-networking/; s/^skip-innodb/#skip-innodb/; s/^default-table-type=.*$/default-table-type=$DefTableType/" "$MyCnf"
-grep '^default-table-type=' "$MyCnf" || echo "default-table-type=$DefTableType" >>"$MyCnf"
+grep -q '^default-table-type=' "$MyCnf" || echo "default-table-type=$DefTableType" >>"$MyCnf"
 echo "GRANT ALL PRIVILEGES ON pluto_main.* to 'root'@'127.0.0.1';" | mysql
 /etc/init.d/mysql restart
 
