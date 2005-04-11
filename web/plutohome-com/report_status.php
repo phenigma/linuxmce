62e405523cde8e95223b92e66056db7ce7910237
radu <radu.cx+linuxmce@gmail.com>
2005-04-11 17:04:56 +0000

    stub: TODO

---
+++ web/plutohome-com/report_status.php
@@ -0,0 +1,8 @@
+<?
+/* TODO: report_status.php
+# Parameters: EK_Installation, EK_Device, EK_DeviceTemplate, int Uptime
+# Database: pluto_tracking
+# Table: Uptime
+# TODO: add data received
+*/
+?>
