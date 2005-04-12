4b86bc80bb08b00508a5ec84e08f2c29efc520c5
radu <radu.cx+linuxmce@gmail.com>
2005-04-12 12:00:46 +0000

    implemented

--- web/plutohome-com/report_status.php
+++ web/plutohome-com/report_status.php
@@ -1,8 +1,19 @@
 <?
-/* TODO: report_status.php
-# Parameters: EK_Installation, EK_Device, EK_DeviceTemplate, int Uptime
-# Database: pluto_tracking
-# Table: Uptime
-# TODO: add data received
-*/
+require_once("/var/www/globalconfig/globalconfig.inc.php");
+
+$esc = "mysql_real_escape_string";
+
+$EK_Installation = $esc($_REQUEST['Installation']);
+$EK_Device = $esc($_REQUEST['Device']);
+$EK_DeviceTemplate = $esc($_REQUEST['DeviceTemplate']);
+$Uptime = $esc($_REQUEST['Uptime']);
+$Orbiter = $esc($_REQUEST['Orbiter']);
+$Router = $esc($_REQUEST['Router']);
+
+$db = mysql_connect($dbServer, $dbUser, $dbPass);
+mysql_select_db("pluto_tracking");
+
+$q = "INSERT INTO Status(EK_Installation,EK_Device,EK_DeviceTemplate,Uptime,Version_Orbiter,Version_Router,Date)
+	VALUES('$EK_Installation','$EK_Device','$EK_Device','$Uptime','$Orbiter','$Router',NOW())";
+mysql_query($q);
 ?>
