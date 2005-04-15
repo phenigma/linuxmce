3e13758034ec7a94f777918922d0df33e85e174f
radu <radu.cx+linuxmce@gmail.com>
2005-04-15 10:50:26 +0000

    force confold for diskless too

--- src/BootScripts/Diskless_Setup.sh
+++ src/BootScripts/Diskless_Setup.sh
@@ -205,6 +205,7 @@ Acquire::ftp::Proxy "ftp://dcerouter:8124";
 Acquire::ftp::ProxyLogin { "USER $(SITE_USER)@$(SITE):$(SITE_PORT)"; "PASS $(SITE_PASS)"; };
 Acquire::http::Proxy::dcerouter "DIRECT";
 Apt::Cache-Limit "12582912";
+Dpkg::Options { "--force-confold"; };
 '
 
 		echo -n "$pluto_apt_conf" >$DlPath/etc/apt/apt.conf.d/30pluto
