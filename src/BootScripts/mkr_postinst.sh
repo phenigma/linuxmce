25f6abe7238ff671b54742981cf2b4bb971cf7b2
radu <radu.cx+linuxmce@gmail.com>
2005-04-18 14:14:02 +0000

    fixed proxy issue

--- src/BootScripts/mkr_postinst.sh
+++ src/BootScripts/mkr_postinst.sh
@@ -2,8 +2,8 @@ update-rc.d Startup_Scripts.sh defaults 99 10
 
 # /etc/apt/apt.conf.d/30pluto
 pluto_apt_conf='// Pluto apt conf add-on
-Acquire::http::Proxy "http://127.0.0.1:8123";
-Acquire::ftp::Proxy "ftp://127.0.0.1:8124";
+Acquire::http::Proxy "http://dcerouter:8123";
+Acquire::ftp::Proxy "ftp://dcerouter:8124";
 Acquire::ftp::ProxyLogin { "USER $(SITE_USER)@$(SITE):$(SITE_PORT)"; "PASS $(SITE_PASS)"; };
 Acquire::http::Proxy::dcerouter "DIRECT";
 Apt::Cache-Limit "12582912";
