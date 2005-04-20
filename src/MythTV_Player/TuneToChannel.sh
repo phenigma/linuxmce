6e24da1dd84e33340aa9b00df1127a3ab79843a3
mtoader <mtoader>
2005-04-20 09:37:47 +0000

    added channel to MessageSend

--- src/MythTV_Player/TuneToChannel.sh
+++ src/MythTV_Player/TuneToChannel.sh
@@ -14,6 +14,12 @@ WHERE externalcommand='$0 $1' and hostname='`hostname`'";
 CONNECTION_ID=$(echo "$QUERY" | $SQL_COMMAND);
 echo "Found this connection id: $CONNECTION_ID and channel number: $2 "; 
 
+QUERY="\
+SELECT xmltvid \
+FROM channel \
+    WHERE chanid='$2'";
 
-/usr/pluto/bin/MessageSend dcerouter 0 $1 1 187 68 $2
+XMLTV_ID=$(echo "$QUERY" | $SQL_COMMAND);
+
+/usr/pluto/bin/MessageSend dcerouter 0 $1 1 187 68 $2 39 $XMLTV_ID
 
