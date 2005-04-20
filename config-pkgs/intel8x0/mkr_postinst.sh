7c383f1ebffdcb6831456717ea9ed95dbfce2a36
mtoader <mtoader>
2005-04-20 16:25:16 +0000

    Added intel8x0 configuration files

---
+++ config-pkgs/intel8x0/mkr_postinst.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+echo "Creating /etc/asound.conf file"
+
+
+cat > /etc/asound.conf <<EOF
+# Override the default output used by ALSA.
+# If you do not override the default, your default
+# device is identical to the (unmixed) analog device
+# shown below.  If you prefer mixed and/or digital
+# output, uncomment the appropriate four lines below
+# (only one slave.pcm line).
+
+# this should be an one liner (the config script needs it that way
+# mihai.t@plutohome.com
+pcm.!default { type plug slave.pcm "dmix-analog" }
+
+#pcm.!default {
+#  type plug
+## Uncomment the following to use mixed analog by default
+#  slave.pcm "dmix-analog"
+## Uncomment the following to use unmixed digital by default
+#  slave.pcm "digital-hw"
+## Uncomment the following to use mixed digital by default
+#  slave.pcm "dmix-digital"
+#}
+
+# Alias for analog output on the nForce2 (hw:0,0)
+# - This is identical to the device named "default"--which
+# always exists and refers to hw:0,0 (unless overridden)
+# - Therefore, we can specify "hw:0,0", "default", or "analog"
+# to access analog output on the nForce2
+pcm.analog {
+ type plug
+ slave.pcm "analog-hw"
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.analog {
+ type hw
+ card 0
+}
+
+# Alias for (rate-converted) mixed analog output on the
+# nForce2 (hw:0,0)
+#  - This will accept audio input--regardless of rate--and
+# convert to the rate required for the dmix plugin
+# (in this case 48000Hz)
+pcm.mixed-analog {
+ type plug
+ slave.pcm "dmix-analog"
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.mixed-analog {
+ type hw
+ card 0
+}
+
+# Alias for (rate-converted) digital (S/PDIF) output on the
+# nForce2 (hw:0,2)
+#  - This will accept audio input--regardless of rate--and
+# convert to the rate required for the S/PDIF hardware
+# (in this case 48000Hz)
+pcm.digital {
+ type plug
+ slave.pcm "digital-hw"
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.digital {
+ type hw
+ card 0
+}
+
+# Alias for mixed (rate-converted) digital (S/PDIF) output on the
+# nForce2 (hw:0,2)
+#  - This will accept audio input--regardless of rate--and
+# convert to the rate required for the S/PDIF hardware
+# (in this case 48000Hz)
+pcm.mixed-digital {
+ type plug
+ slave.pcm "dmix-digital"
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.mixed-digital {
+ type hw
+ card 0
+}
+
+# The following devices are not useful by themselves.  They
+# require specific rates, channels, and formats.  Therefore,
+# you probably do not want to use them directly.  Instead use
+# of of the devices defined above.
+
+# Alias for analog output on the nForce2 (hw:0,0)
+# Do not use this directly--it requires specific rate,
+# channels, and format
+pcm.analog-hw {
+ type hw
+ card 0
+ # The default value for device is 0, so no need to specify
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.analog-hw {
+ type hw
+ card 0
+}
+
+# Alias for digital (S/PDIF) output on the nForce2 (hw:0,2)
+# Do not use this directly--it requires specific rate,
+# channels, and format
+pcm.digital-hw {
+ type hw
+ card 0
+ device 4
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.digital-hw {
+ type hw
+ card 0
+}
+
+# Direct software mixing plugin for analog output on
+# the nForce2 (hw:0,0)
+# Do not use this directly--it requires specific rate,
+# channels, and format
+pcm.dmix-analog {
+ type dmix
+ ipc_key 1234
+ slave {
+   pcm "analog-hw"
+   period_time 0
+   period_size 1024
+   buffer_size 4096
+   rate 48000
+ }
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.dmix-analog {
+ type hw
+ card 0
+}
+
+# Direct software mixing plugin for digital (S/PDIF) output
+# on the nForce2 (hw:0,2)
+# Do not use this directly--it requires specific rate,
+# channels, and format
+pcm.dmix-digital {
+ type dmix
+ ipc_key 1235
+ slave {
+   pcm "digital-hw"
+   period_time 0
+   period_size 1024
+   buffer_size 4096
+   rate 48000
+ }
+}
+
+# Control device (mixer, etc.) for the nForce2 card
+ctl.dmix-digital {
+ type hw
+ card 0
+}
+<<EOF
