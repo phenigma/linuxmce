3cb51b06e7c638099b2e71442e59d91514029934
radu <radu.cx+linuxmce@gmail.com>
2005-04-20 13:14:05 +0000

    mysql_real_escape_string needs a database connection to know what charset to use when escaping...

--- web/plutohome-com/report_status.php
+++ web/plutohome-com/report_status.php
@@ -3,15 +3,15 @@ require_once("/var/www/globalconfig/globalconfig.inc.php");
 
 $esc = "mysql_real_escape_string";
 
+$db = mysql_connect($dbServer, $dbUser, $dbPass, true);
+$isSel=mysql_select_db("pluto_tracking",$db);
+
 $EK_Installation = $esc($_REQUEST['Installation']);
 $EK_Device = $esc($_REQUEST['Device']);
 $EK_DeviceTemplate = $esc($_REQUEST['DeviceTemplate']);
 $Uptime = $esc($_REQUEST['Uptime']);
 $Orbiter = $esc($_REQUEST['Orbiter']);
 $Router = $esc($_REQUEST['Router']);
-
-$db = mysql_connect($dbServer, $dbUser, $dbPass, true);
-$isSel=mysql_select_db("pluto_tracking",$db);
 
 $q = "INSERT INTO Status(EK_Installation,EK_Device,EK_DeviceTemplate,Uptime,Version_Orbiter,Version_DCERouter,Date)
 	VALUES('$EK_Installation','$EK_Device','$EK_DeviceTemplate','$Uptime','$Orbiter','$Router',NOW())";
