16053af0be85f92acad9ec8ecf25d542a4284cd7
radu <radu.cx+linuxmce@gmail.com>
2005-04-21 13:45:16 +0000

    changed tunnel cron period from 10 minutes to 1 minute generalized tunnel functions active tunnel detection also by remote port

--- src/RemoteAssistance/SetupRemoteAccess.sh
+++ src/RemoteAssistance/SetupRemoteAccess.sh
@@ -3,7 +3,8 @@
 . /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/pluto.func
 
-cronEntry="*/10 * * * * root /usr/pluto/bin/SetupRemoteAccess.sh"
+cronCmd="/usr/pluto/bin/SetupRemoteAccess.sh"
+cronEntry="*/1 * * * * root $cronCmd"
 screenName="RemoteAssistance"
 
 RAKey="/usr/pluto/keys/id_dsa_remoteassistance"
@@ -12,11 +13,62 @@ RAKey="/usr/pluto/keys/id_dsa_remoteassistance"
 shopt -s nullglob
 
 screen -wipe &>/dev/null
+
 CreateTunnel()
 {
-	SSHTunnel=$(echo /var/run/screen/S-root/*${screenName}_SSH*)
-	WebTunnel=$(echo /var/run/screen/S-root/*${screenName}_Web*)
+	Suffix="$1"
+	LocalPort="$2"
+	RemotePort="$3"
 
+	Tunnel=$(echo /var/run/screen/S-root/*${screenName}_${Suffix}*)
+
+	# if this script is not in a cron job
+	if ! grep -qF "$cronCmd" /etc/crontab; then
+		# add it to be run every 10 mins
+		echo "$cronEntry" >>/etc/crontab
+		/etc/init.d/cron reload
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Added crontab entry"
+	else
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Crontab entry already present. Not adding."
+	fi
+	
+	# if tunnel is not active create it
+	if [ -z "$Tunnel" ] || ! nc -z pf.plutohome.com "$RemotePort"; then
+		screen -d -m -S "${screenName}_${Suffix}" /usr/pluto/bin/RemoteAccess_Tunnel.sh "$RemotePort" "$LocalPort" "$RAKey"
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SSH tunnel enabled."
+	else
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SSH tunnel already present. Not enabling."
+	fi
+}
+
+RemoveTunnel()
+{
+	Suffix="$1"
+	Tunnel=$(echo /var/run/screen/S-root/*${screenName}_${Suffix}*)
+
+	# remove script from cron job
+	if grep -qF "$cronCmd" /etc/crontab; then
+		grep -vF "$cronCmd" /etc/crontab >/etc/crontab.$$
+		mv /etc/crontab.$$ /etc/crontab
+		/etc/init.d/cron reload
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Crontab entry found. Removed."
+	else
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Crontab entry not found. Not removing."
+	fi
+	
+	# kill port forward if it exists
+	if [ -n "$Tunnel" ]; then
+		PID="${Tunnel##*/}"
+		PID="${PID%%.*}"
+		kill $PID
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "$Suffix tunnel found. Closed."
+	else
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "$Suffix tunnel not found. Not closing."
+	fi
+}
+
+CreateTunnels()
+{
 	# if user doesn't exist
 	if ! grep -q remote /etc/passwd; then
 		# add user "remote" w/ the password specified (var value)
@@ -28,81 +80,26 @@ CreateTunnel()
 		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "User 'remote' already exists. Not adding."
 	fi
 
-	# if this script is not in a cron job
-	if ! grep -qF "$cronEntry" /etc/crontab; then
-		# add it to be run every 10 mins
-		echo "$cronEntry" >>/etc/crontab
-		/etc/init.d/cron reload
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Added crontab entry"
-	else
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Crontab entry already present. Not adding."
-	fi
-	
-	# if tunnels are not active
-	if [ -z "$SSHTunnel" ]; then
-		# create the ssh tunnel (port 22) w/ port=PK_Installation*2+10000
-		RemotePort=$((PK_Installation*2+10000))
-		screen -d -m -S "${screenName}_SSH" /usr/pluto/bin/RemoteAccess_Tunnel.sh $RemotePort 22 "$RAKey"
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SSH tunnel enabled."
-	else
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SSH tunnel already present. Not enabling."
-	fi
-
-	if [ -z "$WebTunnel" ]; then
-		# create the http tunnel (port 80) w/ port=PK_Installation*2+10001
-		RemotePort=$((PK_Installation*2+10001))
-		screen -d -m -S "${screenName}_Web" /usr/pluto/bin/RemoteAccess_Tunnel.sh $RemotePort 80 "$RAKey"
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "HTTP tunnel enabled."
-	else
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "HTTP tunnel already present. Not enabling."
-	fi
+	CreateTunnel "SSH" 22 $((PK_Installation*2+10000))
+	CreateTunnel "Web" 80 $((PK_Installation*2+10001))
 }
 
-RemoveTunnel()
+RemoveTunnels()
 {
-	SSHTunnel=$(echo /var/run/screen/S-root/*${screenName}_SSH*)
-	WebTunnel=$(echo /var/run/screen/S-root/*${screenName}_Web*)
-
+	RemoveTunnel "SSH"
+	RemoveTunnel "Web"
+	
 	if grep -q remote /etc/passwd; then
 		# delete user "remote" if it exists
 		userdel remote || Logging "$TYPE" "$SEVERITY_WARNING" "$0" "Failed to remove user 'remote' with exit code '$?'"
 	else
 		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "User 'remote' not present. Nothing to remove"
 	fi
-	
-	# remove script from cron job
-	if grep -qF "$cronEntry" /etc/crontab; then
-		grep -vF "$cronEntry" /etc/crontab >/etc/crontab.$$
-		mv /etc/crontab.$$ /etc/crontab
-		/etc/init.d/cron reload
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Crontab entry found. Removed."
-	else
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Crontab entry not found. Not removing."
-	fi
-	
-	# kill port forwards if they exist
-	if [ -n "$SSHTunnel" ]; then
-		PID="${SSHTunnel##*/}"
-		PID="${PID%%.*}"
-		kill $PID
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SSH tunnel found. Closed."
-	else
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SSH tunnel not found. Not closing."
-	fi
-	
-	if [ -n "$WebTunnel" ]; then
-		PID="${WebTunnel##*/}"
-		PID="${PID%%.*}"
-		kill $PID
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "HTTP tunnel found. Closed."
-	else
-		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "HTTP tunnel not found. Not closing."
-	fi
 }
 
 if [ -n "$remote" ]; then
-	[ "$1" == "restart" ] && RemoveTunnel
-	CreateTunnel
+	[ "$1" == "restart" ] && RemoveTunnels
+	CreateTunnels
 else
-	RemoveTunnel
+	RemoveTunnels
 fi
