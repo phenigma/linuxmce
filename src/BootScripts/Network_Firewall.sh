6934477df90ad664fe6cb2af9b4169db64161752
radu <radu.cx+linuxmce@gmail.com>
2005-04-22 15:41:21 +0000

    fixed bugs with firewall: running it again and again appended to nat/PREROUTING (older), moved shell functions to the top (new)

--- src/BootScripts/Network_Firewall.sh
+++ src/BootScripts/Network_Firewall.sh
@@ -2,6 +2,40 @@
 
 . /usr/pluto/bin/Network_Parameters.sh
 
+OpenPort()
+{
+	local Protocol Port FilterIP
+	local FilterMsg
+	
+	Protocol="$1"
+	Port="$2"
+	FilterIP="${3:-0.0.0.0/0}"
+
+	[ "$FilterIP" != "0.0.0.0/0" ] && FilterMsg="; Limited to: $FilterIP"
+
+	echo "  Port: $Port/$Protocol$FilterMsg"
+	iptables -A INPUT -p "$Protocol" -s "$FilterIP" --dport "$Port" -j ACCEPT
+}
+
+ForwardPort()
+{
+	local Protocol ExtIP SrcPort DestIP DestPort FilterIP
+	local FilterMsg
+
+	Protocol="$1"
+	ExtIP="$2"
+	SrcPort="$3"
+	DestIP="$4"
+	DestPort="$5"
+	FilterIP="${6:-0.0.0.0/0}"
+
+	[ "$DestPort" -eq 0 ] && DestPort="$SrcPort"
+	[ "$FilterIP" != "0.0.0.0/0" ] && FilterMsg="; Limited to: $FilterIP"
+	
+	echo "  Source port: $SrcPort/$Protocol; Destination: $DestIP:$DestPort$FilterMsg"
+	iptables -t nat -A PREROUTING -p "$Protocol" -s "$FilterIP" -d "$ExtIP" --dport "$SrcPort" -j DNAT --to-destination "$DestIP:$DestPort"
+}
+
 echo "Enabling packet forwarding"
 echo 1 >/proc/sys/net/ipv4/ip_forward
 
@@ -15,6 +49,7 @@ echo "Setting up firewall"
 iptables -P INPUT DROP
 iptables -F INPUT
 iptables -t nat -F POSTROUTING
+iptables -t nat -F PREROUTING
 
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
@@ -33,24 +68,8 @@ if [ -n "$IntIP" ]; then
 fi
 
 echo "Setting up forwarded ports"
-Q="SELECT Protocol,SourcePort,SourcePortEnd,DestinationPort,DestinationIP FROM Firewall WHERE RuleType='port_forward' ORDER BY PK_Firewall"
+Q="SELECT Protocol,SourcePort,SourcePortEnd,DestinationPort,DestinationIP,SourceIP FROM Firewall WHERE RuleType='port_forward' ORDER BY PK_Firewall"
 R=$(RunSQL "$Q")
-
-ForwardPort()
-{
-	local Protocol ExtIP SrcPort DestIP DestPort
-
-	Protocol="$1"
-	ExtIP="$2"
-	SrcPort="$3"
-	DestIP="$4"
-	DestPort="$5"
-
-	[ "$DestPort" -eq 0 ] && DestPort="$SrcPort"
-	
-	echo "  Source port: $SrcPort/$Protocol; Destination: $DestIP:$DestPort"
-	iptables -t nat -A PREROUTING -p "$Protocol" -d "$ExtIP" --dport "$SrcPort" -j DNAT --to-destination "$DestIP:$DestPort"
-}
 
 for Port in $R; do
 	Protocol=$(Field 1 "$Port")
@@ -58,39 +77,31 @@ for Port in $R; do
 	SrcPortEnd=$(Field 3 "$Port")
 	DestPort=$(Field 4 "$Port")
 	DestIP=$(Field 5 "$Port")
+	SrcIP=$(Field 6 "$Port")
 
 	if [ "$SrcPortEnd" -eq 0 ]; then
 		ForwardPort "$Protocol" "$ExtIP" "$SrcPort" "$DestIP" "$DestPort"
 	else
 		DPort="$DestPort"
 		for SPort in $(seq $SrcPort $SrcPortEnd); do
-			ForwardPort "$Protocol" "$ExtIP" "$SPort" "$DestIP" "$DPort"
+			ForwardPort "$Protocol" "$ExtIP" "$SPort" "$DestIP" "$DPort" "$SrcIP"
 			: $((++DPort))
 		done
 	fi
 done
 
 echo "Opening specified ports to exterior"
-Q="SELECT Protocol,SourcePort,SourcePortEnd FROM Firewall WHERE RuleType='core_input' ORDER BY PK_Firewall"
+Q="SELECT Protocol,SourcePort,SourcePortEnd,SourceIP FROM Firewall WHERE RuleType='core_input' ORDER BY PK_Firewall"
 R=$(RunSQL "$Q")
-
-OpenPort()
-{
-	local Protocol Port
-	Protocol="$1"
-	Port="$2"
-
-	echo "  Port: $Port/$Protocol"
-	iptables -A INPUT -p "$Protocol" --dport "$Port" -j ACCEPT
-}
 
 for Port in $R; do
 	Protocol=$(Field 1 "$Port")
 	Port1=$(Field 2 "$Port")
 	Port2=$(Field 3 "$Port")
+	SrcIP=$(Field 4 "$Port")
 
 	[ "$Port2" -eq 0 ] && Port2="$Port1"
 	for FPort in $(seq $Port1 $Port2); do
-		OpenPort "$Protocol" "$FPort"
+		OpenPort "$Protocol" "$FPort" "$SrcIP"
 	done
 done
