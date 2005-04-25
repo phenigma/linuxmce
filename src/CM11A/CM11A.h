ede816cbba137041cafde5d678facc3cbe4be8cc
igor <igor>
2005-04-25 14:34:34 +0000

    removed on/off commands

--- src/CM11A/CM11A.h
+++ src/CM11A/CM11A.h
@@ -49,35 +49,6 @@ public:
 	*/
 
 
-	/** @brief COMMAND: #192 - On */
-	/**  */
-		/** @param #97 PK_Pipe */
-			/** Normally when a device is turned on all the inputs and outputs are selected automatically.  If this parameter is specified, only the settings along this pipe will be set. */
-		/** @param #98 PK_Device_Pipes */
-			/** Normally when a device is turned on the corresponding "pipes" are enabled by default. if this parameter is blank.  If this parameter is 0, no pipes will be enabled.  This can also be a comma seperated list of devices, meaning only the pipes to those devic */
-
-	virtual void CMD_On(int iPK_Pipe,string sPK_Device_Pipes) { string sCMD_Result; CMD_On(iPK_Pipe,sPK_Device_Pipes.c_str(),sCMD_Result,NULL);};
-	virtual void CMD_On(int iPK_Pipe,string sPK_Device_Pipes,string &sCMD_Result,Message *pMessage);
-
-
-	/** @brief COMMAND: #193 - Off */
-	/**  */
-		/** @param #97 PK_Pipe */
-			/** Normally when a device is turned on all the inputs and outputs are selected automatically.  If this parameter is specified, only the settings along this pipe will be set. */
-
-	virtual void CMD_Off(int iPK_Pipe) { string sCMD_Result; CMD_Off(iPK_Pipe,sCMD_Result,NULL);};
-	virtual void CMD_Off(int iPK_Pipe,string &sCMD_Result,Message *pMessage);
-
-
-	/** @brief COMMAND: #194 - Toggle Power */
-	/** Toggle Power on/off */
-		/** @param #8 On/Off */
-			/** Depending on each device On/Off can be interpreted differently, but in genereal On/Off has a value of 1 for on and 0 for Off */
-
-	virtual void CMD_Toggle_Power(string sOnOff) { string sCMD_Result; CMD_Toggle_Power(sOnOff.c_str(),sCMD_Result,NULL);};
-	virtual void CMD_Toggle_Power(string sOnOff,string &sCMD_Result,Message *pMessage);
-
-
 //<-dceag-h-e->
 
 private:
