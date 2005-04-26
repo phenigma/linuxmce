e6112275e4b15f9caa5886259550648e23653771
aaron <aaron.b@plutohome.com>
2005-04-26 17:10:29 +0000

    cleaning orbiter's for a full regen belongs in OrbiterGen's post install

--- src/OrbiterGen/mkr_postinst_cleanog.sh
+++ src/OrbiterGen/mkr_postinst_cleanog.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+echo 'DELETE FROM CachedScreens;' | mysql pluto_main || /bin/true
+echo 'UPDATE Device JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate JOIN DeviceCategory ON FK_DeviceCategory=PK_DeviceCategory SET NeedConfigure=1 WHERE FK_DeviceCategory=5 OR FK_DeviceCategory_Parent=5;' | mysql pluto_main || /bin/true
 echo 'UPDATE Orbiter set Modification_LastGen=0;' | mysql pluto_main || /bin/true
+
