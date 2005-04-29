beda829e30013993497bf3996e8b3ba435ae3bb1
radu <radu.cx+linuxmce@gmail.com>
2005-04-29 09:19:18 +0000

    Both stdout and stderr now go to the same file

--- src/ConfirmDependencies_Script/Initial_Config.sh
+++ src/ConfirmDependencies_Script/Initial_Config.sh
@@ -4,4 +4,4 @@
 Type="router"
 
 mkdir -p /var/log/pluto
-bash -x /usr/pluto/install/Initial_Config_Real.sh "$Type" 1> >(tee /var/log/pluto/Initial_Install.newlog) 2>/var/log/pluto/Initial_Install_trace.newlog
+bash -x /usr/pluto/install/Initial_Config_Real.sh "$Type" &> >(tee /var/log/pluto/Initial_Install.newlog)
