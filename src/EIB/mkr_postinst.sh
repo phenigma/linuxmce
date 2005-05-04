102228bfde5f120fa30a8dd765d0846555456370
radu <radu.cx+linuxmce@gmail.com>
2005-05-04 15:24:47 +0000

    connect to dcerouter for mysql, but still not ok; requires this to be done on the Core

--- src/EIB/mkr_postinst.sh
+++ src/EIB/mkr_postinst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'CREATE DATABASE IF NOT EXISTS `eib`;' | mysql
+echo 'CREATE DATABASE IF NOT EXISTS `eib`;' | mysql -h dcerouter
 echo 'CREATE TABLE IF NOT EXISTS `groupaddresses` (
 `name` varchar(100) default NULL,
 `address` varchar(100) NOT NULL,
@@ -9,6 +9,6 @@ PRIMARY KEY  (`address`)
 # add eib user
 adduser --system --home /var/run/eib eib || true
 
-echo "GRANT ALL PRIVILEGES ON eib.* to 'eib'@'localhost';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
+echo "GRANT ALL PRIVILEGES ON eib.* to 'eib'@'localhost';" | mysql -h dcerouter
+echo "FLUSH PRIVILEGES;" | mysql -h dcerouter
 
