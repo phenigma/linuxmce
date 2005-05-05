e1468f79890dbc69e6d49a0731dec110ec4c68f5
radu <radu.cx+linuxmce@gmail.com>
2005-05-05 11:44:44 +0000

    impemented ttyUSB

--- src/BootScripts/ListSerialPorts.sh
+++ src/BootScripts/ListSerialPorts.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# TODO: USB-to-Serial (Debian removed Keyspan firmware from the kernel sources because it violates DFSG)
-Ports=$(cat /proc/tty/driver/serial | grep -v unknown | grep ': ' | cut -d: -f1)
-for i in $Ports; do
-	echo "/dev/ttyS$i"
-done
+if [[ -f /proc/tty/driver/serial ]]; then
+	Ports=$(cat /proc/tty/driver/serial | grep -v unknown | grep ': ' | cut -d: -f1)
+	for i in $Ports; do
+		echo "/dev/ttyS$i"
+	done
+fi
+
+if [[ -f /proc/tty/driver/usbserial ]]; then
+	Ports=$(cat /proc/tty/driver/usbserial | grep ': ' | cut -d: -f1)
+	for i in $Ports; do
+		echo "/dev/ttyUSB$i"
+	done
+fi
 
 shopt -s nullglob
 for Port in /dev/ttyS_*; do
