daee9d3746038c4c33c9bf10d4a7c6a387641aba
radu <radu.cx+linuxmce@gmail.com>
2005-05-05 09:13:55 +0000

    not displaying sell trace (lines that stat with one or more plus signs); currently, I don't know any other output that falls into that category

--- src/ConfirmDependencies_Script/Initial_Config.sh
+++ src/ConfirmDependencies_Script/Initial_Config.sh
@@ -4,4 +4,5 @@
 Type="router"
 
 mkdir -p /var/log/pluto
-bash -x /usr/pluto/install/Initial_Config_Real.sh "$Type" &> >(tee /var/log/pluto/Initial_Install.newlog)
+bash -x /usr/pluto/install/Initial_Config_Real.sh "$Type" &> >(tee /var/log/pluto/Initial_Install.newlog | grep -v '^++* ')
+sleep 1 # so tee and grep close and flush their buffers
