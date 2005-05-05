a6d3d357b8847c079f36d9aa13852321dd30cba9
mtoader <mtoader>
2005-05-05 14:53:11 +0000

    cl:File_Grids_Plugin minor compiler warning cleanups

--- src/File_Grids_Plugin/FileListGrid.h
+++ src/File_Grids_Plugin/FileListGrid.h
@@ -14,7 +14,7 @@ using namespace DCE;
 class FileListInfo
 {
 public:
-	FileListInfo(bool bIsDirectory,string sPath,bool bIsBack) : m_PKID_MED_Attribute(0), m_bIsDirectory(bIsDirectory), m_sPath(sPath), m_bIsBack(bIsBack) {}
+	FileListInfo(bool bIsDirectory,string sPath,bool bIsBack) : m_PKID_MED_Attribute(0), m_bIsDirectory(bIsDirectory), m_bIsBack(bIsBack), m_sPath(sPath) {}
 	FileListInfo(int PKID_MED_Attribute) : m_PKID_MED_Attribute(PKID_MED_Attribute) {}
 	int m_PKID_MED_Attribute;
 	bool m_bIsDirectory,m_bIsBack;  // Keep track of whether this is a 'go back' or not
