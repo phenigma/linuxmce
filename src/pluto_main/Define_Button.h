0b6af144bd54e0d85c0959d0f5fd21978c4ab995
aaron <aaron.b@plutohome.com>
2005-05-12 23:16:03 +0000

    Automatic Regen

--- src/pluto_main/Define_Button.h
+++ src/pluto_main/Define_Button.h
@@ -116,7 +116,7 @@
 #define BUTTON_right_shift_CONST 169
 #define BUTTON_caps_lock_CONST 170
 #define BUTTON_both_soft_buttons_CONST 173
-#define BUTTON_Terminate_Text_CONST 174
+#define BUTTON_Terminate_Text_CONST 175
 
 #define BUTTON_TABLE "Button"
 #define BUTTON_PK_BUTTON_FIELD "PK_Button"
