c5c4971a36ff846890d6c797968a5accdda4d91b
aaron <aaron.b@plutohome.com>
2005-05-17 11:48:35 +0000

    oops

--- src/database/mkr_postinst_security.sh
+++ src/database/mkr_postinst_security.sh
@@ -6,9 +6,3 @@ echo "$Q" | /usr/bin/mysql
 Q="GRANT ALL PRIVILEGES ON pluto_security.* TO 'root'@'127.0.0.1'; FLUSH PRIVILEGES;"
 echo "$Q" | /usr/bin/mysql
 
-#temporary code because the filter wasn't set properly before
-echo "delete from Alert where psc_id is not null and psc_id>0;" | mysql pluto_security
-echo "delete from ModeChange where psc_id is not null and psc_id>0;" | mysql pluto_security
-echo "delete from Notification where psc_id is not null and psc_id>0;" | mysql pluto_security
-echo "delete from Picture where psc_id is not null and psc_id>0;" | mysql pluto_security
-
