9c35b66259aadb6eb93b31fd459625ba37302c10
radu <radu.cx+linuxmce@gmail.com>
2005-05-17 13:44:04 +0000

    MySQL settings moved to distinct package

--- src/DCERouter/mkr_postinst.sh
+++ src/DCERouter/mkr_postinst.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#enable MySQL networking
-MyCnf=/etc/mysql/my.cnf
-DefTableType=innodb
-sed -i "s/^skip-networking/#skip-networking/; s/^skip-innodb/#skip-innodb/; s/^default-table-type=.*$/default-table-type=$DefTableType/" "$MyCnf"
-grep -q '^default-table-type=' "$MyCnf" || echo "default-table-type=$DefTableType" >>"$MyCnf"
-echo "GRANT ALL PRIVILEGES ON pluto_main.* to 'root'@'127.0.0.1';" | mysql
-/etc/init.d/mysql restart
-
 . /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/SQL_Ops.sh
 
