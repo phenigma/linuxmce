3bc1e6b2cc55c740e32c4845bb60b02394e28377
aaron <aaron.b@plutohome.com>
2005-05-20 15:34:26 +0000

    oops

---
+++ src/Gen_Devices/CDDB_IdentifierBase.cpp
@@ -0,0 +1,37 @@
+#include "CDDB_IdentifierBase.h"
+#include "DeviceData_Impl.h"
+#include "Logger.h"
+
+using namespace DCE;
+#include "CDDB_IdentifierBase.h"
+extern CDDB_Identifier_Command *Create_CDDB_Identifier(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+DeviceData_Impl *CDDB_Identifier_Data::CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition)
+{
+	// Peek ahead in the stream.  We're going to pass in the above pointers anyway so it won't affect the position
+	SerializeClass b;
+	b.ResumeReadWrite(pDataBlock,AllocatedSize,CurrentPosition);
+	int iPK_Device = b.Read_unsigned_long();
+	int iPK_Installation = b.Read_unsigned_long();
+	int iPK_DeviceTemplate = b.Read_unsigned_long();
+	switch(iPK_DeviceTemplate) {
+		case 72:
+			return new CDDB_Identifier_Data();
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateData for unknown type %d.", iPK_DeviceTemplate);
+	return NULL;
+}
+
+Event_Impl *CDDB_Identifier_Event::CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice )
+{
+	switch(dwPK_DeviceTemplate) {
+		case 72:
+			return (Event_Impl *) new CDDB_Identifier_Event(pOCClientSocket, dwDevice);
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateEvent for unknown type %d.", dwPK_DeviceTemplate);
+	return NULL;
+}
+Command_Impl  *CDDB_Identifier_Command::CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent)
+{
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateCommand for unknown type %d.", PK_DeviceTemplate);
+	return NULL;
+}
---
+++ src/Gen_Devices/CDDB_IdentifierBase.h
@@ -0,0 +1,174 @@
+#ifndef CDDB_IdentifierBase_h
+#define CDDB_IdentifierBase_h
+#include "DeviceData_Impl.h"
+#include "Message.h"
+#include "Command_Impl.h"
+
+
+namespace DCE
+{
+//   OUR EVENT CLASS 
+
+class CDDB_Identifier_Event : public Event_Impl
+{
+public:
+	CDDB_Identifier_Event(int DeviceID, string ServerAddress, bool bConnectEventHandler=true) : Event_Impl(DeviceID,72, ServerAddress, bConnectEventHandler) {};
+	CDDB_Identifier_Event(class ClientSocket *pOCClientSocket, int DeviceID) : Event_Impl(pOCClientSocket, DeviceID) {};
+	//Events
+	class Event_Impl *CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice );
+};
+
+
+//   OUR DATA CLASS 
+
+class CDDB_Identifier_Data : public DeviceData_Impl
+{
+public:
+	virtual ~CDDB_Identifier_Data() {};
+	class DeviceData_Impl *CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition);
+	virtual int GetPK_DeviceList() { return 72; } ;
+	virtual const char *GetDeviceDescription() { return "CDDB_Identifier"; } ;
+};
+
+
+
+//   OUR COMMAND CLASS 
+
+class CDDB_Identifier_Command : public Command_Impl
+{
+public:
+	CDDB_Identifier_Command(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL)
+	: Command_Impl(DeviceID, ServerAddress, bLocalMode, pRouter)
+	{
+		if( m_bLocalMode )
+			return;
+		m_pData=NULL;
+		m_pEvent = new CDDB_Identifier_Event(DeviceID, ServerAddress);
+		if( m_pEvent->m_dwPK_Device )
+			m_dwPK_Device = m_pEvent->m_dwPK_Device;
+		int Size; char *pConfig = m_pEvent->GetConfig(Size);
+		if( !pConfig )
+			throw "Cannot get configuration data";
+		m_pData = new CDDB_Identifier_Data();
+		if( Size )
+			m_pData->SerializeRead(Size,pConfig);
+		delete[] pConfig;
+		pConfig = m_pEvent->GetDeviceList(Size);
+		m_pData->m_AllDevices.SerializeRead(Size,pConfig);
+		delete[] pConfig;
+		m_pData->m_pEvent_Impl = m_pEvent;
+		m_pcRequestSocket = new Event_Impl(DeviceID, 72,ServerAddress);
+	};
+	CDDB_Identifier_Command(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter) : Command_Impl(pPrimaryDeviceCommand, pData, pEvent, pRouter) {};
+	virtual ~CDDB_Identifier_Command() {};
+	CDDB_Identifier_Event *GetEvents() { return (CDDB_Identifier_Event *) m_pEvent; };
+	CDDB_Identifier_Data *GetData() { return (CDDB_Identifier_Data *) m_pData; };
+	const char *GetClassName() { return "CDDB_Identifier_Command"; };
+	virtual int PK_DeviceTemplate_get() { return 72; };
+	static int PK_DeviceTemplate_get_static() { return 72; };
+	virtual void ReceivedCommandForChild(DeviceData_Base *pDeviceData_Base,string &sCMD_Result,Message *pMessage) { };
+	virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage) { };
+	Command_Impl *CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent);
+	//Data accessors
+	//Event accessors
+	//Commands - Override these to handle commands from the server
+	virtual void CMD_Identify_Media(string sFilename,string *sID,string &sCMD_Result,class Message *pMessage) {};
+
+	//This distributes a received message to your handler.
+	virtual bool ReceivedMessage(class Message *pMessageOriginal)
+	{
+		map<long, string>::iterator itRepeat;
+		if( Command_Impl::ReceivedMessage(pMessageOriginal) )
+			return true;
+		int iHandled=0;
+		for(int s=-1;s<(int) pMessageOriginal->m_vectExtraMessages.size(); ++s)
+		{
+			Message *pMessage = s>=0 ? pMessageOriginal->m_vectExtraMessages[s] : pMessageOriginal;
+			if (pMessage->m_dwPK_Device_To==m_dwPK_Device && pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND)
+			{
+				switch(pMessage->m_dwID)
+				{
+				case 314:
+					{
+						string sCMD_Result="OK";
+					string sFilename=pMessage->m_mapParameters[13];
+					string sID=pMessage->m_mapParameters[10];
+						CMD_Identify_Media(sFilename.c_str(),&sID,sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+						pMessageOut->m_mapParameters[10]=sID;
+							pMessageOut->m_mapParameters[0]=sCMD_Result;
+							SendMessage(pMessageOut);
+						}
+						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							SendString(sCMD_Result);
+						}
+						if( (itRepeat=pMessage->m_mapParameters.find(72))!=pMessage->m_mapParameters.end() )
+						{
+							int iRepeat=atoi(pMessage->m_mapParameters[72].c_str());
+							for(int i=2;i<=iRepeat;++i)
+								CMD_Identify_Media(sFilename.c_str(),&sID,sCMD_Result,pMessage);
+						}
+					};
+					iHandled++;
+					continue;
+				}
+				iHandled += Command_Impl::ReceivedMessage(pMessage);
+			}
+			else if( pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND )
+			{
+				MapCommand_Impl::iterator it = m_mapCommandImpl_Children.find(pMessage->m_dwPK_Device_To);
+				if( it!=m_mapCommandImpl_Children.end() && !(*it).second->m_bGeneric )
+				{
+					Command_Impl *pCommand_Impl = (*it).second;
+					iHandled += pCommand_Impl->ReceivedMessage(pMessage);
+			}
+			else
+			{
+				DeviceData_Base *pDeviceData_Base = m_pData->m_AllDevices.m_mapDeviceData_Base_Find(pMessage->m_dwPK_Device_To);
+				string sCMD_Result="UNHANDLED";
+				if( pDeviceData_Base && pDeviceData_Base->IsChildOf(m_pData) )
+					ReceivedCommandForChild(pDeviceData_Base,sCMD_Result,pMessage);
+				else
+					ReceivedUnknownCommand(sCMD_Result,pMessage);
+					if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+					{
+							pMessage->m_bRespondedToMessage=true;
+						Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+						pMessageOut->m_mapParameters[0]=sCMD_Result;
+						SendMessage(pMessageOut);
+					}
+					else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+						SendString(sCMD_Result);
+						}
+					if( sCMD_Result!="UNHANDLED" )
+						iHandled++;
+				}
+			}
+			if( iHandled==0 && !pMessage->m_bRespondedToMessage &&
+			(pMessage->m_eExpectedResponse==ER_ReplyMessage || pMessage->m_eExpectedResponse==ER_ReplyString) )
+			{
+				pMessage->m_bRespondedToMessage=true;
+				if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+				{
+					Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+					pMessageOut->m_mapParameters[0]="UNHANDLED";
+					SendMessage(pMessageOut);
+				}
+				else
+					SendString("UNHANDLED");
+			}
+		}
+		return iHandled!=0;
+	}
+}; // end class
+
+
+}
+#endif
