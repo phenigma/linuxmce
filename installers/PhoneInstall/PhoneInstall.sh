608aef3da81249a44ba8c21514d6179059e49b14
radu <radu.cx+linuxmce@gmail.com>
2005-06-03 11:36:08 +0000

    log and return code according to SIS image upload status

--- installers/PhoneInstall/PhoneInstall.sh
+++ installers/PhoneInstall/PhoneInstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. /usr/pluto/bin/pluto.func
+
 #$0 <MAC> <path_to_sis>
 
 if [ "$#" -ne "2" ]; then
@@ -19,4 +21,13 @@ fi
 
 #/usr/pluto/bin/ussp-push /dev/rfcomm0 "$SIS" PlutoMO.sis
 
-(echo "c"; echo "p"; echo "$SIS PlutoMO.sis"; echo "q") | obex_test -b $MAC 9 1>/dev/null
+(echo "c"; echo "p"; echo "$SIS PlutoMO.sis"; echo "q") | obex_test -b $MAC 9 |grep -qF 'PUT successful!'
+Ret=$?
+
+if [[ "$Ret" -eq 0 ]]; then
+	Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SIS '$SIS' uploaded to '$MAC'"
+	exit 0
+else
+	Logging "$TYPE" "$SEVERITY_WARNING" "$0" "SIS '$SIS' failed to upload to '$MAC'"
+	exit 1
+fi
