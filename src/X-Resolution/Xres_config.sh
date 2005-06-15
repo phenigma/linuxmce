33a22c03e7680bc50dbadce793a1c130c3598748
radu <radu.cx+linuxmce@gmail.com>
2005-06-15 18:08:17 +0000

    added code to actually ask the user and change config, restart X with new settings, if needed

--- src/X-Resolution/Xres_config.sh
+++ src/X-Resolution/Xres_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+trap true SIGINT
+
 Width="$1"
 Height="$2"
 Refresh="$3"
@@ -16,3 +18,26 @@ awk '
 	/EndSubSection/ && Display == 1 { print "\t\tModes '"${ModeName//\"/\\\"}"'"; print; Display = 0; next; }
 	{ print }
 ' /etc/X11/XF86Config-4 >/etc/X11/XF86Config-4.test
+
+X :1 -ac -xf86config /etc/X11/XF86Config-4.test &
+sleep 1
+pidOfX="$(ps ax|grep 'X :1 -ac -xf86config /etc/X11/XF86Config-4.test'|grep -v grep|cut -f1 -d' ')"
+setting="$(xrandr -d :1 -q|grep '^\*')"
+resolution="${setting:5:11}"
+refresh="${setting:39:4}"
+DISPLAY=:1 Xdialog --default-no --yesno "Current resolution: $resolution @ $refresh Hz\nIs this OK?" 0x0
+Result=$?
+echo "Result: $Result"
+echo "Killing test X"
+kill $pidOfX
+if [[ "$Result" -eq 0 ]]; then
+	pidOfX=$(ps ax|grep 'X -ac -allowMouseOpenFail vt7'|grep -v grep|cut -f1 -d' ')
+	mv /etc/X11/XF86Config-4{.test,}
+	echo "Killing real X (to restart it with new config)"
+	kill $pidOfX
+	sleep 5
+	/usr/pluto/bin/Start_X.sh
+	/usr/pluto/bin/Start_ratpoison.sh
+else
+	rm /etc/X11/XF86Config-4.test
+fi
