1fe8f5ca41d0a6da56c715039f69687def6676ab
radu <radu.cx+linuxmce@gmail.com>
2005-06-15 08:07:25 +0000

    proxy commented out

--- src/BootScripts/mkr_postinst.sh
+++ src/BootScripts/mkr_postinst.sh
@@ -5,10 +5,10 @@ update-rc.d Startup_Scripts.sh defaults 99 10
 # /etc/apt/apt.conf.d/30pluto
 
 pluto_apt_conf='// Pluto apt conf add-on
-Acquire::http::Proxy "http://'"$DCERouter"':8123";
-Acquire::ftp::Proxy "ftp://'"$DCERouter"':8124";
-Acquire::ftp::ProxyLogin { "USER $(SITE_USER)@$(SITE):$(SITE_PORT)"; "PASS $(SITE_PASS)"; };
-Acquire::http::Proxy::dcerouter "DIRECT";
+//Acquire::http::Proxy "http://'"$DCERouter"':8123";
+//Acquire::ftp::Proxy "ftp://'"$DCERouter"':8124";
+//Acquire::ftp::ProxyLogin { "USER $(SITE_USER)@$(SITE):$(SITE_PORT)"; "PASS $(SITE_PASS)"; };
+//Acquire::http::Proxy::dcerouter "DIRECT";
 Apt::Cache-Limit "12582912";
 Dpkg::Options { "--force-confold"; };
 '
