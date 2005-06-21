020d604a574ea71129467d79d1f974c46111f1f4
cmiron <cmiron>
2005-06-21 06:38:28 +0000

    More debugging

--- src/Bluetooth_Dongle/Bluetooth_Dongle.cpp
+++ src/Bluetooth_Dongle/Bluetooth_Dongle.cpp
@@ -223,18 +223,21 @@ void *ReconnectToBluetoothDongleThread(void *p)
 	    BD_Orbiter *pBD_Orbiter = pBluetooth_Dongle->m_mapOrbiterSockets_Find(sPhoneMacAddress);
 
         //still connected ?
-        if(!pBD_Orbiter || !(pBD_Orbiter->m_pBDCommandProcessor || pBD_Orbiter->m_pOrbiter))
+        if(!pBD_Orbiter)
+            break;
+
+        if(NULL == pBD_Orbiter->m_pBDCommandProcessor && NULL == pBD_Orbiter->m_pOrbiter)
             break;
         
         bm.Release();
 
-        g_pPlutoLogger->Write(LV_WARNING, "HandleBDCommandProcessor for %s still running. I'll try again in 50 ms...", sPhoneMacAddress.c_str());
-        Sleep(50);
+        g_pPlutoLogger->Write(LV_WARNING, "HandleBDCommandProcessor for %s still running. I'll try again in 250 ms...", sPhoneMacAddress.c_str());
+        Sleep(250);
     }
 
     //BDCommandProcessor is disconnected now, let's give mobile a little time to disconnect
-    g_pPlutoLogger->Write(LV_WARNING, "Sleeping 5 sec. to give mobile a little time to disconnect...");
-    Sleep(5000); 
+    g_pPlutoLogger->Write(LV_WARNING, "Sleeping 2 sec. to give mobile a little time to disconnect...");
+    Sleep(2000); 
 
     g_pPlutoLogger->Write(LV_WARNING, "HandleBDCommandProcessor for %s BDCommandProcessor is disconnected now, we can connect to new dongle with id %d", sPhoneMacAddress.c_str(), iDeviceToLink);
     DCE::CMD_Link_with_mobile_orbiter CMD_Link_with_mobile_orbiter(iDeviceToLink, iDeviceToLink, 1, sPhoneMacAddress, sVMC_File);
