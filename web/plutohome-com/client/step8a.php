9583ff58776dc98b7f55596e8266869aa263e8c6
aaron <aaron.b@plutohome.com>
2005-06-29 16:50:01 +0000

    oops

--- web/plutohome-com/client/step8a.php
+++ web/plutohome-com/client/step8a.php
@@ -25,9 +25,6 @@ $out .= '
 		If you understand partioning, you may want to change the values.  By default it will give you an 8GB partition for the Pluto system, 
 		and the rest for your media & data files.  That way you can later re-install the Pluto system from scratch without losing your data.</p>
 		<p>If you have any problems, you can checkout Pluto\'s general support forums, or use our email or live chat support.</p>
-		<p><b>IMPORTANT NOTE:</b> During installation you may see a warning that starts with "You are attempting to install a kernel version that is the same as the version..." 
-			followed by a lot of text, and then "Please Hit return to continue".
-			The warning is harmless and will be supressed in future releases.</p>
 		<p><b><a href="http://plutohome.com/download/cds/pldebsrg.iso" onClick="self.location=\'index.php?section=wizard&action=kick-start%20cd&step=8\';">DOWNLOAD KICK-START CD</a></b>  Alternatively get it by anonymous ftp at ftp.plutohome.com directory: "cds" file: "pldebsrg.iso"</p>
 		</td>
 	</tr>
