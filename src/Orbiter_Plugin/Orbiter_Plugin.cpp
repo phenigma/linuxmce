ac74e69d036814797159a4211dfa3d50d4cdacba
cmiron <cmiron>
2005-07-04 11:39:53 +0000

    Update PlutoMO version.

--- src/Orbiter_Plugin/Orbiter_Plugin.cpp
+++ src/Orbiter_Plugin/Orbiter_Plugin.cpp
@@ -72,7 +72,7 @@ using namespace DCE;
 #include <algorithm>
 
 #define VERSION "<=version=>"
-string g_sLatestMobilePhoneVersion="2005.06.21";
+string g_sLatestMobilePhoneVersion="2005.07.04";
 
 #include "../Media_Plugin/EntertainArea.h"
 #include "../Media_Plugin/Media_Plugin.h"
