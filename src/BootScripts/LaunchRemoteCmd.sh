0c135acc0c6e4cccd4a8fceea56582ffeee238e9
radu <radu.cx+linuxmce@gmail.com>
2005-07-05 15:18:11 +0000

    use ssh on localhost too; needed for processes that hang the terminal, since I can kill the ssh server after I'm done :)

--- src/BootScripts/LaunchRemoteCmd.sh
+++ src/BootScripts/LaunchRemoteCmd.sh
@@ -9,17 +9,13 @@ date -R >>"$Log"
 echo "IP: $IP ; Cmd: $Cmd" >>"$Log"
 
 if [ -n "$IP" -a "$#" -ne 0 ]; then
-	if [ "$IP" != "127.0.0.1" ]; then
-		Output="$(ssh -l root -i /usr/pluto/keys/id_dsa_pluto_apache "$IP" "$Cmd; echo \$?; sleep 1; kill \$PPID" > >(tee -a "$Log") 2> >(cat >>"$Log"))"
-		Ret="$(echo "$Output" | tail -1)"
-		
-		[[ $Ret -ne 0 && $Ret -ne 10 ]] && echo "Failed to contact destination computer (IP: $IP)" && exit 1
-		Lines=$(echo "$Output" | wc -l | awk '{print $1}')
-		Output="$(echo "$Output" | awk 'NR < '$Lines' {print}')"
-		echo "$Output"
-	else
-		eval "$Cmd"
-	fi
+	Output="$(ssh -l root -i /usr/pluto/keys/id_dsa_pluto_apache "$IP" "$Cmd; echo \$?; sleep 1; kill \$PPID" > >(tee -a "$Log") 2> >(cat >>"$Log"))"
+	Ret="$(echo "$Output" | tail -1)"
+	
+	[[ $Ret -ne 0 && $Ret -ne 10 ]] && echo "Failed to contact destination computer (IP: $IP)" && exit 1
+	Lines=$(echo "$Output" | wc -l | awk '{print $1}')
+	Output="$(echo "$Output" | awk 'NR < '$Lines' {print}')"
+	echo "$Output"
 else
 	Msg=""
 	[ -z "$IP" ] && Msg="${Msg}Empty IP address, "
