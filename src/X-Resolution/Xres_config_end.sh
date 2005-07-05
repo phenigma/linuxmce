36afef35bf6fbc6bf56d99163ac63a19b1fb17a3
radu <radu.cx+linuxmce@gmail.com>
2005-07-05 10:08:03 +0000

    tell devices to reload before X is restarted

--- src/X-Resolution/Xres_config_end.sh
+++ src/X-Resolution/Xres_config_end.sh
@@ -2,6 +2,15 @@
 
 Answer="$1"
 
+. /usr/pluto/bin/Config_Ops.sh
+
+TellToReload()
+{
+	for Dev in $(cat /usr/pluto/locks/pluto_spawned_local_devices.txt); do
+		/usr/pluto/bin/MessageSend "$DCERouter" 0 "$Dev" 7 1
+	done
+}
+
 pidOfX="$(ps ax|grep 'X :1 -ac -xf86config /etc/X11/XF86Config-4.test'|grep -v grep|awk '{print $1}')"
 if [[ -z "$pidOfX" ]]; then
 	echo "test X not running. nothing to kill"
@@ -15,6 +24,9 @@ echo "Killing test X"
 kill $pidOfX
 
 if [[ "${Answer:0:1}" == "y" || "${Answer:0:1}" == "Y" ]]; then
+	echo "X reload flag and message"
+	TellToReload
+	
 	pidOfX=$(ps ax|grep 'X -ac -allowMouseOpenFail vt7'|egrep -v 'grep|SCREEN'|awk '{print $1}')
 	mv /etc/X11/XF86Config-4{.test,}
 	echo "Killing real X (to restart it with new config)"
