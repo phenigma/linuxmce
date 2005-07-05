2dc983e5f663beb5ee77a23c19dd72efd33968f4
radu <radu.cx+linuxmce@gmail.com>
2005-07-05 17:28:13 +0000

    InstallNewDevice for CreateDevice

---
+++ src/BootScripts/InstallNewDevice.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+exec >>/var/log/pluto/InstallNewDevice.newlog 2>&1
+
+Device="$1"
+Package="$2"
+
+TryInstall()
+{
+	error=1
+	retries=0
+	while [[ $retries < 10 && $error != 0 ]]; do
+		apt-get -y install "$Package"
+		error=$?
+	done
+
+	if [[ $retries == 10 ]]; then
+		echo "All retries failed"
+	else
+		echo "Package installed successfully"
+	fi
+
+	rm /usr/pluto/locks/installing."$Device"
+}
+
+if [[ -z "$Device" || -z "$Package" ]]; then
+	echo "Bad parameters: '$Device', '$Package'"
+	exit 1
+fi
+
+touch /usr/pluto/locks/installing."$Device"
+TryInstall &
+disown -a
