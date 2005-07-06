f8047324b1ccba87d442a2eb32d8a478b7ad9443
aaron <aaron.b@plutohome.com>
2005-07-06 15:49:18 +0000

    cl:orbiter:You can now use one orbiter as a remote keyboard for a media director

---
+++ src/PlutoUtils/PlutoButtonsToX.h
@@ -0,0 +1,16 @@
+#ifndef PlutoButtonsToX
+#define PlutoButtonsToX
+
+#include "pluto_main/Define_Buttons.h"
+
+int PlutoButtonsToX(int PK_Button)
+{
+	switch( PK_Button )
+	{
+	case BUTTON_a_CONST:
+		return XK_A;
+	}
+	return 0;
+}
+
+#endif
