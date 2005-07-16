6f2416e2dd7d8f1786d5f202b04812b496c5906f
aaron <aaron.b@plutohome.com>
2005-07-16 17:48:28 +0000

    drop criteria tables

--- src/database/mkr_postinst_system.sh
+++ src/database/mkr_postinst_system.sh
@@ -5,6 +5,8 @@ echo ****This may take a long time****
 echo This involves merging all the changes from the
 echo central database back into your local copy.
 echo Please be patient.......
+
+echo "drop table if exists CriteriaParmNesting_D; drop table if exists CriteriaParm_D; drop table if exists Criteria_D" | mysql pluto_main
 (cd /usr/pluto/database; /usr/pluto/bin/sqlCVS -n -D pluto_main -r constants,dce,designer,document,ir,website import) || exit $?
 
 Q="GRANT ALL PRIVILEGES ON pluto_main.* TO 'root'@'127.0.0.1'; FLUSH PRIVILEGES;"
