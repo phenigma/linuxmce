d748c7d497dfa062ec868a5f9421377abcf791ab
aaron <aaron.b@plutohome.com>
2005-07-16 16:06:42 +0000

    force regen afterward

--- src/OrbiterGen/mkr_postinst_cleanog.sh
+++ src/OrbiterGen/mkr_postinst_cleanog.sh
@@ -2,5 +2,5 @@
 
 echo 'DELETE FROM CachedScreens;' | mysql pluto_main || /bin/true
 echo 'UPDATE Device JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate JOIN DeviceCategory ON FK_DeviceCategory=PK_DeviceCategory SET NeedConfigure=1 WHERE FK_DeviceCategory=5 OR FK_DeviceCategory_Parent=5;' | mysql pluto_main || /bin/true
-echo 'UPDATE Orbiter set Modification_LastGen=0;' | mysql pluto_main || /bin/true
+echo 'UPDATE Orbiter set Modification_LastGen=0,Regen=1;' | mysql pluto_main || /bin/true
 echo 'UPDATE Device JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate SET NeedConfigure=1 WHERE FK_DeviceCategory IN (2,3,5);' | mysql pluto_main || /bin/true
