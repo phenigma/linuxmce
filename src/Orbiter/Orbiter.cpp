873d1d7fcd8329c03d1d58e09e26109be389c74e
aaron <aaron.b@plutohome.com>
2005-07-24 19:43:09 +0000

    oops

--- src/Orbiter/Orbiter.cpp
+++ src/Orbiter/Orbiter.cpp
@@ -65,9 +65,6 @@ using namespace DCE;
 extern const char *g_szCompile_Date;
 extern Command_Impl *g_pCommand_Impl;
 
-#define PROFILING_GRID
-
-
 #ifdef WINCE
     #include "wince.h"
 
@@ -1305,7 +1302,9 @@ g_pPlutoLogger->Write(LV_WARNING,"Goto Screen -- wakign up from screen saver");
             PLUTO_SAFETY_LOCK_ERRORSONLY( vm, m_VariableMutex );
             if(  bAddToHistory  )
                 m_listScreenHistory.push_back( m_pScreenHistory_Current );
-            DumpScreenHistory();
+#ifdef DEBUG
+			DumpScreenHistory();
+#endif
             if ( m_listScreenHistory.size(  ) > 64 )
             {
                 delete m_listScreenHistory.front();
@@ -4871,7 +4870,9 @@ g_pPlutoLogger->Write(LV_STATUS,"Go Back currently: %s  cs: %s",this->m_pScreenH
     ScreenHistory *pScreenHistory=NULL;
     while(  m_listScreenHistory.size(  )  )
     {
-DumpScreenHistory();
+#ifdef DEBUG
+		DumpScreenHistory();
+#endif
         // The last screen we went to
         pScreenHistory = m_listScreenHistory.back(  );
 
@@ -5089,8 +5090,8 @@ void Orbiter::CMD_Remove_Screen_From_History(string sPK_DesignObj,string sID,str
     PLUTO_SAFETY_LOCK_ERRORSONLY( vm, m_VariableMutex );
 
 #ifdef DEBUG
-g_pPlutoLogger->Write(LV_STATUS,"CMD_Remove_Screen_From_History %s - %s size: %d",sPK_DesignObj.c_str(),sID.c_str(),(int) m_listScreenHistory.size());
-DumpScreenHistory();
+	g_pPlutoLogger->Write(LV_STATUS,"CMD_Remove_Screen_From_History %s - %s size: %d",sPK_DesignObj.c_str(),sID.c_str(),(int) m_listScreenHistory.size());
+	DumpScreenHistory();
 #endif
 
 	for(list < ScreenHistory * >::iterator it=m_listScreenHistory.begin();it!=m_listScreenHistory.end();)
@@ -7711,311 +7712,6 @@ void Orbiter::CMD_Simulate_Mouse_Click_At_Present_Pos(string sType,string &sCMD_
 
 
 
-
-bool Orbiter::Serialize( bool bWriting, char *&pcDataBlock, unsigned long &dwAllocatedSize, char *&pcCurrentPosition, void *pExtraSerializationData )	
-{
-	// Save the starting points
-	m_pcDataBlock=pcDataBlock; m_dwAllocatedSize=dwAllocatedSize; m_pcCurrentPosition=pcCurrentPosition;
-		
-	// In some cases a class may need extra data to properly serialize in UnknownSerialize.  This is an extra void pointer that can be cast to something useful
-	m_pExtraSerializationData=pExtraSerializationData;
-
-	if( !m_bManuallySetupSerialization )
-	{
-		// For Symbian compatibility
-		MYSTL_ITERATE_VECT( m_vectItemToSerialize, ItemToSerialize, pItem_del, it_del )
-		{
-			delete pItem_del;
-		}
-		MYSTL_CLEAR_LIST(m_vectItemToSerialize);
-		if( bWriting )
-		{
-			Write_unsigned_long(m_iSC_Version);
-			SetupSerialization(m_iSC_Version);
-		}
-		else
-		{
-			unsigned long iSC_Version = Read_unsigned_long();
-			SetupSerialization(iSC_Version);
-		}
-	}
-
-	MYSTL_ITERATE_VECT(m_vectItemToSerialize,ItemToSerialize,pItem,it)
-	{
-
-		// These are self-serializing, we don't need the if( bWriting ) split.  However, these classes are 
-		// derived from SerializeClass.  Therefore, we can't include the class definitions before binary
-		// serialize.  We do a forward declaration, and have to put the actual switch block in a .cpp
-		// file to avoid the cylcical dependencies
-		if( SelfSerializing(bWriting,pItem) )
-			continue;
-			
-		if( bWriting )
-		{
-#ifdef DEBUG_SERIALIZATION
-			cout << "Writing type " << pItem->m_iSerializeDataType << " from class " << SerializeClassClassName() << " at pos: " << (int) CurrentSize() << endl;
-#endif
-			switch(pItem->m_iSerializeDataType)
-			{
-			case SERIALIZE_DATA_TYPE_CHAR:
-				Write_char( *((char *) pItem->m_pItem) );
-				break;
-			case SERIALIZE_DATA_TYPE_SHORT:
-				Write_short( *((short *) pItem->m_pItem) );
-				break;
-			case SERIALIZE_DATA_TYPE_LONG:
-				Write_long( *((long *) pItem->m_pItem) );
-				break;
-			case SERIALIZE_DATA_TYPE_UNSIGNED_LONG:
-				Write_unsigned_long( *((unsigned long *) pItem->m_pItem) );
-				break;
-#ifndef SYMBIAN
-			case SERIALIZE_DATA_TYPE_INT64:
-				Write_int64( *((u_int64_t *) pItem->m_pItem) );
-				break;
-#endif
-			case SERIALIZE_DATA_TYPE_STRING:
-				Write_string( *((string *) pItem->m_pItem) );
-				break;
-#ifndef SYMBIAN
-			case SERIALIZE_DATA_TYPE_VECT_STRING:
-				{
-					vector<string> *pVect = (vector<string> *) pItem->m_pItem;
-					Write_unsigned_long((unsigned long) pVect->size());
-#ifdef DEBUG_SERIALIZATION
-					cout << "Writing " << (unsigned long) pVect->size() << " vector strings" << endl;
-#endif
-					for(size_t s=0; s < pVect->size(); ++s)
-					{
-						Write_string( (*pVect)[s] );
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_BLOCK:
-				{
-					PlutoDataBlock *pBlock = (PlutoDataBlock *) pItem->m_pItem;
-					Write_unsigned_long(pBlock->m_dwSize);
-					Write_block(pBlock->m_pBlock,pBlock->m_dwSize);
-				}
-				break;
-
-			case SERIALIZE_DATA_TYPE_VECT_INT:
-				{
-					vector<int> *pVect = (vector<int> *) pItem->m_pItem;
-					Write_unsigned_long((unsigned long) pVect->size());
-#ifdef DEBUG_SERIALIZATION
-					cout << "Writing " << (unsigned long) pVect->size() << " vector ints" << endl;
-#endif
-					for(size_t s=0; s < pVect->size(); ++s)
-					{
-						Write_unsigned_long( (*pVect)[s] );
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_VECT_PAIR_INT:
-				{
-					vector< pair<int,int> > *pVect = (vector< pair<int,int> > *) pItem->m_pItem;
-					Write_unsigned_long((unsigned long) pVect->size());
-#ifdef DEBUG_SERIALIZATION
-					cout << "Writing " << (unsigned long) pVect->size() << " vector pair ints" << endl;
-#endif
-					for(size_t s=0; s < pVect->size(); ++s)
-					{
-						Write_unsigned_long( (*pVect)[s].first );
-						Write_unsigned_long( (*pVect)[s].second );
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_INT_STRING:
-				{
-					map<int,string> *pMap = (map<int,string> *) pItem->m_pItem;
-					Write_unsigned_long((unsigned long) pMap->size());
-#ifdef DEBUG_SERIALIZATION
-					cout << "Writing " << (unsigned long) pMap->size() << " map int strings" << endl;
-#endif
-					map<int,string>::iterator it;
-					for(it=pMap->begin(); it!=pMap->end(); ++it)
-					{
-						Write_long( (*it).first );
-						Write_string( (*it).second );
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_STRING_STRING:
-				{
-					map<string,string> *pMap = (map<string,string> *) pItem->m_pItem;
-					Write_unsigned_long((unsigned long) pMap->size());
-#ifdef DEBUG_SERIALIZATION
-					cout << "Writing " << (unsigned long) pMap->size() << " map string strings" << endl;
-#endif
-					map<string,string>::iterator it;
-					for(it=pMap->begin(); it!=pMap->end(); ++it)
-					{
-						string key=(*it).first;
-						Write_string( key );
-						Write_string( (*it).second );
-					}
-				}
-				break;
-
-#endif
-
-			default:
-				{
-					if( !UnknownSerialize(pItem,bWriting,m_pcDataBlock,m_dwAllocatedSize,m_pcCurrentPosition) )
-					{
-
-#ifdef DEBUG_SERIALIZATION
-						cout << "Unknown Serialize failed" << endl;
-#else
-
-#ifndef SYMBIAN						
-
-						throw ("Don't know how to serialize data type" + StringUtils::itos(pItem->m_iSerializeDataType)).c_str();
-#else
-						/** @todo show a popup */
-#endif
-#endif
-						return false; // We don't know how to serialize this type
-					}
-				}
-			};
-		}
-		else
-		{
-
-			switch(pItem->m_iSerializeDataType)
-			{
-			case SERIALIZE_DATA_TYPE_CHAR:
-				*((char *) pItem->m_pItem)=Read_char();
-				break;
-			case SERIALIZE_DATA_TYPE_SHORT:
-				*((short *) pItem->m_pItem)=Read_short();
-				break;
-			case SERIALIZE_DATA_TYPE_LONG:
-				*((long *) pItem->m_pItem)=Read_long();
-				break;
-			case SERIALIZE_DATA_TYPE_UNSIGNED_LONG:
-				*((unsigned long *) pItem->m_pItem)=Read_unsigned_long();
-				break;
-#ifndef SYMBIAN
-			case SERIALIZE_DATA_TYPE_INT64:
-				*((u_int64_t *) pItem->m_pItem)=Read_int64();
-				break;
-#endif
-			case SERIALIZE_DATA_TYPE_STRING:
-				Read_string( *((string *) pItem->m_pItem) );
-				break;
-#ifndef SYMBIAN
-			case SERIALIZE_DATA_TYPE_VECT_STRING:
-				{
-					vector<string> *pVect = (vector<string> *) pItem->m_pItem;
-					unsigned long count = Read_unsigned_long();
-#ifdef DEBUG_SERIALIZATION
-					cout << "Reading " << count << " vector strings" << endl;
-#endif
-					for(size_t s=0;s<count;++s)
-					{
-						string value;
-						Read_string(value);
-						pVect->push_back(value);
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_VECT_INT:
-				{
-					vector<int> *pVect = (vector<int> *) pItem->m_pItem;
-					unsigned long count = Read_unsigned_long();
-#ifdef DEBUG_SERIALIZATION
-					cout << "Reading " << count << " vector ints" << endl;
-#endif
-					for(size_t s=0;s<count;++s)
-					{
-						pVect->push_back(Read_unsigned_long());
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_VECT_PAIR_INT:
-				{
-					vector< pair<int,int> > *pVect = (vector< pair<int,int> > *) pItem->m_pItem;
-					unsigned long count = Read_unsigned_long();
-#ifdef DEBUG_SERIALIZATION
-					cout << "Reading " << count << " vector pair ints" << endl;
-#endif
-					for(size_t s=0;s<count;++s)
-					{
-						long l1 = Read_unsigned_long();
-						long l2 = Read_unsigned_long();
-						pVect->push_back( pair<int,int> (l1,l2) );
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_INT_STRING:
-				{
-					map<int,string> *pMap = (map<int,string> *) pItem->m_pItem;
-					unsigned long count=Read_unsigned_long();
-#ifdef DEBUG_SERIALIZATION
-					cout << "Reading " << count << " map int strings" << endl;
-#endif
-					for(size_t s=0;s<count;++s)
-					{
-						int Key = Read_long();
-						string value;
-						Read_string(value);
-						(*pMap)[Key]=value;
-					}
-				}
-				break;
-			case SERIALIZE_DATA_TYPE_STRING_STRING:
-				{
-					map<string,string> *pMap = (map<string,string> *) pItem->m_pItem;
-					unsigned long count=Read_unsigned_long();
-#ifdef DEBUG_SERIALIZATION
-					cout << "Reading " << count << " map string strings" << endl;
-#endif
-					for(size_t s=0;s<count;++s)
-					{
-						string key,value;
-						Read_string(key);
-						Read_string(value);
-						(*pMap)[key]=value;
-					}
-				}
-				break;
-#endif
-			case SERIALIZE_DATA_TYPE_BLOCK:
-				{
-					PlutoDataBlock *pBlock = (PlutoDataBlock *) pItem->m_pItem;
-					pBlock->m_dwSize = Read_unsigned_long();
-					pBlock->m_pBlock=Read_block(pBlock->m_dwSize);
-				}
-				break;
-			default:
-				{
-					if( !UnknownSerialize(pItem,bWriting,m_pcDataBlock,m_dwAllocatedSize,m_pcCurrentPosition) )
-					{
-#ifdef DEBUG_SERIALIZATION
-						cout << "Unknown Serialize failed" << endl;
-#else
-
-#ifndef SYMBIAN							
-						throw ("Don't know how to serialize data type" + StringUtils::itos(pItem->m_iSerializeDataType)).c_str();
-#else
-						/** @todo show a popup */
-#endif
-							
-#endif
-						return false;
-					}
-				}
-			};
-		}
-	}
-
-	// We may have re-allocated the memory block and size, and the position will have changed
-	pcDataBlock=m_pcDataBlock; dwAllocatedSize=m_dwAllocatedSize; pcCurrentPosition=m_pcCurrentPosition;
-	return true;
-}
 
 void Orbiter::ParseGrid(DesignObj_DataGrid *pObj_Datagrid)
 {
