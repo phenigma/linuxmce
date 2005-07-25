cdbdf4d2751bbc195f9c716dbbc9805a526b1595
aaron <aaron.b@plutohome.com>
2005-07-25 17:08:02 +0000

    oops

--- src/database/mkr_postinst_system.sh
+++ src/database/mkr_postinst_system.sh
@@ -6,7 +6,7 @@ echo This involves merging all the changes from the
 echo central database back into your local copy.
 echo Please be patient.......
 
-echo "drop table if exists CriteriaParmNesting_D; drop table if exists CriteriaParm_D; drop table if exists Criteria_D" | mysql pluto_main
+echo "drop table if exists CriteriaParmNesting_D; drop table if exists CriteriaParm_D; drop table if exists Criteria_D" | mysql pluto_main || /bin/true
 (cd /usr/pluto/database; /usr/pluto/bin/sqlCVS -n -D pluto_main -r constants,dce,designer,document,ir,website import) || exit $?
 
 Q="GRANT ALL PRIVILEGES ON pluto_main.* TO 'root'@'127.0.0.1'; FLUSH PRIVILEGES;"
