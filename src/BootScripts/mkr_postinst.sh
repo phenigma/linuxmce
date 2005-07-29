deeeeb852fc36b349936da9939ed4a4f0cef43cc
radu <radu.cx+linuxmce@gmail.com>
2005-07-29 11:55:51 +0000

    Hotplug and discover moved only for MDs

--- src/BootScripts/mkr_postinst.sh
+++ src/BootScripts/mkr_postinst.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 update-rc.d -f Startup_Scripts.sh remove || /bin/true
 update-rc.d Startup_Scripts.sh defaults 22 10
 
 . /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/SQL_Ops.sh
 
 # /etc/apt/apt.conf.d/30pluto
 
@@ -16,8 +19,16 @@ Dpkg::Options { "--force-confold"; };
 
 echo -n "$pluto_apt_conf" >/etc/apt/apt.conf.d/30pluto
 
-update-rc.d -f discover remove || /bin/true
-update-rc.d discover start 80 1 2 3 4 5 .
+Q="SELECT FK_DeviceTemplate FROM Device WHERE PK_Device=$PK_Device"
+DeviceTemplate=$(RunSQL "$Q")
 
-update-rc.d -f hotplug remove || /bin/true
-update-rc.d hotplug start 81 1 2 3 4 5 . stop 89 0 6 . || /bin/true
+DeviceTemplate_Core=7
+DeviceTemplate_MD=28
+
+if [[ -n "$DeviceTemplate" && $DeviceTemplate -eq $DeviceTemplate_MD ]]; then
+	update-rc.d -f discover remove || /bin/true
+	update-rc.d discover start 80 1 2 3 4 5 .
+
+	update-rc.d -f hotplug remove || /bin/true
+	update-rc.d hotplug start 81 1 2 3 4 5 . stop 89 0 6 . || /bin/true
+fi
