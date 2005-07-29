eaabec6ab2abbf4523c17691cd7db872c50b8590
radu <radu.cx+linuxmce@gmail.com>
2005-07-29 16:48:05 +0000

    include Realtek 8169 module in ramdisk

--- src/BootScripts/Create_DisklessMD_FS.sh
+++ src/BootScripts/Create_DisklessMD_FS.sh
@@ -16,7 +16,7 @@ FSarchive=PlutoMD.tar.bz2
 DlPath="/usr/pluto/diskless/$IP"
 HexIP=$(gethostip -x "$IP")
 
-Modules="pcnet32 tg3 e1000 sk98lin fealnx natsemi"
+Modules="pcnet32 tg3 e1000 sk98lin fealnx natsemi r8169"
 
 KERNEL_VERSION="$(uname -r)"
 
