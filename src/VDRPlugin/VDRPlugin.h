c96ae7de4c14db17fcf1576f2b7d2d09ef11dd39
aaron <aaron.b@plutohome.com>
2005-07-30 19:49:37 +0000

    add vdr

---
+++ src/VDRPlugin/VDRPlugin.h
@@ -0,0 +1,80 @@
+//<-dceag-d-b->
+#ifndef VDRPlugin_h
+#define VDRPlugin_h
+
+//	DCE Implemenation for #1706 VDR-Plugin
+
+#include "Gen_Devices/VDRPluginBase.h"
+//<-dceag-d-e->
+
+#include "../Media_Plugin/Media_Plugin.h"
+#include "../Media_Plugin/MediaStream.h"
+#include "../Media_Plugin/MediaHandlerBase.h"
+
+//<-dceag-decl-b->!
+namespace DCE
+{
+	class VDRPlugin : public VDRPlugin_Command, public MediaHandlerBase
+	{
+//<-dceag-decl-e->
+		// Private member variables
+		class Orbiter_Plugin *m_pOrbiter_Plugin;
+
+		// Private methods
+public:
+		// Public member variables
+
+//<-dceag-const-b->
+public:
+		// Constructors/Destructor
+		VDRPlugin(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL);
+		virtual ~VDRPlugin();
+		virtual bool Register();
+		virtual void ReceivedCommandForChild(DeviceData_Base *pDeviceData_Base,string &sCMD_Result,Message *pMessage);
+		virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage);
+//<-dceag-const-e->
+
+//<-dceag-const2-b->
+		// The following constructor is only used if this a class instance embedded within a DCE Device.  In that case, it won't create it's own connection to the router
+		// You can delete this whole section and put an ! after dceag-const2-b tag if you don't want this constructor.  Do the same in the implementation file
+		VDRPlugin(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+//<-dceag-const2-e->
+		/**
+		* @brief
+		*/
+		virtual class MediaStream *CreateMediaStream( class MediaHandlerInfo *pMediaHandlerInfo, int iPK_MediaProvider, vector<class EntertainArea *> &vectEntertainArea, MediaDevice *pMediaDevice, int iPK_Users, deque<MediaFile *> *dequeFilenames, int StreamID );
+
+		/**
+		* @brief Start media playback
+		*/
+		virtual bool StartMedia( class MediaStream *pMediaStream );
+
+		/**
+		* @brief Stop media playback
+		*/
+		virtual bool StopMedia( class MediaStream *pMediaStream );
+
+
+
+//<-dceag-h-b->
+	/*
+				AUTO-GENERATED SECTION
+				Do not change the declarations
+	*/
+
+	/*
+			*****DATA***** accessors inherited from base class
+
+			*****EVENT***** accessors inherited from base class
+
+			*****COMMANDS***** we need to implement
+	*/
+
+
+//<-dceag-h-e->
+	};
+
+//<-dceag-end-b->
+}
+#endif
+//<-dceag-end-e->
