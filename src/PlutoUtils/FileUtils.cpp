a0706ce5bd939d41cd3f629a8481e420ed349bc6
aaron <aaron.b@plutohome.com>
2005-08-11 13:17:56 +0000

    oops

--- src/PlutoUtils/FileUtils.cpp
+++ src/PlutoUtils/FileUtils.cpp
@@ -566,7 +566,7 @@ bool FileUtils::FindFiles(list<string> &listFiles, string sDirectory, string sFi
 				/* somebody didn't test fnmatch well.  file: "The Patriot [Special Edition].dvd" doesn't match "The Patriot [Special Edition].*"
 				so I added the StringUtils::StartsWith */
 				if (sFileSpec_CSV.size()==0 || fnmatch(s.c_str(), entry.d_name, 0) == 0 || StringUtils::EndsWith(entry.d_name, s.c_str(),true)
-					|| (sFileSpec_CSV[sFileSpec_CSV.size()==1]=='*' && StringUtils::StartsWith(entry.d_name,sFileSpec_CSV.substr(0,sFileSpec_CSV.size()-1))) )
+					|| (sFileSpec_CSV[sFileSpec_CSV.size()-1]=='*' && StringUtils::StartsWith(entry.d_name,sFileSpec_CSV.substr(0,sFileSpec_CSV.size()-1))) )
 				{
 					if( bFullyQualifiedPath )
 						listFiles.push_back(sDirectory + entry.d_name);
