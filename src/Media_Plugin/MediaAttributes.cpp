3d242917c254931723fabf886d352fc8913cf5c7
cmiron <cmiron>
2005-08-12 11:48:31 +0000

    Fixed Media_Plugin bug: if the user doesn't enter the name of the playlist and clicks "save to public", Media_Plugin crashes.

--- src/Media_Plugin/MediaAttributes.cpp
+++ src/Media_Plugin/MediaAttributes.cpp
@@ -539,8 +539,9 @@ g_pPlutoLogger->Write(LV_WARNING, "pl3 = %s %s",sPlaylistName.c_str(),pRow_Playl
 
     if ( !pRow_Playlist->Table_Playlist_get()->Commit(true,true) )
     {
-		g_pPlutoLogger->Write(LV_CRITICAL,"Cannot save/create playlist with ID %d error: %s",iPK_Playlist,
-			pRow_Playlist->Table_Playlist_get()->Database_pluto_media_get()->m_sLastMySqlError.c_str());
+        g_pPlutoLogger->Write(LV_CRITICAL,"Cannot save/create playlist with ID %d error: %s",iPK_Playlist,
+	        m_pDatabase_pluto_media->m_sLastMySqlError.c_str());
+
 		return false;
     }
 
