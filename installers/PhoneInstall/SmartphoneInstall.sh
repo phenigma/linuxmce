2ad56d1aaf63c9f067c23da34763d24eb17d218d
cmiron <cmiron>
2005-08-25 13:17:29 +0000

    cl:PlutoMO: need phone install scripts

---
+++ installers/PhoneInstall/SmartphoneInstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#. /usr/pluto/bin/pluto.func
+
+#$0 <MAC> <path_to_sis>
+
+if [ "$#" -ne "2" ]; then
+	echo "Usage: $0 <MAC> <path_to_sis>"
+	exit
+fi
+
+MAC="$1"
+SIS="$2"
+
+#TODO: check if MAC is valid
+
+if [ ! -f "$SIS" -o ! -r "$SIS" ]; then
+	echo "sis file '$SIS' not found or not accessible"
+	exit
+fi
+
+(echo "c"; echo "p"; echo "$SIS Orbiter.cab"; echo "q") | obex_test -b $MAC 3 |grep -qF 'PUT successful!'
+Ret=$?
+
+if [[ "$Ret" -eq 0 ]]; then
+	echo Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "SIS '$SIS' uploaded to '$MAC'"
+	exit 0
+else
+	echo Logging "$TYPE" "$SEVERITY_WARNING" "$0" "SIS '$SIS' failed to upload to '$MAC'"
+	exit 1
+fi
