98317da624c896601a502353ac533e21cfc9c959
aaron <aaron.b@plutohome.com>
2005-08-26 11:23:18 +0000

    oops

--- src/UpdateMedia/mkr_postinst.sh
+++ src/UpdateMedia/mkr_postinst.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 . /usr/pluto/bin/SQL_Ops.sh
-
-#/usr/pluto/bin/UpdateMedia -d /home/public/data
+exit
+/usr/pluto/bin/UpdateMedia -d /home/public/data
 Q="SELECT PK_Users FROM Users"
 R="$(RunSQL "$Q")"
 
 for User in $R; do
 	Dir="/home/user_$User/data"
 	if [[ -d "$Dir" ]]; then
-		#/usr/pluto/bin/UpdateMedia -d "$Dir"
+		/usr/pluto/bin/UpdateMedia -d "$Dir"
 	fi
 done
