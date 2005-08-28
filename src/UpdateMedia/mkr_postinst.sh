718e85eb2e31bab20e4c633b64f05b5a7f5dca4d
aaron <aaron.b@plutohome.com>
2005-08-28 14:25:51 +0000

    oops

--- src/UpdateMedia/mkr_postinst.sh
+++ src/UpdateMedia/mkr_postinst.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
 . /usr/pluto/bin/SQL_Ops.sh
-exit
-/usr/pluto/bin/UpdateMedia -d /home/public/data
+
+/usr/pluto/bin/UpdateMedia -d /home/public/data/documents
+/usr/pluto/bin/UpdateMedia -d /home/public/data/movies
+/usr/pluto/bin/UpdateMedia -d /home/public/data/music
+/usr/pluto/bin/UpdateMedia -d /home/public/data/pictures
+/usr/pluto/bin/UpdateMedia -d /home/public/data/videos
+
 Q="SELECT PK_Users FROM Users"
 R="$(RunSQL "$Q")"
 
 for User in $R; do
-	Dir="/home/user_$User/data"
+	Dir="/home/user_$User/data/documents"
+	if [[ -d "$Dir" ]]; then
+		/usr/pluto/bin/UpdateMedia -d "$Dir"
+	fi
+	Dir="/home/user_$User/data/movies"
+	if [[ -d "$Dir" ]]; then
+		/usr/pluto/bin/UpdateMedia -d "$Dir"
+	fi
+	Dir="/home/user_$User/data/music"
+	if [[ -d "$Dir" ]]; then
+		/usr/pluto/bin/UpdateMedia -d "$Dir"
+	fi
+	Dir="/home/user_$User/data/pictures"
+	if [[ -d "$Dir" ]]; then
+		/usr/pluto/bin/UpdateMedia -d "$Dir"
+	fi
+	Dir="/home/user_$User/data/videos"
 	if [[ -d "$Dir" ]]; then
 		/usr/pluto/bin/UpdateMedia -d "$Dir"
 	fi
