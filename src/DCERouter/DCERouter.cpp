f16971d10b88ca56f6fe974837d6b11d0412ae41
aaron <aaron.b@plutohome.com>
2005-08-31 13:07:32 +0000

    oops

--- src/DCERouter/DCERouter.cpp
+++ src/DCERouter/DCERouter.cpp
@@ -931,8 +931,9 @@ g_pPlutoLogger->Write(LV_CRITICAL,"Checking %d plugins",(int)m_mapPlugIn.size())
 g_pPlutoLogger->Write(LV_CRITICAL,"Checking plugin %d for reload",pPlugIn->m_dwPK_Device);
 							if( !pPlugIn->PendingTasks(&vectPendingTasks) )
 							{
+								string sPendingTasks;
 								for(size_t s=0;s<vectPendingTasks.size();++s)
-									g_pPlutoLogger->Write(LV_STATUS,"Aborting reload as per %s %s",pPlugIn->m_sName.c_str(),vectPendingTasks[s].c_str());
+									sPendingTasks += pPlugIn->m_sName + ": " + vectPendingTasks[s];
 								ReceivedMessage(NULL,new Message(m_dwPK_Device, DEVICEID_EVENTMANAGER, PRIORITY_NORMAL, MESSAGETYPE_EVENT, 
 									EVENT_Reload_Aborted_CONST,3,
 									EVENTPARAMETER_PK_Device_CONST,StringUtils::itos(pPlugIn->m_dwPK_Device).c_str(),
