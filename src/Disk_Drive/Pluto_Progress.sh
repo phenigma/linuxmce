b8dff4ae6bbc17441180cbb66fe262baa169be3b
radu <radu.cx+linuxmce@gmail.com>
2005-09-01 14:58:27 +0000

    fixed server address

--- src/Disk_Drive/Pluto_Progress.sh
+++ src/Disk_Drive/Pluto_Progress.sh
@@ -8,7 +8,7 @@ filename="$2"
 # progress can be either int or float, from stdin
 while read progress text; do
 	progress=${progress%%.*} # int(progress)
-	/usr/pluto/bin/MessageSend 10.2.1.142 "$diskDriveDeviceID" -1000 2 35 35 "$filename" 20 7 13 "name" 30 "$Percentage" > null
+	/usr/pluto/bin/MessageSend "$DCERouter" "$diskDriveDeviceID" -1000 2 35 35 "$filename" 20 7 13 "name" 30 "$Percentage" > null
 	#echo "$progress"
 	#/usr/pluto/bin/MessageSend $DCERouter
 done
