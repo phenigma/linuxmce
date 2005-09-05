5765db582fb4ab5e2cc29a7bdad9aae2d07dd2af
aaron <aaron.b@plutohome.com>
2005-09-05 14:11:05 +0000

    oops

---
+++ src/Gen_Devices/TiraBase.cpp
@@ -0,0 +1,37 @@
+#include "TiraBase.h"
+#include "DeviceData_Impl.h"
+#include "Logger.h"
+
+using namespace DCE;
+#include "TiraBase.h"
+extern Tira_Command *Create_Tira(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+DeviceData_Impl *Tira_Data::CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition)
+{
+	// Peek ahead in the stream.  We're going to pass in the above pointers anyway so it won't affect the position
+	SerializeClass b;
+	b.ResumeReadWrite(pDataBlock,AllocatedSize,CurrentPosition);
+	int iPK_Device = b.Read_unsigned_long();
+	int iPK_Installation = b.Read_unsigned_long();
+	int iPK_DeviceTemplate = b.Read_unsigned_long();
+	switch(iPK_DeviceTemplate) {
+		case 1717:
+			return new Tira_Data();
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateData for unknown type %d.", iPK_DeviceTemplate);
+	return NULL;
+}
+
+Event_Impl *Tira_Event::CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice )
+{
+	switch(dwPK_DeviceTemplate) {
+		case 1717:
+			return (Event_Impl *) new Tira_Event(pOCClientSocket, dwDevice);
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateEvent for unknown type %d.", dwPK_DeviceTemplate);
+	return NULL;
+}
+Command_Impl  *Tira_Command::CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent)
+{
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateCommand for unknown type %d.", PK_DeviceTemplate);
+	return NULL;
+}
---
+++ src/Gen_Devices/TiraBase.h
@@ -0,0 +1,295 @@
+#ifndef TiraBase_h
+#define TiraBase_h
+#include "DeviceData_Impl.h"
+#include "Message.h"
+#include "Command_Impl.h"
+#include "Logger.h"
+
+
+namespace DCE
+{
+//   OUR EVENT CLASS 
+
+class Tira_Event : public Event_Impl
+{
+public:
+	Tira_Event(int DeviceID, string ServerAddress, bool bConnectEventHandler=true) : Event_Impl(DeviceID,1717, ServerAddress, bConnectEventHandler) {};
+	Tira_Event(class ClientSocket *pOCClientSocket, int DeviceID) : Event_Impl(pOCClientSocket, DeviceID) {};
+	//Events
+	class Event_Impl *CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice );
+};
+
+
+//   OUR DATA CLASS 
+
+class Tira_Data : public DeviceData_Impl
+{
+public:
+	virtual ~Tira_Data() {};
+	class DeviceData_Impl *CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition);
+	virtual int GetPK_DeviceList() { return 1717; } ;
+	virtual const char *GetDeviceDescription() { return "Tira"; } ;
+};
+
+
+
+//   OUR COMMAND CLASS 
+
+class Tira_Command : public Command_Impl
+{
+public:
+	Tira_Command(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL)
+	: Command_Impl(DeviceID, ServerAddress, bLocalMode, pRouter)
+	{
+	}
+	virtual bool GetConfig()
+	{
+		if( m_bLocalMode )
+			return true;
+		m_pData=NULL;
+		m_pEvent = new Tira_Event(m_dwPK_Device, m_sHostName);
+		if( m_pEvent->m_dwPK_Device )
+			m_dwPK_Device = m_pEvent->m_dwPK_Device;
+		if( m_sIPAddress!=m_pEvent->m_pClientSocket->m_sIPAddress )	
+			m_sIPAddress=m_pEvent->m_pClientSocket->m_sIPAddress;
+		m_sMacAddress=m_pEvent->m_pClientSocket->m_sMacAddress;
+		if( m_pEvent->m_pClientSocket->m_eLastError!=cs_err_None )
+		{
+			if( m_pEvent->m_pClientSocket->m_eLastError==cs_err_BadDevice )
+			{
+				while( m_pEvent->m_pClientSocket->m_eLastError==cs_err_BadDevice && (m_dwPK_Device = DeviceIdInvalid())!=0 )
+				{
+					delete m_pEvent;
+					m_pEvent = new Tira_Event(m_dwPK_Device, m_sHostName);
+					if( m_pEvent->m_dwPK_Device )
+						m_dwPK_Device = m_pEvent->m_dwPK_Device;
+				}
+			}
+			if( m_pEvent->m_pClientSocket->m_eLastError==cs_err_NeedReload )
+			{
+				if( RouterNeedsReload() )
+				{
+					string sResponse;
+					Event_Impl event_Impl(DEVICEID_MESSAGESEND, 0, m_sHostName);
+					event_Impl.m_pClientSocket->SendString( "RELOAD" );
+					if( !event_Impl.m_pClientSocket->ReceiveString( sResponse ) || sResponse!="OK" )
+					{
+						CannotReloadRouter();
+						g_pPlutoLogger->Write(LV_WARNING,"Reload request denied: %s",sResponse.c_str());
+					}
+				Sleep(10000);  // Give the router 10 seconds before we re-attempt, otherwise we'll get an error right away
+				}	
+			}
+		}
+		
+		if( m_pEvent->m_pClientSocket->m_eLastError!=cs_err_None || m_pEvent->m_pClientSocket->m_Socket==INVALID_SOCKET )
+			return false;
+
+		int Size; char *pConfig = m_pEvent->GetConfig(Size);
+		if( !pConfig )
+			return false;
+		m_pData = new Tira_Data();
+		if( Size )
+			m_pData->SerializeRead(Size,pConfig);
+		delete[] pConfig;
+		pConfig = m_pEvent->GetDeviceList(Size);
+		m_pData->m_AllDevices.SerializeRead(Size,pConfig);
+		delete[] pConfig;
+		m_pData->m_pEvent_Impl = m_pEvent;
+		m_pcRequestSocket = new Event_Impl(m_dwPK_Device, 1717,m_sHostName);
+		return true;
+	};
+	Tira_Command(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter) : Command_Impl(pPrimaryDeviceCommand, pData, pEvent, pRouter) {};
+	virtual ~Tira_Command() {};
+	Tira_Event *GetEvents() { return (Tira_Event *) m_pEvent; };
+	Tira_Data *GetData() { return (Tira_Data *) m_pData; };
+	const char *GetClassName() { return "Tira_Command"; };
+	virtual int PK_DeviceTemplate_get() { return 1717; };
+	static int PK_DeviceTemplate_get_static() { return 1717; };
+	virtual void ReceivedCommandForChild(DeviceData_Base *pDeviceData_Base,string &sCMD_Result,Message *pMessage) { };
+	virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage) { };
+	Command_Impl *CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent);
+	//Data accessors
+	//Event accessors
+	//Commands - Override these to handle commands from the server
+	virtual void CMD_Send_Code(string sText,string &sCMD_Result,class Message *pMessage) {};
+	virtual void CMD_Toggle_Power(string sOnOff,string &sCMD_Result,class Message *pMessage) {};
+	virtual void CMD_Learn_IR(string sOnOff,int iPK_Text,int iPK_Command_Input,string &sCMD_Result,class Message *pMessage) {};
+	virtual void CMD_Set_Screen_Type(int iValue,string &sCMD_Result,class Message *pMessage) {};
+
+	//This distributes a received message to your handler.
+	virtual bool ReceivedMessage(class Message *pMessageOriginal)
+	{
+		map<long, string>::iterator itRepeat;
+		if( Command_Impl::ReceivedMessage(pMessageOriginal) )
+			return true;
+		int iHandled=0;
+		for(int s=-1;s<(int) pMessageOriginal->m_vectExtraMessages.size(); ++s)
+		{
+			Message *pMessage = s>=0 ? pMessageOriginal->m_vectExtraMessages[s] : pMessageOriginal;
+			if (pMessage->m_dwPK_Device_To==m_dwPK_Device && pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND)
+			{
+				switch(pMessage->m_dwID)
+				{
+				case 191:
+					{
+						string sCMD_Result="OK";
+					string sText=pMessage->m_mapParameters[9];
+						CMD_Send_Code(sText.c_str(),sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							pMessageOut->m_mapParameters[0]=sCMD_Result;
+							SendMessage(pMessageOut);
+						}
+						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							SendString(sCMD_Result);
+						}
+						if( (itRepeat=pMessage->m_mapParameters.find(72))!=pMessage->m_mapParameters.end() )
+						{
+							int iRepeat=atoi(pMessage->m_mapParameters[72].c_str());
+							for(int i=2;i<=iRepeat;++i)
+								CMD_Send_Code(sText.c_str(),sCMD_Result,pMessage);
+						}
+					};
+					iHandled++;
+					continue;
+				case 194:
+					{
+						string sCMD_Result="OK";
+					string sOnOff=pMessage->m_mapParameters[8];
+						CMD_Toggle_Power(sOnOff.c_str(),sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							pMessageOut->m_mapParameters[0]=sCMD_Result;
+							SendMessage(pMessageOut);
+						}
+						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							SendString(sCMD_Result);
+						}
+						if( (itRepeat=pMessage->m_mapParameters.find(72))!=pMessage->m_mapParameters.end() )
+						{
+							int iRepeat=atoi(pMessage->m_mapParameters[72].c_str());
+							for(int i=2;i<=iRepeat;++i)
+								CMD_Toggle_Power(sOnOff.c_str(),sCMD_Result,pMessage);
+						}
+					};
+					iHandled++;
+					continue;
+				case 245:
+					{
+						string sCMD_Result="OK";
+					string sOnOff=pMessage->m_mapParameters[8];
+					int iPK_Text=atoi(pMessage->m_mapParameters[25].c_str());
+					int iPK_Command_Input=atoi(pMessage->m_mapParameters[71].c_str());
+						CMD_Learn_IR(sOnOff.c_str(),iPK_Text,iPK_Command_Input,sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							pMessageOut->m_mapParameters[0]=sCMD_Result;
+							SendMessage(pMessageOut);
+						}
+						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							SendString(sCMD_Result);
+						}
+						if( (itRepeat=pMessage->m_mapParameters.find(72))!=pMessage->m_mapParameters.end() )
+						{
+							int iRepeat=atoi(pMessage->m_mapParameters[72].c_str());
+							for(int i=2;i<=iRepeat;++i)
+								CMD_Learn_IR(sOnOff.c_str(),iPK_Text,iPK_Command_Input,sCMD_Result,pMessage);
+						}
+					};
+					iHandled++;
+					continue;
+				case 687:
+					{
+						string sCMD_Result="OK";
+					int iValue=atoi(pMessage->m_mapParameters[48].c_str());
+						CMD_Set_Screen_Type(iValue,sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							pMessageOut->m_mapParameters[0]=sCMD_Result;
+							SendMessage(pMessageOut);
+						}
+						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							SendString(sCMD_Result);
+						}
+						if( (itRepeat=pMessage->m_mapParameters.find(72))!=pMessage->m_mapParameters.end() )
+						{
+							int iRepeat=atoi(pMessage->m_mapParameters[72].c_str());
+							for(int i=2;i<=iRepeat;++i)
+								CMD_Set_Screen_Type(iValue,sCMD_Result,pMessage);
+						}
+					};
+					iHandled++;
+					continue;
+				}
+				iHandled += Command_Impl::ReceivedMessage(pMessage);
+			}
+			else if( pMessage->m_dwMessage_Type == MESSAGETYPE_COMMAND )
+			{
+				MapCommand_Impl::iterator it = m_mapCommandImpl_Children.find(pMessage->m_dwPK_Device_To);
+				if( it!=m_mapCommandImpl_Children.end() && !(*it).second->m_bGeneric )
+				{
+					Command_Impl *pCommand_Impl = (*it).second;
+					iHandled += pCommand_Impl->ReceivedMessage(pMessage);
+			}
+			else
+			{
+				DeviceData_Base *pDeviceData_Base = m_pData->m_AllDevices.m_mapDeviceData_Base_Find(pMessage->m_dwPK_Device_To);
+				string sCMD_Result="UNHANDLED";
+				if( pDeviceData_Base && pDeviceData_Base->IsChildOf(m_pData) )
+					ReceivedCommandForChild(pDeviceData_Base,sCMD_Result,pMessage);
+				else
+					ReceivedUnknownCommand(sCMD_Result,pMessage);
+					if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+					{
+							pMessage->m_bRespondedToMessage=true;
+						Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+						pMessageOut->m_mapParameters[0]=sCMD_Result;
+						SendMessage(pMessageOut);
+					}
+					else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+						SendString(sCMD_Result);
+						}
+					if( sCMD_Result!="UNHANDLED" && sCMD_Result!="UNKNOWN DEVICE" )
+						iHandled++;
+				}
+			}
+			if( iHandled==0 && !pMessage->m_bRespondedToMessage &&
+			(pMessage->m_eExpectedResponse==ER_ReplyMessage || pMessage->m_eExpectedResponse==ER_ReplyString) )
+			{
+				pMessage->m_bRespondedToMessage=true;
+				if( pMessage->m_eExpectedResponse==ER_ReplyMessage )
+				{
+					Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+					pMessageOut->m_mapParameters[0]="UNHANDLED";
+					SendMessage(pMessageOut);
+				}
+				else
+					SendString("UNHANDLED");
+			}
+		}
+		return iHandled!=0;
+	}
+}; // end class
+
+
+}
+#endif
