7300c0e044e3f4bb3a87b0d1c87a15b616b597ae
cmiron <cmiron>
2005-09-16 07:07:50 +0000

    Added SDL.dll too.

--- src/Orbiter/CENet/build_cab.bat
+++ src/Orbiter/CENet/build_cab.bat
@@ -9,6 +9,7 @@ cd Misc
 copy AYGSHELL.DLL c:\$Installers\$OrbiterCE_SDL\
 copy PthreadsCE.dll c:\$Installers\$OrbiterCE_SDL\
 copy logo.gif c:\$Installers\$OrbiterCE_SDL\
+copy SDL.dll c:\$Installers\$OrbiterCE_SDL\
 cd ..
 
 "C:\Program Files\Windows CE Tools\wce420\POCKET PC 2003\Tools\Cabwiz.exe" OrbiterCE_SDL.inf
