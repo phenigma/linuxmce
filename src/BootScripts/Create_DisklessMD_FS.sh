48cf48ec501316c814f7001256cc577da5120cac
johnny <affordablewindturbines@gmail.com>
2005-09-19 08:46:46 +0000

    Broadcom B440x NIC driver (b44)

--- src/BootScripts/Create_DisklessMD_FS.sh
+++ src/BootScripts/Create_DisklessMD_FS.sh
@@ -16,7 +16,7 @@ FSarchive=PlutoMD.tar.bz2
 DlPath="/usr/pluto/diskless/$IP"
 HexIP=$(gethostip -x "$IP")
 
-Modules="pcnet32 tg3 e1000 sk98lin fealnx natsemi r8169"
+Modules="pcnet32 tg3 e1000 sk98lin fealnx natsemi r8169 b44"
 
 KERNEL_VERSION="$(uname -r)"
 
