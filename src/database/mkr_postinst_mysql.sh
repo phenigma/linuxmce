210f2816d81999fc97e8815728cca75c798d0d12
radu <radu.cx+linuxmce@gmail.com>
2005-09-20 13:20:03 +0000

    Debian changed the default conf. Adapted script

--- src/database/mkr_postinst_mysql.sh
+++ src/database/mkr_postinst_mysql.sh
@@ -4,6 +4,7 @@
 MyCnf=/etc/mysql/my.cnf
 DefTableType=innodb
 sed -i "s/^skip-networking/#skip-networking/; s/^skip-innodb/#skip-innodb/; s/^default-table-type=.*$/default-table-type=$DefTableType/" "$MyCnf"
+sed -i "s/^bind-address.*$/bind-address=0.0.0.0/" "$MyCnf"
 grep -q '^default-table-type=' "$MyCnf" || echo "default-table-type=$DefTableType" >>"$MyCnf"
 echo "GRANT ALL PRIVILEGES ON pluto_main.* to 'root'@'127.0.0.1';" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
