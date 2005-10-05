5912165f7060239b34f9329e01381f12b3e9ff98
radu <radu.cx+linuxmce@gmail.com>
2005-10-05 12:43:51 +0000

    debian package build directory

---
+++ external/lirc-0.7.2/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the lirc package
+#
+0 4	* * *	root	lirc_maintenance
---
+++ external/lirc-0.7.2/debian/lirc-default.ex
@@ -0,0 +1,10 @@
+# Defaults for lirc initscript
+# sourced by /etc/init.d/lirc
+# installed at /etc/default/lirc by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
