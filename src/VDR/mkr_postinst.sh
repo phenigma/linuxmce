cfbe6f5dbfcdd564768f757d165739460ac528be
radu <radu.cx+linuxmce@gmail.com>
2005-10-05 16:22:04 +0000

    postinstall script move vdr directories and config and make symlinks, where needed

--- src/VDR/mkr_postinst.sh
+++ src/VDR/mkr_postinst.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+. /usr/pluto/bin/Config_Ops.sh
 
+if [[ -z "$PK_Device" ]]; then
+	echo "Empty PK_Device!"
+	exit 1
+fi
+
+TargetLocation="/home/vdr/md_${PK_Device}"
+
+if mkdir -p "$TargetLocation/" && pushd /var/lib &>/dev/null; then
+	DirsToMove=$(find -maxdepth 1 -name 'video*' -not -type l)
+	for Dir in $DirsToMove; do
+		Dir=${Dir#./}
+		mv "$Dir" "$TargetLocation/"
+		ln -s "$TargetLocation/$Dir" .
+	done
+	popd &>/dev/null
+fi
+
+TimersConf=/var/lib/vdrdevel/timers.conf
+TargetLocation=/home/vdr
+
+if [[ ! -L "$TimersConf" && ! -e "$TargetLocation/timers.conf" ]]; then
+	mkdir -p "$TargetLocation"
+	mv "$TimersConf" "$TargetLocation/"
+	ln -s "$TargetLocation/timers.conf" "$TimersConf"
+fi
