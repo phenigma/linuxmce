7f6f0a6c1102206659d4d2c168bae4207bb6dcd8
radu <radu.cx+linuxmce@gmail.com>
2005-10-05 11:56:58 +0000

    removed kernel dir

--- external/lirc-0.7.2/config-pluto.sh
+++ external/lirc-0.7.2/config-pluto.sh
@@ -1,1 +1,2 @@
-./configure --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --infodir=/usr/share/info --with-kerneldir=/home/src/2.6.13/linux-2.6.13.2 --with-driver=any
+#!/bin/bash
+./configure --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --infodir=/usr/share/info --with-driver=any --enable-sandboxed
