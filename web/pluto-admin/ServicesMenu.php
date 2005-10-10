3c5c0217744fdd3a7d6147f6612cb937f88790ea
cmiron <cmiron>
2005-10-10 14:05:51 +0000

    Added ip phones services menu.

---
+++ web/pluto-admin/ServicesMenu.php
@@ -0,0 +1,13 @@
+<?php
+Header( "Content-type: text/xml");
+echo "<?xml version='1.0'?>";
+?>
+
+<CiscoIPPhoneMenu>
+  <Title>SERVICES</Title>
+  <Prompt>Make Your Selection...</Prompt>
+  <MenuItem>
+    <Name>Orbiter</Name>
+    <URL>http://192.168.80.1/pluto-admin/index.php?section=proxySocket&amp;address=192.168.80.1&amp;port=3451&amp;command=XML</URL>
+  </MenuItem>
+</CiscoIPPhoneMenu>
