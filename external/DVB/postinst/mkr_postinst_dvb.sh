b63cbdd1982fd4e21f59efdac784dafd723cbf43
aaron <aaron.b@plutohome.com>
2005-10-20 08:57:33 +0000

    vdr postinst

--- external/DVB/postinst/mkr_postinst_dvb.sh
+++ external/DVB/postinst/mkr_postinst_dvb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /etc/init.d/vdrdevel ]; then
+if [ -f /etc/init.d/vdrdevel ]; then
 	sed 's/DAEMON=\/usr\/sbin\/runvdrdevel/DAEMON=\/usr\/sbin\/runvdrdevelserver/g' /etc/init.d/vdrdevel > /etc/init.d/vdrdevel.tmp
 	sed 's/PIDFILE=\/var\/run\/runvdrdevel.pid/PIDFILE=\/var\/run\/runvdrdevelserver.pid/g' /etc/init.d/vdrdevel.tmp > /etc/init.d/vdrdevel
 	rm /etc/init.d/vdrdevel.tmp
