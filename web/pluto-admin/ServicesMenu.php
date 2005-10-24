601ada3cc1ff503fe4f6d2c5bdad461b1f8a83e0
cmiron <cmiron>
2005-10-24 07:37:27 +0000

    Flexible ServicesMenu for 7970 phone to allow multiple phones with multiple proxy orbiters.

--- web/pluto-admin/ServicesMenu.php
+++ web/pluto-admin/ServicesMenu.php
@@ -8,6 +8,6 @@ echo "<?xml version='1.0'?>";
   <Prompt>Make Your Selection...</Prompt>
   <MenuItem>
     <Name>Orbiter</Name>
-    <URL>http://192.168.80.1/pluto-admin/index.php?section=proxySocket&amp;address=192.168.80.1&amp;port=3451&amp;command=XML</URL>
+    <URL>http://192.168.80.1/pluto-admin/index.php?section=proxySocket&amp;command=XML</URL>
   </MenuItem>
 </CiscoIPPhoneMenu>
