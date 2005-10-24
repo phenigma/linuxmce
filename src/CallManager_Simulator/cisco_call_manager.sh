229f43e94ec5d917e3e44335a51f4e572a4456e8
cmiron <cmiron>
2005-10-24 07:43:59 +0000

    Added script to create /tftpboot/XmlDefault.cnf.xml

--- src/CallManager_Simulator/cisco_call_manager.sh
+++ src/CallManager_Simulator/cisco_call_manager.sh
@@ -1,1 +1,17 @@
-echo "doing nothing yet"
+echo "<?xml version=\"1.0\"?>
+<Default>
+<callManagerGroup>
+<members>
+<member priority=\"0\">
+<callManager>
+<ports>
+<ethernetPhonePort>2000</ethernetPhonePort>
+</ports>
+<processNodeName>192.168.80.1</processNodeName>
+</callManager>
+</member>
+</members>
+</callManagerGroup>
+<servicesURL>http://192.168.80.1/pluto-admin/ServicesMenu.php</servicesURL>
+</Default>" > /tftpboot/XmlDefault.cnf.xml
+
