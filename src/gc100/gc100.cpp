415d5e446b084fcc01f397cafa3ca9ef7170a654
torindan <torindan>
2005-10-25 11:16:13 +0000

    revert to asynchron pthread_cancel

--- src/gc100/gc100.cpp
+++ src/gc100/gc100.cpp
@@ -136,6 +136,7 @@ bool gc100::GetConfig()
 gc100::~gc100()
 //<-dceag-dest-e->
 {
+	pthread_cancel(m_EventThread);     //pthread_cancel is asynchron so call it first and continue cleanup	
 	m_bQuit = true;                    //force quit
 	Sleep(100);                        //wait a little
 	pthread_join(m_EventThread, NULL); //finish
