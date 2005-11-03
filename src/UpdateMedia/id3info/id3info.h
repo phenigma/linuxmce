19965e4421f3bbc6e7c0e213abd1787e1101f2ee
cmiron <cmiron>
2005-11-03 14:50:48 +0000

    Forgot to add file

---
+++ src/UpdateMedia/id3info/id3info.h
@@ -0,0 +1,10 @@
+#ifndef __ID3INFO_H_
+#define __ID3INFO_H_
+
+#include <map>
+#include <string>
+using namespace std;
+
+void GetId3Info(string sFilename, map<int,string>& mapAttributes);
+
+#endif
