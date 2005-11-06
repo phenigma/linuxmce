b1d84ba5da38aa1ad2bff685d527bea045e4b918
aaron <aaron.b@plutohome.com>
2005-11-06 19:56:44 +0000

    oops

---
+++ src/Gen_Devices/ZWaveBase.cpp
@@ -0,0 +1,37 @@
+#include "ZWaveBase.h"
+#include "DeviceData_Impl.h"
+#include "Logger.h"
+
+using namespace DCE;
+#include "ZWaveBase.h"
+extern ZWave_Command *Create_ZWave(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+DeviceData_Impl *ZWave_Data::CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition)
+{
+	// Peek ahead in the stream.  We're going to pass in the above pointers anyway so it won't affect the position
+	SerializeClass b;
+	b.ResumeReadWrite(pDataBlock,AllocatedSize,CurrentPosition);
+	int iPK_Device = b.Read_unsigned_long();
+	int iPK_Installation = b.Read_unsigned_long();
+	int iPK_DeviceTemplate = b.Read_unsigned_long();
+	switch(iPK_DeviceTemplate) {
+		case 1750:
+			return new ZWave_Data();
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateData for unknown type %d.", iPK_DeviceTemplate);
+	return NULL;
+}
+
+Event_Impl *ZWave_Event::CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice )
+{
+	switch(dwPK_DeviceTemplate) {
+		case 1750:
+			return (Event_Impl *) new ZWave_Event(pOCClientSocket, dwDevice);
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateEvent for unknown type %d.", dwPK_DeviceTemplate);
+	return NULL;
+}
+Command_Impl  *ZWave_Command::CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent)
+{
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateCommand for unknown type %d.", PK_DeviceTemplate);
+	return NULL;
+}
