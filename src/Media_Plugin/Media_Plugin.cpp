cb057837fa9fecb7b49e31f770a57efc21a619d9
aaron <aaron.b@plutohome.com>
2005-11-07 01:16:12 +0000

    oops

--- src/Media_Plugin/Media_Plugin.cpp
+++ src/Media_Plugin/Media_Plugin.cpp
@@ -1120,7 +1120,7 @@ bool Media_Plugin::ReceivedMessage( class Message *pMessage )
 			if( pMessage->m_dwMessage_Type==MESSAGETYPE_COMMAND && (pMessage->m_dwID==COMMAND_Stop_CONST || pMessage->m_dwID==COMMAND_Stop_Media_CONST) )
 			{
 				DCE::CMD_Eject_Disk_Cat CMD_Eject_Disk_Cat(pMessage->m_dwPK_Device_From,DEVICECATEGORY_Disc_Drives_CONST,true,BL_SameComputer);
-				SendCommand(CMD_Eject_Cat);
+				SendCommand(CMD_Eject_Disk_Cat);
 				return true;
 			}
             g_pPlutoLogger->Write( LV_CRITICAL, "An orbiter sent the media handler message type: %d id: %d, but it's not for me and I can't find a stream in it's entertainment area", pMessage->m_dwMessage_Type, pMessage->m_dwID );
