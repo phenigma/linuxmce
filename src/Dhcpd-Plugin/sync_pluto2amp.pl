d66e079f81b9d109fdf94c063b5a83f99eb602e2
torindan <torindan>
2005-11-07 11:17:08 +0000

    Changes in sync script

--- src/Dhcpd-Plugin/sync_pluto2amp.pl
+++ src/Dhcpd-Plugin/sync_pluto2amp.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/perl
 
 use strict;
-use diagnostics;
+#use diagnostics;
 use DBI;
 
 #declare vars (it's safer this way)
 my $DEVICE_ID=0;
 my $DEVICE_EXT = 0;
-my $DEVICE_TYPE = "SIP"; #default is SIP phone
-my $DEVICE_PORT = 5060;  #default for SIP
+my $DEVICE_TYPE = ""; #default is SIP phone
+my $DEVICE_PORT = 0;  #default for SIP
 
 my $CONF_HOST="localhost";
 my $CONF_USER="root";
@@ -22,8 +22,8 @@ my $STATUS = 0;
 #check params
 unless (defined($ARGV[0]))
 {
-    print "USAGE :$0 <DEVICE_ID>\n";
-    exit(-1);
+	print "USAGE :$0 <DEVICE_ID>\n";
+	exit(-1);
 }
 &read_pluto_config();
 
@@ -50,6 +50,7 @@ foreach my $I (@ARGV)
 			&update_device_data();
 		}
 		$DEVICE_PORT = 4569 if($DEVICE_TYPE =~ /^iax/i);
+		$DEVICE_PORT = 5060 if($DEVICE_TYPE =~ /^sip/i);
 		&update_asterisk_db();
 	}
 }
@@ -75,19 +76,19 @@ sub read_pluto_config()
 	while(<CONF>)
 	{
 		chomp;
-    	my ($option, $eq, $value) = split(/ /,$_);
-    	if($option eq "MySqlHost")
-    	{
-        	$CONF_HOST=$value;
-    	}
-    	elsif ($option eq "MySqlUser")
-    	{
-        	$CONF_USER=$value;
-    	}
-    	elsif ($option eq "MySqlPassword")
-    	{
-        	$CONF_PASSWD=$value;
-    	}
+		my ($option, $eq, $value) = split(/ /,$_);
+		if($option eq "MySqlHost")
+		{
+			$CONF_HOST=$value;
+		}
+		elsif ($option eq "MySqlUser")
+		{
+			$CONF_USER=$value;
+		}
+		elsif ($option eq "MySqlPassword")
+		{
+			$CONF_PASSWD=$value;
+		}
 	}
 	close(CONF);
 }
@@ -96,13 +97,12 @@ sub read_pluto_device_data()
 {
 	my $DB_SQL;
 	my $DB_STATEMENT;
-	my $DB_ROW;
 
 	$DB_SQL = "select FK_Device, IK_DeviceData, FK_DeviceData from Device_DeviceData where FK_Device=$DEVICE_ID and (FK_DeviceData=31 or FK_DeviceData=29)";
 	$DB_STATEMENT = $DB_PL_HANDLE->prepare($DB_SQL) or die "Couldn't prepare query '$DB_SQL': $DBI::errstr\n";
 	$DB_STATEMENT->execute() or die "Couldn't execute query '$DB_SQL': $DBI::errstr\n";
-	$STATUS = -1 unless($DB_STATEMENT->fetchrow_hashref());
-	while($DB_ROW = $DB_STATEMENT->fetchrow_hashref())
+	$STATUS = -1;
+	while(my $DB_ROW = $DB_STATEMENT->fetchrow_hashref())
 	{
 		if($DB_ROW->{'FK_DeviceData'} == 31)
 		{
@@ -112,8 +112,9 @@ sub read_pluto_device_data()
 		{
 			$DEVICE_TYPE = $DB_ROW->{'IK_DeviceData'};
 		}
+		$STATUS=0;
 	}
-
+	$DB_STATEMENT->finish();
 }
 
 sub find_next_extension()
@@ -127,19 +128,19 @@ sub find_next_extension()
 	$DB_STATEMENT->execute() or die "Couldn't execute query '$DB_SQL': $DBI::errstr\n";
 	while($DB_ROW = $DB_STATEMENT->fetchrow_hashref())
 	{
-    	my $extention = $DB_ROW->{'IK_DeviceData'};
-    	if($extention > $DEVICE_EXT)
-    	{
-        	$DEVICE_EXT = $extention;
-    	}
+	    	my $extention = $DB_ROW->{'IK_DeviceData'};
+	    	if($extention > $DEVICE_EXT)
+	    	{
+        		$DEVICE_EXT = $extention;
+	    	}
 	}
 	if($DEVICE_EXT == 0)
 	{
-    	$DEVICE_EXT = 200; #200 is default for AMP
+    		$DEVICE_EXT = 200; #200 is default for AMP
 	}
 	else
 	{
-    	$DEVICE_EXT = $DEVICE_EXT+1;
+	    	$DEVICE_EXT = $DEVICE_EXT+1;
 	}
 }
 
@@ -156,7 +157,6 @@ sub update_device_data()
 	$DB_SQL = "update Device_DeviceData SET IK_DeviceData='$DEVICE_TYPE' WHERE FK_Device='$DEVICE_ID' AND FK_DeviceData='29'";
 	$DB_STATEMENT = $DB_PL_HANDLE->prepare($DB_SQL) or die "Couldn't prepare query '$DB_SQL': $DBI::errstr\n";
 	$DB_STATEMENT->execute() or die "Couldn't execute query '$DB_SQL': $DBI::errstr\n";
-
 }
 
 sub update_asterisk_db()
@@ -197,9 +197,10 @@ sub add_to_asterisk_db()
 	
 	unless($DEVICE_TYPE =~/^(iax|sip|zap|custom)$/)
 	{
-		print "Unsupported device type : DEVICE_TYPE\n";
+		print "Unsupported device type : \"$DEVICE_TYPE\"\n";
 		return;
 	}
+	
 	$EXT_VARS{'display'}="extensions";
 	$EXT_VARS{'extdisplay'}="";
 	$EXT_VARS{'action'}="add";
