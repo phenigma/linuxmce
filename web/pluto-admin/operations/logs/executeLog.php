966826fffa7ccbfc80ecb5f4508a994577814eef
radu <radu.cx+linuxmce@gmail.com>
2005-11-18 17:36:10 +0000

    call DHCP setup script

--- web/pluto-admin/operations/logs/executeLog.php
+++ web/pluto-admin/operations/logs/executeLog.php
@@ -3,6 +3,7 @@ $scriptID=$_REQUEST['script'];
 switch($scriptID){
 	case 1:
 		$command[]='sudo -u root /usr/pluto/bin/Diskless_Setup.sh';
+		$command[]='sudo -u root /usr/pluto/bin/DHCP_config.sh';
 		$title='Please wait for the setup process to complete, then do a ‘quick reload router’, and then you can bootup your new diskless media director.';
 	break;
 	case 2:
