d6be3a3828dbe9010638a509e2be6ff4815bb25c
cmiron <cmiron>
2005-11-24 11:03:11 +0000

    cl:ScreenGen: Added ScreenGen project. It will be used to generated screens objects (the "AllScreen.h" header file in Gen_Devices)

---
+++ src/ScreenGen/AllScreensTemplate.h
@@ -0,0 +1,42 @@
+#ifndef AllScreens_h
+#define AllScreens_h
+#include "Message.h"
+#include "PreformedCommand.h"
+
+// Orbiter will create an instance of a class derived from ScreenHandler
+// It must call ReceivedGotoScreenMessage when it receives such a message
+// and it must implement GotoDesignObj, as well as override any
+// screens that have to do something, like SCREEN_DisplayMessageToUser
+
+namespace DCE
+{
+	 //  Commands
+$GEN_CLASSES$
+	class ScreenHandlerBase
+	{
+		map<int,int> *m_p_MapDesignObj;
+
+		int m_p_MapDesignObj_Find(int PK_Screen) { map<int,int>::iterator it = m_p_MapDesignObj->find(PK_Screen); return it==m_p_MapDesignObj->end() ? NULL : (*it).second; }
+		ScreenHandlerBase(map<int,int> *p_MapDesignObj) { m_p_MapDesignObj=p_MapDesignObj; }
+
+		virtual void GotoDesignObj(int PK_DesignObj)=0;
+		virtual void GotoScreen(int PK_Screen)
+		{
+			int PK_DesignObj = m_p_MapDesignObj_Find(PK_Screen);
+			if( PK_DesignObj )
+				GotoDesignObj(PK_DesignObj);
+		}
+
+		/*  Display a message for the user */
+$GEN_METHODS$
+		virtual void ReceivedGotoScreenMessage(Message *pMessage)
+		{
+			switch(pMessage->m_dwID)
+			{
+$GEN_SWITCH_BLOCK$
+			}
+		}
+	};
+}
+
+#endif
---
+++ src/ScreenGen/ScreenGen.cpp
@@ -0,0 +1,344 @@
+#include <iostream>
+#include <fstream>
+
+#include "PlutoUtils/CommonIncludes.h"	
+#include "PlutoUtils/FileUtils.h"
+#include "PlutoUtils/StringUtils.h"
+#include "PlutoUtils/Other.h"
+#include "DCE/Logger.h"
+
+#include "pluto_main/Database_pluto_main.h"
+#include "pluto_main/Table_Screen.h"
+#include "pluto_main/Table_Screen_CommandParameter.h"
+
+//uncomment next line to see if AllScreens.h is compilable
+//#include "Gen_Devices/AllScreens.h"
+
+#define  VERSION "<=version=>"
+//-----------------------------------------------------------------------------------------------------
+namespace DCE
+{
+	class Logger *g_pPlutoLogger;
+}
+//-----------------------------------------------------------------------------------------------------
+static string g_sAppPath;
+static const string csTemplateFile = "AllScreensTemplate.h";
+//-----------------------------------------------------------------------------------------------------
+class ScreenGenerator
+{
+	Database_pluto_main *m_pDatabase_pluto_main;
+	string m_sOutputPath;
+
+	string GenerateClasses();
+	string GenerateMethods();
+	string GenerateSwitchBlock();
+
+public:
+	ScreenGenerator(Database_pluto_main *pDatabase_pluto_main, string sOutputPath);
+	bool GenerateScreensFile();
+};
+//-----------------------------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+	g_sAppPath = FileUtils::BasePath(argv[0]); 
+	g_pPlutoLogger=new FileLogger(stdout);
+
+	string DBHost="dcerouter",DBUser="root",DBPassword="",DBName="pluto_main";	
+	int DBPort=3306;
+	string OutputPath = "Gen_Devices";
+
+	bool bError=false; // An error parsing the command line
+	char c;
+	for(int optnum=1;optnum<argc;++optnum)
+	{
+		if( argv[optnum][0]!='-' )
+		{
+			cerr << "Unknown option " << argv[optnum] << endl;
+			exit(1);
+		}
+
+		c=argv[optnum][1];
+		switch (c)
+		{
+		case 'h':
+			DBHost = argv[++optnum];
+			break;
+		case 'u':
+			DBUser = argv[++optnum];
+			break;
+		case 'p':
+			DBPassword = argv[++optnum];
+			break;
+		case 'D':
+			DBName = argv[++optnum];
+			break;
+		case 'P':
+			DBPort = atoi(argv[++optnum]);
+			break;
+		case 'o':
+			OutputPath = argv[++optnum];
+			break;
+		default:
+			bError=true;
+			break;
+		};
+	}
+
+	if (bError)
+	{
+		cout << "ScreenGen, v." << VERSION << endl
+			<< "Usage: ScreenGen [-o output path] [-h hostname] [-u username] [-p password] [-D database] [-P mysql port]" << endl
+			<< "-o output    -- path to put output file" << endl
+			<< "-h hostname  -- address or DNS of database host, default is `localhost`" << endl
+			<< "-u username  -- username for database connection" << endl
+			<< "-p password  -- password for database connection, default is `` (empty)" << endl
+			<< "-D database  -- database name" << endl
+			<< "-P port      -- port for database connection, default is 3306" << endl;
+		exit(0);
+	}
+
+	Database_pluto_main mds; 
+	if(!mds.Connect(DBHost, DBUser, DBPassword, DBName, DBPort))
+	{
+		g_pPlutoLogger->Write(LV_CRITICAL, "Failed to connect to database (host %s, user %s, password %s, dbname %s, "
+			"port %d", DBHost.c_str(), DBUser.c_str(), DBPassword.c_str(), DBName.c_str(), DBPort);
+		return 1;
+	}
+
+	auto_ptr<ScreenGenerator> psScreenGenerator(new ScreenGenerator(&mds, OutputPath));
+	if(!psScreenGenerator->GenerateScreensFile())
+	{
+		g_pPlutoLogger->Write(LV_CRITICAL, "Failed to generate 'AllScreens.h' file. Aborting...");
+		return 2;
+	}
+
+	return 0;
+}
+//-----------------------------------------------------------------------------------------------------
+ScreenGenerator::ScreenGenerator(Database_pluto_main *pDatabase_pluto_main, string sOutputPath) 
+{
+	m_pDatabase_pluto_main = pDatabase_pluto_main;
+	m_sOutputPath = sOutputPath;
+}
+//-----------------------------------------------------------------------------------------------------
+bool ScreenGenerator::GenerateScreensFile() 
+{
+	string sTemplateFile = csTemplateFile;
+	string sGeneratedData;
+
+	if(!FileUtils::FileExists(sTemplateFile))
+	{
+		sTemplateFile = FileUtils::BasePath(FileUtils::IncludeTrailingSlash(g_sAppPath)) + "ScreenGen/" + sTemplateFile;
+
+		if(!FileUtils::FileExists(sTemplateFile))
+		{
+			sTemplateFile = "../" + sTemplateFile;
+
+			if(!FileUtils::FileExists(sTemplateFile))
+			{
+				g_pPlutoLogger->Write(LV_CRITICAL, "Unable to find template file '%s'", csTemplateFile.c_str());
+				return false;
+			}
+		}
+	}
+
+	if(FileUtils::ReadTextFile(sTemplateFile, sGeneratedData))
+	{
+		string sClasses = GenerateClasses();
+		string sMethods = GenerateMethods();
+		string sSwitchBlock = GenerateSwitchBlock();
+
+		sGeneratedData = StringUtils::Replace(sGeneratedData, "$GEN_CLASSES$", sClasses);
+		sGeneratedData = StringUtils::Replace(sGeneratedData, "$GEN_METHODS$", sMethods);
+		sGeneratedData = StringUtils::Replace(sGeneratedData, "$GEN_SWITCH_BLOCK$", sSwitchBlock);
+	}
+
+	if(!FileUtils::DirExists(m_sOutputPath))
+	{
+		m_sOutputPath = FileUtils::IncludeTrailingSlash(FileUtils::BasePath(g_sAppPath)) + "Gen_Devices";
+		if(!FileUtils::DirExists(m_sOutputPath))
+		{
+			m_sOutputPath = "../Gen_Devices";
+			if(!FileUtils::DirExists(m_sOutputPath))
+			{
+				g_pPlutoLogger->Write(LV_CRITICAL, "Unable to save generated file. Cannot find 'Gen_Devices' folder");    
+				return false;
+			}
+		}
+	}
+
+	string sOutputFile = FileUtils::IncludeTrailingSlash(m_sOutputPath) + "AllScreens.h";
+	if(!FileUtils::WriteTextFile(sOutputFile, sGeneratedData))
+	{
+		g_pPlutoLogger->Write(LV_CRITICAL, "Cannot write file '%s'", sOutputFile.c_str());    
+		return false;
+	}
+    
+	return true;
+}
+//-----------------------------------------------------------------------------------------------------
+string ScreenGenerator::GenerateClasses()
+{
+	g_pPlutoLogger->Write(LV_WARNING, "Generating classes...");
+
+	string sGeneratedClassesData;
+	vector<Row_Screen *> vectRow_Screen;
+	m_pDatabase_pluto_main->Screen_get()->GetRows("1=1", &vectRow_Screen);
+
+	for(vector<Row_Screen *>::iterator it = vectRow_Screen.begin(); it != vectRow_Screen.end(); it++)
+	{
+		string sClass;
+		Row_Screen *pRow_Screen = *it;
+		string sClassName = "SCREEN_" + StringUtils::Replace(pRow_Screen->Description_get(), " ", "_");
+
+		g_pPlutoLogger->Write(LV_STATUS, "Generating class %s...", sClassName.c_str());
+
+		string sConstructorParams;
+		string sParams;
+
+		long nMessageIndex = 1;
+		vector<Row_Screen_CommandParameter *> vectRow_Screen_CommandParameter;
+		m_pDatabase_pluto_main->Screen_CommandParameter_get()->GetRows("FK_Screen = " + 
+			StringUtils::ltos(pRow_Screen->PK_Screen_get()), &vectRow_Screen_CommandParameter);
+
+		for(vector<Row_Screen_CommandParameter *>::iterator itp = vectRow_Screen_CommandParameter.begin(); 
+			itp != vectRow_Screen_CommandParameter.end(); itp++)
+		{
+			Row_Screen_CommandParameter *pCommandParameter = *itp;
+			
+			sConstructorParams += "string sMessage" + StringUtils::ltos(nMessageIndex);
+			sParams += StringUtils::ltos(pCommandParameter->FK_CommandParameter_get()) + 
+				" /* command parm for message " + StringUtils::ltos(nMessageIndex) + " */, sMessage" + 
+				StringUtils::ltos(nMessageIndex);
+
+			if(nMessageIndex < long(vectRow_Screen_CommandParameter.size()))
+			{
+				sConstructorParams += ", ";
+				sParams += ", ";
+			}
+
+			nMessageIndex++;
+		}
+
+		string sPK_Screen = StringUtils::ltos(pRow_Screen->PK_Screen_get());
+		string sClassData = 
+			"\tclass " + sClassName + " : public PreformedCommand\r\n"
+			"\t{\r\n"
+			"\tpublic:\r\n"
+			"\t\t" + sClassName + "(long DeviceIDFrom, long DeviceIDTo" + (vectRow_Screen_CommandParameter.size() ? ",\r\n" : "") +
+			(vectRow_Screen_CommandParameter.size() ? "\t\t\t" + sConstructorParams + ")" : ")") + "\r\n"
+			"\t\t{\r\n"
+			"\t\t\tm_pMessage = new Message(DeviceIDFrom, DeviceIDTo, MESSAGETYPE_COMMAND, PRIORITY_NORMAL, 1, 7 /* goto screen */,\r\n"
+//			"\t\t\t3, \r\n" //what's this?
+			"\t\t\t\t10 /* PK_Screen */," + sPK_Screen + " /* screen ID */" + 
+			(vectRow_Screen_CommandParameter.size() ? ",\r\n\t\t\t\t" + sParams : "") + ");\r\n"
+			"\t\t}\r\n"
+			"\t};\r\n"
+			"\r\n";
+
+		sGeneratedClassesData += sClassData;
+	}
+
+	return sGeneratedClassesData;
+}
+//-----------------------------------------------------------------------------------------------------
+string ScreenGenerator::GenerateMethods()
+{
+	g_pPlutoLogger->Write(LV_WARNING, "Generating methods...");
+
+	string sGeneratedMethodsData;
+
+	vector<Row_Screen *> vectRow_Screen;
+	m_pDatabase_pluto_main->Screen_get()->GetRows("1=1", &vectRow_Screen);
+
+	for(vector<Row_Screen *>::iterator it = vectRow_Screen.begin(); it != vectRow_Screen.end(); it++)
+	{
+		string sClass;
+		Row_Screen *pRow_Screen = *it;
+		string sMethodName = "SCREEN_" + StringUtils::Replace(pRow_Screen->Description_get(), " ", "_");
+		string sPK_Screen = StringUtils::ltos(pRow_Screen->PK_Screen_get());
+
+		g_pPlutoLogger->Write(LV_STATUS, "Generating method %s...", sMethodName.c_str());
+
+		string sParams;
+
+		long nMessageIndex = 1;
+		vector<Row_Screen_CommandParameter *> vectRow_Screen_CommandParameter;
+		m_pDatabase_pluto_main->Screen_CommandParameter_get()->GetRows("FK_Screen = " + 
+			StringUtils::ltos(pRow_Screen->PK_Screen_get()), &vectRow_Screen_CommandParameter);
+
+		for(vector<Row_Screen_CommandParameter *>::iterator itp = vectRow_Screen_CommandParameter.begin(); 
+			itp != vectRow_Screen_CommandParameter.end(); itp++)
+		{
+			Row_Screen_CommandParameter *pCommandParameter = *itp;
+
+			sParams += "string sMessage" + StringUtils::ltos(nMessageIndex);
+			if(nMessageIndex < long(vectRow_Screen_CommandParameter.size()))
+				sParams += ", ";
+
+			nMessageIndex++;
+		}
+
+		string sMethodData = "\t\tvirtual void " + sMethodName + "(" + sParams + 
+			") { GotoScreen(" + sPK_Screen + "); }\r\n";
+
+		sGeneratedMethodsData += sMethodData;
+	}
+
+	return sGeneratedMethodsData;
+}
+//-----------------------------------------------------------------------------------------------------
+string ScreenGenerator::GenerateSwitchBlock()
+{
+	g_pPlutoLogger->Write(LV_WARNING, "Generating cases for switch block...");
+
+	string sGeneratedCasesData;
+
+	vector<Row_Screen *> vectRow_Screen;
+	m_pDatabase_pluto_main->Screen_get()->GetRows("1=1", &vectRow_Screen);
+
+	for(vector<Row_Screen *>::iterator it = vectRow_Screen.begin(); it != vectRow_Screen.end(); it++)
+	{
+		string sClass;
+		Row_Screen *pRow_Screen = *it;
+		string sMethodName = "SCREEN_" + StringUtils::Replace(pRow_Screen->Description_get(), " ", "_");
+		string sPK_Screen = StringUtils::ltos(pRow_Screen->PK_Screen_get());
+
+		g_pPlutoLogger->Write(LV_STATUS, "Generating case for %s...", sPK_Screen.c_str());
+
+		string sParams;
+		string sMessageParamsLines;
+
+		long nMessageIndex = 1;
+		vector<Row_Screen_CommandParameter *> vectRow_Screen_CommandParameter;
+		m_pDatabase_pluto_main->Screen_CommandParameter_get()->GetRows("FK_Screen = " + 
+			StringUtils::ltos(pRow_Screen->PK_Screen_get()), &vectRow_Screen_CommandParameter);
+
+		for(vector<Row_Screen_CommandParameter *>::iterator itp = vectRow_Screen_CommandParameter.begin(); 
+			itp != vectRow_Screen_CommandParameter.end(); itp++)
+		{
+			Row_Screen_CommandParameter *pCommandParameter = *itp;
+
+			sParams += "sMessage" + StringUtils::ltos(nMessageIndex);
+			sMessageParamsLines += "\t\t\t\t\tstring sMessage" + StringUtils::ltos(nMessageIndex) + " = pMessage->m_mapParameters[" +
+				StringUtils::ltos(pCommandParameter->FK_CommandParameter_get()) + "];\r\n";
+			if(nMessageIndex < long(vectRow_Screen_CommandParameter.size()))
+				sParams += ", ";
+
+			nMessageIndex++;
+		}
+
+		string sCaseData = 
+			"\t\t\t\tcase " + sPK_Screen + ":\r\n" + 
+			"\t\t\t\t{\r\n" +
+			sMessageParamsLines + 
+			"\t\t\t\t\t" + sMethodName + "(" + sParams + ");\r\n"
+			"\t\t\t\t\tbreak;\r\n"
+			"\t\t\t\t}\r\n";
+
+		sGeneratedCasesData += sCaseData;
+	}
+
+	return sGeneratedCasesData;
+}
+//-----------------------------------------------------------------------------------------------------
