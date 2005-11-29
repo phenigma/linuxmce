b8fa1d2d0e97b15970fb6d26fc3e90ae331a4f64
torindan <torindan>
2005-11-29 13:37:08 +0000

    Add sending DTMF

--- src/Gen_Devices/SimplePhoneBase.h
+++ src/Gen_Devices/SimplePhoneBase.h
@@ -132,6 +132,7 @@ public:
 	//Event accessors
 	void EVENT_Incoming_Call() { GetEvents()->Incoming_Call(); }
 	//Commands - Override these to handle commands from the server
+	virtual void CMD_Simulate_Keypress(string sPK_Button,string sName,string &sCMD_Result,class Message *pMessage) {};
 	virtual void CMD_Phone_Initiate(string sPhoneExtension,string &sCMD_Result,class Message *pMessage) {};
 	virtual void CMD_Phone_Answer(string &sCMD_Result,class Message *pMessage) {};
 	virtual void CMD_Phone_Drop(string &sCMD_Result,class Message *pMessage) {};
@@ -150,6 +151,33 @@ public:
 			{
 				switch(pMessage->m_dwID)
 				{
+				case 28:
+					{
+						string sCMD_Result="OK";
+					string sPK_Button=pMessage->m_mapParameters[26];
+					string sName=pMessage->m_mapParameters[50];
+						CMD_Simulate_Keypress(sPK_Button.c_str(),sName.c_str(),sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							pMessageOut->m_mapParameters[0]=sCMD_Result;
+							SendMessage(pMessageOut);
+						}
+						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							SendString(sCMD_Result);
+						}
+						if( (itRepeat=pMessage->m_mapParameters.find(72))!=pMessage->m_mapParameters.end() )
+						{
+							int iRepeat=atoi(pMessage->m_mapParameters[72].c_str());
+							for(int i=2;i<=iRepeat;++i)
+								CMD_Simulate_Keypress(sPK_Button.c_str(),sName.c_str(),sCMD_Result,pMessage);
+						}
+					};
+					iHandled++;
+					continue;
 				case 334:
 					{
 						string sCMD_Result="OK";
