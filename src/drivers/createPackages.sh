07470b0622792bb97c4755992be27dfbb3e3920e
radu <radu.cx+linuxmce@gmail.com>
2005-12-02 18:45:42 +0000

    I think this file is not used at all, but update it anyway

--- src/drivers/createPackages.sh
+++ src/drivers/createPackages.sh
@@ -5,13 +5,13 @@ REPO_HOME=/home/samba/repositories/pluto/replacements/main/binary-i386
 make -C ivtv/ clean 
 make -C ivtv/ deb
 
-make -f Makefile.pluto -C video4linux clean 
-make -f Makefile.pluto -C video4linux deb
+make -f Makefile.pluto -C v4l-dvb/v4l clean 
+make -f Makefile.pluto -C v4l-dvb/v4l deb
 
 
 if [ -d "$REPO_HOME" ]; then
 	cp ivtv/*.deb "$REPO_HOME";
-	cp video4linux/*.deb "$REPO_HOME";
+	cp v4l-dvb/v4l/*.deb "$REPO_HOME";
 	pushd "$REPO_HOME";
 	./update-repository
 	popd
