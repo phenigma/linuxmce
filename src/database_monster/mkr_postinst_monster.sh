6cbcd375a6beb48c2f1ed5c6ee939c4707b5122d
aaron <aaron.b@plutohome.com>
2005-12-06 14:18:16 +0000

    monster db

---
+++ src/database_monster/mkr_postinst_monster.sh
@@ -0,0 +1,9 @@
+echo 
+echo 
+echo Setting up monster database.
+echo ****This may take a long time****
+echo This involves merging all the changes from the
+echo central database back into your local copy.
+echo Please be patient.......
+(cd /usr/pluto/database_monster; /usr/pluto/bin/sqlCVS -S 3 -n -D pluto_main -r dce,designer import) || exit $?
+
