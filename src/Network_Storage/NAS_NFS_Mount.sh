cbe4393109964c6a23a837e493df15f3d5497ef1
radu <radu.cx+linuxmce@gmail.com>
2005-12-13 15:20:23 +0000

    unified

--- src/Network_Storage/NAS_NFS_Mount.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. /usr/pluto/bin/NAS_Common.sh
-
-NFS_Options="tcp,intr,rsize=8192,wsize=8192,retrans=10,timeo=50"
-
-NAS_Init "nfs" "$@"
-NAS_GetData
-NAS_Mount "nfs" "$NAS_IP_Address:$NAS_Share_Name" "/mnt/$NAS_Description" "$NFS_Options"
---
+++ src/Network_Storage/Net_Mount.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+. /usr/pluto/bin/pluto.func
+. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/SQL_Ops.sh
+
+DEVICEDATA_Type=47
+DEVICEDATA_TCP_Address=117
+DEVICEDATA_Share_Name=126
+DEVICEDATA_Username=127
+DEVICEDATA_Password=128
+DEVICEDATA_Mount_Point=129
+DEVICEDATA_Use_Pluto_Directory_Structure=130
+
+while [[ "$#" > 0 ]]; do
+	case "$1" in
+		-d) NAS_PK_Device="$2"; shift ;;
+		*) Logging "NAS" $SEVERITY_WARNING "share mount" "Unknown parameter '$1'" ;;
+	esac
+	shift
+done
+
+if [[ -z "$NAS_PK_Device" ]]; then
+	Logging "NAS" $SEVERITY_CRITICAL "share mount" "I didn't receive my device number as a parameter. Ending"
+	exit 1
+fi
+
+SQL="SELECT FK_DeviceData,IK_DeviceData FROM Device_DeviceData
+	WHERE FK_Device=$NAS_PK_Device AND FK_DeviceData IN ($DEVICEDATA_Type,$DEVICEDATA_TCP_Address,$DEVICEDATA_Share_Name,$DEVICEDATA_Username,$DEVICEDATA_Password,$DEVICEDATA_Mount_Point,$DEVICEDATA_Use_Pluto_Directory_Structure)"
+R=$(RunSQL "$SQL")
+
+for Row in $R; do
+	FK_DeviceData=$(Field 1 "$Row")
+	IK_DeviceData=$(Field 2 "$Row")
+
+	case "$FK_DeviceData" in
+		$DEVICEDATA_Type) NAS_Type=$IK_DeviceData ;;
+		$DEVICEDATA_TCP_Address) NAS_IP_Address=$IK_DeviceData ;;
+		$DEVICEDATA_Share_Name) NAS_Share_Name=$IK_DeviceData ;;
+		$DEVICEDATA_Username) NAS_Username=$IK_DeviceData ;;
+		$DEVICEDATA_Password) NAS_Password=$IK_DeviceData ;;
+		$DEVICEDATA_Mount_Point) NAS_Mount_Point=$IK_DeviceData ;;
+		$DEVICEDATA_Use_Pluto_Directory_Structure) NAS_Use_Pluto_Directory_Structure=$IK_DeviceData ;;
+		*) Logging "NAS" $SEVERITY_WARNING "NAS_GetData ($NAS_ShareType)" "Unknown DeviceData '$FK_DeviceData=$IK_DeviceData'" ;;
+	esac
+done
+
+SQL="SELECT Description FROM Device WHERE PK_Device=$NAS_PK_Device"
+R=$(RunSQL "$SQL")
+NAS_Description=$(RunSQL "$R")
+
+Msg="Description: $NAS_Description; Type: $NAS_Type; IP: $NAS_IP_Address; Share name: $NAS_Share_Name; Username: $Username; Mount point: $NAS_MOUNT_Point; Pluto directory structure: $DEVICEDATA_Use_Pluto_Directory_Structure"
+Logging "NAS" $SEVERITY_NORMAL "share mount" "$Msg"
+
+if [[ -z "$NAS_IP_Address" || -z "$NAS_Share_Name" ]]; then
+	Logging "NAS" $SEVERITY_CRITICAL "share mount" "Missing important mount parameters"
+	exit 1
+fi
+
+local Type="$1" Src="$2" Dst="$2" Opts="$3"
+
+Dst="/mnt/${NAS_Description}_${NAS_PK_Device}"
+mkdir -p "$Dst"
+
+case "$NAS_Type" in
+	nfs)
+		FS="nfs"
+		Src="$NAS_IP_Address:$NAS_Share_Name"
+		Opts="tcp,intr,rsize=32768,wsize=32768,retrans=10,timeo=50"
+	;;
+	samba|smbfs)
+		FS="smbfs"
+		Opts="password=$NAS_Password"
+		Src="//$NAS_IP_Address/$NAS_Share_Name"
+		[[ -n "$NAS_Username" ]] && Opts="$Opts,username=$NAS_Username"
+	;;
+	*)
+	;;
+esac
+
+if ! mount -t "$Type" -o "$Opts" "$Src" "$Dst"; then
+	Logging "NAS" $SEVERITY_CRITICAL "share mount" "Mount failed"
+else
+	Logging "NAS" $SEVERITY_CRITICAL "share mount" "Mount succeeded"
+fi
