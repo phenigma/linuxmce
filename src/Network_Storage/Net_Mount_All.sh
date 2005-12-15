d9f29e1e435c4578a522b2f08aed1c0633a76ec1
radu <radu.cx+linuxmce@gmail.com>
2005-12-15 14:28:30 +0000

    enable NFS on Maxtor at boot

--- src/Network_Storage/Net_Mount_All.sh
+++ src/Network_Storage/Net_Mount_All.sh
@@ -2,10 +2,12 @@
 
 . /usr/pluto/bin/SQL_Ops.sh
 . /usr/pluto/bin/pluto.func
+. /usr/pluto/bin/Maxtor_openmss_EnableNFS.sh
 
 DEVICECATEGORY_Network_Storage=150
+DEVICETEMPLATE_Maxtor_NAS=1770
 
-Q="SELECT PK_Device,D.Description,DT.Description
+Q="SELECT D.PK_Device,D.Description,D.FK_DeviceTemplate,DT.Description,D.IPaddress
 	FROM Device D
 	JOIN DeviceTemplate DT ON PK_DeviceTemplate=FK_DeviceTemplate
 	JOIN DeviceCategory ON PK_DeviceCategory=FK_DeviceCategory
@@ -15,7 +17,13 @@ R=$(RunSQL "$Q")
 for Device in $R; do
 	DevPK=$(Field 1 "$Device")
 	DevDesc=$(Field 2 "$Device")
-	DevTDesc=$(Field 3 "$Device")
+	DevTPK=$(Field 3 "$Device")
+	DevTDesc=$(Field 4 "$Device")
+	DevIP=$(Field 5 "$Device")
+
+	if [[ "$DevTPK" == "$DEVICETEMPLATE_Maxtor_NAS" ]]; then
+		EnableMaxtorNFS "$DevIP"
+	fi
 
 	Logging "Net_Mount_All" $SEVERITY_NORMAL "NAS" "Mounting device '$DevDesc' (PK $DevPK, template: '$DevTDesc')"
 	/usr/pluto/bin/Net_Mount.sh -d "$DevPK"
