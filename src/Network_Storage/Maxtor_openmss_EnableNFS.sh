3e0d3ae7ab1ed14b2110e958ee9c7828b10fe16b
radu <radu.cx+linuxmce@gmail.com>
2005-12-15 13:40:17 +0000

    split file; moved function

---
+++ src/Network_Storage/Maxtor_openmss_EnableNFS.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+. /usr/pluto/bin/Network_Parameters.sh
+. /usr/pluto/bin/SQL_Ops.sh
+. /usr/pluto/bin/pluto.func
+
+EnableMaxtorNFS()
+{
+	[[ -z "$IntIP" ]] && return 1
+	
+	local IP="$1" LAN="${IntIP%.*}.0/24" # the LAN is semi-hardcoded like this in Network_Firewall too
+
+	[[ -z "$IP" ]] && return 2
+	
+
+	Logging "Maxtor-openmss" $SEVERITY_STATUS "EnableMaxtorNFS" "Enabling NFS on Maxtor with IP '$IP'"
+	# How this works (because I'm sure I won't understand it later either):
+	# purpose: send a list of commands via a free-access telnet server running on the Maxtor Network Storage (1.2.2-openmss2) device
+	# 1st part echoes the commands, one per line, ending with "exit"
+	# then, it echoes dots
+	# this output is sent to socat
+	# socat's output is send through another while loop
+	# the last while loop exits when it reads "exit", which means it closes socat's stdout
+	# socat ends because of no stdout
+	# the dot displaying thingy exits because socat exited
+	# the dots don't get to the telnet server, but they serve for the ending part, where the "no stdout" condition needs to be triggered
+	
+	# WARNING: the telnet server seems to crash after I don't know how may attempts like this one; no clue to why
+	(echo "mkdir -p /shares/mss-hdd/Pluto
+		portmap
+		rpc.mountd
+		rpc.nfsd
+		exportfs -io rw,no_root_squash,no_all_squash $LAN:/shares/mss-hdd/Pluto
+		exit"
+	while :; do echo -n "."; sleep 1; done) | socat STDIO TCP4:"$IP":23 | while read line; do [[ "$line" == *exit* ]] && break; done
+
+	Logging "Maxtor-openmss" $SEVERITY_STATUS "EnableMaxtorNFS" "Enabled NFS on Maxtor with IP '$IP'"
+	return 0
+}
