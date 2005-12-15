44e9fb7d0f051e38624856e956162d9a88cae6e6
radu <radu.cx+linuxmce@gmail.com>
2005-12-15 00:36:40 +0000

    startup script that mounts all Network Storage devices in the system; runs on all machines

---
+++ src/Network_Storage/Net_Mount_All.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. /usr/pluto/bin/SQL_Ops.sh
+. /usr/pluto/bin/pluto.func
+
+DEVICECATEGORY_Network_Storage=150 # may change after SQLCVS is used to commit
+
+Q="SELECT PK_Device,D.Description,DT.Description
+	FROM Device D
+	JOIN DeviceTemplate DT ON PK_DeviceTemplate=FK_DeviceTemplate
+	JOIN DeviceCategory ON PK_DeviceCategory=FK_DeviceCategory
+	WHERE PK_DeviceCategory=$DEVICECATEGORY_Network_Storage"
+R=$(RunSQL "$Q")
+
+for Device in $R; do
+	DevPK=$(Field 1 "$Device")
+	DevDesc=$(Field 2 "$Device")
+	DevTDesc=$(Field 3 "$Device")
+
+	Logging "Net_Mount_All" $SEVERITY_NORMAL "NAS" "Mounting device '$DevDesc' (PK $DevPK, template: '$DevTDesc')"
+	/usr/pluto/bin/Net_Mount.sh -d "$DevPK"
+done
