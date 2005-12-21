8d19ae16b88195c0b1f4802da6c7d04f2113dc11
radu <radu.cx+linuxmce@gmail.com>
2005-12-21 16:34:34 +0000

    more location-pointing messages

--- src/BootScripts/Update_Packages.sh
+++ src/BootScripts/Update_Packages.sh
@@ -19,12 +19,15 @@ mv /var/log/pluto/upgrade.newlog /var/log/pluto/upgrade.log 2>/dev/null
 exec &> >(tee /var/log/pluto/upgrade.newlog)
 
 echo "Performing system update"
+echo "- Updating package lists"
 apt-get update
 Fail=0
 
 # Harmless, and it cleans up the dpkg journal too
+echo "- Cleaning up dpkg journal"
 dpkg --forget-old-unavail
 
+echo "- Previewing dist-upgrade (step 1)"
 InstPkgs="$(apt-get -s -f dist-upgrade | grep "^Conf " | cut -d' ' -f2 | tr '\n' ' ')"
 RebootPkg="pluto-kernel-upgrade"
 for Pkg in $RebootPkg; do
@@ -34,8 +37,10 @@ for Pkg in $RebootPkg; do
 	fi
 done
 
+echo "- Previewing dist-upgrade (step 2)"
 Count=$(apt-get -f -y -s dist-upgrade | egrep -c '^Inst |^Conf ')
 
+echo "- Doing dist-upgrade"
 if apt-get -V -f -y $DownloadOnly dist-upgrade; then
 	if [[ -n "$DownloadOnly" ]]; then
 		date -R >"$Lock"
@@ -52,6 +57,7 @@ else
 	cp /var/log/pluto/upgrade.newlog /usr/pluto/coredump/
 fi
 
+echo "- Copying kernel package(s) for later use"
 cp /var/cache/apt/archives/kernel-image* /usr/pluto/deb-cache/dists/sarge/main/binary-i386/ 2>/dev/null
 
 if [[ -z "$DownloadOnly" && $Fail -eq 0 ]]; then
