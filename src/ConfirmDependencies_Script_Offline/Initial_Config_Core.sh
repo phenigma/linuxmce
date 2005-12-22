f00b7f08c091f8cd52992c58105ee7f8e64f5443
radu <radu.cx+linuxmce@gmail.com>
2005-12-22 17:10:28 +0000

    commented out lines, just in case

--- src/ConfirmDependencies_Script_Offline/Initial_Config_Core.sh
+++ src/ConfirmDependencies_Script_Offline/Initial_Config_Core.sh
@@ -52,10 +52,10 @@ RestoreCoreConf()
 
 # /etc/apt/apt.conf.d/30pluto
 pluto_apt_conf='// Pluto apt conf add-on
-Acquire::http::Proxy "http://127.0.0.1:8123";
-Acquire::ftp::Proxy "ftp://127.0.0.1:8124";
-Acquire::ftp::ProxyLogin { "USER $(SITE_USER)@$(SITE):$(SITE_PORT)"; "PASS $(SITE_PASS)"; };
-Acquire::http::Proxy::dcerouter "DIRECT";
+//Acquire::http::Proxy "http://127.0.0.1:8123";
+//Acquire::ftp::Proxy "ftp://127.0.0.1:8124";
+//Acquire::ftp::ProxyLogin { "USER $(SITE_USER)@$(SITE):$(SITE_PORT)"; "PASS $(SITE_PASS)"; };
+//Acquire::http::Proxy::dcerouter "DIRECT";
 Apt::Cache-Limit "12582912";
 '
 
