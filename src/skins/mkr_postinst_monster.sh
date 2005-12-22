a6fb3f7261a8f82c3dfc4b0560d5edb8f0034ce5
radu <radu.cx+linuxmce@gmail.com>
2005-12-22 19:56:18 +0000

    change logo

--- src/skins/mkr_postinst_monster.sh
+++ src/skins/mkr_postinst_monster.sh
@@ -9,3 +9,5 @@ ConfSet ReplaceSkin_6 11
 ConfSet UseSkinSettings_7 1
 ConfSet UseSkinSettings_10 3
 ConfSet UseSkinSettings_11 6
+
+mv /var/www/pluto-admin/include/images/{logo_pluto,monster_logo}.jpg
