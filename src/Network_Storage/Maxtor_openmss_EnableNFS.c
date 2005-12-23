44661cd417c5e21db25143a00fc10de3899361ea
radu <radu.cx+linuxmce@gmail.com>
2005-12-23 11:56:06 +0000

    fixed race condition; replaced shell function code with call to the C program

--- src/Network_Storage/Maxtor_openmss_EnableNFS.c
+++ src/Network_Storage/Maxtor_openmss_EnableNFS.c
@@ -18,7 +18,8 @@ const char * commands[] = {
 	"rpc.nfsd >/dev/null 2>&1 </dev/null",
 	/*"exportfs -io rw,no_root_squash,no_all_squash " LAN_SUBST ":/shares/mss-hdd/Pluto\n",*/
 	"echo \"/shares/mss-hdd/Pluto   192.168.80.0/255.255.255.0(rw,sync,wdelay,hide,nocrossmnt,secure,no_root_squash,no_all_squash,subtree_check,secure_locks,acl,mapping=identity,anonuid=-2,anongid=-2)\" >/var/lib/nfs/xtab\n",
-	"exit"
+	"exit",
+	NULL
 };
 
 int main(int argc, char * argv[])
@@ -68,26 +69,30 @@ int main(int argc, char * argv[])
 		}
 		
 		printf("Received: %s", buffer);
-		if (strstr(buffer, "prompt>") == buffer)
+
+		if (commands[cmd_idx] != NULL)
 		{
-			printf("Got prompt\n");
-			if ((strstr(commands[cmd_idx], LAN_SUBST)) != NULL)
+			if (strstr(buffer, "prompt>") == buffer)
 			{
-				new_cmd = strdup(commands[cmd_idx]);
-				cptr = strstr(new_cmd, LAN_SUBST);
-				cptr[1] = 's';
-				printf("Sending cmd %d: %s\n", cmd_idx, new_cmd);
-				printf(new_cmd, LAN);
-				fprintf(fsock, new_cmd, LAN); // not really the safest approach, but...
-				free(new_cmd);
+				printf("Got prompt\n");
+				if ((strstr(commands[cmd_idx], LAN_SUBST)) != NULL)
+				{
+					new_cmd = strdup(commands[cmd_idx]);
+					cptr = strstr(new_cmd, LAN_SUBST);
+					cptr[1] = 's';
+					printf("Sending cmd %d: %s\n", cmd_idx, new_cmd);
+					printf(new_cmd, LAN);
+					fprintf(fsock, new_cmd, LAN); // not really the safest approach, but...
+					free(new_cmd);
+				}
+				else
+				{
+					printf("Sending cmd %d: %s\n", cmd_idx, commands[cmd_idx]);
+					fprintf(fsock, "%s\n", commands[cmd_idx]);
+				}
+				
+				cmd_idx++;
 			}
-			else
-			{
-				printf("Sending cmd %d: %s\n", cmd_idx, commands[cmd_idx]);
-				fprintf(fsock, "%s\n", commands[cmd_idx]);
-			}
-			
-			cmd_idx++;
 		}
 	}
 
--- src/Network_Storage/Maxtor_openmss_EnableNFS.sh
+++ src/Network_Storage/Maxtor_openmss_EnableNFS.sh
@@ -12,28 +12,10 @@ EnableMaxtorNFS()
 
 	[[ -z "$IP" ]] && return 2
 	
-
 	Logging "Maxtor-openmss" $SEVERITY_NORMAL "EnableMaxtorNFS" "Enabling NFS on Maxtor with IP '$IP'"
-	# How this works (because I'm sure I won't understand it later either):
-	# purpose: send a list of commands via a free-access telnet server running on the Maxtor Network Storage (1.2.2-openmss2) device
-	# 1st part echoes the commands, one per line, ending with "exit"
-	# then, it echoes dots
-	# this output is sent to socat
-	# socat's output is send through another while loop
-	# the last while loop exits when it reads "exit", which means it closes socat's stdout
-	# socat ends because of no stdout
-	# the dot displaying thingy exits because socat exited
-	# the dots don't get to the telnet server, but they serve for the ending part, where the "no stdout" condition needs to be triggered
-	
-	# WARNING: the telnet server seems to crash after I don't know how may attempts like this one; no clue to why
-	(echo "mkdir -p /shares/mss-hdd/Pluto
-		portmap
-		rpc.mountd
-		rpc.nfsd
-		exportfs -io rw,no_root_squash,no_all_squash $LAN:/shares/mss-hdd/Pluto
-		exit"
-	while :; do echo -n "."; sleep 1; done) | socat STDIO TCP4:"$IP":23 | while read line; do [[ "$line" == *exit* ]] && break; done
-
+	Logging "Maxtor-openmss" $SEVERITY_NORMAL "EnableMaxtorNFS" "Cmd: /usr/pluto/bin/Maxtor_openmss_EnableNFS \"$IP\""
+	/usr/pluto/bin/Maxtor_openmss_EnableNFS "$IP" > >(tee /var/log/pluto/Maxtor_openmss_EnableNFS.newlog) 2>&1
 	Logging "Maxtor-openmss" $SEVERITY_NORMAL "EnableMaxtorNFS" "Enabled NFS on Maxtor with IP '$IP'"
+
 	return 0
 }
