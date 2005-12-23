483e4c99c03068ce0ab0d7152cefa832d6d7db0a
torindan <torindan>
2005-12-23 09:19:29 +0000

    Fix typo

--- src/BootScripts/SetupUsers.sh
+++ src/BootScripts/SetupUsers.sh
@@ -2,7 +2,7 @@
 
 cd /usr/pluto/bin
 ./SetupUsers_Homes.sh
-if [ -x /usr/pluto/bin/generate_users_voicemenu.pl \
+if [ -x /usr/pluto/bin/generate_users_voicemenu.pl ]
 then
 	./generate_users_voicemenu.pl
 fi
