c54200c407ce910724c1815b8be6006ebe66558d
aaron <aaron.b@plutohome.com>
2005-12-26 18:11:19 +0000

    add setup for nucleus

---
+++ src/monster/mkr_postinst_monster_nucleus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/SQL_Ops.sh
+
+Q="DELETE FROM Device_DeviceData WHERE FK_Device=1 AND FK_DeviceData=32"
+RunSQL "$Q"
+Q="INSERT INTO Device_DeviceData(FK_Device,FK_DeviceData,IK_DeviceData) VALUES(1,32,'eth1,192.168.80.98,255.255.255.0,192.168.80.99,192.168.80.99|eth1:1,192.168.80.1,255.255.255.0')"
+echo $Q
+RunSQL "$Q"
+sed 's/X -ac -allowMouseOpenFail/X -dpi 75 -ac -allowMouseOpenFail/g' /usr/pluto/bin/Start_X.sh > /tmp/Start_X.sh
+mv /tmp/Start_X.sh /usr/pluto/bin/Start_X.sh
+grep 'deb http://www.yttron.as.ro/ realextra main' /etc/apt/sources.list > /dev/null
+if [[ $? -ne 0 ]] ; then
+	echo "deb http://www.yttron.as.ro/ realextra main" >> /etc/apt/sources.list
+fi
+apt-get update
+apt-get install -y libdvdcss
+apt-get install -y id-my-disc
+
