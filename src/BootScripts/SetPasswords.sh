e958966d8dddbfb629d82605c696640080f5dc06
aaron <aaron.b@plutohome.com>
2005-12-26 19:07:51 +0000

    add set passwords

---
+++ src/BootScripts/SetPasswords.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/SQL_Ops.sh
+
+PK_Users=$1
+Password=$2
+PasswordUnix=$(mkpasswd -H md5 "$Password")
+PasswordSamba=$(/usr/pluto/bin/smbpass.pl "$Password")
+
+sqlPassword="${Password//\'/\'}"
+sqlPasswordUnix="${PasswordUnix//\'/\'}"
+sqlPasswordSamba="${PasswordSamba//\'/\'}"
+
+Q="UPDATE Users SET Password='$sqlPassword', Password_Unix='$sqlPasswordUnix', Password_Samba='$sqlPasswordSamba' WHERE PK_Users=PK_Users"
+RunSQL "$Q"
