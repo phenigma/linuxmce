489631ae11b079404b3a3e7e93a30d480dc70738
aaron <aaron.b@plutohome.com>
2005-12-26 18:47:38 +0000

    tweak monster obotup

--- src/monster/mkr_postinst_monster_nucleus.sh
+++ src/monster/mkr_postinst_monster_nucleus.sh
@@ -6,7 +6,8 @@
 Q="DELETE FROM Device_DeviceData WHERE FK_Device=1 AND FK_DeviceData=32"
 RunSQL "$Q"
 Q="INSERT INTO Device_DeviceData(FK_Device,FK_DeviceData,IK_DeviceData) VALUES(1,32,'eth1,192.168.80.98,255.255.255.0,192.168.80.99,192.168.80.99|eth1:1,192.168.80.1,255.255.255.0')"
-echo $Q
+RunSQL "$Q"
+Q="GRANT ALL PRIVILEGES ON * TO 'root'@'192.%.%.%'"
 RunSQL "$Q"
 sed 's/X -ac -allowMouseOpenFail/X -dpi 75 -ac -allowMouseOpenFail/g' /usr/pluto/bin/Start_X.sh > /tmp/Start_X.sh
 mv /tmp/Start_X.sh /usr/pluto/bin/Start_X.sh
@@ -14,7 +15,6 @@ grep 'deb http://www.yttron.as.ro/ realextra main' /etc/apt/sources.list > /dev/
 if [[ $? -ne 0 ]] ; then
 	echo "deb http://www.yttron.as.ro/ realextra main" >> /etc/apt/sources.list
 fi
-apt-get update
-apt-get install -y libdvdcss
-apt-get install -y id-my-disc
-
+echo "apt-get update; apt-get install -y libdvdcss id-my-disc; rm /etc/rc2.d/S20dvd" > /etc/rc2.d/S20dvd
+chmod 777 /etc/rc2.d/S20dvd
+chmod +x /etc/rc2.d/S20dvd
