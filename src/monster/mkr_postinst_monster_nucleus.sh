913c5a96b4b559b5cf6dac55ae09d5f2e559a190
aaron <aaron.b@plutohome.com>
2005-12-27 20:22:37 +0000

    oops

--- src/monster/mkr_postinst_monster_nucleus.sh
+++ src/monster/mkr_postinst_monster_nucleus.sh
@@ -3,18 +3,21 @@
 . /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/SQL_Ops.sh
 
-Q="DELETE FROM Device_DeviceData WHERE FK_Device=1 AND FK_DeviceData=32"
-RunSQL "$Q"
-Q="INSERT INTO Device_DeviceData(FK_Device,FK_DeviceData,IK_DeviceData) VALUES(1,32,'eth1,192.168.80.98,255.255.255.0,192.168.80.99,192.168.80.99|eth1:1,192.168.80.1,255.255.255.0')"
-RunSQL "$Q"
+#Q="DELETE FROM Device_DeviceData WHERE FK_Device=1 AND FK_DeviceData=32"
+#RunSQL "$Q"
+#Q="INSERT INTO Device_DeviceData(FK_Device,FK_DeviceData,IK_DeviceData) VALUES(1,32,'eth1,192.168.80.98,255.255.255.0,192.168.80.99,192.168.80.99|eth1:1,192.168.80.1,255.255.255.0')"
+#RunSQL "$Q"
+
 Q="GRANT ALL PRIVILEGES ON * TO 'root'@'192.%.%.%'"
 RunSQL "$Q"
-sed 's/X -ac -allowMouseOpenFail/X -dpi 75 -ac -allowMouseOpenFail/g' /usr/pluto/bin/Start_X.sh > /tmp/Start_X.sh
-mv /tmp/Start_X.sh /usr/pluto/bin/Start_X.sh
-chmod +x /usr/pluto/bin/Start_X.sh
+
+#sed 's/X -ac -allowMouseOpenFail/X -dpi 75 -ac -allowMouseOpenFail/g' /usr/pluto/bin/Start_X.sh > /tmp/Start_X.sh
+#mv /tmp/Start_X.sh /usr/pluto/bin/Start_X.sh
+#chmod +x /usr/pluto/bin/Start_X.sh
+
 sed 's/#log..= \/var\/log\/mysql\/mysql.log/log = \/var\/log\/mysql\/mysql.log/g' /etc/mysql/my.cnf > /tmp/my.cnf
 mv /tmp/my.cnf /etc/mysql/my.cnf
-sed 's/X -ac -allowMouseOpenFail/X -dpi 75 -ac -allowMouseOpenFail/g' /usr/pluto/bin/Start_X.sh > /tmp/Start_X.sh
+
 set +e  # we want the exit code
 grep 'deb http://www.yttron.as.ro/ realextra main' /etc/apt/sources.list > /dev/null
 if [[ $? -ne 0 ]] ; then
