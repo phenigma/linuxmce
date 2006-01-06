abf2912321f87f13bbddf8e720811e3612439be9
radul <radul>
2006-01-06 15:02:46 +0000

    all print -> log check conn_.nil?

--- src/Generic_Serial_Device/Embedded Devices/1767 DSC Power5020 Serial/Ruby/private_function.rb
+++ src/Generic_Serial_Device/Embedded Devices/1767 DSC Power5020 Serial/Ruby/private_function.rb
@@ -1,51 +1,26 @@
-#private functions  03-Ian-06 17:30
-def readLine
-$index=0
-$line=String.new
-
-cod=conn_.Recv(1, $waitTime)
-$line+=cod
-while cod[0,1] != "\n" do
-	if conn_ != nil then 
-		cod=conn_.Recv(1, $waitTime)
-		$line += cod
-	else
-		$logFile.print "Can not read from serial " + "\n"
-	end
-		
-	if ($line.size>$MaxLineLength) then
-		$logFile.print "Comunication error. Line size" + $line.size.to_s + "\n"
-		#send error ...
-	end	
+#private functions  06-Ian-06 16:45
+def log( buff )
+if  $logFile.nil? then
+	print( buff )
+else
+	$logFile.print( buff )
+	if $bFlush then $logFile.flush() end
 end
-
-logString($line)
-end
-
-def readByte(val)
-valNo=val.to_i
-if valNo + $index <= $line.size then
-	buff = $line[$index,valNo]
-	$index+=valNo
-else 
-	$logFile.print "Error reading   "	
-	$logFile.print "Try to read:" + valNo.to_s + " bytes from index:" + $index.to_s + "   "
-	$logFile.print "Total size:" + $line.size.to_s + "\n"
-	buff=String.new
-end
-
-if $bFlush then $logFile.flush end
-return buff
 end
 
 def logString(word)
-$logFile.print "Line read Size:" + word.size.to_s + "  String:"
+log( "Line read Size:" + word.size.to_s + "  String:" )
 word.each_byte{ |i| 
-	#if i >='0' and i<= '9' then $logFile.print l= end
-	$logFile.print i.to_s + "  "
+	#if i >='0' and i<= '9' then log( l= end
+	log( i.to_s + "  " )
 }
 
-$logFile.print "\n"
+log( "\n" )
+if $bFlush then $logFile.flush end
+end
+
+def badParam(funcName,paramName)
+log( "Bad parameter in: " + funcName + " Parmeter:" + paramName + "\n" )
 if $bFlush then $logFile.flush end
 end
 
@@ -53,27 +28,27 @@ def logState()
 logTime = Time.now
 timeStr = logTime.strftime("%d-%m-%Y  %H:%M:%S  ")
 
-$logFile.print "PC5401 state:" + "\n"
-$logFile.print "Machine time:" + timeStr + "\n"
-$logFile.print "Max partition:" + $MaxPartition.to_s + "   Max sensor:" + $MaxSensor.to_s + "   "
-$logFile.print "Max line length:" + $MaxLineLength.to_s + "\n"
+log( "PC5401 state:" + "\n" )
+log( "Machine time:" + timeStr + "\n" )
+log( "Max partition:" + $MaxPartition.to_s + "   Max sensor:" + $MaxSensor.to_s + "   " )
+log( "Max line length:" + $MaxLineLength.to_s + "\n" )
 
-$logFile.print "PC5401 State:" + $panelState.to_s + " Init:" + $bInit.to_s + "\n"  
-$logFile.print "TimeStamp:" + $bTimeStamp.to_s + " TimeBroadcast:" + $bTimeBroadcast.to_s + "   "
-$logFile.print "Descriptive arm:" + $bDescArm.to_s + "\n"
+log( "PC5401 State:" + $panelState.to_s + " Init:" + $bInit.to_s + "\n"  )
+log( "TimeStamp:" + $bTimeStamp.to_s + " TimeBroadcast:" + $bTimeBroadcast.to_s + "   " )
+log( "Descriptive arm:" + $bDescArm.to_s + "\n" )
 
-$logFile.print "Partitions list: " + $partStr + "\n" 
-$logFile.print "Sensors list: " + $zoneStr + "\n"
+log( "Partitions list: " + $partStr + "\n" )
+log( "Sensors list: " + $zoneStr + "\n" )
 
 #log open zone
-$logFile.print "Sensor open:"
+log( "Sensor open:" )
 $sensorStatus.each{ |key, value|
-	$logFile.print key.to_s + " -> " + value.to_s
-	if $sensorType.has_key?(key) then $logFile.print " -> " + $sensorType[key].to_s end
-	$logFile.print "\n"
+	log( key.to_s + " -> " + value.to_s )
+	if $sensorType.has_key?(key) then log( " -> " + $sensorType[key].to_s ) end
+	log( "\n" )
 }
 
-$logFile.print "\n"
+log( "\n" )
 if $bFlush then $logFile.flush end
 end
 
@@ -85,22 +60,22 @@ timeStr = logTime.strftime("%d-%m-%Y  %H:%M:%S  ")
 
 cmdNo=code.to_i
 
-$logFile.print "Machine time:" + timeStr + "\n"
-$logFile.print "PC5401 State:" + $panelState.to_s + " Init:" + $bInit.to_s + "\n"
-$logFile.print "TimeStamp:" + $bTimeStamp.to_s + " TimeBroadcast:" + $bTimeBroadcast.to_s
-$logFile.print "\n"
+log( "Machine time:" + timeStr + "\n" )
+log( "PC5401 State:" + $panelState.to_s + " Init:" + $bInit.to_s + "\n" )
+log( "TimeStamp:" + $bTimeStamp.to_s + " TimeBroadcast:" + $bTimeBroadcast.to_s )
+log( "\n" )
 
-if (cmdNo < 500) then $logFile.print "Command:" end
-if (cmdNo >= 500) and (cmdNo <= 502) then $logFile.print "Cmd_ack:" end
-if (cmdNo > 502) then $logFile.print "Event:" end
+if (cmdNo < 500) then log( "Command:" ) end
+if (cmdNo >= 500) and (cmdNo <= 502) then log( "Cmd_ack:" ) end
+if (cmdNo > 502) then log( "Event:" ) end
 
-$logFile.print code.to_s + "  " 
+log( code.to_s + "  " )
 if $cmdCode.has_key?(code) then 
-	$logFile.print $cmdCode[code] 
+	log( $cmdCode[code] )
 else 
-	$logFile.print "Unknown code" 
+	log( "Unknown code" )
 end
-$logFile.print "\n\n"
+log( "\n\n" )
 
 if $bFlush then $logFile.flush end
 end
@@ -110,8 +85,8 @@ list=zones.split(",")
 list.each{ |i|
 	list2=i.split("=")
 
-	if list2.size != 2 then 
-		print "Wrong\n" 
+	if (list2.size != 2) then 
+		badParameters( "Mapp", "Zones" ) 
 		next 
 	end      #bad format string
 
@@ -123,9 +98,110 @@ list.each{ |i|
 
 end
 
+def checkSumProc(buff)
+count=0   
+chkStr=String.new
+outStr=buff
+#log( "\nCheckSum call:\n" )
+#log( "Size:" + buff.size.to_s + "  " + buff + "\n" )
+outStr.each_byte{ |i|
+	count += i
+	#log( i.to_s + ":" + count.to_s + "\n" )
+}
+#calculate checksum
+count=count%256
+chkStr= "%02X" % count
+
+outStr += chkStr + "\r" + "\n"      #add check sum + cr/lf
+return outStr
+end
+
+#IO operation
+
+def readLine
+$index=0
+$line=String.new
+
+cod=conn_.Recv(1, $waitTime)
+$line+=cod
+while cod[0,1] != "\n" do
+	if (conn_ != nil) then 
+		cod=conn_.Recv(1, $waitTime)
+		$line += cod
+	else
+		log( "Can not read from serial " + "\n" )
+	end
+		
+	if ($line.size>$MaxLineLength) then
+		log( "Comunication error. Line size" + $line.size.to_s + "\n" )
+		#send error ...
+	end	
+end
+
+logString($line)
+end
+
+def readByte(val)
+valNo=val.to_i
+if (valNo + $index <= $line.size) then
+	buff = $line[$index,valNo]
+	$index+=valNo
+else 
+	log( "Error reading   " )
+	log( "Try to read:" + valNo.to_s + " bytes from index:" + $index.to_s + "   " )
+	log( "Total size:" + $line.size.to_s + "\n" )
+	buff=String.new
+end
+
+if $bFlush then $logFile.flush end
+return buff
+end
+
+def send(buff)
+if conn_.nil? then
+	log( "Problem with sending data to serial!!! \n" )
+else
+	conn_.Send(buff)
+end
+end
+
+def sendCmd(buff)
+buff2=checkSumProc(buff)
+logString(buff2)
+
+if ($panelState == 3) then    #wait to finish another command
+	$cmdBuffer.push(buff2)
+	log( "Adding cmd to buffer" + "\n\n" )
+else                                #ready to process another command
+	sendbuff2)
+	$panelState=3
+	log( "Send cmd" + "\n\n" )
+end
+
+if $bFlush then $logFile.flush end
+end
+
+def sendCmd2()
+log( "Start cmd2: " )
+$logFile.flush
+
+if $cmdBuffer.empty? then      #execute first command from buffer
+	if ($panelState == 3) then $panelState=2 end 
+else                                    #buffer empty
+	log( "Send cmd2: " )
+	send$cmdBuffer.first)
+	logString($cmdBuffer[0])
+
+	$cmdBuffer.delete_at(0)
+end
+
+if $bFlush then $logFile.flush end
+#return buff2
+end
+
 def addSens(val,status,type)
 if $sensorStatus.has_key?(val) then 
-	$logFile.print "Already exist " + val.to_s + "\n" 
+	log( "Already exist " + val.to_s + "\n" )
 else 
 	$sensorStatus[val]=status 
 	$sensorType[val]=type 
@@ -137,38 +213,38 @@ else
 		auxStr += device_.devid_.to_s + "\t" + val.to_s + "\t" + "room1" + "\t" + type.to_s + "\t" + "3" + "\n" 
 		$zoneStr+= auxStr
 
-		$logFile.print "Aux:"        + auxStr     +  "\n"   
-		$logFile.print "zoneStr:"  + $zoneStr  +  "\n"
+		log( "Aux:"        + auxStr     +  "\n"  )
+		log( "zoneStr:"  + $zoneStr  +  "\n" )
 	end
 end      #end new sensor found  
 
-if $bInit == true then        #fire sensor trip only after adding child
+if ($bInit == true) then        #fire sensor trip only after adding child
 	valNo=val.to_i
 	idNo=-1
 
-	$logFile.print "Log children list:" + "\n"
+	log( "Log children list:" + "\n" )
 	children=device_.childdevices_
 	children.each{ |key, value|
 		strAux=value.devdata_[12]
-		$logFile.print "Child id:" + key.to_s + "  "
-		$logFile.print "Child name:" +  strAux + "  "  
-		$logFile.print "Search name:" + valNo.to_s + "\n" 
+		log( "Child id:" + key.to_s + "  " )
+		log( "Child name:" +  strAux + "  "  )
+		log( "Search name:" + valNo.to_s + "\n" )
 
-		if valNo.to_s == strAux then 
-			$logFile.print "Found child with name:" + val.to_s + "  Id:" + key.to_s + "\n"
+		if (valNo.to_s == strAux) then 
+			log( "Found child with name:" + val.to_s + "  Id:" + key.to_s + "\n" )
 			idNo = key.to_i 
 		end
 	}
-	$logFile.print "\n"
-	$logFile.print "Fire 9 event with " + idNo.to_s + " " + status.to_s + "\n"
+	log( "\n" )
+	log( "Fire 9 event with " + idNo.to_s + " " + status.to_s + "\n" )
 	$logFile.flush
 
-	if idNo != -1 then 
+	if (idNo != -1) then 
 		tripEv= Command.new(idNo, -1001, 1, 2, 9);      #9 sensor tripp   key.to_i
 		tripEv.params_[25] = status.to_s                     #25 state
 		SendCommand(tripEv)
 	else
-		$logFile.print "Didn't find the child with name" + val.to_s + "\n"
+		log( "Didn't find the child with name" + val.to_s + "\n" )
 	end
 end
 
@@ -179,12 +255,12 @@ def addPart(val,status)
 #check to see if there is some mapping
 if $partMapping.has_key?(val) == false then  
 	$partMapping[val] = val 
-	$logFile.print "Add mapping:" + val.to_s + "->" + val.to_s + "\n"
+	log( "Add mapping:" + val.to_s + "->" + val.to_s + "\n" )
 end
 
 if $partStatus.has_key?(val) then 
 	bFind = false
-	print "Already exist " + val.to_s + "\n" 
+	log( "Already exist " + val.to_s + "\n" )
 else 
 	bFind = true
 	$partStatus[val]=status 
@@ -193,8 +269,8 @@ else
 	auxStr=String.new
 	auxStr += val.to_s + "\t" + val.to_s + "\t" + "room1" + "\t" + "56" + "\t" + "3" + "\n" 
 	$partStr+= auxStr
-	$logFile.print "Aux:"        + auxStr     +  "\n"   
-	$logFile.print "partStr:"  + $partStr  +  "\n"
+	log( "Aux:"        + auxStr     +  "\n" )
+	log( "partStr:"  + $partStr  +  "\n" )
 end  #found end
 
 if $bFlush then $logFile.flush end
@@ -202,10 +278,10 @@ end
 
 def addChildDevices      #add to data base
 if $partStr.empty? then 
-	$logFile.print "Child list is empty not fire event" + "\n"
+	log( "Child list is empty not fire event" + "\n" )
 else
-	if $bInit == true then 
-		$logFile.print "Already initialized." + "\n"
+	if ($bInit == true) then 
+		log( "Already initialized." + "\n" )
 	else
 		$bInit=true
 		
@@ -214,7 +290,7 @@ else
 		reportEv.params_[13] = $partStr                                         # text
 		SendCommand(reportEv)
 		
-		$logFile.print "Fire 54 event:" + $partStr + "\n"
+		log( "Fire 54 event:" + $partStr + "\n" )
 		logState()
 	end	
 end
@@ -222,68 +298,6 @@ end
 if $bFlush then $logFile.flush end
 end
 
-def badParam(funcName,paramName)
-$logFile.print "Bad parameter in: " + funcName + " Parmeter:" + paramName + "\n"
-if $bFlush then $logFile.flush end
-end
-
-def checkSumProc(buff)
-count=0   
-chkStr=""
-print "\nCheckSum call:\n"
-print "Size:" + buff.size.to_s + "  " + buff + "\n"
-for i in 0..buff.size - 1  
-count=count+buff[i,1].to_i+48
-print buff[i,1] + ":" + count.to_s + "\n" 
-end
-#calculate checksum
-count=count%256
-chkStr= "%X" % count
-print count.to_s + " " + chkStr + "  " + chkStr[0,1] + " " + chkStr[1,1]  + "\n"
-#show list before
-print "Size: " + buff.size.to_s + " " + buff + "\n"
-#add checksum
-buff += chkStr
-#add crl/lf   
-buff +="\r" + "\n"
-#show list after
-print "Size: " + buff.size.to_s + " " + buff + "\n"
-return buff
-end
-
-def sendCmd(buff)
-buff2=checkSumProc(buff)
-logString(buff2)
-
-if $panelState == 3 then    #wait to finish another command
-	$cmdBuffer.push(buff2)
-	$logFile.print "Adding cmd to buffer" + "\n\n"
-else                                #ready to process another command
-	conn_.Send(buff2)
-	$panelState=3
-	$logFile.print "Send cmd" + "\n\n"
-end
-
-if $bFlush then $logFile.flush end
-end
-
-def sendCmd2()
-$logFile.print "Start cmd2: "
-$logFile.flush
-
-if $cmdBuffer.empty? then      #execute first command from buffer
-	if $panelState == 3 then $panelState=2 end 
-else                                    #buffer empty
-	$logFile.print "Send cmd2: "
-	conn_.Send($cmdBuffer.first)
-	logString($cmdBuffer[0])
-
-	$cmdBuffer.delete_at(0)
-end
-
-if $bFlush then $logFile.flush end
-#return buff2
-end
 
 #  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #  DSC function
@@ -291,14 +305,14 @@ end
 def DSCPoll
 buff="000"
 logCmd(buff)
-$logFile.print "\n"
+log( "\n" )
 sendCmd(buff)
 end
 
 def DSCStatusReport2
 buff="001"
 logCmd(buff)
-$logFile.print "\n"
+log( "\n" )
 sendCmd(buff)
 end
 
@@ -308,7 +322,7 @@ logCmd(buff)
 
 $bTimeStamp=bState
 # add data
-if bState == true then 
+if (bState == true) then 
 	buff += '1' 
 else 
 	buff += '0' 
@@ -323,7 +337,7 @@ logCmd(buff)
 
 $bTimeStamp=bState
 # add data
-if bState == true then 
+if (bState == true) then 
 	buff += '1' 
 else 
 	buff += '0' 
@@ -337,7 +351,7 @@ buff="056"
 logCmd(buff)
 $bTimeBroadcast=bState
 # add data
-if bState == true then 
+if (bState == true) then 
 	buff += '1' 
 else 
 	buff += '0' 
@@ -350,7 +364,7 @@ def DSCSimulateAlarmCmd(value)
 buff="060"
 logCmd(buff)
 
-$logFile.print "Value: " + value.to_s +  "\n"
+log( "Value: " + value.to_s +  "\n" )
 #check param
 chkValue=value.to_i
 if (chkValue<1) and (chkValue>3)  then 
@@ -369,7 +383,7 @@ def DSCArmPartition(type,value,password)
 buff="03"
 logCmd(buff)
 
-$logFile.print value.to_s + " armType: " + type + "\n"
+log( value.to_s + " armType: " + type + "\n" )
 #check param
 if (type != "AWAY") and (type != "STAY") and (type != "NODELAY") and (type != "CODE") then 
 	badParam(cmdCode[buff],"arm Type") 
@@ -401,7 +415,7 @@ end
 	
 #data - add the partition number
 buff += partNo.to_s
-if type == "CODE"       then buff += password           end
+if (type == "CODE")       then buff += password           end
 
 sendCmd(buff)
 end
@@ -410,7 +424,7 @@ def DSCDisarmPartition(value,password)
 buff="040"
 logCmd(buff)
 
-$logFile.print "Partition:" + value.to_s + " Code:" + password + "\n"
+log( "Partition:" + value.to_s + " Code:" + password + "\n" )
 #check param
 if (password.size < 4) or (password.size > 6) then 
 	badParam($cmdCode[buff],"Password to long/short") 
