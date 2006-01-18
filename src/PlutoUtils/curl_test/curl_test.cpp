b4ab90524f13b39dbf0a0705a7b3c6117f55ffa1
cmiron <cmiron>
2006-01-18 12:34:27 +0000

    Added test project for PlutoUtils -> HttpUtils

---
+++ src/PlutoUtils/curl_test/curl_test.cpp
@@ -0,0 +1,64 @@
+// curl_test.cpp : Defines the entry point for the console application.
+//
+
+#include "../HttpUtils.h"
+#include "conio.h"
+
+string URLEncode(string sInput)
+{
+	char hexValues[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+	const char *inBuffer = sInput.c_str();
+	char *outBuffer = new char[(sInput.length() * 3) + 1];
+	char *baseOutBuffer = outBuffer;
+
+	while ( *inBuffer != 0 )
+	{
+		switch ( *inBuffer )
+		{
+		case '\"': case '<': case '>': case '%': case '\\':
+		case  '^': case '[': case ']': case '`': case '+':
+		case  '$': case ',': case '@': case ';': case '/':
+		case  '!': case '#': case '?': case '=': case '&':
+		case  ':':
+			*outBuffer++ = '%';
+			*outBuffer++ = hexValues[(*inBuffer >> 4) & 0x0F];
+			*outBuffer++ = hexValues[*inBuffer & 0x0F];
+			break;
+		default:
+			if ( *inBuffer > 32 && *inBuffer < 123 )
+				*outBuffer++ = *inBuffer;
+			else
+			{
+				*outBuffer++ = '%';
+				*outBuffer++ = hexValues[(*inBuffer >> 4) & 0x0F];
+				*outBuffer++ = hexValues[*inBuffer & 0x0F];
+			}
+		}
+
+		inBuffer++;
+	}
+	*outBuffer = '\0';
+
+	string result(baseOutBuffer);
+	delete baseOutBuffer;
+	return result;
+}
+
+int main(int argc, char* argv[])
+{
+	vector<string> vectHeaders;
+	//vectHeaders.push_back("Accept: text/html, */*");
+
+	map<string, string> mapParams;
+	mapParams["XML"] = URLEncode("<CiscoIPPhoneExecute><ExecuteItem URL=\"http://192.168.80.1/pluto-admin/ServicesMenu.php\"/></CiscoIPPhoneExecute>");
+
+	string Response = HttpPost("http://localhost/CGI/Execute", vectHeaders, mapParams, "a", "a", 9999);
+	getch();
+
+	return 0;
+}
+
+
+
+
