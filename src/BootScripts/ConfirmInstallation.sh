f36a431436f17c67031f93083aaea1947d9ce7c1
radu <radu.cx+linuxmce@gmail.com>
2006-01-25 15:06:04 +0000

    10s timeout

--- src/BootScripts/ConfirmInstallation.sh
+++ src/BootScripts/ConfirmInstallation.sh
@@ -6,7 +6,7 @@
 Q="SELECT COUNT(*) FROM Installation WHERE PK_Installation=1"
 R=$(RunSQL "$Q")
 if [[ "$R" -ne 0 ]]; then
-	NewInstNumber=$(wget -O - http://plutohome.com/new_installation.php)
+	NewInstNumber=$(wget --timeout=10 -O - http://plutohome.com/new_installation.php)
 	if [[ -n "$NewInstNumber" ]]; then
 		/usr/pluto/bin/sqlCVS -t Installation -O 1 -N "$NewInstNumber" change_key
 		ConfSet PK_Installation "$NewInstNumber"
