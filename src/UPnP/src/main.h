375f22deb233b9745bbfaaedda71a356d4f397e4
edgar <edgar>
2006-01-30 18:08:33 +0000

    creation

---
+++ src/UPnP/src/main.h
@@ -0,0 +1,25 @@
+//
+// C++ Interface: main
+//
+// Description: 
+//
+//
+// Author: Edgar Grimberg <edgar.g@plutohome.com>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include "DCE/Logger.h"
+#include "DCE/ServerLogger.h"
+
+using namespace DCE;
+
+namespace DCE
+{
+	extern	Logger *g_pPlutoLogger;
+}
+
+#endif //_MAIN_H_
