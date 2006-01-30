31913e9d444acb7718c70a666e6cf16cc88fa07a
vali <vali>
2006-01-30 10:05:57 +0000

    Added language file for irrigation scenarios

---
+++ web/pluto-admin/languages/en/irrigationScenarios.lang.php
@@ -0,0 +1,11 @@
+<?
+$TEXT_IRRIGATION_SCENARIOS_CONST='Irrigation Scenarios';
+$TEXT_ADD_IRRIGATION_SCENARIO_CONST='Add scenario';
+$TEXT_NO_IRRIGATION_SCENARIOS_CONST='No irrigation scenarios for this room.';
+$TEXT_IRRIGATION_SCENARIOS_CONFIRM_DELETE_CONST='Are you sure you want to delete this scenario?';
+$TEXT_IRRIGATION_SCENARIO_ADDED_CONST='New irrigation Scenario added.';
+$TEXT_IRRIGATION_SCENARIO_DELETED_CONST='Irrigation scenario deleted.';
+$TEXT_IRRIGATION_SCENARIO_TEST_CONST='The command to test irrigation scenario was sent.';
+$TEXT_IRRIGATION_SCENARIO_UPDATED_CONST='Irrigation Scenario updated';
+$TEXT_IRRIGATION_SCENARIO_POSITION_UPDATED_CONST='Positions was updated';
+?>
