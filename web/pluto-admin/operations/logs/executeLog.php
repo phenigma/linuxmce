21d754dc8689fefc46ad9b6e406bb673a1dc1b78
radu <radu.cx+linuxmce@gmail.com>
2006-02-02 09:51:41 +0000

    Message Box when finished running Diskless Setup list of commands

--- web/pluto-admin/operations/logs/executeLog.php
+++ web/pluto-admin/operations/logs/executeLog.php
@@ -4,7 +4,13 @@ switch($scriptID){
 	case 1:
 		$command[]='sudo -u root /usr/pluto/bin/Diskless_Setup.sh';
 		$command[]='sudo -u root /usr/pluto/bin/DHCP_config.sh';
-		$title='Please wait for the setup process to complete, then do a ‘quick reload router’, and then you can bootup your new diskless media director.';
+		$title='Please wait for the setup process to complete, then do a "quick reload router", and then you can bootup your new diskless media director.';
+		$end='
+		<script language="JavaScript">
+		<!--
+			alert("Finished setting up diskless Media Directors");
+		// -->
+		</script>';
 	break;
 	case 2:
 		$command[]=stripslashes(urldecode($_REQUEST['command']));
@@ -31,5 +37,6 @@ for ($i = 0; $i < count($command); $i++)
 		print '<br><br>';
 	}
 }
+print $end;
 ?>
 </pre>
