ee08ec9f83a8c52ed47844a4c579f0b1a3d521db
vali <vali>
2006-02-10 12:56:06 +0000

    Resolved Mantis #1519, added on user's homepage the voice mail messages from AMP. Changed AMP files at minimum to authentificate to voicemail pages from AMP, without major changes to their api.
    -This line, and those below, will be ignored--
    
    M    userHome.php

--- web/pluto-admin/operations/userHome.php
+++ web/pluto-admin/operations/userHome.php
@@ -3,14 +3,14 @@ function userHome($output,$dbADO) {
 	// include language files
 	include(APPROOT.'/languages/'.$GLOBALS['lang'].'/common.lang.php');
 	include(APPROOT.'/languages/'.$GLOBALS['lang'].'/userHome.lang.php');
-	
-/* @var $dbADO ADOConnection */
-/* @var $rs ADORecordSet */
-$out='';
-$dbADO->debug=false;
-$reloadTree = @$_REQUEST['reloadTree']=='false'?false:true;
 
-$scriptInHead='
+	/* @var $dbADO ADOConnection */
+	/* @var $rs ADORecordSet */
+	$out='';
+	$dbADO->debug=false;
+	$reloadTree = @$_REQUEST['reloadTree']=='false'?false:true;
+
+	$scriptInHead='
 <script>
 	'.($reloadTree==true?'
 	if (parent.frames[\'treeframe\']) {
@@ -19,12 +19,12 @@ $scriptInHead='
 </script>
 ';
 
-$out.=$TEXT_USER_HOME_TEXT_CONST;
+	$out.=setLeftMenu($dbADO).'<iframe src="amp/recordings/index.php?s=voicemail" style="width:98%;height:85%"></iframe>';
 
-$output->setHelpSrc('/support/index.php?section=document&docID=35');
-$output->setBody($out);
-$output->setScriptInHead($scriptInHead);
-$output->setTitle(APPLICATION_NAME.' :: '.$TEXT_USER_HOME_CONST);			
-$output->output();  		
+	$output->setHelpSrc('/support/index.php?section=document&docID=35');
+	$output->setBody($out);
+	$output->setScriptInHead($scriptInHead);
+	$output->setTitle(APPLICATION_NAME.' :: '.$TEXT_USER_HOME_CONST);
+	$output->output();
 }
 ?>
