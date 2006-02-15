48285173cc5e56c99fdc54b149bd57041f00c93f
radu <radu.cx+linuxmce@gmail.com>
2006-02-15 13:51:39 +0000

    added <device> parameter and error messages

--- src/Disk_Drive/cdop.c
+++ src/Disk_Drive/cdop.c
@@ -7,7 +7,7 @@
 
 inline int usage()
 {
-	printf("Usage: cdop <lock|unlock>\n");
+	printf("Usage: cdop <device> {lock|unlock}\n");
 	return 1;
 }
 
@@ -16,20 +16,31 @@ int main(int argc, const char * argv[])
 	int dev;
 	int state;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
 		return usage();
 	}
 
-	if (strcmp(argv[1], "lock") == 0)
+	if (strcmp(argv[2], "lock") == 0)
 		state = 1;
-	else if (strcmp(argv[1], "unlock") == 0)
+	else if (strcmp(argv[2], "unlock") == 0)
 		state = 0;
 	else
 		return usage();
 	
-	dev = open("/dev/cdrom", O_RDONLY);
-	ioctl(dev, CDROM_LOCKDOOR, state);
+	dev = open(argv[1], O_RDONLY);
+	if (dev == -1)
+	{
+		perror("Failed to open cdrom");
+		return 2;
+	}
+	
+	if (ioctl(dev, CDROM_LOCKDOOR, state) == -1)
+	{
+		perror("Failed to perform operation");
+		return 3;
+	}
+	
 	close(dev);
 
 	return 0;
