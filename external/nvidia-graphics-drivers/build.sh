784e3e3356cbbfde1ae17fc6bfdd6cd24cbaf684
radu <radu.cx+linuxmce@gmail.com>
2006-02-17 16:30:02 +0000

    updated version

--- external/nvidia-graphics-drivers/build.sh
+++ external/nvidia-graphics-drivers/build.sh
@@ -7,4 +7,4 @@ pushd modules/nvidia-kernel/ &>/dev/null
 KSRC=/lib/modules/`uname -r`/build KVERS=`uname -r` fakeroot debian/rules binary_modules
 popd &>/dev/null
 
-cp modules/nvidia-kernel-`uname -r`_1.0.8178-1_i386.deb ..
+cp modules/nvidia-kernel-`uname -r`_1.0.8178-2_i386.deb ..
