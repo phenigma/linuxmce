84450ac7413751823e6fdd6d156184964a579f0f
radul <radul>
2006-02-17 14:15:26 +0000

    Child selected by type not by id

--- src/Generic_Serial_Device/Embedded Devices/NOEMON-CANBus/Ruby/ReceivedChildCommand.rb
+++ src/Generic_Serial_Device/Embedded Devices/NOEMON-CANBus/Ruby/ReceivedChildCommand.rb
@@ -1,4 +1,4 @@
-# 17-Feb-06 13:38 Receive command from child
+# 17-Feb-06 16:00 Receive command from child
 if ( $bInit == false ) then
 	log( "The initialisation is not finish yet\n")
 	return
@@ -9,25 +9,38 @@ log( "Received command from child" + "\n" )
 cmdId = cmd.id_
 cmdTo = cmd.devidto_
 cmdType= cmd.type_
-childName=device_.childdevices_[cmdTo].devdata_[12]
-childNo= childName.to_i
+childType = device_.childdevices_[cmdTo].devtemplid_
 log( "CmdId:" + cmdId.to_s + "   " )
 log( "CmdTo:" + cmdTo.to_s + "   " )
 log( "CmdType:" + cmdType.to_s + "   " )
-log( "Child name:" + childName + "\n" )
+log( "Child type:" + childType.to_s + "\n" )
 
+childName=String.new
 deviceBranch=0
 deviceUnit=1
 
 val = device_.childdevices_[cmdTo]
-if (val.devdata_.has_key?(40)) and (val.devdata_.has_key?(144)) then
-	log( "Id:" + val.devdata_[12] + "   " )
-	log( "Branch:" + val.devdata_[40] + "   " )
-	log( "Unit:" + val.devdata_[144]  + "\n" )
-			
+#try to read name from 138 or 12
+if ( val.devdata_.has_key?(138) ) then
+	childName = val.devdata_[138]
+else
+	if ( val.devdata_.has_key?(12) ) then
+		childName = val.devdata_[12]
+	end
+end
+
+if ( val.devdata_.has_key?(40) ) then
 	deviceBranch = val.devdata_[40].to_i
+end
+
+if ( val.devdata_.has_key?(144) ) then
 	deviceUnit = val.devdata_[144].to_i
 end
+
+childNo= childName.to_i
+log( "Child name:" + childName + "   " )
+log( "Device branch:" + deviceBranch.to_s + "   " )
+log( "Device unit:" + deviceUnit.to_s  + "\n" )
 
 aux = deviceBranch * 256 + deviceUnit
 idBranch = "%04u" % aux
@@ -38,18 +51,24 @@ if ( $branchList.include?(idBranch) == false ) then
 	NOEMONStatus($pcBranch,$pcNo,deviceBranch,deviceUnit)
 end
 
+#    38  is the device template for lights
+# 1780  is the device template for StandardIrrigationSprinkler
 case cmdId
 	when 192      #  OFF
-	if (childNo<=$MaxLightId) then 
+	if ( childType == 38 ) then 
 		NOEMONTurnLight(childNo,true,$pcBranch,$pcNo,deviceBranch,deviceUnit)
-	else
-		NOEMONTurnRelay(childNo-$MaxLightId,true,$pcBranch,$pcNo,deviceBranch,deviceUnit)
+	end
+	
+	if ( childType == 1780 )
+		NOEMONTurnRelay(childNo,true,$pcBranch,$pcNo,deviceBranch,deviceUnit)
 	end
 	when 193      #  ON
-	if (childNo<=$MaxLightId) then 
+	if ( childType == 38 ) then 
 		NOEMONTurnLight(childNo,false,$pcBranch,$pcNo,deviceBranch,deviceUnit)
-	else
-		NOEMONTurnRelay(childNo-$MaxLightId,false,$pcBranch,$pcNo,deviceBranch,deviceUnit )
+	end
+	
+	if ( childType == 1780 )
+		NOEMONTurnRelay(childNo,false,$pcBranch,$pcNo,deviceBranch,deviceUnit )
 	end
 	
 	when 184      #  SetLevel
