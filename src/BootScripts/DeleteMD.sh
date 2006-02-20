514aefa2fb842dc905cf104b522bd35d45be4a6c
radu <radu.cx+linuxmce@gmail.com>
2006-02-20 13:33:37 +0000

    delete SSH key corresponding to deleted MD (closes #1716)

--- src/BootScripts/DeleteMD.sh
+++ src/BootScripts/DeleteMD.sh
@@ -4,14 +4,15 @@ IP="$1"
 MAC="$2"
 lcdMAC=$(echo ${MAC//:/-} | tr 'A-Z' 'a-z')
 
-sed -i "/$IP/ d" /root/.ssh/known_hosts &>/dev/null
-
 if [[ -z "$IP" ]]; then
 	echo "Error: Empty IP"
 	exit
 fi
 
 echo "Deleting MD files. MAC=$MAC IP=$IP"
+
+echo "Deleting SSH host key from Core"
+sed -i "/$IP/ d" /root/.ssh/known_hosts &>/dev/null
 
 echo "Filesystem"
 rm -rf /home/diskless/"$IP"
