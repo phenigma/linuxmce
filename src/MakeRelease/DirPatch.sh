fe2dcde41c395981a6f97c8ede9178e4f3221092
radu <radu.cx+linuxmce@gmail.com>
2006-02-20 15:12:21 +0000

    Updated DirPatch to be recursive and be aware of directory changes

--- src/MakeRelease/DirPatch.sh
+++ src/MakeRelease/DirPatch.sh
@@ -3,13 +3,21 @@
 replacementsL="/home/samba/repositories/pluto/replacements/main/binary-i386"
 replacementsR="/home/mirrors/replacements/main/binary-i386"
 
-# TODO: make recursive
-ScriptL="
-for i in *; do
-	stat -c %s \"\$i\" | tr '\n' ' '
-	md5sum \"\$i\"
-done | awk '{print \$3,\$2,\$1}' | sort -k1
-"
+Script()
+{
+	find | while read line; do
+		if [[ -d "$line" ]]; then
+			echo -n "0 0 $line "
+			echo "D"
+		elif [[ -f "$line" ]]; then
+			stat -c %s "$line" | tr '\n' ' '
+			md5sum "$line" | tr '\n' ' '
+			echo "F"
+		fi
+	done | awk '{print $4,$3,$2,$1}' | sort -k1,2
+}
+
+ScriptL="$(declare -f Script); Script"
 
 ScriptR="${ScriptL//\\\\/\\\\}"
 ScriptR="${ScriptR//\"/\\\"}"
@@ -21,9 +29,9 @@ location=$(pwd)
 for section in $sections; do
 	eval dirL=\$${section}L
 	eval dirR=\$${section}R
-	(cd "$dirL"; eval "$ScriptL") | sort -k1 >$section.local
-	ssh uploads@plutohome.com "cd \"$dirR\"; eval \"$ScriptR\"" | sort -k1 >$section.remote
-	diff -u $section.{remote,local} | egrep '^[+-]' | egrep -v '^\+\+\+ |^--- ' | sort -k1 | awk '{print $1}' >$section.changes
+	(cd "$dirL"; eval "$ScriptL") | sort -k1,2 >$section.local
+	ssh uploads@plutohome.com "cd \"$dirR\"; eval \"$ScriptR\"" | sort -k1,2 >$section.remote
+	diff -u $section.{remote,local} | egrep '^[+-]' | egrep -v '^\+\+\+ |^--- ' | sort -k1,2 | awk '{print $1,$2}' >$section.changes
 
 	: >$section.patch.sh
 	chmod +x $section.patch.sh
@@ -31,14 +39,30 @@ for section in $sections; do
 	echo '#!/bin/bash' >>$section.patch.sh
 	echo "mkdir -p $dirR" >>$section.patch.sh
 	echo "cd $dirR" >>$section.patch.sh
-	files=$(cat $section.changes | grep '^-' | cut -c2- | tr '\n' ' ')
-	for file in $files; do
-		echo "rm -f $file" >>$section.patch.sh
+
+	# Removed files/directories
+	entries=$(cat $section.changes | grep '^-' | cut -c2- | tr '\n ' ' _')
+	for entry in $entries; do
+		Type="${entry:0:1}"
+		FileName="${entry:2}"
+		case "$Type" in
+			F) echo "rm -f $FileName" >>$section.patch.sh ;;
+			D) echo "rmdir $FileName" >>$section.patch.sh ;;
+			*) echo "# Unknown type $Type" ;;
+		esac
 	done
 
-	files=$(cat $section.changes | grep '^\+' | cut -c2- | tr '\n' ' ')
-	for file in $files; do
-		echo "# add $file" >>$section.patch.sh
+	# Added files/directories
+	entries=$(cat $section.changes | grep '^\+' | cut -c2- | tr '\n ' ' _')
+	files=
+	for entry in $entries; do
+		Type="${entry:0:1}"
+		FileName="${entry:2}"
+		case "$Type" in
+			F) echo "# add file $FileName" >>$section.patch.sh; files="$files $FileName" ;;
+			D) echo "# add directory $FileName" >>$section.patch.sh ;; # don't add directories to the list; tar knows paths and we only need changed files
+			*) echo ""
+		esac
 	done
 	(cd $dirL; tar -czvf $location/$section.tar.gz $files)
 	echo "tar -xzvf /home/uploads/$section.tar.gz" >>$section.patch.sh
