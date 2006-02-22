4e9ab3a15b4da8a4e353daebc0e572c769ad0e57
rgavril <rgavril>
2006-02-22 15:56:18 +0000

    Startup script for Generic Firewire Camera device

---
+++ src/BootScripts/Start_vloopback.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function showUsage {
+        echo "Usage: $0 -r <RouterID> -d <DeviceID>"
+        echo
+}
+
+while getopts r:d: name
+do
+	echo "-"$name
+        case $name in
+                r) RouterID=$OPTARG ;;
+                d) PK_Device=$OPTARG ;;
+                ?)
+                        showUsage
+                        exit 1
+                ;;
+        esac
+done
+
+shift $(($OPTIND - 1))
+
+SQL="
+	SELECT 
+		IK_DeviceData 
+	FROM 
+		Device_DeviceData
+        WHERE 
+		FK_Device=$PK_Device AND FK_DeviceData=XX?
+"
+R=$(RunSQL "$SQL")
+IK_DeviceData=$(Filed 1 "$R")
+
+dc1394_vloopback --daemon --video1394=/dev/video1394-$IK_DeviceData
+
