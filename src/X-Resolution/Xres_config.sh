3120a255d6de9bf5a19f02eb42662c94469141c3
radu <radu.cx+linuxmce@gmail.com>
2006-02-27 17:04:59 +0000

    adepted to use new Xconfigure.sh script

--- src/X-Resolution/Xres_config.sh
+++ src/X-Resolution/Xres_config.sh
@@ -9,7 +9,7 @@ Type="$4"
 
 while [[ "$#" -gt 0 ]]; do
 	case "$1" in
-		"-f") Force=yes ;;
+		"-f") Force=--force ;;
 	esac
 	shift
 done
@@ -24,32 +24,8 @@ ShowDialog()
 	kill "$pidOfX"
 }
 
-ModeName="\"${Width}x${Height}\""
-Modeline="\"${Width}x${Height}\"$(/usr/pluto/bin/xtiming.pl "$Width" "$Height" "$Refresh" "$Type" | cut -d' ' -f2-)"
-
-awk -v "Force=$Force" '
-	BEGIN { Monitor = 0; Display = 0; }
-	/Modeline/ || /Modes/ { next }
-	/Section..*"Monitor"/ { print; Monitor = 1; next; }
-	Monitor == 1 && (/HorizSync/ || /VertRefresh/) { next; }
-	/EndSection/ && Monitor == 1 {
-		print "\tModeline '"${Modeline//\"/\\\"}"'";
-		if (Force == "yes") {
-			print "\tHorizSync 28-500";
-		}
-		else
-		{
-			print "\tHorizSync 28-90";
-		}
-		print "\tVertRefresh '"$Refresh"'";
-		print;
-		Monitor = 0;
-		next;
-	}
-	/SubSection..*"Display"/ { print; Display = 1; next; }
-	/EndSubSection/ && Display == 1 { print "\t\tModes '"${ModeName//\"/\\\"}"'"; print; Display = 0; next; }
-	{ print }
-' /etc/X11/XF86Config-4 >/etc/X11/XF86Config-4.test
+cp /etc/X11/XF86Config-4{,.test}
+/usr/pluto/bin/Xconfigure.sh --conffile /etc/X11/XF86Config-4.test --resolution "${Width}x${Height}@${Refresh}" $Force
 
 X :1 -ac -xf86config /etc/X11/XF86Config-4.test &
 pidOfX=
