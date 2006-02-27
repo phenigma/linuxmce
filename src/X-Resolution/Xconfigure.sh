d8537d8c46be0e3db0b90e764f9e31da89ba4025
radu <radu.cx+linuxmce@gmail.com>
2006-02-27 14:54:13 +0000

    suppress lshwd output; fix awk call

--- src/X-Resolution/Xconfigure.sh
+++ src/X-Resolution/Xconfigure.sh
@@ -11,14 +11,14 @@ done
 
 if [[ "$Defaults" == y ]]; then
 	cp /usr/pluto/templates/XF86Config-4.in /etc/X11/XF86Config-4.$$
-	lshwd -ox
+	lshwd -ox >/dev/null
 	DisplayDriver="$(grep 'Driver' /tmp/xinfo | awk '{print $2}' | tr -d '"')"
 	rm /tmp/xinfo
 	case "$DisplayDriver" in
 		nv) PackageIsInstalled pluto-nvidia-video-drivers && DisplayDriver="nvidia" ;;
 		radeon|ati) PackageIsInstalled fglrx-driver && DisplayDriver="fglrx" ;;
 	esac
-	awk -v "DisplayDriver=$DispayDriver" /usr/pluto/bin/X-ChangeDisplayDriver.awk /etc/X11/XF86Config-4.$$ >/etc/X11/XF86Config-4
+	awk -v "DisplayDriver=$DisplayDriver" -f/usr/pluto/bin/X-ChangeDisplayDriver.awk /etc/X11/XF86Config-4.$$ >/etc/X11/XF86Config-4
 	rm /etc/X11/XF86Config-4.$$
 	exit 0
 fi
