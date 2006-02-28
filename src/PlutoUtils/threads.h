636bde76e77fb71169a290e7f01d7a1d67d9aea8
cmiron <cmiron>
2006-02-28 08:02:01 +0000

    Added thread safe parameterized singleton (using pluto mutex).

---
+++ src/PlutoUtils/threads.h
@@ -0,0 +1,98 @@
+#ifndef __THREADS_HPP__
+#define __THREADS_HPP__
+
+#include "MultiThreadIncludes.h"
+
+namespace cpp {
+
+	namespace Threading { 
+
+///////////////////////////////////////////////////////////////////////////
+//	class SingleThreaded
+///////////////////////////////////////////////////////////////////////////
+
+		//	Defines the locking mechanisim for single threaded model
+		//	which is actually empty
+		template<typename T>
+		class SingleThreaded {
+		public : 
+			typedef T VolatileType;
+
+		protected : 
+			inline explicit SingleThreaded() {}
+			inline ~SingleThreaded() {}
+
+		protected : 
+			//	Perform thread locks
+			class LockThread {
+			public : 
+				inline explicit LockThread() { SingleThreaded::Lock(); }
+	            inline ~LockThread() { SingleThreaded::Unlock(); }
+
+			private : 
+				inline explicit LockThread(LockThread const&) {}
+				inline LockThread& operator=(LockThread const&) { return *this; }
+			};	//	end of class LockThread
+
+		private : 
+			//	thread synchronization helps
+			friend LockThread;
+
+			inline static void Lock() {}
+			inline static void Unlock() {}
+
+		private : 
+			inline explicit SingleThreaded(SingleThreaded const&) {}
+			inline SingleThreaded& operator=(SingleThreaded const&) { return *this; }
+		};	//	end of class SingleThreaded
+
+
+///////////////////////////////////////////////////////////////////////////
+//	class PlutoMultiThreaded
+///////////////////////////////////////////////////////////////////////////
+
+		//	Defines the locking mechanisim for single threaded model
+		//	
+		template<typename T>
+		class MultiThreaded {
+		public : 
+			typedef T VolatileType;
+
+		protected : 
+			inline explicit MultiThreaded() { m_Mutex.Init(NULL); }
+			inline ~MultiThreaded() { pthread_mutex_destroy(&m_Mutex.mutex);}
+
+		protected : 
+			//	Perform thread locks
+			class LockThread {
+			public : 
+				inline explicit LockThread() { MultiThreaded::Lock(); }
+	            inline ~LockThread() { MultiThreaded::Unlock(); }
+
+			private : 
+				inline explicit LockThread(LockThread const&) {}
+				inline LockThread& operator=(LockThread const&) { return *this; }
+			};	//	end of class LockThread
+
+		private : 
+			//	thread synchronization helps
+			friend LockThread;
+			static pluto_pthread_mutex_t m_Mutex;
+			//static PlutoLock m_PlutoLock;
+
+			inline static void Lock() {pthread_mutex_lock(&m_Mutex.mutex);}
+			inline static void Unlock() {pthread_mutex_unlock(&m_Mutex.mutex);}
+
+		private : 
+			inline explicit MultiThreaded(MultiThreaded const&) {}
+			inline MultiThreaded& operator=(MultiThreaded const&) { return *this; }
+		};	//	end of class MultiThreaded
+
+		template<typename T> pluto_pthread_mutex_t MultiThreaded<T>::m_Mutex("mutex");
+
+
+	};	//	end of namespace Threading
+
+};	//	end of namespace cpp
+
+#endif
