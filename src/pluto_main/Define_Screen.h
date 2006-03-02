1418cba744c9767c0b7d8b66ce7fba40ea70920e
aaron <aaron.b@plutohome.com>
2006-03-02 00:43:11 +0000

    Automatic Regen

--- src/pluto_main/Define_Screen.h
+++ src/pluto_main/Define_Screen.h
@@ -205,6 +205,8 @@
 #define SCREEN_Need_Reload_Router_CONST 204
 #define SCREEN_Need_Regen_Orbiter_CONST 205
 #define SCREEN_Sensors_Viewed_By_Camera_CONST 206
+#define SCREEN_mnuPVROSD_CONST 207
+#define SCREEN_mnuPVRRecording_CONST 208
 
 #define SCREEN_TABLE "Screen"
 #define SCREEN_PK_SCREEN_FIELD "PK_Screen"
