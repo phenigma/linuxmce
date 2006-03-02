0c70ae179f8e05223bd1f0ad202a9d91b17d3403
radu <radu.cx+linuxmce@gmail.com>
2006-03-02 12:43:55 +0000

    added locks

--- src/BootScripts/UpdateAvailableSerialPorts.sh
+++ src/BootScripts/UpdateAvailableSerialPorts.sh
@@ -2,7 +2,9 @@
 
 . /usr/pluto/bin/SQL_Ops.sh
 . /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/LockUtils.sh
 
+WaitLock "UpdateAvailableSerialPorts" "UASP"
 SerialPorts=$(/usr/pluto/bin/ListSerialPorts.sh | tr '\n' ',')
 SerialPorts=${SerialPorts%,}
 
@@ -11,3 +13,4 @@ DEVICEDATA_Available_Serial_Ports=133
 Q="REPLACE INTO Device_DeviceData(FK_Device, FK_DeviceData, IK_DeviceData)
 	VALUES ('$PK_Device', '$DEVICEDATA_Available_Serial_Ports', '${SerialPorts//\'/\'}')"
 RunSQL "$Q"
+Unlock "UpdateAvailableSerialPorts" "UASP"
