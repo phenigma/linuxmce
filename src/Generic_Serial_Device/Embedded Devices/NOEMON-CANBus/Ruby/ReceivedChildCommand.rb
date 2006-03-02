6dec3d1ba76e435466faae6c12cc0b923ca9dd92
radul <radul>
2006-03-02 14:55:20 +0000

    All information in device data 12

--- src/Generic_Serial_Device/Embedded Devices/NOEMON-CANBus/Ruby/ReceivedChildCommand.rb
+++ src/Generic_Serial_Device/Embedded Devices/NOEMON-CANBus/Ruby/ReceivedChildCommand.rb
@@ -1,11 +1,15 @@
-# 27-Feb-06 15:50 Receive command from child
+# 02-March-06 16:31 Receive command from child
 if ( $bInit == false ) then
 	log( "The initialisation is not finish yet\n")
 	return
 end
 
+log( "Received command from child" + "   " ) 
+logTime = Time.now
+timeStr = logTime.strftime("%d-%m-%Y  %H:%M:%S  ")
+log( "Time:" + timeStr.to_s + "\n" )
 
-log( "Received command from child" + "\n" ) 
+
 cmdId = cmd.id_
 cmdTo = cmd.devidto_
 cmdType= cmd.type_
@@ -20,34 +24,24 @@ deviceBranch=0
 deviceUnit=1
 
 val = device_.childdevices_[cmdTo]
-#try to read name from 138 or 12
-if ( val.devdata_.has_key?(138) ) then
-	childName = val.devdata_[138]
-else
-	if ( val.devdata_.has_key?(12) ) then
-		childName = val.devdata_[12]
-	else
-		log( "Couldn't read device id:" + "\n" )
+if ( val.devdata_.has_key?(12) ) then
+	childData = val.devdata_[12]
+	childList = childData.split( "," )
+	if ( childList.size() < 3 ) then
+		log( "Wrong format " + childData + "\n" )
 		return
 	end
-end
-
-if ( val.devdata_.has_key?(40) ) then
-	deviceBranch = val.devdata_[40].to_i
+	
+	log( "Read child data" + "   " )
+	deviceBranch  =  childList[0].to_i 
+	deviceUnit      =  childList[1].to_i
+	childNo          =  childList[2].to_i
 else
-	log( "Couldn't read device branch:" + "\n" )
+	log( "Couldn't read child data id:" + "\n" )
 	return
 end
 
-if ( val.devdata_.has_key?(147) ) then
-	deviceUnit = val.devdata_[147].to_i
-else
-	log( "Couldn't read devide unit:" + "\n" )
-	return
-end
-
-childNo= childName.to_i
-log( "Child name:" + childName + "   " )
+log( "Child name:" + childNo.to_s + "   " )
 log( "Device branch:" + deviceBranch.to_s + "   " )
 log( "Device unit:" + deviceUnit.to_s  + "\n" )
 
@@ -55,9 +49,10 @@ aux = deviceBranch * 256 + deviceUnit
 idBranch = "%04u" % aux
 
 if ( $branchList.include?(idBranch) == false ) then
-	log( "New branch found" )
-	$branchList.push(aux)
-	NOEMONStatus($pcBranch,$pcNo,deviceBranch,deviceUnit)
+	log( "New branch found" + idBranch.to_s + "\n" )
+	return 
+	#$branchList.push(aux)
+	#NOEMONStatus($pcBranch,$pcNo,deviceBranch,deviceUnit)
 end
 
 #    38  is the device template for lights
