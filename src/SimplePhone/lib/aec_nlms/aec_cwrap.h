a1f0a618066d3eda2acc699872d0bbe1a9020cf5
torindan <torindan>
2006-03-02 16:21:30 +0000

    Delete old iaxclient lib

--- src/SimplePhone/lib/aec_nlms/aec_cwrap.h
+++
@@ -1,9 +0,0 @@
-
-struct aec_nlms {
-	void *data;
-	void (*process)(struct aec_nlms *aec,short *in_out, short *ref, int samples);
-	void (*setambientdb)(struct aec_nlms *aec, float db);
-	void (*destroy)(struct aec_nlms *aec);
-};
-
-struct aec_nlms *aec_nlms_init(  );
