751aa2afd4b84a6ea863a33820f471ee9aecb458
radu <radu.cx+linuxmce@gmail.com>
2006-03-07 15:34:07 +0000

    Lowest priority

--- src/BootScripts/UpdateMediaDaemon.sh
+++ src/BootScripts/UpdateMediaDaemon.sh
@@ -2,6 +2,6 @@
 
 counter=30
 while ((counter > 0)); do
-	/usr/pluto/bin/UpdateMedia -h localhost -d /home/public/data -B
+	nice -n 20 /usr/pluto/bin/UpdateMedia -h localhost -d /home/public/data -B
 	((counter--))
 done
