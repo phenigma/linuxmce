791a987b95e3e55f01c79c8153652ee4d10e094c
rgavril <rgavril>
2006-03-08 16:10:41 +0000

    Orbiter is now startin XFree86 if he doesn't find it

--- src/Orbiter/LaunchOrbiter.sh
+++ src/Orbiter/LaunchOrbiter.sh
@@ -8,5 +8,13 @@ then
 fi
 
 export SDL_VIDEO_X11_WMCLASS="Orbiter"
+
+# test if XFree86 is running else manualy start it
+isX11Running=$(ps -A | grep XFree86 | wc -l)
+if [[ "$isX11Running" != "1" ]]; then
+	/usr/pluto/bin/Start_X.sh
+	export DISPLAY=:0
+fi
+
 exec ./Orbiter "$@"
 
