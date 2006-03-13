7685c31ce6c96d2ef4bf94803313b0a62b1c9b5c
rgavril <rgavril>
2006-03-13 16:46:45 +0000

    Script to be called from orbiter to force a upgrade

---
+++ src/PlutoUpdateSystem/ForceUpdates.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#MessageSend $IP -targetType template 0 26 1 67 13 "/bin/rm"
+
+## Kill all DownloadUpdates.sh scripts that are running
+DownloadUpdates_PIDS=$(pidof DownloadUpdates.sh)
+for pid in $DownloadUpdates_PIDS do;
+	kill $pid
+	wait $pid
+done
+
+## If onther ForceUpdates.sh instances are running, kill them too
+ForceUpdates_PIDS=$(pidof ForceUpdates.sh)
+for pid in $ForceUpdates_PIDS do;
+	if [[ "$pid" != "$$" ]]; then
+		kill $pid
+		wait $pid
+	fi
+done
+
+## Go ahead, force that upgrade
+UpdatesOkStamp=$(date +%s)
+touch /usr/pluto/var/Updates/UpdatesOk.flag
+echo $UpdatesOkStamp > /usr/pluto/var/Updates/UpdatesOk.flag
+
+## Force the reboot
+reboot
