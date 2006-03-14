50a87c0c9d59d040c8c22861015f7aaba7490046
radu <radu.cx+linuxmce@gmail.com>
2006-03-14 13:13:11 +0000

    fixed displayed strings for MDs

--- src/BootScripts/Network_DisplaySettings.sh
+++ src/BootScripts/Network_DisplaySettings.sh
@@ -9,13 +9,19 @@ done
 
 . /usr/pluto/bin/Network_Parameters.sh
 . /usr/pluto/bin/Config_Ops.sh
-
-if [[ -z "$IntIf" ]] && PackageIsInstalled pluto-dcerouter; then
-	IntIf="$ExtIf:0"
-	IntIP=192.168.80.1
-	IntNetmask=255.255.255.0
+if PackageIsInstalled pluto-dcerouter; then
+	if [[ -z "$IntIf" ]]; then
+		IntIf="$ExtIf:0"
+		IntIP=192.168.80.1
+		IntNetmask=255.255.255.0
+	fi
+else
+	IntIf=
+	IntIP=
+	IntNetmask=
+	ExtractData "$(ParseInterfaces | head -1)"
 fi
-	
+
 case "$DCERouter" in
 	localhost) RouterIP="127.0.0.1" ;;
 	dcerouter) RouterIP="192.168.80.1" ;; # TODO: remove hardcoded assumption
@@ -26,7 +32,7 @@ if [[ "$Orbiter" == y ]]; then
 	if [[ -n "$IntIf" ]]; then
 		echo "router=$RouterIP; internal=$IntIP($IntIf); external=$ExtIP($ExtIf)"
 	else
-		echo "router=$RouterIP; IP=$ExtIP($ExtIf)"
+		echo "router=$RouterIP; IP=$ExtIP"
 	fi
 else
 	echo "DCE Router: $DCERouter"
@@ -35,6 +41,6 @@ else
 		echo "Internal interface: $IntIf; IP: $IntIP"
 		echo "External interface: $ExtIf; IP: $ExtIP"
 	else
-		echo "Interface: $ExtIf; IP: $ExtIP"
+		echo "IP: $ExtIP"
 	fi
 fi
