643916dbbc20bd6ea0a18e4fe256ed60ac0d3f10
radu <radu.cx+linuxmce@gmail.com>
2006-03-14 15:04:29 +0000

    stop dhcp server and prevent dhcp from starting if disabled (closes #1829)

--- src/BootScripts/DHCP_config.sh
+++ src/BootScripts/DHCP_config.sh
@@ -3,7 +3,11 @@
 . /usr/pluto/bin/LockUtils.sh
 . /usr/pluto/bin/Network_Parameters.sh
 
-[[ -z "$DHCPsetting" ]] && exit
+if [[ -z "$DHCPsetting" ]]; then
+	rm -f /etc/dhcp3/dhcpd.conf
+	/etc/init.d/dhcp3-server stop
+	exit
+fi
 WaitLock "PlutoDHCP" "PlutoDHCP_config"
 /usr/pluto/bin/PlutoDHCP >/etc/dhcp3/dhcpd.conf
 /etc/init.d/dhcp3-server restart
