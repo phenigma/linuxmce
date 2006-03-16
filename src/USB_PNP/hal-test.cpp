218d948b78173ff9b487806eddb6d91066d06b4b
edgar <edgar>
2006-03-16 20:25:57 +0000

    isolated the message send part in a function

--- src/USB_PNP/hal-test.cpp
+++ src/USB_PNP/hal-test.cpp
@@ -43,6 +43,82 @@ void mainloop_integration (LibHalContext *ctx, DBusConnection * dbus_connection)
 	dbus_connection_setup_with_g_main (dbus_connection, NULL);
 }
 
+void sendMessage(char *params[], int count, string &returnValue)
+{
+	Event_Impl *pEvent = new Event_Impl(DEVICEID_MESSAGESEND, 0, hostname);
+	if(pEvent != NULL)
+	{			
+		for(int i=0;true;++i) // Wait up to 30 seconds
+		{
+			pEvent->m_pClientSocket->SendString("READY");
+			string sResponse;
+			if( !pEvent->m_pClientSocket->ReceiveString(sResponse,5) )
+			{
+				g_pPlutoLogger->Write(LV_CRITICAL,"Cannot communicate with router");
+				throw(new string("Cannot communicate with router"));
+			}
+			if( sResponse=="YES" )
+				break;
+			else if( sResponse=="NO" )
+			{
+				if( i>5 )
+				{
+					g_pPlutoLogger->Write(LV_CRITICAL,"Router not ready after 30 seconds.  Aborting....");
+					throw(new string("Router not ready after 30 seconds.  Aborting...."));
+				}
+				g_pPlutoLogger->Write(LV_STATUS,"DCERouter still loading.  Waiting 5 seconds");
+				Sleep(5000);
+			}
+			else
+			{
+				g_pPlutoLogger->Write(LV_CRITICAL,"Router gave unknown response to ready request %s",sResponse.c_str());
+				throw(new string("Router gave unknown response to ready request"));
+			}
+		}
+
+		Message *pMsg=new Message(13, params);
+		if(pMsg != NULL)
+		{
+			eExpectedResponse ExpectedResponse = pMsg->m_eExpectedResponse;
+			if( ExpectedResponse == ER_ReplyMessage )
+			{
+				// There are out parameters, we need to get a message back in return
+				Message *pResponse = pEvent->SendReceiveMessage( pMsg );
+				if( !pResponse || pResponse->m_dwID != 0 )
+				{
+					if(pResponse)
+					{
+						delete pResponse;
+						pResponse = NULL;
+					}								
+					g_pPlutoLogger->Write(LV_DEBUG, "Failed to send message" );
+				}
+				else
+				{
+					for( map<long, string>::iterator it=pResponse->m_mapParameters.begin();it!=pResponse->m_mapParameters.end();++it)
+					{
+						cout << (*it).first << ":" << (*it).second << endl;
+						returnValue = (*it).second;
+					}
+				}
+				delete pResponse;
+				pResponse = NULL;
+			}
+			else
+			{
+				g_pPlutoLogger->Write(LV_DEBUG, "message should have out parameters (PK_Device (int))");
+			}
+		}
+		else
+		{
+			throw(new string("pMsg == NULL"));
+		}
+	}
+	else
+		throw(new string("pEvent == NULL"));
+}
+
+
 void myDeviceAdded(LibHalContext * ctx, const char * udi)
 {
 	gchar *bus = hal_device_get_property_string (ctx, udi, "info.bus");
@@ -55,30 +131,31 @@ void myDeviceAdded(LibHalContext * ctx, const char * udi)
 		
 		if(usb_device_product_id == ZWAVE_PRODUCT_ID && usb_device_vendor_id == ZWAVE_VENDOR_ID)
 		{
+			gchar *product = hal_device_get_property_string (ctx, udi, "info.product");
+			gchar *vendor = hal_device_get_property_string (ctx, udi, "info.vendor");
+			gchar *serial = hal_device_get_property_string (ctx, udi, "usb_device.serial");
+			gchar *info_udi = hal_device_get_property_string (ctx, udi, "info.udi");
+			gchar *sysfs_path = hal_device_get_property_string (ctx, udi, "usb_device.linux.sysfs_path");
+			char buffer[1024];
 			
-				gchar *product = hal_device_get_property_string (ctx, udi, "info.product");
-				gchar *vendor = hal_device_get_property_string (ctx, udi, "info.vendor");
-				gchar *serial = hal_device_get_property_string (ctx, udi, "usb_device.serial");
-				gchar *info_udi = hal_device_get_property_string (ctx, udi, "info.udi");
-				gchar *sysfs_path = hal_device_get_property_string (ctx, udi, "usb_device.linux.sysfs_path");
-				char buffer[1024];
-				
-				
-				printf("%s | %s | %s | %s | %s | %s\n", bus, product, vendor, serial, info_udi, sysfs_path);
-				if(hostname == NULL)
-				{
-					hostname = (char *)malloc(10);
-					if(hostname != NULL)
-						strcpy(hostname, "localhost");
-				}
-				/*./MessageSend 10.0.0.87 -targetType template -o 0 27 1 718 44 1754 156 1 57 -1
-					0:OK
-					2:30
-				 * */
-				sprintf(buffer, "MessageSend %s -targetType template -o 0 27 718 44 %d 156 %d 57 -1", hostname, PLUTO_ZWAVE_TEMPLATE_ID, 
-					PLUTO_CORE_DEVICE_ID);
-				printf("%s\n", buffer);
-	//				system(buffer);
+			
+			printf("%s | %s | %s | %s | %s | %s\n", bus, product, vendor, serial, info_udi, sysfs_path);
+			if(hostname == NULL)
+			{
+				hostname = (char *)malloc(10);
+				if(hostname != NULL)
+					strcpy(hostname, "localhost");
+			}
+			/*./MessageSend 10.0.0.87 -targetType template -o 0 27 1 718 44 1754 156 1 57 -1
+				0:OK
+				2:30
+			 * */
+			snprintf(buffer, sizeof(buffer), "MessageSend %s -targetType template -o 0 27 1 718 44 %d 156 %d 57 -1", hostname, PLUTO_ZWAVE_TEMPLATE_ID, 
+				PLUTO_CORE_DEVICE_ID);
+			printf("%s\n", buffer);
+//				system(buffer);
+			try
+			{
 				
 				bool isNewDevice = true;
 				//find out if it is a new device by calling the command 
@@ -86,75 +163,41 @@ void myDeviceAdded(LibHalContext * ctx, const char * udi)
 				//function from General_Info_Plugin
 				if(isNewDevice)
 				{
-					char *params[12];
+					char *params[13];
 
 					params[0]  =	"-targetType";
 					params[1]  =	"template";
 					params[2]  =	"-o";
 					params[3]  =	"0";
 					params[4]  =	"27";
-					params[5]  =	"718";
-					params[6]  =	"44";
-					params[7]  =	PLUTO_ZWAVE_TEMPLATE_ID_STR;
-					params[8]  =	"156";
-					params[9] =	PLUTO_CORE_DEVICE_ID_STR;
-					params[10] =	"57";
-					params[11] =	"-1";
-							
-					Event_Impl *pEvent = new Event_Impl(DEVICEID_MESSAGESEND, 0, hostname);
-								
-					Message *pMsg=new Message(13, params);
-					if(pMsg != NULL)
-					{
-						eExpectedResponse ExpectedResponse = pMsg->m_eExpectedResponse;
-						if( ExpectedResponse == ER_ReplyMessage )
-						{
-							
-							
-							// There are out parameters, we need to get a message back in return
-							Message *pResponse = pEvent->SendReceiveMessage( pMsg );
-							if( !pResponse || pResponse->m_dwID != 0 )
-							{
-								if(pResponse)
-								{
-									delete pResponse;
-									pResponse = NULL;
-								}								
-								g_pPlutoLogger->Write(LV_DEBUG, "Failed to send message" );
-							}
-							else
-							{
-								for( map<long, string>::iterator it=pResponse->m_mapParameters.begin();it!=pResponse->m_mapParameters.end();++it)
-								{
-									cout << (*it).first << ":" << (*it).second << endl;
-								}
-							}
-							delete pResponse;
-							pResponse = NULL;
-						}
-						else
-						{
-							g_pPlutoLogger->Write(LV_DEBUG, "message should have out parameters (PK_Device (int))");
-						}
-					}
-					else
-					{
-						g_pPlutoLogger->Write(LV_DEBUG, "could not create Message object");
-					}
-					delete pMsg;
-					pMsg = NULL;
+					params[5]  =	"1";
+					params[6]  =	"718";
+					params[7]  =	"44";
+					params[8]  =	PLUTO_ZWAVE_TEMPLATE_ID_STR;
+					params[9]  =	"156";
+					params[10] =	PLUTO_CORE_DEVICE_ID_STR;
+					params[11] =	"57";
+					params[12] =	"-1";
+					string response;
+					sendMessage(params, 13, response);
 				}
+			}
+			catch(string ex)
+			{
+				g_pPlutoLogger->Write(LV_CRITICAL, "exception thrown: %s", ex.c_str());
+			}
 
-				g_free (product);
-				product = NULL;
-				g_free (vendor);
-				vendor = NULL;
-				g_free (serial);
-				serial = NULL;
-				g_free (info_udi);
-				info_udi = NULL;
-				g_free (sysfs_path);
-				sysfs_path = NULL;
+			g_free (product);
+			product = NULL;
+			g_free (vendor);
+			vendor = NULL;
+			g_free (serial);
+			serial = NULL;
+			g_free (info_udi);
+			info_udi = NULL;
+			g_free (sysfs_path);
+			sysfs_path = NULL;
+
 		}
 	}
 
