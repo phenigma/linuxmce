1c65dd0f8e29b7087713ecf79024ca925000720a
djrobx <djrobx@hotmail.com>
2006-03-17 07:23:31 +0000

    Text strings for hdtv settings

--- web/pluto-admin/languages/en/mythSettings.lang.php
+++ web/pluto-admin/languages/en/mythSettings.lang.php
@@ -1,5 +1,15 @@
 <?
 $TEXT_MYTH_SETTINGS_CONST='Myth Settings';
+$TEXT_MYTH_INSTRUCTIONS_CONST='To configure Myth, please go to the computing applications sections on the orbiter and choose MythTV Setup.   Return to this page if you wish to identify HDTV channels detected on a cable system.';
 $TEXT_PICK_THE_TUNING_DEVICE_CONST='Pick the tuning device';
 $TEXT_MYTH_SETTINGS_UPDATED_CONST='Myth settings updated.';
+$TEXT_MYTH_NOMYTHPLAYER='Cannot locate a MythTV player on selected device.';
+$TEXT_MYTH_UNASSIGNED_CABLE_CONST='You have an HDTV tuner set up for QAM cable.   Below is the list of channels that are not assigned to the guide.   Click the tune button to view the channel to see what it is, then choose the channel from the drop-down.   You may also choose to import from a known existing channel lineup.';
+$TEXT_MYTH_ASSIGNED_CABLE_CONST='Here are the channels that are associated with guide information.   If you need to re-assign an incorrect channel, remove it and reload the guide data to restore it to the list of unmapped channels:';
+$TEXT_MYTH_SET_CONST='Assign';
+$TEXT_MYTH_TUNE_CONST='Tune to channel';
+$TEXT_MYTH_LOAD_EPG_CONST='Finish and load guide data.';
+$TEXT_MYTH_CHANNEL_CONST='Channel';
+$TEXT_MYTH_STATION_CONST='Station';
+$TEXT_MYTH_PICK_MD='Show video on: ';
 ?>
