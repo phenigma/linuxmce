779a252b67a45b7f2e3c7491637ec882bc8bd5ee
radu <radu.cx+linuxmce@gmail.com>
2006-03-20 10:21:30 +0000

    added logging

--- src/BootScripts/SetTimeZone.sh
+++ src/BootScripts/SetTimeZone.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+. /usr/pluto/bin/pluto.func
+
 TimeZone="$1"
+
+Logging "$TYPE" "$SEVERITY_NORMAL" "Timezone" "Setting timezone to $TimeZone"
 
 File="/usr/share/zoneinfo/$TimeZone"
 [[ -z "$TimeZone" || ! -f "$File" ]] && exit 1
