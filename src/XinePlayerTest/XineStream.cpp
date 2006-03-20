6742da6b3b9dbb52bd0e1d94be022aca5799a8fe
radul <radul>
2006-03-20 11:01:37 +0000

    XineStream class

--- src/XinePlayerTest/XineStream.cpp
+++ src/XinePlayerTest/XineStream.cpp
@@ -1,28 +1,53 @@
 #include "XineStream.h"
 
+XineStream::XineStream()
+{ 
+	m_pStream = NULL;
+}
+
+XineStream::XineStream(xine_stream_t* pStream)
+{ 
+	m_pStream = pStream; 
+}
+
+XineStream::~XineStream()
+{
+	if( m_pStream );
+	// dispose here the stream?
+}
+
+void XineStream::operator =(xine_stream_t *pStream)
+{
+	if( !m_pStream )
+		m_pStream = pStream;
+}
+
 void  XineStream::ReadInfo()
 {
-	BitRate         =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_BITRATE           );
-	SeekAble        =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_SEEKABLE          );
-	VideoWidth      =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_WIDTH       );
-	VideoHeight     =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_HEIGHT      );
-	VideoRatio      =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_RATIO       );
-	VideoChannels   =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_CHANNELS    );
-	VideoStreams    =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_STREAMS     );
-	VideoBitRate    =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_BITRATE     );
-	VideoFOURCC     =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_FOURCC      );
-	VideoHandled    =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_VIDEO_HANDLED     );
-	FrameDuration   =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_FRAME_DURATION    );
-	AudioChanenels  =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_AUDIO_CHANNELS    );
-	AudioBits       =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_AUDIO_BITS        );
-	AudioSampleRate =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_AUDIO_SAMPLERATE  );
-	AudioBitRate    =  xine_get_stream_info( m_Stream, XINE_STREAM_INFO_AUDIO_BITRATE     );
+	if( !m_pStream )
+		return;
+	
+	BitRate         =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_BITRATE           );
+	SeekAble        =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_SEEKABLE          );
+	VideoWidth      =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_WIDTH       );
+	VideoHeight     =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_HEIGHT      );
+	VideoRatio      =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_RATIO       );
+	VideoChannels   =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_CHANNELS    );
+	VideoStreams    =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_STREAMS     );
+	VideoBitRate    =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_BITRATE     );
+	VideoFOURCC     =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_FOURCC      );
+	VideoHandled    =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_VIDEO_HANDLED     );
+	FrameDuration   =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_FRAME_DURATION    );
+	AudioChanenels  =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_AUDIO_CHANNELS    );
+	AudioBits       =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_AUDIO_BITS        );
+	AudioSampleRate =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_AUDIO_SAMPLERATE  );
+	AudioBitRate    =  xine_get_stream_info( m_pStream, XINE_STREAM_INFO_AUDIO_BITRATE     );
 }
 
 
 void XineStream::ScheduleOsdObject(XineOsdObject *pOsd, int timeOffsetShow, int timeOffsetHide)
 {
-	if (pOsd==NULL)
+	if( !pOsd || !m_pStream )
 		return;
 		
 	xine_osd_show( *pOsd, timeOffsetShow);
