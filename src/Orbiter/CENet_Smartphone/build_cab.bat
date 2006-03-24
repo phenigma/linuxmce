f380f5810608286aaafe617e51b176dd1d1b30d7
cmiron <cmiron>
2006-03-24 10:34:19 +0000

    Fixed batch file to use the right logo.

--- src/Orbiter/CENet_Smartphone/build_cab.bat
+++ src/Orbiter/CENet_Smartphone/build_cab.bat
@@ -1,4 +1,5 @@
-del "c:\$Installers\$OrbiterSmartphone\*.*" /Q
+del "c:\$Installers\$OrbiterSmartphone\*.exe" /Q
+del "c:\$Installers\$OrbiterSmartphone\*.dll" /Q
 
 cd ..\..\bin
 copy OrbiterSmartphone.exe "c:\$Installers\$OrbiterSmartphone\"
@@ -7,7 +8,6 @@ cd ..\Orbiter\CENet_Smartphone
 cd Misc
 copy AYGSHELL.DLL "c:\$Installers\$OrbiterSmartphone\"
 copy PthreadsCE.dll "c:\$Installers\$OrbiterSmartphone\"
-copy logo.gif "c:\$Installers\$OrbiterSmartphone\"
 cd ..
 
 "C:\Program Files\Windows CE Tools\wce420\SMARTPHONE 2003\Tools\CabwizSP.exe" Orbiter.inf
