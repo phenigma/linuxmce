b7afe18ae4c22b99acdbbb82021dfce8601a8d0d
radu <radu.cx+linuxmce@gmail.com>
2006-03-25 14:45:12 +0000

    pkg0

--- external/nvidia-graphics-drivers-1.0.7676/adapt-source.sh
+++ external/nvidia-graphics-drivers-1.0.7676/adapt-source.sh
@@ -21,7 +21,7 @@ for i in debian/* debian.binary/*; do
 		continue
 	fi
 	[[ -d "$i" ]] && continue
-	sed -i 's/7174/'"$version"'/g; s/pkg0/pkg1/g; s/glxtokens/glxext/g' "$i"
+	sed -i 's/7174/'"$version"'/g; s/glxtokens/glxext/g' "$i"
 done
 
 sed -i 's/README\(.txt\)*/README.txt/g' debian/nvidia-glx.docs*
