896d8a56d73bbb78dc493a428220ba5548fa8220
ciprian.m <ciprian.m>
2006-03-28 10:56:35 +0000

    Last OpenGL update which fix last known issue with highlight/selected area

---
+++ src/Orbiter/OpenGL/GL2DEffects/gl2deffectselectedarea.cpp
@@ -0,0 +1,54 @@
+#include "gl2deffectselectedarea.h"
+
+#include "GL2DEffectHighLightArea.h"
+
+#include "gl2deffectfactory.h"
+
+GL2DEffectSelectedArea::GL2DEffectSelectedArea(GL2DEffectFactory * EffectsEngine, int TimeForCompleteEffect)
+: GL2DEffectHighLight(EffectsEngine, TimeForCompleteEffect)
+{
+	Configured = false;
+	Button = dynamic_cast<TBasicWindow*> (Effects->Widgets->CreateWidget(
+		BASICWINDOW,
+		0, 0, 0, 0,
+		"None"
+		));
+}
+
+GL2DEffectSelectedArea::~GL2DEffectSelectedArea()
+{
+	Effects->Widgets->DeleteWidget(Button);
+	Button = NULL;
+}
+
+
+void GL2DEffectSelectedArea::Configure(PlutoRectangle* EffectSourceSize)
+{
+	if (!EffectSourceSize)
+		return;
+	this->EffectSourceSize.Left = (float)EffectSourceSize->X;
+	this->EffectSourceSize.Top = (float)(Effects->Widgets->GetHeight()) - EffectSourceSize->Y - EffectSourceSize->Height;
+	this->EffectSourceSize.Width = (float)EffectSourceSize->Width;
+	this->EffectSourceSize.Height = (float)EffectSourceSize->Height;
+}
+
+void GL2DEffectSelectedArea::Paint(int Time)
+{
+	if(!Configured)
+	{
+		Start = Effects->MilisecondTimmer();
+		Configured = true;
+		Button->SetRectCoordinates(EffectSourceSize);
+		Button->SetVisible(true);
+	}
+	float Step = Stage(float(Time));
+	if (Step <0.5)
+	{
+		Button->SetBackgroundColor(1.0f, 0.0f, 0.0f, Step);
+	}
+	else
+	{
+		Button->SetBackgroundColor(1.0f, 0.0f, 0.0f, 0.5f - (Step-0.5f)*2.f*0.3f);
+	}
+
+}
