d783ac6f0e6137b4fa7204600eaea3bed6063e68
ciprian.m <ciprian.m>
2006-03-28 10:55:23 +0000

    Last OpenGL update which fix last known issue with highlight/selected area

---
+++ src/Orbiter/OpenGL/GL2DEffects/GL2DEffectNoEffect.d
@@ -0,0 +1,12 @@
+GL2DEffectNoEffect.o: OpenGL/GL2DEffects/GL2DEffectNoEffect.cpp \
+  OpenGL/GL2DEffects/GL2DEffectNoEffect.h OpenGL/GL2DEffects/gl2deffect.h \
+  DesignObj_Orbiter.h DesignObj_Data.h ../SerializeClass/SerializeClass.h \
+  ../PlutoUtils/FileUtils.h ../PlutoUtils/StringUtils.h \
+  ../PlutoUtils/CommonIncludes.h ../PlutoUtils/Other.h \
+  ../PlutoUtils/getch.h ../PlutoUtils/MyStl.h \
+  ../SerializeClass/ShapesColors.h TextStyle.h \
+  ../PlutoUtils/GraphicFormat.h OpenGL/GL2DEffects/gl2deffectfactory.h \
+  ../PlutoUtils/MultiThreadIncludes.h \
+  OpenGL/GL2DWidgets/DrawingWidgetsEngine.h \
+  OpenGL/GL2DWidgets/basewidget.h OpenGL/GLDefs.h OpenGL/math3dutils.h \
+  OpenGL/GL2DWidgets/basicwindow.h
