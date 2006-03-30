11ece32cea6da43c4e7e0409d6c4f44e3a17cd23
aaron <aaron.b@plutohome.com>
2006-03-30 01:32:04 +0000

    add plug and play device

---
+++ src/Gen_Devices/Plug_And_PlayBase.d
@@ -0,0 +1,10 @@
+Plug_And_PlayBase.o: ../Gen_Devices/Plug_And_PlayBase.cpp \
+  ../Gen_Devices/Plug_And_PlayBase.h ../DCE/DeviceData_Impl.h \
+  ../DCE/Event_Impl.h ../DCE/ClientSocket.h ../DCE/Socket.h \
+  ../PlutoUtils/MultiThreadIncludes.h ../PlutoUtils/CommonIncludes.h \
+  ../DCE/Message.h ../SerializeClass/SerializeClass.h \
+  ../PlutoUtils/FileUtils.h ../PlutoUtils/StringUtils.h \
+  ../PlutoUtils/Other.h ../PlutoUtils/getch.h ../PlutoUtils/MyStl.h \
+  ../DCE/DeviceData_Base.h ../pluto_main/Define_DeviceCategory.h \
+  ../DCE/Command_Impl.h ../DCE/HandleRequestSocket.h ../DCE/Logger.h \
+  ../DCE/PlutoLockLogger.h
