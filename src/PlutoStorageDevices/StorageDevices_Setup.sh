cb3a1995a6d6d38755140e34f37e56029f600f27
rgavril <rgavril>
2006-03-31 11:42:41 +0000

    Added pluto dir struct creating in StorageDevices_Setup

--- src/PlutoStorageDevices/StorageDevices_Setup.sh
+++ src/PlutoStorageDevices/StorageDevices_Setup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+## Start autmounter
 if [[ -x /etc/init.d/autofs ]]; then
 	/etc/init.d/autofs restart
 else
 	echo "WARNING: Autmount daemon doesn't exist ?!"
 fi
 
-/usr/pluto/bin/StorageDevices_ExportsNFS.sh
+## Setup mount for all storage devices (runs on every computer)
 /usr/pluto/bin/StorageDevices_Mounts.sh
+
+## Exports our local storage devices so other computers can access them (runs on every computer)
+/usr/pluto/bin/StorageDevices_ExportsNFS.sh
+
+## Creates a Pluto Directory structure on local devices (runs on every computer)
+/usr/pluto/bin/StorageDevices_PlutoDirStructure.sh
+
+## Creates symlinks to storage device in /home (runs only on core)
 /usr/pluto/bin/StorageDevices_Symlinks.sh
 
