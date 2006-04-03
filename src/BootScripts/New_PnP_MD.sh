30e00da68bd24545b5abdc9cd7020208caae0392
radu <radu.cx+linuxmce@gmail.com>
2006-04-03 16:33:43 +0000

    added setup code for new MD

--- src/BootScripts/New_PnP_MD.sh
+++ src/BootScripts/New_PnP_MD.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+. /usr/pluto/bin/pluto.func
+
+RemoteIP="$1"
+RemoteMAC="$2"
+
+Logging "$TYPE" "$SEVERITY_NORMAL" "New PnP MD" "Setting up new PnP MD with MAC address '$RemoteMAC' and provisional IP '$RemoteIP'"
+
+Interactor_Port=7238
+
+DEVICETEMPLATE_Generic_PC_as_MD=28
+
+NewDevice=$(/usr/pluto/bin/CreateDevice -d "$DEVICETEMPLATE_Generic_PC_as_MD" -M "$RemoteMAC" | tail -1)
+Logging "$TYPE" "$SEVERITY_NORMAL" "Created MD with Device ID '$NewDevice'"
+
+NewIP=$(/usr/pluto/bin/PlutoDHCP.sh -d "$New_Device" -a)
+Logging "$TYPE" "$SEVERITY_NORMAL" "Assigned permanent IP '$NewIP' to device '$NewDevice'"
+
+Logging "$TYPE" "$SEVERITY_NORMAL" "Running diskless MD setup command suite"
+# Note: keep this in sync with /var/www/pluto-admin/operations/logs/executeLog.php, case 1
+/usr/pluto/bin/Diskless_Setup.sh
+/usr/pluto/bin/DHCP_config.sh
+/usr/pluto/bin/Diskless_ExportsNFS.sh
+# End of list to be kept in sync
+Logging "$TYPE" "$SEVERITY_NORMAL" "Finished running diskless MD setup command suite"
+
+Logging "$TYPE" "$SEVERITY_NORMAL" "Rebooting new MD"
+echo "reboot" | nc "$RemoteIP" "$Interactor_Port"
