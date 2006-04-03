4a09e1e6958fcd2445e4d89e411094e86c6760ff
radu <radu.cx+linuxmce@gmail.com>
2006-04-03 17:15:15 +0000

    extra output

--- misc_utils/NewMD_interactor/90_start_interactor.sh
+++ misc_utils/NewMD_interactor/90_start_interactor.sh
@@ -7,12 +7,14 @@ bright=".[1m"
 color=".[1;33m"
 normal=".[0m"
 
-echo "$bright**$fil**$normal"
-echo "$bright* $color$msg$normal$bright *$normal"
-echo "$bright**$fil**$normal"
-
 MyIP=$(/sbin/ifconfig eth0 | awk 'NR==2 { print substr($2, index($2, ":")) }')
 MyMAC=$(/sbin/ifconfig eth0 | awk 'NR==1 { print $5 }')
 Gateway=$(/sbin/route -n | awk '/^0\.0\.0\.0/ { print $2 }')
+
+echo "IP: $MyIP; MAC: $MyMAC; Gateway: $Gateway"
+
+echo "$bright**$fil**$normal"
+echo "$bright* $color$msg$normal$bright *$normal"
+echo "$bright**$fil**$normal"
 
 /sbin/interactor "$Gateway" "$MyIP" "$MyMAC"
