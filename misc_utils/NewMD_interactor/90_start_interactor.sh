d52c99ee9b2d5cbf400c9b2540bf78fcabc1227f
radu <radu.cx+linuxmce@gmail.com>
2006-04-03 17:14:16 +0000

    script to start the interactor from the initrd

---
+++ misc_utils/NewMD_interactor/90_start_interactor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+msg="Please wait while MD filesystem is created. System will reboot when done"
+fil="************************************************************************"
+
+bright=".[1m"
+color=".[1;33m"
+normal=".[0m"
+
+echo "$bright**$fil**$normal"
+echo "$bright* $color$msg$normal$bright *$normal"
+echo "$bright**$fil**$normal"
+
+MyIP=$(/sbin/ifconfig eth0 | awk 'NR==2 { print substr($2, index($2, ":")) }')
+MyMAC=$(/sbin/ifconfig eth0 | awk 'NR==1 { print $5 }')
+Gateway=$(/sbin/route -n | awk '/^0\.0\.0\.0/ { print $2 }')
+
+/sbin/interactor "$Gateway" "$MyIP" "$MyMAC"
