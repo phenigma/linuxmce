1d7660fa7c5231a8a86d4c8a238d30ed4b4a13f3
torindan <torindan>
2006-04-05 13:08:05 +0000

    add delete_phoneline script

---
+++ src/Asterisk/delete_phoneline.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+#check params
+unless(defined($ARGV[0]) && defined($ARGV[1]) && defined($ARGV[2]))
+{
+    print "USAGE :$0 <line_nr> <line_name> <phone_nr>\n";
+    exit(-1);
+}
+
+my $LINE_NR = $ARGV[0];
+my $LINE_NAME = $ARGV[1];
+my $PHONE_NR = $ARGV[2];
+
+$LINE_NAME =~ s/(\w+)[-]out/$1/;
+
+system("curl 'http://localhost/pluto-admin/amp/admin/config.php?display=6&extdisplay=OUT_$LINE_NR&action=deltrunk'");
+system("curl 'http://localhost/pluto-admin/amp/admin/config.php?display=7&extdisplay=$PHONE_NR/&action=delIncoming'");
+
+system("curl 'http://localhost/pluto-admin/amp/admin/config.php?display=8' > /tmp/curl.log");
+open(PAGE,"/tmp/curl.log") or die "Bad thing happend";
+my $OUT_ROUTE = "";
+while(<PAGE>)
+{
+    chomp;
+    if($_ =~ /<a\s+[^>]+href[=]\"[^"]+?extdisplay[=]([^"]+)\">\d+\s+$LINE_NAME<\/a>/)
+    {
+        $OUT_ROUTE = $1;
+    }
+}
+close(PAGE);
+system("curl 'http://localhost/pluto-admin/amp/admin/config.php?display=8&extdisplay=$OUT_ROUTE&action=delroute'");
+system("curl 'http://localhost/pluto-admin/amp/admin/config.php?display=8&clk_reload=true'");
+
