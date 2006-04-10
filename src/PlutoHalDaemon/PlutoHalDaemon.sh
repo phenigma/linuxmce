11499375841de17b008290d9e90db7451965a79b
rgavril <rgavril>
2006-04-10 14:27:13 +0000

    Respawn pluto hal daemon every 10 seconds if is dead

--- src/PlutoHalDaemon/PlutoHalDaemon.sh
+++ src/PlutoHalDaemon/PlutoHalDaemon.sh
@@ -5,3 +5,19 @@
 
 ## Start our daemon in a screen
 screen -d -m -S "PlutoHalDaemon.sh" /usr/pluto/bin/pluto-hald dcerouter
+
+while [[ "1" == "1" ]];do
+
+	sleep 10
+
+	pidHal=$(pidof hald)
+	if [[ $pidHal == "" ]];then
+		/etc/init.d/dbus-1 restart
+	fi
+	
+
+	pidPlutoHal=$(pidof pluto-hald)
+	if [[ $pidPlutoHal == "" ]]; then
+		screen -d -m -S "PlutoHalDaemon.sh" /usr/pluto/bin/pluto-hald dcerouter
+	fi
+done
