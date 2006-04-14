0ec27586141b3bf996340df1c58354030a7435bb
rgavril <rgavril>
2006-04-14 16:27:18 +0000

    Start icewm instead of ratpoison

--- src/BootScripts/Start_X.sh
+++ src/BootScripts/Start_X.sh
@@ -4,7 +4,7 @@
 Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Starting X server"
 
 #screen -d -m -S XWindowSystem X -ac -allowMouseOpenFail vt7
-screen -d -m -S XWindowSystem xinit /usr/pluto/bin/ratpoison -- :0 -ac -allowMouseOpenFail vt7
+screen -d -m -S XWindowSystem xinit /usr/bin/icewm-session -- :0 -ac -allowMouseOpenFail vt7
 #sleep 1
 
 # Disable DPMS and screen saver
