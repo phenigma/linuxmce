923895467a0566c1f0cd1da3bc70e02caa062f11
rgavril <rgavril>
2006-04-14 10:52:56 +0000

    Configuration script for internal disk drive

---
+++ src/PlutoStorageDevices/StorageDevices_InternalDiskDrive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Params=("$@")
+for ((i = 0; i < ${#Params[@]}; i++)); do
+	case "${Params[$i]}" in
+		-d) ((i++)); Device_ID="${Params[$i]}" ;;
+		-i) ((i++)); Device_IP="${Params[$i]}" ;;
+		-m) ((i++)); Device_MAC="${Params[$i]}" ;;
+	esac
+done
+
+/usr/pluto/bin/StorageDevices_Setup.sh --cascade
+
