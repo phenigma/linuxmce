40742c39ed1363a4621bcd30052dfbbfcb4e6255
radu <radu.cx+linuxmce@gmail.com>
2006-04-28 16:38:42 +0000

    logs

--- src/AVWizard/scripts/UpdateResolution.sh
+++ src/AVWizard/scripts/UpdateResolution.sh
@@ -5,15 +5,19 @@
 Param="$1"
 case "$Param" in
 	4[_:]3)
+		echo "--> Aspect ratio 4:3"
 		exit 0; # Already in 4:3; no change
 	;;
 	16[_:]9)
+		echo "--> Aspect ratio 16:9"
 		$Wiz -set Video_Radio '16_9'
 		$Wiz -set Video_Resolution '480p'
 		$Wiz -set Video_Refresh '60'
 		"$BaseDir"/Xconfigure.sh --conffile "$XF86Config" --defaults --resolution '848x480'
 		kill -USR1 $PPID
 	;;
-	resolution[+-]|refresh[+-]) ;;
+	resolution[+-]|refresh[+-])
+		echo "--> TODO: $Param"
+	;;
 	*) echo "Unknown parameter: '$Param'"; exit 1 ;;
 esac
