b27e16bda508a140e58c35d821ce6add44e6423e
rgavril <rgavril>
2006-04-28 13:47:53 +0000

    No need to continue this

--- src/PlutoHalDaemonNG/PlutoHalDaemon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Make sure the had / dbus-1 are started
-/etc/init.d/dbus-1 start
-
-## Start our daemon in a screen
-screen -d -m -S "PlutoHalDaemon.sh" /usr/pluto/bin/pluto-hald dcerouter
