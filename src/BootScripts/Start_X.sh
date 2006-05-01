ca10f65ff14878e1869c31430d04af180f7568b9
radu <radu.cx+linuxmce@gmail.com>
2006-05-01 16:04:41 +0000

    Added posibility to specify parameters to X client and to launch X in foregound (to wait until it exits)

--- src/BootScripts/Start_X.sh
+++ src/BootScripts/Start_X.sh
@@ -2,19 +2,26 @@
 . /usr/pluto/bin/pluto.func
 
 XClient=/usr/bin/icewm-session
+XClientParm=()
+Background=y
 
 for ((i = 1; i < "$#"; i++)); do
 	case "${!i}" in
-		--client) ((i++)); XClient=${!i} ;;
+		-client) ((i++)); XClient=${!i} ;;
+		-parm) ((i++)); XClientParm=("${XClientParm[@]}" ${!i}) ;;
+		-fg) Background=n
 	esac
 done
 
-Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Starting X server (client: $XClient)"
+Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "Starting X server (client: $XClient; parms: ${XClientParm[*]})"
 
 # Start X11
-screen -d -m -S XWindowSystem xinit "$XClient" -- :0 -ac -allowMouseOpenFail vt7
-
-# Start everouter for gyration mouse
-if [[ -x /usr/pluto/bin/StartGyrationEvrouter.sh ]]; then
-	screen -d -m -S GyrationMouse /usr/pluto/bin/StartGyrationEvrouter.sh
+if [[ "$Background" == y ]]; then
+	screen -d -m -S XWindowSystem xinit "$XClient" "${XClientParm[@]}" -- :0 -ac -allowMouseOpenFail vt7
+	# Start everouter for gyration mouse
+	if [[ -x /usr/pluto/bin/StartGyrationEvrouter.sh ]]; then
+		screen -d -m -S GyrationMouse /usr/pluto/bin/StartGyrationEvrouter.sh
+	fi
+else
+	xinit "$XClient" "${XClientParm[@]}" -- :0 -ac -allowMouseOpenFail vt7
 fi
