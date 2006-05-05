9aaca060d0a97b50e81aa433dae71352de4cd143
vali <vali>
2006-05-05 12:48:56 +0000

    Added extra slash at the end of find command

--- web/pluto-admin/include/utils.inc.php
+++ web/pluto-admin/include/utils.inc.php
@@ -864,7 +864,7 @@ function grabDirectory ($path, $depth) {
 function grabFiles($path,$fileParm='-type f',$startingWith='') {
 	$filesArray=array();
 	// required to read files larger than 2G
-	$PathParm=($startingWith!='')?'"'.$path.'/'.$startingWith.'"*':'"'.$path.'"';
+	$PathParm=($startingWith!='')?'"'.$path.'/'.$startingWith.'"*':'"'.$path.'/"';
 	$cmd='sudo -u root find '.$PathParm.' '.$fileParm.' -maxdepth 1 -not -name \'*.id3\'';
 	//echo $cmd;
 	exec($cmd,$retArray);
