132990eeb4fdfb75681c01104ca6069679008a07
rgavril <rgavril>
2006-05-09 13:28:31 +0000

    not used in pluto

--- external/alsa-plugins-1.0.10/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the alsa-plugins package
-#
-0 4	* * *	root	alsa-plugins_maintenance
--- external/alsa-plugins-1.0.10/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(alsa-plugins):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="alsa-plugins" command="/usr/bin/alsa-plugins"
