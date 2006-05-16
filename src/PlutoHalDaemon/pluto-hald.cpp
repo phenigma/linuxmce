9247de25cb4b9682221c22c09d764bbf7b37566d
eugenc <eugenc>
2006-05-16 17:49:05 +0000

    Important HAL API changes.

--- src/PlutoHalDaemon/pluto-hald.cpp
+++ src/PlutoHalDaemon/pluto-hald.cpp
@@ -33,6 +33,8 @@ map<unsigned int, int> templatesMap;
 
 char *hostname = NULL;
 
+DBusError halError;
+
 using namespace DCE;
 
 namespace DCE
@@ -230,21 +232,21 @@ void myDeviceAdded(LibHalContext * ctx, const char * udi)
 {
 //	static char last_udi[2048];
 
-	gchar *bus = hal_device_get_property_string (ctx, udi, "info.bus");
+	gchar *bus = libhal_device_get_property_string (ctx, udi, "info.bus", &halError);
 	if( bus != NULL &&
 		strcmp(bus, "usb_device") == 0 &&
 		strlen(bus) == strlen("usb_device") )
 	{
 //		strcpy(last_udi, udi);
 		//hal_device_print (ctx, udi);
-		int usb_device_product_id = hal_device_get_property_int(ctx, udi, "usb_device.product_id");
-		int usb_device_vendor_id = hal_device_get_property_int(ctx, udi, "usb_device.vendor_id");
+		int usb_device_product_id = libhal_device_get_property_int(ctx, udi, "usb_device.product_id", &halError);
+		int usb_device_vendor_id = libhal_device_get_property_int(ctx, udi, "usb_device.vendor_id", &halError);
 
 		map<unsigned int, int>::iterator it;
 		it = templatesMap.find(((usb_device_vendor_id & 0xffff) << 16) | (usb_device_product_id & 0xffff) );
 		if(it != templatesMap.end())
 		{
-			gchar *info_udi = hal_device_get_property_string (ctx, udi, "info.udi");
+			gchar *info_udi = libhal_device_get_property_string (ctx, udi, "info.udi", &halError);
 			
 			try
 			{
@@ -328,13 +330,13 @@ void myDeviceAdded(LibHalContext * ctx, const char * udi)
 
 void myDeviceNewCapability(LibHalContext * ctx, const char * udi, const char *capability)
 {
-	gchar *serial_port = hal_device_get_property_string (ctx, udi, "linux.sysfs_path");
+	gchar *serial_port = libhal_device_get_property_string (ctx, udi, "linux.sysfs_path", &halError);
 	if(serial_port != NULL)
 	{
-		gchar *parent = hal_device_get_property_string (ctx, hal_device_get_property_string(ctx, udi, "info.parent"), "info.parent");
-		gchar *info_udi = hal_device_get_property_string (ctx, parent, "info.udi");
-		int usb_device_product_id = hal_device_get_property_int(ctx, parent, "usb_device.product_id");
-		int usb_device_vendor_id = hal_device_get_property_int(ctx, parent, "usb_device.vendor_id");
+		gchar *parent = libhal_device_get_property_string (ctx, libhal_device_get_property_string(ctx, udi, "info.parent", &halError), "info.parent", &halError);
+		gchar *info_udi = libhal_device_get_property_string (ctx, parent, "info.udi", &halError);
+		int usb_device_product_id = libhal_device_get_property_int(ctx, parent, "usb_device.product_id", &halError);
+		int usb_device_vendor_id = libhal_device_get_property_int(ctx, parent, "usb_device.vendor_id", &halError);
 		
 		g_pPlutoLogger->Write(LV_DEBUG, "udi = %s parent = %s capability = %s serial port = %s\n", udi, parent, capability, serial_port);
 		
@@ -437,7 +439,7 @@ void myDeviceRemoved(LibHalContext * ctx, const char * udi)
 void initialize(LibHalContext * ctx)
 {
 	int num_devices = 0;
-	char **devices = hal_get_all_devices (ctx, &num_devices);
+	char **devices = libhal_get_all_devices (ctx, &num_devices, &halError);
 	gchar *bus = NULL;
 
 	//get all template IDs and producer_id vendor_id
@@ -445,22 +447,22 @@ void initialize(LibHalContext * ctx)
 	{
 		char *udi = devices[i];
 		g_pPlutoLogger->Write(LV_DEBUG, "init udi = %s\n", udi);
-		bus = hal_device_get_property_string (ctx, udi, "info.bus");
+		bus = libhal_device_get_property_string (ctx, udi, "info.bus", &halError);
 		if( 0 == strcmp(bus, "usb_device") )
 		{
-			int usb_device_product_id = hal_device_get_property_int(ctx, udi, "usb_device.product_id");
-			int usb_device_vendor_id = hal_device_get_property_int(ctx, udi, "usb_device.vendor_id");
+			int usb_device_product_id = libhal_device_get_property_int(ctx, udi, "usb_device.product_id", &halError);
+			int usb_device_vendor_id = libhal_device_get_property_int(ctx, udi, "usb_device.vendor_id", &halError);
 			
 			map<unsigned int, int>::iterator it =
 				templatesMap.find( (unsigned int) ((usb_device_vendor_id & 0xffff) << 16) | (usb_device_product_id & 0xffff) );
 			g_pPlutoLogger->Write(LV_DEBUG, "searching for %08x \n", (unsigned int) ((usb_device_vendor_id & 0xffff) << 16) | (usb_device_product_id & 0xffff));
 			if( it != templatesMap.end() )
 			{
-//					gchar *product = hal_device_get_property_string (ctx, udi, "info.product");
-//					gchar *vendor = hal_device_get_property_string (ctx, udi, "info.vendor");
-//					gchar *serial = hal_device_get_property_string (ctx, udi, "usb_device.serial");
-					gchar *info_udi = hal_device_get_property_string (ctx, udi, "info.udi");
-//					gchar *sysfs_path = hal_device_get_property_string (ctx, udi, "usb_device.linux.sysfs_path");
+//					gchar *product = libhal_device_get_property_string (ctx, udi, "info.product");
+//					gchar *vendor = libhal_device_get_property_string (ctx, udi, "info.vendor");
+//					gchar *serial = libhal_device_get_property_string (ctx, udi, "usb_device.serial");
+					gchar *info_udi = libhal_device_get_property_string (ctx, udi, "info.udi", &halError);
+//					gchar *sysfs_path = libhal_device_get_property_string (ctx, udi, "usb_device.linux.sysfs_path");
 					
 				try
 				{
@@ -537,16 +539,16 @@ void initialize(LibHalContext * ctx)
 		}
 		else if( 0 == strcmp(bus, "usb-serial") )
 		{
-			gchar *parent = hal_device_get_property_string (ctx, hal_device_get_property_string(ctx, udi, "info.parent"), "info.parent");
-			gchar *info_udi = hal_device_get_property_string (ctx, parent, "info.udi");
-			int usb_device_product_id = hal_device_get_property_int(ctx, parent, "usb_device.product_id");
-			int usb_device_vendor_id = hal_device_get_property_int(ctx, parent, "usb_device.vendor_id");
+			gchar *parent = libhal_device_get_property_string (ctx, libhal_device_get_property_string(ctx, udi, "info.parent", &halError), "info.parent", &halError);
+			gchar *info_udi = libhal_device_get_property_string (ctx, parent, "info.udi", &halError);
+			int usb_device_product_id = libhal_device_get_property_int(ctx, parent, "usb_device.product_id", &halError);
+			int usb_device_vendor_id = libhal_device_get_property_int(ctx, parent, "usb_device.vendor_id", &halError);
 			
 			map<unsigned int, int>::iterator it =
 				templatesMap.find( (unsigned int) ((usb_device_vendor_id & 0xffff) << 16) | (usb_device_product_id & 0xffff) );
 			if( it != templatesMap.end() )
 			{
-				gchar *serial_port = hal_device_get_property_string (ctx, udi, "linux.sysfs_path");
+				gchar *serial_port = libhal_device_get_property_string (ctx, udi, "linux.sysfs_path", &halError);
 				if(serial_port != NULL)
 				{
 					string portID;
@@ -621,7 +623,6 @@ void initialize(LibHalContext * ctx)
 
 int main(int argc, char* argv[])
 {
-	LibHalFunctions funcs;
 	LibHalContext * ctx = NULL;
 	GMainLoop * loop = NULL;
 	
@@ -712,30 +713,39 @@ int main(int argc, char* argv[])
 	
 	loop = g_main_loop_new (NULL, FALSE);
 	
-	funcs.main_loop_integration = mainloop_integration;
+/*	funcs.main_loop_integration = mainloop_integration;
 	funcs.device_added = myDeviceAdded;
 	funcs.device_removed = myDeviceRemoved;
 	funcs.device_new_capability = myDeviceNewCapability;
 	funcs.device_lost_capability = NULL;
 	funcs.device_property_modified = NULL;
-	funcs.device_condition = NULL;
+	funcs.device_condition = NULL;*/
 	
-	ctx = hal_initialize(&funcs, 0);
+	ctx = libhal_ctx_new();
 	if ( ctx == NULL )
 	{
 		g_pPlutoLogger->Write(LV_DEBUG, "CTX is NULL!\n");
 		return 1;
 	}
+	
+	libhal_ctx_set_device_added(ctx, myDeviceAdded);
+	libhal_ctx_set_device_removed(ctx, myDeviceRemoved);
+	libhal_ctx_set_device_new_capability(ctx, myDeviceNewCapability);
+	
+	libhal_ctx_init(ctx, &halError);
+	
 	initialize(ctx);
 	
 	// remove the lock file
 	unlink(lockName.c_str());
 	
-	hal_device_property_watch_all(ctx);
+	libhal_device_property_watch_all(ctx, &halError);
 
 	g_main_loop_run(loop);
 	
-	hal_shutdown(ctx);
+	libhal_ctx_shutdown(ctx, &halError);
+	libhal_ctx_free(ctx);
+	
 	free(hostname);
 	
 	return 0;
