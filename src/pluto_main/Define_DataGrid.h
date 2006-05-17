40621795900f7fe32a0558d31cf29f602fc31607
radul <radul>
2006-05-17 11:42:31 +0000

    AV Wizard

--- src/pluto_main/Define_DataGrid.h
+++ src/pluto_main/Define_DataGrid.h
@@ -70,8 +70,10 @@
 #define DATAGRID_Select_Available_Inputs_CONST 68
 #define DATAGRID_Media_Connector_Type_CONST 69
 #define DATAGRID_Confirm_Inputs_Codes_CONST 70
-#define DATAGRID_Select_available_DSPMODES_CONST 71
+#define DATAGRID_Select_Available_DSPModes_CONST 71
 #define DATAGRID_Device_Templates_By_Categ_CONST 73
+#define DATAGRID_Confirm_Inputs_Order_CONST 74
+#define DATAGRID_Confirm_DSPModes_Order_CONST 75
 
 #define DATAGRID_TABLE "DataGrid"
 #define DATAGRID_PK_DATAGRID_FIELD "PK_DataGrid"
--- src/pluto_main/Define_DesignObj.h
+++ src/pluto_main/Define_DesignObj.h
@@ -2091,8 +2091,9 @@
 #define DESIGNOBJ_hiddenShowMediaTracks_CONST 5041
 #define DESIGNOBJ_hiddenRoomPlaceholder_CONST 5042
 #define DESIGNOBJ_dgTVDspModes_CONST 5043
-#define DESIGNOBJ_dgChangeOrder_CONST 5044
+#define DESIGNOBJ_dgDSPModesOrder_CONST 5044
 #define DESIGNOBJ_dgMediaTypeSingleInput_CONST 5047
+#define DESIGNOBJ_dgInputsOrder_CONST 5048
 
 #define DESIGNOBJ_TABLE "DesignObj"
 #define DESIGNOBJ_PK_DESIGNOBJ_FIELD "PK_DesignObj"
