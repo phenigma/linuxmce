b4c481822ad826c19c28516a2320e460cb8f9bea
rgavril <rgavril>
2006-05-17 07:52:50 +0000

    Bluez utils was updates and the init script has been renamed

--- src/Bluetooth_Dongle/mkr_postinst.sh
+++ src/Bluetooth_Dongle/mkr_postinst.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sed -i 's!\<pin_helper.*$!pin_helper /usr/pluto/bin/pluto-pinhelper.sh;!' /etc/bluetooth/hcid.conf
-/etc/init.d/bluez-utils restart
+/etc/init.d/bluetooth restart || /bin/true
