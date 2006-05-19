0f637eff190c78d7faf4e2282fac98521439a367
radu <radu.cx+linuxmce@gmail.com>
2006-05-19 08:25:24 +0000

    updated screens; added mnemonics for wizard steps

--- src/AVWizard/scripts/AVWizardWrapper.sh
+++ src/AVWizard/scripts/AVWizardWrapper.sh
@@ -2,6 +2,15 @@
 
 . /usr/pluto/bin/AVWizard-Common.sh
 
+STEP_Start=1
+STEP_DisplayRatio=2
+STEP_VideoConnector=3
+STEP_ScreenResolution=4
+STEP_AudioVolume=5
+STEP_AudioAC3=6
+STEP_AudioDTS=7
+STEP_Final=8
+
 Done=0
 NextStep=0
 export DISPLAY=:0
@@ -17,8 +26,8 @@ while [[ "$Done" -eq 0 ]]; do
 	VideoRefresh=$(WizGet VideoRefresh)
 
 	case "$WizStep" in
-		1) NextStep=2 ;;
-		3) NextStep=3 ;;
+		$STEP_DisplayRatio) NextStep=$STEP_VideoConnector ;;
+		$STEP_ScreenResolution) NextStep=$STEP_ScreenResolution ;;
 		*) echo "Interrupted in step '$WizStep', but shouldn't be" ;;
 	esac
 done
