c5c726919d19c3c6b9b04cac6122480eb5fce33e
aaron <aaron.b@plutohome.com>
2006-05-23 00:39:13 +0000

    debugging the new pnp plugin

--- src/pnp_detection_scripts/YamahaReceiver.sh
+++ src/pnp_detection_scripts/YamahaReceiver.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-echo "device $1"
-echo "queue $2"
-echo "path $3"
-echo "name $4"
-
-#failure
-#/usr/pluto/bin/MessageSend localhost -r 0 $1 806 224 $2 13 "$4" 18 "test error"
-
-#success
-#/usr/pluto/bin/MessageSend localhost -r 0 $1 806 224 $2 13 "$4" 44 1752
-
-#no match
-#/usr/pluto/bin/MessageSend localhost -r 0 $1 806 224 $2 13 "$4" 44 0
+echo "Yamaha Detection script"
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "\s1000m\11000\03\s1000m\11000\03" -i 1 -s "AF"
+if [[ "$?" -ne 0 ]]; then
+echo "It's not a Yamaha Receiver"
+/usr/pluto/bin/MessageSend localhost -r 0 $1 1 806 224 $2 13 "$4" 44 0
+else
+echo "It is a Yamaha Receiver"
+/usr/pluto/bin/MessageSend localhost -r 0 $1 1 806 224 $2 13 "$4" 44 1752
+fi
