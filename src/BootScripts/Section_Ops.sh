fc6c593d7a406e0cf8af05d8a78f36412cc35651
radu <radu.cx+linuxmce@gmail.com>
2006-05-25 14:56:45 +0000

    replaced spaces with tabs

--- src/BootScripts/Section_Ops.sh
+++ src/BootScripts/Section_Ops.sh
@@ -11,37 +11,37 @@
 
 ## Creates and/or populates a section from a file
 function PopulateSection {
-        local File=$1
-        local SectionName=$2
-        local SectionData=$3
+	local File=$1
+	local SectionName=$2
+	local SectionData=$3
 
-        ## Create the file if it doesn't exist
-        if [[ ! -f $File ]]; then
-                touch $File
-        fi
+	## Create the file if it doesn't exist
+	if [[ ! -f $File ]]; then
+		touch $File
+	fi
 
-        ## Check if our section exists in the file
-        grep -q "^## BEGIN : $SectionName" $File
-        if [[ "$?" != "0" ]]; then
-                echo -e "\n## BEGIN : $SectionName\n\n## END : $SectionName\n" >> $File
-        fi
+	## Check if our section exists in the file
+	grep -q "^## BEGIN : $SectionName" $File
+	if [[ "$?" != "0" ]]; then
+		echo -e "\n## BEGIN : $SectionName\n\n## END : $SectionName\n" >> $File
+	fi
 
-        ## Replace the section
-        rep="## BEGIN : $SectionName\n$SectionData\n## END : $SectionName"
-        awk -vrep="$rep" "/^## BEGIN : $SectionName/{print rep}/^## BEGIN : $SectionName/,/## END : $SectionName/{next}1" < $File > $File.$$
-        mv $File.$$ $File
+	## Replace the section
+	rep="## BEGIN : $SectionName\n$SectionData\n## END : $SectionName"
+	awk -vrep="$rep" "/^## BEGIN : $SectionName/{print rep}/^## BEGIN : $SectionName/,/## END : $SectionName/{next}1" < $File > $File.$$
+	mv $File.$$ $File
 
 }
 
 ## Clears and removes a section from a file
 function ClearSection {
-        local File=$1
-        local SectionName=$2
-        local SectionData=$3
+	local File=$1
+	local SectionName=$2
+	local SectionData=$3
 
-        if [[ -f $File ]]; then
-                PopulateSection "$File" "$SectionName" ""
-        fi
+	if [[ -f $File ]]; then
+		PopulateSection "$File" "$SectionName" ""
+	fi
 }
 
 ## Checkes if a section exists in a file
