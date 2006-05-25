bf463c7272ba7fe5a97a3c9cc283438d4cf0c46c
radu <radu.cx+linuxmce@gmail.com>
2006-05-25 15:04:09 +0000

    added quotes in places

--- src/BootScripts/Section_Ops.sh
+++ src/BootScripts/Section_Ops.sh
@@ -10,48 +10,50 @@
 
 
 ## Creates and/or populates a section from a file
-function PopulateSection {
-	local File=$1
-	local SectionName=$2
-	local SectionData=$3
+PopulateSection()
+{
+	local File="$1"
+	local SectionName="$2"
+	local SectionData="$3"
 
 	## Create the file if it doesn't exist
-	if [[ ! -f $File ]]; then
-		touch $File
+	if [[ ! -f "$File" ]]; then
+		touch "$File"
 	fi
 
 	## Check if our section exists in the file
-	grep -q "^## BEGIN : $SectionName" $File
+	grep -q "^## BEGIN : $SectionName" "$File"
 	if [[ "$?" != "0" ]]; then
-		echo -e "\n## BEGIN : $SectionName\n\n## END : $SectionName\n" >> $File
+		echo -e "\n## BEGIN : $SectionName\n\n## END : $SectionName\n" >> "$File"
 	fi
 
 	## Replace the section
 	rep="## BEGIN : $SectionName\n$SectionData\n## END : $SectionName"
-	awk -vrep="$rep" "/^## BEGIN : $SectionName/{print rep}/^## BEGIN : $SectionName/,/## END : $SectionName/{next}1" < $File > $File.$$
-	mv $File.$$ $File
-
+	awk -vrep="$rep" "/^## BEGIN : $SectionName/{print rep}/^## BEGIN : $SectionName/,/## END : $SectionName/{next}1" < "$File" > "$File".$$
+	mv "$File"{.$$,}
 }
 
 ## Clears and removes a section from a file
-function ClearSection {
-	local File=$1
-	local SectionName=$2
-	local SectionData=$3
+ClearSection()
+{
+	local File="$1"
+	local SectionName="$2"
+	local SectionData="$3"
 
-	if [[ -f $File ]]; then
+	if [[ -f "$File" ]]; then
 		PopulateSection "$File" "$SectionName" ""
 	fi
 }
 
 ## Checkes if a section exists in a file
-function CheckSectionExists {
-	local File=$1
-	local SectionName=$2
+CheckSectionExists()
+{
+	local File="$1"
+	local SectionName="$2"
 
-	ret=false	
-	if grep -q "^## BEGIN : $SectionName" ;then
-		if grep -q "^## END : $SectionName" ;then
+	ret=false
+	if grep -q "^## BEGIN : $SectionName" "$File"; then
+		if grep -q "^## END : $SectionName" "$File"; then
 			ret=true
 		fi
 	fi
