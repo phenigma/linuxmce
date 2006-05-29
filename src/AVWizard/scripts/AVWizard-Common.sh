e3ffca62e3eca5365916566a62849ae2edc57844
radu <radu.cx+linuxmce@gmail.com>
2006-05-29 14:16:30 +0000

    disable the 1080p resolution

--- src/AVWizard/scripts/AVWizard-Common.sh
+++ src/AVWizard/scripts/AVWizard-Common.sh
@@ -7,9 +7,30 @@ BaseDir=/usr/pluto/bin
 Wiz="$BaseDir"/AVWizard
 AudioMixerSettingsAwk="$BaseDir"/AudioMixerSettings.awk
 
-Resolutions_VESA=('640x480' '800x600' '1024x768' '1280x1024' '1600x1200')
-Resolutions_HDTV=('480p=848x480' '720p=1280x720' '1080i=1920x1080i' '1080p=1920x1080')
-Refresh_Rates=('50' '60' '65' '72' '75' '80' '85' '100' '120')
+Resolutions_VESA=(
+	'640x480'
+	'800x600'
+	'1024x768'
+	'1280x1024'
+	'1600x1200'
+)
+Resolutions_HDTV=(
+	'480p=848x480'
+	'720p=1280x720'
+	'1080i=1920x1080i'
+	#'1080p=1920x1080i'
+)
+Refresh_Rates=(
+	'50'
+	'60'
+	'65'
+	'72'
+	'75'
+	'80'
+	'85'
+	'100'
+	'120'
+)
 
 XF86Config="/etc/X11/xorg.conf.avwizard"
 XineConf="/etc/pluto/xine.conf.avwizard"
