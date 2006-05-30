a8690f84007f53885c72a8e121232524562f48c4
radul <radul>
2006-05-30 14:58:48 +0000

    fix

--- src/pnp_detection_scripts/DSCPanel.sh
+++ src/pnp_detection_scripts/DSCPanel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "DSCPanel Power5020 Detection script"
-/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "\30\30\30\39\30\0D\0A" -i 1 -s "\35\30\30\30\30\30\38\35\0D\0A"
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "\30\30\30\39\30\0D\0A" -i 1 -s "\35\30\30\30\30\30\32\35\0D\0A"
 if [[ "$?" -ne 0 ]]; then
 echo "It's not a DSCPanel Power5020"
 /usr/pluto/bin/MessageSend localhost -r 0 $1 1 806 224 $2 13 "$4" 44 0
