504ad4c46b1d796601fe4ba9183129f8f05278ab
remus <remus>
2006-06-02 10:16:57 +0000

    new defines for compiler warnings

--- src/utilities/defines/define_other.h
+++ src/utilities/defines/define_other.h
@@ -28,5 +28,21 @@
 // case CONST : return "CONST"
 #define CASE_const_ret_str(x) case x: return #x
 
+// there should be no more any compilers needing the "#else" version
+#if 1
+#    define UNUSED_ARG(X) /* X */
+#else
+#    define UNUSED_ARG(X) X
+#endif
+
+// use it for generated code to get rid of "unused variable" warning
+#ifdef __cplusplus
+#    ifdef __BORLANDC__
+#        define UNUSED_VAR(X) X
+#    else
+template <class T> inline void UNUSED_VAR(const T& UNUSED_ARG(t)) { }
+#    endif
+#endif
+
 #endif
 // _DEFINE_OTHER_H_
