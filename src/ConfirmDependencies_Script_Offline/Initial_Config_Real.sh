7110accf550c7b24319228c13e8402a72ec9d861
rgavril <rgavril>
2006-06-02 16:35:43 +0000

    Removing very old death unused stuff

--- src/ConfirmDependencies_Script_Offline/Initial_Config_Real.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Dummy file. Shouldn't be run at all"
--- src/ConfirmDependencies_Script_Offline/post-debootstrap.sh
+++ src/ConfirmDependencies_Script_Offline/post-debootstrap.sh
@@ -6,7 +6,7 @@ cp -f /cdrom/Pluto-Install/pkgsel /target/usr/lib/base-config/menu/
 # copy install scripts to /usr/pluto/install
 DIR=/target/usr/pluto/install
 mkdir -p "$DIR"
-SCRIPTS="ConfirmDependencies_Debian.sh Initial_Config.sh Initial_Config_Core.sh Initial_Config_MD.sh Initial_Config_Finish.sh Common.sh Download_Direct.sh Download_SVN.sh Download_CVS.sh"
+SCRIPTS="Initial_Config.sh Initial_Config_Core.sh Initial_Config_MD.sh Initial_Config_Finish.sh Common.sh Download_Direct.sh Download_SVN.sh Download_CVS.sh"
 for S in $SCRIPTS; do
 	cp -f /cdrom/Pluto-Install/"$S" "$DIR"
 done
@@ -18,9 +18,6 @@ if [ -d /cdrom/Debian-Cache ]; then
 	ln -s sarge /target/usr/pluto/deb-cache/dists/testing
 	ln -s sarge /target/usr/pluto/deb-cache/dists/stable
 fi
-
-mkdir -p /target/usr/pluto/install/deb-critical
-cp /cdrom/Pluto-Install/Critical-Deb/* /target/usr/pluto/install/deb-critical
 
 echo 'LANG="en_US.UTF8"
 LANGUAGE="en_US:en_GB:en"' >/target/etc/environment
