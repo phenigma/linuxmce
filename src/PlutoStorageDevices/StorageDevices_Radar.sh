94b13627dc4a848269ef5a270f6e94fb7f8c3f1d
rgavril <rgavril>
2006-06-06 12:45:48 +0000

    Use the pluto pnp daemon for detecting / blacklisting the partitions

--- src/PlutoStorageDevices/StorageDevices_Radar.sh
+++ src/PlutoStorageDevices/StorageDevices_Radar.sh
@@ -32,23 +32,6 @@ function Detect {
 	## Available partitions
 	availPart=$(cat /proc/partitions | awk '/(sd|hd).[0-9]/ { print $4 }')
 
-	## Remove Blacklisted partitions (ignored)
-	touch /usr/pluto/share/StorageDevices_Blacklist.txt
-	blackPart=$(cat /usr/pluto/share/StorageDevices_Blacklist.txt)
-	availPart=$(substractParts "$availPart" "$blackPart")
-
-	## Remove Already configured partitions
-	Q="SELECT PK_Device FROM Device WHERE FK_DeviceTemplate = $TPL_GENERIC_INTERNAL_DRIVE AND FK_Device_ControlledVia=$PK_Device"
-	InternalOwnStorageDevices=$(RunSQL "$Q")
-
-	confPart=""
-	for Device in $InternalOwnStorageDevices; do
-		Device_ID=$(Field 1 "$Device")
-		Device_BlockDevice=$(RunSQL "SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_Device=$Device_ID AND FK_DeviceData=$DD_BLOCK_DEVICE" | sed 's/\/dev\///g')
-		confParts="$confPart $Device_BlockDevice"
-	done
-	availPart=$(substractParts "$availPart" "$confPart")
-
 	## Remove Mounted partitions
 	mountedPart=$(mount | awk '/dev\/(sd|hd).[0-9]/ {print $1}' | sed 's/\/dev\///g')
 	availPart=$(substractParts "$availPart" "$mountedPart")
@@ -70,82 +53,14 @@ function Detect {
 	done
 	availPart=$auxPart
 
-	## Display the new internal disk drive found wizard on available orbiters
-	Q="
-		SELECT
-			PK_Device
-		FROM
-			Device
-			JOIN DeviceTemplate ON PK_DeviceTemplate = FK_DeviceTemplate
-			JOIN DeviceCategory ON PK_DeviceCategory = FK_DeviceCategory
-		WHERE
-			PK_DeviceCategory IN (5,2,3)
-	"
-	OrbiterList=$(RunSQL "$Q")
-
-	OrbiterIDList=""
-	for Orbiter in $OrbiterList; do
-		if [[ $OrbiterIDList != "" ]]; then
-			OrbiterIDList="$OrbiterIDList,"
-		fi
-
-		Orbiter_ID=$(Field 1 "$Orbiter")
-		OrbiterIDList="$OrbiterIDList""$Orbiter_ID"
-	done
-
 	## Test if we found any available partitions
 	if [[ $availPart != "" ]] ;then
 		
-                Q="
-                        SELECT
-                                Device.Description,
-                                Room.Description
-                        FROM
-                                Device
-                                JOIN Room ON PK_Room = FK_Room
-                        WHERE
-                                PK_Device = $PK_Device
-                        LIMIT 1
-                "
-                Info=$(RunSQL "$Q")
-                Comp_Description=$(Field 1 "$Info")
-                Comp_Room=$(Field 2 "$Info")
-
 		for partition in $availPart ;do
-			fdisk_Output=$( fdisk -l $(echo "/dev/$partition" | sed 's/[0-9]*$//') | grep ^/dev/$partition )
-			Partition_Type=$(echo $fdisk_Output  | cut -d' ' -f6-14)
-			Partition_Size=$(( `fdisk -s /dev/$partition`  / 1024 ))
-			
-			InfoMessage="I detected $partition, a $Partition_Size MB $Partition_Type partition in computer '$Comp_Description' from room '$Comp_Room'"
-			/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 741 159 228 109 "/dev/$partition" 156 $PK_Device 163 "$InfoMessage"
-			
-			#TODO: change this hack, is only here so the detection looks like is serialized
-			exit 0
+			#/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 741 159 228 109 "/dev/$partition" 156 $PK_Device 163 "$InfoMessage"
+			/usr/pluto/bin/MessageSend $DCERouter 0 -1001 2 65 55 "152|/dev/$partition" 52 8 49 1790			
 		done
 	fi
 }
 
-function Blacklist {
-	local BlockDevice=$1
-	echo ${BlockDevice##*/} >> /usr/pluto/share/StorageDevices_Blacklist.txt
-}
-
-Params=("$@")
-if [[ "$Params" == "" ]]; then
-	Params="-d"	
-fi
-
-for ((i = 0; i < ${#Params[@]}; i++)); do
-        case "${Params[$i]}" in
-		-d|--detect)
-			Detect
-			exit 0
-		;;
-                -b|--blacklist) 
-			((i++))
-			BlockDevice="${Params[$i]}"
-			Blacklist $BlockDevice
-			exit 0
-		;;
-        esac
-done
+Detect
