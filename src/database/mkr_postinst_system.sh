511f0727f10c3ba8e5742fbff5d6255cfe7de4df
aaron <aaron.b@plutohome.com>
2006-06-07 16:36:17 +0000

    optimize import of city

--- src/database/mkr_postinst_system.sh
+++ src/database/mkr_postinst_system.sh
@@ -8,6 +8,7 @@ echo Please be patient.......
 
 echo "drop table if exists CriteriaParmNesting_D; drop table if exists CriteriaParm_D; drop table if exists Criteria_D" | mysql pluto_main || /bin/true
 (cd /usr/pluto/database; /usr/pluto/bin/sqlCVS -n -D pluto_main -r constants,dce,designer,document,ir,website import) || exit $?
+mysql pluto_main < /usr/pluto/database/city.dump
 
 Q="GRANT ALL PRIVILEGES ON pluto_main.* TO 'root'@'127.0.0.1'; FLUSH PRIVILEGES;"
 echo "$Q" | /usr/bin/mysql
