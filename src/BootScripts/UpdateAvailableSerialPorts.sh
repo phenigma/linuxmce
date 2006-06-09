3dec5053a5b496d5b71b666594279e653816400e
radu <radu.cx+linuxmce@gmail.com>
2006-06-09 10:03:15 +0000

    Keep psc_mod field for DeviceData Available_Serial_Ports unchanged (closes #2273)

--- src/BootScripts/UpdateAvailableSerialPorts.sh
+++ src/BootScripts/UpdateAvailableSerialPorts.sh
@@ -14,7 +14,7 @@ Q="INSERT INTO Device_DeviceData(FK_Device, FK_DeviceData, IK_DeviceData)
 	VALUES ('$PK_Device', '$DEVICEDATA_Available_Serial_Ports', '${SerialPorts//\'/\'}')"
 RunSQL "$Q" || /bin/true
 
-Q="UPDATE Device_DeviceData SET IK_DeviceData='${SerialPorts//\'/\'}'
+Q="UPDATE Device_DeviceData SET IK_DeviceData='${SerialPorts//\'/\'}', psc_mod=psc_mod
 	WHERE FK_Device='$PK_Device' AND FK_DeviceData='$DEVICEDATA_Available_Serial_Ports'"
 RunSQL "$Q"
 Unlock "UpdateAvailableSerialPorts" "UASP"
