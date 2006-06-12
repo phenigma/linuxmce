a50978fc12b6a818102f6385a5b7bf241edab4c2
radu <radu.cx+linuxmce@gmail.com>
2006-06-12 16:11:43 +0000

    escape bash filename pattern special characters, except for '*' and '?', when searching for files; wrote this after a filename like 'bla bla bla [bla bla] bla' appeared, because the [...] sequence is a valid filename pattern in bash and gets interpreted as such

--- src/PlutoUtils/FileUtils.cpp
+++ src/PlutoUtils/FileUtils.cpp
@@ -670,12 +670,12 @@ bool FileUtils::FindFiles(list<string> &listFiles,string sDirectory,string sFile
             size_t pos = 0;
             while(true)
             {
-                string s = StringUtils::Tokenize(sFileSpec_CSV, string(","), pos);
+                string sShellPattern = StringUtils::BashPatternEscape(StringUtils::Tokenize(sFileSpec_CSV, string(","), pos), "*?");
 
 				/* somebody didn't test fnmatch well.  file: "The Patriot [Special Edition].dvd" doesn't match "The Patriot [Special Edition].*"
 				so I added the StringUtils::StartsWith */
-				if (sFileSpec_CSV.size()==0 || fnmatch(s.c_str(), entry.d_name, 0) == 0 || StringUtils::EndsWith(entry.d_name, s.c_str(),true)
-					|| (sFileSpec_CSV[sFileSpec_CSV.size()-1]=='*' && StringUtils::StartsWith(entry.d_name,sFileSpec_CSV.substr(0,sFileSpec_CSV.size()-1))) )
+				if (sFileSpec_CSV.size()==0 || fnmatch(sShellPattern.c_str(), entry.d_name, 0) == 0 || StringUtils::EndsWith(entry.d_name, sShellPattern.c_str(), true)
+					|| (sFileSpec_CSV[sFileSpec_CSV.size() - 1] == '*' && StringUtils::StartsWith(entry.d_name, sFileSpec_CSV.substr(0, sFileSpec_CSV.size() - 1))))
 				{
 					if( bFullyQualifiedPath )
 						listFiles.push_back(sDirectory + entry.d_name);
