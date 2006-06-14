a55efb978fda735aaf1e4c382b85a77f3dff0638
radu <radu.cx+linuxmce@gmail.com>
2006-06-14 14:02:13 +0000

    cleaned up

--- src/shift_state/shift_state.c
+++ src/shift_state/shift_state.c
@@ -1,13 +1,10 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
 //#include "shift_keys.h"
 
 int main()
 {
 	char c = 6;
-	errno = 0;
 	if (ioctl(0, TIOCLINUX, &c) == 0)
 		printf("%d\n", c);
 
