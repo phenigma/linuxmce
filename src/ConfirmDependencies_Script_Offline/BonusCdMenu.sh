48af0355d0c501a7fede618216c8cf1e21225819
voodoo <vali>
2006-06-22 13:41:06 +0000

    Separate script for bonus CD (as Radu requested)

---
+++ src/ConfirmDependencies_Script_Offline/BonusCdMenu.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+. /usr/pluto/install/Common.sh
+
+# XXX: Setup Bonus CD
+echo "The Pluto Bonus CD version 1 has some extras, such as a video setup"
+echo "wizard to help get you up and running. If you have the Pluto Bonus CD"
+echo "version 1, please insert it into your drive now and choose Y after it is in."
+echo "Otherwise, choose N."
+echo ""
+BonusCD=$(Ask "Did you insert the \"Pluto Bonus CD 1\" in drive ? [y/N]")
+if [[ "$BonusCD" == Y || "$BonusCD" == y ]]; then
+
+        if [ ! -d /cdrom ]; then
+                mkdir /cdrom
+        fi
+        /bin/mount /dev/cdrom /cdrom 2>/dev/null
+
+        while [ ! -d "/cdrom/bonuscd1" ]; do
+                echo "This in not a valid \"Pluto Bonus CD 1\". Please insert the correct CD and try again."
+                /usr/bin/eject
+                echo "Press any key when you inserted the correct CD in drive."
+                read key
+
+                if [[ ! -n "$key" ]]; then
+                        /bin/mount /dev/cdrom /cdrom 2>/dev/null
+                fi
+        done
+        echo "Installing extra packages from \"Pluto Bonus CD 1\""
+        echo "... PLEASE WAIT ..."
+
+        for files in $(ls /cdrom/bonuscd1); do
+                pkgname=$(echo $files | awk -F '_' '{print $1}')
+                echo "Installing package name : $files"
+                cd /cdrom/bonuscd1
+                dpkg -i $files 1>/dev/null 2>/dev/null
+        done
+
+        cd /cdrom/bonuscd1-cache
+        apt-get -y -f install dpkg-dev 2>/dev/null 1>/dev/null
+        WorkDir="/usr/pluto/deb-cache/dists/sarge/main/binary-i386"
+        cp -r *.deb $WorkDir
+        cd $WorkDir
+        echo "Regenerating Packages.gz in debcache. This will require 1 minute. Please wait."
+        dpkg-scanpackages . /dev/null | sed 's,\./,dists/sarge/main/binary-i386/,g' | gzip -9c > Packages.gz
+        clear
+        cd ..
+        /usr/bin/eject
+        echo ""
+        echo "\"Pluto Bonus CD 1\" succesfuly installed !"
+        echo ""
+else
+        echo "Skipping \"Pluto Bonus CD 1\" install ..."
+fi
+
