819878548c5f74bd4b138c39eaa34e393954e143
rgavril <rgavril>
2006-06-27 13:52:51 +0000

    A little cleanup in the postinst script, but i have a better ideea ...

--- src/BootScripts/mkr_postinst.sh
+++ src/BootScripts/mkr_postinst.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-update-rc.d -f Startup_Scripts.sh remove || /bin/true
-#update-rc.d Startup_Scripts.sh defaults 22 10
-
 . /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/SQL_Ops.sh
 
-# /etc/apt/apt.conf.d/30pluto
+## Update the PlutoVersion variable from pluto.conf
+Version=$(dpkg -s pluto-boot-scripts | grep '^Version:' | sed  's/Version: //')
+ConfSet "PlutoVersion" "$Version"
+
+## Generate the /etc/apt/apt.conf.d/30pluto file
 rm -rf /var/cache/polipo/*
 pluto_apt_conf='// Pluto apt conf add-on
-//Acquire::http::Proxy "http://'"$DCERouter"':8123";
-//Acquire::ftp::Proxy "ftp://'"$DCERouter"':8124";
-//Acquire::ftp::ProxyLogin { "USER $(SITE_USER)@$(SITE):$(SITE_PORT)"; "PASS $(SITE_PASS)"; };
-//Acquire::http::Proxy::dcerouter "DIRECT";
 Apt::Cache-Limit "12582912";
 Dpkg::Options { "--force-confold"; };
 Acquire::http::timeout "10";
 Acquire::ftp::timeout "10";
 '
-
 echo -n "$pluto_apt_conf" >/etc/apt/apt.conf.d/30pluto
-cp /usr/pluto/templates/logrotate.pluto.tmpl /etc/logrotate.d/pluto
 
-sed -i 's,www\.geocities\.com/medencid/,www.yttron.as.ro/,g' /etc/apt/sources.list
-sed -i '\,/usr/pluto/bin/Update_Packages\.sh, d' /etc/crontab
-
+## Ouch 
 chmod 777 /usr/pluto/locks
 
-Version=$(dpkg -s pluto-boot-scripts | grep '^Version:' | sed  's/Version: //')
-ConfSet "PlutoVersion" "$Version"
+## Copy our logrotate policy (every package should have his own policy but we are from pluto :)
+cp /usr/pluto/templates/logrotate.pluto.tmpl /etc/logrotate.d/pluto
+
+
+## Old hacks used to make the upgrade process smoother, will leave it here for some time 
+sed -i 's,www\.geocities\.com/medencid/,www.yttron.as.ro/,g' /etc/apt/sources.list
+sed -i '\,/usr/pluto/bin/Update_Packages\.sh, d' /etc/crontab
+update-rc.d -f Startup_Scripts.sh remove || /bin/true
+
