f0f19878ac72f57afd79cc8525d54407dc33c886
radu <radu.cx+linuxmce@gmail.com>
2006-06-27 13:18:22 +0000

    detect timezone script

--- src/BootScripts/Timezone_Detect.sh
+++ src/BootScripts/Timezone_Detect.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+. /usr/pluto/bin/Config_Ops.sh
 
+if [[ "$TimeZoneSet" == 1 ]]; then
+	exit # timezone has already been set once
+fi
+
+TimeZone=$(wget --timeout=10 -O - http://plutohome.com/geoip/detect_timezone.php)
+if [[ -z "$TimeZone" ]]; then
+	exit # timezone could not be detected
+fi
+
+/usr/pluto/bin/SetTimeZone.sh "$TimeZone"
+ConfSet TimeZoneSet 1
