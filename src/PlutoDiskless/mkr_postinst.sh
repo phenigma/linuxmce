26d8db1bc0975d2ee4d567cb1ec83fa002a0bb3f
rgavril <rgavril>
2006-06-28 09:38:22 +0000

    Fails when installling on clean system

--- src/PlutoDiskless/mkr_postinst.sh
+++ src/PlutoDiskless/mkr_postinst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ## This is safe to be runned at every install/upgrade
-/usr/pluto/bin/Diskless_Setup.sh --skiplock
-/usr/pluto/bin/DHCP_config.sh
-/usr/pluto/bin/Diskless_ExportsNFS.sh
+/usr/pluto/bin/Diskless_Setup.sh --skiplock || /bin/true
+/usr/pluto/bin/DHCP_config.sh || /bin/true
+/usr/pluto/bin/Diskless_ExportsNFS.sh || /bin/true
