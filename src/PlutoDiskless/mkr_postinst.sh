8f924dc0304675e2d43e6e9dd7d562603a3a6f7c
rgavril <rgavril>
2006-06-29 08:40:28 +0000

    Force the creation of the /usr/pluto/diskless as a symlink

--- src/PlutoDiskless/mkr_postinst.sh
+++ src/PlutoDiskless/mkr_postinst.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+## Create the /home/diskless and the /usr/pluto/diskless symlink
+if [[ ! -d /home/diskless ]] ;then
+	mkdir -p /home/diskless
+fi
+
+if [[ ! -L /usr/pluto/diskless && -d /usr/pluto/diskless ]]; then
+        mv /usr/pluto/diskless /home
+fi
+
+if [[ ! -L /usr/pluto/diskless ]] ;then
+	ln -s /home/diskless /usr/pluto/diskless
+fi
+
 ## This is safe to be runned at every install/upgrade
 /usr/pluto/bin/Diskless_Setup.sh --skiplock || /bin/true
 /usr/pluto/bin/DHCP_config.sh || /bin/true
