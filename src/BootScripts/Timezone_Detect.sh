018ed4544140c8dae019845e2f6f33b917e19c13
radu <radu.cx+linuxmce@gmail.com>
2006-06-29 18:19:29 +0000

    changed timezone URL

--- src/BootScripts/Timezone_Detect.sh
+++ src/BootScripts/Timezone_Detect.sh
@@ -6,7 +6,7 @@ if [[ "$TimeZoneSet" == 1 ]]; then
 	exit # timezone has already been set once
 fi
 
-TimeZone=$(wget --timeout=10 -O - http://plutohome.com/geoip/detect_timezone.php)
+TimeZone=$(wget --timeout=10 -O - http://timezone.plutohome.com/geoip/detect_timezone.php)
 if [[ -z "$TimeZone" ]]; then
 	exit # timezone could not be detected
 fi
