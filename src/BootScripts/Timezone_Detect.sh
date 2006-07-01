7bdd651108422999b844b206d67a3cd3c55645d3
kirill.b <kirill.b>
2006-07-01 19:10:23 +0000

    setting also location in Installation DB

--- src/BootScripts/Timezone_Detect.sh
+++ src/BootScripts/Timezone_Detect.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/SQL_Ops.sh
 
 if [[ "$TimeZoneSet" == 1 ]]; then
 	exit # timezone has already been set once
 fi
 
-TimeZone=$(wget --timeout=10 -O - http://timezone.plutohome.com/geoip/detect_timezone.php)
-if [[ -z "$TimeZone" ]]; then
-	exit # timezone could not be detected
+Info=$(wget --timeout=10 -O - http://timezone.plutohome.com/geoip/detect_timezone.php)
+if [[ -z "$Info" ]]; then
+	exit # timezone/location could not be detected
 fi
+
+TimeZone=$(echo "$Info" | cut -f1)
+
+if [[ -z "$TimeZone" ]]; then
+        exit # timezone/location could not be detected
+fi
+
+PK_City=$(echo "$Info" | cut -f2)
+City=$(echo "$Info" | cut -f3)
+Region=$(echo "$Info" | cut -f4)
+PK_Country=$(echo "$Info" | cut -f5)
+
+if [[ -z "$PK_City" ]]; then
+	exit # location could not be detected
+fi
+
+
+Query="UPDATE Installation SET FK_City=$PK_City, City=\"$City\", State=\"$Region\", FK_Country=$PK_Country"
+
+RunSQL "$Query"
 
 /usr/pluto/bin/SetTimeZone.sh "$TimeZone"
 ConfSet TimeZoneSet 1
