51a582cec032cede2fb931201e3729a6fbb66e36
radu <radu.cx+linuxmce@gmail.com>
2006-07-06 14:49:48 +0000

    moved TellToReload function to header

--- src/X-Resolution/Xres_config_end.sh
+++ src/X-Resolution/Xres_config_end.sh
@@ -3,13 +3,7 @@
 Answer="$1"
 
 . /usr/pluto/bin/Config_Ops.sh
-
-TellToReload()
-{
-	for Dev in $(cat /usr/pluto/locks/pluto_spawned_local_devices.txt); do
-		/usr/pluto/bin/MessageSend "$DCERouter" 0 "$Dev" 7 1
-	done
-}
+. /usr/pluto/bin/Utils.sh
 
 pidOfX="$(ps ax|grep 'X :1 -ac -config /etc/X11/xorg.conf.test'|grep -v grep|awk '{print $1}')"
 if [[ -z "$pidOfX" ]]; then
@@ -25,7 +19,7 @@ kill $pidOfX
 
 if [[ "${Answer:0:1}" == "y" || "${Answer:0:1}" == "Y" ]]; then
 	echo "X reload flag and message"
-	TellToReload
+	ReloadDevicesOnThisMachine
 	
 	pidOfX=$(ps ax|grep 'X :0 -ac -allowMouseOpenFail vt7'|egrep -v 'grep|SCREEN'|awk '{print $1}')
 	mv /etc/X11/xorg.conf{.test,}
