97728ba9c83ae5372ee052677260cabd6243064f
rgavril <rgavril>
2006-07-06 13:10:36 +0000

    Fix some posible errors when upgrading

--- src/PlutoUpgradeHelper/002-000-011-DisklessFsMovedToHome.sh
+++ src/PlutoUpgradeHelper/002-000-011-DisklessFsMovedToHome.sh
@@ -6,10 +6,11 @@ if [[ -f /etc/diskless.conf ]] ;then
 fi
 
 if [[ ! -L /usr/pluto/diskless && -d /usr/pluto/diskless ]]; then
+	rm -rf /home/diskless
         mv /usr/pluto/diskless /home
 else
         mkdir -p /home/diskless
 fi
-rm -f /usr/pluto/diskless
+rm -rf /usr/pluto/diskless
 ln -s /home/diskless /usr/pluto/diskless
 
