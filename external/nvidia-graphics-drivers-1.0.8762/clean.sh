9e8e3a2c81722bb2e96378f98d13725f51841961
radu <radu.cx+linuxmce@gmail.com>
2006-07-07 10:15:21 +0000

    updated clean.sh

--- external/nvidia-graphics-drivers-1.0.8762/clean.sh
+++ external/nvidia-graphics-drivers-1.0.8762/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2)
+Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2|cut -d. -f3|cut -d- -f1)
 for i in $(svn stat | grep '\?' | awk '{print $2}'); do
 	[[ "$i" = "NVIDIA-Linux-x86-1.0-${Version}-pkg0.run" ]] && continue
 	rm -rf "$i"
