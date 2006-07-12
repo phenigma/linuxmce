eab95cc5e36febb37cbc3d1b0243666264e5a0c1
radu <radu.cx+linuxmce@gmail.com>
2006-07-12 20:20:56 +0000

    install and delete AutoInst dir, not WorkDir (which is the cache!)

--- src/ConfirmDependencies_Script_Offline/BonusCdAutoInst.sh
+++ src/ConfirmDependencies_Script_Offline/BonusCdAutoInst.sh
@@ -2,6 +2,6 @@
 
 if [[ "$BonusCD" == Y || "$BonusCD" == y ]]; then
 	echo "Installing packages copied earlier from \"Pluto Bonus CD 1\""
-	dpkg -i "$BonusWorkDir"/*.deb
-	rm -rf "$BonusWorkDir"
+	dpkg -i "$BonusAutoInstDir"/*.deb
+	rm -rf "$BonusAutoInstDir"
 fi
