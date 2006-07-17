c47286677b91b61ed3ae0c1fe03bc2bafb5dbbfe
radu <radu.cx+linuxmce@gmail.com>
2006-07-17 16:12:01 +0000

    update DeviceData too

--- src/BootScripts/Spawn_Wrapper.sh
+++ src/BootScripts/Spawn_Wrapper.sh
@@ -4,8 +4,11 @@
 . /usr/pluto/bin/SQL_Ops.sh
 . /usr/pluto/bin/Utils.sh
 
+set -x
+
 DEVICEDATA_Audio_Settings=88
 DEVICECATEGORY_Media_Director=8
+DEVICEDATA_Output_Speaker_arrangement=71
 
 UpdateXineConf()
 {
@@ -48,9 +51,23 @@ UpdateXineConf()
 		XineConfSet audio.speaker_arrangement "Pass Through"
 		XineConfSet audio.alsa_front_device spdif
 		amixer sset 'IEC958 Playback Source' 'AC-Link' >/dev/null
+
+		Q="
+			UPDATE Device_DeviceData
+			SET IK_DeviceData='Pass Through'
+			WHERE FK_Device='$SpawnedDevice' AND FK_DeviceData='$DEVICEDATA_Output_Speaker_arrangement'
+		"
+		RunSQL "$Q"
 	else
 		XineConfSet audio.speaker_arrangement "Stereo 2.0"
 		XineConfSet audio.alsa_front_device default
+
+		Q="
+			UPDATE Device_DeviceData
+			SET IK_DeviceData='Stereo 2.0'
+			WHERE FK_Device='$SpawnedDevice' AND FK_DeviceData='$DEVICEDATA_Output_Speaker_arrangement'
+		"
+		RunSQL "$Q"
 	fi
 
 	if [[ "$AudioSetting" == *K* ]]; then
@@ -59,6 +76,12 @@ UpdateXineConf()
 		XineConfSet audio.synchronization.force_rate 0
 	fi
 }
+
+for ((i = 2; i <= "$#"; i++)); do
+	case "${!i}" in
+		-d) ((i++)); SpawnedDevice=${!i} ;;
+	esac
+done
 
 Spawnee="$(basename "$1")"
 case "$Spawnee" in
