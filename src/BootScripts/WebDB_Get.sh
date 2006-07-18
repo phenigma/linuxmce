521b328266b642caabb1bb1098350cd974756b65
radu <radu.cx+linuxmce@gmail.com>
2006-07-18 09:09:35 +0000

    delete only unchanged values

--- src/BootScripts/WebDB_Get.sh
+++ src/BootScripts/WebDB_Get.sh
@@ -27,7 +27,7 @@ PSC=$(head -2 "$OutputFile" | tail -1)
 PSC="${PSC#-- psc_id: }"
 
 if [[ -n "$Table" && -n "$PSC" ]]; then
-	Q="DELETE FROM $Table WHERE psc_id IN ($PSC)"
+	Q="DELETE FROM $Table WHERE psc_id IN ($PSC) AND psc_mod=0"
 	RunSQL "$Q"
 fi
 
