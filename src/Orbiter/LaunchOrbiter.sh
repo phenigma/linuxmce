88babb37dce0f33b09c32b81aa1ec4f81976b14e
radu <radu.cx+linuxmce@gmail.com>
2006-07-26 15:25:57 +0000

    alternative option: Orbiter vs OrbiterGL

--- src/Orbiter/LaunchOrbiter.sh
+++ src/Orbiter/LaunchOrbiter.sh
@@ -2,6 +2,14 @@
 
 export SDL_VIDEO_X11_WMCLASS="Orbiter"
 
+Executable=./Orbiter
+OrbiterConf=/etc/Orbiter.conf
+
+# TODO: update Config_Ops.sh to support user-specified files, and use that here
+if grep -q "UseOpenGL.*=,*1" "$OrbiterConf"; then
+	Executable=./OrbiterGL
+fi
+
 # test if XFree86 is running else manualy start it
 isX11Running=$(ps -A | grep Xorg | wc -l)
 if [[ "$isX11Running" != "1" ]]; then
@@ -10,5 +18,4 @@ if [[ "$isX11Running" != "1" ]]; then
 fi
 
 xset m 1 1
-exec ./Orbiter "$@"
-
+exec "$Executable" "$@"
