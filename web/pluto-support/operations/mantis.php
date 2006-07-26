1c14201bd56ba93464d5f32d94e476fb6be2d330
vali <vali>
2006-07-26 07:43:12 +0000

    Fixed mantis link when user is not logged.

--- web/pluto-support/operations/mantis.php
+++ web/pluto-support/operations/mantis.php
@@ -7,7 +7,7 @@ require('../include/config/config.inc.php');
 	$newParams=(strpos($oldParams,'&')!==false)?substr($oldParams,strpos($oldParams,'&')+1):$oldParams;
 
 	if(!isset($_SESSION['userID'])){
-		header("Location: ".$forumHost);
+		header("Location: http://plutohome.com/support/mantis/");
 		exit();
 	}else{
 		header("Location: http://plutohome.com/support/mantis/login.php?username=".$_SESSION['username']."&password=".$_SESSION['extPassword']."&login=1&return=main_page.php");
