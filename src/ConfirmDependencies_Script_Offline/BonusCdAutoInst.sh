f4db9e1079b917e48293d1107cbb7d231f7269df
voodoo <vali>
2006-07-27 15:34:57 +0000

    changed bonuscd autoinstall

--- src/ConfirmDependencies_Script_Offline/BonusCdAutoInst.sh
+++ src/ConfirmDependencies_Script_Offline/BonusCdAutoInst.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+BonusWorkDir="/usr/pluto/deb-cache/dists/sarge/main/binary-i386"
+
 if [[ "$BonusCD" != N && "$BonusCD" != n ]]; then
 	echo "Installing packages copied earlier from \"Pluto Bonus CD 1\""
-	dpkg -i "$BonusAutoInstDir"/*.deb
-	rm -rf "$BonusAutoInstDir"
+	echo "Processing video-wizard-videos package ..."
+	cd $BonusWorkDir
+        dpkg -i video-wizard-videos-*.deb 1>/dev/null 2>/dev/null
 fi
+
