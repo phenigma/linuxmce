9327240f8847ea2a71a6452eccae73a6106347eb
cmiron <cmiron>
2006-07-27 07:20:26 +0000

    Regen

--- src/pluto_main/Define_DataGrid.h
+++ src/pluto_main/Define_DataGrid.h
@@ -80,6 +80,7 @@
 #define DATAGRID_IR_Commands_CONST 78
 #define DATAGRID_Cities_CONST 79
 #define DATAGRID_Installable_Apps_CONST 80
+#define DATAGRID_Storage_Devices_CONST 82
 
 #define DATAGRID_TABLE "DataGrid"
 #define DATAGRID_PK_DATAGRID_FIELD "PK_DataGrid"
