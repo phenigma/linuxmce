520cc8ace670089ec9e62f8a3a32fc28312090ef
voodoo <vali>
2006-07-27 08:46:46 +0000

    changed bonuscd script for offline packages.gz regeneration to work

--- src/ConfirmDependencies_Script_Offline/BonusCdMenu.sh
+++ src/ConfirmDependencies_Script_Offline/BonusCdMenu.sh
@@ -41,12 +41,18 @@ if [[ "$BonusCD" != N && "$BonusCD" != n ]]; then
 	dpkg -i libselinux1_*.deb 1>/dev/null 2>/dev/null
 	dpkg -i binutils_*.deb 1>/dev/null 2>/dev/null
 	dpkg -i patch_*.deb 1>/dev/null 2>/dev/null
+	dpkg -i perl-modules*.deb 1>/dev/null 2>/dev/null
+	dpkg -i perl*.deb 1>/dev/null 2>/dev/null
 	dpkg -i dpkg_*.deb 1>/dev/null 2>/dev/null
 	dpkg -i dpkg-dev_*.deb 1>/dev/null 2>/dev/null
 
 	cd "$BonusWorkDir"
+	if [ -f /usr/bin/dpkg-scanpackages ]; then
 	echo "Regenerating Packages.gz in debcache. This will require 1 minute. Please wait."
 	dpkg-scanpackages . /dev/null | sed 's,\./,dists/sarge/main/binary-i386/,g' | gzip -9c > Packages.gz
+	else
+	echo "Command not found : dpkg-scanpackages. Packages.gz regeneration skipped."
+	fi
 	clear
 	cd ..
 	/usr/bin/eject
