90766060715a95db86d33cf6f6c2b074ae26b3e4
radu <radu.cx+linuxmce@gmail.com>
2006-07-28 12:31:30 +0000

    log bash script activity

--- src/Dhcpd-Plugin/Dhcpd-Plugin.sh
+++ src/Dhcpd-Plugin/Dhcpd-Plugin.sh
@@ -2,6 +2,6 @@
 
 date -R
 echo Launched the script
-/usr/bin/tail -F /var/log/syslog | grep --line-buffered 'dhcpd: ' | /usr/pluto/bin/Dhcpd-Plugin-Parser.sh
+/usr/bin/tail -F /var/log/syslog | grep --line-buffered 'dhcpd: ' | bash -x /usr/pluto/bin/Dhcpd-Plugin-Parser.sh
 
 echo Exiting
