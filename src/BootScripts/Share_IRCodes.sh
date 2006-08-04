72da1f7e16b91abe30c110ff2b007545d05f2416
kirill.b <kirill.b>
2006-08-04 11:30:15 +0000

    Fixed wrong command option

--- src/BootScripts/Share_IRCodes.sh
+++ src/BootScripts/Share_IRCodes.sh
@@ -10,6 +10,6 @@ fi
 timestamp=`date +%Y%m%d%H%M%S`
 
 # Check in dce,ir first since that may change the primary key in InfraredGroup_Command_Preferred
-/usr/pluto/bin/sqlCVS -H sqlcvs.plutohome.com -h localhost -a -n -r dce,ir -d "IR_${timestamp}_$PK_Installation" -U "$PK_Users~nopass" -e checkin >>/var/log/pluto/Share_IR.log
-/usr/pluto/bin/sqlCVS -H sqlcvs.plutohome.com -h localhost -a -n -r local -t InfraredGroup_Command_Preferred -d "IR_${timestamp}_$PK_Installation" -U "$PK_Users~nopass" -e checkin >>/var/log/pluto/Share_IR.log
+/usr/pluto/bin/sqlCVS -H sqlcvs.plutohome.com -h localhost -a -n -r dce,ir -c "IR_${timestamp}_$PK_Installation" -U "$PK_Users~nopass" -e checkin >>/var/log/pluto/Share_IR.log
+/usr/pluto/bin/sqlCVS -H sqlcvs.plutohome.com -h localhost -a -n -r local -t InfraredGroup_Command_Preferred -c "IR_${timestamp}_$PK_Installation" -U "$PK_Users~nopass" -e checkin >>/var/log/pluto/Share_IR.log
 
