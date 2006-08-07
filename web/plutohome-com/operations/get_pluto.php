6072cfc900dbdaa00e817d5e6b006739c88f4736
vali <vali>
2006-08-07 15:29:30 +0000

    Added second CD.

--- web/plutohome-com/operations/get_pluto.php
+++ web/plutohome-com/operations/get_pluto.php
@@ -1,6 +1,6 @@
 <?
 function get_pluto($output,$conn){
-	global $PlutoSupportHost,$PlutoHomeHost;
+	global $PlutoSupportHost,$PlutoHomeHost,$wikiHost;
 	
 	$out='
 <table width="100%"  border="0" cellspacing="0" cellpadding="0">
@@ -26,29 +26,37 @@ function get_pluto($output,$conn){
 
  
 
-1.  Download the latest kick-start CD:<br><br>
+1.  Download the latest kick-start CDs:<br><br>
+	<B>CD #1:</B><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;Version ****KICK_START_NAME_1****<br>
 
-	&nbsp;&nbsp;&nbsp;&nbsp;Version ----KICK_START_NAME----<br>
+    &nbsp;&nbsp;&nbsp;&nbsp;MD5: ****MD5SUM_1****<br>
 
-    &nbsp;&nbsp;&nbsp;&nbsp;MD5: ----MD5SUM----<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<a href="****HTTP_URL_CD_1****">HTTP download</a><br>
 
-	&nbsp;&nbsp;&nbsp;&nbsp;<a href="'.$PlutoHomeHost.'----KICK_START_NAME----">HTTP download</a><br>
+	&nbsp;&nbsp;&nbsp;&nbsp;<a href="****FTP_URL_CD_1****">FTP download</a><br><br>
 
-	&nbsp;&nbsp;&nbsp;&nbsp;<a href="ftp://ftp.plutohome.com/pub/">FTP download: ----KICK_START_NAME----</a><br><br>
+
+	<B>CD #2:</B> Mandatory if your Pluto Core doesn\'t have broadband.  If you do, it is optional, but still highly recommended.  It contains sample a/v files that make setup much easier.<br>
+	&nbsp;&nbsp;&nbsp;&nbsp;Version ****KICK_START_NAME_2****<br>
+
+    &nbsp;&nbsp;&nbsp;&nbsp;MD5: ****MD5SUM_2****<br>
+
+	&nbsp;&nbsp;&nbsp;&nbsp;<a href="****HTTP_URL_CD_2****">HTTP download</a><br>
+
+	&nbsp;&nbsp;&nbsp;&nbsp;<a href="****FTP_URL_CD_2****">FTP download</a><br><br>
 
  
 
-   2. Burn it using any CD burning software that can burn an .ISO image.  Virtually all cd burning software does this.<br><br>
+   2. Burn them using any CD burning software that can burn an .ISO image.  Virtually all cd burning software does this.<br><br>
 
  
 
-   3. Read the <a href="'.$PlutoSupportHost.'index.php?section=document&docID=212">Quick Start Guide</a>.  Part 3 explains how to boot off the kick-start cd to make your PC a Pluto Core or hybrid.  Note that by default the CD will erase the contents of the hard drive.<br><br>
+   3. Read the <a href="'.$wikiHost.'/index.php/Installation">Quick Start Guide</a>.  Part 3 explains how to boot off the kick-start cd to make your PC a Pluto Core or hybrid.  Note that by default the CD will erase the contents of the hard drive.<br><br>
 
-Get source code using <a href="http://subversion.tigris.org/">SVN</a> like this:<br>
-svn co http://svn.plutohome.com/pluto/trunk/<br><br>
+ 
 
-
-If you really know what you’re doing, click the ‘Download’ link on our <a href="'.$PlutoSupportHost.'index.php?section=mainDownload&package=0">support site</a> to download individual modules.  Note there are hundreds of modules, and it may be quite difficult to get it to work this way.</td>
+If you really know what you\'re doing, click the \'Download\' link on our <a href="'.$PlutoSupportHost.'index.php?section=mainDownload&package=0">support site</a> to download individual modules.  Note there are hundreds of modules, and it may be quite difficult to get it to work this way.  Or get the source code from anonymous svn like this: svn co http://svn.plutohome.com/pluto/trunk/</td>
 		</tr>
 		<tr>
 			<td>&nbsp;</td>
