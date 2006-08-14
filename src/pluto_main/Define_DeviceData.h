15d30829c7536b68bc9706215174ab2f7109f929
kirill.b <kirill.b>
2006-08-14 21:46:49 +0000

    Manual addition

--- src/pluto_main/Define_DeviceData.h
+++ src/pluto_main/Define_DeviceData.h
@@ -146,6 +146,7 @@
 #define DEVICEDATA_Offset_CONST 167
 #define DEVICEDATA_Hardware_acceleration_CONST 168
 #define DEVICEDATA_Use_alpha_blended_UI_CONST 169
+#define DEVICEDATA_Use_Deinterlacing_CONST 170
 
 #define DEVICEDATA_TABLE "DeviceData"
 #define DEVICEDATA_PK_DEVICEDATA_FIELD "PK_DeviceData"
