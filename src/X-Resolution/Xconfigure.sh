a8a7c67a9e1f8b733800d91553f5eb85d7f0b702
radu <radu.cx+linuxmce@gmail.com>
2006-08-15 18:49:43 +0000

    Added hardcoded^W predefined HDTV modelines as requested; if a bit of luck, nothing else will break because of these

--- src/X-Resolution/Xconfigure.sh
+++ src/X-Resolution/Xconfigure.sh
@@ -9,6 +9,23 @@ resHD720=1280x720
 resHD1080=1920x1080
 resHD576=720x576 # SDTV actually, EDTV with doublescan, but nVidia calls it HD so...
 
+# Hardcoded^W Predefined modelines, found on the web: http://forums.entechtaiwan.net/viewtopic.php?t=3730
+# Things to notice:
+# - the dot clock value is fixed, regardless of refresh, and the other timings are elongated to compensate
+# - none of the modeline generation utilities out there are able to generate these for me
+# - no modeline utility is able to fix both the refresh rate and the dot clock and adjust the timings to compensate
+# - these don't seem to comply to VESA GTF in any way
+
+Modeline_1280x720_50='"1280x720" 74.250 1280 1720 1760 1980 720 741 746 750 +hsync +vsync'
+Modeline_1920x540_50='"1920x540" 74.250 1920 2448 2492 2640 540 574 579 562 +hsync +vsync'
+Modeline_720x576_50='"720x576" 27.000 720 732 796 864 576 581 586 625 -hsync -vsync'
+Modeline_1280x720_60='"1280x720" 74.250 1280 1390 1430 1650 720 725 730 750 +hsync +vsync'
+Modeline_1920x540_60='"1920x540" 74.250 1920 2008 2052 2200 540 542 547 562 +hsync +vsync'
+Modeline_720x480_60='"720x480" 27.000 720 736 798 858 480 489 495 525 -hsync -vsync'
+Modeline_720x576_60='"720x576" 27.000 720 732 796 864 576 581 586 625 -hsync -vsync'
+Modeline_1920x1080i_50='"1920x1080" 74.250 1920 2448 2492 2640 1080 1148 1158 1124 interlace +hsync +vsync'
+Modeline_1920x1080i_60='"1920x1080" 74.250 1920 2008 2052 2200 1080 1084 1094 1124 interlace +hsync +vsync'
+
 TestConfig()
 {
 	X :2 -probeonly -config "$ConfigFile" &>/dev/null
@@ -17,10 +34,25 @@ TestConfig()
 
 GenModeline()
 {
-	local ResX="$1" ResY="$2" Refresh="$3" ScanType="$4"
+	local ResX="$1" ResY="$2" Refresh="${3:-60}" ScanType="$4"
 	local Modeline
-	Modeline=$(/usr/pluto/bin/xtiming.pl "$ResX" "$ResY" "$Refresh" "$ScanType")
-	Modeline="${Modeline/@*\"/\"}"
+	local VarName
+
+	VarName="Modeline_${ResX}x${ResY}"
+	if [[ "$ScanType" == interlace ]]; then
+		VarName="${VarName}i"
+	fi
+	VarName="${VarName}_${Refresh}"
+
+	if [[ -n "${!VarName}" ]]; then
+		# use a hardcoded^W predefined modline if exists
+		Modeline="${!VarName}"
+	else
+		# generate a modeline otherwise
+		Modeline=$(/usr/pluto/bin/xtiming.pl "$ResX" "$ResY" "$Refresh" "$ScanType")
+		Modeline="${Modeline/@*\"/\"}"
+	fi
+
 	echo "$Modeline"
 }
 
