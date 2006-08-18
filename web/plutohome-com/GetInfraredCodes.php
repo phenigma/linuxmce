395336cc27ea249a12bb1150a047a1857133466c
vali <vali>
2006-08-18 11:39:43 +0000

    Changed page to use main_sqlcvs instead of pluto_main.

--- web/plutohome-com/GetInfraredCodes.php
+++ web/plutohome-com/GetInfraredCodes.php
@@ -2,36 +2,38 @@
 require('include/config/config.inc.php');
 require('include/template.class.inc.php');
 require('include/utils.inc.php');
-
-/* @var $dbADO ADOConnection */
+/* @var $mainSqlCVSADO ADOConnection */
 /* @var $res ADORecordSet */ 
+$dsn = $dbPlutoMainType.'://'.urlencode($dbPlutoMainUser).':'.urlencode($dbPlutoMainPass).'@'.$dbPlutoMainServer.'/'.urlencode('main_sqlcvs'); 
+$mainSqlCVSADO = &ADONewConnection($dsn);
+
 $out='ERROR: no parameters specified.';
 $GLOBALS['RestrictedCommandsArray']=array(194=>'Toggle power',192=>'On',193=>'Off',205=>'1',89=>'Vol Up',90=>'Vol down',63=>'Skip Fwd',64=>'Skip Back');
 
 // e.g. GetInfraredCodes.php?PK_InfraredGroup=333&PK_DeviceTemplate=222
 if(isset($_GET['PK_InfraredGroup'])){
-	$out=getCodesByTemplate((int)$_GET['PK_InfraredGroup'],(int)@$_GET['PK_DeviceTemplate'],$dbADO);
+	$out=getCodesByTemplate((int)$_GET['PK_InfraredGroup'],(int)@$_GET['PK_DeviceTemplate'],$mainSqlCVSADO);
 }
 
 // Create=1, return schema
 if(isset($_GET['Create'])){
-	$out=getInfraredGroupCommandCreate($dbADO);
+	$out=getInfraredGroupCommandCreate($mainSqlCVSADO);
 }
 
 // PK_Manufacturer=8&PK_DeviceCategory=10
 // retrieve all codes with the same manufacturer and device category	
 if(isset($_GET['PK_Manufacturer'])){
-	$out=getCodesByManufacturer((int)$_GET['PK_Manufacturer'],(int)@$_GET['PK_DeviceCategory'],$dbADO);
+	$out=getCodesByManufacturer((int)$_GET['PK_Manufacturer'],(int)@$_GET['PK_DeviceCategory'],$mainSqlCVSADO);
 }
 
 if(isset($_GET['psc_ids'])){
-	$out=getCodesByIDs($_GET['psc_ids'],$dbADO);
+	$out=getCodesByIDs($_GET['psc_ids'],$mainSqlCVSADO);
 }
 
 print "-- Database import\n".$out."\n-- EOF\n";
 
 
-function getCodesByTemplate($PK_InfraredGroup,$PK_DeviceTemplate,$dbADO){
+function getCodesByTemplate($PK_InfraredGroup,$PK_DeviceTemplate,$mainSqlCVSADO){
 	global $dbPlutoMainDatabase,$dbPlutoMainUser,$dbPlutoMainPass;
 	$restricted=(int)@$_REQUEST['Restricted'];
 	
@@ -51,7 +53,7 @@ function getCodesByTemplate($PK_InfraredGroup,$PK_DeviceTemplate,$dbADO){
 	$filter='WHERE 1=1 '.((count($andConditions)>0)?'AND '.join(' AND ',$andConditions):'').((count($orConditions)>0)?' AND ('.join(' OR ',$orConditions).')':'');
 	
 
-	$res=$dbADO->Execute('SELECT InfraredGroup_Command.psc_id FROM InfraredGroup_Command '.@$filter);
+	$res=$mainSqlCVSADO->Execute('SELECT InfraredGroup_Command.psc_id FROM InfraredGroup_Command '.@$filter);
 
 	$codes=array();
 	while($row=$res->FetchRow()){
@@ -73,7 +75,7 @@ function getCodesByTemplate($PK_InfraredGroup,$PK_DeviceTemplate,$dbADO){
 	return $out;
 }
 
-function getInfraredGroupCommandCreate($dbADO){
+function getInfraredGroupCommandCreate($mainSqlCVSADO){
 	global $dbPlutoMainDatabase,$dbPlutoMainUser,$dbPlutoMainPass;
 	
 	$dumpCmd='mysqldump '.$dbPlutoMainDatabase.' -u'.$dbPlutoMainUser.' -p'.$dbPlutoMainPass.' InfraredGroup_Command --no-data --skip-comments';
@@ -90,7 +92,7 @@ function getInfraredGroupCommandCreate($dbADO){
 	return "-- psc_id: \n".$out;
 }
 
-function getCodesByManufacturer($PK_Manufacturer,$PK_DeviceCategory,$dbADO){
+function getCodesByManufacturer($PK_Manufacturer,$PK_DeviceCategory,$mainSqlCVSADO){
 	global $dbPlutoMainDatabase,$dbPlutoMainUser,$dbPlutoMainPass;
 	$restricted=(int)@$_REQUEST['Restricted'];
 	
@@ -113,7 +115,7 @@ function getCodesByManufacturer($PK_Manufacturer,$PK_DeviceCategory,$dbADO){
 	}
 	$filter='WHERE 1=1 '.((count($andConditions)>0)?'AND '.join(' AND ',$andConditions):'').((count($orConditions)>0)?' AND ('.join(' OR ',$orConditions).')':'');
 	
-	$res=$dbADO->Execute('SELECT InfraredGroup_Command.psc_id FROM InfraredGroup_Command INNER JOIN InfraredGroup ON FK_InfraredGroup=PK_InfraredGroup '.@$filter);
+	$res=$mainSqlCVSADO->Execute('SELECT InfraredGroup_Command.psc_id FROM InfraredGroup_Command INNER JOIN InfraredGroup ON FK_InfraredGroup=PK_InfraredGroup '.@$filter);
 
 	$codes=array();
 	while($row=$res->FetchRow()){
@@ -135,7 +137,7 @@ function getCodesByManufacturer($PK_Manufacturer,$PK_DeviceCategory,$dbADO){
 	return $out;
 }
 
-function getCodesByIDs($IDs,$dbADO){
+function getCodesByIDs($IDs,$mainSqlCVSADO){
 	global $dbPlutoMainDatabase,$dbPlutoMainUser,$dbPlutoMainPass;
 	
 	if($IDs==''){
@@ -144,7 +146,7 @@ function getCodesByIDs($IDs,$dbADO){
 	$arrayIDs=explode(',',$IDs);
 	
 	$filter='WHERE InfraredGroup_Command.psc_id IN ('.join(',',$arrayIDs).')';
-	$res=$dbADO->Execute('SELECT InfraredGroup_Command.psc_id FROM InfraredGroup_Command INNER JOIN InfraredGroup ON FK_InfraredGroup=PK_InfraredGroup '.@$filter);
+	$res=$mainSqlCVSADO->Execute('SELECT InfraredGroup_Command.psc_id FROM InfraredGroup_Command INNER JOIN InfraredGroup ON FK_InfraredGroup=PK_InfraredGroup '.@$filter);
 
 	$codes=array();
 	while($row=$res->FetchRow()){
