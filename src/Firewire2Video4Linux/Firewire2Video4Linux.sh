f67a5094dae8b48460b7a5199acdead2ee97e6f2
rgavril <rgavril>
2006-08-23 08:45:55 +0000

    * New package so it can install when needed and add dependencies

---
+++ src/Firewire2Video4Linux/Firewire2Video4Linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is creating some v4l pipes that are to be used by
+# Motion_Wrapper for pipeing video1394 content into them.
+
+PipeDevOffset=30
+PipeCount=16
+
+modprobe video1394
+modprobe raw1394
+modprobe vloopback pipes=$PipeCount dev_offset=$PipeDevOffset
+
---
+++ src/Firewire2Video4Linux/mkr_postinst.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/pluto/bin/Firewire2Video4Linux.sh
+
+
