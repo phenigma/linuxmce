c99d904f3469a75bce9030efa191324eb9ca5c69
vali <vali>
2006-08-25 12:19:58 +0000

    Added backup child devices to svn.

---
+++ web/pluto-admin/backupChildDevices.php
@@ -0,0 +1,25 @@
+<?php
+session_name('Pluto-admin');
+session_start();
+
+require('include/config/config.inc.php');
+require('include/utils.inc.php');
+
+$deviceID=(int)@$_REQUEST['deviceID'];
+if($deviceID==0){
+	die('ERROR: Device ID not specified.');
+}
+
+$tmpfile='device_'.$deviceID.'.txt';
+$cmd='sudo -u root /usr/pluto/bin/CreateDevice -h dcerouter -C '.$deviceID.' -e /tmp/'.$tmpfile;
+$response=exec_batch_command($cmd,1);
+
+if(file_exists('/tmp/'.$tmpfile)){
+	header('Content-type: application/force-download');
+	header('Content-Transfer-Encoding: Binary');
+	header('Content-disposition: attachment; filename='.$tmpfile);
+	readfile('/tmp/'.$tmpfile);
+}else{
+	die('ERROR: Temporary file not generated: <hr>'.nl2br($response));
+}
+?>
