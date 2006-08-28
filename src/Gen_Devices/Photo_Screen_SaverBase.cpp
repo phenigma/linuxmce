f86cc9841eb3f25def3d02bd011d5b142d646ee7
cmiron <cmiron>
2006-08-28 10:39:27 +0000

    Base class for Photo_Screen_Saver

---
+++ src/Gen_Devices/Photo_Screen_SaverBase.cpp
@@ -0,0 +1,37 @@
+#include "Photo_Screen_SaverBase.h"
+#include "DeviceData_Impl.h"
+#include "Logger.h"
+
+using namespace DCE;
+#include "Photo_Screen_SaverBase.h"
+extern Photo_Screen_Saver_Command *Create_Photo_Screen_Saver(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+DeviceData_Impl *Photo_Screen_Saver_Data::CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition)
+{
+	// Peek ahead in the stream.  We're going to pass in the above pointers anyway so it won't affect the position
+	SerializeClass b;
+	b.ResumeReadWrite(pDataBlock,AllocatedSize,CurrentPosition);
+	int iPK_Device = b.Read_unsigned_long();
+	int iPK_Installation = b.Read_unsigned_long();
+	int iPK_DeviceTemplate = b.Read_unsigned_long();
+	switch(iPK_DeviceTemplate) {
+		case 1821:
+			return new Photo_Screen_Saver_Data();
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateData for unknown type %d.", iPK_DeviceTemplate);
+	return NULL;
+}
+
+Event_Impl *Photo_Screen_Saver_Event::CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice )
+{
+	switch(dwPK_DeviceTemplate) {
+		case 1821:
+			return (Event_Impl *) new Photo_Screen_Saver_Event(pOCClientSocket, dwDevice);
+	};
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateEvent for unknown type %d.", dwPK_DeviceTemplate);
+	return NULL;
+}
+Command_Impl  *Photo_Screen_Saver_Command::CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent)
+{
+	g_pPlutoLogger->Write(LV_STATUS, "Got CreateCommand for unknown type %d.", PK_DeviceTemplate);
+	return NULL;
+}
