3911d061ed5b2ab84d2e030be4cc93b0df34d9eb
radu <radu.cx+linuxmce@gmail.com>
2006-08-28 13:25:43 +0000

    SetupSound

--- src/BootScripts/SetupSound.sh
+++ src/BootScripts/SetupSound.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/Utils.sh
+
+DEVICECATEGORY_Sound_Cards=124
+
+SoundCard=$(FindDevice_Category "$PK_Device" "$DEVICECATEGORY_Sound_Cards")
+
+if [[ -z "$SoundCard" ]]; then
+	# no sound card device found; nothing to do
+	exit 0
+fi
+
+Q="
+	SELECT CommandLine
+	FROM DeviceTemplate
+	JOIN Device ON FK_DeviceTemplate=PK_DeviceTemplate AND PK_Device='$SoundCard'
+"
+CommandLine="$(RunSQL "$Q")"
+/usr/pluto/bin/"$CommandLine"
