0ed32da38bebef0126e1f2409d59fe154fa15cb7
ciprian.m <ciprian.m>
2006-08-30 10:55:13 +0000

    Improoved file browser for Photo screen saver plugin

--- src/Photo_Screen_Saver/FileBrowser.cpp
+++ src/Photo_Screen_Saver/FileBrowser.cpp
@@ -1,22 +1,51 @@
 #include "FileBrowser.h"
 
+#include "../PlutoUtils/FileUtils.h"
+
 FileBrowser::FileBrowser(string SearchFolder)
 {
 	FirstFile = true;
+#ifdef WIN32
+	this->SearchFolder = "/usr/pluto/orbiter/skins/Basic/menu2";
+#endif
+	AddExtension("png");
+	AddExtension("jpg");
+	LastFile = "";
 }
 
 FileBrowser::~FileBrowser(void)
 {
 }
 
+void FileBrowser::AddExtension(string Extension)
+{
+	list<string> MatchFiles;
+	FileUtils::FindFiles(MatchFiles, SearchFolder, "*."+Extension, false, true);
+	for(list<string>::iterator FileDesc = MatchFiles.begin(), 
+		End = MatchFiles.end(); FileDesc != End; FileDesc++)
+	{
+		string FileName = *FileDesc;
+		FileList.push_back(FileName);
+	}
+
+
+}
+
 string FileBrowser::NextFile()
 {
 	string Result;
+	if(!FileList.size())
+		return Result;
 
-	FirstFile = !FirstFile;
-	if(FirstFile)
-		Result = "/IMG_0001.jpg";
-	else
-		Result = "/IMG_0004.jpg";
+	if(FileList.size() == 1)
+		return FileList[0];
+		
+	int Position = -1;
+	do {
+		Position = rand() % FileList.size();
+		Result = FileList[Position];
+	} while(Result == LastFile);
+	LastFile = Result;
+
 	return Result;
 }
