5e39a84fe2815053e11fe4daf8f6a07da105fe94
ciprian.m <ciprian.m>
2006-08-30 10:44:10 +0000

    Remove two unused files && remove again one warning (virtual destructor for virtual functions)

--- src/Photo_Screen_Saver/ZoomBase.h
+++ src/Photo_Screen_Saver/ZoomBase.h
@@ -14,7 +14,7 @@ protected:
 	int ScreenWidth, ScreenHeight, Width, Height;
 public:
 	ZoomBase(MeshFrame* PictureObject, int ScreenWidth, int ScreenHeight, int Width, int Height, int StartTime, int ZoomTime);
-	~ZoomBase(void);
+	virtual ~ZoomBase(void);
 
 	virtual bool Update(int Time) = 0;
 
