c45c77cc6adfa8b612e8f7f2991f1c18c2bbd1fc
radu <radu.cx+linuxmce@gmail.com>
2006-09-04 10:09:35 +0000

    changed path to /home so /home/user* is also included

--- src/BootScripts/UpdateMediaDaemon.sh
+++ src/BootScripts/UpdateMediaDaemon.sh
@@ -2,6 +2,6 @@
 
 counter=30
 while ((counter > 0)); do
-	nice -n 20 /usr/pluto/bin/UpdateMedia -h localhost -d /home/public/data -B
+	nice -n 20 /usr/pluto/bin/UpdateMedia -h localhost -d /home -B
 	((counter--))
 done
