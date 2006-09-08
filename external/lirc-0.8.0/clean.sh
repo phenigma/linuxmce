5167c140b2c2005c4e93909c42d6d936064c7063
radu <radu.cx+linuxmce@gmail.com>
2006-09-08 19:51:51 +0000

    better clean

--- external/lirc-0.8.0/clean.sh
+++ external/lirc-0.8.0/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2|cut -d. -f3|cut -d- -f1)
-for i in $(svn stat --no-ignore | grep '\?' | awk '{print $2}'); do
+for i in $(svn stat --no-ignore | egrep '^\?|^I' | awk '{print $2}'); do
 	rm -rf "$i"
 done
 
