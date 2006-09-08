4fb56e3cb73ed0401cf998426599c87be092c3f6
radu <radu.cx+linuxmce@gmail.com>
2006-09-08 20:45:32 +0000

    ported changes from lirc-0.8.0 dir

--- external/lirc/clean.sh
+++ external/lirc/clean.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2|cut -d. -f3|cut -d- -f1)
-for i in $(svn stat | grep '\?' | awk '{print $2}'); do
+for i in $(svn stat --no-ignore | egrep '^\?|^I' | awk '{print $2}'); do
 	rm -rf "$i"
 done
 
-for i in $(svn stat | awk '{print $2}'); do
-	svn revert "$i"
-done
+svn revert -R .
