397382d4749b62a58bfca4a273c0d223860778c7
radu <radu.cx+linuxmce@gmail.com>
2006-09-08 20:42:15 +0000

    build update

--- external/lirc-0.8.0/build.sh
+++ external/lirc-0.8.0/build.sh
@@ -6,11 +6,10 @@ Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2)
 
 dpkg-buildpackage -rfakeroot -b -us -uc
 
-dpkg -i ../lirc-modules-source_${Version}_all.deb || exit 1
-
 rm -rf modules
 tar -xzf debian/lirc-modules-source/usr/src/lirc-modules.tar.gz
 pushd modules/lirc/ &>/dev/null
+echo 'LIRC_MODULES="all"' >lirc-modules-source.conf
 KSRC=/lib/modules/${KVER}/build KVERS=${KVER} fakeroot debian/rules binary-modules
 KSRC=/lib/modules/${KVER}-smp/build KVERS=${KVER}-smp fakeroot debian/rules binary-modules
 popd &>/dev/null
