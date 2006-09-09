00e0d10f553ee3825f2994af51db5e2917920767
aaron <aaron.b@plutohome.com>
2006-09-09 19:41:15 +0000

    no gif for gallery

--- src/Orbiter_Plugin/Orbiter_Plugin.cpp
+++ src/Orbiter_Plugin/Orbiter_Plugin.cpp
@@ -2705,7 +2705,7 @@ void Orbiter_Plugin::CMD_Get_Screen_Saver_Files(int iPK_Device,string *sFilename
 		"FROM File "
 		"JOIN File_Attribute ON FK_File=PK_File "
 		"JOIN Attribute ON FK_Attribute=PK_Attribute AND FK_AttributeType=" TOSTRING(ATTRIBUTETYPE_Screen_Saver_For_MD_CONST) " "
-		"WHERE Name='' or Name='" + StringUtils::itos(iPK_Device) + "' AND Missing=0";
+		"WHERE Name='' or Name='" + StringUtils::itos(iPK_Device) + "' AND Missing=0 AND Filename like '%jpg'";
 
 	PlutoSqlResult result_set1,result_set2;
 
@@ -2734,7 +2734,7 @@ void Orbiter_Plugin::CMD_Get_Screen_Saver_Files(int iPK_Device,string *sFilename
 			"FROM File "
 			"JOIN File_Attribute ON FK_File=PK_File "
 			"JOIN Attribute ON FK_Attribute=PK_Attribute AND FK_AttributeType=" TOSTRING(ATTRIBUTETYPE_Keyword_CONST) " "
-			"WHERE Name IN (" + sKeywords + ") AND "
+			"WHERE Name IN (" + sKeywords + ") AND Filename like '%jpg' AND "
 			"PK_File IN (" + sPK_File + ");";
 
 		// Create a new result set based on the tags
