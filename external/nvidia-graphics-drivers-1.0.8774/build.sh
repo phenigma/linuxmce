4beb6c5e8341303b04b9998b63f44ad703bf50fc
radu <radu.cx+linuxmce@gmail.com>
2006-09-10 21:16:03 +0000

    clean modules before building, otherwise weird things will happen

--- external/nvidia-graphics-drivers-1.0.8774/build.sh
+++ external/nvidia-graphics-drivers-1.0.8774/build.sh
@@ -6,8 +6,8 @@ dpkg-buildpackage -rfakeroot -b -us -uc
 
 tar -xzf nvidia-kernel-source.tar.gz
 pushd modules/nvidia-kernel/ &>/dev/null
-KSRC=/lib/modules/${KVER}/build KVERS=${KVER} fakeroot debian/rules binary_modules
-KSRC=/lib/modules/${KVER}-smp/build KVERS=${KVER}-smp fakeroot debian/rules binary_modules
+KSRC=/lib/modules/${KVER}/build KVERS=${KVER} fakeroot debian/rules clean binary_modules
+KSRC=/lib/modules/${KVER}-smp/build KVERS=${KVER}-smp fakeroot debian/rules clean binary_modules
 popd &>/dev/null
 
 Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2)
