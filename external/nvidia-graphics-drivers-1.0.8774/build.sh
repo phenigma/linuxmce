f99d1450c18cd20a8c9b19f2f5ab44c20f2a8054
radu <radu.cx+linuxmce@gmail.com>
2006-09-10 17:58:50 +0000

    update

--- external/nvidia-graphics-drivers-1.0.8774/build.sh
+++ external/nvidia-graphics-drivers-1.0.8774/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 KVER=$(uname -r)
+KVER=${KVER%-smp}
 dpkg-buildpackage -rfakeroot -b -us -uc
 
 tar -xzf nvidia-kernel-source.tar.gz
 pushd modules/nvidia-kernel/ &>/dev/null
 KSRC=/lib/modules/${KVER}/build KVERS=${KVER} fakeroot debian/rules binary_modules
+KSRC=/lib/modules/${KVER}-smp/build KVERS=${KVER}-smp fakeroot debian/rules binary_modules
 popd &>/dev/null
 
 Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2)
-cp modules/nvidia-kernel-${KVER}_${Version}_i386.deb ..
+cp modules/nvidia-kernel-${KVER}{,-smp}_${Version}_i386.deb ..
