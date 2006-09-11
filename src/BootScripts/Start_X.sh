c496ab83a472f8ef999c2b0da764f534345a84a1
rgavril <rgavril>
2006-09-11 13:58:03 +0000

    Hack for xorg.conf bug

--- src/BootScripts/Start_X.sh
+++ src/BootScripts/Start_X.sh
@@ -2,6 +2,39 @@
 . /usr/pluto/bin/pluto.func
 . /usr/pluto/bin/Utils.sh
 
+function assureXorgSane() {
+
+	xorgLines=$(cat /etc/X11/xorg.conf | wc -l)
+	if [ $xorgLines -le 15 ] ;then
+		Logging "$TYPE" "$SEVERITY_NORMAL" "$0" "File xorg.conf has only $xorgLines lines, rebuilding"
+		EVICETEMPLATE_OnScreen_Orbiter=62
+		DEVICETEMPLATE_OrbiterPlugin=12
+		DEVICECATEGORY_Media_Director=8
+		DEVICEDATA_ScreenWidth=100
+		DEVICEDATA_ScreenHeight=101
+		DEVICEDATA_PK_Size=25
+		DEVICEDATA_Video_settings=89
+		DEVICEDATA_Spacing=150
+		DEVICEDATA_Offset=167
+
+		. /usr/pluto/bin/Utils.sh
+		. /usr/pluto/bin/SQL_Ops.sh
+
+		ComputerDev=$(FindDevice_Category "$PK_Device" "$DEVICECATEGORY_Media_Director" '' 'include-parent')
+
+		Q="SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_Device='$ComputerDev' AND FK_DeviceData='$DEVICEDATA_Video_settings' LIMIT 1"
+		VideoSetting=$(RunSQL "$Q")
+		VideoSetting=$(Field "1" "$VideoSetting")
+		
+		refresh=$(echo $VideoSetting | cut -d '/' -f2)
+		resolution=$(echo $VideoSetting | cut -d '/' -f1)
+		width=$(echo $resolution | cut -d' ' -f1)
+		height=$(echo $resolution | cut -d' ' -f2)
+
+		/usr/pluto/bin/Xconfigure.sh --defaults --resolution "${width}x${height}@${refresh}"
+	fi
+}
+
 XClient=/usr/pluto/bin/Start_IceWM.sh
 XClientParm=()
 XServerParm=(-logverbose 5)
@@ -25,6 +58,9 @@ VT=vt"$((7+VT))"
 
 /usr/pluto/bin/SoundCards_Setup.sh
 /usr/pluto/bin/CaptureCards_Setup.sh
+
+assureXorgSane
+assureXorgSane
 
 # Start X11
 if [[ "$Background" == y ]]; then
