110022a2b5e07dce88a8eaebbf88f75d32cd4e4d
radu <radu.cx+linuxmce@gmail.com>
2006-09-27 15:54:00 +0000

    build requested kernel modules

--- external/nvidia-graphics-drivers-1.0.8774/build.sh
+++ external/nvidia-graphics-drivers-1.0.8774/build.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-KVER=$(uname -r)
-KVER=${KVER%-smp}
-dpkg-buildpackage -rfakeroot -b -us -uc
+set -ex
+
+Flavor="$1"
+shift
+Kernels=("$@")
+
+if [[ "${#Kernels[*]}" -eq 0 ]]; then
+	exit 1
+fi
+
+DestDir="/home/samba/repositories/"$Flavor"/replacements/main/binary-i386/"
+
+KDEPS=
+for KVER in "${Kernels[@]}"; do
+	KDEPS="$KDEPS, linux-image-$KVER"
+done
+KDEPS="$KDEPS" dpkg-buildpackage -rfakeroot -b -us -uc
+
+Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2)
+cp ../nvidia-glx{,-dev}_${Version}_i386.deb "$DestDir"
 
 tar -xzf nvidia-kernel-source.tar.gz
 pushd modules/nvidia-kernel/ &>/dev/null
-KSRC=/lib/modules/${KVER}/build KVERS=${KVER} fakeroot debian/rules clean binary_modules
-KSRC=/lib/modules/${KVER}-smp/build KVERS=${KVER}-smp fakeroot debian/rules clean binary_modules
+for KVER in "${Kernels[@]}"; do
+	KSRC=/lib/modules/${KVER}/build KVERS=${KVER} fakeroot debian/rules clean binary_modules
+	cp ../nvidia-kernel-${KVER}_${Version}_i386.deb "$DestDir"
+done
 popd &>/dev/null
-
-Version=$(dpkg-parsechangelog -l./debian/changelog |grep ^Version|cut -d' ' -f2)
-cp modules/nvidia-kernel-${KVER}{,-smp}_${Version}_i386.deb ..
