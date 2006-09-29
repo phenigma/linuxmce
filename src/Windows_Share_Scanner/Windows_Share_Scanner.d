a5a87640cff00693845f609ee1e3fde3b4442606
djrobx <djrobx@hotmail.com>
2006-09-29 06:30:38 +0000

    windows share scanner prelim

---
+++ src/Windows_Share_Scanner/Windows_Share_Scanner.d
@@ -0,0 +1,15 @@
+Windows_Share_Scanner.o Windows_Share_Scanner.d : Windows_Share_Scanner.cpp \
+  Windows_Share_Scanner.h ../Gen_Devices/Windows_Share_ScannerBase.h \
+  ../DCE/DeviceData_Impl.h ../DCE/Event_Impl.h ../DCE/ClientSocket.h \
+  ../DCE/Socket.h ../PlutoUtils/MultiThreadIncludes.h \
+  ../PlutoUtils/CommonIncludes.h ../DCE/Message.h \
+  ../SerializeClass/SerializeClass.h ../PlutoUtils/FileUtils.h \
+  ../PlutoUtils/StringUtils.h ../PlutoUtils/Other.h ../PlutoUtils/getch.h \
+  ../PlutoUtils/MyStl.h ../DCE/DeviceData_Base.h \
+  ../pluto_main/Define_DeviceCategory.h ../DCE/Command_Impl.h \
+  ../DCE/HandleRequestSocket.h ../DCE/MessageBuffer.h ../DCE/Logger.h \
+  ../DCE/PlutoLockLogger.h ../pluto_main/Define_Command.h \
+  ../pluto_main/Define_CommandParameter.h \
+  ../pluto_main/Define_DeviceTemplate.h ../pluto_main/Define_Event.h \
+  ../pluto_main/Define_EventParameter.h ../pluto_main/Define_DeviceData.h \
+  ../Gen_Devices/AllCommandsRequests.h ../DCE/PreformedCommand.h
---
+++ src/Windows_Share_Scanner/Windows_Share_Scanner.h
@@ -0,0 +1,61 @@
+//<-dceag-d-b->
+#ifndef Windows_Share_Scanner_h
+#define Windows_Share_Scanner_h
+
+//	DCE Implemenation for #1815 Windows Share Scanner
+
+#include "Gen_Devices/Windows_Share_ScannerBase.h"
+//<-dceag-d-e->
+
+//<-dceag-decl-b->
+namespace DCE
+{
+	class Windows_Share_Scanner : public Windows_Share_Scanner_Command
+	{
+//<-dceag-decl-e->
+		// Private member variables
+
+		// Private methods
+public:
+		// Public member variables
+		bool m_bExit;
+//<-dceag-const-b->
+public:
+		// Constructors/Destructor
+		Windows_Share_Scanner(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL);
+		virtual ~Windows_Share_Scanner();
+		virtual bool GetConfig();
+		virtual bool Register();
+		virtual void ReceivedCommandForChild(DeviceData_Impl *pDeviceData_Impl,string &sCMD_Result,Message *pMessage);
+		virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage);
+//<-dceag-const-e->
+		virtual void ScanSharesThread();
+
+//<-dceag-const2-b->
+		// The following constructor is only used if this a class instance embedded within a DCE Device.  In that case, it won't create it's own connection to the router
+		// You can delete this whole section and put an ! after dceag-const2-b tag if you don't want this constructor.  Do the same in the implementation file
+		Windows_Share_Scanner(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+//<-dceag-const2-e->
+
+//<-dceag-h-b->
+	/*
+				AUTO-GENERATED SECTION
+				Do not change the declarations
+	*/
+
+	/*
+			*****DATA***** accessors inherited from base class
+
+			*****EVENT***** accessors inherited from base class
+	void EVENT_Device_Detected(string sMac_Address,string sText,string sIP_Address,int iPK_DeviceTemplate,string sVendorModelID,int iPK_CommMethod,int iPK_PnpProtocol,string sPNP_Serial_Number,string sDeviceData,string sCategory);
+
+			*****COMMANDS***** we need to implement
+	*/
+
+//<-dceag-h-e->
+	};
+
+//<-dceag-end-b->
+}
+#endif
+//<-dceag-end-e->
