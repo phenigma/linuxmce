9476a5f79ad942d94f40a56ace598340f9cc2c18
vali <vali>
2006-10-02 13:41:17 +0000

    Replaced nested queries to a single large query; loading time decreased 10 times.

--- web/pluto-admin/operations/dce/dceDeviceData.php
+++ web/pluto-admin/operations/dce/dceDeviceData.php
@@ -31,31 +31,44 @@ function dceDeviceData($output,$dbADO) {
 				<td><B>'.$TEXT_USED_BY_CONST.'</B></td>
 			</tr>';
 		$queryDD='
-			SELECT DeviceData.Description, ParameterType.Description AS Type, PK_DeviceData
+			SELECT DeviceData.Description, ParameterType.Description AS Type, PK_DeviceData,DeviceTemplate.Description AS Template,PK_DeviceTemplate,DeviceTemplate_DeviceData.Description AS Tooltip
 			FROM DeviceData
 			INNER JOIN ParameterType ON FK_ParameterType=PK_ParameterType
-			ORDER BY DeviceData.Description ASC';
+			INNER JOIN DeviceTemplate_DeviceData ON FK_DeviceData=PK_DeviceData
+			INNER JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate
+			ORDER BY DeviceData.Description ASC
+			';
 		$resDD=$dbADO->Execute($queryDD);
-		$pos=0;
-		while($rowDD=$resDD->FetchRow()){
-			$queryDT_DD='
-				SELECT DeviceTemplate.Description, PK_DeviceTemplate, DeviceTemplate_DeviceData.Description AS Tooltip
-				FROM DeviceTemplate_DeviceData
-				INNER JOIN DeviceTemplate ON FK_DeviceTemplate=PK_DeviceTemplate
-				WHERE FK_DeviceData=?';
-			$resDT_DD=$dbADO->Execute($queryDT_DD,$rowDD['PK_DeviceData']);
-			$dtLinksArray=array();
+		$ddArray=array();
+		while($row=$resDD->FetchRow()){
+			$ddArray[$row['PK_DeviceData']]['Description']=$row['Description'];
+			$ddArray[$row['PK_DeviceData']]['Type']=$row['Type'];
+			$ddArray[$row['PK_DeviceData']]['PK_DeviceTemplate'][]=$row['PK_DeviceTemplate'];
+			$ddArray[$row['PK_DeviceData']]['Template'][]=$row['Template'];
+			$ddArray[$row['PK_DeviceData']]['Tooltip'][]=$row['Tooltip'];
+			
+			/*
 			while($rowDT_DD=$resDT_DD->FetchRow()){
 				$dtLinksArray[]='<a href="index.php?section=editMasterDevice&model='.$rowDT_DD['PK_DeviceTemplate'].'" title="'.$rowDT_DD['Tooltip'].'">'.$rowDT_DD['Description'].'</a>';
+			}
+			*/
+		}
+
+		$pos=0;
+		foreach ($ddArray AS $ddID=>$values){
+			$dtLinksArray=array();
+			for($i=0;$i<count($values['PK_DeviceTemplate']);$i++){
+				$dtLinksArray[]='<a href="index.php?section=editMasterDevice&model='.$values['PK_DeviceTemplate'][$i].'" title="'.$values['Tooltip'][$i].'">'.$values['Template'][$i].'</a>';
 			}
 			
 			$pos++;
 			$out.='
 				<tr bgcolor="'.(($pos%2==0)?'#EEEEEE':'').'">
-					<td><a href="javascript:void(0);" onClick="windowOpen(\'index.php?section=editDeviceData&from=dceDeviceData&deviceDataID='.$rowDD['PK_DeviceData'].'\',\'status=0,resizable=1,width=500,height=250,toolbars=true\');">'.$rowDD['Description'].'</a></td>
-					<td>'.$rowDD['Type'].'</td>
+					<td><a href="javascript:void(0);" onClick="windowOpen(\'index.php?section=editDeviceData&from=dceDeviceData&deviceDataID='.$ddID.'\',\'status=0,resizable=1,width=500,height=250,toolbars=true\');">'.$values['Description'].'</a></td>
+					<td>'.$values['Type'].'</td>
 					<td>'.join(', ',$dtLinksArray).'</td>
 				</tr>';
+			
 		}
 		$out.='
 		</table>
