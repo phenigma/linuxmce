ee3e871eff98544a6b5183f6bc2d77601e4c243a
eugenc <eugenc>
2006-10-03 16:17:31 +0000

    small fix

--- src/Generic_Serial_Device/Embedded Devices/1781 Apex Destiny 6100/Ruby/SetHouseMode.rb
+++ src/Generic_Serial_Device/Embedded Devices/1781 Apex Destiny 6100/Ruby/SetHouseMode.rb
@@ -37,15 +37,15 @@ case valueNo
 when 1      #unarmed at home
 	ApexDisarmPartition("1",password) 
 when 2      # armed away
-	ApexArmPartition("AWAY","1",password ) 
+	ApexArmPartition("AWAY","1",password) 
 when 3      # armed at home
-	DSCArmPartition("HOME","1",password) 
+	ApexArmPartition("HOME","1",password) 
 when 4      # sleeping
 	log( "Case 4:\n" )
 when 5      # entertaining
 	log( "Case 5:\n" )
 when 6      # armed extended away
-	ApexArmPartition("AWAY","1",password ) 
+	ApexArmPartition("AWAY","1",password) 
 end
 
 end
