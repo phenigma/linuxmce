442761953c0afe9c59e57268eb3e86420e613cd9
radu <radu.cx+linuxmce@gmail.com>
2006-10-03 08:33:55 +0000

    cleanup

--- src/OrbiterGen/Renderer.cpp
+++ src/OrbiterGen/Renderer.cpp
@@ -1233,13 +1233,6 @@ void DoRender(string font, string output,int width,int height,class DesignObj_Ge
 RendererImage * Renderer::CreateBlankCanvas(PlutoSize size,bool bFillIt)
 {
     RendererImage * Canvas = new RendererImage;
-    Uint32 rmask, gmask, bmask, amask;
-
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    rmask = 0xff000000; gmask = 0x00ff0000; bmask = 0x0000ff00; amask = 0x000000ff;
-#else
-    rmask = 0x000000ff; gmask = 0x0000ff00; bmask = 0x00ff0000; amask = 0xff000000;
-#endif
 
     int Width = size.Width;
     int Height = size.Height;
