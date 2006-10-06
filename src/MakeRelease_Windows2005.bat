f715dc45f1fba199650d725982c1c3f705c66c84
cmiron <cmiron>
2006-10-06 08:01:21 +0000

    Updated build log for WM5

--- src/MakeRelease_Windows2005.bat
+++ src/MakeRelease_Windows2005.bat
@@ -5,7 +5,7 @@ CALL SET INPUT_PATH=c:\pluto.makerelease\trunk
 CALL SET MAKERELEASE_PATH=C:\pluto\trunk\src
 CALL SET LOG_PATH=c:\@build_log
 
-del %LOG_PATH%\BuildCE.log
+del %LOG_PATH%\Build_WM2005.log
 
 %MAKERELEASE_PATH%\Debug\MakeRelease.exe -h 10.0.0.170 -o 12 -s %INPUT_PATH% -n / -c -k 119,138
 
