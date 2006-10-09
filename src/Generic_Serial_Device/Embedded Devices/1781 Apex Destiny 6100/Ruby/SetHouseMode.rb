5549d8b918d29e6cd56ea9d3550eaeb0eb22895f
mihai.d <mihai.d>
2006-10-09 01:12:13 +0000

    * STUPID bug, it cost me 2 hours

--- src/Generic_Serial_Device/Embedded Devices/1781 Apex Destiny 6100/Ruby/SetHouseMode.rb
+++ src/Generic_Serial_Device/Embedded Devices/1781 Apex Destiny 6100/Ruby/SetHouseMode.rb
@@ -1,15 +1,15 @@
 
 case value_to_assign.to_i
 when 1      #unarmed at home
-	ApexDisarmPartition("1",password) 
+	ApexDisarmPartition("01",password) 
 when 2      # armed away
-	ApexArmPartition("AWAY","1",password) 
+	ApexArmPartition("AWAY","01",password) 
 when 3      # armed at home
-	ApexArmPartition("HOME","1",password) 
+	ApexArmPartition("HOME","01",password) 
 when 4      # sleeping
 	log( "Case 4:\n" )
 when 5      # entertaining
 	log( "Case 5:\n" )
 when 6      # armed extended away
-	ApexArmPartition("AWAY","1",password)
+	ApexArmPartition("AWAY","01",password)
 end
