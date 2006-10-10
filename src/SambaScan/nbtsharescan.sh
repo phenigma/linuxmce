e4367f9f76cf5a9c44e137265c31749e592dbb81
rgavril <rgavril>
2006-10-10 10:52:54 +0000

    Rewrite because: Code Cleanup + Drop propretary nbtscan tool and use debian's free one

--- src/SambaScan/nbtsharescan.sh
+++ src/SambaScan/nbtsharescan.sh
@@ -1,53 +1,61 @@
 #!/bin/bash
-
-# 
-
-resultip=0
-resultmachine=1
-resultmac=2
-resulttype=3
-
-DBHostName=localhost
-DBUserName=root
-DBPassword=
-DBName=pluto_main
-
 . /usr/pluto/bin/SQL_Ops.sh
 . /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/Network_Parameters.sh
+
+## Loging function
+function Log() {
+	echo "$(date) $*"
+}
+
+## Transform decimal-dot netmask in decimal netmask
+function netmaskLen() {
+        netmask=$1
+        len=0
+        for i in 1 2 3 4 ;do
+                netmaskPart=$(echo $netmask | cut -d'.' -f$i)
+
+                while [ $netmaskPart -gt 0 ] ;do
+                        len=$(( len + $netmaskPart % 2 ))
+                        netmaskPart=$(( $netmaskPart / 2 ))
+                done
+        done
+
+        echo $len
+}
 
 
-## 
-# Scan for windows shares
+## Store in IntNetwork the network address with netmask in decimal format
+IntNetwork="${IntIP}/$(netmaskLen "${IntNetmask}")"
 
-## Radu/Razvan the following should not be hardcoded but rather come from the 'internal network' and the 24 is if the subnet mask is 255.255.255.0, otherwise it's 16 if it's 255.255.0.0
-./nbtscan -m 192.168.2.0/24 | while read line
-do
-	arrresult=($line)
-	if [ ${#arrresult[*]} = 4 ]; then
+
+## Scan the network for samba servers
+Log "Scanning network $IntNetwork"
+for outputLine in $( nbtscan $IntNetwork -s '#' -q | tr -d ' ') ;do
+	## Get the important info
+	serverIP=$(echo $outputLine | cut -d'#' -f1)
+	serverName=$(echo $outputLine | cut -d'#' -f2)
+	serverMAC=$(echo $outputLine | cut -d'#' -f5)
+
+	Log "Processing $serverIP"
+
+	## Check to see if the ip is allready in the database
+	Q="
+		SELECT IPaddress FROM Device WHERE IPaddress='${serverIP}' LIMIT 1;
+		SELECT IPaddress FROM UnknownDevices WHERE IPaddress='${serverIP}' LIMIT 1;
+		SELECT IPaddress FROM PnpQueue WHERE IPaddress='${serverIP}' LIMIT 1
+	"
+	Result=$(RunSQL "$Q")
+
+	## If the IP is not there allready
+	if [[ "$Result" == "" ]] ;then
+		## Report it to the router
+		/usr/pluto/bin/MessageSend $DCERouter 0 -1001 2 65 52 3 53 2 49 1837 5 "$serverMAC" 28 "$serverIP" 55 "186|$serverName"
+		Log "Reporting $serverIP [$serverName] to the router"
 		
-		# check to see if this IP is already known
-	
-		Q1="SELECT IPaddress FROM Device WHERE IPaddress='${arrresult[$resultip]}' LIMIT 1"
-		Q2="SELECT IPaddress FROM UnknownDevices WHERE IPaddress='${arrresult[$resultip]}' LIMIT 1"
-		Q3="SELECT IPaddress FROM PnpQueue WHERE IPaddress='${arrresult[$resultip]}' LIMIT 1"
-		echo "$Q"
-		
-		IP=$(RunSQL "$Q1")
-				
-		echo "Result is $IP"
+	## If the ip allready exists in the database
+	else
+		Log "$serverIP is allready in the database"
+	fi 
 
-## Radu/Razvan -- this needs to run all 3 queries, and if any of the results are not empty (don't bother doing a compare, just a 'not empty', then the if is true and we will skip it
-
-		if [ $IP = ${arrresult[$resultip]} ]; then
-
-			echo "$IP is already in the System.";
-			
-		else
-
-				/usr/pluto/bin/MessageSend 192.168.2.152 0 -1001 2 65 52 3 53 2 49 1837 5 "${arrresult[$resultmac]}" 28 "${arrresult[$resultip]}" 55 "186|${arrresult[$resultmachine]}"
-		fi
-		
-		echo "${arrresult[$resultip]},*,${arrresult[$resultmachine]}"
-		
-	fi
 done
