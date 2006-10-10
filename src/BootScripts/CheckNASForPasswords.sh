ad8a3383d1de4f8330420529171ff610d32d81f4
rgavril <rgavril>
2006-10-10 15:02:12 +0000

    * Fixed typo in restartX * Check nas for password reports 'no password' until we find a nas that realy needs one

--- src/BootScripts/CheckNASForPasswords.sh
+++ src/BootScripts/CheckNASForPasswords.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+PnpQueueID=$1
+DeviceID=$2
+
 . /usr/pluto/bin/SQL_Ops.sh
 . /usr/pluto/bin/Config_Ops.sh
 
-Q="SELECT IPaddress FROM PnpQueue WHERE PK_PnpQueue=$1"
-IP=$(RunSQL "$Q")
+## Get the device ip address from the pnp queue
+Q="SELECT IPaddress FROM PnpQueue WHERE PK_PnpQueue=$PnpQueueID"
+IPaddress=$(RunSQL "$Q")
+if [[ $IPaddress == "" ]] ;then
+	echo "Error: Can't find any PnpQueue entry with PK_PnpQueue = $PnpQueueID"
+	exit 1
+fi
 
-echo "Checking IP $IP for passwords"
+echo "Checking IP $IPaddress for passwords"
 
 # send the message CMD_Pre_Pnp_Script_Done with 109 Data String = parm 182 DEVICEDATA_Password_Required_CONST = 1
-/usr/pluto/bin/MessageSend $DCERouter 0 $2 1 822 119 1 224 $1 109 "182|1"
+/usr/pluto/bin/MessageSend $DCERouter 0 $DeviceID 1 822 119 1 224 $PnpQueueID 109 "182|0"
--- src/BootScripts/RestartX.sh
+++ src/BootScripts/RestartX.sh
@@ -12,7 +12,7 @@ fi
 orbiterID=$1
 computerIP=$2
 
-/usr/pluto/bin/LanchRemoteCmd.sh "$computerIP" "
+/usr/pluto/bin/LaunchRemoteCmd.sh "$computerIP" "
 	. /usr/pluto/bin/Utils.sh;
 	kill $(ps ax|grep 'X :0 -ignoreABI -ac -allowMouseOpenFail vt7'|egrep -v 'grep|SCREEN'|awk '{print $1}')
 	sleep 5;
