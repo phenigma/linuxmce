6e0a5e8cfe2613902edf593759ef57f97baf5271
rgavril <rgavril>
2006-10-12 08:20:08 +0000

    Samba/NFS Shares now use the ip address of the parent device

--- src/PlutoStorageDevices/Configure_1768.sh
+++ src/PlutoStorageDevices/Configure_1768.sh
@@ -19,14 +19,5 @@ if [[ "$Device_ID" == "" ]]; then
 	echo "ERROR: No PK_Device assigned to this device"
 fi
 
-## Check Device ip address
-if [[ "$Device_IP" == "" ]]; then
-	Q="SELECT IPaddress FROM Device WHERE PK_Device = '$Device_ID'"
-	Device_IP=$(RunSQL "$Q")
-fi
-if [[ "$Device_IP" == "" ]]; then
-	echo "ERROR: No IP associated with the device $Device_ID"
-fi
-
 /usr/pluto/bin/StorageDevices_PlutoDirStructure.sh -d $Device_ID
 /usr/pluto/bin/StorageDevices_Setup.sh --cascade
