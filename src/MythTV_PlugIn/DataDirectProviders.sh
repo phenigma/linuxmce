9aacad6fa556d325cc590003444b474cd891c069
alex.f <alex.f>
2006-10-12 14:15:11 +0000

    Added finding the lineups on data directs service and echo this out to stdout

--- src/MythTV_PlugIn/DataDirectProviders.sh
+++ src/MythTV_PlugIn/DataDirectProviders.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
+USER=$1
+PASS=$2
+if [ $# -ne 0 ]; then
+	echo "$(date) Logging" >> /var/log/pluto/tribune.log
+	POSTFILE="/tmp/DDpost"
+	RESPONSE="/tmp/DDresponce"
+	echo -ne "<?xml version='1.0' encoding='utf-8'?>
+<SOAP-ENV:Envelope
+xmlns:SOAP-ENV='http://schemas.xmlsoap.org/soap/envelope/'
+xmlns:xsd='http://www.w3.org/2001/XMLSchema'
+xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+xmlns:SOAP-ENC='http://schemas.xmlsoap.org/soap/encoding/'>
+<SOAP-ENV:Body>
+<ns1:download  xmlns:ns1='urn:TMSWebServices'>
+<startTime xsi:type='xsd:dateTime'>">$POSTFILE
+	echo -ne `date +%Y-%m-%dT00:00:00Z`>>$POSTFILE
+	echo -ne "</startTime>
+<endTime xsi:type='xsd:dateTime'>">>$POSTFILE
+	echo -ne `date +%Y-%m-%dT00:00:00Z`>>$POSTFILE
+	echo -ne "</endTime>
+</ns1:download>
+</SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+">>$POSTFILE
 
-echo "$(date) Logging" >> /var/log/pluto/tribune.log
-echo "OK"
-echo "1	Brisbane Direct TV"
-echo "2	Brisbane Comcast"
-echo "3	Brisbane Over the Air"
-
+	/usr/bin/wget --http-user=$USER --http-passwd=$PASS --post-file=$POSTFILE --header=Accept-Encoding:gzip "http://datadirect.webservices.zap2it.com/tvlistings/xtvdService" --output-document=-|gzip -d>$RESPONSE
+	echo "OK"
+	for i in `cat $RESPONSE|grep -e "^<lineup "|sed "s/ /#/g"|cut -d"'" -f2,4`; do
+		echo "`echo $i|sed "s/#/ /g"|sed "s/'/      /g"`"
+	done
+#echo "1	Brisbane Direct TV"
+#echo "2	Brisbane Comcast"
+#echo "3	Brisbane Over the Air"
+	rm $POSTFILE $RESPONSE
+else
+	echo "ERROR"
+fi
