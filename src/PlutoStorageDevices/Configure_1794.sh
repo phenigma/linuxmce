0b725da41a10d0077305c2b75fe320167a652903
rgavril <rgavril>
2006-10-12 13:25:50 +0000

    Several bug fixes

--- src/PlutoStorageDevices/Configure_1794.sh
+++ src/PlutoStorageDevices/Configure_1794.sh
@@ -48,78 +48,79 @@ Device_Password=$(Field "1" "$R")
 tempMntDir=/tmp/mnt/$pid
 mkdir -p $tempMntDir
 
-## Get a list of samba shares exported by this device
-for share in $(smbclient -U ${Device_Username}%${Device_Password} --list=//$Device_IP  --grepable | grep "^Disk" | cut -d'|' -f2) ;do
+## For every share of this samba server
+for share in $(smbclient -U ${Device_Username}%${Device_Password} --list=//$Device_IP  --grepable | grep "^Disk" | cut -d'|' -f2 | tr ' ' '\\') ;do
 
-	## Create a Plug & Play UniqueID
+	share=$(echo $share | tr '\\' ' ')
 	pnpUID="\\\\${Device_IP}\\${share}"
+	mountedOK="false"
 
-	## Try to mount them without a password
-	mount -o guest //$Device_IP/$share $tempMntDir
-	success=$?
-	
-	## If the mount succeded withoud a password :
-	if [[ "$success" == "0" ]] ;then
-
-		## We umount the share from the temp dir
-		umount -f -l $tempMntDir
-		## And notify the router about our succes so he wouldn't ask the user for password
-		/usr/pluto/bin/MessageSend dcerouter $Device_ID -1001 2 65 52 3 53 2 49 1768 55 "182|0|$DD_SHARE|$share" 54 "$pnpUID"
-
-	## If the mound didn't succed without a user password
-	else
-		## We should try use our user and password on this share to see if it works
-		## Suggestion : try brute-force the password so we won't need to ask the user, this would be super user frendly :))
-		mount -o username=${Device_Username},password=${Device_Password} //$Device_IP/$share $tempMntDir
+	## Try to mount it without a password
+	if [[ "$mountedOK" == "false" ]] ;then
+		mount -o guest "//$Device_IP/$share" "$tempMntDir"
 		success=$?
 		
-		## If the mount succeded with the user/pass for the parent device
+		if [[ "$success" == "0" ]] ;then
+			umount -f -l $tempMntDir
+			/usr/pluto/bin/MessageSend dcerouter $Device_ID -1001 2 65 52 3 53 2 49 1768 55 "182|0|$DD_SHARE|$share" 54 "$pnpUID"
+			mountedOK="true"
+		fi
+	fi
+	
+	## Try to mount it with user/pass of the parent device (this device)
+	if [[ "$mountedOK" == "false" ]] ;then
+		mount -o username=${Device_Username},password=${Device_Password} "//$Device_IP/$share" "$tempMntDir"
+		success=$?
+		
 		if [[ "$success" == "0" ]] ;then
 			umount -f -l $tempMntDir
 			/usr/pluto/bin/MessageSend dcerouter $Device_ID -1001 2 65 52 3 53 2 49 1768 55 "182|0|$DD_SHARE|$share|$DD_USERNAME|$Device_Username|$DD_PASSWORD|$Device_Password" 54 "$pnpUID"
-
-		## If the mount didn't succeded wit the user/pass of the parent device
-		else
-			## Create a list of user/pass combinations from the allready added childrens (our little brothers and sisters :)
-			Q="
-				SELECT
-					Username.IK_DeviceData,
-					Password.IK_DeviceData
-				FROM
-					Device
-					INNER JOIN Device_DeviceData Username ON ( Device.PK_Device = Username.FK_Device AND Username.FK_DeviceData = $DD_USERNAME ) 
-					INNER JOIN Device_DeviceData Password ON ( Device.PK_Device = Password.FK_Device AND Password.FK_DeviceData = $DD_PASSWORD )
-				WHERE
-					FK_Device_ControlledVia = $PK_Device
-			"
-			R=$(RunSQL "$Q")
-
-			## Check with every combinationp
-			siblingUserPassWorking="0"
-			for UserPass in "$R" ;do
-				Brother_Username=$(Field "1" "$UserPass")
-				Brother_Password=$(Field "2" "$UserPass")
-		
-				mount -o username=${Brother_Username},password=${Brother_Password} //$Device_IP/$share $tempMntDir
-		                success=$?
-
-				if [[ "$success" == "0" ]] ;then
-					umount -f -l $tempDir
-					/usr/pluto/bin/MessageSend dcerouter $Device_ID -1001 2 65 52 3 53 2 49 1768 55 "182|0|$DD_SHARE|$share|$DD_USERNAME|$Brother_Username|$DD_PASSWORD|$Brother_Password" 54 "$pnpUID"
-
-					siblingUserPassWorking="1"
-					break
-				fi
-			done
-
-			## If it didn't worked with siblings user/pass then prompt the user to input the user/pass			
-			if [[ "$siblingUserPassWorking" == "0" ]] ;then
-				/usr/pluto/bin/MessageSend dcerouter $Device_ID -1001 2 65 52 3 53 2 49 1768 55 "182|1|$DD_SHARE|$share" 54 "$pnpUID"
-			fi
+			mountedOK="true"
 		fi
 	fi
-done
-rmdir $tempMntDir
 
-#/usr/pluto/bin/StorageDevices_PlutoDirStructure.sh -d $Device_ID
-#/usr/pluto/bin/StorageDevices_Setup.sh --cascade
+	## Try with the username/pass of the brother devices
+	if [[ "$mountedOK" == "false" ]] ;then
+		Q="
+			SELECT
+				Username.IK_DeviceData,
+				Password.IK_DeviceData
+			FROM
+				Device
+				INNER JOIN Device_DeviceData Username ON ( Device.PK_Device = Username.FK_Device AND Username.FK_DeviceData = $DD_USERNAME ) 
+				INNER JOIN Device_DeviceData Password ON ( Device.PK_Device = Password.FK_Device AND Password.FK_DeviceData = $DD_PASSWORD )
+			WHERE
+				FK_Device_ControlledVia = $PK_Device
+		"
+		R=$(RunSQL "$Q")
+
+		siblingUserPassWorking="0"
+
+		for UserPass in "$R" ;do
+			Brother_Username=$(Field "1" "$UserPass")
+			Brother_Password=$(Field "2" "$UserPass")
+	
+			mount -o username=${Brother_Username},password=${Brother_Password} "//$Device_IP/$share" "$tempMntDir"
+			success=$?
+
+			if [[ "$success" == "0" ]] ;then
+				umount -f -l $tempDir
+				echo "# Mount of '\\$Device_IP\\$share' succeded with  user/password of a brother device"
+				/usr/pluto/bin/MessageSend dcerouter $Device_ID -1001 2 65 52 3 53 2 49 1768 55 "182|0|$DD_SHARE|$share|$DD_USERNAME|$Brother_Username|$DD_PASSWORD|$Brother_Password" 54 "$pnpUID"
+
+				siblingUserPassWorking="1"
+				mountedOK="true"
+			fi
+			
+			[[ "$siblingUserPassWorking" == "1" ]] && break
+		done
+	fi
+
+	
+	## Notify the router that we didn't found any user/pass combination
+	if [[ "$mountedOK" == "false" ]] ;then
+		/usr/pluto/bin/MessageSend dcerouter $Device_ID -1001 2 65 52 3 53 2 49 1768 55 "182|1|$DD_SHARE|$share" 54 "$pnpUID"
+	fi
+done
+
+rmdir $tempMntDir
