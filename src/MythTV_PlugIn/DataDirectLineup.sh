9cbffd04cedc416429f4bad6e763b83b14fa8b2f
aaron <aaron.b@plutohome.com>
2006-10-12 01:32:26 +0000

    data direct

---
+++ src/MythTV_PlugIn/DataDirectLineup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "$(date) Logging" >> /var/log/pluto/tribune.log
+echo "OK"
+echo "1	CNN"
+echo "2	HBO"
+echo "3	NBC"
+
---
+++ src/MythTV_PlugIn/DataDirectProviders.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "$(date) Logging" >> /var/log/pluto/tribune.log
+echo "OK"
+echo "1	Brisbane Direct TV"
+echo "2	Brisbane Comcast"
+echo "3	Brisbane Over the Air"
+
