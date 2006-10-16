07605b8a29a289c9a9bb37e753edc5f2a25924c9
rgavril <rgavril>
2006-10-16 11:47:05 +0000

    Fix matis 0002846: Aditional modules specified on MD's page on web admin..

---
+++ src/BootScripts/LoadMDModules.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/SQL_Ops.sh
+
+DD_MODULES=106
+DT_GENERIC_PC_AS_MD=28
+
+R=$(RunSQL "SELECT FK_DeviceTemplate FROM Device WHERE PK_Device = $PK_Device")
+ThisDeviceTemplate=$(Field "1" "$R")
+
+if [[ "ThisDeviceTemplate" == "$DT_GENERIC_PC_AS_MD" ]] ;then
+	R=$(RunSQL "SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_Device=${PK_Device} AND FK_DeviceData=$DD_MODULES")
+else
+	Q="
+		SELECT
+			IK_DeviceData
+		FROM
+			Device_DeviceData
+			INNER JOIN Device ON Device.PK_Device = Device_DeviceData.FK_Device
+		WHERE
+			FK_DeviceData=$DD_MODULES
+			AND
+			Device.FK_Device_ControlledVia = $PK_Device
+			AND
+			Device.FK_DeviceTemplate = $DT_GENERIC_PC_AS_MD
+	"
+	R=$(RunSQL "$Q")
+fi
+
+ModulesList=$(Field "1" "$R")
+for module in $( echo $ModulesList  | tr ',' ' ') ;do
+	modprobe $module
+done
