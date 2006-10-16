d0091c5a2010e473cb06e76ad2cde828260b1176
vali <vali>
2006-10-16 06:53:24 +0000

    Fixed quick regen commnad by adding -a value for parm 21.

--- web/pluto-admin/operations/myDevices/orbiters/wizardOrbiters.php
+++ web/pluto-admin/operations/myDevices/orbiters/wizardOrbiters.php
@@ -332,7 +332,7 @@ function wizardOrbiters($output,$dbADO) {
 				$updateOrbiters=true;
 				$ResetRouter=((int)@$_POST['reset_'.$value]==1)?' 24 1':'';
 				
-				$commandToSend='/usr/pluto/bin/MessageSend localhost -targetType template '.$value.' '.$GLOBALS['OrbiterPlugIn'].' 1 266 2 '.$value.' 21 '.$ResetRouter;
+				$commandToSend='/usr/pluto/bin/MessageSend localhost -targetType template '.$value.' '.$GLOBALS['OrbiterPlugIn'].' 1 266 2 '.$value.' 21 "-a" '.$ResetRouter;
 				exec($commandToSend,$tmp);
 				$regen='Q_'.$value;
 			}
