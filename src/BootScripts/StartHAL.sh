5e1176df71e3be749abe145de1a11ec8311d7f44
aaron <aaron.b@plutohome.com>
2006-10-22 21:59:06 +0000

    more tweaks to install process

--- src/BootScripts/StartHAL.sh
+++ src/BootScripts/StartHAL.sh
@@ -4,6 +4,7 @@
 . /usr/pluto/bin/Utils.sh
 
 DEVICETEMPLATE_HAL=1808
+DEVICETEMPLATE_APPSERVER=26
 
 HALdev=$(FindDevice_Template "$PK_Device" "$DEVICETEMPLATE_HAL" "norecursion")
 if [[ -z "$HALdev" ]]; then
@@ -12,3 +13,12 @@ if [[ -z "$HALdev" ]]; then
 fi
 
 screen -d -m -S "HAL-$HALdev" /usr/pluto/bin/Spawn_Device.sh $HALdev $DCERouter HAL
+
+ASdev=$(FindDevice_Template "$PK_Device" "$DEVICETEMPLATE_APPSERVER" "norecursion")
+if [[ -z "$ASdev" ]]; then
+	# App Server device not found
+	exit 1
+fi
+
+screen -d -m -S "APP-$ASdev" /usr/pluto/bin/Spawn_Device.sh $ASdev $DCERouter APP
+
