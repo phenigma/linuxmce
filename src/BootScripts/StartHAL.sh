4026e8e16a453e2154bdb5648211990c10c08a3b
radu <radu.cx+linuxmce@gmail.com>
2006-10-23 09:55:31 +0000

    fixed command line for App_Server to start

--- src/BootScripts/StartHAL.sh
+++ src/BootScripts/StartHAL.sh
@@ -4,7 +4,7 @@
 . /usr/pluto/bin/Utils.sh
 
 DEVICETEMPLATE_HAL=1808
-DEVICETEMPLATE_APPSERVER=26
+DEVICETEMPLATE_AppServer=26
 
 HALdev=$(FindDevice_Template "$PK_Device" "$DEVICETEMPLATE_HAL" "norecursion")
 if [[ -z "$HALdev" ]]; then
@@ -14,11 +14,10 @@ fi
 
 screen -d -m -S "HAL-$HALdev" /usr/pluto/bin/Spawn_Device.sh $HALdev $DCERouter HAL
 
-ASdev=$(FindDevice_Template "$PK_Device" "$DEVICETEMPLATE_APPSERVER" "norecursion")
+ASdev=$(FindDevice_Template "$PK_Device" "$DEVICETEMPLATE_AppServer" "norecursion")
 if [[ -z "$ASdev" ]]; then
 	# App Server device not found
 	exit 1
 fi
 
-screen -d -m -S "APP-$ASdev" /usr/pluto/bin/Spawn_Device.sh $ASdev $DCERouter APP
-
+screen -d -m -S "APP-$ASdev" /usr/pluto/bin/Spawn_Device.sh $ASdev $DCERouter App_Server
