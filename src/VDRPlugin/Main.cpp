9915b0328fa5642f803ea89a591d99b9f9f5c63b
radu <radu.cx+linuxmce@gmail.com>
2006-10-25 08:04:17 +0000

    reverted s&r commits

--- src/VDRPlugin/Main.cpp
+++ src/VDRPlugin/Main.cpp
@@ -9,9 +9,9 @@
 
 // In source files stored in archives and packages, these 2 lines will have the release version (build)
 // and the svn revision as a global variable that can be inspected within a core dump
-#define  VERSION "2.0.0.41"
-const char *g_szCompile_Date="Mon Oct 23 19:05:50 2006";
-int g_SvnRevision=12680;
+#define  VERSION "<=version=>"
+const char *g_szCompile_Date="<=compile_date=>";
+/*SVN_REVISION*/
 
 namespace DCE
 {
