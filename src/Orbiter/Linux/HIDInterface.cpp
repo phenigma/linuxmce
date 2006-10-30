94c264d0afd46222aa4a47b7450edbc40ba57f99
aaron <aaron.b@plutohome.com>
2006-10-30 23:03:07 +0000

    debug

--- src/Orbiter/Linux/HIDInterface.cpp
+++ src/Orbiter/Linux/HIDInterface.cpp
@@ -67,18 +67,19 @@ void *ProcessHIDEvents(void *p)
 		}
 		else if (ret != HID_RET_FAIL_INT_READ)
 		{
-g_pPlutoLogger->Write(LV_WARNING,"ProcessHIDEvents hid_get_input_report ret %d\n", ret);
 
 			Orbiter::Event *pEvent = new Orbiter::Event;
 			pEvent->type=Orbiter::Event::HID;
 
-			printf("HID packet: ");
+			char packet_data[200];
+			sprintf(packet_data,"HID packet: ");
 			for (i = 0; i < RECV_PACKET_LEN; i++)
 			{
 				pEvent->data.hid.m_pbHid[i] = packet[i];
-				printf("%02x ", packet[i]);
+				sprintf(packet_data,"%02x ", packet[i]);
 			}
-			printf(".\n");
+
+g_pPlutoLogger->Write(LV_WARNING,"ProcessHIDEvents hid_get_input_report ret %d packet %s\n", ret, packet_data);
 
 			if( packet[0]==8 && packet[1]==0x20 )
 			{
@@ -88,31 +89,22 @@ g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents got a bind request.  Donig it.
 				char write_packet[5];
 				write_packet[0]=8;
 				write_packet[1]=0x20;
-				write_packet[2]=0x01;
+				write_packet[2]=0x01;  // The remote ID
 				write_packet[3]=0;
-
+/*
 int len = usb_interrupt_write(hid->dev_handle,0x80, (char *) write_packet, 4, 250);
 g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents  usb_interrupt_write %d",(int) len);
-
+*/
 int ctrl = usb_control_msg(hid->dev_handle, 0x21, 9, 0 /*int value*/, 0 /* int index */, write_packet, 4, 250);
 g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents  usb_control_msg %d",(int) ctrl);
 
-
+/*
 
 				hid_return ret_write = hid_interrupt_write(hid, 0x80, (char *) write_packet, 4, 250);
 #ifdef DEBUG
 g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents hid_return was %d",(int) ret_write);
 #endif
-
-				hid_return ret_write2 = hid_interrupt_write(hid, 0x81, (char *) write_packet, 4, 250);
-#ifdef DEBUG
-g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents hid_return2 was %d",(int) ret_write2);
-#endif
-
-				hid_return ret_write3 = hid_interrupt_write(hid, 0x82, (char *) write_packet, 4, 250);
-#ifdef DEBUG
-g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents hid_return3 was %d",(int) ret_write3);
-#endif
+*/
 			}
 
 #ifdef DEBUG
@@ -120,6 +112,7 @@ g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents queueing to orbiter   rrr");
 #endif
 			pOrbiter->CallMaintenanceInMiliseconds(0, &Orbiter::QueueEventForProcessing, pEvent, pe_NO, false );
 		}
+		g_pPlutoLogger->Write(LV_STATUS,"ProcessHIDEvents hid_get_input_report ret %d *IDLE*\n", ret);
 	} while (!pOrbiter->m_bQuit);
 
 	ret = hid_close(hid);
