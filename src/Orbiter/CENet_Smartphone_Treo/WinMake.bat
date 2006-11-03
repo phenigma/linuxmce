20a29d1fd8a937d92e6044506fd19ff2fdaed105
cmiron <cmiron>
2006-11-03 08:39:46 +0000

    Temporary use the debug version.

--- src/Orbiter/CENet_Smartphone_Treo/WinMake.bat
+++ src/Orbiter/CENet_Smartphone_Treo/WinMake.bat
@@ -2,4 +2,9 @@ SET PATH="c:\Program Files\Microsoft Visual Studio 8\Common7\IDE"
 
 devenv.exe Orbiter.sln /rebuild DEBUG /out %LOG_PATH%\Build_WM2005.log 
 
+rem Temporary use the debug version
+copy "..\..\Debug\OrbiterTreo.exe" "..\..\bin"
+
 SET PATH="c:\Program Files\Microsoft eMbedded C++ 4.0\Common\EVC\Bin"
+
+
