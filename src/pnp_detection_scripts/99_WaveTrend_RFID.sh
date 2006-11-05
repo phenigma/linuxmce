4b586dd855c92e405bfbc811c96e8363c2d7ed07
aaron <aaron.b@plutohome.com>
2006-11-05 03:43:56 +0000

    rfid detection

--- src/pnp_detection_scripts/99_WaveTrend_RFID.sh
+++ src/pnp_detection_scripts/99_WaveTrend_RFID.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Wavetrend RFID queue $2"
-/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 115200 -t "\aa\00\00\00\01\03\02" -i 1 -s "\0a\61"
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 115200 -t "\aa\00\00\00\01\03\02" -i 1 -s "\aa\00"
 if [[ "$?" -ne 0 ]]; then
 echo "It's not a Wavetrend RFID"
 /usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 0
