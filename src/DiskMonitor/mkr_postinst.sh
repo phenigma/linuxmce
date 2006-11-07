daca8365cbb188664b416b1be47b0f1af1312da5
rgavril <rgavril>
2006-11-07 13:02:50 +0000

    Do the checking more often

--- src/DiskMonitor/mkr_postinst.sh
+++ src/DiskMonitor/mkr_postinst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Add a cron entry
-cronEntry="*/10 * * * * root /usr/pluto/bin/DiskSpaceMonitor.sh"
+cronEntry="*/3 * * * * root /usr/pluto/bin/DiskSpaceMonitor.sh"
 if ! grep -qF '/usr/pluto/bin/DiskSpaceMonitor.sh' /etc/crontab; then
         echo "$cronEntry" >>/etc/crontab
         invoke-rc.d cron reload
