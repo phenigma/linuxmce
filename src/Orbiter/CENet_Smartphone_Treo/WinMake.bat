14412e3645aa749824ff7875543868afc4ab846f
cmiron <cmiron>
2006-11-07 13:34:13 +0000

    Switched back to release version for Orbiter Treo.

--- src/Orbiter/CENet_Smartphone_Treo/WinMake.bat
+++ src/Orbiter/CENet_Smartphone_Treo/WinMake.bat
@@ -1,9 +1,6 @@
 SET PATH="c:\Program Files\Microsoft Visual Studio 8\Common7\IDE"
 
-devenv.exe Orbiter.sln /rebuild DEBUG /out %LOG_PATH%\Build_WM2005.log 
-
-rem Temporary use the debug version
-copy "..\..\Debug\OrbiterTreo.exe" "..\..\bin"
+devenv.exe Orbiter.sln /rebuild RELEASE /out %LOG_PATH%\Build_WM2005.log 
 
 SET PATH="c:\Program Files\Microsoft eMbedded C++ 4.0\Common\EVC\Bin"
 
