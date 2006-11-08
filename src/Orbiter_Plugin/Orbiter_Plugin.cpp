d3feeec3ba6b47c7285a0744706e5d71da44177e
cmiron <cmiron>
2006-11-08 21:57:17 +0000

    Added BD_CP_ShowMenu to PlutoVIP and PlutoMO symbian. Updated PlutoMO's version.

--- src/Orbiter_Plugin/Orbiter_Plugin.cpp
+++ src/Orbiter_Plugin/Orbiter_Plugin.cpp
@@ -87,7 +87,7 @@ using namespace DCE;
 #include <algorithm>
 
 #define VERSION "<=version=>"
-string g_sLatestMobilePhoneVersion="2006.02.09";
+string g_sLatestMobilePhoneVersion="2006.11.09";
 
 #include "../Media_Plugin/EntertainArea.h"
 #include "../Media_Plugin/Media_Plugin.h"
