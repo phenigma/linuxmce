7da67c8f28f9b4fa5a9a5bdd70c48c70a2f2475c
radu <radu.cx+linuxmce@gmail.com>
2006-11-16 21:19:04 +0000

    don't die on updatemedia crash

--- src/UpdateMedia/mkr_postinst.sh
+++ src/UpdateMedia/mkr_postinst.sh
@@ -3,7 +3,7 @@
 . /usr/pluto/bin/SQL_Ops.sh
 
 # One time also update the thumbnails and search tokens
-/usr/pluto/bin/UpdateMedia -t -s -d /home/public/data
+/usr/pluto/bin/UpdateMedia -t -s -d /home/public/data || :
 
 Q="SELECT PK_Users FROM Users"
 R="$(RunSQL "$Q")"
@@ -11,18 +11,18 @@ R="$(RunSQL "$Q")"
 for User in $R; do
 	Dir="/home/user_$User/data/documents"
 	if [[ -d "$Dir" ]]; then
-		/usr/pluto/bin/UpdateMedia -d "$Dir"
+		/usr/pluto/bin/UpdateMedia -d "$Dir" || :
 	fi
 	Dir="/home/user_$User/data/audio"
 	if [[ -d "$Dir" ]]; then
-		/usr/pluto/bin/UpdateMedia -d "$Dir"
+		/usr/pluto/bin/UpdateMedia -d "$Dir" || :
 	fi
 	Dir="/home/user_$User/data/pictures"
 	if [[ -d "$Dir" ]]; then
-		/usr/pluto/bin/UpdateMedia -d "$Dir"
+		/usr/pluto/bin/UpdateMedia -d "$Dir" || :
 	fi
 	Dir="/home/user_$User/data/videos"
 	if [[ -d "$Dir" ]]; then
-		/usr/pluto/bin/UpdateMedia -d "$Dir"
+		/usr/pluto/bin/UpdateMedia -d "$Dir" || :
 	fi
 done
