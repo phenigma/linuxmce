dce6d17cc37c0c2d2b329ed113edcc52c1d5c004
radu <radu.cx+linuxmce@gmail.com>
2006-11-21 14:14:11 +0000

    added error condition

--- src/MakeRelease/MR_Conf.sh
+++ src/MakeRelease/MR_Conf.sh
@@ -10,6 +10,11 @@ ConfEval()
 	local Ret=0
 	local Flavor="${1:-pluto}"
 
+	if [[ ! -f "/etc/MakeRelease/$Flavor.conf" ]]; then
+		echo "ERROR: /etc/MakeRelease/$Flavor.conf doesn not exist"
+		exit 1
+	fi
+	
 	# Note to self: this "read from file into while" is absolutely necessary
 	#               "cmd | while ..." spawns a subshell and our veriables will be set there instead of our current shell
 	#               "while ...; do ...; done <file" works the way we need
