e906bba73ab3b28b798f4fe152cfb2de1993e1ad
radu <radu.cx+linuxmce@gmail.com>
2006-11-29 09:27:14 +0000

    fixed path (using intuition alone)

--- src/MythTV_PlugIn/FillDbAndFetchIcons.sh
+++ src/MythTV_PlugIn/FillDbAndFetchIcons.sh
@@ -4,7 +4,7 @@
 /usr/bin/mythfilldatabase --import-icon-map /home/mythtv/master_iconmap.xml --update-icon-map
 echo "LOCK TABLE schemalock WRITE;" | mysql mythconverg  # Be sure we're not in the middle of a schema upgrade -- myth doesn't check this
 #myth always puts them in /.root/mythtv.  We want /home/mythtv
-echo "update channel set icon=replace(icon,'/.root/mythtv','/home/mythtv')" | mysql mythconverg  # These should be shared
+echo "update channel set icon=replace(icon,'/root/.mythtv','/home/mythtv')" | mysql mythconverg  # These should be shared
 mkdir -p /home/mythtv/channels
 mv /root/.mythtv/channels/* /home/mythtv/channels
 invoke-rc.d mythtv-backend restart
