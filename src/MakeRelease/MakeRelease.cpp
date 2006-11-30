8997596684079907cf96d989bafce76b56cdaec8
rgavril <rgavril>
2006-11-30 12:56:29 +0000

    * Ubuntu/Pluto packages will get the correct dependencies  * Ununtu/Pluto packages won't depend on pluto-kernel-upgrade

--- src/MakeRelease/MakeRelease.cpp
+++ src/MakeRelease/MakeRelease.cpp
@@ -1854,15 +1854,23 @@ string Makefile = "none:\n"
 
 	// Get a list of all the other packages which we depend on, and which have Debian sources.  We are going to add them to the .deb as dependencies
 	vector<Row_Package_Source *> vect_pRow_Package_Source_Dependencies;
-	pRow_Package_Source->Table_Package_Source_get()->GetRows("JOIN Package_Package ON Package_Package.FK_Package_DependsOn=Package_Source.FK_Package AND Package_Package.FK_Package=" + 
-		StringUtils::itos(pRow_Package_Source->FK_Package_get()) + " WHERE FK_RepositorySource IN (" + StringUtils::itos(REPOSITORYSOURCE_Pluto_Debian_CONST)
+	if (pRow_Package_Source->FK_RepositorySource_get() != REPOSITORYSOURCE_Ubuntu_Pluto_Addons_CONST) {
+		pRow_Package_Source->Table_Package_Source_get()->GetRows("JOIN Package_Package ON Package_Package.FK_Package_DependsOn=Package_Source.FK_Package AND Package_Package.FK_Package=" + 
+		StringUtils::itos(pRow_Package_Source->FK_Package_get()) + " WHERE FK_RepositorySource IN (" + StringUtils::itos(pRow_Package_Source->FK_RepositorySource_get())
 		+ "," + StringUtils::itos(REPOSITORYSOURCE_Debian_CONST) + "," + StringUtils::itos(REPOSITORYSOURCE_MythTV_CONST) + ")",
 		&vect_pRow_Package_Source_Dependencies);
+	} else {
+		pRow_Package_Source->Table_Package_Source_get()->GetRows("JOIN Package_Package ON Package_Package.FK_Package_DependsOn=Package_Source.FK_Package AND Package_Package.FK_Package=" + 
+		StringUtils::itos(pRow_Package_Source->FK_Package_get()) + " WHERE FK_RepositorySource IN (" + StringUtils::itos(pRow_Package_Source->FK_RepositorySource_get())
+		+ "," + StringUtils::itos(REPOSITORYSOURCE_Ubuntu_CONST) + "," + StringUtils::itos(REPOSITORYSOURCE_MythTV_CONST) + ")",
+		&vect_pRow_Package_Source_Dependencies);
+	}
 
 	string sDepends,sPreDepends;
 
 	if( pRow_Package_Source->FK_Package_getrow()->FK_Manufacturer_get()==1
-			&& !isDriverPackage(pRow_Package_Source->FK_Package_get()) ) // Pluto && ! kernel_module
+			&& !isDriverPackage(pRow_Package_Source->FK_Package_get())
+	        && pRow_Package_Source->FK_RepositorySource_get() != REPOSITORYSOURCE_Ubuntu_Pluto_Addons_CONST ) // Pluto && ! kernel_module
 	{
 		sPreDepends = "pluto-kernel-upgrade ";
 	}
