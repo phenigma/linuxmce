88b72ce9731928137c5bbb3d08a94e2749e81e08
aaron <aaron.b@plutohome.com>
2006-12-04 00:04:31 +0000

    hid remote buttons working again

--- src/Orbiter/Linux/HIDInterface.cpp
+++ src/Orbiter/Linux/HIDInterface.cpp
@@ -255,7 +255,8 @@ bool PlutoHIDInterface::ProcessHIDButton(char *inPacket)
 	{
 		Orbiter::Event *pEvent = new Orbiter::Event;
 		pEvent->type=Orbiter::Event::BUTTON_UP;
-		pEvent->data.button.m_iPK_Button = m_iHoldingDownButton + 100000;
+		pEvent->data.button.m_iPK_Button = 0;
+		pEvent->data.button.m_iKeycode = m_iHoldingDownButton + 100000;
 		m_pOrbiter->CallMaintenanceInMiliseconds(0, &Orbiter::QueueEventForProcessing, pEvent, pe_NO, false );
 		if( p_Packet[3]==0 )
 		{
@@ -276,7 +277,8 @@ bool PlutoHIDInterface::ProcessHIDButton(char *inPacket)
 	m_iHoldingDownButton=p_Packet[3];
 	Orbiter::Event *pEvent = new Orbiter::Event;
 	pEvent->type=Orbiter::Event::BUTTON_DOWN;
-	pEvent->data.button.m_iPK_Button = m_iHoldingDownButton + 100000; // We're using especially high numbers, > 100000 for these special buttons
+	pEvent->data.button.m_iPK_Button = 0;
+	pEvent->data.button.m_iKeycode = m_iHoldingDownButton + 100000; // We're using especially high numbers, > 100000 for these special buttons
 
 	m_pOrbiter->CallMaintenanceInMiliseconds(0, &Orbiter::QueueEventForProcessing, pEvent, pe_NO, false );
 	return true;
