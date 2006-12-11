31e7f4271c60e218fb0b09d0dd3682a7212103a4
alex.c <alex.c>
2006-12-11 15:08:05 +0000

    First test if the PROGRAM line exists in the conf file

--- src/PlutoRaidTools/mkr_postinst.sh
+++ src/PlutoRaidTools/mkr_postinst.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "PROGRAM /usr/pluto/bin/monitoring_RAID.sh" >> /etc/mdadm/mdadm.conf
-
-invoke-rc.d mdadm reload
+result=$(cat /etc/mdadm/mdadm.conf | grep 'PROGRAM /usr/pluto/bin/monitoring_RAID.sh')
+if [[ -z $result ]] ;then
+	echo "PROGRAM /usr/pluto/bin/monitoring_RAID.sh" >> /etc/mdadm/mdadm.conf
+	invoke-rc.d mdadm reload
+fi
