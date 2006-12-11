b1803a46b29195428d3cf7adf5d558f59e704ea6
alex.c <alex.c>
2006-12-11 14:24:49 +0000

    For now we will not  create one deamon per array

--- src/PlutoRaidTools/create_RAID0.sh
+++ src/PlutoRaidTools/create_RAID0.sh
@@ -33,10 +33,11 @@ if [[ $NewAdd == 0 ]] ;then
 	done
 	#NrDrives=$(($NrDrives+1))
 	#create the array with mdadm
+	rm -f "/dev/.static$name"
 	echo "y" | mdadm --create $name --auto --level=0 --raid-devices=$NrDrives $ActiveDrives
 
 	raidSize=$(mdadm --query $name | head -1 |cut -d' ' -f2)
 	Q="UPDATE Device_DeviceData SET IK_DeviceData = '$raidSize' WHERE FK_Device = $Device and FK_DeviceData = $DISK_SIZE_ID"
 	$(RunSQL "$Q")
-	/usr/pluto/bin/start_RAID_monitoring.sh $name
+	#/usr/pluto/bin/start_RAID_monitoring.sh $name
 fi
