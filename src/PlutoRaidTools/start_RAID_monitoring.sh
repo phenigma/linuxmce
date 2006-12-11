8492a05e5eaf26d3c49ea8cd9817ad4117b1e020
alex.c <alex.c>
2006-12-11 14:21:03 +0000

    Changed the way how raid array will be monitored

--- src/PlutoRaidTools/start_RAID_monitoring.sh
+++ src/PlutoRaidTools/start_RAID_monitoring.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-#mds=$(cat /proc/partitions | awk '/(md)/ { print $4 }')
+echo "PROGRAM /usr/pluto/bin/monitoring_RAID.sh" >> /etc/mdadm/mdadm.conf
 
-#for md in $mds; do
-#	new_mds="$new_mds/dev/$md "
-#done
+mdadm --monitor --scan --daemonise
 
-#PID=$(cat /tmp/raid_pid)
-#kill -9 "$PID"
-md=$1
-mdadm --monitor --delay=300 --daemonise --program=/usr/pluto/bin/monitoring_RAID.sh "$md"
-
-#echo $PID > /tmp/raid_pid
 
