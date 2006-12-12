edb998d1d927a07dca74a0088971faab6c12e40a
alex.c <alex.c>
2006-12-12 09:26:06 +0000

    Fix postinstall script

--- src/PlutoRaidTools/mkr_postinst.sh
+++ src/PlutoRaidTools/mkr_postinst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result=$(cat /etc/mdadm/mdadm.conf | grep 'PROGRAM /usr/pluto/bin/monitoring_RAID.sh')
+result=$(cat /etc/mdadm/mdadm.conf | grep 'PROGRAM /usr/pluto/bin/monitoring_RAID.sh' || /bin/true)
 if [[ -z $result ]] ;then
 	echo "PROGRAM /usr/pluto/bin/monitoring_RAID.sh" >> /etc/mdadm/mdadm.conf
 	invoke-rc.d mdadm reload
