00ce6be6720fe6bef276c856f7df827b80804383
automagic <automagic>
2006-12-28 23:12:35 +0000

    Automatic Regen

--- src/pluto_main/Define_DeviceData.h
+++ src/pluto_main/Define_DeviceData.h
@@ -177,6 +177,7 @@
 #define DEVICEDATA_Supports_NPOT_Textures_CONST 203
 #define DEVICEDATA_RAID_Status_CONST 204
 #define DEVICEDATA_Shortcut_CONST 205
+#define DEVICEDATA_Dont_Auto_Configure_CONST 206
 
 #define DEVICEDATA_TABLE "DeviceData"
 #define DEVICEDATA_PK_DEVICEDATA_FIELD "PK_DeviceData"
