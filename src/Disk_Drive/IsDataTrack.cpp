2edd2cc8f317a98e879074c5b3386aa95d06b538
radu <radu.cx+linuxmce@gmail.com>
2007-01-08 12:23:09 +0000

    fixed end of range

--- src/Disk_Drive/IsDataTrack.cpp
+++ src/Disk_Drive/IsDataTrack.cpp
@@ -39,7 +39,7 @@ int main(int argc, char * argv[])
 	int nTrack = atoi(argv[2]);
 	if (nTrack < 1 || nTrack > hdr.cdth_trk1)
 	{
-		cout << "ERROR: Track number outside of disk range: 1-" << nTrack << endl;
+		cout << "ERROR: Track number outside of disk range: 1-" << hdr.cdth_trk1 << endl;
 		return -1;
 	}
 
