11a76f92d793025b262700499a1073074e3c2e98
radu <radu.cx+linuxmce@gmail.com>
2007-01-11 08:32:46 +0000

    update-grub is in PATH

--- src/ConfirmDependencies_Script_Offline/VIA_Setup.sh
+++ src/ConfirmDependencies_Script_Offline/VIA_Setup.sh
@@ -5,5 +5,5 @@ if ! grep -q "^# kopt=.* pci=conf1" /boot/grub/menu.lst; then
 	oldKopt=$(grep "^# kopt=.*" /boot/grub/menu.lst)
 	newKopt="$oldKopt pci=conf1"
 	sed -i "s~$oldKopt~$newKopt~g" /boot/grub/menu.lst
-	/usr/sbin/update-grub
+	update-grub
 fi
