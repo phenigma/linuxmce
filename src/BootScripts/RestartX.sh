2d53e13405b1cebf168d8b94f1cf779b6d4f8e64
radu <radu.cx+linuxmce@gmail.com>
2007-01-24 10:59:46 +0000

    copied from X-Restart

--- src/BootScripts/RestartX.sh
+++ src/BootScripts/RestartX.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+. /usr/pluto/bin/Config_Ops.sh
+XDisplay=":$Display"
+
+
 function DisplayUsage() {
 	echo "Usage: RestartX.sh <orbiterID> <computerIP>"
 }
@@ -14,11 +18,11 @@ computerIP=$2
 
 /usr/pluto/bin/LaunchRemoteCmd.sh "$computerIP" "
 	. /usr/pluto/bin/Utils.sh;
+#<-mkr_b_via_b->
+exit
+#<-mkr_b_via_e->
+
 	bash -x /usr/pluto/bin/Xconfigure.sh --keep-resolution --update-video-driver | tee /var/log/pluto/Xconfigure.log
-	EXT=
-	#<-mkr_b_ubuntu_b->
-	EXT=".pluto"
-	#<-mkr_b_ubuntu_e->
-	cp /etc/X11/xorg.conf.pluto{"$EXT",.test}
-	#kill $(ps ax|grep 'X :0 -ignoreABI -ac -allowMouseOpenFail vt7'|egrep -v 'grep|SCREEN'|awk '{print $1}')
+	cp /etc/X11/xorg.conf.pluto{,.test}
+	#kill \$(ps ax|grep \"X $XDisplay -ignoreABI -ac -allowMouseOpenFail vt7\"|egrep -v 'grep|SCREEN'|awk '{print \$1}'\)
 	/usr/pluto/bin/Xres_config_end.sh y" &
