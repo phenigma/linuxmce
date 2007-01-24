73a749178721ff75a9047ad7c4d2f8fcefcb01d8
radu <radu.cx+linuxmce@gmail.com>
2007-01-24 17:49:44 +0000

    removed extra files

--- external/pluto-fuse-smb/fusesmb.c
+++ external/pluto-fuse-smb/fusesmb.c
@@ -1,10 +1,4 @@
 /*
- * SMB for FUSE
- *
- * Mount complete "Network Neighbourhood"
- *
- * Copyright (C) 2006 Vincent Wagelaar
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -19,6 +13,58 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
+char * fusesmb_auth_fn_username = NULL, * fusesmb_auth_fn_password = NULL;
+
+static void fusesmb_auth_fn(const char *server, const char *share,
+                            char *workgroup, int wgmaxlen,
+                            char *username, int unmaxlen,
+                            char *password, int pwmaxlen)
+{
+    (void)workgroup;
+    (void)wgmaxlen;
+
+    /* Don't authenticate for workgroup listing */
+    if (NULL == server || server[0] == '\0')
+        return;
+	if (fusesmb_auth_fn_username != NULL)
+		strncpy(username, fusesmb_auth_fn_username, unmaxlen);
+	if (fusesmb_auth_fn_password != NULL)
+		strncpy(password, fusesmb_auth_fn_password, pwmaxlen);
+	return;
+}
+
+/*
+ * Create a new libsmbclient context with all necessary options
+ */
+static SMBCCTX *fusesmb_context(smbc_get_auth_data_fn fn)
+{
+    /* Initializing libsbmclient */
+    SMBCCTX *ctx;
+    ctx = smbc_new_context();
+    if (ctx == NULL)
+        return NULL;
+
+    ctx->callbacks.auth_fn = fn;
+    //ctx->debug = 4;
+    /* Timeout a bit bigger, by Jim Ramsay */
+    ctx->timeout = 10000;       //10 seconds
+    /* Kerberos authentication by Esben Nielsen */
+#if defined(SMB_CTX_FLAG_USE_KERBEROS) && defined(SMB_CTX_FLAG_FALLBACK_AFTER_KERBEROS)
+    ctx->flags |=
+        SMB_CTX_FLAG_USE_KERBEROS | SMB_CTX_FLAG_FALLBACK_AFTER_KERBEROS;
+#endif
+    //ctx->options.one_share_per_server = 1;
+    ctx = smbc_init_context(ctx);
+    return ctx;
+}
+
+SMBCCTX *fusesmb_new_context(const char * username, const char * password)
+{
+    fusesmb_auth_fn_username = username;
+	fusesmb_auth_fn_password = password;
+    return fusesmb_context(fusesmb_auth_fn);
+}
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
@@ -38,8 +84,6 @@
 #include <pthread.h>
 #include <libsmbclient.h>
 #include <time.h>
-#include "debug.h"
-#include "smbctx.h"
 
 #define MY_MAXPATHLEN (MAXPATHLEN + 256)
 
@@ -496,7 +540,6 @@ static int fusesmb_rename(const char *path, const char *new_path)
 
 static void *fusesmb_init()
 {
-    debug();
     return NULL;
 }
 
