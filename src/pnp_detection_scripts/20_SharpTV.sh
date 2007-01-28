c0f1936cb830c8b770efc7975f5b3fa6b428c79a
aaron <aaron.b@plutohome.com>
2007-01-28 03:46:34 +0000

    tweaks to myth

--- src/pnp_detection_scripts/20_SharpTV.sh
+++ src/pnp_detection_scripts/20_SharpTV.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Sharp TV Detection script queue $2"
-/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "POWR1   \r\s500mPOWR0   \r" -i 1 -s "OK\r"
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "POWR0   \r\s500mPOWR1   \r" -i 1 -s "OK\r"
 if [[ "$?" -ne 0 ]]; then
 echo "It's not a Sharp TV"
 /usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 0
