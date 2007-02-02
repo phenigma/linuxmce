d1c36fbe19059b7063207845b0161bbde0fc4103
rgavril <rgavril>
2007-02-02 14:50:08 +0000

    Stop running gexmls in postinst

--- src/Add_Software/init.d/getxmls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. /usr/pluto/bin/Config_Ops.sh
-
-if [[ "$OfflineMode" == "true" ]]; then
-	exit 0
-fi
-
-/usr/pluto/bin/getxmls && rm -f /etc/init.d/getxmls.sh
--- src/Add_Software/mkr_postinst.sh
+++ src/Add_Software/mkr_postinst.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-#if [[ -z "$2" ]]; then # 1st install
-#	/usr/pluto/bin/getxmls || :
-#fi
-
 ln -s /etc/init.d/instcheck.sh /etc/rc2.d/S23instcheck.sh || :
-ln -s /etc/init.d/getxmls.sh /etc/rc2.d/S22getxmls.sh || :
-
-if [[ ! -r /etc/diskless.conf ]] ;then
-	invoke-rc.d getxmls.sh start
-fi
