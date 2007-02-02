1d009abe903eadbb7c0bb00698eaf2680ba1a910
radu <radu.cx+linuxmce@gmail.com>
2007-02-02 10:36:57 +0000

    prefixed variables which come from parameters

--- src/UbuntuDiskless/files.d/filesd-parms.sh
+++ src/UbuntuDiskless/files.d/filesd-parms.sh
@@ -5,13 +5,13 @@
 for ((i = 1; i <= "$#"; i++)); do
 	Parm="${!i}"
 	case "$Parm" in
-		--root) ((i++)) RootLocation="${!i}" ;;
-		--device) ((i++)) Device="${!i}" ;;
+		--root) ((i++)) Parm_RootLocation="${!i}" ;;
+		--device) ((i++)) Parm_Device="${!i}" ;;
 		*) echo "ERR: Unknown parameter '$Parm'"; exit 1 ;;
 	esac
 done
 
-if [[ ! -d "$RootLocation" ]] ;then
-	echo "ERROR: Directory "$RootLocation" does not exist"
+if [[ ! -d "$Parm_RootLocation" ]] ;then
+	echo "ERROR: Directory "$Parm_RootLocation" does not exist"
 	exit 1
 fi
