64ecd4417ee3286f8a7349c8b48f1fe22e19630d
radu <radu.cx+linuxmce@gmail.com>
2007-02-06 15:08:06 +0000

    use tee-pluto instead of tee

--- src/pluto-nvidia-drivers/mkr_postinst.sh
+++ src/pluto-nvidia-drivers/mkr_postinst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /etc/X11
-bash -x /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee /var/log/pluto/Xconfigure.log
+bash -x /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee-pluto /var/log/pluto/Xconfigure.log
 for KVer in <-mkr_t_MakeRelease_Kernel->; do
 	depmod "$KVer" || :
 done
