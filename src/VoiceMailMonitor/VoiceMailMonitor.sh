4d83f05337d479de6f65bdbd0dc47b13a19c1b10
radu <radu.cx+linuxmce@gmail.com>
2007-02-06 15:08:30 +0000

    use tee-pluto instead of tee

--- src/VoiceMailMonitor/VoiceMailMonitor.sh
+++ src/VoiceMailMonitor/VoiceMailMonitor.sh
@@ -4,4 +4,4 @@ while :; do
 	/usr/pluto/bin/VoiceMailMonitor
 	echo "VoiceMail Monitor exited with code '$?'"
 	echo "---------------------------------------"
-done | tee -a /var/log/pluto/VoiceMailMonitor.log
+done | tee-pluto -a /var/log/pluto/VoiceMailMonitor.log
