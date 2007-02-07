75b5ea349abdd83622a2ad41ab69f6c9394045c0
radu <radu.cx+linuxmce@gmail.com>
2007-02-07 17:40:34 +0000

    prefixed function names with AptSrc_ ; added AddSource function ; separated demonstration code

--- src/AptUtils/AptSources.sh
+++ src/AptUtils/AptSources.sh
@@ -3,37 +3,56 @@
 # NOTE: This script doesn't check for validity of sources.list
 
 # TODO: use something faster than md5sum, but to the same effect: unique hash
-ParseSourcesList()
+AptSrc_AddSource()
+{
+	local Source="$*"
+	local type uri distribution components
+	local id
+
+	read type uri distribution components < <(echo "$Source")
+	if [[ -z "$type" ]]; then
+		return 1
+	fi
+
+	uri="${uri%/}/" # make sure URI ends with a slash
+	id=$(echo "$type $uri $distribution" | md5sum | cut -d' ' -f1)
+
+	if [[ "$AptSrc_id_list" != *" $id "* ]]; then
+		AptSrc_id_list="$AptSrc_id_list$id "
+		eval "AptSrc_base_${id}='$type $uri $distribution'"
+		eval "AptSrc_comp_${id}=' '" # AptSrc_comp_* has a leading space
+	fi
+	for comp in $components; do
+		Var="AptSrc_comp_${id}"
+		if [[ "${!Var}" == *" $comp "* ]]; then # AptSrc_comp_* is bound and separated by spaces
+			continue
+		fi
+		eval "AptSrc_comp_${id}=\"\${AptSrc_comp_${id}}$comp \"" # AptSrc_comp_* has a trailing space
+	done
+
+	return 0
+}
+
+AptSrc_ParseSourcesList()
 {
 	local SourcesList="${1:-/etc/apt/sources.list}"
-	local line type uri distribution components
+	local Source type uri distribution components
 	local id comp Var
 
 	AptSrc_id_list=' '
-	while read line; do
-		line="${line%%#*}" # remove comments
-		read type uri distribution components < <(echo "$line")
-		[[ -z "$type" ]] && continue # empty line
-		
-		uri="${uri%/}/" # make sure URI ends with a slash
-		id=$(echo "$type $uri $distribution" | md5sum | cut -d' ' -f1)
-		if [[ "$AptSrc_id_list" != *" $id "* ]]; then
-			AptSrc_id_list="$AptSrc_id_list$id "
-			eval "AptSrc_base_${id}='$type $uri $distribution'"
-			eval "AptSrc_comp_${id}=' '" # AptSrc_comp_* has a leading space
-		fi
-		for comp in $components; do
-			Var="AptSrc_comp_${id}"
-			if [[ "${!Var}" == *" $comp "* ]]; then # AptSrc_comp_* is bound and separated by spaces
-				continue
-			fi
-			eval "AptSrc_comp_${id}=\"\${AptSrc_comp_${id}}$comp \"" # AptSrc_comp_* has a trailing space
-		done
+	while read Source; do
+		Source="${Source%%#*}" # remove comments
+		AptSrc_AddSource "$Source" || :
 	done < "$SourcesList"
+	AptSrc_WasParsed=AptSrc_WasParsed
 }
 
-WriteSourcesList()
+AptSrc_WriteSourcesList()
 {
+	if [[ -z "$AptSrc_WasParsed" ]]; then
+		AptSrc_ParseSourcesList
+	fi
+
 	local VarBase VarComp
 	local id
 
@@ -44,5 +63,13 @@ WriteSourcesList()
 	done
 }
 
-ParseSourcesList "$1"
-WriteSourcesList
+if [[ -n "$AptSrc_Demo" ]]; then
+	AptSrc_ParseSourcesList "${1:-./sample-sources.list}"
+	AptSrc_WriteSourcesList
+	echo "-------"
+	AptSrc_AddSource "deb http://ftp.ro.debian.org/debian sarge main"
+	AptSrc_AddSource "deb http://ftp.ro.debian.org/debian etch main"
+	AptSrc_AddSource "deb http://ftp.ro.debian.org/debian sid main"
+	AptSrc_AddSource "deb http://ftp.ro.debian.org/debian sid non-free"
+	AptSrc_WriteSourcesList
+fi
