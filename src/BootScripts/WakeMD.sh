56682ccc22c8dd2cbf6826de9e368e987fbd9c10
radu <radu.cx+linuxmce@gmail.com>
2007-02-12 17:37:16 +0000

    wake MD code

--- src/BootScripts/WakeMD.sh
+++ src/BootScripts/WakeMD.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+. /usr/pluto/bin/Network_Parameters.sh
+HostMAC="$1"
+
+if [[ -z "$HostMAC" ]]; then
+	echo "Usage: WakeMD.sh <MAC>"
+	exit 1
+fi
+
+/usr/sbin/etherwake -b -i "$IntIf" "$HostMAC"
