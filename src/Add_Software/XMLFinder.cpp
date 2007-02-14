10368bf16c1a55ae2637cf134c6e78dd35a76fc2
cmiron <cmiron>
2007-02-14 13:13:41 +0000

    Added module XMLFinder to getxmls

---
+++ src/Add_Software/XMLFinder.cpp
@@ -0,0 +1,49 @@
+#include "XMLFinder.h"
+
+#include "../PlutoUtils/FileUtils.h"
+#include "../DCE/Logger.h"
+
+using namespace std;
+using namespace DCE;
+//--------------------------------------------------------------------------------------------------
+string XMLFinder::FindURLToXML(string sFilename)
+{
+	const string csPackagesToken = "packages.xml";
+	const string csHrefToken = "<a href=\"";
+	const int cnNeighbourhood = 100;
+
+	string sHTMLData;
+	if(FileUtils::ReadTextFile(sFilename, sHTMLData))
+	{
+		size_t pos = sHTMLData.find(csPackagesToken);
+		string sUsefullData = sHTMLData.substr(pos > cnNeighbourhood ? pos - cnNeighbourhood : 0, 
+			cnNeighbourhood + csPackagesToken.length());
+
+		string sUrlRawData =  sUsefullData.substr(
+			sUsefullData.find(csHrefToken) + csHrefToken.length(), 
+			sUsefullData.find(csPackagesToken) + csPackagesToken.length());
+
+		return sUrlRawData;
+	}
+
+	return "";
+}
+
+//--------------------------------------------------------------------------------------------------
+#ifdef UNIT_TEST_XML_FINDER
+
+namespace DCE
+{
+	Logger *g_pPlutoLogger;
+};
+
+int main()
+{
+	g_pPlutoLogger = new FileLogger(stdout);
+
+	string sURLToXML = XMLFinder::FindURLToXML("/temp/search.html");
+	g_pPlutoLogger->Write(LV_WARNING, "Found xml : %s", sURLToXML.c_str());
+
+	return 0;
+}
+#endif
---
+++ src/Add_Software/XMLFinder.h
@@ -0,0 +1,13 @@
+#ifndef __XML_FINDER_H__
+#define __XML_FINDER_H__ 
+
+#include <string>
+//--------------------------------------------------------------------------------------------------
+class XMLFinder
+{
+public:
+
+	static std::string FindURLToXML(std::string sFilename);
+};
+//--------------------------------------------------------------------------------------------------
+#endif //__XML_FINDER_H__
