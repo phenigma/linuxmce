c9f4f4135632846aa76effd16055612057823f9f
alex.c <alex.c>
2007-02-14 10:45:40 +0000

    Tribune update

--- src/Tribune/MapManagement.h
+++ src/Tribune/MapManagement.h
@@ -1,19 +1,25 @@
 #ifndef MapManagement_h
 #define MapManagement_h
 
-#include <map>
+#include <iostream>
+#include <fstream>
 #include <string>
+#include <map>
+
+using namespace std;
 
 class MapManagement{
 	
 public:
 	MapManagement();
-	static map<string,string> GetProgramRecordMap( );
-	static map<int,string> GetStationMap( );
-	static map<string,string> GetScheduleMap( );
-	static map<string,string> GetActorMap( );
-	static map<string,string> GetGenreMap( );
-	static map<string,string> GetRoleMap( );
-}
+	static map <string,string> lista;
+	static map <string,string> GetProgramRecordMap( );
+	static map <int,string> GetStationMap( );
+	static map <string,string> GetScheduleMap( );
+	static map <string,string> GetActorMap( );
+	static map <string,string> GetGenreMap( );
+	static map <string,string> GetRoleMap( );
+
+};
 
 #endif
