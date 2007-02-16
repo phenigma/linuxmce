161865323d835ef6f2bdc464a58f8e9ffb8cecfb
rgavril <rgavril>
2007-02-16 18:30:07 +0000

    Script to build a self extractig tar.gz

---
+++ src/mce-installer/build-mce-installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pushd src
+tar -czf mce-installer.tar.gz mce-installer mce-installer.glade mce-installer-core.sh mce-installer-diskedmd.sh pluto.jpg
+cat ../mce-installer-header.sh mce-installer.tar.gz > ../mce-installer.sh
+chmod +x ../mce-installer.sh
+rm mce-installer.tar.gz
+popd
+
