1eaff534d7b037d1c0031e36838c78246dbb8e04
rgavril <rgavril>
2007-02-16 17:18:59 +0000

    Make sure to remove that restriction (ubuntu only)

--- src/pluto-nvidia-drivers/mkr_postinst.sh
+++ src/pluto-nvidia-drivers/mkr_postinst.sh
@@ -2,6 +2,7 @@
 
 mkdir -p /etc/X11
 bash -x /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee-pluto /var/log/pluto/Xconfigure.log
+rm -f /etc/modprobe.d/lrm-video || :
 for KVer in <-mkr_t_MakeRelease_Kernel->; do
 	depmod "$KVer" || :
 done
