571118e5a2ec11a091f0b195d6b530cfc34f6195
cmiron <cmiron>
2007-02-19 07:43:49 +0000

    Designer missing files.

---
+++ src/designer/HADesigner/SkinLanguageStatus.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace HADesigner
+{
+	/// <summary>
+	/// Summary description for SkinLanguageStatus.
+	/// </summary>
+	public class SkinLanguageStatus
+	{
+		int m_nTextSkinID = -1;
+		int m_nTextLanguageID = -1;
+		int m_nDesignObjSkinID = -1;
+		int m_nDesignObjLanguageID = -1;
+		static private SkinLanguageStatus m_Instance = null;
+
+		public int TextSkinID
+		{
+			get	{return m_nTextSkinID;}
+			set	{m_nTextSkinID = value;}
+		}
+		
+		public int TextLanguageID
+		{
+			get	{return m_nTextLanguageID;}
+			set	{m_nTextLanguageID = value;}
+		}
+
+		public int DesignObjSkinID
+		{
+			get	{return m_nDesignObjSkinID;}
+			set	{m_nDesignObjSkinID = value;}
+		}
+
+		public int DesignObjLanguageID
+		{
+			get	{return m_nDesignObjLanguageID;}
+			set	{m_nDesignObjLanguageID = value;}
+		}
+
+		static public SkinLanguageStatus Instance()
+		{
+			if(null == m_Instance)
+				m_Instance = new SkinLanguageStatus();
+
+			return m_Instance;
+		}
+
+		private SkinLanguageStatus()
+		{
+		}
+	}
+}
