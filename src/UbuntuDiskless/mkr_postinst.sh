f5678d5f42a0a8910c26a6f76e6b69aee48cde6d
rgavril <rgavril>
2007-02-23 16:28:31 +0000

    Set correct hostname for diskless devices & run Diskless_CreateTBZ at boot

--- src/UbuntuDiskless/mkr_postinst.sh
+++ src/UbuntuDiskless/mkr_postinst.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /usr/pluto/bin/Diskless_BuildDefaultImage.sh || echo "WARNING: Diskless_BuildDefaultImage.sh returned an error!"
+/usr/pluto/bin/Diskless_CreateTBZ.sh || echo "WARNING: Diskless_CreateTBZ.sh returned an error!"
