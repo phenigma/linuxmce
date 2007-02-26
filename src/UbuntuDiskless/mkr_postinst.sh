778352a382fe48d854931fc82acb1b465dcb34d4
rgavril <rgavril>
2007-02-26 10:32:15 +0000

    Don't run CreateTBZ when we first install this package

--- src/UbuntuDiskless/mkr_postinst.sh
+++ src/UbuntuDiskless/mkr_postinst.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 /usr/pluto/bin/Diskless_BuildDefaultImage.sh || echo "WARNING: Diskless_BuildDefaultImage.sh returned an error!"
-/usr/pluto/bin/Diskless_CreateTBZ.sh || echo "WARNING: Diskless_CreateTBZ.sh returned an error!"
+
+# summary of how this script can be called:
+#        * <postinst> `configure' <most-recently-configured-version>
+if [[ "$2" != "" ]] ;then
+	/usr/pluto/bin/Diskless_CreateTBZ.sh || echo "WARNING: Diskless_CreateTBZ.sh returned an error!"
+fi
