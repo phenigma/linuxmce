67c62edf109c9fda7d1c443d78c828a00d930d7f
radu <radu.cx+linuxmce@gmail.com>
2007-02-26 11:30:32 +0000

    parallel 'queue', trying to send each message until succeeding

--- src/Dhcpd-Plugin/Dhcpd-Plugin-Queue.sh
+++ src/Dhcpd-Plugin/Dhcpd-Plugin-Queue.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-while read mac ip garbage; do
-	QueueNew=()
+. /usr/pluto/bin/Config_Ops.sh
 
-	for Pair in "${Queue[@]}"; do
-		read Qmac Qip < <(echo "$Pair")
-		if ! /usr/pluto/bin/MessageSend $DCERouter $PK_Device -1001 2 65 52 3 53 2 5 "$Qmac" 28 "$Qip"; then
-			QueueNew=("${QueueNew[@]}" "$Pair")
-		fi
+RetrySend()
+{
+	local Mac="$1" IP="$2"
+	while ! /usr/pluto/bin/MessageSend "$DCERouter" "$PK_Device" -1001 2 65 52 3 53 2 5 "$MAC" 28 "$IP"; do
+		sleep 1
 	done
-	
-	Queue=("${QueueNew[@]}")
+}
 
-	if ! /usr/pluto/bin/MessageSend $DCERouter $PK_Device -1001 2 65 52 3 53 2 5 "$mac" 28 "$ip"; then
-		Queue=(${Queue[@]} "$mac $ip")
-	fi
+while read mac ip garbage; do
+	RetrySend "$mac" "$ip" &
 done
