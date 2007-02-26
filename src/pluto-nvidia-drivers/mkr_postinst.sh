992cc153b9b8ad1aef5f81d6c1829628404d4b3f
rgavril <rgavril>
2007-02-26 11:56:57 +0000

    Force users to use vesa it they have nvidia/nv to bypass a nvidia bug

--- src/pluto-nvidia-drivers/mkr_postinst.sh
+++ src/pluto-nvidia-drivers/mkr_postinst.sh
@@ -3,7 +3,8 @@
 mkdir -p /etc/X11
 bash -x /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee-pluto /var/log/pluto/Xconfigure.log
 rm -f /etc/modprobe.d/lrm-video || :
-sed -i 's/"nv"/"nvidia"/g' /etc/X11/xorg.conf || :
+sed -i 's/"nv"/"vesa"/g' /etc/X11/xorg.conf || :
+sed -i 's/"nvidia"/"vesa"/g' /etc/X11/xorg.conf || :
 for KVer in <-mkr_t_MakeRelease_Kernel->; do
 	depmod "$KVer" || :
 done
