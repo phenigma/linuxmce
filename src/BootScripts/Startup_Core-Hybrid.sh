94d6b9677b5b167205e643e844d9ea437294a0a0
rgavril <rgavril>
2007-02-26 20:15:21 +0000

    Commiting event.d startup script to start core/hybrid

---
+++ src/BootScripts/Startup_Core-Hybrid.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+X_Is_Ok=true
+
+## Test to see if X exists
+if [[ ! -x /usr/bin/xinit ]] ;then
+	X_Is_Ok=false
+fi
+
+if [[ "$X_Is_Ok" == "true" ]] ;then
+	X_Start_Time=$(date +%s)
+	xinit /usr/bin/mce-launcher -- :3 -config /etc/X11/xorg.conf.pluto -br
+	X_Stop_Time=$(date +%s)
+
+	## If X exited to quick
+	if [[ $(( $X_Stop_Time - $X_Start_Time )) -lt 4 ]] ;then
+		X_Is_Ok=false
+	fi
+fi
+
+## If X cannot start, run the script in console
+if [[ "$X_Is_Ok" == "false" ]] ;then
+	/usr/share/mce-launcher/scripts/mce-launcher-core.sh
+fi
+
+
