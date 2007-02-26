296fbd9a400e0b3913988fed8a692fbab6a97dda
rgavril <rgavril>
2007-02-26 13:18:46 +0000

    Change default installation type to ISO

--- src/mce-installer/src/main.c
+++ src/mce-installer/src/main.c
@@ -105,6 +105,7 @@ main (int argc, char *argv[])
 	setting_runDhcpServer = TRUE;
 	setting_netExtKeep = TRUE;
 	setting_netIntIPN = "192.168.80";
+	setting_installType = INSTALL_TYPE_ISO;
 
 	history = g_queue_new();
 
