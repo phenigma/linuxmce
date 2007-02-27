1bcb7d8b60c47883fc533d1ce788bb7b0c276d45
vali <vali>
2007-02-27 11:52:36 +0000

    Removed debug lines from config file; used an "include if exist" clause to keep debug settings outside svn.

--- web/pluto-admin/include/config/config.inc.php
+++ web/pluto-admin/include/config/config.inc.php
@@ -1,61 +1,34 @@
 <?PHP
-  	//debuging
-  	$GLOBALS['inDebug']=0;
   	
   	// todo: set lang when translation is finished
   	$GLOBALS['lang']='en';
   	$_SESSION['skin']=0;
   	
-  	if($GLOBALS['inDebug']!=1){
-  		// production settings
-  		error_reporting(E_ALL ^ E_NOTICE);
-  		$GLOBALS['globalConfigPath']='/var/www/globalconfig/';
-  		$GLOBALS['floorplansPath']='/var/www/pluto-admin/floorplans';
-  		$GLOBALS['usersPicsPath']='/var/www/pluto-admin/users/';
-  		$GLOBALS['roomsPicsPath']='/var/www/pluto-admin/rooms/';
-  		$GLOBALS['scenariosPicsPath']='/var/www/pluto-admin/scenarios/';
-  		$GLOBALS['pluto.conf']='/etc/pluto.conf';
-  		$GLOBALS['wap.conf']='/etc/wap.conf';
-  		$GLOBALS['pluto_callerid.conf']='/etc/pluto-callerid.conf';
-  		$GLOBALS['ErrorLog']='/var/log/pluto/pluto-admin.log';
-  		$GLOBALS['databasesArray']=array('pluto_main_devel'=>'pluto_main','pluto_myth_devel'=>'pluto_myth');
-  		
-  		// display loading time at the bottom of the page
-  		$GLOBALS['display_loading_time']=0;
-  		
-  		// set it to path with write permission to enable loading time logging orr leave blank to disable
-  		$GLOBALS['save_loading_time']='/var/www/pluto-admin/cached/';
-  		
-  		// log file for external calls
-  		$GLOBALS['WebExecLogFile']='/var/log/pluto/webExecLog.log';
-  	}else{
-  		error_reporting(E_ALL);
-  		$GLOBALS['globalConfigPath']='/home/users/vali/work/web/globalconfig/';
-  		$GLOBALS['floorplansPath']='/home/users/vali/work/web/pluto-admin/floorplans/';
-  		$GLOBALS['usersPicsPath']='/home/users/vali/work/web/pluto-admin/users/';
-  		$GLOBALS['roomsPicsPath']='/home/users/vali/work/web/pluto-admin/rooms/';
-  		$GLOBALS['scenariosPicsPath']='/home/users/vali/work/web/pluto-admin/scenarios/';
-  		$GLOBALS['pluto.conf']='/home/users/vali/work/web/pluto.conf';
-  		$GLOBALS['wap.conf']='/home/users/vali/work/web/wap.conf';
-  		$GLOBALS['pluto_callerid.conf']='/home/users/vali/work/web/pluto-callerid.conf';
-  		$GLOBALS['ErrorLog']='/var/log/pluto/pluto-admin.log';
-  		$GLOBALS['databasesArray']=array('pluto_main'=>'pluto_main','pluto_myth'=>'pluto_myth');
-  		
-  		// display loading time at the bottom of the page
-  		$GLOBALS['display_loading_time']=1;
-  		
-  		// set it to path with write permission to enable loading time logging orr leave blank to disable
-  		$GLOBALS['save_loading_time']='/home/users/vali/work/web/pluto-admin/cached/';
+  	error_reporting(E_ALL ^ E_NOTICE);
+  	$GLOBALS['globalConfigPath']='/var/www/globalconfig/';
+  	$GLOBALS['floorplansPath']='/var/www/pluto-admin/floorplans';
+  	$GLOBALS['usersPicsPath']='/var/www/pluto-admin/users/';
+  	$GLOBALS['roomsPicsPath']='/var/www/pluto-admin/rooms/';
+  	$GLOBALS['scenariosPicsPath']='/var/www/pluto-admin/scenarios/';
+  	$GLOBALS['pluto.conf']='/etc/pluto.conf';
+  	$GLOBALS['wap.conf']='/etc/wap.conf';
+  	$GLOBALS['pluto_callerid.conf']='/etc/pluto-callerid.conf';
+  	$GLOBALS['ErrorLog']='/var/log/pluto/pluto-admin.log';
+  	$GLOBALS['databasesArray']=array('pluto_main_devel'=>'pluto_main','pluto_myth_devel'=>'pluto_myth');
 
-  		// log file for external calls
-  		$GLOBALS['WebExecLogFile']='/var/log/pluto/webExecLog.log';
+  	// display loading time at the bottom of the page
+  	$GLOBALS['display_loading_time']=0;
 
-  		// todo: remove
-  		// setting get get parameter &red=1 set css style to red stylesheet and preserve it, set to 0 to return to pluto
-  		if(isset($_REQUEST['red'])){
-  			$_SESSION['skin']=(int)$_REQUEST['red'];
-  		}
-  	}
+  	// set it to path with write permission to enable loading time logging orr leave blank to disable
+  	$GLOBALS['save_loading_time']='/var/www/pluto-admin/cached/';
+
+  	// log file for external calls
+  	$GLOBALS['WebExecLogFile']='/var/log/pluto/webExecLog.log';
+
+  	// if debug config exist, use it to override svn settings
+	if(file_exists('include/config/config.debug.inc.php')){
+		include('include/config/config.debug.inc.php');
+	}
 
   	if($_SESSION['skin']==0){
 		$_SESSION['cssFile']='include/styles/main.css';
@@ -91,7 +64,7 @@
 	
 	
 	setlocale(LC_ALL, LANG);
-	
+
 	//database
 	require_once("database.inc.php");
 	
