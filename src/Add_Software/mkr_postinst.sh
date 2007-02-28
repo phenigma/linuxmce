d881536daae8e0ca1884027a32fb6819c7e16d92
radu <radu.cx+linuxmce@gmail.com>
2007-02-28 20:33:23 +0000

    also run on upgrade (Eugen's request)

--- src/Add_Software/mkr_postinst.sh
+++ src/Add_Software/mkr_postinst.sh
@@ -2,6 +2,6 @@
 
 ln -s /etc/init.d/instcheck.sh /etc/rc2.d/S23instcheck.sh || :
 
-if [[ ! -f /etc/diskless.conf && -z "$2" ]] ;then
+if [[ ! -f /etc/diskless.conf ]] ;then
 	/usr/pluto/bin/getxmls &>/dev/null </dev/null &
 fi											
