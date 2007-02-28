08255edde048bcda24a665733cffdd938fa1dd99
alex.c <alex.c>
2007-02-28 12:28:24 +0000

    run getxml after install

--- src/Add_Software/mkr_postinst.sh
+++ src/Add_Software/mkr_postinst.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ln -s /etc/init.d/instcheck.sh /etc/rc2.d/S23instcheck.sh || :
+
+if [[ ! -f /etc/diskless.conf && -z "$2" ]] ;then
+	/usr/pluto/bin/getxmls &>/dev/null </dev/null &
+fi											
