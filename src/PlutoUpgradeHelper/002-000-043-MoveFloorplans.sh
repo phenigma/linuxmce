bccc1959b670d782e51a8a257d48871395bee527
radu <radu.cx+linuxmce@gmail.com>
2007-03-01 22:41:36 +0000

    make sure any stray directory where the symlink should be is treated as it should

--- src/PlutoUpgradeHelper/002-000-043-MoveFloorplans.sh
+++ src/PlutoUpgradeHelper/002-000-043-MoveFloorplans.sh
@@ -5,6 +5,10 @@ if [[ -f /etc/diskless.conf ]] ;then
 fi
 
 if [[ ! -L /usr/pluto/orbiter/floorplans && -d /usr/pluto/orbiter/floorplans ]]; then
-	mv /usr/pluto/orbiter/floorplans /home/pluto/
+	if [[ ! -d /home/pluto/floorplans ]]; then
+		mkdir -p /home/pluto/floorplans
+	fi
+	mv /usr/pluto/orbiter/floorplans/* /home/pluto/floorplans/
+	rm -rf /usr/pluto/orbiter/floorplans
 	ln -sn /home/pluto/floorplans /usr/pluto/orbiter/floorplans
 fi
