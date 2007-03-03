023bf29ab41935b1212bf8f3ba8ffa970540ee53
radu <radu.cx+linuxmce@gmail.com>
2007-03-03 00:25:16 +0000

    restore timezone

--- src/ConfirmDependencies_Script_Offline/Initial_Config_Core.sh
+++ src/ConfirmDependencies_Script_Offline/Initial_Config_Core.sh
@@ -216,6 +216,8 @@ echo "$PlutoConf" >/etc/pluto.conf
 
 if [[ "$UpgradeMode" == "true" ]];then
 	cp /.backup/pluto.conf /etc/pluto.conf
+	cp /.backup/localtime /etc/
+	cp /.backup/timezone /etc/
 	mkdir -p /var/lib/mysql
 	mv /.backup/mysql/* /var/lib/mysql
 fi
