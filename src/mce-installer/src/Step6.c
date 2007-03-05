e078a254953b471495dcb81f42cc69256ff8dae6
rgavril <rgavril>
2007-03-05 21:16:17 +0000

    This should have been in svn from friday now .. :(

---
+++ src/mce-installer/src/Step6.c
@@ -0,0 +1,40 @@
+/*
+
+     This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+     See the GNU General Public License for more details.
+
+*/
+#include "common.h"
+
+
+GtkWidget *buttonFinish;
+
+void on_Step6_finish_clicked(GtkWidget *widget, gpointer data) {
+	gtk_main_quit();
+}
+
+
+void displayStep6(void) {
+	printf("Step6\n");
+
+	// Cleanup window
+	cleanupContainer(mainBox);
+	cleanupContainer(mainButtonBox);
+
+	// Wizard text
+	GtkWidget *label = gtk_label_new_for_wizard ("LinuxMCE Finished installing, you need to reboot you computer before starting the pluto services.");
+	gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
+	gtk_box_pack_start(GTK_BOX(mainBox), label, TRUE, TRUE, 0);
+	
+	// Button Finish
+	buttonFinish = gtk_button_new_from_stock(GTK_STOCK_CLOSE);
+	gtk_container_add(GTK_CONTAINER(mainButtonBox), buttonFinish);
+	g_signal_connect(G_OBJECT(buttonFinish), "clicked", G_CALLBACK(on_Step6_finish_clicked), NULL);
+
+	// Redraw window	
+	gtk_widget_show_all(mainBox);
+	gtk_widget_show_all(mainButtonBox);
+}
---
+++ src/mce-installer/src/mce-installer-getnetdata.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## Try to figure out what is the external network interface 
+## by looking at the defaul router interface
+netExtName=$(route -n | grep "^0.0.0.0" | tail -1 | awk '{print $8}')
+
+## If the external interface is still null, nada, niente, nimic
+## just use the first one in the list
+if [[ "$netExtName" == "" ]] ;then
+	netExtName=$(ifconfig -a | grep "^eth" | awk '{ print $1 }' | head -1)
+fi
+
+## If we didn't got any external netwoking interface thill now, we might well quit
+## Since is nothing that we can do
+if [[ "$netExtName" == "" ]] ;then
+	echo ""
+	echo ""
+	exit 0
+fi
+
+## If we found what the internal network interface is, we should see if we cat get
+## a internal one too
+netIntName=$(ifconfig -a | grep "^eth" | grep -v "^${netExtName}" | awk '{print $1}' | head -1)
+
+## If we didn't get one going, we'll use an alias for the external one
+if [[ "$netIntName" == "" ]] ;then
+	netIntName="${netExtName}:0"
+fi
+
+netExtIP=$(ifconfig $netExtName | grep "inet addr" | sed 's/.*inet addr:\([0-9.]*\).*Bcast.*/\1/g')
+netExtMask=$(ifconfig $netExtName | grep "inet addr" | sed 's/.*Mask:\([0-9.]*\).*$/\1/g')
+netExtGate=$(route -n | grep "^0.0.0.0" | tail -1 | awk '{print $2}')
+netExtDNS1=$(cat /etc/resolv.conf | grep "^nameserver" | head -1 | awk '{print $2}')
+netExtDNS2=$(cat /etc/resolv.conf | grep "^nameserver" | tail -1 | awk '{print $2}')
+
+netInternetWorking=0
+nc -z www.google.com 80 2>/dev/null && netInternetWorking=1
+
+echo "$netExtName|$netExtIP|$netExtMask|$netExtGate|$netExtDNS1|$netExtDNS2|$netInternetWorking"
+echo "$netIntName"
