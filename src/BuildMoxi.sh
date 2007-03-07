3b5ce1c6e8cd9b6eccfcb4ed3c53fcf822079b3c
rgavril <rgavril>
2007-03-07 18:03:19 +0000

    Added PlutoUtils to Moxi build

--- src/BuildMoxi.sh
+++ src/BuildMoxi.sh
@@ -14,6 +14,12 @@ make -f Makefile.Moxi clean
 make -f Makefile.Moxi
 popd
 
+## PlutoUtils
+pushd PlutoUtils
+make -f Makefile.Moxi clean
+make -f Makefile.Moxi
+popd
+
 ## MessageSend
 pushd MessageSend
 make -f Makefile.Moxi clean
