6b697c1fb6f86776ff96fb10fb7227dce984d914
rgavril <rgavril>
2007-03-12 22:09:53 +0000

    Add network detection scritp to the self extracting archive

--- src/mce-installer/build-mce-installer.sh
+++ src/mce-installer/build-mce-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd src
-tar -czf mce-installer.tar.gz mce-installer mce-installer.glade mce-installer-core.sh mce-installer-preseed.sh mce-installer-diskedmd.sh wget-wrapper.sh pluto.png
+tar -czf mce-installer.tar.gz mce-installer mce-installer.glade mce-installer-core.sh mce-installer-preseed.sh mce-installer-diskedmd.sh wget-wrapper.sh pluto.png mce-installer-getnetdata.sh
 cat ../mce-installer-header.sh mce-installer.tar.gz > ../mce-installer.sh
 chmod +x ../mce-installer.sh
 rm mce-installer.tar.gz
