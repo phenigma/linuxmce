6bb0d1a42c051a46030354a381ca0e24aa59fe51
rgavril <rgavril>
2007-03-13 05:03:34 +0000

    Somebody forgot to do his work, a big thank you

--- src/mce-installer/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mce-installer package
-#
-0 4	* * *	root	mce-installer_maintenance
--- src/mce-installer/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mce-installer):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="mce-installer" command="/usr/bin/mce-installer"
