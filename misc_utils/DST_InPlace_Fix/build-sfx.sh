40410e638652c8852b1b4a709d6f576712a8df19
radu <radu.cx+linuxmce@gmail.com>
2007-03-15 18:58:47 +0000

    DST fix for North America, hack

---
+++ misc_utils/DST_InPlace_Fix/build-sfx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat self_extract_header.sh tzdata2007c.tar.gz >FixDST.sh
---
+++ misc_utils/DST_InPlace_Fix/self_extract_header.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo ""
+echo "Pluto fix for North America DST"
+echo ""
+
+# create a temp directory to extract to.
+export WRKDIR=`mktemp -d /tmp/selfextract.XXXXXX`
+
+SKIP=$(awk '/^__ARCHIVE_FOLLOWS__/ { print NR + 1; exit 0; }' "$0")
+
+# Take the TGZ portion of this file and pipe it to tar.
+tail -n +"$SKIP" "$0" | tar xz -C "$WRKDIR"
+
+# apply the fix
+pushd "$WRKDIR" >/dev/null
+zic -d zoneinfo northamerica
+cd zoneinfo
+cp -r * /usr/share/zoneinfo/
+
+popd >/dev/null
+
+# delete the temp files
+rm -rf $WRKDIR
+
+exit 0
+
+__ARCHIVE_FOLLOWS__
