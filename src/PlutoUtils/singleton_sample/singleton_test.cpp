7534cd333cac79afac4d3baa25b14bae4d2d016c
cmiron <cmiron>
2007-03-18 21:45:42 +0000

    Singleton sample class (with thread safe access to the instance, using double check pattern). NOTE: Don't use it in our low level mutex tracking class, since it's using it.

---
+++ src/PlutoUtils/singleton_sample/singleton_test.cpp
@@ -0,0 +1,46 @@
+// singleton_test.cpp : Defines the entry point for the console application.
+//
+#include "../singleton.h"
+#include "../threads.h"
+#include <iostream>
+
+using namespace cpp;
+using namespace cpp::Threading;
+using namespace std;
+
+class MyClassImpl : public Singleton<MyClassImpl>
+{
+public:
+	void DoSomethingFromMain() { cout << "MainThread: I'm doin' something" << endl; }
+	void DoSomethingFromThread() { cout << "Thread: I'm doin' something" << endl; }
+};
+
+typedef Singleton<MyClassImpl, CreateUsingNew<MyClassImpl>, DefaultLifetime, MultiThreaded> MyClass; //not safe threaded
+
+void *MyThread(void *)
+{
+	int i = 1000;
+	while(i-- > 0)
+	{
+		MyClass::Instance().DoSomethingFromThread();
+	}
+
+	return NULL;
+}
+
+int main(int argc, char* argv[])
+{
+	pthread_t id;
+	pthread_create(&id, NULL, MyThread, NULL);
+	
+	int i = 1000;
+	while(i-- > 0)
+	{
+		MyClass::Instance().DoSomethingFromMain();
+	}
+
+	pthread_join(id, NULL);
+
+	return 0;
+}
+
