d1ebac514967739cb0aecc2c020ad5b5078d2ecc
rgavril <rgavril>
2007-03-19 23:00:46 +0000

    Build script

---
+++ src/Ubuntu_Helpers/BuildUbuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. /root/Ubuntu_Helpers/BuildUbuntu_Functions.sh
+
+#Install_Build_Needed_Packages
+Create_Diskless_Archive
+Import_Pluto_Skins
+Checkout_Pluto_Svn #branches/2.0.0.44
+Build_Pluto_Replacements
+Build_MakeRelease_Binary
+Import_Build_Database
+Create_Fake_Windows_Binaries
+Build_Pluto_Stuff
+Create_Local_Repository
+Create_ISO
+Upload_Build_Archive
+
+echo "Finished"
+date -R
+read
+
