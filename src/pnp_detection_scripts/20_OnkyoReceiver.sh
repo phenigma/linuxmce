ee31834182c4efefe6deccf53bfcf7deee943d72
aaron <aaron.b@plutohome.com>
2007-03-29 02:25:25 +0000

    add onkyo/pioneer pnp

---
+++ src/pnp_detection_scripts/20_OnkyoReceiver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Onkyo/Integra Detection script queue $2"
+
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "!1PWRQSTN\r" -i 1 -s "PWR0"
+if [[ "$?" -ne 0 ]]; then
+echo "It's not a Onkyo Receiver"
+/usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 0
+else
+echo "It is a Onkyo Receiver"
+/usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 1883
+fi
--- src/pnp_detection_scripts/99_PioneerPlasma.sh
+++ src/pnp_detection_scripts/99_PioneerPlasma.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Pioneer Plasma script queue $2"
-/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 4800 -t "\0201AMN\03" -i 1 -s "XXX" -s "AMN"
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 4800 -t "\0201AMN\03" -i 1 -s "\02XXX" -s "\02AMN"
+#don't search for something we send because anything that echoes will be a match
 if [[ "$?" -ne 0 ]]; then
 echo "It's not a Pioneer Plasma"
 /usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 0
