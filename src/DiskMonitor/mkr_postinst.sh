358ec7cb8a00b2f60efdd482ad68bdd6ce9c68f5
alex.c <alex.c>
2007-04-05 10:16:17 +0000

    modifications related to crontab

--- src/DiskMonitor/mkr_postinst.sh
+++ src/DiskMonitor/mkr_postinst.sh
@@ -3,8 +3,13 @@
 
 ## Add a cron entry
 cronEntry="*/3 * * * * root /usr/pluto/bin/DiskSpaceMonitor.sh"
-if ! grep -qF '/usr/pluto/bin/DiskSpaceMonitor.sh' /etc/crontab; then
-        echo "$cronEntry" >>/etc/crontab
-        invoke-rc.d cron reload
-fi
+#if ! grep -qF '/usr/pluto/bin/DiskSpaceMonitor.sh' /etc/crontab; then
+#        echo "$cronEntry" >>/etc/crontab
+#        invoke-rc.d cron reload
+#fi
 
+if [[ ! -e /etc/cron.d/DiskSpaceMonitor ]] ;then
+	echo "$cronEntry" >>/etc/cron.d/DiskSpaceMonitor
+	invoke-rc.d cron reload
+fi
+		
--- src/DiskMonitor/mkr_postrm.sh
+++ src/DiskMonitor/mkr_postrm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## Remove the cron entry
-cat  /etc/crontab  | grep -v "DiskSpaceMonitor.sh" > /etc/crontab.$$
-mv /etc/crontab.$$ /etc/crontab
+#cat  /etc/crontab  | grep -v "DiskSpaceMonitor.sh" > /etc/crontab.$$
+#mv /etc/crontab.$$ /etc/crontab
+rm -f /etc/cron.d/DiskSpaceMonitor
 
