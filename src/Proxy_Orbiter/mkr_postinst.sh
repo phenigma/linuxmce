47db360353c7b2da36cba17d29fa0eb251250941
alex.c <alex.c>
2007-04-05 10:40:09 +0000

    backup conf file before changing it

--- src/Proxy_Orbiter/mkr_postinst.sh
+++ src/Proxy_Orbiter/mkr_postinst.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
 
+cp /etc/bluetooth/hcid.conf /etc/bluetooth/hcid.conf.pbackup
 sed -i 's!\<pin_helper.*$!pin_helper /usr/pluto/bin/pluto-pinhelper.sh;!' /etc/bluetooth/hcid.conf
 invoke-rc.d bluez-utils restart
