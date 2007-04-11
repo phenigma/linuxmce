d74cf73c0803f55956be27f1dcc0e9f0878f6153
cmiron <cmiron>
2007-04-11 10:37:24 +0000

    UIdiag will be used to identify if a system will perform well with UI2 alpha blending or UI2 with masking.

---
+++ src/utilities/linux/UIdiag/UIdiag.c
@@ -0,0 +1,519 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#ifdef WIN32
+#include "stdafx.h"
+#include <windows.h>
+#endif
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#ifndef WIN32
+#include <SDL/SDL_syswm.h>
+#include <X11/extensions/XTest.h>
+#include <X11/extensions/shape.h>
+#endif
+
+#ifdef WIN32
+#include "GL/glext.h"
+#else
+#include <GL/glx.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <fstream>
+#include <string>
+using namespace std;
+
+/* screen width, height, and bit depth */
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+#define SCREEN_BPP     32
+
+/* Set up some booleans */
+#define TRUE  1
+#define FALSE 0
+
+/* This is our SDL surface */
+SDL_Surface *surface;
+
+GLfloat xrot; /* X Rotation ( NEW ) */
+GLfloat yrot; /* Y Rotation ( NEW ) */
+GLfloat zrot; /* Z Rotation ( NEW ) */
+
+GLuint texture[1]; /* Storage For One Texture ( NEW ) */
+
+string sTextureName;
+bool bUseComposite = false;
+bool bUseMask = false;
+
+float MaxU = 1.0f;
+float MaxV = 1.0f;
+
+/* function to release/destroy our resources and restoring the old desktop */
+void Quit( int returnCode )
+{
+	/* clean up the window */
+	SDL_Quit( );
+
+	/* and exit appropriately */
+	exit( returnCode );
+}
+
+int MinPowerOf2(int Value)
+{
+	int Result = 1;
+	while(Result<Value)
+		Result*= 2;
+	return Result;	
+}
+
+/* function to load in bitmap as a GL texture */
+int LoadGLTextures( )
+{
+	/* Create storage space for the texture */
+	SDL_Surface *TextureImage[1]; 
+
+	/* Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit */
+	if ( ( TextureImage[0] = IMG_Load(sTextureName.c_str())))
+	{
+		int Width = MinPowerOf2(TextureImage[0]->w);
+		int Height = MinPowerOf2(TextureImage[0]->h);
+
+		if(Width != TextureImage[0]->w || Height != TextureImage[0]->h)
+		{
+			Uint32 saved_flags = TextureImage[0]->flags&(SDL_SRCALPHA|SDL_RLEACCELOK);
+			Uint8  saved_alpha = TextureImage[0]->format->alpha;
+
+			if ((saved_flags & SDL_SRCALPHA) == SDL_SRCALPHA) 
+				SDL_SetAlpha(TextureImage[0], 0, 0);
+
+			SDL_Surface *LocalSurface = SDL_CreateRGBSurface(SDL_SWSURFACE, Width, Height, 
+				TextureImage[0]->format->BitsPerPixel, 
+				TextureImage[0]->format->Rmask, TextureImage[0]->format->Gmask, TextureImage[0]->format->Bmask,
+				TextureImage[0]->format->Amask);
+
+			SDL_BlitSurface(TextureImage[0], NULL, LocalSurface, NULL);
+
+			if ((saved_flags & SDL_SRCALPHA) == SDL_SRCALPHA) 
+				SDL_SetAlpha(LocalSurface, saved_flags, saved_alpha);
+
+			SDL_FreeSurface(TextureImage[0]); 
+			TextureImage[0] = LocalSurface;
+		}
+
+		/* Create The Texture */
+		glGenTextures( 1, &texture[0] );
+
+		/* Typical Texture Generation Using Data From The Bitmap */
+		glBindTexture( GL_TEXTURE_2D, texture[0] );
+
+		if(TextureImage[0]->format->BytesPerPixel == 4)
+		{
+			/* Generate The Texture */
+			glTexImage2D( GL_TEXTURE_2D, 0, 4, TextureImage[0]->w,
+				TextureImage[0]->h, 0, GL_RGBA,
+				GL_UNSIGNED_BYTE, TextureImage[0]->pixels );
+		}
+		else
+		{
+			/* Generate The Texture */
+			glTexImage2D( GL_TEXTURE_2D, 0, 3, TextureImage[0]->w,
+				TextureImage[0]->h, 0, GL_RGB,
+				GL_UNSIGNED_BYTE, TextureImage[0]->pixels );
+		}
+
+		/* Linear Filtering */
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+		glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+	}
+	else
+		return false;
+
+	/* Free up any memory we may have used */
+	if ( TextureImage[0] )
+		SDL_FreeSurface( TextureImage[0] );
+
+	return true;
+}
+
+/* function to reset our viewport after a window resize */
+int resizeWindow( int width, int height )
+{
+	/* Height / width ration */
+	GLfloat ratio;
+
+	/* Protect against a divide by zero */
+	if ( height == 0 )
+		height = 1;
+
+	ratio = ( GLfloat )width / ( GLfloat )height;
+
+	/* Setup our viewport. */
+	glViewport( 0, 0, ( GLint )width, ( GLint )height );
+
+	/*
+	* change to the projection matrix and set
+	* our viewing volume.
+	*/
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity( );
+
+	/* Set our perspective */
+	gluPerspective( 45.0f, ratio, 0.1f, 100.0f );
+
+	/* Make sure we're chaning the model view and not the projection */
+	glMatrixMode( GL_MODELVIEW );
+
+	/* Reset The View */
+	glLoadIdentity( );
+
+	glShadeModel(GL_SMOOTH);
+	//Realy Nice perspective calculations
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glClearDepth(1.0);                       // Depth Buffer Setup
+	//glEnable(GL_DEPTH_TEST);                 // Enable Depth Buffer
+	glDepthFunc(GL_LESS);	
+
+	glEnable(GL_TEXTURE_2D);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	return( TRUE );
+}
+
+/* function to handle key press events */
+void handleKeyPress( SDL_keysym *keysym )
+{
+	switch ( keysym->sym )
+	{
+	case SDLK_ESCAPE:
+		/* ESC key was pressed */
+		Quit( 0 );
+		break;
+	case SDLK_F1:
+		/* F1 key was pressed
+		* this toggles fullscreen mode
+		*/
+		SDL_WM_ToggleFullScreen( surface );
+		break;
+	default:
+		break;
+	}
+
+	return;
+}
+
+/* general OpenGL initialization function */
+int initGL( GLvoid )
+{
+
+	/* Load in the texture */
+	if ( !LoadGLTextures( ) )
+		return FALSE;
+
+	/* Enable Texture Mapping ( NEW ) */
+	glEnable( GL_TEXTURE_2D );
+
+	/* Enable smooth shading */
+	glShadeModel( GL_SMOOTH );
+
+	/* Set the background black */
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.5f );
+
+	/* Depth buffer setup */
+	glClearDepth( 1.0f );
+
+	/* Enables Depth Testing */
+	glEnable( GL_DEPTH_TEST );
+
+	/* The Type Of Depth Test To Do */
+	glDepthFunc( GL_LEQUAL );
+
+	/* Really Nice Perspective Calculations */
+	glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
+
+	return( TRUE );
+}
+
+/* Here goes our drawing code */
+int drawGLScene( GLvoid )
+{
+	/* These are to calculate our fps */
+	static GLint T0     = 0;
+	static GLint T1mask = 0;
+	static GLint Frames = 0;
+
+	/* Clear The Screen And The Depth Buffer */
+	glClearColor(0.5f, 0.0f, 0.0f, 0.0f);
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+	/* Move Into The Screen 5 Units */
+	glLoadIdentity( );
+	glTranslatef( 0.0f, 0.0f, -5.0f );
+
+	glRotatef( xrot, 1.0f, 0.0f, 0.0f); /* Rotate On The X Axis */
+	glRotatef( yrot, 0.0f, 1.0f, 0.0f); /* Rotate On The Y Axis */
+	glRotatef( zrot, 0.0f, 0.0f, 1.0f); /* Rotate On The Z Axis */
+
+	/* Select Our Texture */
+	glBindTexture( GL_TEXTURE_2D, texture[0] );
+
+	/* NOTE:
+	*   The x coordinates of the glTexCoord2f function need to inverted
+	* for SDL because of the way SDL_LoadBmp loads the data. So where
+	* in the tutorial it has glTexCoord2f( 1.0f, 0.0f ); it should
+	* now read glTexCoord2f( 0.0f, 0.0f );
+	*/
+
+	glBegin(GL_QUADS);
+	// Front Face
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
+	// Back Face
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
+	// Top Face
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+	// Bottom Face
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+	// Right face
+	glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);	// Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);	// Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);	// Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);	// Bottom Left Of The Texture and Quad
+	// Left Face
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);	// Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);	// Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);	// Top Left Of The Texture and Quad
+	glEnd();
+
+	/* Draw it to the screen */
+	SDL_GL_SwapBuffers( );
+
+	GLint t = SDL_GetTicks();
+	
+	if(bUseMask && t - T1mask >= 1000)
+	{
+		SDL_SysWMinfo info;
+		SDL_VERSION(&info.version); 
+		if(SDL_GetWMInfo(&info))
+		{
+			XRectangle rects[2] = 
+			{
+				{0, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2},
+				{SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT}
+			};
+			
+			XShapeCombineRectangles (
+				info.info.x11.display,
+				info.info.x11.wmwindow,
+				ShapeBounding,
+				0, 0,
+				rects,
+				2, ShapeSet, Unsorted);
+
+			printf("Applied mask\n");
+			T1mask = t;
+		}
+		else
+		{
+			Quit(1);
+		}
+	}
+
+	Frames++;
+
+	/* Gather our frames per second */
+	if (t - T0 >= 5000) 
+	{
+		GLfloat seconds = (t - T0) / 1000.0;
+		GLfloat fps = Frames / seconds;
+		//todo
+		printf("%d frames in %g seconds = %g FPS\n", Frames, seconds, fps);
+		T0 = t;
+		Frames = 0;
+	}
+
+#ifndef WIN32
+	usleep(40000);
+#endif
+	xrot += 0.3f; /* X Axis Rotation */
+	yrot += 0.2f; /* Y Axis Rotation */
+	zrot += 0.4f; /* Z Axis Rotation */
+
+	return( TRUE );
+}
+
+#ifdef WIN32
+int _tmain(int argc, char * /*_TCHAR*/ argv[])
+#else
+int main( int argc, char **argv )
+#endif
+{
+	/* Flags to pass to SDL_SetVideoMode */
+	int videoFlags;
+	/* main loop variable */
+	int done = FALSE;
+	/* used to collect events */
+	SDL_Event event;
+	/* this holds some info about our display */
+	const SDL_VideoInfo *videoInfo;
+	/* whether or not the window is active */
+	int isActive = TRUE;
+
+	sTextureName = "logo.png";
+
+	if(argc >= 2)
+	{
+		bUseComposite = string(argv[1]) == "composite";
+		bUseMask = string(argv[1]) == "mask";
+	}
+
+	printf("Texture to use: %s\n", sTextureName.c_str());
+	printf("Composite: %s\n\n", bUseComposite ? "enabled" : "disabled");
+	printf("Mask: %s\n\n", bUseMask ? "enabled" : "disabled");
+
+	/* initialize SDL */
+	if ( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE ) < 0 )
+	{
+		fprintf( stderr, "Video initialization failed: %s\n",
+			SDL_GetError( ) );
+		Quit(1);
+	}
+
+	/* Fetch the video info */
+	videoInfo = SDL_GetVideoInfo( );
+
+	if ( !videoInfo )
+	{
+		fprintf( stderr, "Video query failed: %s\n", SDL_GetError( ) );
+		Quit(1);
+	}
+
+	/* the flags to pass to SDL_SetVideoMode */
+	videoFlags  = SDL_OPENGL;          /* Enable OpenGL in SDL */
+	videoFlags |= SDL_GL_DOUBLEBUFFER; /* Enable double buffering */
+	videoFlags |= SDL_HWPALETTE;       /* Store the palette in hardware */
+	videoFlags |= SDL_RESIZABLE;       /* Enable window resizing */
+
+	/* This checks to see if surfaces can be stored in memory */
+	if ( videoInfo->hw_available )
+		videoFlags |= SDL_HWSURFACE;
+	else
+		videoFlags |= SDL_SWSURFACE;
+
+	/* This checks if hardware blits can be done */
+	if ( videoInfo->blit_hw )
+		videoFlags |= SDL_HWACCEL;
+
+	/* Sets up OpenGL double buffering */
+	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+
+#ifndef WIN32
+	if(bUseComposite)
+	{
+		//composite stuff
+		SDL_GL_SetAttribute(SDL_GL_RENDER_TYPE,   GLX_RGBA_BIT);
+		SDL_GL_SetAttribute(SDL_GL_DRAWABLE_TYPE, GLX_WINDOW_BIT);
+
+		SDL_GL_SetAttribute(SDL_GL_RED_SIZE,      8);
+		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,    8);
+		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,     8);
+		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,    8);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,  0);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,    0);
+	}
+#endif
+
+	/* get a SDL surface */
+	surface = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, videoFlags );
+
+	/* Verify there is a surface */
+	if ( !surface )
+	{
+		fprintf( stderr,  "Video mode set failed: %s\n", SDL_GetError( ) );
+		Quit(1);
+	}
+
+	/* initialize OpenGL */
+	initGL( );
+
+	/* resize the initial window */
+	resizeWindow( SCREEN_WIDTH, SCREEN_HEIGHT );
+
+	/* wait for events */
+	while ( !done )
+	{
+		/* handle the events in the queue */
+
+		while ( SDL_PollEvent( &event ) )
+		{
+			switch( event.type )
+			{
+
+			case SDL_ACTIVEEVENT:
+				/* Something's happend with our focus
+				* If we lost focus or we are iconified, we
+				* shouldn't draw the screen
+				*/
+				if ( event.active.gain == 0 )
+					isActive = FALSE;
+				else
+					isActive = TRUE;
+				break;			    
+
+			case SDL_VIDEORESIZE:
+				/* handle resize event */
+				surface = SDL_SetVideoMode( event.resize.w,
+					event.resize.h,
+					32, videoFlags );
+				if ( !surface )
+				{
+					fprintf( stderr, "Could not get a surface after resize: %s\n", SDL_GetError( ) );
+					Quit( 1 );
+				}
+				resizeWindow( event.resize.w, event.resize.h );
+				break;
+			case SDL_KEYDOWN:
+				/* handle key presses */
+				handleKeyPress( &event.key.keysym );
+				break;
+			case SDL_QUIT:
+				/* handle quit requests */
+				printf("about to quit!\n");
+				done = TRUE;
+				break;
+			default:
+				break;
+			}
+		}
+
+		/* draw the scene */
+		//if ( isActive )
+		drawGLScene( );
+	}
+
+	/* clean ourselves up and exit */
+	Quit(0);
+
+	/* Should never get here */
+	return(0);
+} 
+
