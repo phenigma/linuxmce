8280cb760281c8d615bfdaee2931ce17583e5b98
rgavril <rgavril>
2007-04-16 09:40:27 +0000

    Rewriten as the email requested to

--- src/BootScripts/ForciblyKillProcess.sh
+++ src/BootScripts/ForciblyKillProcess.sh
@@ -3,4 +3,36 @@
 # Takes as a parameter the name of a program, and it will forcibly kill it, first by sending
 # the processes a kill, and then a kill -9 if they don't die
 
-killall -9 "$1"
+process_name="$1"
+running_pids=$(pidof $process_name)
+
+if [[ "$running_pids" == "" ]] ;then 
+	echo "No program with that name running ."
+	exit 0
+fi
+
+echo "Killing : $running_pids"
+kill $running_pids
+start_time=$(date +%s)
+echo -n "Waiting to die "
+while [[ $(( start_time + 5 )) -gt $( date +%s ) ]] ;do
+	
+	left_running=""
+	for pid in $running_pids ;do
+		if [[ -d /proc/$pid ]] ;then
+			left_running="$left_running $pid"
+		fi
+	done
+
+	if [[ "$left_running" != "" ]] ;then
+		echo -n "."
+		running_pids="$left_running"
+	else
+		echo
+		exit 0
+	fi
+done
+echo
+
+echo "Force Killing: $running_pids"
+kill -9 $running_pids
