d2d1786912eaf8005e1457b30c0d31252f10510d
rgavril <rgavril>
2007-04-19 15:06:58 +0000

    fix bug

--- src/mce-installer/build-mce-installer.sh
+++ src/mce-installer/build-mce-installer.sh
@@ -3,7 +3,7 @@
 pushd src
 cp /usr/lib/libvte.so* ./
 cp /usr/lib/libglade*so* ./
-tar -czf mce-installer.tar.gz mce-installer mce-installer.glade mce-installer-core.sh mce-installer-preseed.sh mce-installer-diskedmd.sh wget-wrapper.sh pluto.png mce-installer-getnetdata.sh libvte.so* libglade*so*
+tar -czf mce-installer.tar.gz mce-installer mce-installer.glade mce-installer-core.sh mce-installer-preseed.sh mce-installer-diskedmd.sh wget-wrapper.sh pluto.png mce-installer-getnetdata.sh libvte.so* libglade*so* mirrors
 cat ../mce-installer-header.sh mce-installer.tar.gz > ../mce-installer.sh
 rm libvte.so*
 rm libglade*so*
