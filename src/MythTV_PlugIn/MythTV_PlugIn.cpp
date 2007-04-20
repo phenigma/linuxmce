1cd30e3622acd04706f3b49f29aa5c5e1ae26625
aaron <aaron.b@plutohome.com>
2007-04-20 06:13:54 +0000

    redo the tv bookmarking

--- src/MythTV_PlugIn/MythTV_PlugIn.cpp
+++ src/MythTV_PlugIn/MythTV_PlugIn.cpp
@@ -1389,6 +1389,24 @@ void MythTV_PlugIn::CheckForNewRecordings()
 			if( row[10] )
 				m_pMedia_Plugin->CMD_Add_Media_Attribute(row[10],0,"",ATTRIBUTETYPE_Rated_CONST,"",pRow_File->PK_File_get(),&PK_Attribute_Misc);
 
+LoggerWrapper::GetInstance()->Write(LV_CRITICAL,"CheckForNewRecordings %d %d",PK_Attribute_Title,PK_Attribute_Episode);
+/*
+			if( PK_Attribute_Title || PK_Attribute_Episode )
+			{
+				if( row[12] )
+					sSQL = string("seriesid='") + row[12] + "'";
+				else
+					sSQL = "";
+
+				if( row[13] )
+				{
+					if( sSQL.empty()==false )
+						sSQL += " OR ";
+					sSQL += string("programid='") + row[13] + "'";
+				}
+*/				
+				LoggerWrapper::GetInstance()->Write(LV_CRITICAL,"CheckForNewRecordings %s",sSQL.c_str());
+
 			sSQL = 
 				"SELECT role,name FROM recordedcredits JOIN people on recordedcredits.person=people.person "
 				"WHERE chanid='" + string(row[0]) + "' AND starttime='" + row[1] + "'";
@@ -1412,24 +1430,6 @@ void MythTV_PlugIn::CheckForNewRecordings()
 
 				}
 			}
-
-LoggerWrapper::GetInstance()->Write(LV_CRITICAL,"CheckForNewRecordings %d %d",PK_Attribute_Title,PK_Attribute_Episode);
-
-			if( PK_Attribute_Title || PK_Attribute_Episode )
-			{
-				if( row[12] )
-					sSQL = string("seriesid='") + row[12] + "'";
-				else
-					sSQL = "";
-
-				if( row[13] )
-				{
-					if( sSQL.empty()==false )
-						sSQL += " OR ";
-					sSQL += string("programid='") + row[13] + "'";
-				}
-				
-				LoggerWrapper::GetInstance()->Write(LV_CRITICAL,"CheckForNewRecordings %s",sSQL.c_str());
 
 				PlutoSqlResult result;
 				if( (result.r=m_pMySqlHelper_Myth->mysql_query_result( "SELECT EK_Picture FROM `pluto_myth`.Picture WHERE " + sSQL ) ) && ( row=mysql_fetch_row( result.r ) ) )
