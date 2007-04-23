6195e1d2bdf4f4c5ef1003d442301c0bc8f43f6a
vali <vali>
2007-04-23 13:34:02 +0000

    Fixed restart.php to use correct command to hold/suspend core/mds.

--- web/pluto-admin/operations/others/restart.php
+++ web/pluto-admin/operations/others/restart.php
@@ -92,48 +92,48 @@ function restart($output,$dbADO) {
 		$MDArray=explode(',',@$_POST['MDArray']);
 
 		if(isset($_POST['halt_core'])){
-			$command='/usr/pluto/bin/MessageSend -targetType template 0 27 1 268 21 H 2 '.$coreID;
-			exec($command);
+			$command='/usr/pluto/bin/MessageSend localhost -targetType template 0 27 1 323 21 H 2 '.$coreID;
+			exec_batch_command($command);
 		}
 
 		if(isset($_POST['suspend_core'])){
-			$command='/usr/pluto/bin/MessageSend -targetType template 0 27 1 268 21 S 2 '.$coreID;
-			exec($command);
+			$command='/usr/pluto/bin/MessageSend localhost -targetType template 0 27 1 323 21 S 2 '.$coreID;
+			exec_batch_command($command);
 		}
 
 		if(isset($_POST['reboot_core'])){
 			$command='sudo -u root /sbin/reboot';
-			exec($command);
+			exec_batch_command($command);
 		}
 
 		foreach ($MDArray AS $mdID){
 			if(isset($_POST['halt_'.$mdID])){
-				$command='/usr/pluto/bin/MessageSend -targetType template 0 27 1 268 21 H 2 '.$mdID;
-				exec($command);
+				$command='/usr/pluto/bin/MessageSend localhost -targetType template 0 27 1 323 21 H 2 '.$mdID;
+				exec_batch_command($command);
 			}
 	
 			if(isset($_POST['suspend_'.$mdID])){
-				$command='/usr/pluto/bin/MessageSend -targetType template 0 27 1 268 21 S 2 '.$mdID;
-				exec($command);
+				$command='/usr/pluto/bin/MessageSend localhost -targetType template 0 27 1 323 21 S 2 '.$mdID;
+				exec_batch_command($command);
 			}
 	
 			if(isset($_POST['reboot_'.$mdID])){
 				// extract IP
 				$mdData=getAssocArray('Device','PK_Device','IPAddress',$dbADO,'WHERE PK_Device='.$mdID);
 				$command="sudo -u root /usr/pluto/bin/LaunchRemoteCmd.sh '$mdData[$mdID]' 'reboot'";
-				exec($command);
+				exec_batch_command($command);
 			}
 		}
 		
 		if(isset($_REQUEST['device']) && (int)$_REQUEST['device']!=0){
 			$device=(int)$_REQUEST['device'];
-			$command='/usr/pluto/bin/MessageSend -targetType template 0 27 1 268 21 '.$action.' 2 '.$device;
-			exec($command);
+			$command='/usr/pluto/bin/MessageSend localhost -targetType template 0 27 1 323 21 '.$action.' 2 '.$device;
+			exec_batch_command($command);
 		}
 
 		if(isset($_POST['quick_reload'])){
 			$command='/usr/pluto/bin/MessageSend localhost 0 -1000 7 1';
-			exec($command);
+			exec_batch_command($command);
 		}
 
 		header('Location: index.php?section=restart&msg='.$TEXT_COMMAND_SENT_CONST.' '.$command);
