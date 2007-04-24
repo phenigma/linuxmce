f4b99d96d282bf48c815b1f7dc11e16d10a9cbc9
rgavril <rgavril>
2007-04-24 14:51:05 +0000

    Adding Kubuntu Cache cd script

--- src/mce-installer/src/mce-installer-LinuxMceCD.sh
+++ src/mce-installer/src/mce-installer-LinuxMceCD.sh
@@ -4,18 +4,17 @@
 FROM_CD=1
 FROM_ISO=2
 
-
 CD_Dir=$(mktemp -d)
 
 if [[ "$c_linuxmceCdFrom" == "$FROM_ISO" ]] ;then
-	mount -o loop "$c_linuxmceCdFrom" "$CD_Dir" || exit 1
+	mount -o loop "$c_linuxmceCdIsoPath" "$CD_Dir" || exit 1
 else
-	mount /dev/cdrom "$CD_Dir"
+	mount /dev/cdrom "$CD_Dir" || exit 4
 fi
 
 mkdir -p /usr/pluto/deb-cache || exit 2
 cp "${CD_Dir}"/*.deb /usr/pluto/deb-cache || exit 3
-cp "${CD_Dir}"/Packages* /usr/pluto/deb-cache/
+cp "${CD_Dir}"/Packages* /usr/pluto/deb-cache/ || exit 5
 
 umount -lf $CD_Dir || :
 rm -rf $CD_Dir
--- src/mce-installer/src/mce-installer-UbuntuExtraCD.sh
+++ src/mce-installer/src/mce-installer-UbuntuExtraCD.sh
@@ -1,1 +1,22 @@
-exit 1
+#!/bin/bash -e
+. /tmp/mce_wizard_data.sh
+
+FROM_CD=1
+FROM_ISO=2
+
+
+CD_Dir=$(mktemp -d)
+
+if [[ "$c_ubuntuExtraCdFrom" == "$FROM_ISO" ]] ;then
+	mount -o loop "$c_ubuntuExtraCdPath" "$CD_Dir" || exit 1
+else
+	mount /dev/cdrom "$CD_Dir" || exit 4
+fi
+
+mkdir -p /usr/pluto/deb-cache || exit 2
+cp "${CD_Dir}"/cachecd1-cache/*.deb /usr/pluto/deb-cache || exit 3
+
+umount -lf $CD_Dir || :
+rm -rf $CD_Dir
+
+exit 0
