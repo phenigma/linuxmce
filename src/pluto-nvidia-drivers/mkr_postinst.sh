cf830b003d140634d152be412381dc2cef993b8a
rgavril <rgavril>
2007-04-25 14:58:45 +0000

    No need to do this any more

--- src/pluto-nvidia-drivers/mkr_postinst.sh
+++ src/pluto-nvidia-drivers/mkr_postinst.sh
@@ -11,8 +11,6 @@ if ! BlacklistConfFiles '/etc/X11/xorg.conf' ;then
 	if [ ! -e /etc/X11/xorg.conf.pbackup ] ;then
 		cp /etc/X11/xorg.conf /etc/X11/xorg.conf.pbackup || :
 	fi
-	sed -i 's/"nv"/"vesa"/g' /etc/X11/xorg.conf || :
-	sed -i 's/"nvidia"/"vesa"/g' /etc/X11/xorg.conf || :
 fi 
 for KVer in <-mkr_t_MakeRelease_Kernel->; do
 	depmod "$KVer" || :
