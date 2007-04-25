828c8e8c0606f681e831f5a611e5c47430751211
aaron <aaron.b@plutohome.com>
2007-04-25 20:19:42 +0000

    compile typo

--- src/DCE/Command_Impl.cpp
+++ src/DCE/Command_Impl.cpp
@@ -346,9 +346,9 @@ bool Command_Impl::SpawnChildDevice( int PK_Device, string sCommand, string sDis
 		}
 	}
 
-	LoggerWrapper::GetInstance()->Write( LV_STATUS, "Spawning device: %d %s on display: %s ip: %s", pDeviceData_Impl_Child->m_dwPK_Device, pDeviceData_Impl_Child->m_sCommandLine.c_str(), sDisplay.c_str(), m_sIPAddress.c_str() );
-	system( ("screen -d -m -h 3000 -S " + pDeviceData_Impl_Child->m_sCommandLine + "_" + StringUtils::itos( pDeviceData_Impl_Child->m_dwPK_Device ) +
-			" /bin/bash -x " + sPrefix + "Spawn_Device.sh " + StringUtils::itos(pDeviceData_Impl_Child->m_dwPK_Device) + " " + m_sIPAddress + " " + sDisplay + " " + pDeviceData_Impl_Child->m_sCommandLine).c_str() );
+	LoggerWrapper::GetInstance()->Write( LV_STATUS, "Spawning device: %d %s on display: %s ip: %s", PK_Device, sCommandLine.c_str(), sDisplay.c_str(), m_sIPAddress.c_str() );
+	system( ("screen -d -m -h 3000 -S " + sCommandLine + "_" + StringUtils::itos( PK_Device ) +
+			" /bin/bash -x " + sPrefix + "Spawn_Device.sh " + StringUtils::itos(PK_Device) + " " + m_sIPAddress + " " + sDisplay + " " + m_sCommandLine).c_str() );
 #else
 	//TO BE IMPLEMENTED
 	/*
