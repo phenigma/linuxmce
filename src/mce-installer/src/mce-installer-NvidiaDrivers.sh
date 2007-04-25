41c2e78cc12c1bcfdf6795de1b225bced288e670
rgavril <rgavril>
2007-04-25 14:56:34 +0000

    Added a buch of options needed by the nvidia driver

--- src/mce-installer/src/mce-installer-NvidiaDrivers.sh
+++ src/mce-installer/src/mce-installer-NvidiaDrivers.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-sleep 2
-#apt-get install nvidia-glx
-#apt-get install linux-restricted-modules-generic
+if [[ ! -f /etc/X11/xorg.conf.pbackup ]] ;then
+	cp /etc/X11/xorg.conf /etc/X11/xorg.conf.pbackup
+fi
+
+apt-get install nvidia-glx
+apt-get install linux-restricted-modules-generic
 sed -i 's/Driver.*"nv"/Driver "nvidia"/g' /etc/X11/xorg.conf
+nvidia-xconfig --no-use-edid
+nvidia-xconfig --exact-mode-timings-dvi
+nvidia-xconfig --no-logo
+nvidia-xconfig --no-bandwidth-test
+nvidia-xconfig --no-dynamic-twinview
+nvidia-xconfig --xvmc-uses-textures
+nvidia-xconfig --allow-glx-with-composite
+nvidia-xconfig --add-argb-glx-visuals
+nvidia-xconfig --render-accel
+
