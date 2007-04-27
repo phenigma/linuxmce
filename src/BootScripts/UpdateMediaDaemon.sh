46286dd6279636c41109911e7df9eb990ce48f73
alex.c <alex.c>
2007-04-27 13:09:20 +0000

    put lock

--- src/BootScripts/UpdateMediaDaemon.sh
+++ src/BootScripts/UpdateMediaDaemon.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+. /usr/pluto/bin/LockUtils.sh
+
+trap 'Unlock "UM" "Update_Media"' EXIT
+Lock "UM" "Update_Media"
 counter=30
 while ((counter > 0)); do
 	nice -n 20 /usr/pluto/bin/UpdateMedia -h localhost -d /home/public/data -B -t | tee-pluto -a /var/log/pluto/UpdateMedia.log
