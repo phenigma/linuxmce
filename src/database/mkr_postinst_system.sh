b6ab0a6cc7831b8a286473555d1214b174c003ca
aaron <aaron.b@plutohome.com>
2007-04-27 03:10:30 +0000

    weirdness in mysql5 that it won't allow anymore 2 timestamp fields defaulting to current_timestamp

--- src/database/mkr_postinst_system.sh
+++ src/database/mkr_postinst_system.sh
@@ -9,6 +9,7 @@ echo Please be patient.......
 
 echo "drop table if exists CriteriaParmNesting_D; drop table if exists CriteriaParm_D; drop table if exists Criteria_D" | mysql pluto_main || /bin/true
 echo "drop table if exists psc_document_batdet; drop table if exists psc_document_bathdr; drop table if exists psc_document_batuser; drop table if exists psc_document_repset; drop table if exists psc_document_schema; drop table if exists psc_document_tables; drop table if exists Document; drop table if exists Document_Comment;" | mysql pluto_main || /bin/true
+echo "alter table `Document_Comment` ,change `Date` `Date` datetime   NOT NULL " | mysql pluto_main || /bin/true
 
 (cd /usr/pluto/database; /usr/pluto/bin/sqlCVS -n -D pluto_main -r constants,dce,designer,document,ir,website import) || exit $?
 mysql pluto_main < /usr/pluto/database/city.dump
