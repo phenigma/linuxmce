30a976c0fc64a84f4d6cf9e7de26f980f190ed48
vali <vali>
2007-04-27 14:26:09 +0000

    Added forgot password php file.

---
+++ web/fiire/forgot.php
@@ -0,0 +1,26 @@
+<?
+session_name('FIIRE');
+session_start();
+
+include('lib/config.php');
+include('lib/functions.php');
+include('lib/init.php');
+
+$title='FIIRE :: Forgot Password';
+$page_template=implode('',file('templates/template.html'));
+$content=forgot_password($conn);
+$login_form=login_header($conn);
+$navArray=array(
+	'index.php'=>'Home',
+	'forgot.php'=>'Forgot password'
+);
+
+// set dynamic fields
+$variables=array();
+$variables=set_variable($variables,'title',$title);
+$variables=set_variable($variables,'content',$content);
+$variables=set_variable($variables,'login_form',$login_form);
+$variables=set_variable($variables,'nav_menu', nav_menu($navArray));
+
+outputHTML($variables,$page_template);
+?>
