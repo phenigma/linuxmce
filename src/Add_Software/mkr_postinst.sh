b54ac52d36bcf7931b648792b8e8bb3250297043
radu <radu.cx+linuxmce@gmail.com>
2007-05-03 10:44:08 +0000

    temporary log and disown

--- src/Add_Software/mkr_postinst.sh
+++ src/Add_Software/mkr_postinst.sh
@@ -4,5 +4,6 @@
 ln -s /etc/init.d/instcheck.sh /etc/rc2.d/S23instcheck.sh || :
 
 if [[ ! -f /etc/diskless.conf ]] ;then
-	/usr/pluto/bin/getxmls &>/dev/null </dev/null &
+	/usr/pluto/bin/getxmls &>/var/log/pluto/getxmls.postinst.addsoftware.log </dev/null &
+	disown -a
 fi											
