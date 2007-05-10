e36eed32889bd5d8f49ceaeb44591a806eaf2cd8
alex.c <alex.c>
2007-05-10 14:08:43 +0000

    add in init.d to run at boot voicemail monitor

---
+++ src/VoiceMailMonitor/mkr_postinst.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+Content="
+#!/bin/bash
+
+case \"\$1\" in
+  start)
+        screen -S VoiceMailMonitor -d -m /bin/bash /usr/pluto/bin/VoiceMailMonitor.sh
+        ;;
+  stop)
+        ;;
+  reload|force-reload)
+        ;;
+  restart)
+        ;;
+  *)
+        echo \"Usage: /etc/init.d/voiceMailMonitor {start|stop|reload|force-reload|restart}\"
+        exit 1
+esac
+"
+
+echo "$Content" > /etc/init.d/voiceMailMonitor
+
+chmod +x /etc/init.d/voiceMailMonitor || :
+
+update-rc.d -f voiceMailMonitor start 99 2 3 4 5 || /bin/true 
