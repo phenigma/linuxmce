d2fe05c2ed4c271d0cc4c88f0ac69de6447f733c
alex.c <alex.c>
2007-05-11 11:26:25 +0000

    add debug log

--- src/Add_Software/mkr_postinst.sh
+++ src/Add_Software/mkr_postinst.sh
@@ -4,6 +4,7 @@
 ln -s /etc/init.d/instcheck.sh /etc/rc2.d/S23instcheck.sh || :
 
 if [[ ! -f /etc/diskless.conf ]] ;then
-	/usr/pluto/bin/getxmls &>/dev/null </dev/null &
+#	/usr/pluto/bin/getxmls &>/dev/null </dev/null &
+	/usr/pluto/bin/getxmls &>/var/log/pluto/add_software_debug.log &
 	disown -a
 fi											
