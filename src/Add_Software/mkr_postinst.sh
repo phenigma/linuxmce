4ffc0b717332a8ba188576fb2c90c8a2c8f70cf1
alex.c <alex.c>
2007-05-14 13:54:03 +0000

    getxmls will not run at postinst anymore

--- src/Add_Software/mkr_postinst.sh
+++ src/Add_Software/mkr_postinst.sh
@@ -3,8 +3,8 @@
 
 ln -s /etc/init.d/instcheck.sh /etc/rc2.d/S23instcheck.sh || :
 
-if [[ ! -f /etc/diskless.conf ]] ;then
+#if [[ ! -f /etc/diskless.conf ]] ;then
 #	/usr/pluto/bin/getxmls &>/dev/null </dev/null &
-	/usr/pluto/bin/getxmls &>/var/log/pluto/add_software_debug.log &
-	disown -a
-fi											
+#	/usr/pluto/bin/getxmls &>/var/log/pluto/add_software_debug.log &
+#	disown -a
+#fi											
