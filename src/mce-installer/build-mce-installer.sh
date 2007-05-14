b4ada68fd9fdbbfc6fd895b4d1d255a0bfdd488b
rgavril <rgavril>
2007-05-14 12:58:57 +0000

    added more dependencies to run uidiag from the installer

--- src/mce-installer/build-mce-installer.sh
+++ src/mce-installer/build-mce-installer.sh
@@ -3,7 +3,9 @@
 pushd src
 cp /usr/lib/libvte.so* ./
 cp /usr/lib/libglade*so* ./
-cp /opt/libsdl1.2-1.2.7+1.2.8cvs20041007/lib/libSDL*
+cp /opt/libsdl1.2-1.2.7+1.2.8cvs20041007/lib/libSDL* ./
+cp /usr/lib/libSDL_ttf* ./
+cp /usr/lib/libSDL_image* ./
 cp /usr/bin/dpkg-scanpackages ./
 
 
@@ -12,9 +14,10 @@ pushd ../../utilities/linux/UIdiag/
 	make all
 popd
 cp ../../utilities/linux/UIdiag/UIdiag ./
+cp ../../utilities/linux/UIdiag/*.png ./
 
 
-tar -czf mce-installer.tar.gz mce-installer mce-installer-*.sh mce-installer.glade mce-installer-core.sh mce-installer-preseed.sh mce-installer-diskedmd.sh wget-wrapper.sh pluto.png mce-installer-getnetdata.sh libvte.so* libglade*so* libSDL* mirrors dpkg-scanpackages UIdiag
+tar -czf mce-installer.tar.gz mce-installer mce-installer-*.sh mce-installer.glade mce-installer-core.sh mce-installer-preseed.sh mce-installer-diskedmd.sh wget-wrapper.sh pluto.png mce-installer-getnetdata.sh libvte.so* libglade*so* libSDL* mirrors dpkg-scanpackages UIdiag *.png
 cat ../mce-installer-header.sh mce-installer.tar.gz > ../mce-installer.sh
 rm libvte.so*
 rm libglade*so*
