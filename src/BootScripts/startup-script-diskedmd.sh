73e80d8bef64b6f5adcf909269dc0c1c628c0d18
rgavril <rgavril>
2007-05-22 14:41:15 +0000

    Startup script for disked media directors

---
+++ src/BootScripts/startup-script-diskedmd.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+LogFile="/var/log/startup-script.log"
+exec &> >(tee $LogFile)
+
+
+DisplayProgress() {
+	Text=$1
+	echo "${Progress}% ${Text}"
+}
+
+StartService() {
+	ServiceDescription=$1
+	ServiceCmd=$2
+	ServiceBkg=$3
+	Progress=$(( $Progress + $ProgressUnit ))
+
+
+	if [[ -x $(echo $ServiceCmd | cut -d ' ' -f1) ]] ;then
+		DisplayProgress "$ServiceDescription"
+		echo -n "$ServiceDescription ..."
+		$ServiceCmd $ServiceBkg
+		err=$?
+
+		if [[ "$err" == "0" ]] ;then
+			echo "ok"
+		else
+			echo "fail"
+		fi
+	fi
+
+	return $err
+}
+
+StartDaemon() {
+	ServiceDescription=$1
+	ServiceCmd=$2
+	ServiceScreen=$3
+	Progress=$(( $Progress + $ProgressUnit ))
+
+	if [[ -x $(echo $ServiceCmd | cut -d ' ' -f1) ]] ;then
+		DisplayProgress "$ServiceDescription"
+		echo -n "$ServiceDescription ..."
+		screen -d -m -S "$ServiceScreen" $ServiceCmd 
+		err=$?
+
+		if [[ "$err" == "0" ]] ;then
+			echo "ok"
+		else
+			echo "fail"
+		fi
+	fi
+
+	return $err
+}
+
+ThisFile=$0
+Progress=0
+ProgressChunks=$(grep "^ *StartService \|^ *StartDaemon " $ThisFile | wc -l)
+ProgressUnit=$(( 100 / $ProgressChunks ))
+
+setterm -blank >/dev/console             # disable console blanking
+chmod 777 /etc/pluto.conf 2>/dev/null    # ensure access rights on pluto.conf
+rm /var/log/pluto/running.pids 2>/dev/null
+chmod 777 /var/log/pluto 2>/dev/null
+rm -f /dev/ttyS_*                        # remove all ttyS_* (created by gc100s) entries from /dev
+mkdir -p /usr/pluto/locks                # clean up locks
+rm -f /usr/pluto/locks/*                 # clean up locks
+
+
+if [[ -f /usr/pluto/bin/Config_Ops.sh ]]; then
+	 . /usr/pluto/bin/Config_Ops.sh
+fi
+if [[ -f /usr/pluto/bin/pluto.func ]] ;then
+	. /usr/pluto/bin/pluto.func
+fi
+if [[ -f /usr/pluto/bin/SQL_Ops.sh ]] ;then
+	. /usr/pluto/bin/SQL_Ops.sh
+fi
+
+
+StartService "Loading Kernel Modules" "/usr/pluto/bin/LoadModules.sh"
+
+StartService "Setting SSH Keys" "/usr/pluto/bin/SSH_Keys.sh" "&"
+StartService "Setting Coredump Location" "/usr/pluto/bin/corefile.sh"
+StartService "Configuring Pluto Storage Devices" "/usr/pluto/bin/StorageDevices_Setup.sh" "&"
