ddb278ba28326a8440494516d7eb967cd24b4524
radu <radu.cx+linuxmce@gmail.com>
2007-05-22 18:33:22 +0000

    removed hacks

--- src/Dhcpd-Plugin/mkr_postrm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm /usr/sbin/dhcpd3 || :
-dpkg-divert --remove --rename --package pluto-dhcpd-plugin --divert /usr/lib/pluto-dhcpd-plugin/dhcpd3 /usr/sbin/dhcpd3 > /dev/null
--- src/Dhcpd-Plugin/mkr_preinst.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p /usr/lib/pluto-dhcpd-plugin
-dpkg-divert --add --rename --package pluto-dhcpd-plugin --divert /usr/lib/pluto-dhcpd-plugin/dhcpd3 /usr/sbin/dhcpd3 > /dev/null
