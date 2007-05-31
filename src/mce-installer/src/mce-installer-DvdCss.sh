7f08cb72f62a1fe4fe4def6d299ebcc64019249a
rgavril <rgavril>
2007-05-31 11:46:08 +0000

    Timeout after 100 seconds when installing libdvdcss

--- src/mce-installer/src/mce-installer-DvdCss.sh
+++ src/mce-installer/src/mce-installer-DvdCss.sh
@@ -6,6 +6,20 @@
 Setup_Apt_Conffiles
 
 apt-get -y install libdvdread3 || exit 1
-#apt-get -y install libdvdcss2 
-/usr/share/doc/libdvdread3/install-css.sh || :
+/usr/share/doc/libdvdread3/install-css.sh &
+wait_pid=$!
+
+## Wait for 100 seconds to finish
+end_date=$(( $(date +%s) + 100 ))
+while [[ -d /proc/$wait_pid ]] ;do
+	sleep 0.5
+	if [[ "$end_date" -gt "$(date +%s)" ]] ;then
+		echo "Waiting for dvdcss to be installed ..."
+	else
+		echo "Operation timed out"
+		kill "$wait_pid"
+		exit 1
+	fi
+done
+
 exit 0
