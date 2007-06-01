76b8f8bfc77d6b48446521fb5b2068868f43d7b3
rgavril <rgavril>
2007-06-01 08:55:42 +0000

    Wrote script

--- src/mce-installer/src/mce-installer-UI-InstallXine.sh
+++ src/mce-installer/src/mce-installer-UI-InstallXine.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
+
+function Install {
+	package_name="$1"
+	package_installed="false"
+
+	if [[ "$(aptitude show ${package_name} | grep State | cut -d ' ' -f2)" == "installed" ]] ;then
+	        package_installed="true"
+	fi
+
+	if [[ "$package_installed" == "false" ]] ;then
+	        dpkg -i /media/cdrom/cachecd1-cache/${package_name}_*.deb && package_installed="true"
+	fi
+
+	if [[ "$package_installed" == "false" ]] ;then
+		apt-get update
+		apt-get -f -y install "${package_name}" && package_installed="true"
+	fi
+}
+
 mount /dev/cdrom /media/cdrom || :
 
-if [[ "$(aptitude show nvidia-glx | grep State | cut -d ' ' -f2)" == "installed" ]] ;then
-        nvidia_glx_installed="true"
-fi
-
-if [[ "$nvidia_glx_installed" == "false" ]] ;then
-        dpkg -i /media/cdrom/cachecd1-cache/nvidia-glx_*.deb && nvidia_glx_installed="true"
-fi
-
-if [[ "$nvidia_glx_installed" == "false" ]] ;then
-        apt-get update
-        apt-get -f -y install nvidia-glx && nvidia_glx_installed="true"
-fi
-
-if [[ "$(aptitude show linux-restricted-modules-generic | grep State | cut -d ' ' -f2)" == "installed" ]] ;then
-        linux_restricted_modules_generic_installed="true"
-fi
-
-if [[ "$linux_restricted_modules_generic_installed" == "false" ]] ;then
-        apt-get update
-        apt-get -f -y install linux-restricted-modules-generic && linux_restricted_modules_generic_installed="true"
-fi
-
-#if [[ "$linux_restricted_modules_generic_installed" == "false" ]] ;then
-#       dpkg -i 
-#fi
-
-if [[ "$linux_restricted_modules_generic_installed" == "false" || "$nvidia_glx_installed" == "false" ]] ;then
-        echo "ERROR: Some of the pacakges could not be installed"
-        exit 1
-fi
-
+Install "libcucul0"
+Install "libcaca0"
+Install "libmad0"
+Install "libxine1-ffmpeg"
+Install "xine-ui"
