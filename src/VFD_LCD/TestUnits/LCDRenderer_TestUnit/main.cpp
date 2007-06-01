c406128d97800773750acb3f6b3637a61a889b7c
cmiron <cmiron>
2007-06-01 11:36:54 +0000

    Command line parameters for LCDRenderer.

--- src/VFD_LCD/TestUnits/LCDRenderer_TestUnit/main.cpp
+++ src/VFD_LCD/TestUnits/LCDRenderer_TestUnit/main.cpp
@@ -2,11 +2,22 @@
 
 int main( int argc, char* argv[] )
 {
-	LCDRenderer lcd_renderer("/dev/ttyUSB0");
+	DCE::LoggerWrapper::GetInstance()->Write(LV_STATUS, "Usage: LCDRenderer <port> <text to display>\n");
+
+	string sSerialPort("/dev/ttyUSB0");
+	string sTestText("This is a test");
+
+	if(argc >= 2)
+		sSerialPort = argv[1];
+
+	if(argc >= 3)
+		sTestText = argv[2];
+
+	LCDRenderer lcd_renderer(sSerialPort);
 
 	DisplayState display_state;
 
-	display_state.m_sDescription = "This is a test";
+	display_state.m_sDescription = sTestText;
 	lcd_renderer.Render(display_state);
 
 	return 0;
