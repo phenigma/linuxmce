c571ab2e5a17ec5e869a65f10ba70c7cb491ad7c
rgavril <rgavril>
2007-06-12 13:41:59 +0000

    Make message less confusing

--- src/Update/LMCEUpdate_Notify.sh
+++ src/Update/LMCEUpdate_Notify.sh
@@ -66,7 +66,7 @@ function Action_Ask() {
 	Action_Yes="-targetType device <%=!%> $AppServerID 1 67 13 \"/usr/pluto/bin/LMCEUpdate_Notify.sh\" 51 \"--answer${Tab}yes\""
 	 Action_No="-targetType device <%=!%> $AppServerID 1 67 13 \"/usr/pluto/bin/LMCEUpdate_Notify.sh\" 51 \"--answer${Tab}no\""
 	
-	/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 741 159 53 9 "New updates available. Would you like to install them?|Yes|No" 137 "$Action_Yes|$Action_No" || exit 1
+	/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 741 159 53 9 "New updates available. Would you like to install them next time you start Linux MCE ?|Yes|No" 137 "$Action_Yes|$Action_No" || exit 1
 }
 
 function Action_Answer {
