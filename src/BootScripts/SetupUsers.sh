4e7680fff8c67dc0850ba19ea2362af7f42b305a
aaron <aaron.b@plutohome.com>
2007-06-13 13:58:34 +0000

    add logging to chgrp issues

--- src/BootScripts/SetupUsers.sh
+++ src/BootScripts/SetupUsers.sh
@@ -2,7 +2,7 @@
 
 echo "$(date -R) SetupUsers" >> /var/log/pluto/SetupUsers.log
 cd /usr/pluto/bin
-bash -x ./SetupUsers_Homes.sh >> /var/log/pluto/SetupUsers.log
+bash -x ./SetupUsers_Homes.sh >> /var/log/pluto/SetupUsers.log  2>&1
 if [ -x /usr/pluto/bin/generate_users_voicemenu.pl ]
 then
 	./generate_users_voicemenu.pl
