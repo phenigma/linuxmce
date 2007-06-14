912a2503a6f0d49c8f588bf696d4984cd7a63fac
radu <radu.cx+linuxmce@gmail.com>
2007-06-14 14:39:29 +0000

    video part of audio video setup script

---
+++ src/BootScripts/SetupAudioVideo.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/Utils.sh
+. /usr/pluto/bin/SQL_Ops.sh
+
+DEVICECATEGORY_Media_Director=8
+DEVICECATEGORY_Video_Cards=125
+DEVICECATEGORY_Sound_Cards=124
+
+DEVICETEMPLATE_OnScreen_Orbiter=62
+
+DEVICEDATA_Video_settings=89
+DEVICEDATA_Audio_settings=88
+DEVICEDATA_Reboot=236
+DEVICEDATA_Connector=68
+DEVICEDATA_TV_Standard=229
+
+SettingsFile=/etc/pluto/lastaudiovideo.conf
+
+# don't let KDE override xorg.conf
+rm -f {/home/*,/root}/.kde/share/config/displayconfigrc
+
+Reboot=NoReboot
+ReloadX=NoReloadX
+
+ComputerDev=$(FindDevice_Category "$PK_Device" "$DEVICECATEGORY_Media_Director" '' 'include-parent')
+OrbiterDev=$(FindDevice_Template "$ComputerDev" "$DEVICETEMPLATE_OnScreen_Orbiter")
+VideoCardDev=$(FindDevice_Category "$ComputerDev" "$DEVICECATEGORY_Video_Cards")
+SoundCardDev=$(FindDevice_Category "$ComputerDev" "$DEVICECATEGORY_Sound_Cards")
+
+ReadConf()
+{
+	## config file format (direcly parsable by bash):
+	# name=value
+	if [[ -f "$SettingsFile" ]]; then
+		while read line; do
+			eval "OldSetting_$line"
+		done <"$SettingsFile"
+	fi
+}
+
+GetVideoSetting()
+{
+	local Q
+	local VideoSetting
+	
+	Q="
+		SELECT IK_DeviceData
+		FROM Device_DeviceData
+		WHERE
+			FK_Device='$ComputerDev'
+			AND FK_DeviceData='$DEVICEDATA_Video_settings'
+		LIMIT 1
+	"
+	VideoSetting=$(RunSQL "$Q")
+	VideoSetting=$(Field "1" "$VideoSetting")
+
+	if [[ -n "$VideoSetting" ]]; then
+		Refresh=$(echo $VideoSetting | cut -d '/' -f2)
+		ResolutionInfo=$(echo $VideoSetting | cut -d '/' -f1)
+		ResX=$(echo $ResolutionInfo | cut -d' ' -f1)
+		ResY=$(echo $ResolutionInfo | cut -d' ' -f2)
+		if [[ -z "$Refresh" || -z "$ResX" || -z "$ResY" ]]; then
+			Logging "$TYPE" "$SEVERITY_CRITICAL" "Xconfigure" "Malformed DeviceData: VideoSetting='$VideoSetting'"
+			MalformedVideoSetting='Malformed VideoSetting'
+		fi
+	fi
+
+	if [[ -z "$MalformedVideoSetting" ]]; then
+		echo "$VideoSetting"
+	fi
+}
+
+wmtweaks_default()
+{
+	echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE mcs-option SYSTEM "mcs-option.dtd">
+
+<mcs-option>
+	<option name="Xfwm/UseCompositing" type="int" value="1"/>
+</mcs-option>'
+}
+
+SetWMCompositor()
+{
+	local UseCompositing="$1"
+	local WMTweaksFile
+	local user
+
+	for user in /home/*; do
+		if [[ ! -d "$user" ]]; then
+			continue
+		fi
+		WMTweaksFile="$user/.config/xfce4/mcs_settings/wmtweaks.xml"
+		mkdir -p "$(dirname "$WMTweaksFile")"
+		if [[ -f "$WMTweaksFile" ]]; then
+			sed -i '/Xfwm\/UseCompositing/ s/value="."/value="'"$UseCompositing"'"/g' "$WMTweaksFile"
+		else
+			wmtweaks_default >"$WMcompositor"
+		fi
+	done
+}
+
+SaveSettings()
+{
+	local Var VarName
+
+	for Var in ${!OldSetting_*}; do
+		VarName="${Var#OldSetting_}"
+		eval "Save_$VarName=\"${!Var}\""
+	done
+
+	for Var in ${!NewSetting_*}; do
+		VarName="${Var#NewSetting_}"
+		eval "Save_$VarName=\"${!Var}\""
+	done
+
+	for Var in ${!Save_*}; do
+		VarName="${Var#Save_}"
+		echo "$VarName=\"${!Var}\""
+	done >"$SettingsFile"
+}
+
+VideoSettings_Check()
+{
+	local DB_VideoSetting
+	local Update_XorgConf="NoXorgConf" Update_WMcompositor="NoWMcompositor"
+	local DB_Reboot
+
+	DB_VideoSetting=$(GetVideoSetting)
+	DB_OpenGL=$(OpenGLeffects)
+	DB_AlphaBlending=$(AlphaBlendingEnabled)
+	DB_Connector=$(GetDeviceData "$ComputerDev" "$DEVICEDATA_Connector")
+	DB_TVStandard=$(GetDeviceData "$ComputerDev" "$DEVICEDATA_TV_Standard")
+	DB_Reboot=$(GetDeviceData "$VideoCardDev" "$DEVICEDATA_Reboot")
+
+	if [[ -n "$DB_VideoSetting" && "$DB_VideoSetting" != "$OldSetting_VideoSetting" ]]; then
+		Update_XorgConf="XorgConf"
+		NewSetting_VideoSetting="$DB_VideoSetting"
+	fi
+	if [[ -n "$DB_OpenGL" && "$DB_OpenGL" != "$OldSetting_OpenGL" ]]; then
+		NewSetting_OpenGL="$DB_OpenGL"
+	fi
+	if [[ -n "$DB_AlphaBlending" && "$DB_AlphaBlending" != "$OldSetting_AlphaBlending" ]]; then
+		Update_XorgConf="XorgConf"
+		Update_WMcompositor="WMcompositor"
+		NewSetting_AlphaBlending="$DB_AlphaBlending"
+	fi
+	if [[ -n "$DB_Connector" && "$DB_Connector" != "$OldSetting_Connector" ]]; then
+		Update_XorgConf="XorgConf"
+		NewSetting_Connector="$DB_Connector"
+	fi
+	if [[ -n "$DB_TVStandard" && "$DB_TVStandard" != "$OldSetting_TVStandard" ]]; then
+		Update_XorgConf="XorgConf"
+		NewSetting_TVStandard="$DB_TVStandard"
+	fi
+
+	if [[ "$Update_XorgConf" == "XorgConf" ]]; then
+		/usr/pluto/bin/Xconfigure.sh
+	fi
+	if [[ "$Update_WMcompositor" == "WMcompositor" ]]; then
+		SetWMCompositor "$DB_AlphaBlending"
+	fi
+
+	if [[ "$Update_XorgConf" == "XorgConf" || "$Update_WMcompositor" == "WMcompositor" ]]; then
+		if [[ "$DB_Reboot" == 1 ]]; then
+			Reboot="Reboot"
+		else
+			ReloadX="ReloadX"
+		fi
+	fi
+}
+
+AudioSettings_Check()
+{
+	:
+}
+
+ReadConf
+VideoSettings_Check
+
+SaveSettings
