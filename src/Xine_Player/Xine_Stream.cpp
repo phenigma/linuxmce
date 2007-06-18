e8e11b72880469dec04db54f281adbe9330f55f0
kirill.b <kirill.b>
2007-06-18 11:44:50 +0000

    printing position info only if stream is initialized

--- src/Xine_Player/Xine_Stream.cpp
+++ src/Xine_Player/Xine_Stream.cpp
@@ -944,7 +944,10 @@ void *Xine_Stream::EventProcessingLoop( void *arguments )
 		// updating every second - position
 		if ( ++iCounter >= 10 )
 		{
-			LoggerWrapper::GetInstance()->Write( LV_WARNING, "[ID: %d] %s (seek %d) t.c. ctr %d freq %d,", pStream->m_iStreamID, pStream->GetPosition().c_str(), pStream->m_iSpecialSeekSpeed, iCounter_TimeCode, pStream->m_iTimeCodeReportFrequency );
+			if (pStream->m_bInitialized)
+			{
+				LoggerWrapper::GetInstance()->Write( LV_WARNING, "[ID: %d] %s (seek %d) t.c. ctr %d freq %d,", pStream->m_iStreamID, pStream->GetPosition().c_str(), pStream->m_iSpecialSeekSpeed, iCounter_TimeCode, pStream->m_iTimeCodeReportFrequency );
+			}
 			iCounter = 0;
 			
 			//if it is a time - reporting our timecode to player object
