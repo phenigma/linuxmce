6e31097d37f7e7fc446e960c62666f615db721f2
rgavril <rgavril>
2007-06-18 13:28:14 +0000

    Remove the NotifyUpdates screen from all orbiters after user answers on one of them

--- src/Update/LMCEUpdate_Notify.sh
+++ src/Update/LMCEUpdate_Notify.sh
@@ -19,32 +19,30 @@ XML_CURRENT="${UPDATES_DIR}/updates.xml"
 XML_NEW="${UPDATES_DIR}/updates.xml.new"
 XML_LOCK="${UPDATES_DIR}/updates.xml.lock"
 
+## Generate a list containig all orbiter ids ($OrbiterIDList)
+Q="
+        SELECT
+                PK_Device
+        FROM
+                Device
+                JOIN DeviceTemplate ON PK_DeviceTemplate = FK_DeviceTemplate
+                JOIN DeviceCategory ON PK_DeviceCategory = FK_DeviceCategory
+        WHERE
+                PK_DeviceCategory IN (5,2,3)
+"
+OrbiterList=$(RunSQL "$Q")
 
-function Action_Ask() {
-	## Generate a list containig all orbiter ids ($OrbiterIDList)
-	Q="
-	        SELECT
-	                PK_Device
-	        FROM
-	                Device
-	                JOIN DeviceTemplate ON PK_DeviceTemplate = FK_DeviceTemplate
-	                JOIN DeviceCategory ON PK_DeviceCategory = FK_DeviceCategory
-	        WHERE
-	                PK_DeviceCategory IN (5,2,3)
-	"
-	OrbiterList=$(RunSQL "$Q")
-	
-	OrbiterIDList=""
-	for Orbiter in $OrbiterList; do
-	        if [[ $OrbiterIDList != "" ]]; then
-	                OrbiterIDList="$OrbiterIDList,"
-	        fi
-	
-	        Orbiter_ID=$(Field 1 "$Orbiter")
-	        OrbiterIDList="$OrbiterIDList""$Orbiter_ID"
-	done
-	
-	
+OrbiterIDList=""
+for Orbiter in $OrbiterList; do
+        if [[ $OrbiterIDList != "" ]]; then
+                OrbiterIDList="$OrbiterIDList,"
+        fi
+
+        Orbiter_ID=$(Field 1 "$Orbiter")
+        OrbiterIDList="$OrbiterIDList""$Orbiter_ID"
+done
+
+function Action_Ask() {	
 	## AppServer Id for this computer (should only be hybrid)
 	Q="
 	        SELECT
@@ -66,10 +64,12 @@ function Action_Ask() {
 	Action_Yes="-targetType device <%=!%> $AppServerID 1 67 13 \"/usr/pluto/bin/LMCEUpdate_Notify.sh\" 51 \"--answer${Tab}yes\""
 	 Action_No="-targetType device <%=!%> $AppServerID 1 67 13 \"/usr/pluto/bin/LMCEUpdate_Notify.sh\" 51 \"--answer${Tab}no\""
 	
-	/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 741 159 53 9 "New updates available. Would you like to install them next time you start Linux MCE ?|Yes|No" 137 "$Action_Yes|$Action_No" || exit 1
+	/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 741 10 "UpdatesNotify" 159 53 9 "New updates available. Would you like to install them next time you start Linux MCE ?|Yes|No" 137 "$Action_Yes|$Action_No" || exit 1
 }
 
 function Action_Answer {
+	/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 8 10 "UpdatesNotify" 159 53
+
 	local answer="$1"
 	if [[ "$answer" == "yes" ]] ;then
 		Q="Update Device_DeviceData SET IK_DeviceData = '1' WHERE FK_Device='$PK_Device' AND FK_DeviceData='235'"
