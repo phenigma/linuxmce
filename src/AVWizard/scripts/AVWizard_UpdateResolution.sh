ce49ee982db4bb4445723c0cc3752ed482df8a9d
radu <radu.cx+linuxmce@gmail.com>
2007-06-19 14:44:45 +0000

    fixed variable names

--- src/AVWizard/scripts/AVWizard_UpdateResolution.sh
+++ src/AVWizard/scripts/AVWizard_UpdateResolution.sh
@@ -10,20 +10,29 @@ case "$Param" in
 		VideoOutput=$(WizGet VideoOutput)
 
 		ResolutionDefaults="640x480 60 640 480 VGA"
-		if [[ -f "/tmp/avwizard-resolution-default.txt" ]]; then
-			ResolutionDefaults="$(</tmp/avwizard-resolution.txt)"
+		if [[ -f "/tmp/avwizard-resolution-defaults.txt" ]]; then
+			ResolutionDefaults="$(</tmp/avwizard-resolution-defaults.txt)"
 		fi
 
 		DefaultResolution=$(SpcField 1 "$ResolutionDefaults")
 		DefaultRefresh=$(SpcField 2 "$ResolutionDefaults")
 		DefaultConnector=$(SpcField 5 "$ResolutionDefaults")
 
+		DefaultResolutionFullName=$(Resolution_GetFullName "$DefaultResolution")
+		if [[ "$DefaultResolutionFullName" == *=* ]]; then
+			DefaultResolution_Name="${DefaultResolutionFullName%%=*}"
+			DefaultResolution_Size="${DefaultResolutionFullName#*=}"
+		else
+			DefaultResolution_Name="$DefaultResolutionFullName"
+			DefaultResolution_Size="$DefaultResolutionFullName"
+		fi
+
 		echo "--> Reset resolution to default: $DefaultResolution@$DefaultRefresh/$DefaultConnector (was resolution: $VideoResolution@$VideoRefresh/$VideoOutput)"
 		if [[ "$VideoRefresh" == "$DefaultRefresh" && "$VideoResolution" == "$DefaultResolution" && "$VideoOutput" == "$DefaultConnector" ]]; then
 			exit 0 # No change required
 		fi
 		killall -USR2 AVWizard
-		bash -x "$BaseDir"/Xconfigure.sh --conffile "$XF86Config" --defaults --resolution "$DefaultResolution" --output "$DefaultConnector" | tee-pluto /var/log/pluto/Xconfigure.log
+		bash -x "$BaseDir"/Xconfigure.sh --conffile "$XF86Config" --defaults --resolution "$DefaultResolution_Size" --output "$DefaultConnector" | tee-pluto /var/log/pluto/Xconfigure.log
 		echo "$ResolutionDefaults" >/tmp/avwizard-resolution.txt
 	;;
 	set_resolution)
