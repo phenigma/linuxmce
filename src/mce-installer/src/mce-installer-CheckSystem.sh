8e36419bbfddf31a6b810f8e8a01704c0037b1b3
rgavril <rgavril>
2007-06-20 12:27:30 +0000

    mce-installer-CheckSystem.sh : script for checking packages before installing

---
+++ src/mce-installer/src/mce-installer-CheckSystem.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+original_list="./original.list"
+local_list="/tmp/local.list"
+diff_list="/tmp/diff.list"
+clean_system="true"
+
+function GenerateDiff {
+	while read orig_name orig_ver ;do
+		local local_line=$(grep "^$orig_name " $local_list)
+		if [[ "$local_line" == "" ]] ;then
+			echo "MISSING '$orig_name'" > "$diff_list"
+			clean_system="false"
+			continue
+		fi
+	
+		local local_name=$(echo $local_line | cut -d' ' -f1)
+		local local_ver=$(echo $local_line | cut -d' ' -f2-99)
+		
+		if [[ "$orig_ver" != "$local_ver" ]] ;then
+			echo "CHANGED '$orig_name' '$local_ver' '$orig_ver'" > "$diff_list"
+			clean_system="false"
+			continue
+		fi
+	done < "$original_list"
+}
+
+function GenerateList {
+	local temp_file="$(mktemp)"
+	dpkg -l | grep ^ii | awk '{print $2; print $3}' > $temp_file
+
+	local x
+	local y
+	while read x && read y ;do
+	        echo "$x $y" >> "$local_list"
+	done <  "$temp_file"
+
+	rm -f "$temp_file"
+}
+
+GenerateList
+GenerateDiff
+
+if [[ "$clean_system" == "false" ]] ;then
+	exit 1
+fi
