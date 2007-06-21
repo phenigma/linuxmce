00119124a751901c433200efacf780cf14963bf5
radu <radu.cx+linuxmce@gmail.com>
2007-06-21 14:37:51 +0000

    on diskless, start KDE as root

--- src/BootScripts/Start_KDE.sh
+++ src/BootScripts/Start_KDE.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 . /usr/pluto/bin/Config_Ops.sh
-
-if su - "$TheChosenOne" -c "DISPLAY=:$Display kcheckrunning"; then
-	exit
-fi
+. /usr/pluto/bin/Utils.sh
 
 Old_IFS="$IFS"
 IFS=:
 
-while read user x uid gid rest; do
-	if ((uid < 1000 || uid >= 10000)); then
-		continue
-	fi
-	TheChosenOne="$user"
-	break
-done </etc/passwd
+DEVICEDATA_Diskless_Boot=9
+
+IsDiskless=$(GetDeviceData "$PK_Device" "$DEVICEDATA_Diskless_Boot")
+
+if [[ "$IsDiskless" == 1 ]]; then
+	TheChosenOne=root
+else
+	while read user x uid gid rest; do
+		if ((uid < 1000 || uid >= 10000)); then
+			continue
+		fi
+		TheChosenOne="$user"
+		break
+	done </etc/passwd
+fi
 
 IFS="$Old_IFS"
+
+if su - "$TheChosenOne" -c "DISPLAY=:$Display kcheckrunning"; then
+	exit
+fi
 
 su - "$TheChosenOne" -c "DISPLAY=:$Display startkde" &>/dev/null </dev/null &
 disown -a
