d91a08f0471be72f76c431705f5c7ac9d028625d
alex.c <alex.c>
2007-06-21 13:40:26 +0000

    delete unuseful timeout informations

--- src/mce-installer/src/mce-installer-Lame.sh
+++ src/mce-installer/src/mce-installer-Lame.sh
@@ -6,19 +6,5 @@
 Setup_Apt_Conffiles
 
 apt-get -y install lame || exit 1
-wait_pid=$!
-
-## Wait for 100 seconds to finish
-end_date=$(( $(date +%s) + 100 ))
-while [[ -d /proc/$wait_pid ]] ;do
-	sleep 0.5
-	if [[ "$end_date" -gt "$(date +%s)" ]] ;then
-		echo "Waiting for lame to be installed ..."
-	else
-		echo "Operation timed out"
-		kill "$wait_pid"
-		exit 1
-	fi
-done
 
 exit 0
