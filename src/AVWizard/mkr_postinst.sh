690c9e7ab2f1a9acd061ef478286789f1e45a941
radu <radu.cx+linuxmce@gmail.com>
2007-06-25 16:05:53 +0000

    only on machines that start kdm at boot

--- src/AVWizard/mkr_postinst.sh
+++ src/AVWizard/mkr_postinst.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-update-rc.d check_avwizard start 98 2 . >/dev/null
-update-rc.d 0start_avwizard start 99 2 . >/dev/null
+if [[ -f /etc/init.d/kdm ]]; then
+	update-rc.d check_avwizard start 98 2 . >/dev/null
+	update-rc.d 0start_avwizard start 99 2 . >/dev/null
+fi
