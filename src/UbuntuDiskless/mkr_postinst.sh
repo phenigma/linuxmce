18c116d57a5909816954a7ddfa3caa928a7fab70
alex.c <alex.c>
2007-06-27 08:49:31 +0000

    call the haltMDs script only at stop

--- src/UbuntuDiskless/mkr_postinst.sh
+++ src/UbuntuDiskless/mkr_postinst.sh
@@ -9,5 +9,5 @@ if [[ "$2" != "" ]] ;then
 	/usr/pluto/bin/Diskless_CreateTBZ.sh || echo "WARNING: Diskless_CreateTBZ.sh returned an error!"
 fi
 
-update-rc.d -f haltMDs start 01 0 6 . || /bin/true
+#update-rc.d -f haltMDs start 01 0 6 . || /bin/true
 update-rc.d -f haltMDs stop 01 0 6 . || /bin/true
