20b63e41667f52fa1a1abc42588f114249d35d6a
radu <radu.cx+linuxmce@gmail.com>
2007-06-27 15:52:25 +0000

    reorganized

--- src/Ubuntu_Helpers/build-qemu-hdd.sh
+++ src/Ubuntu_Helpers/build-qemu-hdd.sh
@@ -1,34 +1,59 @@
-#!/bin/bash
+#!/bin/bash 
 
-TEMP_DIR=$(mktemp -d)
+set -e
+set -x
 
-ImgFilename=qemu.img
-HDDSize=$((10*1024)) # size in MB
-HDDImg="$TEMP_DIR/$ImgFilename"
-RootPartSize=$(((HDDSize-1)/1024)) # size in GB
-PartLinks="$TEMP_DIR/partlinks"
-PartPrefix="$PartLinks/${ImgFilename}_p"
-DebootstrapDir="$TEMP_DIR/debootstrap"
+WORK_DIR="/var/plutobuild/qemu"
+mkdir -p "$WORK_DIR"
 
-dd if=/dev/zero of="$HDDImg" count="$HDDSize" bs=1M
+HDD_SIZE="$(( 10240 / 2 ))" # MBytes
+HDD_FILE="$WORK_DIR/qemu.img"
+PART_DIR="$WORK_DIR/partlinks"
+PART_FILE="$PART_DIR/$(basename $HDD_FILE)_p"
 
-parted -s "$HDDImg" -- mklabel msdos
-parted -s "$HDDImg" -- mkpart primary ext2 0 "$RootPartSize"GB
-parted -s "$HDDImg" -- mkpart extended "$RootPartSize"GB -1
-parted -s "$HDDImg" -- mkpart logical linux-swap "$RootPartSize"GB -1
 
-mkdir -p "$PartLinks"
-/root/misc_utils/plosetup "$HDDImg" "$PartLinks"
+function hdd_create {
+	local ROOT_PARTITION_SIZE="$((($HDD_SIZE/1024)-1))" # GBytes
 
-mkfs.ext3 "${PartPrefix}1"
+	## Generate a HDD_SIZE filled with 0 bytes
+	dd of="$HDD_FILE" bs=1M count=0 seek="$HDD_SIZE"
+#	dd if=/dev/zero of="$HDD_FILE" bs=1M count="$HDD_SIZE"
+	
+	## Create partitions on the new generated hdd file
+	parted -s "$HDD_FILE" -- mklabel msdos
+	parted -s "$HDD_FILE" -- mkpart primary ext2 0 "$ROOT_PARTITION_SIZE"GB
+	parted -s "$HDD_FILE" -- mkpart extended "$ROOT_PARTITION_SIZE"GB -1
+	parted -s "$HDD_FILE" -- mkpart logical linux-swap "$ROOT_PARTITION_SIZE"GB -1
 
-mkdir -p "$DebootstrapDir"
-mount "${PartPrefix}1" "$DebootstrapDir"
-debootstrap "feisty" "$DebootstrapDir" "http://ro.archive.ubuntu.com/ubuntu/"
-umount "$DebootstrapDir"
+	## Generate the loopback devices for all the partitions of the harddrive
+	mkdir -p "$PART_DIR"
+	/root/misc_utils/plosetup "$HDD_FILE" "$PART_DIR" "82 83 fd"
 
-/root/misc_utils/plosetup -c "$PartLinks"
+	## Fromat the '/' partition of the harddrive
+	mkfs.ext3 "${PART_FILE}1"
+	mkswap "${PART_FILE}5"
+}
 
-mv "$HDDImg" /var/plutobuild/
+function hdd_debootstrap {
+	local DEBOOTRAP_DIR="$WORK_DIR/debootstrap"
+	mkdir -p $DEBOOTRAP_DIR
 
-rm -rf "$TEMP_DIR"
+	mount "${PART_FILE}1" "$DEBOOTRAP_DIR"
+	debootstrap "feisty" "$DEBOOTRAP_DIR" "http://ro.archive.ubuntu.com/ubuntu/"
+	umount "${PART_FILE}1"
+}
+
+function hdd_prepare_for_install {
+	mount "${PART_FILE}1" "$DEBOOTRAP_DIR"
+	chroot "${PART_FILE}1" apt-get -f -y install sshd
+	umount "${PART_FILE}1"
+}
+
+function hdd_cleanup {
+	/root/misc_utils/plosetup -c "$PART_DIR"
+}
+
+hdd_create
+hdd_debootstrap
+hdd_prepare_for_install
+hdd_cleanup
