55bfd53316822d0877c896a1510a04bf7d510092
radu <radu.cx+linuxmce@gmail.com>
2007-06-29 13:58:35 +0000

    reverted dedicated exception (didn't work anyway)

--- src/AVWizard/mkr_postinst.sh
+++ src/AVWizard/mkr_postinst.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-if [[ -f /etc/init.d/kdm ]]; then
-	update-rc.d check_avwizard start 98 2 . >/dev/null
-	update-rc.d 0start_avwizard start 99 2 . >/dev/null
-fi
+update-rc.d check_avwizard start 98 2 . >/dev/null
+update-rc.d 0start_avwizard start 99 2 . >/dev/null
