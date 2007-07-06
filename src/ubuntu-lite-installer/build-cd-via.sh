9fa8358d862dd54c31fcab602064a8358b32faf2
radu <radu.cx+linuxmce@gmail.com>
2007-07-06 11:50:17 +0000

    via installer, variation of linucmce dvd installer

---
+++ src/ubuntu-lite-installer/build-cd-via.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -x
+
+WorkDir="/var/plutobuild/lite-installer-via"
+WorkDirUbuntu="/var/plutobuild/lite-installer"
+LiveCDDir="$WorkDir/livecd"
+SquashFSDir="$WorkDir/squashfs"
+LiveCDtgz="$WorkDirUbuntu/ubuntu-livecd.tgz"
+SquashFStgz="$WorkDirUbuntu/ubuntu-squashfs-base.tgz"
+
+## TODO: change paths to SVN checkout directory
+InstallerScript="lite-installer-via.sh"
+RcSScript="install-lmce-via"
+FirstRunScript="firstboot-via"
+
+LiveCDISO="$WorkDir/via-linuxmce.iso"
+FilesystemArchives=("boot.tgz" "kubuntu.tgz" "pluto44.tgz")
+
+PrepareWorkDir()
+{
+	CleanupWorkDir
+	mkdir -p "$LiveCDDir" "$SquashFSDir"
+	tar -C "$LiveCDDir" -xzf "$LiveCDtgz"
+	tar -C "$SquashFSDir" -xzf "$SquashFStgz"
+}
+
+CreateSquashFS()
+{
+	cp "$InstallerScript" "$SquashFSDir/usr/bin/"
+	cp "$RcSScript" "$SquashFSDir/etc/init.d/"
+	ln -snf "/etc/init.d/$(basename "$RcSScript")" "$SquashFSDir/etc/rcS.d/S99install-lmce-via"
+	chroot "$SquashFSDir" dpkg-query -W --showformat='${Package} ${Version}\n' \
+	   > "$LiveCDDir/casper/filesystem.manifest"
+	mksquashfs "$SquashFSDir" "$LiveCDDir/casper/filesystem.squashfs"
+}
+
+CreateLiveCD()
+{
+	local Archive
+
+	mkdir -p "$LiveCDDir/lmce-image-via/"
+	for Archive in "${FilesystemArchives[@]}"; do
+		ln "$Archive" "$LiveCDDir/lmce-image-via/"
+	done
+	cp "$FirstRunScript" "$LiveCDDir/lmce-image-via/"
+	(cd "$LiveCDDir" && find . -type f -not -path ./md5sum.txt -print0 | xargs -0 md5sum | tee md5sum.txt)
+	mkisofs -o "$LiveCDISO" \
+		-b isolinux/isolinux.bin \
+		-c isolinux/boot.cat \
+		-no-emul-boot \
+		-boot-load-size 4 \
+		-boot-info-table \
+		-r \
+		-V "Custom VIA LMCE-Core" \
+		-cache-inodes \
+		-J \
+		-l \
+		"$LiveCDDir"
+}
+
+CleanupWorkDir()
+{
+	rm -rf "$LiveCDDir" "$SquashFSDir"
+}
+
+PrepareWorkDir
+CreateSquashFS
+CreateLiveCD
+CleanupWorkDir
---
+++ src/ubuntu-lite-installer/lite-installer-via.sh
@@ -0,0 +1,231 @@
+#!/bin/bash
+
+set -e
+
+Debug=0
+
+TargetHdd=
+BootUUID=
+SwapUUID=
+RootUbuntuUUID=
+RootDebianUUID=
+
+GetConsole()
+{
+	exec &>/dev/tty1 </dev/tty1
+	dmesg -n1
+	chvt 1
+}
+
+GetHddToUse()
+{
+	local HddList Done Drive i Choice DiskSize DiskDev
+	local -a Hdd
+
+	HddList=$(fdisk -l|grep ^Disk |cut -d' ' -f2-4|sed -r 's,[:,],,g; s, ,:,g')
+	if [[ "$Debug" == 1 ]]; then
+		HddList="/dev/loop0:0:GB"
+	fi
+	if [[ -z "$HddList" ]]; then # No hard drives
+		echo "Error: No hard drives found"
+		exit 1
+	else
+		echo "Hard drives in the system:"
+		Done=0
+		while [[ "$Done" == 0 ]]; do
+			i=1
+			for Drive in $HddList; do
+				DiskDev="${Drive%%:*}"
+				DiskSize="${Drive#*:}"
+				set +e
+				DiskDescription="$(hdparm -I $DiskDev | grep "Model Number:" | cut -d ':' -f2 | sed 's/^ *//g')"
+				DiskSerial="$(hdparm -I $DiskDev | grep "Serial Number:" | cut -d ':' -f2 | sed 's/^ *//g')"
+				set -e
+				echo "$i. $DiskDev ($DiskDescription $DiskSerial ${DiskSize//:/ })"
+				Hdd[$i]="$DiskDev"
+				((i++))
+			done
+			((i--))
+			echo -n "Choose which drive to install the system on: "
+			read Choice
+			if [[ "$Choice" == *[^0-9]* || "$Choice" -lt 1 || "$Choice" -gt "$i" ]]; then
+				echo "Invalid selection. Try again"
+			else
+				TargetHdd="${Hdd[$Choice]}"
+				Done=1
+			fi
+		done
+	fi
+
+	echo "Chosen hdd: $TargetHdd"
+	if [[ -z "$TargetHdd" ]]; then
+		echo "Got out of loop with empty selection. This shouldn't be possible."
+		exit 1
+	fi
+}
+
+PartitionHdd()
+{
+set +e
+	swapoff -a
+	parted -s "$TargetHdd" -- mklabel msdos
+	parted -s "$TargetHdd" -- mkpart primary ext2 0 500MB
+	parted -s "$TargetHdd" -- mkpart extended 500MB -1s
+	parted -s "$TargetHdd" -- mkpart logical linux-swap 500MB 1500MB
+	parted -s "$TargetHdd" -- mkpart logical ext2 1500MB 6500MB
+	parted -s "$TargetHdd" -- mkpart logical ext2 6500MB 11500MB
+	sleep 1
+	blockdev --rereadpt "$TargetHdd"
+	sleep 5
+set -e
+}
+
+FormatPartitions()
+{
+	if [[ "$Debug" == 1 ]]; then
+		return 0
+	fi
+	echo y|mkfs.ext3 "$TargetHdd"1
+	mkswap "$TargetHdd"5
+	echo y|mkfs.ext3 "$TargetHdd"6
+	echo y|mkfs.ext3 "$TargetHdd"7
+
+	blkid -w /etc/blkid.tab || :
+	BootUUID=$(vol_id -u "$TargetHdd"1)
+	SwapUUID=$(vol_id -u "$TargetHdd"5)
+	RootDebianUUID=$(vol_id -u "$TargetHdd"6)
+	RootUbuntuUUID=$(vol_id -u "$TargetHdd"7)
+}
+
+MountPartitions()
+{
+	mkdir -p /media/target/{boot,debian,ubuntu}
+	mount "$TargetHdd"1 /media/target/boot
+	mount "$TargetHdd"6 /media/target/debian
+	mount "$TargetHdd"7 /media/target/ubuntu
+	mount --bind /media/target/boot /media/target/debian/boot
+	mount --bind /media/target/boot /media/target/ubuntu/boot
+}
+
+ExtractArchives()
+{
+	echo "Extracting archives (this will take a while)"
+
+	# Boot
+	echo "Boot archive"
+	tar -C /media/target --checkpoint=10000 -zxf /cdrom/lmce-image/boot.tgz
+
+	# Debian
+	echo "Debian archive"
+	tar -C /media/target --checkpoint=10000 -zxf /cdrom/lmce-image/pluto44.tgz
+
+	# Kubuntu
+	echo "Kubuntu archive"
+	tar -C /media/target --checkpoint=10000 -zxf /cdrom/lmce-image/kubuntu.tgz
+
+	# Update the UUIDs
+	rm /media/target/ubuntu/etc/blkid.tab || :
+	blkid -w /media/target/ubuntu/etc/blkid.tab
+
+	#Copy the fist run script
+	cp /cdrom/lmce-image-via/firstboot-via /media/target/debian/etc/rc2.d/S90firstboot
+}
+
+SetupFstab()
+{
+	local fstab_text
+
+	fstab_text="# /etc/fstab: static file system information.
+#
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>                                                                                      proc            /proc           proc    defaults        0       0
+${TargetHdd}6   /               ext3    defaults,errors=remount-ro,user_xattr 0       1
+${TargetHdd}1   /boot           ext3    defaults,user_xattr 0       2
+${TargetHdd}5   none            swap    sw              0       0
+/dev/cdrom      /media/cdrom0   udf,iso9660 user,noauto     0       0
+"
+	echo "$fstab_text" >/media/target/debian/etc/fstab
+
+	fstab_text="# /etc/fstab: static file system information.
+#
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>
+proc            /proc           proc    defaults        0       0
+#UUID=$RootUbuntuUUID
+${TargetHdd}7 /               ext3    defaults,errors=remount-ro 0       1
+#UUID=$BootUUID
+${TargetHdd}1 /boot           ext3    defaults          0       2
+#UUID=$SwapUUID
+${TargetHdd}5 none            swap    sw              0       0
+/dev/cdrom        /media/cdrom0   udf,iso9660 user,noauto     0       0
+"
+	echo "$fstab_text" >/media/target/ubuntu/etc/fstab
+}
+
+InstallGrub()
+{
+	mkdir -p /media/target/ubuntu/{proc,sys,dev}
+	mount --bind /dev/ /media/target/ubuntu/dev/
+	mount -t proc -o bind /proc /media/target/ubuntu/proc
+	grub-install --recheck --root-directory=/media/target/ubuntu "$TargetHdd"
+	umount /media/target/ubuntu/{proc,dev}
+
+	sed -ir "s,root=UUID=.* ro quiet splash,root=${TargetHdd}7 ro quiet splash,g" /media/target/boot/grub/menu.lst
+	sed -ir "s,root=UUID=.* ro single,root=${TargetHdd}7 ro single,g" /media/target/boot/grub/menu.lst
+	sed -ir 's/^default[\t ]+.*$/default saved/g' /media/target/boot/grub/menu.lst
+	/usr/sbin/grub-set-default --root-directory=/media/target/boot 2 # boot Debian by default
+}
+
+TargetCleanup()
+{
+	local NR ifnr ifname link ifmac rest line
+
+	NR=0
+	echo "# This file assigns persistent names to network interfaces.
+# See iftab(5) for syntax.
+" >/media/target/ubuntu/etc/iftab
+
+	while read line; do
+		if ((NR < 2)); then
+			((NR++))
+			continue
+		fi
+		if ((NR % 2 == 0)); then
+			read ifnr ifname rest < <(echo "$line")
+		else
+			read link ifmac rest < <(echo "$line")
+			echo "${ifname%:} mac $ifmac arp 1"
+		fi
+		((NR++))
+	done < <(/sbin/ip l) >>/media/target/ubuntu/etc/iftab
+	#chroot /media/target/debian/ update-initramfs -u
+	chroot /media/target/ubuntu/ update-initramfs -u
+	#chroot /media/target update-grub
+}
+
+UnmountPartitions()
+{
+	set +e
+	umount /media/target/{debian,ubuntu}/boot
+	umount /media/target/*
+	set -e
+}
+
+Reboot()
+{
+	clear
+	echo "*******************************************************************************"
+	echo "Installation complete. Press Enter or Control+Alt+Delete to reboot the computer"
+	echo "*******************************************************************************"
+	read
+	reboot
+}
+
+GetConsole
+GetHddToUse
+PartitionHdd
+FormatPartitions
+ExtractArchives
+SetupFstab
+InstallGrub
+TargetCleanup
+UnmountPartitions
+Reboot
