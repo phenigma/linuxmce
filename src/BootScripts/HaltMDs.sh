35e05d8a9c57d7fe3a3df17f762c5f5860c65475
rgavril <rgavril>
2007-07-10 13:04:46 +0000

    Shut down firre stations also

--- src/BootScripts/HaltMDs.sh
+++ src/BootScripts/HaltMDs.sh
@@ -12,13 +12,20 @@ if [ "$P" == "D" ]; then
 	FROM Device_DeviceData
 	JOIN Device ON PK_Device=FK_Device
 	JOIN DeviceTemplate ON PK_DeviceTemplate=FK_DeviceTemplate
-	WHERE FK_DeviceTemplate=28 AND FK_DeviceData=9 AND IPaddress IS NOT NULL AND MACaddress IS NOT NULL AND IK_DeviceData='1'"
+	WHERE FK_DeviceTemplate=28 AND FK_DeviceData=9 AND IPaddress IS NOT NULL AND MACaddress IS NOT NULL AND IK_DeviceData='1'
+
+	UNION
+	SELECT IPaddress FROM Device WHERE FK_DeviceTemplate=1893 AND IPaddress IS NOT NULL AND MACaddress IS NOT NULL
+	"
 else
 	# Selecting all MDs
 	Q="SELECT IPaddress
 	FROM Device
 	JOIN DeviceTemplate ON PK_DeviceTemplate=FK_DeviceTemplate
-	WHERE FK_DeviceTemplate=28 AND IPaddress IS NOT NULL AND MACaddress IS NOT NULL"
+	WHERE FK_DeviceTemplate=28 AND IPaddress IS NOT NULL AND MACaddress IS NOT NULL
+	UNION
+        SELECT IPaddress FROM Device WHERE FK_DeviceTemplate=1893 AND IPaddress IS NOT NULL AND MACaddress IS NOT NULL
+	"
 fi
 
 R=$(RunSQL "$Q")
