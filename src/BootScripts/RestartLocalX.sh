c519ce17b2fbc15ba0c5cfd4184a27beec3b834d
radu <radu.cx+linuxmce@gmail.com>
2007-07-10 16:55:46 +0000

    tell X to die, or kill it if it won't

--- src/BootScripts/RestartLocalX.sh
+++ src/BootScripts/RestartLocalX.sh
@@ -7,7 +7,22 @@ exec >>/var/log/pluto/RestartLocalX.log 2>&1
 date -R
 
 if [[ -f /etc/event.d/pluto || "$PK_Distro" == 1 ]] || grep -q Startup_Core-Hybrid /etc/rc2.d/S99kdm; then
-	kill $(pidof X)
+	PidOfX=$(pidof X)
+	if [[ -n "$PidOfX" ]]; then
+		kill $PidOfX
+
+		# wait for X to die
+		Count=5
+		while [[ -d /proc/$PidOfX && "$Count" -gt 0 ]]; do
+			sleep 1
+			((Count--))
+		done
+		if [[ -d /proc/$PidOfX ]]; then
+			# it won't die, so let's give it a hand
+			kill -9 $PidOfX
+		fi
+	fi
+
 	if [[ "$PK_Distro" == 1 ]]; then
 		sleep 1
 		/usr/pluto/bin/Start_X.sh
