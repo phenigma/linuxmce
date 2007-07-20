613850b8ab11ef6df2f40c8adf9ab27be267bf5f
rgavril <rgavril>
2007-07-20 15:04:25 +0000

    Always run lmce_launch_manager using the wrapper

--- src/BootScripts/Startup_Core-Hybrid.sh
+++ src/BootScripts/Startup_Core-Hybrid.sh
@@ -4,20 +4,12 @@ invoke-rc.d mysql start
 
 . /usr/pluto/bin/Config_Ops.sh
 export DISPLAY=:${Display}
-while /bin/true ;do
-	XPID=$(</var/run/plutoX$Display.pid)
-	if [[ -z "$XPID" || ! -d /proc/"$XPID" ]] ;then
-		/usr/pluto/bin/Start_X.sh -config /etc/X11/xorg.conf
-	fi
+XPID=$(</var/run/plutoX$Display.pid)
+if [[ -z "$XPID" || ! -d /proc/"$XPID" ]] ;then
+	/usr/pluto/bin/Start_X.sh -config /etc/X11/xorg.conf
+fi
 
-	export KDE_DEBUG=1
-	/usr/pluto/bin/lmce_launch_manager
-
-	while [[ "$(pidof lmce_launch_manager)" != "" ]] ;do
-		sleep 5
-	done
-	export -n KDE_DEBUG
-done
-
+export KDE_DEBUG=1
+/usr/pluto/bin/lmce_launch_manager.sh
 
 
--- src/BootScripts/Startup_DisklessMD.sh
+++ src/BootScripts/Startup_DisklessMD.sh
@@ -109,17 +109,10 @@ StartService "Configuring Pluto Storage Devices" "/usr/pluto/bin/StorageDevices_
 
 . /usr/pluto/bin/Config_Ops.sh
 export DISPLAY=:${Display}
-while /bin/true ;do
-	XPID=$(</var/run/plutoX$Display.pid)
-	if [[ -z "$XPID" || ! -d /proc/"$XPID" ]] ;then
-		/usr/pluto/bin/Start_X.sh -config /etc/X11/xorg.conf
-	fi
+XPID=$(</var/run/plutoX$Display.pid)
+if [[ -z "$XPID" || ! -d /proc/"$XPID" ]] ;then
+	/usr/pluto/bin/Start_X.sh -config /etc/X11/xorg.conf
+fi
 
-	export KDE_DEBUG=1
-	/usr/pluto/bin/lmce_launch_manager
-
-	while [[ "$(pidof lmce_launch_manager)" != "" ]] ;do
-		sleep 5
-	done
-	export -n KDE_DEBUG
-done
+export KDE_DEBUG=1
+/usr/pluto/bin/lmce_launch_manager.sh
