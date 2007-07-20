386e61c2fd62ffb5d956de91285ef43a394878f4
radu <radu.cx+linuxmce@gmail.com>
2007-07-20 09:52:04 +0000

    unhijacked script; found another script that does exactly the same thing: DHCP_Config.sh

--- src/BootScripts/Restart_DHCP.sh
+++ src/BootScripts/Restart_DHCP.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# XXX: This script is a workaround to the "LPF/icmp socket permission denied that we're experiencing"
+# XXX: This is NOT A FIX
+# XXX: It restarts the DHCP server if needed
+
 . /usr/pluto/bin/pluto.func
 . /usr/pluto/bin/LockUtils.sh
 . /usr/pluto/bin/Network_Parameters.sh
@@ -8,9 +12,16 @@
 if [ ! -e /etc/dhcp3/dhcpd.conf.pbackup ] ;then
 	cp /etc/dhcp3/dhcpd.conf /etc/dhcp3/dhcpd.conf.pbackup
 fi
-WaitLock "PlutoDHCP" "DHCP_Restart" nolog
-if ! BlacklistConfFiles '/etc/dhcp3/dhcpd.conf' ;then
-	/usr/pluto/bin/PlutoDHCP >/etc/dhcp3/dhcpd.conf
-	invoke-rc.d dhcp3-server restart
-fi
-Unlock "PlutoDHCP" "DHCP_Restart" nolog
+while :; do
+	WaitLock "PlutoDHCP" "DHCP_Restart" nolog
+	IsRunning=$(pgrep dhcpd3)
+	if [[ -z "$IsRunning" && -n "$DHCPsetting" ]]; then
+		if ! BlacklistConfFiles '/etc/dhcp3/dhcpd.conf' ;then
+			Logging "$TYPE" "$SEVERITY_CRITICAL" "DHCP_Restart" "DHCP not found running; restarting it"
+			/usr/pluto/bin/PlutoDHCP >/etc/dhcp3/dhcpd.conf
+			invoke-rc.d dhcp3-server restart
+		fi
+	fi
+	Unlock "PlutoDHCP" "DHCP_Restart" nolog
+	sleep 5
+done
