e4e0dc2323c7083a95dabdb5bf1ea670f20fbc6d
cmiron <cmiron>
2007-07-24 12:03:13 +0000

    Cleanup

--- AutoTests/Start_Automated.sh
+++ AutoTests/Start_Automated.sh
@@ -17,8 +17,5 @@ for IP in $IPS ;do
 
 	sleep 60
 
-#/usr/pluto/bin/MessageSend 10.0.0.89 -targetType template 0 26 1 67 241 1 13 "/usr/bin/wget" 51 "-l2	-nH	-r	-P	/tmp/	http://10.0.0.150/AutoTests/Scripts" 95 "-targetType template 0 26 1 67 13 \"/bin/bash\" 51 \"/tmp/AutoTests/Scripts/Start.sh\""
-
-
 	/usr/pluto/bin/MessageSend $IP -targetType template 0 26 1 67 241 1 13 "/usr/bin/wget" 51 "-l2	-nH	-r	-P	/tmp/	http://10.0.0.150/AutoTests/Scripts" 95 "0 @AppDev@ 1 67 13 \"/bin/bash\" 241 1 51 \"/tmp/AutoTests/Scripts/Start.sh\""
 done
--- AutoTests/Stop_Automated.sh
+++ AutoTests/Stop_Automated.sh
@@ -2,8 +2,6 @@
 
 IPS="10.0.0.85 10.0.0.89 10.0.0.86"
 
-#/usr/pluto/bin/MessageSend 10.0.0.89 -targetType template 0 26 1 67 241 1 13 "/usr/bin/wget" 51 "-l2	-nH	-r	-P	/tmp/	http://10.0.0.150/AutoTests/Scripts" 95 "-targetType template 0 26 1 67 13 \"/bin/bash\" 51 \"/tmp/AutoTests/Scripts/Stop.sh\""
-
 for IP in $IPS ;do
 /usr/pluto/bin/MessageSend $IP -targetType template 0 26 1 67 241 1 13 "/usr/bin/wget" 51 "-l2	-nH	-r	-P	/tmp/	http://10.0.0.150/AutoTests/Scripts" 95 "0 @AppDev@ 1 67 13 \"/bin/bash\" 241 1 51 \"/tmp/AutoTests/Scripts/Stop.sh\""
 done
