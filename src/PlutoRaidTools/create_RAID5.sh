4f55b63f2d956ea45fb038124168dd54ef16ee71
rgavril <rgavril>
2007-07-24 11:19:56 +0000

    Add progress indicator used by pluto web admin

--- src/PlutoRaidTools/create_RAID5.sh
+++ src/PlutoRaidTools/create_RAID5.sh
@@ -67,10 +67,34 @@ if [[ "$3" != "demonized" ]] ;then
 	fi
 else
 	## Format the new raid
-	echo "y" | mkfs.ext3 $2
+	echo "FORMAT,1" > /tmp/${Device}_Raid.log
+
+	echo "y" | mkfs.ext3 $2 > /var/log/mkfs_${Device} &
+
+	percent=0
+	while "$percent" != 100 ;do
+		sleep 4
+		mkfsOutput=$(cat /var/log/mkfs_${Device}  | grep "Writing inode tables: " | sed -r 's/.*[^0-9]([0-9]+\/[0-9]+|done)[^0-9]*/\1/')a
+
+		if [[ "$mkfsOutput" == "" ]] ;then
+			percent=1
+
+		elif [[ "$mkfsOutput" == "done" ]] ;then
+			percent=100
+
+		else
+			done=$(echo $mkfsOutput | cut -d'/' -f1)
+			total=$(echo $mkfsOutput | cut -d'/' -f2)
+			percent=$(((done * 100) / total ))
+		fi
+
+		echo "FORMAT,$percent" > /tmp/${Device}_Raid.log
+	done
+
 
 	## Create a pluto directory structure (depending on UsePlutoDirStructure device data)
 	/usr/pluto/bin/StorageDevices_PlutoDirStructure.sh -d $Device
 	/usr/pluto/bin/StorageDevices_Setup.sh
-			
+	
+	rm /tmp/${Device}_Raid.log
 fi
