c243cadc1128db6bb9940f3e2486020c7eaf62a8
rgavril <rgavril>
2007-07-25 11:12:04 +0000

    Changed to sane indentation so i can read the damn file. I think even bash inerpreter can get lost in such a bad indented code

--- src/PlutoRaidTools/monitoring_RAID.sh
+++ src/PlutoRaidTools/monitoring_RAID.sh
@@ -24,122 +24,144 @@ else
 fi
 
 case "$event" in
-	#"DeviceDisappeared" )
-	 #Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
-	 #DeviceID=$(RunSQL "$Q")
-	 #Q="UPDATE Device_DeviceData SET IK_DeviceData = 3 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
-	 #$(RunSQL "$Q")
-	 #Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Device appears to no longer be configured' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-	 #$(RunSQL "$Q")
-	#;;
+#	"DeviceDisappeared" )
+#		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
+#		DeviceID=$(RunSQL "$Q")
+#		Q="UPDATE Device_DeviceData SET IK_DeviceData = 3 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
+#		$(RunSQL "$Q")
+#		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Device appears to no longer be configured' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+#		$(RunSQL "$Q")
+#	;;
+
 	"RebuildStarted" )
-	 Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
-     DeviceID=$(RunSQL "$Q")
-     Q="UPDATE Device_DeviceData SET IK_DeviceData = 1 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
-     RunSQL "$Q"
-	 Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Building Started' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-	 RunSQL "$Q"
-	raidSize=$(mdadm --query $md | head -1 |cut -d' ' -f2)
-	Q="UPDATE Device_DeviceData SET IK_DeviceData = '$raidSize' WHERE FK_Device = $DeviceID and FK_DeviceData = $DISK_SIZE_ID"
-	RunSQL "$Q"
-	 ;; 
+		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
+		DeviceID=$(RunSQL "$Q")
+
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 1 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
+		RunSQL "$Q"
+
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Building Started' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+		RunSQL "$Q"
+
+		raidSize=$(mdadm --query $md | head -1 |cut -d' ' -f2)
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = '$raidSize' WHERE FK_Device = $DeviceID and FK_DeviceData = $DISK_SIZE_ID"
+		RunSQL "$Q"
+	;; 
+
 	"RebuildFinished" )
-	Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
-	DeviceID=$(RunSQL "$Q")
-	Q="UPDATE Device_DeviceData SET IK_DeviceData = 4 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
-	RunSQL "$Q"
-	Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Done' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID" 
-	RunSQL "$Q"
-	raidSize=$(mdadm --query $md | head -1 |cut -d' ' -f2)
-	Q="UPDATE Device_DeviceData SET IK_DeviceData = '$raidSize' WHERE FK_Device = $DeviceID and FK_DeviceData = $DISK_SIZE_ID"
-	RunSQL "$Q"
+		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
+		DeviceID=$(RunSQL "$Q")
+
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 4 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
+		RunSQL "$Q"
+
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Done' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID" 
+		RunSQL "$Q"
+
+		raidSize=$(mdadm --query $md | head -1 |cut -d' ' -f2)
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = '$raidSize' WHERE FK_Device = $DeviceID and FK_DeviceData = $DISK_SIZE_ID"
+		RunSQL "$Q"
 	;;
+
 	"Rebuild"* )
-	 Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
-     DeviceID=$(RunSQL "$Q")
-     Q="UPDATE Device_DeviceData SET IK_DeviceData = 1 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
-     RunSQL "$Q"
-	 len=$((${#event}))
-	 progress=${event:$len-2:$len}
-	 Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Building in progress ($progress %)' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-	 RunSQL "$Q"
-	raidSize=$(mdadm --query $md | head -1 |cut -d' ' -f2)
-	Q="UPDATE Device_DeviceData SET IK_DeviceData = '$raidSize' WHERE FK_Device = $DeviceID and FK_DeviceData = $DISK_SIZE_ID"
-	RunSQL "$Q"
-	 ;;
+		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
+		DeviceID=$(RunSQL "$Q")
+
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 1 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
+		RunSQL "$Q"
+
+		len=$((${#event}))
+		progress=${event:$len-2:$len}
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Building in progress ($progress %)' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+		RunSQL "$Q"
+
+		raidSize=$(mdadm --query $md | head -1 |cut -d' ' -f2)
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = '$raidSize' WHERE FK_Device = $DeviceID and FK_DeviceData = $DISK_SIZE_ID"
+		RunSQL "$Q"
+	;;
 
 	"Fail" )
-	failedDevs=$(mdadm --detail $md | awk '/faulty/ {print $6}')
-	for failedDev in $failedDevs; do
-		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$failedDev' AND FK_DeviceData = $BLOCK_DEVICE_ID"
+		failedDevs=$(mdadm --detail $md | awk '/faulty/ {print $6}')
+		for failedDev in $failedDevs; do
+			Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$failedDev' AND FK_DeviceData = $BLOCK_DEVICE_ID"
+			DeviceID=$(RunSQL "$Q")
+			Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Failed disk' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+			RunSQL "$Q"
+		done
+
+		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
 		DeviceID=$(RunSQL "$Q")
-		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Failed disk' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-		RunSQL "$Q"
-	done
-	Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
-	DeviceID=$(RunSQL "$Q")
-	Q="SELECT COUNT(PK_Device) FROM Device WHERE FK_DeviceTemplate = $HARD_DRIVE_DEVICE_TEMPLATE AND FK_Device_ControlledVia = $DeviceID"
-	HardDriveNr=$(RunSQL "$Q")
-	Q="SELECT COUNT(IK_DeviceData)
-	   FROM Device_DeviceData 
-	   INNER JOIN Device ON Device_DeviceData.FK_Device = Device.PK_Device 
-	   WHERE Device.FK_Device_ControlledVia = $DeviceID AND
-	         Device_DeviceData.FK_DeviceData = $STATE_ID AND 
-		 Device_DeviceData.IK_DeviceData = 'Failed disk'"
-        FailedDriveNr=$(RunSQL "$Q")
-	Q="SELECT FK_DeviceTemplate FROM Device WHERE PK_Device = $DeviceID"
-	RaidTemplate=$(RunSQL "$Q")
-	case "$RaidTemplate" in 
-		"$RAID5_DEVICE_TEMPLATE" | "$RAID1_DEVICE_TEMPLATE" )
-		if (( $HardDriveNr == 2 && $FailedDriveNr == 1 )) ||  (( $FailedDriveNr > 1 )) ;then
-			Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Damaged. Device appears to no longer be configured ' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-			RunSQL "$Q"
-		else	
-			Q="UPDATE Device_DeviceData SET IK_DeviceData = 'One active disk is down. In order to correct the problem please add a spare disk. If you already have a spare disk attached, rebuilding will automatically start!' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-			RunSQL "$Q"
-		fi
-		;;
-		"$RAID0_DEVICE_TEMPLATE" )
-		if (( $FailedDriveNr >= 1 )) ;then
-			Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Damaged. Device appears to no longer be configured ' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-			RunSQL "$Q"
-		fi
-		;;
-	esac
+
+		Q="SELECT COUNT(PK_Device) FROM Device WHERE FK_DeviceTemplate = $HARD_DRIVE_DEVICE_TEMPLATE AND FK_Device_ControlledVia = $DeviceID"
+		HardDriveNr=$(RunSQL "$Q")
+
+		Q="SELECT COUNT(IK_DeviceData)
+		   FROM Device_DeviceData 
+		   INNER JOIN Device ON Device_DeviceData.FK_Device = Device.PK_Device 
+		   WHERE Device.FK_Device_ControlledVia = $DeviceID AND
+			 Device_DeviceData.FK_DeviceData = $STATE_ID AND 
+			 Device_DeviceData.IK_DeviceData = 'Failed disk'"
+		FailedDriveNr=$(RunSQL "$Q")
+
+		Q="SELECT FK_DeviceTemplate FROM Device WHERE PK_Device = $DeviceID"
+		RaidTemplate=$(RunSQL "$Q")
+		case "$RaidTemplate" in 
+			"$RAID5_DEVICE_TEMPLATE" | "$RAID1_DEVICE_TEMPLATE" )
+				if (( $HardDriveNr == 2 && $FailedDriveNr == 1 )) ||  (( $FailedDriveNr > 1 )) ;then
+					Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Damaged. Device appears to no longer be configured ' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+					RunSQL "$Q"
+				else	
+					Q="UPDATE Device_DeviceData SET IK_DeviceData = 'One active disk is down. In order to correct the problem please add a spare disk. If you already have a spare disk attached, rebuilding will automatically start!' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+					RunSQL "$Q"
+				fi
+			;;
+
+			"$RAID0_DEVICE_TEMPLATE" )
+				if (( $FailedDriveNr >= 1 )) ;then
+					Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Damaged. Device appears to no longer be configured ' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+					RunSQL "$Q"
+				fi
+			;;
+		esac
 	;;
+
 	"FailSpare" )
-	Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$device' AND FK_DeviceData = $BLOCK_DEVICE_ID"
-	DeviceID=$(RunSQL "$Q")
-	Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Spare failed to rebuild a faulty disk' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-	RunSQL "$Q"
+		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$device' AND FK_DeviceData = $BLOCK_DEVICE_ID"
+		DeviceID=$(RunSQL "$Q")
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Spare failed to rebuild a faulty disk' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+		RunSQL "$Q"
 	;;
+
 	"SpareActive" )
-	 Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$device' AND FK_DeviceData = $BLOCK_DEVICE_ID"
-	 DeviceID=$(RunSQL "$Q")
-	 Q="UPDATE Device_DeviceData SET IK_DeviceData = 'OK' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
-	 RunSQL "$Q"
-	 Q="UPDATE Device_DeviceData SET IK_DeviceData = '0' WHERE FK_Device = $DeviceID and FK_DeviceData = $SPARE_ID"
-	 RunSQL "$Q"
+		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$device' AND FK_DeviceData = $BLOCK_DEVICE_ID"
+		DeviceID=$(RunSQL "$Q")
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'OK' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID"
+		RunSQL "$Q"
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = '0' WHERE FK_Device = $DeviceID and FK_DeviceData = $SPARE_ID"
+		RunSQL "$Q"
 	;;
+
 	"DegradedArray" )
-	Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
-	DeviceID=$(RunSQL "$Q")
-	Q="UPDATE Device_DeviceData SET IK_DeviceData = 5 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
-	RunSQL "$Q"
-	Q="SELECT COUNT(IK_DeviceData)
-	   FROM Device_DeviceData 
-	   INNER JOIN Device ON Device_DeviceData.FK_Device = Device.PK_Device 
-	   WHERE Device.FK_Device_ControlledVia = $DeviceID AND
-	         Device_DeviceData.FK_DeviceData = $SPARE_ID AND 
-		 Device_DeviceData.IK_DeviceData = 1"
-        SpareNr=$(RunSQL "$Q")
-	if (( $SpareNr >= 1 )) ;then
-		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Start Building' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID" 
+		Q="SELECT FK_Device FROM Device_DeviceData WHERE IK_DeviceData = '$md'  AND FK_DeviceData = $BLOCK_DEVICE_ID"
+		DeviceID=$(RunSQL "$Q")
+
+		Q="UPDATE Device_DeviceData SET IK_DeviceData = 5 WHERE FK_Device = $DeviceID and FK_DeviceData = $NEW_ADD_ID"
 		RunSQL "$Q"
-	else
-		Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Degraded device. Add a spare disk to correct the problem!' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID" 
-		RunSQL "$Q"
-	fi
+
+		Q="SELECT COUNT(IK_DeviceData)
+		   FROM Device_DeviceData 
+		   INNER JOIN Device ON Device_DeviceData.FK_Device = Device.PK_Device 
+		   WHERE Device.FK_Device_ControlledVia = $DeviceID AND
+			 Device_DeviceData.FK_DeviceData = $SPARE_ID AND 
+			 Device_DeviceData.IK_DeviceData = 1"
+		SpareNr=$(RunSQL "$Q")
+		if (( $SpareNr >= 1 )) ;then
+			Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Start Building' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID" 
+			RunSQL "$Q"
+		else
+			Q="UPDATE Device_DeviceData SET IK_DeviceData = 'Degraded device. Add a spare disk to correct the problem!' WHERE FK_Device = $DeviceID and FK_DeviceData = $STATE_ID" 
+			RunSQL "$Q"
+		fi
 	;;
 esac
 	
