c848c13831f9b8fb4c6fc4b0f169f781fed8f134
rgavril <rgavril>
2007-07-27 16:57:22 +0000

    Fix Backup / Restore

--- src/BootScripts/PlutoRestorePoint.sh
+++ src/BootScripts/PlutoRestorePoint.sh
@@ -7,17 +7,17 @@ BKPDIR=$(date +%Y-%m-%d)
 FULLPATH="$MASTERDIR/download/$BKPDIR"
 
 Usage () {
-echo "$0 [command]"
-echo "Commands :"
-echo "	--backup -> create a system backup"
-echo "	--restore -> restore system from a backup file"
-echo "	--restore --skip-md5 -> system restore without md5 control file"
-exit
+	echo "$0 [command]"
+	echo "Commands :"
+	echo "	--backup -> create a system backup"
+	echo "	--restore -> restore system from a backup file"
+	echo "	--restore --skip-md5 -> system restore without md5 control file"
+	exit
 }
 
 
 if [[ -z $1 ]]; then
-Usage
+	Usage
 fi
 
 # create restore point
@@ -31,6 +31,7 @@ if [[ "$1" == "--backup" ]]; then
 	if [[ ! -d $BKPDIR ]]; then
 		mkdir -p $MASTERDIR/download/$BKPDIR
 	fi
+	
 	# backup mysql 
 	mkdir -p $FULLPATH/mysql
 	dbtables=$(mysql -u root -D pluto_main -N -e "select Tablename from psc_local_tables")
@@ -40,22 +41,23 @@ if [[ "$1" == "--backup" ]]; then
 	do
 		/usr/bin/mysqldump -e --quote-names --allow-keywords --add-drop-table -u root pluto_main $table  > $FULLPATH/mysql/$table-$BKPDIR.sql
 	done
+	
 	# backup pluto-admin
 	mkdir -p $FULLPATH/usr/pluto/orbiter
 	cp -r /usr/pluto/orbiter/floorplans $FULLPATH/usr/pluto/orbiter
 	cp -r /usr/pluto/orbiter/users $FULLPATH/usr/pluto/orbiter
 	cp -r /usr/pluto/orbiter/rooms $FULLPATH/usr/pluto/orbiter
 	cp -r /usr/pluto/orbiter/scenarios $FULLPATH/usr/pluto/orbiter
+	
 	# backup conf files
 	mkdir -p $FULLPATH/etc
 	cp -r /etc/pluto.conf $FULLPATH/etc
-	#cp -r /etc/passwd $FULLPATH/etc
-	#cp -r /etc/shadow $FULLPATH/etc
-	#cp -r /etc/apache2 $FULLPATH/etc
+	
 	# make archive
 	cd $MASTERDIR/download	
 	tar cfj backup-$BKPDIR.tar.bz2 $BKPDIR
 	md5sum backup-$BKPDIR.tar.bz2 > backup-$BKPDIR.md5
+
 	echo "Restore point created."
 	rm -rf $FULLPATH
 fi  
@@ -64,57 +66,34 @@ fi
 if [[ "$1" == "--restore" ]]; then
 	cd $MASTERDIR/upload
 	backupfile=$(ls $MASTERDIR/upload | grep tar)
-	#md5file=$(ls $MASTERDIR/upload | grep md5)
-	#if [[ "$2" == "--skip-md5" ]]; then
 	
-		if [[ "$backupfile" != "" ]]; then
-			tar xfj "$backupfile"
-			BKPDIR=$(find $MASTERDIR/upload -type d -maxdepth 1 -mindepth 1)
-			# process data from backup
-			cp -r $BKPDIR/etc/* /etc
-			cp -r $BKPDIR/usr/* /usr
-			chown -R  www-data /usr/pluto/orbiter/floorplans/
-			# restore mysql
-			cd $BKPDIR/mysql
-			dbtables=$(ls)
-			# restore old mysql data
-			for table in $dbtables
-			do
-				/usr/bin/mysql -u root -D pluto_main < $table
-			done
-			rm -rf $MASTERDIR/upload/*
-			rmdir $MASTERDIR/upload
-		fi
-	#else	
-	#	if [[ "$md5file" == "" ]]; then
-	#		echo "Control file not found. Exiting ..."
-	#	fi
-	#	md5test=$(md5sum -c $md5file | awk '{print $2}')
-	#	if [[ "$md5test" != "OK" ]]; then
-	#		echo "File integrity check failed ! Process stopped !"
-	#		exit 1
-		#else 
-		#	echo ""
-         #       	if [[ "$backupfile" != "" ]]; then
-          #              	tar xfj "$backupfile"
-           #             	BKPDIR=$(find $MASTERDIR/upload -type d -maxdepth 1 -mindepth 1)
-           #             	# process data from backup
-           #             	cp -r $BKPDIR/etc/* /etc
-           #             	cp -r $BKPDIR/usr/* /usr
-		   #  				chown -R  www-data /usr/pluto/orbiter/floorplans/
-           #             	# restore mysql
-           #             	cd $BKPDIR/mysql
-           #             	dbtables=$(ls)
-           #             	# restore old mysql data
-           #             	for table in $dbtables
-		   #	            	do
-           #                     	/usr/bin/mysql -u root -D pluto_main < $table
-           #             	done
-           #             	rm -rf $MASTERDIR/upload/*
-		   #					rmdir $MASTERDIR/upload
-           #     	fi
+	if [[ "$backupfile" != "" ]]; then
+		tar xfj "$backupfile"
+		BKPDIR=$(find $MASTERDIR/upload -maxdepth 1 -mindepth 1 -type d)
+		# process data from backup
+		cp -L -r $BKPDIR/etc/* /etc
+		cp -L -r $BKPDIR/usr/* /usr
+		chown -R  www-data /usr/pluto/orbiter/floorplans/
+		# restore mysql
+		cd $BKPDIR/mysql
+		dbtables=$(ls)
+		# restore old mysql data
+		for table in $dbtables
+		do
+			/usr/bin/mysql -u root -D pluto_main < $table
+		done
 
-		#fi
-	#fi
+		## Reinstall this packages so we'll have an updates database schema
+		apt-get -y --reinstall install pluto-system-database
+		apt-get -y --reinstall install pluto-media-database
+		apt-get -y --reinstall install pluto-local-database
+		apt-get -y --reinstall install pluto-security-database
+		apt-get -y --reinstall install pluto-telecom-database
+
+		rm -rf $MASTERDIR/upload/*
+		rmdir $MASTERDIR/upload
+	fi
+
+
 fi
 
