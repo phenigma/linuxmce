ef06676734e8d3dfb009ca384d5001ff2eaea68a
radu <radu.cx+linuxmce@gmail.com>
2007-08-07 16:06:58 +0000

    imported local changes

--- src/NewMD_interactor/90_start_interactor.sh
+++ src/NewMD_interactor/90_start_interactor.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/dash
 
 MyIP=$(/sbin/ifconfig eth0 | awk 'NR==2 { print substr($2, index($2, ":") + 1) }')
 MyMAC=$(/sbin/ifconfig eth0 | awk 'NR==1 { print $5 }')
 Gateway=$(/sbin/route -n | awk '/^0\.0\.0\.0/ { print $2 }')
 
 Architecture="686"
-if grep -q "^processor.: x" /proc/cpuinfo ;then
+is2ndCpu=$(awk '/processor.*: 1/' /proc/cpuinfo)
+if [ -n "$is2ndCpu"  ] ;then
 	Architecture="686-smp"
 fi
 
