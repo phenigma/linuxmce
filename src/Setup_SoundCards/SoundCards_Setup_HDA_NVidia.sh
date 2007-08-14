3d44ab63421298d648321f76359adf85babf561e
aaron <aaron.b@plutohome.com>
2007-08-14 18:44:30 +0000

    sound setup

--- src/Setup_SoundCards/SoundCards_Setup_HDA_NVidia.sh
+++ src/Setup_SoundCards/SoundCards_Setup_HDA_NVidia.sh
@@ -5,4 +5,7 @@ AudioSettings="$1"
 if [[ "$AudioSettings" == *[CO]* ]]; then
 	# S/PDIF needs this to be set like this
 	amixer sset 'IEC958' unmute >/dev/null
+else
+	# S/PDIF needs this to be set like this
+	amixer sset 'IEC958' mute >/dev/null
 fi
