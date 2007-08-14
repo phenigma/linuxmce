1378c7a83bc180fe718fae9dec3c4b758ef5e0c4
aaron <aaron.b@plutohome.com>
2007-08-14 23:21:50 +0000

    tv

--- src/pnp_detection_scripts/20_VidikronTV.sh
+++ src/pnp_detection_scripts/20_VidikronTV.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Vidikron script queue $2"
-/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "[S3E0001\s[S3E0000" -i 1 -s "]0000"
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 19200 -t "[S3E0001\s500m[S3E0000" -i 1 -s "]0000"
 if [[ "$?" -ne 0 ]]; then
 echo "It's not a Vidikron"
 /usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 0
