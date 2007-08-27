a4839c15999a09c26011c95aa57ad00f67b2cb14
rgavril <rgavril>
2007-08-27 12:42:04 +0000

    Reallocated all ip addresses when 'refresh_all' passed on command line

--- src/BootScripts/DHCP_config.sh
+++ src/BootScripts/DHCP_config.sh
@@ -4,12 +4,31 @@
 . /usr/pluto/bin/Network_Parameters.sh
 . /usr/pluto/bin/Utils.sh
 
+function refresh_all {
+	Q="SELECT PK_Device, IPaddress from Device WHERE IPaddress != ''"
+	R=$(RunSQL "$Q")
+
+	for Device in $R ;do
+		Device_Id=$(Field "1" "$Device")
+		Device_IP=$(Field "2" "$Device")
+
+		RunSQL "UPDATE Device SET IPaddress='' WHERE PK_Device = $Device_Id"
+		/usr/pluto/bin/PlutoDHCP -d "$Device_Id" -a
+	done
+
+}
+
 WaitLock "PlutoDHCP" "PlutoDHCP_config"
 if [[ -z "$DHCPsetting" ]]; then
 	rm -f /etc/dhcp3/dhcpd.conf
 	invoke-rc.d dhcp3-server stop
 	exit
 fi
+
+if [[ "$1" == "refresh_all" ]] ;then
+	refresh_all
+fi
+
 if ! BlacklistConfFiles '/etc/dhcp3/dhcpd.conf' ;then
 	if [ ! -e /etc/dhcp3/dhcpd.conf.pbackup ] ;then
 		cp /etc/dhcp3/dhcpd.conf /etc/dhcp3/dhcpd.conf.pbackup
