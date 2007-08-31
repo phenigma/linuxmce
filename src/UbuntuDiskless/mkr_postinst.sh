da283b2e828319b06752140ec91022693b9a9e39
rgavril <rgavril>
2007-08-31 16:21:33 +0000

    Build the image only once

--- src/UbuntuDiskless/mkr_postinst.sh
+++ src/UbuntuDiskless/mkr_postinst.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 /usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
 
-/usr/pluto/bin/Diskless_BuildDefaultImage.sh || echo "WARNING: Diskless_BuildDefaultImage.sh returned an error!"
+if [[ "$1" != "upgrade" ]] ;then
+	/usr/pluto/bin/Diskless_BuildDefaultImage.sh || echo "WARNING: Diskless_BuildDefaultImage.sh returned an error!"
+fi
 
 # summary of how this script can be called:
 #        * <postinst> `configure' <most-recently-configured-version>
-if [[ "$2" != "" ]] ;then
-	/usr/pluto/bin/Diskless_CreateTBZ.sh || echo "WARNING: Diskless_CreateTBZ.sh returned an error!"
-fi
+#if [[ "$2" != "" ]] ;then
+#	/usr/pluto/bin/Diskless_CreateTBZ.sh || echo "WARNING: Diskless_CreateTBZ.sh returned an error!"
+#fi
 
 #update-rc.d -f haltMDs start 01 0 6 . || /bin/true
 update-rc.d -f haltMDs stop 01 0 6 . || /bin/true
