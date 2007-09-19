1ef26799ce83df0c21bc3cd12c1065d0ed18c7fd
brian <brian>
2007-09-19 12:42:16 +0000

    Adding API documentation

--- src/Xine_Player/Xine_Stream.h
+++ src/Xine_Player/Xine_Stream.h
@@ -4,7 +4,7 @@
      www.plutohome.com
 
      Phone: +1 (877) 758-8648
- 
+
 
      This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
      This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
@@ -13,11 +13,9 @@
      See the GNU General Public License for more details.
 
 */
-/**
-*
-* @file Xine_Stream.h
-* @brief header for Xine_Stream class
-*
+
+/** @file Xine_Stream.h
+* Header for Xine_Stream class
 */
 
 #ifndef XINE_STREAM_H
@@ -38,11 +36,16 @@
 
 #include "Xine_Stream_Factory.h"
 
-
+/** @namespace DCE
+D
+*/
 namespace DCE
 {
 	class Xine_Stream_Factory;
-	
+
+    /** @class Xine_Stream
+    For ???
+    */
 	class Xine_Stream
 	{
 		public:
@@ -71,32 +74,32 @@ namespace DCE
 				PLAYBACK_FF_32 = 32000,
 				PLAYBACK_FF_64 = 64000,
 			} PlayBackSpeedType;
-		
+
 			// media info
 			string m_sCurrentFile;
 			int m_iChapter;
 			int m_iTitle;
-			
+
 			void DisplaySpeedAndTimeCode();
 			void Seek(int pos,int tolerance_ms);
-			
+
 			//returns complex position construction
 			string GetPosition();
 			bool m_bDontReportCompletion;
-			
+
 			//reports timecode
 			void ReportTimecode();
-						
+
 			static int CalculatePosition(string &sMediaPosition,string *sMRL,int *Subtitle,int *Angle,int *AudioTrack);
-			
+
 			bool hasChapters();
-			
+
 		private:
 			// stream access controlling mutex
 			pluto_pthread_mutex_t m_streamMutex;
-			
+
 			bool m_bInitialized;
-			
+
 			Xine_Stream_Factory *m_pFactory;
 
 			// xine resources for the stream:
@@ -108,10 +111,10 @@ namespace DCE
 			//  audio and video drivers
 			string m_sXineAudioDriverName;
 			string m_sXineVideoDriverName;
-			
+
 			// actually used drivers
 			string m_sUsedVideoDriverName;
-			
+
 			// xine library link
 			xine_t *m_pXineLibrary;
 			// stream data objects
@@ -119,27 +122,27 @@ namespace DCE
 			xine_event_queue_t *m_pXineStreamEventQueue;
 			// post-processing plugins
 			xine_post_t *m_pXineVisualizationPlugin, *m_pXineDeinterlacePlugin;
-	
+
 			// stream properties
 			bool m_bHasVideo;
 			bool m_bHasAudio;
-			
+
 			string m_sMediaInfo;
-			
+
 			int m_iPlaybackSpeed;
-			
+
 			bool getRealStreamPosition(int &positionTime, int &totalTime);
 			int m_iCachedStreamPosition;
 			int m_iCachedStreamLength;
 		public:
-			int m_iMenuButtons;			
+			int m_iMenuButtons;
 			void FireMenuOnScreen(int iButtons);
                         void SendMessageToOrbiter(string sMessage);
-                        
+
 		private:
-			
+
 			int m_iSeekMuteStatus;
-			
+
 			string readMediaInfo();
 			void ReadAVInfo();
 			void SendAVInfo();
@@ -148,14 +151,14 @@ namespace DCE
 			bool m_bIsVDR;   // Is this live tv from VDR?
 			string m_sAudioInfo;
 			string m_sVideoInfo;
-			
+
 			bool m_bUseDeinterlacing;
 			string m_sDeinterlacingConfig;
                         int m_iMediaID;
                         string m_sMediaType;
-                        
+
                         void UpdateTitleChapterInfo();
-			
+
 		private:
 			int m_iSpecialSeekSpeed;
 			int m_iSpecialOneTimeSeek;
@@ -168,30 +171,30 @@ namespace DCE
 			Window windows[ 2 ];
 			Cursor cursors[ 2 ];
 			Pixmap noCursor;
-						
+
 			// creates X11 windows for output
 			bool CreateWindows();
 			bool DestroyWindows();
-			
+
 			// inits xine A/V output ports
 			bool InitXineAVOutput();
-			
+
 			// events processing thread
 			pthread_t threadEventLoop;
-			
+
 			int XServerEventProcessor(XEvent &event );
-			
+
 			// stream thread state
 			bool m_bExitThread;
-			
+
 			public:
 			bool m_bIsRendering;
-			
+
 			private:
 			// Do the seeking in these functions
 			void StartSpecialSeek( int Speed );
 			void StopSpecialSeek();
-			
+
 			void HandleSpecialSeekSpeed();  // Used for handling rewind and fast forward
 
 			// Used to handle rewind and fastforward to keep track of where we last seeked
@@ -203,10 +206,10 @@ namespace DCE
 
 			// OSD xine object
 			xine_osd_t *m_xine_osd_t;
-			
+
 			int m_iPrebuffer;
-			
-			
+
+
 			public:
 			int m_iStreamID;
 			bool m_bBroadcastOnly;
@@ -221,7 +224,7 @@ namespace DCE
 			 * @brief Will convert a point to coordinates relative to the playback window.
 			 */
 			int translate_point( int gui_x, int gui_y, int *video_x, int *video_y );
-			
+
 			bool setDebuggingLevel( bool newValue );
 
 			bool m_isSlimClient;
@@ -229,17 +232,17 @@ namespace DCE
 		public:
 			Xine_Stream(Xine_Stream_Factory* pFactory, xine_t *pXineLibrary, int ID=0, int iTimeCodeReportFrequency=0, int iRequestingObject=0, bool bBroadcastOnly=false);
 			~Xine_Stream();
-			
+
 			/**
 		 	* @brief Find current position for stream.
 			 */
 			int getStreamPlaybackPosition( int &positionTime, int &totalTime, int attemptsCount=10, bool *getResult=NULL, bool alwaysFromCache=false );
 
-			
+
 			// utility functions
 			bool StartupStream();
 			bool ShutdownStream();
-			
+
 			bool EnableDeinterlacing();
 			void DisableDeinterlacing();
 			bool EnableVisualizing();
@@ -250,15 +253,15 @@ namespace DCE
 			static void XineStreamEventListener( void *streamObject, const xine_event_t *event );
 			//  X events processor
 			static void *EventProcessingLoop( void *arguments );
-			
-			
+
+
 			// media functions
 			bool OpenMedia(string fileName, string &sMediaInfo, string sMediaPosition);
 			bool CloseMedia();
 
-			
+
 			bool playStream( string mediaPosition);
-			
+
     /**
 			 * @brief change the stream playback speed.
 		 */
@@ -269,7 +272,7 @@ namespace DCE
 			 * @brief get current playback speed
 		 */
 			PlayBackSpeedType getPlaybackSpeed();
-			
+
 
     /**
 			 * @brief restart the stream playback.
@@ -283,7 +286,7 @@ namespace DCE
 
 			void pauseMediaStream();
 
-			
+
     /**
  		 * @brief Enable broadcasting on this stream
 		 */
@@ -294,29 +297,29 @@ namespace DCE
 			 * @brief send a Xine event to the engine
 		 */
 			void sendInputEvent( int eventID );
-			
+
     /**
 			 * @brief Take a screenshot of the currenly playing stream.
 		 */
 			void make_snapshot( string sFormat, int iWidth, int iHeight, bool bKeepAspect, char*&pData, int &iDataSize );
 			void grab_x_window( int &width, int &height, char*&pData, int &iDataSize );
-			
+
 			//void setPlaybackSpeed( int speed );
-			
+
 			bool setSubtitle( int Value );
 			bool setAudio( int Value );
 			int getSubtitle();
 			int getAudio();
-			
+
 			bool setAspectRatio(string);
-			
+
 			int m_iZoomLevel;
 			bool setZoomLevel(string sZL="");
-			
+
 			const char *TranslateLanguage( const char *abbreviation );
 
 			void playbackCompleted( bool bWithErrors );
-    
+
 			/**
 			 * @brief move the button down in a DVD menu.
 		 */
@@ -328,7 +331,7 @@ namespace DCE
 			void selectPrevButton();
 
 			void pushCurrentButton();
-			
+
     /**
 			 * @brief in a DVD playback this will go to the respective menu (RootMenu, etc.)
 		 */
@@ -365,14 +368,18 @@ namespace DCE
 																			 int *dest_x, int *dest_y,
 																			 int *dest_width, int *dest_height,
 																			 double *dest_pixel_aspect,
-																			 int *win_x, int *win_y 
+																			 int *win_x, int *win_y
 //<-mkr_b_via_b->
 																			, int *display_np
-//<-mkr_b_via_e->																			 
+//<-mkr_b_via_e->
 																			 );
 
 		public:
-			class Dynamic_Pointer
+
+            /** @class Dynamic_Pointer
+            For ???
+            */
+            class Dynamic_Pointer
 			{
 				public:
 					Dynamic_Pointer(
@@ -391,18 +398,18 @@ namespace DCE
 					time_t m_start_time;
 			};
 			Dynamic_Pointer *m_pDynamic_Pointer;
-	
+
 			void getScreenShot( int iWidth, int iHeight, char *&pData, int &iDataSize, string &sFormat, string &sCMD_Result );
 			string getRenderingWindowName();
 			int getDeviceId();
 			bool isSlimClient();
 			void setSlimClient( bool isSlimClient );
 			void hideWindows();
-			
+
 	};
-	
-	
-	
+
+
+
 }
 
 #endif
--- src/Xine_Player/Xine_Stream_Factory.h
+++ src/Xine_Player/Xine_Stream_Factory.h
@@ -4,7 +4,7 @@
      www.plutohome.com
 
      Phone: +1 (877) 758-8648
- 
+
 
      This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
      This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
@@ -13,10 +13,9 @@
      See the GNU General Public License for more details.
 
 */
-/**
-*
-* @file Xine_Stream_Factory.h
-* @brief header for Xine_Stream_Factory class
+
+/** @file Xine_Stream_Factory.h
+Header for Xine_Stream_Factory class.
 *
 */
 
@@ -44,7 +43,7 @@ namespace DCE
 {
 	class Xine_Stream;
 	class Xine_Player;
-	
+
 	class Xine_Stream_Factory
 	{
 		friend class Xine_Stream;
@@ -52,80 +51,80 @@ namespace DCE
 			bool m_bInitialized;
 			xine_t *m_pXineLibrary;
 			string m_sConfigFile;
-			
+
 			string m_sXineAudioDriverName;
 			string m_sXineVideoDriverName;
-			
+
 			bool m_bUseDeinterlacing;
 			string m_sDeinterlacingConfig;
-			
+
 			// detects current output video and audio drivers
 			void DetectOutputDrivers();
-			
+
 			map<int, Xine_Stream*> streamsMap;
 			//map<int, Xine_Stream*> broadcastersMap;
-			
+
 			// factory access controlling mutex
 			pluto_pthread_mutex_t m_factoryMutex;
-			
+
 			// X subsystem properties
 			string m_sWindowTitle;
-			
+
 		public: // hack for g++-3.3
 			Display *m_pXDisplay;
 
 		private:
 			int m_iCurrentScreen;
 			int m_iCurrentWindow;
-			
+
 			// window init routines
 			bool CreateWindows();
 			bool DestroyWindows();
-			
+
 			// X11 windows
 			Window windows[ 2 ];
-			
+
 			int m_iImgWidth;
 			int m_iImgHeight;
 			int m_iImgXPos;
 			int m_iImgYPos;
-			
+
 			// owning player
 			Xine_Player *m_pPlayer;
-			
+
 			// identifying xine version and setting stuff
 			void IdentifyXineStuff();
-			
+
 		public:
 			Xine_Stream_Factory(Xine_Player *pOwner);
 			~Xine_Stream_Factory();
-			
+
 			bool StartupFactory();
 			bool ShutdownFactory();
 
 			string GetAudioDriver() { return m_sXineAudioDriverName; };
 			string GetVideoDriver() { return m_sXineVideoDriverName; };
-			
+
 			void setAudioSettings();
 			void setVideoDriver(string strVideoDriver);
-			
+
 			Xine_Stream *GetStream(int streamID, bool createIfNotExist=false, int requestingObject=-1, bool bBroadcasting=false, int iZoomLevel=100 );
-			
+
 			void ReportAudioTracks(string sTracks);
 			void ReportSubtitles(string sSubtitles);
 			void ReportTimecodeViaIP(int iStreamID, int Speed);
 			void ReportAVInfo( string sFilename, int iStreamID, string sMediaInfo, string sAudioInfo, string sVideoInfo );
-			
+
 			void CloseStreamAV(int iStreamID);
-			
+
 			void DestroyStream(int iStreamID);
-			
+
 			void HideStreamWindows(int iStreamID);
-			
+
 			int m_iLastRenderingStream;
-			
+
 			double m_dScreenPixelAspect;
-                        
+
                         void SendMessageToOrbiter(string sMessage);
 	};
 
