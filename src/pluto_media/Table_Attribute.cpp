6da5d3e6767d7d4cf99ed6905935c96f6759a307
cmiron <cmiron>
2007-09-28 11:17:03 +0000

    Regen'd pluto_media with the latest sql2cpp.

--- src/pluto_media/Table_Attribute.cpp
+++ src/pluto_media/Table_Attribute.cpp
@@ -302,7 +302,7 @@ PLUTO_SAFETY_LOCK_ERRORSONLY(sl,table->database->m_DBMutex);
 if (is_null[2])
 return "NULL";
 
-char *buf = new char[-1];
+char *buf = new char[5000000];
 db_wrapper_real_escape_string(table->database->m_pDB, buf, m_Name.c_str(), (unsigned long) min((size_t)-1,m_Name.size()));
 string s=string()+"\""+buf+"\"";
 delete[] buf;
