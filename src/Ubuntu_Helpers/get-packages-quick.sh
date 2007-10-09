4600e8f71cbb05b4ffe08362a6072e07919d1df4
automagic <automagic>
2007-10-09 11:31:56 +0000

    Started cleaning up all harcoded stuff for cd building

--- src/Ubuntu_Helpers/get-packages-quick.sh
+++ src/Ubuntu_Helpers/get-packages-quick.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
+
+## Decide on the name that the iso will have
 iso_name="$1"
 if [[ "$iso_name" == "" ]] ;then
 	iso_name="linuxmce-1.1"
 fi
 
+## Prepare the directory in whith the cd will be built
 if [ ! -e /var/plutobuild/cache-cd/deb-cache ] ;then
 	mkdir -p /var/plutobuild/cache-cd/deb-cache
 else
 	rm -f /var/plutobuild/cache-cd/deb-cache/*
 	rm -f /var/plutobuild/cache-cd/mce-installer*.deb
 fi
+
+## Remove old iso
 rm -f /var/www/$iso_name-packages.iso
 
-#ls /var/ubuntu/| grep -v pluto | xargs -i -t cp /var/ubuntu/'{}' /var/plutobuild/cache-cd/deb-cache
-#find /var/www/ -maxdepth 1 -type f -not -name 'pluto-src-*' -not -name 'Packages*' -print0 | xargs -0 -i -t cp '{}' /var/plutobuild/cache-cd/deb-cache
+## Copy the debs that where created in /var/www on this cd
 find /var/www/ -maxdepth 1 -type f -name '*.deb' -print0 | xargs -0 -i -t cp '{}' /var/plutobuild/cache-cd/deb-cache
 
+
+## Remove blacklisted packages from this cd
 while read file; do
 	rm -f "/var/plutobuild/cache-cd/deb-cache/$file"_*.deb
 done </root/Ubuntu_Helpers/RemoveFromCD.cfg
 
+## Add some extra packages to this cd
 ExtraPacks="libcucul0 libcaca0 libmad0 libxine1-ffmpeg xine-ui"
-
 for pack in $ExtraPacks ;do
 	cp /var/ubuntu/cachecd1-cache/$pack*  /var/plutobuild/cache-cd/deb-cache
 done
@@ -30,18 +36,19 @@ for pack in /var/moved-from-cache/* ;do
 	cp $pack /var/plutobuild/cache-cd/deb-cache
 done
 
+## Build a Packages.gz file in the deb-cache dir of the cd
 pushd /var/plutobuild/cache-cd/deb-cache
 	dpkg-scanpackages . /dev/null > Packages
 	cat Packages | gzip -9c > Packages.gz
 popd
 
+
+## Generate the iso
 pushd /var/plutobuild/
-cp /var/www/mce-installer*.deb ./cache-cd/
-#if [[ -z "$Version" ]]; then
-#	sql_slave_db="pluto_main_build"
-#	export Version=$(echo "select VersionName from Version" | mysql $sql_slave_db | tail -1);
-#fi
-#mkisofs -f -J -r -o "${iso_name}-packages.iso" ./cache-cd
-mkisofs -f -joliet-long -r -o "${iso_name}-packages.iso" ./cache-cd
-mv "${iso_name}-packages.iso" /var/www
+	cp /var/www/mce-installer*.deb ./cache-cd/
+	mkisofs -f -joliet-long -r -o "${iso_name}-packages.iso" ./cache-cd
+	mv "${iso_name}-packages.iso" /var/www
 popd
+
+## Clean the temporary dir
+rm -rf /var/plutobuild/cache-cd
