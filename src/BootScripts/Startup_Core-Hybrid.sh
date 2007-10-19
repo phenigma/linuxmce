193a68c2eb60c7f7619123d3a94eafc58cde2d36
radu <radu.cx+linuxmce@gmail.com>
2007-10-19 15:09:01 +0000

    clean up database in case of upgrade

--- src/BootScripts/Startup_Core-Hybrid.sh
+++ src/BootScripts/Startup_Core-Hybrid.sh
@@ -3,6 +3,23 @@
 invoke-rc.d mysql start
 
 . /usr/pluto/bin/Config_Ops.sh
+
+if [[ -f /etc/pluto/install_cleandb ]]; then
+	. /usr/pluto/bin/SQL_Ops.sh
+
+	# Reconfigure all devices after install
+	Q="UPDATE Device SET NeedConfigure=1, Status='**RUN_CONFIG**'"
+	RunSQL "$Q"
+
+	# Install the software again for the devices
+	Q="DELETE FROM Package_Device"
+	RunSQL "$Q"
+
+	/usr/pluto/bin/DHCP_config.sh
+
+	rm -f /etc/pluto/install_cleandb
+fi
+
 export DISPLAY=:${Display}
 XPID=$(</var/run/plutoX$Display.pid)
 if [[ -z "$XPID" || ! -d /proc/"$XPID" ]] ;then
