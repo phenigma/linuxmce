26b4cb5f42d1873bae936909fa1b42f13a3439d1
cmiron <cmiron>
2007-10-24 12:29:23 +0000

    Make the modification only for ARCH64. Revert it for 32bit arch

--- src/utilities/linux/window_manager/wmctrl/wmctrl.cpp
+++ src/utilities/linux/window_manager/wmctrl/wmctrl.cpp
@@ -1902,7 +1902,12 @@ gchar * WmCtrl::get_property(Display *disp, Window win,
     }
 
     /* null terminate the result to make string handling easier */
-    tmp_size = sizeof(Window) * ret_nitems;
+#ifdef ARCH64
+	tmp_size = sizeof(Window) * ret_nitems;
+#else
+	tmp_size = (ret_format / 8) * ret_nitems;
+#endif
+
     ret = (char *)g_malloc(tmp_size + 1);
     memcpy(ret, ret_prop, tmp_size);
     ret[tmp_size] = '\0';
