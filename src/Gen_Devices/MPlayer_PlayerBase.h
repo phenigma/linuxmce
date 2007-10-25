3a2cc2ddf3e8d90983c4ec0c04537f93c73d4a12
kirill.b <kirill.b>
2007-10-25 15:29:51 +0000

    regen

--- src/Gen_Devices/MPlayer_PlayerBase.h
+++ src/Gen_Devices/MPlayer_PlayerBase.h
@@ -102,6 +102,14 @@ public:
 	* @brief Device data access methods:
 	*/
 
+	string Get_Name()
+	{
+		if( m_bRunningWithoutDeviceData )
+			return m_pEvent_Impl->GetDeviceDataFromDatabase(m_dwPK_Device,DEVICEDATA_Name_CONST);
+		else
+			return m_mapParameters[DEVICEDATA_Name_CONST];
+	}
+
 };
 
 
@@ -206,6 +214,7 @@ public:
 	virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage) { };
 	Command_Impl *CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent);
 	//Data accessors
+	string DATA_Get_Name() { return GetData()->Get_Name(); }
 	//Event accessors
 	void EVENT_Playback_Completed(string sMRL,int iStream_ID,bool bWith_Errors) { GetEvents()->Playback_Completed(sMRL.c_str(),iStream_ID,bWith_Errors); }
 	void EVENT_Playback_Started(string sMRL,int iStream_ID,string sSectionDescription,string sAudio,string sVideo) { GetEvents()->Playback_Started(sMRL.c_str(),iStream_ID,sSectionDescription.c_str(),sAudio.c_str(),sVideo.c_str()); }
