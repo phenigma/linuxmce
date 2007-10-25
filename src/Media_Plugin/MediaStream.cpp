3e53e8a22105c61b8633ea36a2086ce69377f7a5
kirill.b <kirill.b>
2007-10-25 18:08:36 +0000

    HDDVD and Bluray are definitely video streams

--- src/Media_Plugin/MediaStream.cpp
+++ src/Media_Plugin/MediaStream.cpp
@@ -270,7 +270,9 @@ bool MediaStream::ContainsVideo()
 		m_iPK_MediaType==MEDIATYPE_np_DVD_CONST ||
 		m_iPK_MediaType==MEDIATYPE_np_VideoTape_CONST ||
 		m_iPK_MediaType==MEDIATYPE_np_LaserDisc_CONST ||
-		m_iPK_MediaType==MEDIATYPE_np_Game_CONST;
+		m_iPK_MediaType==MEDIATYPE_np_Game_CONST ||
+		m_iPK_MediaType==MEDIATYPE_pluto_HDDVD_CONST ||
+		m_iPK_MediaType==MEDIATYPE_pluto_BD_CONST;
 }
 
 
