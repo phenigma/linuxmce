cb453344fa69b0a73b284d5ab960dfdf288dc501
cmiron <cmiron>
2007-11-13 15:08:47 +0000

    Fixed join to conference of outside number.

--- src/Telecom_Plugin/Telecom_Plugin.cpp
+++ src/Telecom_Plugin/Telecom_Plugin.cpp
@@ -2119,9 +2119,9 @@ void Telecom_Plugin::CMD_PL_Join_Call(int iPK_Users,string sPhoneExtension,strin
 		sConferenceID = CallStatus::GetStringConferenceID( pCallStatus->GetConferenceID() );
 	}
 
-	if( !InternalMakeCall(0, sPhoneNumber, sConferenceID) )
+	if( !InternalMakeCall(0, sConferenceID, sPhoneNumber) )
 	{
-		sCMD_Result = "ERROR : couldn't make a call from " + sPhoneNumber + " to " + sConferenceID;
+		sCMD_Result = "ERROR : couldn't make a call from " + sConferenceID + " to " + sPhoneNumber;
 	}
 }
 
