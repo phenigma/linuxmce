68d5320e3e11838b467660f97d609f897d0fabba
rgavril <rgavril>
2007-11-26 10:53:50 +0000

    Disable old hacks

--- src/pluto-nvidia-drivers/mkr_postinst.sh
+++ src/pluto-nvidia-drivers/mkr_postinst.sh
@@ -7,18 +7,13 @@
 mkdir -p /etc/X11
 bash -x /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee-pluto /var/log/pluto/Xconfigure.log
 rm -f /etc/modprobe.d/lrm-video || :
-if ! BlacklistConfFiles '/etc/X11/xorg.conf' ;then
-	if [ ! -e /etc/X11/xorg.conf.pbackup ] ;then
-		cp /etc/X11/xorg.conf /etc/X11/xorg.conf.pbackup || :
-	fi
-fi 
-for KVer in <-mkr_t_MakeRelease_Kernel->; do
-	depmod "$KVer" || :
-done
+#for KVer in <-mkr_t_MakeRelease_Kernel->; do
+#	depmod "$KVer" || :
+#done
 
-mkdir -p /etc/rcS.d || :
-echo "#!/bin/bash
-modprobe -r nvidia
-modprobe nvidia
-" > /etc/rcS.d/99_fix_nvidia_kernel  || :
-chmod +x /etc/rcS.d/98_fix_nvidia_kernel || :
+#mkdir -p /etc/rcS.d || :
+#echo "#!/bin/bash
+#modprobe -r nvidia
+#modprobe nvidia
+#" > /etc/rcS.d/99_fix_nvidia_kernel  || :
+#chmod +x /etc/rcS.d/99_fix_nvidia_kernel || :
