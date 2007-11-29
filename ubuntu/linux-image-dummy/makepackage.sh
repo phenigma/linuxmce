550eaaaa02c40127e1d3c6ffeedb95673601ce42
rgavril <rgavril>
2007-11-29 13:23:01 +0000

    Build Package

--- ubuntu/linux-image-dummy/makepackage.sh
+++ ubuntu/linux-image-dummy/makepackage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 Moon_KernelVersion=$(uname -r)
+Moon_KernelArch=$(apt-config dump | grep 'APT::Architecture' | sed 's/APT::Architecture "\(.*\)".*/\1/g')
 Moon_RootLocation='package/'
 mkdir -p $Moon_RootLocation/{boot,lib/modules}
 
@@ -12,3 +13,10 @@ sed -i 's/^.*BOOT=.*/BOOT=nfs/g' /etc/initramfs-tools/initramfs.conf
 mkinitramfs -d /etc/initramfs-tools/ -o ${Moon_RootLocation}/boot/initrd.img-${Moon_KernelVersion}
 sed -i 's/^.*BOOT=.*/BOOT=local/g' /etc/initramfs-tools/initramfs.conf
 
+
+cp ${Moon_RootLocation}/DEBIAN/control{.in,}
+sed -i "s/{Kernel_Version}/${Moon_KernelVersion}/g" ${Moon_RootLocation}/DEBIAN/control
+sed -i "s/{Kernel_Arch}/${Moon_KernelArch}/g"       ${Moon_RootLocation}/DEBIAN/control
+
+cd ${Moon_RootLocation}
+dpkg-deb -b . .. 
