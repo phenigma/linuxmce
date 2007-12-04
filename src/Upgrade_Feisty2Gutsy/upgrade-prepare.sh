b1244124e5a4a3a16bc1a58c011246183c3dce26
rgavril <rgavril>
2007-12-04 11:43:37 +0000

    Switched from whiptail to echo

--- src/Upgrade_Feisty2Gutsy/upgrade-prepare.sh
+++ src/Upgrade_Feisty2Gutsy/upgrade-prepare.sh
@@ -1,51 +1,69 @@
 #!/bin/bash
 
-TMP_FILE="$(mktemp)"
-
 FROM_DVD=1
 FROM_NET=2
 
+COLOUR_WHITE="\033[1;37m"
+COLOUR_RED="\033[1;31m"
+COLOUR_NONE="\033[0m"
+
 function Error {
 	local Message="$*"
-	whiptail \
-		--title 'LinuxMCE Release Upgrade' \
-		--msgbox "ERROR :\n$Message" \
-		10 40
 
+	echo	
+	echo -e "${COLOUR_RED}ERROR : $Message ${COLOUR_NONE}"
+	echo
+	read
 	exit 1
 }
 
-## First menu
-whiptail \
-	--title  'LinuxMCE Release Upgrade' \
-	--menu "Chose Upgrade Method" \
-	10 50 2 \
-	"$FROM_DVD" 'Use LinuxMCE 0710 DVD to get the packages' \
-	"$FROM_NET" 'Download the packages from the Internet' \
-	2> "$TMP_FILE"
-InstallFrom=$(cat "$TMP_FILE")
+function Message {
+	local Message="$*"
+	echo -e "${COLOUR_WHITE}${Message}${COLOUR_NONE}"
+}
+
+clear
+
+InstallFrom=0
+while [[ "$InstallFrom" != "$FROM_DVD"  && "$InstallFrom" != "$FROM_NET" ]] ;do
+	echo "Chose Upgrade Method:"
+	echo " 1 - Use LinuxMCE 0710 DVD to get the packages"
+	echo " 2 - Download the packages from the Internet"
+	echo
+	echo -n "Option: "
+	read InstallFrom
+	echo
+done
 
 
-## Extract deb-cache from dvd
 if [[ "$InstallFrom" == "$FROM_DVD" ]] ;then
-	whiptail --title 'LinuxMCE Release Upgrade' --clear --msgbox "Please insert the LinuxMCE 0710 DVD" 10 40
+	echo "Please insert the LinuxMCE 0710 DVD and press ENTER ..."
+	read
 	clear
 
-	echo "Mounting LinuxMCE DVD"
+	Message "* Mounting LinuxMCE DVD"
 	sleep 0.5
 	TEMP_DIR="$(mktemp -d)"
-#	mount /dev/cdrom "$TEMP_DIR" || Error "Cannot mount LinuxMCE 0710 DVD"
+	mount /dev/cdrom "$TEMP_DIR" || Error "Cannot mount LinuxMCE 0710 DVD"
 	sleep 1
 
-	echo "Copying Packages from the DVD"
-	mkdir -p /usr/pluto/deb-cache-new/
-#	cat "$TEMP_DIR/linux-mce.tar.gz_0"* | tar zxv --checkpoint --strip 4 -C /usr/pluto/deb-cache-new './usr/pluto/deb-cache' || Error "There was a problem while reading the DVD"
+	Message "* Copying package from DVD"
+	mkdir -p /usr/pluto/deb-cache-new/ || Error "Cannot create target directory on disk"
 
-	whiptail --title "LinuxMCE Release Upgrade" --clear --msgbox "Copied OK.\n\nYou need to reboot your computer to start the upgrade proccess." 10 50
-	exit 0	
-else
-	whiptail --title 'LinuxMCE Release Upgrade' --clear --msgbox "The dowload will begin in background. You will be prompted in Orbiter when the download will finish." 10 50
-	clear
+	test -f "$TEMP_DIR/linux-mce.tar.gz_01"  || Error "Cannout find LinuxMCE 0710 image on the DVD"
+	test -f "$TEMP_DIR/linux-mce.tar.gz_02"  || Error "Cannout find LinuxMCE 0710 image on the DVD"
+
+	cat "$TEMP_DIR/linux-mce.tar.gz_0"* | tar zxv --checkpoint --strip 4 -C /usr/pluto/deb-cache-new './usr/pluto/deb-cache' || Error "There was a problem while reading the DVD"
+
+	Message "*  You need to reboot your computer to start the upgrade process"
+	read
+	exit 0
 fi
 
-rm -rf "$TMP_FILE"
+
+if [[ "$InstallFrom" == "$FROM_NET" ]] ;then
+	Message "The dowload will begin in background. You will be prompted in Orbiter when the download will finish."
+	read
+	exit 0
+fi
+
