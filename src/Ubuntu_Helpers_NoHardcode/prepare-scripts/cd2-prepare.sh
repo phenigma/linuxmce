8de1d0c8730669a0877ac252060120b2d9e1bfbf
rgavril <rgavril>
2007-12-10 15:02:13 +0000

    Fix prepare cd2

--- src/Ubuntu_Helpers_NoHardcode/prepare-scripts/cd2-prepare.sh
+++ src/Ubuntu_Helpers_NoHardcode/prepare-scripts/cd2-prepare.sh
@@ -9,7 +9,7 @@ pushd "${build_dir}/cd2-packages/cachecd1-cache"
 	aptitude download `cat /etc/lmce-build/cd2-packages` || exit 1
 
 	## Video Wizard Videos
-	cp "${replacements_dir}"/video-wizard-videos_*.deb ./
-	cp "${replacements_dir}"/tee-pluto_*.deb ./
+#	cp "${replacements_dir}"/video-wizard-videos_*.deb ./
+#	cp "${replacements_dir}"/tee-pluto_*.deb ./
 popd
 
