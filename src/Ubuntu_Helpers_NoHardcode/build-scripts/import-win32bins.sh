23f943e1a93940eac8e617994de4b4cf7f91da72
rgavril <rgavril>
2007-12-12 10:55:05 +0000

    Fake import-win32bin.sh for testing

---
+++ src/Ubuntu_Helpers_NoHardcode/build-scripts/import-win32bins.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+. /etc/lmce-build/builder.conf
+. /usr/local/lmce-build/common/logging.sh
+
+set -e
+set -x
+
+function import_win32bins() {
+	touch ${svn_dir}/trunk/src/bin/Pluto_S60.sis
+	touch ${svn_dir}/trunk/src/bin/Orbiter.CAB
+	touch ${svn_dir}/trunk/src/bin/Orbiter_Treo.CAB
+
+	touch ${svn_dir}/trunk/src/bin/UpdateBinary.exe
+	touch ${svn_dir}/trunk/src/bin/UpdateBinaryCE.exe 
+	touch ${svn_dir}/trunk/src/bin/UpdateBinaryCE_x86.exe
+	touch ${svn_dir}/trunk/src/bin/{Orbiter_Win32.dat,Orbiter_CeNet4_XScale.dat}
+	touch "${svn_dir}/trunk/src/bin/Symbian Series 60 mobile.vmc"
+	touch "${svn_dir}/trunk/src/bin/Windows Mobile Smartphone.vmc"
+	touch ${svn_dir}/trunk/src/bin/Orbiter_CeNet4_x86.dat
+
+	touch ${svn_dir}/trunk/src/bin/OrbiterInstaller.msi
+	touch ${svn_dir}/trunk/src/bin/OrbiterCE_SDL.CAB
+	touch ${svn_dir}/trunk/src/bin/Orbiter_CeNet4_XScale.CAB
+	touch ${svn_dir}/trunk/src/bin/Orbiter_CeNet4_x86.CAB
+	touch ${svn_dir}/trunk/src/bin/ImportContacts.zip
+
+	touch ${svn_dir}/trunk/src/bin/Orbiter.exe
+	touch ${svn_dir}/trunk/src/bin/Orbiter_CeNet4_XScale.exe
+	touch ${svn_dir}/trunk/src/bin/OrbiterSmartphone.exe
+	touch ${svn_dir}/trunk/src/bin/OrbiterCE_SDL.exe
+	touch ${svn_dir}/trunk/src/bin/Orbiter_CeNet4_x86.exe
+
+	touch ${svn_dir}/trunk/src/bin/AYGSHELL.DLL
+	touch ${svn_dir}/trunk/src/bin/PthreadsCE.dll
+	touch ${svn_dir}/trunk/src/bin/Orbiter.MD5
+	touch ${svn_dir}/trunk/src/bin/logo.gif
+
+	touch ${svn_dir}/trunk/src/bin/PlutoBaSInstaller.msi
+	touch ${svn_dir}/trunk/src/bin/PlutoRebootSetup.msi
+
+#pushd ${svn_dir}/trunk/src/bin
+#scp pluto@10.0.2.4:'/home/builds/Windows_Output_LinuxMCE/src/bin/*' ./
+#popd
+
+#pushd ${svn_dir}/trunk/src/lib
+#scp pluto@10.0.2.4:'/home/builds/Windows_Output_LinuxMCE/src/lib/*' ./
+#popd
+}
+
+DisplayMessage "*** STEP: Importing WIN32 binaries"
+trap 'Error "Undefined error in $0"' EXIT
+
+import_win32bins
+
+trap - EXIT
