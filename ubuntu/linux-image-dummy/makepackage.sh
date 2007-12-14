0f8661169771552b4474a58df152e1c3329ff91a
rgavril <rgavril>
2007-12-14 10:06:27 +0000

    Don't copy whole lib/modules, look in linux-image deb to see what to copy

--- ubuntu/linux-image-dummy/makepackage.sh
+++ ubuntu/linux-image-dummy/makepackage.sh
@@ -3,16 +3,28 @@
 Moon_KernelVersion=$(uname -r)
 Moon_KernelArch=$(apt-config dump | grep 'APT::Architecture' | sed 's/APT::Architecture "\(.*\)".*/\1/g')
 Moon_RootLocation='package/'
+
 mkdir -p $Moon_RootLocation/{boot,lib/modules}
 
+# Copy kernel image and sysmap
 cp /boot/vmlinuz-${Moon_KernelVersion} ${Moon_RootLocation}/boot
 cp /boot/System.map-${Moon_KernelVersion} ${Moon_RootLocation}/boot/
-cp -r /lib/modules/${Moon_KernelVersion} ${Moon_RootLocation}/lib/modules
 
+# Generate NFS bootable initrd
 sed -i 's/^.*BOOT=.*/BOOT=nfs/g' /etc/initramfs-tools/initramfs.conf
 mkinitramfs -d /etc/initramfs-tools/ -o ${Moon_RootLocation}/boot/initrd.img-${Moon_KernelVersion}
 sed -i 's/^.*BOOT=.*/BOOT=local/g' /etc/initramfs-tools/initramfs.conf
 
+# Copy from /lib/modules only whare belongs to linux-image-`uname -r`
+dpkg -L linux-image-${Moon_KernelVersion}  | grep '^/lib/modules/'  | sed 's|^/lib/modules/||g' | while read line ;do
+	if [[ -f "/lib/modules/$line" ]] ;then
+		cp "/lib/modules/$line" "${Moon_RootLocation}/lib/modules/$line"
+	elif [[ -d "/lib/modules/$line" ]] ;then
+		mkdir -p "${Moon_RootLocation}/lib/modules/$line"
+	else
+		echo "Skiped $line"
+	fi
+done
 
 cp ${Moon_RootLocation}/DEBIAN/control{.in,}
 sed -i "s/{Kernel_Version}/${Moon_KernelVersion}/g" ${Moon_RootLocation}/DEBIAN/control
