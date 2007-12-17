29288b9be97d2b711e430e2fe5e329904d03875d
rgavril <rgavril>
2007-12-17 16:54:54 +0000

    Some minor changes for i386

--- src/Ubuntu_Helpers_NoHardcode/install.sh
+++ src/Ubuntu_Helpers_NoHardcode/install.sh
@@ -11,6 +11,21 @@ echo "Installing Default Configs For $Distro-$Arch"
 rm -f "/etc/lmce-build"
 ln -s "$(pwd)/conf-files/${Distro}-${Arch}/" "/etc/lmce-build"
 
+echo "Creating symlink in /usr/local/lmce-build"
+rm -f "/usr/local/lmce-build"
+ln -s "$(pwd)" "/usr/local/lmce-build"
+
+echo "Configure APT preferences"
+echo '
+Package: *
+Pin: origin
+Pin-Priority: 9999
+
+Package: *
+Pin: release v=7.10,o=Ubuntu,a=gutsy,l=Ubuntu
+Pin-Priority: 9998
+' > /etc/apt/preferences
+
 # Generate ssh key for builder if !exist
 if [[ ! -f /etc/lmce-build/builder.key ]] ;then
 	echo "Generating SSH Key for this host : /etc/lmce-build/builder.key"
