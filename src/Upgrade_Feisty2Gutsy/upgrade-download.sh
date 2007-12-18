5cf937b17579adf4e822e03e81a8fd5dd5eba0a9
rgavril <rgavril>
2007-12-18 10:13:04 +0000

    Display a message on orbiter when the download had finished

--- src/Upgrade_Feisty2Gutsy/upgrade-download.sh
+++ src/Upgrade_Feisty2Gutsy/upgrade-download.sh
@@ -24,5 +24,31 @@ fi
 wget --directory-prefix=/var/gutsy-upgrade-scripts/ --continue --tries=5 --timeout=5  --output-file=/var/gutsy-upgrade-scripts/download.log \
 	ftp://ftp.iasi.roedu.net/mirrors/ubuntulinux.org/releases/7.10/ubuntu-7.10-alternate-amd64.iso || exit 1
 
+## Generate a list containig all orbiter ids ($OrbiterIDList)
+. /usr/pluto/bin/SQL_Ops.sh
+Q="
+        SELECT
+                PK_Device
+        FROM
+                Device
+                JOIN DeviceTemplate ON PK_DeviceTemplate = FK_DeviceTemplate
+                JOIN DeviceCategory ON PK_DeviceCategory = FK_DeviceCategory
+        WHERE
+                PK_DeviceCategory IN (5,2,3)
+"
+OrbiterList=$(RunSQL "$Q")
+
+OrbiterIDList=""
+for Orbiter in $OrbiterList; do
+        if [[ $OrbiterIDList != "" ]]; then
+                OrbiterIDList="$OrbiterIDList,"
+        fi
+
+        Orbiter_ID=$(Field 1 "$Orbiter")
+        OrbiterIDList="$OrbiterIDList""$Orbiter_ID"
+done
+/usr/pluto/bin/MessageSend localhost 0 $OrbiterIDList 1 741 10 "UpgradeFinished" 159 53 183 0 9 "Finished downloading 0710 Update. You need to reboot you Hybrid/Core to install the updates."
+
+
 
 touch /var/gutsy-upgrade-scripts/upgrade-ready
