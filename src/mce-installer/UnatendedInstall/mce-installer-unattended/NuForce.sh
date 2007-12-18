f1d8a85a1d1547d260b562ebcf1e2abb71f1f63a
radu <radu.cx+linuxmce@gmail.com>
2007-12-18 18:13:37 +0000

    disable Orbiter Embedded Phone device

--- src/mce-installer/UnatendedInstall/mce-installer-unattended/NuForce.sh
+++ src/mce-installer/UnatendedInstall/mce-installer-unattended/NuForce.sh
@@ -17,6 +17,7 @@ DEVICETEMPLATE_Media_Plugin=2
 DEVICETEMPLATE_OnScreen_Orbiter=62
 DEVICETEMPLATE_Asterisk=45
 DEVICETEMPLATE_MythTV_Player=35
+DEVICETEMPLATE_Orbiter_Embedded_Phone=1759
 
 DEVICEDATA_ScreenWidth=100
 DEVICEDATA_ScreenHeight=101
@@ -141,7 +142,8 @@ DatabaseDefaults()
 		"INSERT INTO Room(FK_Installation, FK_RoomType, Description) VALUES('$PK_Installation', 1, 'NuForce')"
 		"UPDATE Device SET FK_Room=1 WHERE PK_Device=1"
 		"UPDATE Device_DeviceData SET IK_DeviceData='' WHERE FK_DeviceData=28"
-		"UPDATE Device SET Disabled=1 WHERE FK_DeviceTemplate IN ($DEVICETEMPLATE_Asterisk, $DEVICETEMPLATE_MythTV_Player)"
+		"UPDATE Device SET Disabled=1 WHERE FK_DeviceTemplate IN
+			($DEVICETEMPLATE_Asterisk, $DEVICETEMPLATE_MythTV_Player, $DEVICETEMPLATE_Orbiter_Embedded_Phone)"
 		"INSERT INTO Users(UserName) VALUES('nuforce')"
 	)
 
