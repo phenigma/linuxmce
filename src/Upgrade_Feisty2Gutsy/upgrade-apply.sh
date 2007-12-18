6ea8b286b5b44938ca1e8dfd5abe2e25670b4783
rgavril <rgavril>
2007-12-18 12:07:00 +0000

    Added Logging & Error Reporting in update-apply script

--- src/Upgrade_Feisty2Gutsy/upgrade-apply.sh
+++ src/Upgrade_Feisty2Gutsy/upgrade-apply.sh
@@ -1,34 +1,69 @@
 #!/bin/bash
 
 set -e
+
+exec 100>&1
+exec >"/var/log/upgrade-apply.log"
+exec 2>&1
+
+function DisplayMessage {
+        echo "`date +%H:%M:%S`  $*" >&100
+}
+
+function Error {
+        set +x
+        trap - EXIT
+        echo
+        DisplayDisplayMessage "ERROR: $*"
+        echo                    		>&100
+        echo     		              	>&100
+        tail -20 /var/log/upgrade-apply.log	>&100
+        echo                    		>&100
+
+        exit 1
+}
+
+DisplayMessage "*** STEP: Running MakeRelease"
+trap 'Error "Got a error in file $0"' EXIT
+
 set -x
 
 if [[ ! -f /var/gutsy-upgrade-scripts/upgrade-ready ]] ;then
+	trap - EXIT
+	DisplayMessage "Updates didn't finished downloading, continue booting ..."
 	exit 0
 fi
 
-cd /usr/share/gutsy-upgrade-scripts/
+cd /usr/share/gutsy-upgrade-scripts/scripts
 
 ## Preseed some packages to avoid the need of interactive debconf
+DisplayMessage "Preseeding packages"
 ./upgrade-preseed.sh
 
 ## Regen Packages.gz
+DisplayMessage "Setting up deb-cache"
 apt-get -f -y --force-yes install dpkg-dev
 pushd /usr/pluto/deb-cache-new
 	dpkg-scanpackages -m . /dev/null > Packages
        	gzip -c Packages > Packages.gz
 popd
 
+## Move deb-cache-new to deb-cache
+mv /usr/pluto/deb-cache{,-old}
+mv /usr/pluto/deb-cache{-new,}
+
 ## Put aside some files
 mv /etc/apt/sources.list{,.backup-upgrade} || :
 mv /etc/init.d/kdm{,.backup-upgrade} || :
 
 ## Modify sources.list for the upgade
+DisplayMessage "Setting up apt"
 echo "
 deb file:/usr/pluto/deb-cache/ ./
 deb http://archive.ubuntu.com/ubuntu/ gutsy main restricted universe multiverse
 " > /etc/apt/sources.list
 
+## Setting up apt
 echo "
 Package: *
 Pin: origin
@@ -39,17 +74,17 @@ Pin: release v=7.10,o=Ubuntu,a=gutsy,l=Ubuntu
 Pin-Priority: 9998
 " > /etc/apt/preferences
 
-## Move deb-cache-new to deb-cache
-mv /usr/pluto/deb-cache{,-old}
-mv /usr/pluto/deb-cache{-new,}
 
 ## Do the actual upgrade
+DisplayMessage "Started updating packages"
+sleep 2
 apt-get update
 apt-get -f -y --force-yes install mysql-server-5.0
 /etc/init.d/mysql start
 apt-get -f -y --force-yes dist-upgrade
 
 ## Fix PK_Distro in pluto.conf
+DisplayMessage "Performing post upgrade fixes"
 . /usr/pluto/bin/Config_Ops.sh
 ConfSet "PK_Distro" "15"
 
@@ -60,6 +95,7 @@ update-grub
 
 ## Reset model device datas
 . /usr/pluto/bin/SQL_Ops.sh
+RunSQL "UPDATE Device_DeviceData SET IK_DeviceData = '15' WHERE PK_Device IN (SELECT PK_Device FROM Device WHERE FK_DeviceTemplate IN (7, 28)) AND FK_DeviceData='7'"
 RunSQL "UPDATE Device_DeviceData SET IK_DeviceData = 'LMCE_CORE_u0710_i386' WHERE IK_DeviceData = 'LMCE_CORE_1_1'"
 RunSQL "UPDATE Device_DeviceData SET IK_DeviceData = 'LMCE_MD_u0710_i386'   WHERE IK_DeviceData = 'LMCE_MD_1_1'"
 RunSQL "UPDATE Device_DeviceData SET IK_DeviceData = '0' WHERE FK_DeviceData = '234'"
@@ -74,4 +110,12 @@ rm -rf /home/updates/*
 rm -f /usr/share/gutsy-upgrade-scripts/upgrade-ready
 
 ## Don't run download script again
+DisplayMessage "Upgrade Finished"
 rm -f /etc/cron.d/gutsy-upgrade-scripts
+
+trap - EXIT
+
+DisplayMessage "Press <ENTER> to continue ..."
+read
+
+
