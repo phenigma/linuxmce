f6a83f038633e4ead6452df0ca7b4725f25fa67b
rgavril <rgavril>
2007-12-18 08:09:55 +0000

    Set the correct order of the upgrade operations

--- src/Upgrade_Feisty2Gutsy/upgrade-apply.sh
+++ src/Upgrade_Feisty2Gutsy/upgrade-apply.sh
@@ -12,6 +12,13 @@ fi
 ## Preseed some packages to avoid the need of interactive debconf
 ./upgrade-preseed.sh
 
+## Regen Packages.gz
+apt-get -f -y --force-yes install dpkg-dev
+pushd /usr/pluto/deb-cache-new
+	dpkg-scanpackages -m . /dev/null > Packages
+       	gzip -c Packages > Packages.gz
+popd
+
 ## Put aside some files
 mv /etc/apt/sources.list{,.backup-upgrade} || :
 mv /etc/init.d/kdm{,.backup-upgrade} || :
@@ -19,7 +26,7 @@ mv /etc/init.d/kdm{,.backup-upgrade} || :
 ## Modify sources.list for the upgade
 echo "
 deb file:/usr/pluto/deb-cache/ ./
-#deb http://archive.ubuntu.com/ubuntu/ gutsy main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/ gutsy main restricted universe multiverse
 " > /etc/apt/sources.list
 
 echo "
@@ -39,6 +46,7 @@ mv /usr/pluto/deb-cache{-new,}
 ## Do the actual upgrade
 apt-get update
 apt-get -f -y --force-yes install mysql-server-5.0
+/etc/init.d/mysql start
 apt-get -f -y --force-yes dist-upgrade
 
 ## Fix PK_Distro in pluto.conf
@@ -58,13 +66,6 @@ RunSQL "UPDATE Device_DeviceData SET IK_DeviceData = '0' WHERE FK_DeviceData = '
 
 ## Remove old 0704 updates
 rm -rf /home/updates/*
-
-## Regen Packages.gz
-pushd /usr/pluto/deb-cache-new
-	dpkg-scanpackages -m . /dev/null > Packages
-       	gzip -c Packages > Packages.gz
-popd
-
 
 ## Don't run again
 rm -f /usr/share/gutsy-upgrade-scripts/upgrade-ready
