a3487f7187023d48e125a25bfbb55cd57ba1c4b1
rgavril <rgavril>
2007-12-18 09:39:23 +0000

    Updates ..

--- src/Upgrade_Feisty2Gutsy/debian/init.d
+++ src/Upgrade_Feisty2Gutsy/debian/init.d
@@ -4,11 +4,14 @@ case "$1" in
 	start)
 		# Stop Usplash
 		/etc/init.d/usplash start
+		chvt 8
 		sleep 3
 
-		echo "We're here bastard ..."
-		sleep 20
+		echo "Trying to update the system ..."
+		/usr/share/gutsy-upgrade-scripts/scripts/upgrade-apply.sh
 
+		echo "Press <ENTER> to continue ....."
+		read 
 		#
 		;;
 	*)
