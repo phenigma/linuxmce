44655eb32124080a3e46d8d733458bb5a70dba90
radu <radu.cx+linuxmce@gmail.com>
2008-01-09 13:35:38 +0000

    fixed typo

--- web/pluto-admin/languages/en/packagesList.lang.php
+++ web/pluto-admin/languages/en/packagesList.lang.php
@@ -1,7 +1,7 @@
 <?
 $TEXT_PACKAGES_CONST='Packages';
 $TEXT_GROUP_BY_MANUFACTURER_CONST='Group by manufacturer';
-$TEXT_SHOW_DEPENDENCIES_CONST='Show dependancies';
+$TEXT_SHOW_DEPENDENCIES_CONST='Show dependencies';
 $TEXT_ALPHABETICAL_ORDER_CONST='Alphabetical order';
 $TEXT_NO_MANUFACTURER_CONST='No manufacturer';
 $TEXT_CREATE_NEW_PACKAGE_CONST='Create new package';
