1b29d4d1627a6422981f10f3af2cfa93ffdb9b84
radu <radu.cx+linuxmce@gmail.com>
2008-01-11 16:34:02 +0000

    code to uninstall packages

--- src/BootScripts/remove_pvr_packages.sh
+++ src/BootScripts/remove_pvr_packages.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+MythPkg=(
+	libmyth-0.20 mythtv-backend mythtv-common mythtv-database mythtv-frontend mythtv-transcode-utils
+	mythweb pluto-mythtv-player pluto-mythtv-plugin
+)
+
+VDRPkg=(
+	lmcevdr libxine-xvdr vdr vdr-dev vdr-plugin-control vdr-plugin-xineliboutput
+	libxineliboutput-sxfe xineliboutput-sxfe
+)
+
+RemovePkg()
+{
+	local Pkg
+
+	for Pkg in "$@"; do
+		apt-get -y -f remove "$Pkg"
+	done
+}
+
+Selection="$1"
+
+case "$Selection" in
+	mythtv)
+		RemovePkg "${MythPkg[@]}"
+	;;
+	vdr)
+		RemovePkg "${VDRPkg[@]}"
+	;;
+	*)
+		echo "Unknown selection: '$Selection'"
+	;;
+esac
