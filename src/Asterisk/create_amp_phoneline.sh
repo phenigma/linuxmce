d23b661673675f50ccae3882f0c565f8430f54a8
radu <radu.cx+linuxmce@gmail.com>
2008-01-14 11:09:57 +0000

    added list of provides, updated phoneline setup filenames

--- src/Asterisk/create_amp_phoneline.sh
+++ src/Asterisk/create_amp_phoneline.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
 
-Type="$1"
+Keyword="$1"
 shift
 
-case "$Type" in
-	"broadvoice")
-		AmpScript="/usr/pluto/bin/create_amp_broadvoice.pl"
-	;;
-	"freeworlddialup")
-		AmpScript="/usr/pluto/bin/create_amp_fwd.pl"
-	;;
-	"inphonex")
-		AmpScript="/usr/pluto/bin/create_amp_inphonex.pl"
-	;;
-	"efon")
-		AmpScript="/usr/pluto/bin/create_amp_efon.pl"
-	;;
-	"teliax")
-		AmpScript="/usr/pluto/bin/create_amp_teliax.pl"
-	;;
-	"nufone")
-		AmpScript="/usr/pluto/bin/create_amp_nufone.pl"
-	;;
-	"voiceeclipse")
-		AmpScript="/usr/pluto/bin/create_amp_voiceeclipse.pl"
-	;;
-	"sipgate")
-		AmpScript="/usr/pluto/bin/create_amp_sipgate.pl"
-	;;
-esac
-
+AmpScript="/usr/pluto/bin/create_amp_$Keyword.pl"
 "$AmpScript" "$@"
 /usr/pluto/bin/create_pluto_dialplan.pl
