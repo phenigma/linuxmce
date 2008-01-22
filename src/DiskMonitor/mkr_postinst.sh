007438cb6a8ac0d9f58eb6c596a018480e7ab26e
radu <radu.cx+linuxmce@gmail.com>
2008-01-22 12:41:41 +0000

    suppress cron output

--- src/DiskMonitor/mkr_postinst.sh
+++ src/DiskMonitor/mkr_postinst.sh
@@ -2,7 +2,7 @@
 /usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
 
 ## Add a cron entry
-cronEntry="*/3 * * * * root /usr/pluto/bin/DiskSpaceMonitor.sh &>/dev/null"
+cronEntry="*/3 * * * * bash -c 'root /usr/pluto/bin/DiskSpaceMonitor.sh &>/dev/null'"
 #if ! grep -qF '/usr/pluto/bin/DiskSpaceMonitor.sh' /etc/crontab; then
 #        echo "$cronEntry" >>/etc/crontab
 #        invoke-rc.d cron reload
