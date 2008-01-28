3efeafd6e7b79eb3ed417cd4c45fffd647e50784
rgavril <rgavril>
2008-01-28 13:49:06 +0000

    Create wrapper that pluto-admin can call to refresh raid statuses regardless the computer that the raid is on

---
+++ src/PlutoRaidTools/refresh_status_RAID.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. /usr/pluto/bin/SQL_Ops.sh
+
+DD_BLOCK_DEVICE=152
+DT_APP_SERVER=26
+
+DeviceID="$1"
+
+DeviceName=$(RunSQL "SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_DeviceData='$DD_BLOCK_DEVICE' AND FK_Device='$DeviceID'");
+DeviceName=$(Field 1 "$DeviceName")
+
+DeviceComputer=$(RunSQL "SELECT FK_Device_ControlledVia FROM Device WHERE PK_Device='$DeviceID'")
+DeviceComputer=$(Field 1 "$DeviceComputer")
+
+if [[ "$DeviceComputer" != "$PK_Device" ]] ;then
+	DeviceAppServer=$(RunSQL "SELECT PK_Device FROM Device WHERE FK_Device_ControlledVia='$DeviceComputer' AND FK_DeviceTemplate='$DT_APP_SERVER'")
+	DeviceAppServer=$(Field 1 "$DeviceAppServer")
+
+	/usr/pluto/bin/MessageSend dcerouter -targetType device 0 "$DeviceAppServer" 1 67 13 "/usr/pluto/bin/monitoring_RAID.sh" 51 "WEB_ADMIN_REFRESH\t$DeviceName"
+else
+	/usr/pluto/bin/monitoring_RAID.sh "WEB_ADMIN_REFRESH" "$DeviceName"
+fi
