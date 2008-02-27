24d25f84bb9126c739848454fbe7320448aa7f36
cmiron <cmiron>
2008-02-27 12:49:46 +0000

    Reverted some code

--- src/Orbiter/DesignObj_DataGrid.cpp
+++ src/Orbiter/DesignObj_DataGrid.cpp
@@ -234,9 +234,9 @@ g_PlutoProfiler->Stop("send command");
 						pDataGridTable->SetData(GridCurCol,pDataGridTable->m_iUpRow,pCell);
 					}
 
-					if ( pDataGridTable->GetRows() > GridCurRow + m_MaxRow)
+					if ( pDataGridTable->GetRows() > GridCurRow + m_MaxRow - (pDataGridTable->m_iUpRow!=-1 ? 1 : 0) )
 					{
-						pDataGridTable->m_iDownRow = GridCurRow + m_MaxRow;
+						pDataGridTable->m_iDownRow = GridCurRow + m_MaxRow - 1;
 						DataGridCell * pCell = new DataGridCell( "<Scroll down>" );
 						pCell->m_Colspan = pDataGridTable->m_ColumnCount;
 						pDataGridTable->SetData(GridCurCol,pDataGridTable->m_iDownRow,pCell);
