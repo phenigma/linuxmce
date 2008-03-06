b230bd6e2b4c4b366e9965752fafa56a6bda9d0b
kirill.b <kirill.b>
2008-03-06 10:57:07 +0000

    create dvdcss folder and tag it if it doesn't exist

--- src/Disk_Drive/mkr_postinst.sh
+++ src/Disk_Drive/mkr_postinst.sh
@@ -2,6 +2,14 @@
 
 . /usr/pluto/bin/Config_Ops.sh
 
+DVDCSS_CACHE="/home/.dvdcss"
+
 if [[ "$DVDKeysCache" == "" ]] ;then
-	ConfSet "DVDKeysCache" "/home/.dvdcss"
+	ConfSet "DVDKeysCache" "$DVDCSS_CACHE"
 fi
+
+if [ ! -d "$DVDCSS_CACHE" ] ;then
+    mkdir "$DVDCSS_CACHE"
+    echo -e -n "Signature: 8a477f597d28d172789f06886806bc55\r\n# This file is a cache directory tag created by libdvdcss.\r\n# For information about cache directory tags, see:\r\n#   http://www.brynosaurus.com/cachedir/\r\n" > "$DVDCSS_CACHE/CACHEDIR.TAG"
+fi
+
