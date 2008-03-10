e0e463c44343f781b6891eee5c0f2484f081af88
radu <radu.cx+linuxmce@gmail.com>
2008-03-10 13:52:59 +0000

    changed message displayed to the user, made pretty; added button to go to the Pending Tasks screen

--- src/Disk_Drive/DiskCopy_ProgressExtract.sh
+++ src/Disk_Drive/DiskCopy_ProgressExtract.sh
@@ -7,7 +7,7 @@ while read line; do
 	lastline="$line"
 	if [[ "$line" == "Error: "* ]]; then
 		if [[ -n "$OrbiterDev" ]]; then
-			/usr/pluto/bin/MessageSend localhost 0 "$OrbiterDev" 1 741 10 "ripping warning" 159 53 9 "ripping -- warning ... bad sectors... etc|OK" 137 "" 163 "ripping"
+			/usr/pluto/bin/MessageSend localhost 0 "$OrbiterDev" 1 741 10 "ripping warning" 159 53 9 "Errors were encountered while ripping the disk. The bad sectors will be skipped and replaced with zeros, but if the disk is very scratched, it may take a long while for the operation to complete. If you don't want to wait, you can cancel the operation from the 'Pending tasks' screen|OK|Pending tasks" 137 "-targetType device 0 $OrbiterDev 1 4|-targetType device 0 $OrbiterDev 1 741 159 82" 163 "ripping"
 		fi
 	elif [[ "$line" == "Position: "*"; Progress: "*"%" ]]; then
 		Percent=${line##*Progress: }
