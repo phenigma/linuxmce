06b058fd202a344d15b3c63d085a694341bded1d
radu <radu.cx+linuxmce@gmail.com>
2008-03-21 15:56:28 +0000

    use the HTTP_HOST for creating the link to fuppes instead of the external address; it's more portable and works accross firewalls that forward the port

--- web/pluto-admin/operations/others/upnpServer.php
+++ web/pluto-admin/operations/others/upnpServer.php
@@ -20,7 +20,7 @@ function upnpServer($output,$dbADO) {
 		$data=exec_batch_command('sudo -u root /usr/pluto/bin/Network_DisplaySettings.sh --all',1);
 		$ret=parse_ini_str($data);
 
-		$out.='<iframe src="http://'.$ret['EXTERNAL_IP'].':3877/" style="width:98%;height:600px;"></iframe>';
+		$out.='<iframe src="http://'.$_SERVER['HTTP_HOST'].':3877/" style="width:98%;height:600px;"></iframe>';
 		/*
 		// config changed to allow external access
 		if(isset($ret['INTERNAL_IP']) && same_network($ip,$ret['INTERNAL_IP'],$ret['INTERNAL_NETMASK'])){
