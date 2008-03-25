9190b07a61343c57577135e022c40427b4d7e438
cmiron <cmiron>
2008-03-25 11:55:02 +0000

    Added test unit for filefiles

---
+++ src/PlutoUtils/utils_tests/main_findfiles.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "../FileUtils.h"
+
+int main(int argc, char* argv[])
+{
+	string sTargetDir = "/home/test";
+	string sFileSpec = "*.lock";
+
+	cout << "Number of parameters specified: " << argc << endl;
+
+	if(argc >= 3)
+		sFileSpec = argv[2];
+
+	if(argc >= 2)
+		sTargetDir = argv[1];
+
+	cout << "Target folder : '" << sTargetDir << "'" << endl;
+	cout << "File spec : '" << sFileSpec << "'" << endl;
+	cout << endl;
+
+	list<string> listFiles;
+	FileUtils::FindFiles(listFiles, sTargetDir, sFileSpec);	
+
+	cout << "Files from folder per file spec: " << endl;
+	for(list<string>::iterator it = listFiles.begin(); it != listFiles.end(); ++it)
+	{
+		cout << "\t" << *it << endl;
+	}
+
+	return 0;
+}
