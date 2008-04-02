798db6df0d10e64e7c0f276e83b3da6c04ea68db
radu <radu.cx+linuxmce@gmail.com>
2008-04-02 15:54:22 +0000

    remove \n from Logger messages

--- src/PlutoUtils/ProcessUtils.cpp
+++ src/PlutoUtils/ProcessUtils.cpp
@@ -75,7 +75,7 @@ int ProcessUtils::SpawnApplication(string sCmdExecutable, string sCmdParams, str
 
     if (sCmdExecutable == "")
     {
-		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Received empty Executable and Parameters for '%s'\n", sAppIdentifier.c_str());
+		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Received empty Executable and Parameters for '%s'", sAppIdentifier.c_str());
         return 0;
     }
 
@@ -87,26 +87,26 @@ int ProcessUtils::SpawnApplication(string sCmdExecutable, string sCmdParams, str
 	string::size_type pos = 0;
     int i = 0;
 
-	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() sCMDExec %s parms: %s size: %d\n",
+	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() sCMDExec %s parms: %s size: %d",
 		sCmdExecutable.c_str(),sCmdParams.c_str(),(int) sCmdParams.size());
 
 	// this looks to complicated but i don;t have time to make it cleaner :-( mtoader@gmail.com)
     args[0] = (char *) strdup(sCmdExecutable.c_str());
-LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"dupped exec %s\n",args[0]);
+LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"dupped exec %s",args[0]);
 	while ( pos!=string::npos && pos<sCmdParams.size() && i < MaxArgs - 1)
 {
 string s=StringUtils::Tokenize(sCmdParams,"\t",pos);
 const char *ps=strdup(s.c_str());
-LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"dupped arg %d %s\n",i,ps);
+LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"dupped arg %d %s",i,ps);
 
 		args[++i] = strdup(s.c_str());
 }
 
 	args[++i] = 0;
-	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Found %d arguments\n", i);
+	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Found %d arguments", i);
 
     for (int x = 0 ; x < i; x++)
-		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Argument %d: %s\n", x, args[x]);
+		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Argument %d: %s", x, args[x]);
 
 	int in[2];
 	pipe(in);
@@ -124,7 +124,7 @@ LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"dupped arg %d %s\n",i,ps);
         {
 			// setenv("DISPLAY", ":0", 1);
             //now, exec the process
-            LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication(): Spawning\n");
+            LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication(): Spawning");
 			setpgrp();
 
 			string sLogFile;
@@ -161,7 +161,7 @@ LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"dupped arg %d %s\n",i,ps);
         }
 
         case -1:
-            LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Error starting %s, err: %s\n", sCmdExecutable.c_str(), strerror(errno));
+            LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() Error starting %s, err: %s", sCmdExecutable.c_str(), strerror(errno));
             return 0;
 
 		default:
@@ -169,9 +169,9 @@ LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"dupped arg %d %s\n",i,ps);
 			for(int i=0;i<i;++i)
 				free(args[i]);
 
-			LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"Freed args\n");
+			LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"Freed args");
 
-			LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() adding this %d pid to the spawned list for %s\n", pid, sAppIdentifier.c_str());
+			LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnApplication() adding this %d pid to the spawned list for %s", pid, sAppIdentifier.c_str());
 
 			if ( g_mapIdentifierToPidData.find(sAppIdentifier) == g_mapIdentifierToPidData.end() )
                 g_mapIdentifierToPidData[sAppIdentifier] = map<int, PidData *>();
@@ -218,12 +218,12 @@ bool ProcessUtils::KillApplication(string sAppIdentifier, vector<void *> &associ
 
 	if (element == g_mapIdentifierToPidData.end())
     {
-        LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication() No application '%s' found. None killed\n", sAppIdentifier.c_str());
+        LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication() No application '%s' found. None killed", sAppIdentifier.c_str());
 		return false;
     }
 
 	MapPidToData &mapPidsToData = element->second;
-	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication(): Found %d '%s' applications.\n", mapPidsToData.size(), sAppIdentifier.c_str());
+	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication(): Found %d '%s' applications.", mapPidsToData.size(), sAppIdentifier.c_str());
 
 	associatedData.clear();
 	MapPidToData::const_iterator itPidsToData = mapPidsToData.begin();
@@ -240,7 +240,7 @@ bool ProcessUtils::KillApplication(string sAppIdentifier, vector<void *> &associ
 	g_mapIdentifierToPidData.erase(element);
 	sl.Release();
 
-	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication(): Mutex unlocked Sending kill signal.\n");
+	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication(): Mutex unlocked Sending kill signal.");
 	// we are doing the killing without the lock taken (since this killing might trigger another call into one of our functions which might cause a deadlock).
 	vector<int>::const_iterator itPids = pidsArray.begin();
 	while ( itPids != pidsArray.end() )
@@ -256,20 +256,20 @@ bool ProcessUtils::KillApplication(string sAppIdentifier, vector<void *> &associ
 
 bool ProcessUtils::ApplicationExited(int pid, string &associatedName, void *&associatedData, bool removeIt)
 {
-	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::ApplicationExited() pid exited: %d\n", pid);
+	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::ApplicationExited() pid exited: %d", pid);
 
 	PLUTO_SAFETY_LOCK(sl,g_ProcessUtilsMutex);
 
 	MapIdentifierToPidData::iterator applicationElement = g_mapIdentifierToPidData.begin();
 	while ( applicationElement != g_mapIdentifierToPidData.end() )
 	{
-		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::ApplicationExited() Looking at: %s\n", applicationElement->first.c_str());
+		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::ApplicationExited() Looking at: %s", applicationElement->first.c_str());
 
 		MapPidToData &mapPidsToData = applicationElement->second;
 		MapPidToData::iterator itPidsToData = mapPidsToData.begin();
 		while ( itPidsToData != mapPidsToData.end() && itPidsToData->first != pid )
 		{
-			LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::ApplicationExited() checking pid %d\n", itPidsToData->first);
+			LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::ApplicationExited() checking pid %d", itPidsToData->first);
 			itPidsToData++;
 		}
 
@@ -313,12 +313,12 @@ bool ProcessUtils::SendKeysToProcess(string sAppIdentifier,string sKeys)
 
 	if (element == g_mapIdentifierToPidData.end())
     {
-        LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication() No application '%s' found. None to send keys to\n", sAppIdentifier.c_str());
+        LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::KillApplication() No application '%s' found. None to send keys to", sAppIdentifier.c_str());
 		return false;
     }
 
 	MapPidToData &mapPidsToData = element->second;
-	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SendKeysToProcess(): Found %d '%s' applications, sending %s.\n", mapPidsToData.size(), sAppIdentifier.c_str(), sKeys.c_str());
+	LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SendKeysToProcess(): Found %d '%s' applications, sending %s.", mapPidsToData.size(), sAppIdentifier.c_str(), sKeys.c_str());
 
 	MapPidToData::const_iterator itPidsToData = mapPidsToData.begin();
 	while ( itPidsToData != mapPidsToData.end() )
@@ -483,7 +483,7 @@ bool ProcessUtils::SpawnDaemon(const char * path, char * args[], bool bLogOutput
 		string sArgs;
 		for (int i = 0; args[i] != NULL; i++)
 			sArgs += string("") + args[i] + ",";
-		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnDaemon: path=%s; args=%s\n", path, sArgs.c_str());
+		LoggerWrapper::GetInstance()->Write(LV_PROCESSUTILS,"ProcessUtils::SpawnDaemon: path=%s; args=%s", path, sArgs.c_str());
 	}
 
 	switch (pid = fork())
