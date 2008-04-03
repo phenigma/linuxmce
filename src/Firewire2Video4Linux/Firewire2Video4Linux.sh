75400c96fc07216e758843f769e5e1d40c26d8a3
rgavril <rgavril>
2008-04-03 12:02:01 +0000

    Change offsets to something smaller

--- src/Firewire2Video4Linux/Firewire2Video4Linux.sh
+++ src/Firewire2Video4Linux/Firewire2Video4Linux.sh
@@ -3,8 +3,8 @@
 # This script is creating some v4l pipes that are to be used by
 # Motion_Wrapper for pipeing video1394 content into them.
 
-PipeDevOffset=30
-PipeCount=16
+PipeDevOffset=10
+PipeCount=8
 
 modprobe video1394
 modprobe raw1394
