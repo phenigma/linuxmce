81eee313a9a946905f2112d94f3a5a5bbb27a0e8
radu <radu.cx+linuxmce@gmail.com>
2008-04-04 16:23:08 +0000

    use asym_analog instead of plug:dmix

--- src/SimplePhone/SimplePhone.cpp
+++ src/SimplePhone/SimplePhone.cpp
@@ -85,7 +85,7 @@ bool SimplePhone::GetConfig()
 	/* Get MD Audio Settings */
 	string sAudioSettings = Get_MD_AudioSettings();
 	LoggerWrapper::GetInstance()->Write(LV_STATUS, "MD Audio Settings: %s", sAudioSettings.c_str());
-	string sAlsaDevice = "plug:dmix";
+	string sAlsaDevice = "asym_analog";
 
 	for (size_t i = 0; i < sAudioSettings.length(); i++)
 	{
@@ -97,7 +97,7 @@ bool SimplePhone::GetConfig()
 				break;
 			case 'S':
 			case 'L':
-				sAlsaDevice = "plug:dmix";
+				sAlsaDevice = "asym_analog";
 				break;
 			default:
 				LoggerWrapper::GetInstance()->Write(LV_STATUS, "Flag unprocessed: '%c'", sAudioSettings[i]);
