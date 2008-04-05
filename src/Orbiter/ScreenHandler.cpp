5bb01ac4fd846f3bb0331c95be6b676dd66386ec
aaron <aaron.b@plutohome.com>
2008-04-05 04:13:11 +0000

    fix typo in last commit

--- src/Orbiter/ScreenHandler.cpp
+++ src/Orbiter/ScreenHandler.cpp
@@ -1543,10 +1543,10 @@ void ScreenHandler::SCREEN_Cannot_Reload_Router(long PK_Screen, string sDescript
 		//OK
 		m_pOrbiter->m_mapTextString[TEXT_Ok_CONST],"",
 		//Force text
-		m_pOrbiter->m_mapTextString[TEXT_Force_Reload_CONST],"",
+		m_pOrbiter->m_mapTextString[TEXT_Force_Reload_CONST],
 		//the command
 		StringUtils::itos(m_pOrbiter->m_dwPK_Device) + " " TOSTRING(DEVICEID_DCEROUTER) 
-		" " TOSTRING(MESSAGETYPE_SYSCOMMAND) " " TOSTRING(SYSCOMMAND_RELOAD_FORCED)
+		" 7 6"
 	);
 }
 //-----------------------------------------------------------------------------------------------------
