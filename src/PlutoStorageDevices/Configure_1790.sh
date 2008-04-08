1ea3f75c8006f44919e0d74bec45c8f58ea4128d
rgavril <rgavril>
2008-04-08 08:31:21 +0000

    Disable mount limit for ext3 drives to avoid warning messages

--- src/PlutoStorageDevices/Configure_1790.sh
+++ src/PlutoStorageDevices/Configure_1790.sh
@@ -4,6 +4,10 @@
 # Configuration script for : Device Template 1790 - Generic Internal Drive
 #
 
+. /usr/pluto/bin/SQL_Ops.sh
+
+DD_UUID=267
+
 Params=("$@")
 for ((i = 0; i < ${#Params[@]}; i++)); do
 	case "${Params[$i]}" in
@@ -12,6 +16,10 @@ for ((i = 0; i < ${#Params[@]}; i++)); do
 		-m) ((i++)); Device_MAC="${Params[$i]}" ;;
 	esac
 done
+
+# Disable automatic filesystem checking for ext3 drives
+Device_UUID=$(RunSQL "SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_Device=$Device_ID AND FK_DeviceData=$DD_UUID")
+tune2fs -c 0 "/dev/disk/by-uuid/$Device_UUID" 2>/dev/null || :
 
 /usr/pluto/bin/StorageDevices_Setup.sh --cascade
 
