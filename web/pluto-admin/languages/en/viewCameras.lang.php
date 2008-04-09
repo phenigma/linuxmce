131d123bda3e665632f029c9201eed42de6f84e9
vali <vali>
2008-04-09 13:10:00 +0000

    Changed view camera to self-refresh cameras.

--- web/pluto-admin/languages/en/viewCameras.lang.php
+++ web/pluto-admin/languages/en/viewCameras.lang.php
@@ -7,4 +7,5 @@ $TEXT_NO_CAMERA_SELECTED_CONST='No camera selected.';
 $TEXT_SELECTED_CAMERAS_CONST='Selected cameras';
 $TEXT_NO_CAMERA_DEVICES_IN_INSTALLATION_CONST='No camera devices.';
 $TEXT_NO_ROOM_CONST='Not assigned to a room';
+$TEXT_REFRESH_INTERVAL_CONST='Refresh interval';
 ?>
