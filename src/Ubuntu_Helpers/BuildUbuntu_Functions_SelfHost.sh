05a06233c2aefe05f06ea49fff9d880ac78ca3d5
danielk <danielk@cuymedia.net>
2008-04-10 20:48:09 +0000

    Missing sudo

--- src/Ubuntu_Helpers/BuildUbuntu_Functions_SelfHost.sh
+++ src/Ubuntu_Helpers/BuildUbuntu_Functions_SelfHost.sh
@@ -431,7 +431,7 @@ function SUDO_Install_Build_Needed_Packages_SUDO
         printf "\n"
 
         echo "Installing mysql stuff"
-        apt-get -y install libmysqlclient15-dev > /dev/null
+        sudo apt-get -y install libmysqlclient15-dev > /dev/null
 
         echo "Installing libxine development packages"
 	pkgs="libcaca-dev liblircclient-dev libtheora-dev libflac-dev=1.1.2-5ubuntu2 libmodplug-dev libgnomevfs2-dev libspeex-dev libmad0-dev libxvmc-dev automake1.9 autoconf libtool libcdio-dev sgmltools-lite dpatch transfig libavformat-dev libpostproc-dev libavcodec-dev libraw1394-dev libdc1394-13-dev"
