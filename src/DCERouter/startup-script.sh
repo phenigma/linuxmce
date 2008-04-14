2d272d0296c5223e543c2dd9995e531cad8d75dc
rgavril <rgavril>
2008-04-14 14:09:30 +0000

    Double/Triple Check that smaba is running after StorageDevices_Setup

--- src/DCERouter/startup-script.sh
+++ src/DCERouter/startup-script.sh
@@ -117,7 +117,8 @@ StartDaemon  "Starting Dhcp Plugin" "/usr/pluto/bin/Dhcpd-Plugin.sh" "DhcpdPlugi
 StartDaemon  "Start PNP MD Plugin" "/usr/pluto/bin/Start_NewMD_interactor.sh" "NewMDinteractor"
 StartDaemon  "Start Voicemail Monitor" "/usr/pluto/bin/VoiceMailMonitor.sh" "VoiceMailMonitor"
 StartService "Creating Firewire 2 Video4Linux Pipes" "/usr/pluto/bin/Firewire2Video4Linux.sh"
-StartService "Configuring Pluto Storage Devices" "/usr/pluto/bin/StorageDevices_Setup.sh" "&"
+StartService "Configuring Pluto Storage Devices" "/usr/pluto/bin/StorageDevices_Setup.sh"
+/etc/init.d/samba start
 StartService "Starting UPNP Server" "/usr/pluto/bin/StorageDevices_UPNPServer.sh" "&"
 StartService "Samba Server Detection" "/usr/pluto/bin/StorageDevices_SambaRadar.sh"
 StartService "Detecting Timezone" "/usr/pluto/bin/Timezone_Detect.sh" "&"
