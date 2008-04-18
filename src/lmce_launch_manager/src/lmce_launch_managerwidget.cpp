40a9344f8c5937a677b84f9aabe959e323b10769
aaron <aaron.b@plutohome.com>
2008-04-18 13:42:03 +0000

    more verbose text

--- src/lmce_launch_manager/src/lmce_launch_managerwidget.cpp
+++ src/lmce_launch_manager/src/lmce_launch_managerwidget.cpp
@@ -859,7 +859,7 @@ bool lmce_launch_managerWidget::initialize_LMdevice(bool bRetryForever/*=false*/
 		{
 			//display message, log and continue
 			QTimer *msgTimer = new QTimer(this, "msgTimer");
-			QMessageBox *mb = new QMessageBox(QString("Information"), QString("Please reload router..."), QMessageBox::NoIcon, QMessageBox::Ok, QMessageBox::NoButton, QMessageBox::NoButton);
+			QMessageBox *mb = new QMessageBox(QString("Information"), QString("Please go to an existing Orbiter and choose 'quick reload router'.  This media director will start after you do..."), QMessageBox::NoIcon, QMessageBox::Ok, QMessageBox::NoButton, QMessageBox::NoButton);
 			connect(msgTimer, SIGNAL(timeout), mb, SLOT(close()));
 			msgTimer->stop();
 			msgTimer->start(5000, true);
