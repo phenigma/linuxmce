8bc6eb8e7b74cb55f4f88a0c5eee9848adf6ea07
rgavril <rgavril>
2008-05-22 09:33:35 +0000

    Fix some bugs in the diversion that cause the module not to load

--- src/pluto-nvidia-drivers/mkr_preinst.sh
+++ src/pluto-nvidia-drivers/mkr_preinst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [[ "$1" == "install" ]] ;then
-	dpkg-divert --add --rename --package pluto-nvidia-drivers --divert /sbin/lrm-video.pluto /sbin/lrm-video
+	dpkg-divert --add --rename --package pluto-nvidia-video-drivers --divert /sbin/lrm-video.pluto /sbin/lrm-video
 fi
