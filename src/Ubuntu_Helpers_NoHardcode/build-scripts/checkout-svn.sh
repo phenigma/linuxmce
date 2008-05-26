79c157db21d7912cfb42fde6880f90425662110e
rgavril <rgavril>
2008-05-26 09:25:19 +0000

    Changed the svn checkout script so it can work with branches

--- src/Ubuntu_Helpers_NoHardcode/build-scripts/checkout-svn.sh
+++ src/Ubuntu_Helpers_NoHardcode/build-scripts/checkout-svn.sh
@@ -4,47 +4,27 @@
 
 function Checkout_Svn {
 	DisplayMessage "**** STEP : SVN CHECKOUT"
-	local Branch="${1:-trunk}"
-	local BranchParts="$2" # ex: "src web"; the rest will come from trunk
 
-	local AllParts="src ubuntu web misc_utils installers config-pkgs"
-	if [[ -z "$BranchParts" ]]; then
-		BranchParts="$AllParts"
-	fi
 
 	DisplayMessage "Removing old svn checkout dir"
 	[[ -d "$svn_dir" ]] && mkdir -p "$svn_dir"
 	rm -rf "${svn_dir}/trunk"
 	
-	for svn_module in ${BranchParts}; do
-		mkdir -p "${svn_dir}/trunk/$svn_module"
-		DisplayMessage "Checking out ${svn_url}/pluto/$Branch/$svn_module"
-		svn co "${svn_url}/pluto/${Branch}/${svn_module}"  "${svn_dir}/trunk/$svn_module" || Error "Failed to checkout {$svn_url}/pluto/$Branch/$svn_module"
-	done
-
-	# get unmarked parts from trunk
+	local AllParts="src ubuntu web misc_utils installers config-pkgs"
 	for svn_module in ${AllParts}; do
-		if [[ " $BranchParts " == *" $svn_module "* ]]; then
-			# this part was marked to be taken from the branch
-			continue
-		fi
-
-		#get part from trunk
-		mkdir -p ${svn_dir}/trunk/$svn_module
-		DisplayMessage "Checking out {$svn_url}/pluto/trunk/$svn_module"
-		svn co ${svn_url}/pluto/trunk/$svn_module  ${svn_dir}/trunk/$svn_module || Error "Failed to checkout {$svn_url}/pluto/trunk/$svn_module"
+		mkdir -p "${svn_dir}/trunk/$svn_module"
+		DisplayMessage "Checking out ${svn_url}/$svn_module"
+		svn co "${svn_url}/${svn_module}"  "${svn_dir}/trunk/$svn_module" || Error "Failed to checkout ${svn_url}/$svn_module"
 	done
-
 
 	if [[ "$svn_private_url" != "" ]] && [[ "$svn_private_user" != "" ]] && [[ "$svn_private_pass" != "" ]] ;then
 		pushd ${svn_dir}/trunk/src
-			DisplayMessage "Checking out ${svn_private_url}/pluto-private/$Branch/"
-			svn co --username "$svn_private_user" --password "$svn_private_pass" ${svn_private_url}/pluto-private/"$Branch"/src/ZWave/ || Error "Failed to checkount ${svn_private_url}/pluto-private/$Branch/src/ZWave"
-			svn co --username "$svn_private_user" --password "$svn_private_pass" ${svn_private_url}/pluto-private/"$Branch"/src/Fiire_Scripts/ || Error "Failed to checkount ${svn_private_url}/pluto-private/$Branch/src/Fiire_Scripts"
-			svn co --username "$svn_private_user" --password "$svn_private_pass" ${svn_private_url}/pluto-private/"$Branch"/src/RFID_Interface/ || Error "Failed to checkount ${svn_private_url}/pluto-private/$Branch/src/RFID_Interface"
+			DisplayMessage "Checking out ${svn_private_url}"
+			svn co --username "$svn_private_user" --password "$svn_private_pass" ${svn_private_url}/src/ZWave/          || Error "Failed to checkount ${svn_private_url}/src/ZWave"
+			svn co --username "$svn_private_user" --password "$svn_private_pass" ${svn_private_url}/src/Fiire_Scripts/  || Error "Failed to checkount ${svn_private_url}/src/Fiire_Scripts"
+			svn co --username "$svn_private_user" --password "$svn_private_pass" ${svn_private_url}/src/RFID_Interface/ || Error "Failed to checkount ${svn_private_url}/src/RFID_Interface"
 		popd
 	fi
 }
 
-Checkout_Svn
-
+Checkout_Svn 
