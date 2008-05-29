eb675b3c8636b498c20a398c3d01dd83d2ed6098
radu <radu.cx+linuxmce@gmail.com>
2008-05-29 20:18:20 +0000

    Reset button does more to restore default settings. Fresh install autodetects mode of operation. Router will keep local DHCP IP, even if it runs in switch mode - results some sort of managed switch. Default to the 192.168.81.1/24 IP/network

--- src/Embedded_Boot/usr/bin/Pluto_Reset.sh
+++ src/Embedded_Boot/usr/bin/Pluto_Reset.sh
@@ -3,4 +3,9 @@
 mkdir -p /etc/pluto/save/
 cp /etc/pluto/* /etc/pluto/save/
 cp /etc/pluto/orig/* /etc/pluto/
+
+uci set network.wan.proto=dhcp
+uci set network.eth0.vlan0="1 2 3 4 5*"
+uci set network.eth0.vlan1="0 5"
+uci commit
 reboot
