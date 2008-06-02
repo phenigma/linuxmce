ecf471f6077d5944a2f9a9f6a52e9ea6d66df144
rgavril <rgavril>
2008-06-02 10:49:41 +0000

    Report ip and mac

---
+++ src/Embedded_Boot/usr/bin/Report_AP.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Bridged mode
+if [[ "`uci get network.eth0.vlan1`" == '' ]] ;then
+	# With Static IP
+	if [[ "`uci get network.lan.proto`" == 'static' ]] ;then
+		Report_IP=`uci get network.lan.ipaddr`
+
+	# With Dynamic IP
+	else
+		Report_IP=`ifconfig br-lan | grep 'inet addr:' | sed 's/.*inet addr:\([0-9.]*\).*/\1/g'`
+	fi
+
+# Routing mode
+else
+ 	# With Static IP
+	if [[ "`uci get network.wan.proto`" == 'static' ]] ;then
+		Report_IP=`uci get network.wan.ipaddr`
+	
+	# With Dynamic IP
+	else
+		Report_IP=`ifconfig eth0.1 | grep 'inet addr:' | sed 's/.*inet addr:\([0-9.]*\).*/\1/g'`
+	fi
+fi
+
+Report_MAC=`ifconfig eth0 | grep 'HWaddr' | sed 's/.*HWaddr \([0-9A-F:]*\).*/\1/g'`
+
+
+Reporting "$Report_IP - $Report_MAC"
+wget "http://plutohome.com/ReportAP.php?IP=${Report_IP}&MAC=${Report_MAC}"
