1c2c9124aad6a165014c443af473431953493d8a
radu <radu.cx+linuxmce@gmail.com>
2008-06-04 11:08:37 +0000

    reorganize smart network setup flow

--- src/Embedded_Boot/usr/bin/Pluto_Reset.sh
+++ src/Embedded_Boot/usr/bin/Pluto_Reset.sh
@@ -5,6 +5,7 @@ cp /etc/pluto/* /etc/pluto/save/
 cp /etc/pluto/orig/* /etc/pluto/
 
 uci set network.wan.proto=dhcp
+uci set network.wan.ifname=eth0.1
 uci set network.eth0.vlan0="1 2 3 4 5*"
 uci set network.eth0.vlan1="0 5"
 uci commit
