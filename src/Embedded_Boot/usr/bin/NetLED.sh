277f4027cf61350e8d40b8c500922ca9830b5590
radu <radu.cx+linuxmce@gmail.com>
2008-06-05 08:37:04 +0000

    improve network detection: when at least one server is reacheable, it means there could be an Internet connection

--- src/Embedded_Boot/usr/bin/NetLED.sh
+++ src/Embedded_Boot/usr/bin/NetLED.sh
@@ -40,23 +40,26 @@ Srv_plutohome=plutohome.com
 PID_NoNet=
 
 while :; do
+	Reach=0
 	for key in $Keys; do
 		eval "server=\$(echo \$Srv_$key)"
 		IP=$(/usr/bin/NetworkTest $server 80 2>/dev/null)
 		Reachable=$?
 		eval "IP_$key=$IP"
-		eval "Reach_$key=$Reachable"
+		if [[ "$Reachable" -eq 0 ]]; then
+			Reach=$(expr $Reach + 1)
+		fi
 	done
+
+	if [[ "$Reach" -eq 0 ]]; then
+		NoInternet
+		sleep 5
+		continue
+	fi
 
 	LastIP=
 	for key in $Keys; do
 		eval "IP=\$(echo \$IP_$key)"
-		eval "Reach=\$(echo \$Reach_$key)"
-		if [[ "$Reach" != 0 ]]; then
-			NoInternet
-			sleep 5
-			continue 2
-		fi
 		if [[ "$IP" == "$LastIP" ]]; then
 			NoInternet
 			sleep 5
