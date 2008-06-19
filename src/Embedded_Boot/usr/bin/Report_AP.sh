16207e76501e4eb6d073dde39af4f2ded5265c91
rgavril <rgavril>
2008-06-19 15:26:23 +0000

    Need to improvise a little when on dhcp

--- src/Embedded_Boot/usr/bin/Report_AP.sh
+++ src/Embedded_Boot/usr/bin/Report_AP.sh
@@ -5,7 +5,12 @@ log() {
 }
 
 LanIP=`uci get network.lan.ipaddr`
-WanIP=`uci get network.wan.ipaddr`
+if [[ "`uci get network.wan.proto`" == 'dhcp' ]] ;then
+	WanIface=`network.wan.ifname`
+	WanIP=`ifconfig $WanIface | grep 'inet addr:' | sed 's/.*inet addr:\([0-9.]*\).*/\1/g'`
+else
+	WanIP=`uci get network.wan.ipaddr`
+fi
 MAC=`ifconfig eth0 | grep 'HWaddr' | sed 's/.*HWaddr \([0-9A-F:]*\).*/\1/g'`
 
 log "Reporting lan:$LanIP wan:$WanIP mac:$MAC"
