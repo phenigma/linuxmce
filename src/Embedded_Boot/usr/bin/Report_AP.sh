6838fe4968c598221657f77d9753ea30d1224e57
rgavril <rgavril>
2008-06-19 14:55:58 +0000

    Logging and complete/clean reporting

--- src/Embedded_Boot/usr/bin/Report_AP.sh
+++ src/Embedded_Boot/usr/bin/Report_AP.sh
@@ -1,40 +1,12 @@
 #!/bin/sh
 
-# Bridged mode
-if [[ "`uci get network.eth0.vlan1`" == '' ]] ;then
-	# With Static IP
-	if [[ "`uci get network.lan.proto`" == 'static' ]] ;then
-		Report_IP=`uci get network.lan.ipaddr`
+log() {
+	logger -s -t 'fiire-report' $*
+}
 
-	# With Dynamic IP
-	else
-		wait=0
-		while [[ "$Report_IP" == "" ]] && [[ "$wait" != "15" ]] ;do
-			Report_IP=`ifconfig br-lan | grep 'inet addr:' | sed 's/.*inet addr:\([0-9.]*\).*/\1/g'`
-			sleep 1
-			wait=$(( wait + 1 ))
-		done
-	fi
+LanIP=`uci get network.lan.ipaddr`
+WanIP=`uci get network.wan.ipaddr`
+MAC=`ifconfig eth0 | grep 'HWaddr' | sed 's/.*HWaddr \([0-9A-F:]*\).*/\1/g'`
 
-# Routing mode
-else
- 	# With Static IP
-	if [[ "`uci get network.wan.proto`" == 'static' ]] ;then
-		Report_IP=`uci get network.wan.ipaddr`
-	
-	# With Dynamic IP
-	else
-		wait=0
-		while [[ "$Report_IP" == "" ]] && [[ "$wait" != "15" ]] ;do
-			Report_IP=`ifconfig eth0.1 | grep 'inet addr:' | sed 's/.*inet addr:\([0-9.]*\).*/\1/g'`
-			sleep 1
-			wait=$(( wait + 1 ))
-		done
-	fi
-fi
-
-Report_MAC=`ifconfig eth0 | grep 'HWaddr' | sed 's/.*HWaddr \([0-9A-F:]*\).*/\1/g'`
-
-
-echo "Reporting $Report_IP - $Report_MAC"
-wget -qO/dev/null "http://plutohome.com/ReportAP.php?IP=${Report_IP}&MAC=${Report_MAC}"
+log "Reporting lan:$LanIP wan:$WanIP mac:$MAC"
+wget -qO/dev/null "http://local.fiire.com/ReportAP.php?LanIP=${LanIP}&WanIP=${WanIP}&MAC=${MAC}"
