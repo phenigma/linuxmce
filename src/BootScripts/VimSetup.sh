299a35742c1bacbedb489e04d63451a7c172bcd8
radu <radu.cx+linuxmce@gmail.com>
2008-06-24 17:28:05 +0000

    the -i parameter doesn't work for pattern ranges

--- src/BootScripts/VimSetup.sh
+++ src/BootScripts/VimSetup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -ir '/syntax on/ s/^"//; /set background/ s/^"//; /if has\("autocmd"\)/,/^$/ s/^"//; /set showcmd/ s/^"//; /set showmatch/ s/^"//' /etc/vim/vimrc
+sed -r '/syntax on/ s/^"//; /set background/ s/^"//; /if has\("autocmd"\)/,/^$/ s/^"//; /set showcmd/ s/^"//; /set showmatch/ s/^"//' /etc/vim/vimrc >/etc/vim/vimrc.$$
+mv /etc/vim/vimrc{.$$,}
