d5a7a019d973f636506957242558931adc122e97
danielk <danielk@cuymedia.net>
2008-07-08 13:02:09 +0000

    Initial checkin of a pluto-avwizard-sounds package. Will be renamed and svn propset in a seperate commit.

---
+++ src/pluto-avwizard-sounds-1.1/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(pluto-avwizard-sounds):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="pluto-avwizard-sounds" command="/usr/bin/pluto-avwizard-sounds"
---
+++ src/pluto-avwizard-sounds-1.1/debian/pluto-avwizard-sounds-default.ex
@@ -0,0 +1,10 @@
+# Defaults for pluto-avwizard-sounds initscript
+# sourced by /etc/init.d/pluto-avwizard-sounds
+# installed at /etc/default/pluto-avwizard-sounds by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
