799fa7ec63eb77cff771393245cb39eff85e2cc0
cj_man <cj_man@pdsnet.ro>
2008-07-11 12:40:32 +0000

    added support for RA_EMAIL

--- src/Embedded_Boot/usr/bin/fiire-ra-config.sh
+++ src/Embedded_Boot/usr/bin/fiire-ra-config.sh
@@ -3,6 +3,7 @@
 USERNAME="$1"
 PASSWORD="$2"
 RA_DISABLED="$3"
+RA_EMAIL="$4"
 
 CONF_FILE="/etc/fiire-ra/fiire-ra.conf"
 SSH_KEY_FILE="/etc/fiire-ra/keys/fiire-ra.key"
@@ -51,6 +52,7 @@ echo "RA_DISABLED=$RA_DISABLED" >> "$CONF_FILE"
 echo "USERNAME=$USERNAME"  >> "$CONF_FILE"
 echo "PASSWORD=$PASSWORD"  >> "$CONF_FILE"
 echo "PORT=$PORT"          >> "$CONF_FILE"
+echo "RA_EMAIL=$RA_EMAIL"  >> "$CONF_FILE"
 
 chmod -R 700 "`dirname "$SSH_KEY_FILE"`"
 
