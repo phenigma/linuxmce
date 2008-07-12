d8abee5295052a6c4a1bde95a0155c4124bfb490
jdecker <jondecker76@windstream.net>
2008-07-12 13:57:05 +0000

    Added more verbage for sqlCVS web admin pages

--- web/pluto-admin/languages/en/sqlcvs.lang.php
+++ web/pluto-admin/languages/en/sqlcvs.lang.php
@@ -16,6 +16,9 @@ $TEXT_TRY_AGAIN_CONST='Try again';
 $TEXT_PARAMETERS_ERROR_CONST='Parameters error';
 $TEXT_ERROR_DETAILS_CONST='Error details';
 $TEXT_DATABASE_CONST='Database';
-$TEXT_COMMIT_INFO_CONST='Note: If you have sqlCVS credentials use them to commit you changes as authorised, otherwise your commits will be submitted for review and only after this will be accepted into main database.';
-$TEXT_SQLCVS_COMMENT='If you would like a comment sent along with this sqlCVS action, please type it below.<BR>(For example, a reference to a Mantis Ticket number or the reason for this change)';
+$TEXT_COMMIT_INFO_CONST='Note: If you have sqlCVS credentials use them to commit your changes as authorised, otherwise your commits will be submitted for review and only after this will they be accepted into the main database.';
+$TEXT_SQLCVS_COMMENT='If you would like a comment sent along with this sqlCVS action, please type it below.<BR>(For example, a reference to a Mantis Ticket number or the reason for this change)<BR>Note: Comments only apply to checkins';
+$TEXT_SQLCVS_CHECKIN_HINT='Use this page if you want to checkin entire repositories/tables all at once. If you want to have finer control and<BR>checkin individual changes, please do this from the sqlCVS Diff page.';
+$TEXT_SQLCVS_UPDATE_HINT='Use this page to pull in the latest changes from the master server. None of your local changes will be affected.';
+$TEXT_SQLCVS_DIFF_HINT='Use this page to checkin or revert any changes to your local database. If you are looking to pull in the latest changes from the master server, please use the sqlCVS Update page.';
 ?>
