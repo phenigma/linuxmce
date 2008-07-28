f56cc91751c1e3b12b13eedf0c4ac3a713ab6d27
cj_man <cj_man@pdsnet.ro>
2008-07-28 13:11:36 +0000

    del other interfaces and restart services in order net /fw / dnsmasq

--- src/Embedded_Boot/usr/bin/RouterMode_Gateway.sh
+++ src/Embedded_Boot/usr/bin/RouterMode_Gateway.sh
@@ -10,9 +10,13 @@ fi
 
 echo "$(date -R) Router mode: gateway / begin" >>/tmp/log.routermode
 
-uci set dhcp.$Cfg.dynamicdhcp=1
-uci set dhcp.$CfgMasq.authoritative=1
+#enable dhcp
+		uci set dhcp.$Cfg.ignore=0
+		uci set dhcp.$Cfg.dynamicdhcp=1
+		uci set dhcp.$Cfg.force=1
+		uci set dhcp.$CfgMasq.authoritative=1
 
+#conf vlan's
 Model=$(cat /proc/diag/model)
 case "$Model" in
 	"ASUS WL-500g Premium")
@@ -27,14 +31,20 @@ esac
 
 uci set network.wan.ifname=eth0.1
 
+#kill dhcp client on other if
 Dhclient=$(ps ax|grep 'udhcpc .*br-lan:0'|grep -v grep|awk '{print $1}')
 if [[ -n "$Dhclient" ]]; then
 	kill "$Dhclient"
 fi
 
 uci commit
+sync
 
+#del other if
+ifconfig br-lan:0 down
+
+#restart services
+/etc/init.d/network restart
 /etc/init.d/firewall restart
 /etc/init.d/dnsmasq restart
-/etc/init.d/network restart
 echo "$(date -R) Router mode: gateway / end" >>/tmp/log.routermode
--- src/Embedded_Boot/usr/bin/RouterMode_Switch.sh
+++ src/Embedded_Boot/usr/bin/RouterMode_Switch.sh
@@ -10,21 +10,31 @@ fi
 
 echo "$(date -R) Router mode: switch / begin" >>/tmp/log.routermode
 
-uci set dhcp.$Cfg.dynamicdhcp=0
-uci set dhcp.$CfgMasq.authoritative=0
+#disable dhcpd
+		uci set dhcp.$Cfg.ignore=0
+		uci set dhcp.$Cfg.force=1
+		uci set dhcp.$Cfg.dynamicdhcp=0
+		uci set dhcp.$CfgMasq.authoritative=0
+
+#conf vlan's
 uci set network.eth0.vlan0="0 1 2 3 4 5*"
 uci del network.eth0.vlan1
+
 uci set network.wan.ifname=br-lan:0
 
-ifconfig eth0.1 down
+#kill dhcp client on other if
 Dhclient=$(ps ax|grep 'udhcpc .*eth0.1'|grep -v grep|awk '{print $1}')
 if [[ -n "$Dhclient" ]]; then
 	kill "$Dhclient"
 fi
 
 uci commit
+sync
 
+#del other if
+ifconfig eth0.1 down
+
+/etc/init.d/network restart
 /etc/init.d/firewall restart
 /etc/init.d/dnsmasq restart
-/etc/init.d/network restart
 echo "$(date -R) Router mode: switch / end" >>/tmp/log.routermode
