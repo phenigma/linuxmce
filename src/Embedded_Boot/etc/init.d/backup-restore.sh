47f2c115e19e269a22b6e136e965593084a6e1dc
cj_man <cj_man@pdsnet.ro>
2008-08-07 11:45:48 +0000

    split in backup-restore for critical backup and backup-restore-nc fo non critical backups

--- src/Embedded_Boot/etc/init.d/backup-restore.sh
+++ src/Embedded_Boot/etc/init.d/backup-restore.sh
@@ -1,31 +1,21 @@
 #!/bin/sh /etc/rc.common
 
 START=03
-BACKUP_STORAGE_URL="http://backup.fiire.com/"
+log_file="/tmp/log.upgrade"
 
 start() {
 	BackupID=`nvram get backupid`
 	if [[ "$BackupID" != "" ]] ;then
-
+		
 		# Restore critical config files
+		echo "`date` - Restore critical config files" >> $log_file
 		nvram get backup | uudecode > /tmp/bk2nvram.tar.gz
 		cd /
-		tar zxvf /tmp/bk2nvram.tar.gz
+		tar zxvf /tmp/bk2nvram.tar.gz 2>&1 1>>$log_file
 		#rm -f /tmp/bk2nvram.tar.gz
-
-		# Start networking
-		/etc/init.d/network start
-
-		# Restore non critical config files
-		InstallationID=`cat /etc/pluto/installation_number`
-		wget -O /tmp/bk2net.tar.gz "$BACKUP_STORAGE_URL/$InstallationID-$BackupID"
-		cd /
-		tar zxvf /tmp/bk2net.tar.gz
-		rm -f /tmp/bk2net.tar.gz
-
+        echo "`date` - Clear nvram backup variable" >> $log_file
 		nvram set backup=""
-		nvram set backupid=""
-		nvram commit
+		#nvram commit
 	fi
 }
 
