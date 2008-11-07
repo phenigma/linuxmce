0334f05bf31199065f56c7e9f594a75fe5eedd1c
hari <hari@vt100.at>
2008-11-07 17:13:04 +0000

    better portability

--- src/ZWave/ZWApi.cpp
+++ src/ZWave/ZWApi.cpp
@@ -210,16 +210,20 @@ void *ZWApi::ZWApi::decodeFrame(char *frame, size_t length) {
 						case GENERIC_TYPE_GENERIC_CONTROLLER:
 							DCE::LoggerWrapper::GetInstance()->Write(LV_ZWAVE,"GENERIC TYPE: Generic Controller");
 							// [internal id] \t [description] \t [room name] \t [device template] \t [floorplan id] \n
+#ifdef DEVICETEMPLATE_ZWave_Controller_CONST
 							sprintf(tempbuf2, "%d\t\t\t%d\t\n", tmp_nodeid, DEVICETEMPLATE_ZWave_Controller_CONST);
 							newNode->plutoDeviceTemplateConst = DEVICETEMPLATE_ZWave_Controller_CONST;
 							deviceList += tempbuf2;
+#endif
 							break;
 						;;
 						case GENERIC_TYPE_STATIC_CONTROLLER:
 							DCE::LoggerWrapper::GetInstance()->Write(LV_ZWAVE,"GENERIC TYPE: Static Controller");
+#ifdef DEVICETEMPLATE_ZWave_Controller_CONST
 							sprintf(tempbuf2, "%d\t\t\t%d\t\n", tmp_nodeid, DEVICETEMPLATE_ZWave_Controller_CONST);
 							newNode->plutoDeviceTemplateConst = DEVICETEMPLATE_ZWave_Controller_CONST;
 							deviceList += tempbuf2;
+#endif
 							break;
 						;;
 						case GENERIC_TYPE_THERMOSTAT:
