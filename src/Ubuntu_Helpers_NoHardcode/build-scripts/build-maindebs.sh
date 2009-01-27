b7c456b447b8690adcacade3e62cfa4a940bbb96
tschak <thom.cherryhomes@gmail.com>
2009-01-27 17:29:36 +0000

    Added shift-key-state back into maindebs.

--- src/Ubuntu_Helpers_NoHardcode/build-scripts/build-maindebs.sh
+++ src/Ubuntu_Helpers_NoHardcode/build-scripts/build-maindebs.sh
@@ -82,7 +82,7 @@ function build_main_debs() {
 		"intrepid")
 			Distro_ID="17"
 			exclude_list=$exclude_list,$mkr_tira
-			exclude_list=$exclude_list,$mkr_shift_key_state
+#			exclude_list=$exclude_list,$mkr_shift_key_state
 #			exclude_list=$exclude_list,$mkr_simplephone
 			exclude_list=$exclude_list,$mkr_launch_manager_package
 			exclude_list=$exclude_list,$mkr_irtrans_package
