b8adf5b08a2636913c60e44336b11937679cc883
posde <posde@theinternet.de>
2009-03-03 08:42:46 +0000

    Fixes #125 - dpkg-divert is only run when needed

--- src/PlutoStorageDevices/mkr_postrm.sh
+++ src/PlutoStorageDevices/mkr_postrm.sh
@@ -18,5 +18,7 @@ rm -f /etc/rc2.d/StorageDevices_StatusRadar.sh
 rm -f /etc/init.d/StorageDevices_StatusRadar.sh
 
 ## Remove umount diversion
-dpkg-divert --remove /bin/umount
-dpkg-divert --remove /bin/mount
+if [ remove = "$1" ]; then 
+	dpkg-divert --package pluto-storage-devices --remove --rename --divert /bin/umount.wraped /bin/umount
+	dpkg-divert --package pluto-storage-devices --remove --rename --divert /bin/mount.wraped /bin/umount
+fi
