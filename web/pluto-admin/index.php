6cc96a7c3a508839c6ec165ffbab411cc66f955c
jdecker <jondecker76@windstream.net>
2009-03-13 01:02:34 +0000

    refs #97 - adds redirect to new lmce-admin URL

---
+++ web/pluto-admin/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>LinuxMCE - Redirecting.....</title>
+<meta http-equiv="REFRESH" content="0;url=../lmce-admin"></HEAD>
+<BODY>
+&nbsp;
+</BODY>
+</HTML>
