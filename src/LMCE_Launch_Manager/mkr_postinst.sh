46382564c0a98469d5bce04aa629007aa64438f0
jdecker <jondecker76@windstream.net>
2009-03-28 23:38:51 +0000

    refs #48 - disables cleancore's startCore.sh script temporarily for LM testing.

--- src/LMCE_Launch_Manager/mkr_postinst.sh
+++ src/LMCE_Launch_Manager/mkr_postinst.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
+#create launcher icons
 for dir in /home/* ;do
 	if [ -d "$dir/Desktop" ] ;then
 		ln -s /usr/share/applications/lmce_launch_manager.desktop "$dir/Desktop/lmce_launch_manager.desktop" || :
 		chmod 666 "$dir/Desktop/lmce_launch_manager.desktop" || :
 	fi
 done
+
+#Temporarily disable startCore.sh cleancore script for 3-29-2009 build LM testing.
+#Backup the original, then replace it with a "neutered" version
+mv /usr/pluto/bin/startCore.sh /usr/pluto/bin/startCore.sh._backup
+cat >/usr/pluto/bin/startCore.sh <<"EOF"
+#!/bin/sh
+
+exit 0
+EOF
+chmod +x /usr/pluto/bin/startCore.sh
 
