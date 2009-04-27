3a027a84c5ba78325b55fd1263a58d2420c40c68
jdecker <jondecker76@windstream.net>
2009-04-27 04:04:40 +0000

    refs #124 - Post install modifies mythconverge.storagegroup.groupname to be a VARCHAR(64) instead of (32). This is because 32 characters is not enough room to display some LMCE device names.

--- src/MythTV_PlugIn/mkr_postinst.sh
+++ src/MythTV_PlugIn/mkr_postinst.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 . /usr/pluto/bin/Utils.sh
+. /usr/pluto/bin/SQL_Ops.sh
+. /usr/pluto/bin/Config_Ops.sh
 
 AddBookmark "http://dcerouter/mythweb/" "MythWeb"
 
 adduser mythtv public || :
 mkdir -p /root/.mythtv && touch /root/.mythtv/ignoregroup || :
+
+#Alter mythconverg.storagegroup - the default VARCHAR(32) is too short for some LMCE drive names to fully be displayed
+Q="ALTER TABLE storagegroup MODIFY groupname VARCHAR(64)"
+UseDB "mythconverg"
+RunSQL "$Q"
 
 #Re-run SetupUsers_Homes PlutoStorageDevices to make sure that the mythtv user gets added to each user's group
 /usr/pluto/bin/SetupUsers_Homes.sh
