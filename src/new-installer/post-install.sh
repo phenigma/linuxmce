3c46fef10d7aea070bf8e274d34056d168e33e2c
posde <posde@theinternet.de>
2009-05-04 07:56:28 +0000

    refs #166 - no patch in post-instal.sh anymore

--- src/new-installer/post-install.sh
+++ src/new-installer/post-install.sh
@@ -13,6 +13,6 @@ addAdditionalTTYStart
 mkdir -p /home/backup/upload
 
 
-# NOTE: Fix for initramfs-tools
-patch -Np0 -i mkinitramfs_dot-fix1.patch -r /dev/null >/dev/null 2>&1 || echo "The initramfs-tools dot-problem was already fixed."
+## NOTE: Fix for initramfs-tools - no longer needed - done in mce-install.sh
+#patch -Np0 -i mkinitramfs_dot-fix1.patch -r /dev/null >/dev/null 2>&1 || echo "The initramfs-tools dot-problem was already fixed."
 
