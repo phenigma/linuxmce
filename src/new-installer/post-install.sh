cee367880e5241fe71fdc3dcf269f5ce9ee671b8
posde <posde@theinternet.de>
2009-05-09 08:21:20 +0000

    #26 - Add note about needing to reboot (incl. telling update-notifier about it)

--- src/new-installer/post-install.sh
+++ src/new-installer/post-install.sh
@@ -16,3 +16,9 @@ mkdir -p /home/backup/upload
 ## NOTE: Fix for initramfs-tools - no longer needed - done in mce-install.sh
 #patch -Np0 -i mkinitramfs_dot-fix1.patch -r /dev/null >/dev/null 2>&1 || echo "The initramfs-tools dot-problem was already fixed."
 
+#
+echo 
+echo The first phase of the install process is completed
+echo Reboot the system to start the final process.
+echo
+/usr/share/update-notifier/notify-reboot-required
