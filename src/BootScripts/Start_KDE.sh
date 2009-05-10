02280f860b6eaa64f8ff6d8dd87cc3c33a3e385a
posde <posde@theinternet.de>
2009-05-10 18:16:35 +0000

    Fixes #183 - Changing IFS only when needed

--- src/BootScripts/Start_KDE.sh
+++ src/BootScripts/Start_KDE.sh
@@ -3,8 +3,6 @@
 . /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/Utils.sh
 
-Old_IFS="$IFS"
-IFS=:
 
 DEVICEDATA_Diskless_Boot=9
 
@@ -13,6 +11,8 @@ IsDiskless=$(GetDeviceData "$PK_Device" "$DEVICEDATA_Diskless_Boot")
 if [[ "$IsDiskless" == 1 ]]; then
 	TheChosenOne=root
 else
+	Old_IFS="$IFS"
+	IFS=:
 	while read user x uid gid rest; do
 		if ((uid < 1000 || uid >= 10000)); then
 			continue
@@ -20,9 +20,9 @@ else
 		TheChosenOne="$user"
 		break
 	done </etc/passwd
+	IFS="$Old_IFS"
 fi
 
-IFS="$Old_IFS"
 
 if su - "$TheChosenOne" -c "DISPLAY=:$Display kcheckrunning"; then
 	exit
