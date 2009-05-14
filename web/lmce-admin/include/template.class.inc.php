668baadc0927cb467fb8fa2c8b1347e96d6ad9dd
posde <posde@theinternet.de>
2009-05-14 19:32:57 +0000

    fixes #196 - Screensaver admin is back where it belongs, thanks to merkur2k

--- web/lmce-admin/include/template.class.inc.php
+++ web/lmce-admin/include/template.class.inc.php
@@ -75,7 +75,7 @@ function setTemplateFileType($type) {
 	$this->templateType = $type;
 	switch($type){
 		case 'homeWithFrames':
-			if(@$_REQUEST['section']=='mediaFilesSyncFrameset' || @$_REQUEST['section']=='mediaBrowserFrameset'){
+		        if(@$_REQUEST['section']=='mediaFilesSyncFrameset' || @$_REQUEST['section']=='mediaBrowserFrameset' || @$_REQUEST['section']=='screenSaverFrameset'){
 				$_SESSION['lastLeftFrameSrc']='';
 				$_SESSION['lastRightFrameSrc']='';
 			}
