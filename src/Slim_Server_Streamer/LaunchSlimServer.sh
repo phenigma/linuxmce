534a9b2f89b001c3d87655393a03fa28652e7251
posde <posde@theinternet.de>
2009-05-14 20:07:57 +0000

    SlimServer plugin should start squeezecenter and not slimserver

--- src/Slim_Server_Streamer/LaunchSlimServer.sh
+++ src/Slim_Server_Streamer/LaunchSlimServer.sh
@@ -3,6 +3,6 @@
 PORT="${1:-9090}"
 PARAMS="--cliport $PORT --audiodir /home/public/data/audio"
 
-sed -ri "s,SLIMOPTIONS=.*,SLIMOPTIONS=\"$PARAMS\",g" /etc/default/slimserver
+sed -ri "s,SLIMOPTIONS=.*,SLIMOPTIONS=\"$PARAMS\",g" /etc/default/squeezecenter
 
-invoke-rc.d slimserver restart
+invoke-rc.d squeezecenter restart
