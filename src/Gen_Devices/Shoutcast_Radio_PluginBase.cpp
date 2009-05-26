40de1358ffb83b96fdf7095b7f147bcd735f058d
sambuca <magnebra@online.no>
2009-05-26 19:32:53 +0000

    Initial working version of Shoutcast Radio Plugin

---
+++ src/Gen_Devices/Shoutcast_Radio_PluginBase.cpp
@@ -0,0 +1,37 @@
+#include "Shoutcast_Radio_PluginBase.h"
+#include "DeviceData_Impl.h"
+#include "Logger.h"
+
+using namespace DCE;
+#include "Shoutcast_Radio_PluginBase.h"
+extern Shoutcast_Radio_Plugin_Command *Create_Shoutcast_Radio_Plugin(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+DeviceData_Impl *Shoutcast_Radio_Plugin_Data::CreateData(DeviceData_Impl *Parent,char *pDataBlock,unsigned long AllocatedSize,char *CurrentPosition)
+{
+	// Peek ahead in the stream.  We're going to pass in the above pointers anyway so it won't affect the position
+	SerializeClass b;
+	b.ResumeReadWrite(pDataBlock,AllocatedSize,CurrentPosition);
+	int iPK_Device = b.Read_unsigned_long();
+	int iPK_Installation = b.Read_unsigned_long();
+	int iPK_DeviceTemplate = b.Read_unsigned_long();
+	switch(iPK_DeviceTemplate) {
+		case 1929:
+			return new Shoutcast_Radio_Plugin_Data();
+	};
+	LoggerWrapper::GetInstance()->Write(LV_STATUS, "Got CreateData for unknown type %d.", iPK_DeviceTemplate);
+	return NULL;
+}
+
+Event_Impl *Shoutcast_Radio_Plugin_Event::CreateEvent( unsigned long dwPK_DeviceTemplate, ClientSocket *pOCClientSocket, unsigned long dwDevice )
+{
+	switch(dwPK_DeviceTemplate) {
+		case 1929:
+			return (Event_Impl *) new Shoutcast_Radio_Plugin_Event(pOCClientSocket, dwDevice);
+	};
+	LoggerWrapper::GetInstance()->Write(LV_STATUS, "Got CreateEvent for unknown type %d.", dwPK_DeviceTemplate);
+	return NULL;
+}
+Command_Impl  *Shoutcast_Radio_Plugin_Command::CreateCommand(int PK_DeviceTemplate, Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent)
+{
+	LoggerWrapper::GetInstance()->Write(LV_STATUS, "Got CreateCommand for unknown type %d.", PK_DeviceTemplate);
+	return NULL;
+}
