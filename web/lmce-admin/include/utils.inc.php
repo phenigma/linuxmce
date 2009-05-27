ce0db66a439ed2cdc71e45c99000ae2b4a66720d
jdecker <jondecker76@windstream.net>
2009-05-27 16:40:39 +0000

    refs #97 - Another Pluto>LMCE rename dealing with the use of LMCE's directory structure

--- web/lmce-admin/include/utils.inc.php
+++ web/lmce-admin/include/utils.inc.php
@@ -3124,7 +3124,7 @@ function formatDeviceData($deviceID,$DeviceDataArray,$dbADO,$isIPBased=0,$specif
 					$resTable=$dbADO->Execute($queryTable);
 					$deviceDataBox.='<select name="deviceData_'.$deviceID.'_'.$rowDDforDevice['FK_DeviceData'].'" '.$itemDisabled.' class="'.$cssStyle.'">
 												<option value="0">'.(in_array($rowDDforDevice['FK_DeviceCategory'],array($GLOBALS['StorageDevices'],$GLOBALS['RaidCategory'],$GLOBALS['NetworkStorage']))?'Public':'').'</option>';
-					$deviceDataBox.=(in_array($rowDDforDevice['FK_DeviceCategory'],array($GLOBALS['StorageDevices'],$GLOBALS['RaidCategory'],$GLOBALS['NetworkStorage'])))?'<option value="-1" '.(($ddValue==-1)?'selected':'').'>Pluto\'s structure</option>':'';							
+					$deviceDataBox.=(in_array($rowDDforDevice['FK_DeviceCategory'],array($GLOBALS['StorageDevices'],$GLOBALS['RaidCategory'],$GLOBALS['NetworkStorage'])))?'<option value="-1" '.(($ddValue==-1)?'selected':'').'>Use LMCE\'s directory structure</option>':'';							
 					
 					while($rowTable=$resTable->FetchRow()){
 						$itemStyle=($tableName=='FloorplanObjectType' && is_null(@$rowTable['FK_DesignObj_Control']))?' style="background-color:red;"':'';
