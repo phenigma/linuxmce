2a5e7a621476303719547bf8abbe2ebeec318adf
hari <hari@vt100.at>
2009-06-12 14:31:07 +0000

    refs #43 - more case and path fixes

--- src/PlutoMO/group/buildsis-linux-v3.sh
+++ src/PlutoMO/group/buildsis-linux-v3.sh
@@ -3,4 +3,6 @@ export PATH=~/gnupoc:${PATH}
 export EPOCROOT=~/symbian-sdks/s60_30/
 bldmake bldfiles
 abld build gcce urel
-
+makesis PlutoMO_3rd.pkg
+# makekeys -cert -expdays 3650 mykey.key mycert.cer
+signsis PlutoMO_3rd.sis PlutoMO_3rd.sisx mycert.cer mykey.key
