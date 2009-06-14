3339a7a43b06fcde84bc08b592d57df132154432
posde <posde@theinternet.de>
2009-06-14 16:47:07 +0000

    #54 - First successful make run

--- ubuntu/vdr-from-dianemo/make-me-vdr.sh
+++ ubuntu/vdr-from-dianemo/make-me-vdr.sh
@@ -9,11 +9,12 @@
 #		- Adding vdr-plugin-vompserver
 
 function BuildPackages() {
-	PackagesToBuild=$1
+	PackagesToBuild="$*"
 	for Package in $PackagesToBuild
 	do
 		pushd  "$Package"
-		PATCHVARIANT=$PATCHVARIANT dpkg-buildpackage
+		echo "Building $Package"
+		PATCHVARIANT=$PATCHVARIANT dpkg-buildpackage>../logfile 2>../error
 		cd ..
 		popd
 	done
@@ -22,14 +23,16 @@ function BuildPackages() {
 
 
 PATCHVARIANT="lmce"
-PACKAGES_FIRST_RUN="vdr-1.6.0 vdr-plugin-svdrpservice-0.0.4"
+PACKAGES_FIRST_RUN="vdr-plugin-svdrpservice-0.0.4"
+
 
 BuildPackages $PACKAGES_FIRST_RUN
+echo "Done building first batch of packages"
 #
 # We need to have the svdrpservice-dev package to build the remote timer.
 #
-dpkg -i svdrpservice-dev
-PACKAGES_SECOND_RUN="vdr-plugin-remotetimers-0.1.1~dia1 vdr-plugin-streamdev-0.3.3~cvs20080406.1321 vdr-plugin-xineliboutput-1.0.4 vdr-plugin-vompserver-0.3.0"
+dpkg -i svdrpservice-dev_0.0.4-1~dia1_all.deb
+PACKAGES_SECOND_RUN="vdr-1.6.0 vdr-plugin-remotetimers-0.1.1~dia1 vdr-plugin-streamdev-0.3.3~cvs20080406.1321 vdr-plugin-xineliboutput-1.0.4 vdr-plugin-vompserver-0.3.0"
 BuildPackages $PACKAGES_SECOND_RUN
 #
 # But we do not need to clutter the builder with this package.
