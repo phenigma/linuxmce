e62e49a56ae48a0f7f820292798ed9989ff07d9e
tschak <thom.cherryhomes@gmail.com>
2009-06-19 06:40:47 +0000

    Refs #243 - DCEGen stubs for the Activate Orbiter Plasmoid

--- src/Gen_Devices/AllCommandsRequests.h
+++ src/Gen_Devices/AllCommandsRequests.h
@@ -26566,31 +26566,27 @@ namespace DCE
 	};
 	class CMD_XPromptReload : public PreformedCommand {
 	public:
-		CMD_XPromptReload(long DeviceIDFrom, long DeviceIDTo,string sText) { m_pMessage = new Message(DeviceIDFrom, DeviceIDTo, MESSAGETYPE_COMMAND, PRIORITY_NORMAL, 
+		CMD_XPromptReload(long DeviceIDFrom, long DeviceIDTo) { m_pMessage = new Message(DeviceIDFrom, DeviceIDTo, MESSAGETYPE_COMMAND, PRIORITY_NORMAL, 
 			COMMAND_XPromptReload_CONST,
-			1 /* number of parameters */,
-			COMMANDPARAMETER_Text_CONST, sText.c_str()); }
+			0 /* number of parameters */); }
 	};
 	class CMD_XPromptReload_DL : public PreformedCommand {
 	public:
-		CMD_XPromptReload_DL(long DeviceIDFrom, string DeviceIDTo,string sText) { m_pMessage = new Message(DeviceIDFrom, DeviceIDTo, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
+		CMD_XPromptReload_DL(long DeviceIDFrom, string DeviceIDTo) { m_pMessage = new Message(DeviceIDFrom, DeviceIDTo, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
 			COMMAND_XPromptReload_CONST,
-			1 /* number of parameters */,
-			COMMANDPARAMETER_Text_CONST, sText.c_str()); }
+			0 /* number of parameters */); }
 	};
 	class CMD_XPromptReload_DT : public PreformedCommand {
 	public:
-		CMD_XPromptReload_DT(long DeviceIDFrom, long MasterDevice, eBroadcastLevel eB,string sText) { m_pMessage = new Message(DeviceIDFrom, MasterDevice, eB, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
+		CMD_XPromptReload_DT(long DeviceIDFrom, long MasterDevice, eBroadcastLevel eB) { m_pMessage = new Message(DeviceIDFrom, MasterDevice, eB, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
 			COMMAND_XPromptReload_CONST,
-			1 /* number of parameters */,
-			COMMANDPARAMETER_Text_CONST, sText.c_str()); }
+			0 /* number of parameters */); }
 	};
 	class CMD_XPromptReload_Cat : public PreformedCommand {
 	public:
-		CMD_XPromptReload_Cat(long DeviceIDFrom, long DeviceCategory, bool bIncludeChildren, eBroadcastLevel eB,string sText) { m_pMessage = new Message(DeviceIDFrom, DeviceCategory, bIncludeChildren, eB, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
+		CMD_XPromptReload_Cat(long DeviceIDFrom, long DeviceCategory, bool bIncludeChildren, eBroadcastLevel eB) { m_pMessage = new Message(DeviceIDFrom, DeviceCategory, bIncludeChildren, eB, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
 			COMMAND_XPromptReload_CONST,
-			1 /* number of parameters */,
-			COMMANDPARAMETER_Text_CONST, sText.c_str()); }
+			0 /* number of parameters */); }
 	};
 }
 #endif
