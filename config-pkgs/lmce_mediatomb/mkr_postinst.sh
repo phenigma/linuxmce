7a2b781c04d404e2391c5128771aec2ea3f2f617
posde <posde@theinternet.de>
2009-06-30 16:13:31 +0000

    Dont try to create the database if the db exists

--- config-pkgs/lmce_mediatomb/mkr_postinst.sh
+++ config-pkgs/lmce_mediatomb/mkr_postinst.sh
@@ -2,6 +2,9 @@
 # Configure Mediatomb to properly integrate with LMCE.
 echo "INTERFACE=\"eth1\"">>/etc/default/mediatomb
 usermod -G public -a mediatomb
-echo "CREATE DATABASE pluto_mediatomb;"|mysql
+DB_EXISTS=0
+mysql -e "show databases"|grep -q pluto_mediatomb && DB_EXISTS=1
+if [ $DB_EXISTS == 0 ]; then
+        mysql -e "CREATE DATABASE pluto_mediatomb;"
+fi
 /etc/init.d/mediatomb restart
-
