45a5769358871b1380d15be43754a3386e0346ad
posde <posde@theinternet.de>
2009-06-30 16:25:31 +0000

    Fix default/mediatomb as well, and use proper mysql

--- config-pkgs/lmce_mediatomb/mkr_postinst.sh
+++ config-pkgs/lmce_mediatomb/mkr_postinst.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # Configure Mediatomb to properly integrate with LMCE.
+sed -i "/INTERFACE/d" /etc/default/mediatomb
+# grep -v "INTERFACE" /etc/default/mediatomb>/etc/default/mediatomb
 echo "INTERFACE=\"eth1\"">>/etc/default/mediatomb
 usermod -G public -a mediatomb
-DB_EXISTS=0
-mysql -e "show databases"|grep -q pluto_mediatomb && DB_EXISTS=1
-if [ $DB_EXISTS == 0 ]; then
-        mysql -e "CREATE DATABASE pluto_mediatomb;"
-fi
+mysql -e "CREATE DATABASE IF NOT EXISTS pluto_mediatomb"
 /etc/init.d/mediatomb restart
