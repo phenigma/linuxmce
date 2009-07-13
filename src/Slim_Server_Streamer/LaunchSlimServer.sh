6bfb0209f551dae95f4134738104f7a4f8b04144
posde <posde@theinternet.de>
2009-07-13 13:10:23 +0000

    Fixes #200 - sqlCVS update is required to have LaunchSlimServer.sh as the command line for device template 53

--- src/Slim_Server_Streamer/LaunchSlimServer.sh
+++ src/Slim_Server_Streamer/LaunchSlimServer.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-PORT="${1:-9090}"
+Params=("$@")
+for ((i = 0; i < ${#Params[@]}; i++)); do
+	case "${Params[$i]}" in
+	-d) ((i++)); DeviceID="${Params[$i]}" ;;
+        esac
+done
+                                
+
+PORT="9090"
 PARAMS="--cliport $PORT --audiodir /home/public/data/audio"
 
 sed -ri "s,SLIMOPTIONS=.*,SLIMOPTIONS=\"$PARAMS\",g" /etc/default/squeezecenter
 
 invoke-rc.d squeezecenter restart
+
+./Slim_Server_Streamer -d $DeviceID
