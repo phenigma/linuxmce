b3d7497bd6608d2911ded37a2462554246cee8fb
tschak <thom.cherryhomes@gmail.com>
2009-08-09 19:00:19 +0000

    Further Fixes for Builder...

--- src/MakeRelease_Windows.bat
+++ src/MakeRelease_Windows.bat
@@ -8,16 +8,16 @@ CALL SET LOG_PATH=c:\@build_log
 del %LOG_PATH%\Build.log
 
 ren %INPUT_PATH%\src\pluto.sln pluto.sln.temp
-%MAKERELEASE_PATH%\MakeRelease.exe -h 192.168.80.1 -o 7 -s %INPUT_PATH% -n / -c -k 192,193,213,212,201,237,255,254,277,278,126,145,226,227,119,138,124,143,241,242,154,155,159,160,202,203,303,304,272,281,280,279,195,194,191,190,162,161,128,147,341,342,471,470,346,347
+%MAKERELEASE_PATH%\MakeRelease.exe -h 192.168.80.1 -o 7 -s %INPUT_PATH% -n / -c -k 192,193,213,212,201,237,255,254,277,278,126,145,119,138,124,143,241,242,154,155,159,160,202,203,303,304,272,281,280,279,195,194,191,190,162,161,128,147,341,342,471,470,346,347
 ren %INPUT_PATH%\src\pluto.sln.temp pluto.sln
 
 echo Updating Orbiter release binary for OrbiterInstaller
-copy C:\pluto\trunk\src\bin\Orbiter.exe C:\pluto.makerelease\trunk\installers\Installers\Orbiter\Files\
+copy C:\pluto\trunk\src\bin\Orbiter.exe C:\pluto\trunk\installers\Installers\Orbiter\Files\
 
-cd C:\pluto\trunk\installers\Installers\Orbiter
+cd C:\pluto\trunk\installers\Installers\NewOrbiterInstaller\NewOrbiterInstaller
 CALL WinMake.bat
 
-cd C:\pluto\trunk\installers\Installers\Orbiter\Release\
+cd C:\pluto\trunk\installers\Installers\NewOrbiterInstaller\NewOrbiterInstaller\Release\
 copy *.msi C:\pluto\trunk\src\bin
 
 
