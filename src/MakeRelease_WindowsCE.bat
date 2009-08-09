7e57e95cf11cb52ecb3279901c9a5859f329a532
tschak <thom.cherryhomes@gmail.com>
2009-08-09 08:13:28 +0000

    Fix for our current builder.

--- src/MakeRelease_WindowsCE.bat
+++ src/MakeRelease_WindowsCE.bat
@@ -1,13 +1,13 @@
 SET PATH="c:\Program Files\Microsoft eMbedded C++ 4.0\Common\EVC\Bin"
 
 CALL SET CONFIGURATION=release
-CALL SET INPUT_PATH=c:\pluto.makerelease\trunk
-CALL SET MAKERELEASE_PATH=C:\pluto\trunk\src
+CALL SET INPUT_PATH=c:\pluto\trunk
+CALL SET MAKERELEASE_PATH=C:\makerelease
 CALL SET LOG_PATH=c:\@build_log
 
 del %LOG_PATH%\BuildCE.log
 
-%MAKERELEASE_PATH%\Debug\MakeRelease.exe -h 10.0.2.4 -o 12 -s %INPUT_PATH% -n / -c -k 119,138
+%MAKERELEASE_PATH%\MakeRelease.exe -h 192.168.80.1 -o 12 -s %INPUT_PATH% -n / -c -k 119,138
 
 cd "Orbiter\CENet_Smartphone"
 call build_cab.bat
