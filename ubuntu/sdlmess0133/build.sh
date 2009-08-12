b1810a8c5571ccfd6a5110d8e09207c2653909b4
posde <posde@theinternet.de>
2009-08-12 08:25:13 +0000

    Fixes #302 - use builder.conf to check for target arch

--- ubuntu/sdlmess0133/build.sh
+++ ubuntu/sdlmess0133/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-ARCH=`uname -m`
+. /etc/lmce-build/builder.conf
 
-if [ $ARCH = "x86_64"  ]
+if [ $arch = "amd64"  ]
 then
 	echo Adding 64-bit pointer handling.
+	MKOPTS="PTR64=1"
 fi
 
 make $MKOPTS -f makefile.sdl
