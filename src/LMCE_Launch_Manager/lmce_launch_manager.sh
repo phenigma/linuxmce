8bb57643792eddaad05e7af645e654ea8d321780
posde <posde@theinternet.de>
2009-09-21 19:22:43 +0000

    Refs #348 - If AutostartMedia=1 don't start X.

--- src/LMCE_Launch_Manager/lmce_launch_manager.sh
+++ src/LMCE_Launch_Manager/lmce_launch_manager.sh
@@ -3,26 +3,32 @@
 . /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/LockUtils.sh
 
-if [[ "$Display" == "" ]] ;then
-	if [[ "$DISPLAY" == "" ]] ;then
-		export DISPLAY=:0
-	fi
-else
-	export DISPLAY=":${Display}"
-fi
+/usr/pluto/bin/corefile.sh
 
+echo Display is $Display >> /tmp/possy
+if ["$AutostartMedia" -eq "1"] ; then
+	# Only start X if we want to autostart the media.
+	if [[ "$Display" == "" ]] ;then
+		if [[ "$DISPLAY" == "" ]] ;then
+			export DISPLAY=:0
+		fi
+	else
+		export DISPLAY=":${Display}"
+	fi
+fi
 if ! Lock "Launch_Manager" "LM"; then
 	# LM is running
-	echo "LM already running"
+	echo "LM already running">>/tmp/possy
 	wmctrl -a "Linux MCE Launch Manager"
 	exit
 fi
 
 trap 'Unlock "Launch_Manager" "LM"' EXIT
-
+ps ax|grep X>>/tmp/possy
 KDE_DEBUG=1
 LM_BINARY=/usr/pluto/bin/LMCE_Launch_Manager
 LM_EXIT_CODE=-1
+echo Display is now $DISPLAY >> /tmp/possy
 
 while [[ "$LM_EXIT_CODE" != "0" ]] ;do
 	/usr/bin/screen -d -m -S LMCE_Launch_Manager $LM_BINARY --nofork --nocrashhandler
