b4b8f42fcb7c91ff5be7fdf262d0382f5917c78f
posde <posde@theinternet.de>
2009-09-21 20:01:57 +0000

    Refs #346 - Make sure only the needed scripts get executed, and remove all calls to the clean core scripts

--- src/LMCE_Launch_Manager/mkr_postinst.sh
+++ src/LMCE_Launch_Manager/mkr_postinst.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #create launcher icons
+
 for dir in /home/* ;do
 	if [ -d "$dir/Desktop" ] ;then
-		ln -s /usr/share/applications/lmce_launch_manager.desktop "$dir/Desktop/lmce_launch_manager.desktop" || :
-		chmod 666 "$dir/Desktop/lmce_launch_manager.desktop" || :
+		if [ -f "$dir/Desktop/lmce_launch_manager.desktop" ]; then
+			# The current lmce_launch_manager is a CLI app.
+			rm "$dir/Desktop/lmce_launch_manager.desktop"
+		fi 
+#		ln -s /usr/share/applications/lmce_launch_manager.desktop "$dir/Desktop/lmce_launch_manager.desktop" || :
+#		chmod 666 "$dir/Desktop/lmce_launch_manager.desktop" || :
 	fi
 done
 
-#Temporarily disable startCore.sh cleancore script for 3-29-2009 build LM testing.
-#Backup the original, then replace it with a "neutered" version
-if [ -f /usr/pluto/bin/startCore.sh ] ; then
-	mv /usr/pluto/bin/startCore.sh /usr/pluto/bin/startCore.sh._backup
-	cat >/usr/pluto/bin/startCore.sh <<"EOF"
-#!/bin/sh
+# Make sure the old startup scripts no longer exists
+rm /etc/rc5.d/{S99core,S99launch-manager}
 
-exit 0
-EOF
-	chmod +x /usr/pluto/bin/startCore.sh
-fi
-
+# Make sure the new startup scripts are being used
+ln -sfv /usr/pluto/bin/Start_X.sh /etc/rc5.d/S30Start_X.sh
+ln -sfv /usr/pluto/bin/startup-script.sh /etc/rc5.d/S93startup-script.sh
+ln -sfv /usr/pluto/bin/lmce_launch_manager.sh /etc/rc5.d/S99lmce_launch_manager.sh
