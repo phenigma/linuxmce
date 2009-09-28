f9ae39566292d3d09da2c5877f9ded626f09af67
posde <posde@theinternet.de>
2009-09-28 08:40:05 +0000

    Refs #209 - Add Avenard repo to DVD installer

--- src/new-installer/pre-install-from-DVD.sh
+++ src/new-installer/pre-install-from-DVD.sh
@@ -18,11 +18,11 @@ AddRepoToSources "file:${LOCAL_REPO_BASE} ${LOCAL_REPO_DIR}"
 AddRepoToSources "http://deb.linuxmce.org/ubuntu/ ${DISTRO}  ${COMPOS}"
 AddRepoToSources "http://debian.slimdevices.com/ stable  main"
 
-if AddRepoToSources "http://packages.medibuntu.org/ ${DISTRO}  free non-free"
-then
-	AddGpgKeyToKeyring http://packages.medibuntu.org/medibuntu-key.gpg
-fi
+AddRepoToSources "http://packages.medibuntu.org/ ${DISTRO}  free non-free"
+AddGpgKeyToKeyring http://packages.medibuntu.org/medibuntu-key.gpg
 
+AddRepoToSources "http://www.avenard.org/files/ubuntu-repos intrepid release"
+AddGpgKeyToKeyRing  http://www.avenard.org/files/ubuntu-repos/ubuntu-repos.key
 apt-get update
 # We add all the deb's here, that can be pre-installed without causing problems
 apt-get -y -f --force-yes install video-wizard-videos
