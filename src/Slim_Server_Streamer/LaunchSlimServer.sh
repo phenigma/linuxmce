3523003fb4f289d0c790c2f7812c2a5ca5814f7f
posde <posde@theinternet.de>
2009-10-01 19:00:07 +0000

    Modified launch script to use the new executable squeezeboxserver

--- src/Slim_Server_Streamer/LaunchSlimServer.sh
+++ src/Slim_Server_Streamer/LaunchSlimServer.sh
@@ -11,8 +11,8 @@ done
 PORT="9090"
 PARAMS="--cliport $PORT --audiodir /home/public/data/audio"
 
-sed -ri "s,SLIMOPTIONS=.*,SLIMOPTIONS=\"$PARAMS\",g" /etc/default/squeezecenter
+sed -ri "s,SLIMOPTIONS=.*,SLIMOPTIONS=\"$PARAMS\",g" /etc/default/squeezeboxserver
 
-invoke-rc.d squeezecenter restart
+invoke-rc.d squeezeboxserver restart
 
 ./Slim_Server_Streamer -d $DeviceID
