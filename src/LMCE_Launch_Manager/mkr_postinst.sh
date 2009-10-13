0859cc4ee69964e358abb00d53d0a52c11ec2fb4
posde <posde@theinternet.de>
2009-10-13 21:26:32 +0000

    Refs #26 - Remove all of the startup scripts, and replace it with a single new one

--- src/LMCE_Launch_Manager/mkr_postinst.sh
+++ src/LMCE_Launch_Manager/mkr_postinst.sh
@@ -17,6 +17,11 @@ done
 rm -f /etc/rc5.d/{S99core,S99launch-manager}
 
 # Make sure the new startup scripts are being used
-ln -sfv /usr/pluto/bin/Start_X.sh /etc/rc5.d/S30Start_X.sh
-ln -sfv /usr/pluto/bin/startup-script.sh /etc/rc5.d/S93startup-script.sh
-ln -sfv /usr/pluto/bin/lmce_launch_manager.sh /etc/rc5.d/S99lmce_launch_manager.sh
+#ln -sfv /usr/pluto/bin/Start_X.sh /etc/rc5.d/S30Start_X.sh
+#ln -sfv /usr/pluto/bin/startup-script.sh /etc/rc5.d/S93startup-script.sh
+#ln -sfv /usr/pluto/bin/lmce_launch_manager.sh /etc/rc5.d/S99lmce_launch_manager.sh
+rm -f /usr/pluto/bin/Start_X.sh /etc/rc5.d/S30Start_X.sh
+rm -f /usr/pluto/bin/startup-script.sh /etc/rc5.d/S93startup-script.sh
+rm -f /usr/pluto/bin/lmce_launch_manager.sh /etc/rc5.d/S99lmce_launch_manager.sh
+
+ln -sfv /etc/init.f/linuxmce /etc/rc5.d/S93linuxmce
