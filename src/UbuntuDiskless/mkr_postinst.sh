e5626da85ab8e5b2f2fd6363bc93a72c2bfc8dde
posde <posde@theinternet.de>
2009-10-15 13:43:57 +0000

    Refs #22 - Only execute Debug_LogKernelModules.sh if the script already exists

--- src/UbuntuDiskless/mkr_postinst.sh
+++ src/UbuntuDiskless/mkr_postinst.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-/usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
 
 # summary of how this script can be called:
 #        * <postinst> `configure' <most-recently-configured-version>
 #if [[ "$2" != "" ]] ;then
 #	/usr/pluto/bin/Diskless_CreateTBZ.sh || echo "WARNING: Diskless_CreateTBZ.sh returned an error!"
 #fi
+
+if [ -x /usr/pluto/bin/Debug_LogKernelModules.sh ]; then
+	/usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
+fi
 
 #update-rc.d -f haltMDs start 01 0 6 . || /bin/true
 update-rc.d -f haltMDs stop 01 0 6 . || /bin/true
