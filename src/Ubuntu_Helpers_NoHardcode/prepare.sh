7996c41d87e03352a688948f58a3ee7fb9a4369b
posde <posde@theinternet.de>
2009-10-26 21:19:49 +0000

    Modify order of scripts from prepare, to make sure the sources.list is up-to-date

--- src/Ubuntu_Helpers_NoHardcode/prepare.sh
+++ src/Ubuntu_Helpers_NoHardcode/prepare.sh
@@ -4,9 +4,9 @@ set -e
 
 prepare_scripts_dir="/usr/local/lmce-build/prepare-scripts"
 
-"${prepare_scripts_dir}/install-build-pkgs.sh"
 "${prepare_scripts_dir}/cd1-prepare.sh"
 "${prepare_scripts_dir}/cd2-prepare.sh"
+"${prepare_scripts_dir}/install-build-pkgs.sh"
 "${prepare_scripts_dir}/create-diskless-debootstrap.sh"
 "${prepare_scripts_dir}/import-external-files.sh"
 "${prepare_scripts_dir}/preseed-build-pkgs.sh"
