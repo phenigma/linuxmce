06b5ba960c4838275203b650d98506deffe165d0
posde <posde@theinternet.de>
2009-10-27 22:20:35 +0000

    Refs #366 - hack to get FK_Text field into the system - TODO use sqlcvs for this

--- src/BootScripts/update2utf8.pl
+++ src/BootScripts/update2utf8.pl
@@ -12,19 +12,27 @@ $datas = "";
 
 foreach $dbname (@databases) {
 	$sql = "SELECT TABLE_NAME FROM information_schema.TABLES Where TABLE_SCHEMA = '$dbname' AND TABLE_NAME != 'Schema' AND TABLE_COLLATION != 'utf8_general_ci';";
-	print "$sql\n";
-	$st = $db->prepare($sql) or die "Error in code";
-	$st->execute() or die "Error on execute";
+#	print "$sql\n";
+	$st = $db->prepare($sql) or die "Error in prepare $sql\n";
+	$st->execute() or die "Error on execute $sql\n";
 	while($local_row = $st->fetchrow_hashref()) {
 		$table_name = $local_row->{'TABLE_NAME'};
 		$sql = "ALTER TABLE $dbname.$table_name CHARACTER SET utf8;";
-		print "$sql\n";
-		$st2 = $db->prepare($sql) or die "Error in code";
-		$st2->execute() or die "Error on execute";
+#		print "$sql\n";
+		$st2 = $db->prepare($sql) or die "Error in prepare $sql";
+		$st2->execute() or die "Error on execute $sql";
 		$st2->finish();
 	}
 	$st->finish();
 }
+
+# Add new field to CommandGroup
+$sql = "ALTER TABLE pluto_main.CommandGroup ADD FK_Text INT(11) DEFAULT NULL AFTER TemplateParm2";
+$st2 = $db->prepare($sql)
+# or die "Error in prepare $sql\n";
+$st2->execute()
+# or die "Error on execute $sql\n";
+                                 
 $db->disconnect();
 
 print "[DONE]\n";
