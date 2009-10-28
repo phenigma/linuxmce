5952b1e97eb4a0c8c4290b9025e1aff55e92c343
posde <posde@theinternet.de>
2009-10-28 06:51:13 +0000

    Refs #209 - Don't utilize the lmce repo for install

--- src/new-installer/pre-install-from-DVD.sh
+++ src/new-installer/pre-install-from-DVD.sh
@@ -15,7 +15,7 @@ InstallNeededPackages
 CreatePackagesFiles
 
 AddRepoToSources "file:${LOCAL_REPO_BASE} ${LOCAL_REPO_DIR}"
-AddRepoToSources "http://deb.linuxmce.org/ubuntu/ ${DISTRO}  ${COMPOS}"
+# AddRepoToSources "http://deb.linuxmce.org/ubuntu/ ${DISTRO}  ${COMPOS}"
 AddRepoToSources "http://debian.slimdevices.com/ stable  main"
 AddRepoToSources "http://security.ubuntu.com/ubuntu intrepid-security main restricted universe multiverse"
 AddRepoToSources "http://archive.ubuntu.com/ubuntu intrepid main restricted universe multiverse"
