2543c01d6f6ee8299482fb68f3cfa20e98d2bb73
posde <posde@theinternet.de>
2009-11-07 21:31:11 +0000

    Fixes #414 - Add audiodir, playlistdir and wizardDone to server.prefs for SqueezeBoxServer if they don't exist correctly

--- src/Slim_Server_Streamer/LaunchSlimServer.sh
+++ src/Slim_Server_Streamer/LaunchSlimServer.sh
@@ -10,9 +10,19 @@ done
 
 PORT="9090"
 PARAMS="--cliport $PORT --audiodir /home/public/data/audio"
-
 sed -ri "s,SLIMOPTIONS=.*,SLIMOPTIONS=\"$PARAMS\",g" /etc/default/squeezeboxserver
 
-invoke-rc.d squeezeboxserver restart
+found=0
+
+# Three settings need to exist in the server.prefs file.
+grep "audiodir: /home/public/data/audio" /var/lib/squeezeboxserver/prefs/server.prefs && let found+=1
+grep "playlistdir: /home/public/data/audio" /var/lib/squeezeboxserver/prefs/server.prefs && let found+=1
+grep "wizardDone: 1" /var/lib/squeezeboxserver/prefs/server.prefs && let found+=1
+if [ "$found" -ne "3"] ; then
+        echo "audiodir: /home/public/data/audio" >> /var/lib/squeezeboxserver/prefs/server.prefs
+        echo "playlistdir: /home/public/data/audio" >> /var/lib/squeezeboxserver/pres/server.prefs
+        echo "wizardDone: 1" >> /var/lib/squeezeboxserver/prefs/server.prefs
+	invoke-rc.d squeezeboxserver restart
+fi                                
 
 ./Slim_Server_Streamer -d $DeviceID
