2e2b0f4cba86825d1df47a4d44ca861d53bc752d
posde <posde@theinternet.de>
2009-11-20 06:53:02 +0000

    Fixes #455 - Fix typo, the package doing the dpkg-divert is called pluto-boot-scripts not pluto-bootscripts

--- src/BootScripts/mkr_preinst.sh
+++ src/BootScripts/mkr_preinst.sh
@@ -2,7 +2,7 @@
 ## Create divertion for NetworkManager as it destroys our settings
 
 if [ install = "$1" -o upgrade = "$1" ]; then
-        dpkg-divert --add --rename --package pluto-bootscripts --divert /usr/sbin/NetworkManager.wraped /usr/sbin/NetworkManager
+        dpkg-divert --add --rename --package pluto-boot-scripts --divert /usr/sbin/NetworkManager.wraped /usr/sbin/NetworkManager
 fi
                 
                 
