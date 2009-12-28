fe003a998317af11423cdcc4a0c5263b7b750085
posde <posde@theinternet.de>
2009-12-28 12:12:12 +0000

    Refs #188 - some perl fixes

--- src/BootScripts/lmce.pl
+++ src/BootScripts/lmce.pl
@@ -7,6 +7,7 @@
 use strict;
 use DBI;
 require "/usr/pluto/bin/config_ops.pl";
+my $DB_PL_HANDLE;
 
 $DB_PL_HANDLE = DBI->connect(&read_pluto_cred()) or die "Could not connect to MySQL";
 
@@ -19,11 +20,16 @@ sub getCoreIP {
   return $IP;
 }
 
-function get_device_devicedata(( {
+sub get_device_devicedata() 
+{
   # This function should be called with the DeviceID and DeviceData ID
-  my ($DEVICE_ID, $DEVICE_DATA) = @_;
-  # Return specific device data for a specific device
+  my $DEVICE_ID;
+  my $DEVICE_DATA;
   my $IK_DeviceData;
+  $DEVICE_ID = $_[0];
+  $DEVICE_DATA = $_[1];
+  # Return specific device data for a specific device
+  $IK_DeviceData = "";
   my $sth = $DB_PL_HANDLE->prepare("SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_Device = $DEVICE_ID AND FK_DeviceData = $DEVICE_DATA;");
   $sth->execute || die "Sql Error receiving Device Data $DEVICE_DATA for Device $DEVICE_ID";
   if (my $row = $sth->fetchrow_hashref())
