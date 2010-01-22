842e348fd0f8592fde5a94c8c26fa1bf89c65c43
posde <posde@theinternet.de>
2010-01-22 23:22:21 +0000

    Refs #574 - modified formatting for preferences file

--- src/new-installer/mce-install-common.sh
+++ src/new-installer/mce-install-common.sh
@@ -45,10 +45,10 @@ $newlines
 function PinMythTVAt22 {
 	# Make sure, we never use a newer version that 0.22
 	echo "
-	Package: mythtv mythtv-frontend mythtv-backend mythtv-common mythtv-database mythtv-transcode-utils mythweb libmyth-perl
-	Pin: version 2:0.22*
-	Pin-Priority: 1001
-	">>/etc/apt/preferences
+Package: mythtv mythtv-frontend mythtv-backend mythtv-common mythtv-database mythtv-transcode-utils mythweb libmyth-perl
+Pin: version 2:0.22*
+Pin-Priority: 1001
+">>/etc/apt/preferences
 }
 	
 
