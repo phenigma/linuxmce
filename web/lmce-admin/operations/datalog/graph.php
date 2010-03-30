36674f45f90f23edbc6bc73698ed91e4fa7aa2bb
sambuca <magnebra@online.no>
2010-03-30 19:15:39 +0000

    Fixes #662 commiting patches from apagg

--- web/lmce-admin/operations/datalog/graph.php
+++ web/lmce-admin/operations/datalog/graph.php
@@ -9,91 +9,98 @@ $mysqldb="lmce_datalog";
 $unit = mysql_real_escape_string($_GET['unit']);
 $device = mysql_real_escape_string($_GET['device']);
 $days = mysql_real_escape_string($_GET['days']);
+$color = mysql_real_escape_string($_GET['color']);
+
+if (empty($days)) {
+     $days=1;
+}
+
+if (empty($color)) {
+     $color='red@0.4';
+}
 
 $connection=mysql_connect($mysqlhost, $mysqluser, $mysqlpwd) or die ("ERROR: could not connect to the database!");
 mysql_select_db($mysqldb, $connection) or die("ERROR: could not select database!");
 
 $query = mysql_query("select Description,timestamp,Datapoint,Unit from Datapoints,Unit where FK_Unit=PK_Unit and EK_Device= ".$device." and timestamp > DATE_SUB(NOW(), INTERVAL ".$days." DAY) order by timestamp");
 
-// create the graph
-$Graph =& Image_Graph::factory('graph', array(800, 200));  
+// Create the graph and set the size
+$Graph =& Image_Graph::factory('graph', array(800, 200)); 
 
 // add a TrueType font
 $Font =& $Graph->addNew('font', '/usr/share/fonts/truetype/msttcorefonts/verdana.ttf'); 
 
-// set the font size to 11 pixels
-$Font->setSize(8); 
-
+// set the font size 
+$Font->setSize(7); 
 $Graph->setFont($Font); 
 
-$AxisYName = "";
-$graphName = "";
-/*$query2 = mysql_query("select Description,Unit from Unit where PK_Unit = ".$unit);
-while ($unit = mysql_fetch_array($query2)){
-      $AxisYName = $unit[1];
-      $graphName = $graphName." - ".$unit[0];
-}
-*/
+// Create a dataset for the graph
+$Dataset =& Image_Graph::factory('dataset'); 
 
-// $Legend->setPlotarea($Plotarea);         
+$t=0; //Counter
+$num = mysql_numrows($query); //find number off datapoints for the graph
 
-// create the dataset
-// $Dataset =& Image_Graph::factory('random', array(10, 2, 15, true)); 
-$Dataset =& Image_Graph::factory('dataset');
-
+// Create the datapoints for the Dataset and the lables for x-axis
 while ($datapoint = mysql_fetch_array($query)){
-  if ($AxisYName == "") {
-    $AxisYName = $datapoint[3];
-  }
-  if ($graphName == "") {
-    $graphName = "Device ".$device.' - '.$datapoint[0];
-  }
-  $Dataset->addPoint($datapoint[1], $datapoint[2]);
+     if ($AxisYName == "") {
+          $AxisYName = $datapoint[3];
+     }
+     if ($graphName == "") {
+          $graphName = "Device ".$device.' - '.$datapoint[0];
+     }
+     $Dataset->addPoint(strtotime($datapoint[1]), $datapoint[2]);
+     ++$t;
+     if ($t==1){
+          $first=strtotime($datapoint[1]);
+          $firstDate=$datapoint[1];
+     }
+     if ($t==$num){
+          $last=strtotime($datapoint[1]);
+     }
 }
 
+// Create arrays for x-axis lables
+$firstFixed=substr_replace($firstDate, '00:00', -5);
+
+for ($fullHour= strtotime($firstFixed)+3600, $i=0 ; $fullHour<$last ; $fullHour=$fullHour+(7200*$days), ++$i){
+     $array1[$i]=$fullHour;
+     $array2[0][$fullHour]=date('jS-H:i', $fullHour);
+}
+
+// setup the plotarea
 $Graph->add(
-    Image_Graph::vertical(
-        Image_Graph::factory('title', array($graphName, 12)),  
-     //   Image_Graph::vertical(
-            $Plotarea = Image_Graph::factory('plotarea'),
-            //$Plotarea = Image_Graph::factory('plotarea', array('axis', 'axis'))
-   //         $Legend = Image_Graph::factory('legend')
-       //     90
-       // ),
-        5
-    )
-);
-// create the 1st plot as smoothed area chart using the 1st dataset
-    
-$Plot =& $Plotarea->addNew('area', array(&$Dataset)); 
-// $Plot =& $Plotarea->addNew('bar', array(&$Dataset)); 
+   Image_Graph::vertical(
+      Image_Graph::factory('title', array($graphName, 10)),        
+      $Plotarea = Image_Graph::factory('plotarea', array('axis', 'axis')),
+      5
+   )
+);   
 
-// set a line color
-$Plot->setLineColor('gray'); 
-
-// set a standard fill style
-$Plot->setFillColor('red@0.4'); 
-
-$AxisA =& $Plotarea->getAxis(IMAGE_GRAPH_AXIS_X);
-$AxisA->setFontSize(6); 
-$AxisA->setLabelInterval(6, 1);
-$AxisA->setTickOptions(-5, 5, 1);
-
-// $AxisA->setLabelInterval('auto', 1);
-$AxisA->setLabelOption('offset', 5);
-// $AxisA->setTickOptions(-1, 1, 2);
-// $AxisA->setLabelOption('showtext', false, 2); 
-
-// Axis Y unit
+// y-axis Name
 $AxisY =& $Plotarea->getAxis(IMAGE_GRAPH_AXIS_Y);
 $AxisY->setTitle($AxisYName, "vertical");
 
+// x-axis Name
+$AxisX =& $Plotarea->getAxis(IMAGE_GRAPH_AXIS_X);
+$AxisX->setTitle('Time', "horizontal");       
+
+// Lable interval on the x-axis
+$AxisX->setLabelInterval($array1); 
+
+// create an data label array for formatting label data based on an array
+$ArrayData =& Image_Graph::factory('Image_Graph_DataPreprocessor_Array',$array2);
+
+// use the data label array on the X axis
+$AxisX->setDataPreprocessor($ArrayData);     
+
+// Create the Plot
+$Plot =& $Plotarea->addNew('area', &$Dataset); 
+$Plot->setFillColor($color); 
+
+// Add grid
 $Grid =& Image_Graph::factory('line_grid');
 $Plotarea->add($Grid);
 
-// output the Graph
 $Graph->done(); 
-
-
 ?>
 
--- web/lmce-admin/operations/datalog/viewDatalog.php
+++ web/lmce-admin/operations/datalog/viewDatalog.php
@@ -1,5 +1,33 @@
 <?
+
+// Creates an array from a string created with 'multimplode'
+function multiexplode($string){
+     $expl1 = explode(',',$string);
+     $i=0;
+     while ($i < count($expl1)){
+          $expl2[$i]=explode(';',$expl1[$i]);
+          ++$i;
+     }
+     return($expl2);
+}
+
+
+//Creates a string from an array level1 separated using(,) and level2 using (;)
+function multimplode($array){
+     if (!empty($array)){
+          reset($array);
+          while (list($key, $val) = each($array)) {
+               $imp[$key] = implode(';',$val);
+          }
+          return(implode(',',$imp));
+     }
+     else {
+          return ('');
+     }
+}
+
 function viewDatalog($output,$dbADO){
+
 	// include language files
 	include(APPROOT.'/languages/'.$GLOBALS['lang'].'/common.lang.php');
 	
@@ -21,77 +49,72 @@ function viewDatalog($output,$dbADO){
 			setTimeout("doReload()",60000);
 		</script>';
 
+     $mysqlhost="localhost"; // MySQL-Host
+     $mysqluser="root"; // MySQL-User
+     $mysqlpwd=""; // Password
+     $mysqldb="lmce_datalog";
+
+     $connection=mysql_connect($mysqlhost, $mysqluser, $mysqlpwd) or die ("ERROR: could not connect to the database!");
+     mysql_select_db($mysqldb, $connection) or die("ERROR: could not select database!");
+
 	$PK_Datalogger = getDeviceFromDT($installationID,1949 /* DeviceTemplate_DataLogger_Plugin */,$dbADO);
-	$deviceString = getDeviceData($PK_Datalogger,59 /* Configuration */,$dbADO);
-	
+	$deviceString = getDeviceData($PK_Datalogger,59 /* Configuration */,$dbADO); 
+
 	$action = isset($_REQUEST['action'])?cleanString($_REQUEST['action']):'form';
 
-	if ($action == 'addDevice') {
-	   $addDevice = $_REQUEST['addDevice'];
-	   if (strpos($deviceString, $addDevice) !== false) {
-	       $out.='<p>Device already listed</p>';
-	   } else {
-	       if (strlen($deviceString) > 0) {
-	           $deviceString.=',';
-	       }
-	       $deviceString.=$addDevice;
-	       set_device_data($PK_Datalogger,59,$deviceString,$dbADO);
-	       $out.='<p>Graph added for device '.$addDevice.'</p>';
-	   }
-	} else if ($action == 'removeDevice') {
-	   $removeDevice = $_REQUEST['removeDevice'];
-	   if (strpos($deviceString, $removeDevice) === false) {
-	       $out.='<p>Device not listed</p>';
-	   } else {
-	       $tempDevices = '';
-	       if (strlen($deviceString) > 0) {
-		   $devices = explode(',', $deviceString);
-		   // iterate all current devices, and add all but the one we want to remove
-		   for ($i = 0; $i < sizeof($devices); $i++) {
-		       if ($devices[$i] != $removeDevice) {
-		           $tempDevices.=$devices[$i].',';
-		       }
-		   }
-		   $deviceString = substr($tempDevices, 0, strlen($tempDevices)-1);
-		}		   
-//	     	$out.='<p>'.$removeDevice.' - '.$deviceString.'</p>';
+// Creates an array from the Template configuration
+     if (!empty($deviceString)){
+          $conf=multiexplode($deviceString);
+     }
 
-	        set_device_data($PK_Datalogger,59,$deviceString,$dbADO);
-	   }
-	}
+// Add ore remove devices from the page
+     if ($action=='addDevice'){
+          $conf[]=array($_REQUEST['addDevice'],$_REQUEST['days'],$_REQUEST['color']);
+          set_device_data($PK_Datalogger,59,multimplode($conf),$dbADO);
+     }
+     else if ($action=='removeDevice'){
+          unset ($conf[$_REQUEST['removeDevice']]);
+          $conf = array_values($conf);
+          set_device_data($PK_Datalogger,59,multimplode($conf),$dbADO);
+     }
 
-	// show add list
-	$query = "select * from Device";
-	$resGP = $dbADO->Execute($query);
-	$out.='<form action="index.php" method="POST" name="datalog">
-	<input type="hidden" name="section" value="viewDatalog">
-	<input type="hidden" name="action" value="addDevice">';
+// Form to select layout
+     $out.='<form name="datalog" method="POST" action="index.php">
+    	<input type="hidden" value="viewDatalog" name="section">
+    	<input type="hidden" value="addDevice" name="action">
+    	<p>Add graph for device: </p>
+    	<select id="addDevice" name="addDevice">';
 
-	$out.='<p>Add graph for device: </p><select name="addDevice" id="addDevice">';
-	if ($resGP) {
-		while ($row=$resGP->FetchRow()) {
-			$out.= '<option value="'.$row['PK_Device'].'">'.$row['Description'].' ('.$row['PK_Device'].')</option>';
-		}
-	}
-	$out.='</select><input type="submit" class="button" name="add" value="Add" />';
-	$out.='</form>';
+// Create list of devices that have data in lmce_datalog
+     $query = mysql_query("select distinct EK_Device from Datapoints ORDER BY EK_Device");
+     while ($devices = mysql_fetch_array($query)){
+          $q = "SELECT `Description` FROM `Device` where `PK_Device` = ".$devices['EK_Device'];
+          $resGP = $dbADO->Execute($q);
+          $row=$resGP->FetchRow();
+         	$out.= '<option value="'.$devices['EK_Device'].'">'.$row['Description'].' ('.$devices['EK_Device'].')</option>';
+     }
 
-	// show graphs
+     $out.='</select><select id="days" name="days"><option value="1">1-day</option><option value="3">3-days</option>
+     <option value="7">7-days</option></select>
+    	<select id="color" name="color"><option value="red@0.4">red</option><option value="blue@0.4">blue</option>
+    	<option value="yellow@0.4">yellow</option><option value="green@0.4">green</option></select>
+    	<input type="submit" value="Add" name="add" class="button"></form>';
+
+// show graphs
 	$out.='<form action="index.php" method="POST" name="datalogGraph">
 	<input type="hidden" name="section" value="viewDatalog">
 	<input type="hidden" name="action" value="removeDevice">
 	<input type="hidden" name="removeDevice" value="">';
 
-	if (strlen($deviceString) > 0) {
-		$devices = explode(',', $deviceString);
-		for ($i = 0; $i < sizeof($devices); $i++) {
-		    $out = $out.'<IMG SRC="operations/datalog/graph.php?device='.$devices[$i].'&days=1">';
-		    $out = $out.'<input type="submit" class="button" name="remove" onclick="document.datalogGraph.removeDevice.value='.$devices[$i].'" value="Remove"/><br>';
+	if (!empty($conf)) {
+		for ($i = 0; $i < count($conf); $i++) {
+		    $out = $out.'<IMG SRC="operations/datalog/graph.php?device='.$conf[$i][0].'&days='.$conf[$i][1].'&color='.$conf[$i][2].'">';
+		    $out = $out.'<input type="submit" class="button" name="remove" onclick="document.datalogGraph.removeDevice.value='.$i.'" value="Remove"/><br>';
 	    	}
-	} else {
+	} 
+	else {
 	  $out = $out.'<p>No devices selected for graph display</p>';
 	}
-	$out.='</form>';
 
 	$output->setBody($out);
 	$output->setMenuTitle($TEXT_ADVANCED_CONST.' |');
