fd4d3e6ccf0ae721e5fff17416139fe9d5b15eed
mandingo <mandingo@linuxmce.org>
2010-05-19 21:08:55 +0000

    remark debhelper, official mythtv .23 can use 7.0 and greater, and remark dep package issues which are handled in the replacement.sh

--- src/Ubuntu_Helpers_NoHardcode/prepare-scripts/install-build-pkgs.sh
+++ src/Ubuntu_Helpers_NoHardcode/prepare-scripts/install-build-pkgs.sh
@@ -20,23 +20,23 @@ pushd /usr/src
 popd
 
 # Build new debhelper package
-pushd /usr/src
+#pushd /usr/src
       # download new file
-      wget http://ftp.de.debian.org/debian/pool/main/d/debhelper/debhelper_7.4.20.tar.gz
-      echo "Unpacking debhelper_7.4.20.tar.gz"
-      tar -zxvf debhelper_7.4.20.tar.gz
-      echo "Creating debhelper-7.4.20 debian package"
-      mv debhelper debhelper_7.4.20
-      cd debhelper_7.4.20
-      sed -i 's/--utf8//' Makefile
-      dpkg-buildpackage -rfakeroot -b -us -uc
-      echo "Installing debhelper-7.4.20 debian package"
-      dpkg -i --force-all /usr/src/debhelper_7.4.20_all.deb
-popd
+#      wget http://ftp.de.debian.org/debian/pool/main/d/debhelper/debhelper_7.4.20.tar.gz
+#      echo "Unpacking debhelper_7.4.20.tar.gz"
+#      tar -zxvf debhelper_7.4.20.tar.gz
+#      echo "Creating debhelper-7.4.20 debian package"
+#      mv debhelper debhelper_7.4.20
+#      cd debhelper_7.4.20
+#      sed -i 's/--utf8//' Makefile
+#      dpkg-buildpackage -rfakeroot -b -us -uc
+#      echo "Installing debhelper-7.4.20 debian package"
+#      dpkg -i --force-all /usr/src/debhelper_7.4.20_all.deb
+#popd
 
 # Install packages needed for mythtv .23 build
-apt-get -q -f -y --force-yes install yasm libfaac-dev libfribidi-dev liba52-0.7.4-dev
+#apt-get -q -f -y --force-yes install yasm libfaac-dev libfribidi-dev liba52-0.7.4-dev
 # Remove packages that are conflicts with mythtv .23 build
-dpkg -r libmyth-dev qt3-dev-tools libqt3-mt-dev libartsc0-dev
+#dpkg -r libmyth-dev qt3-dev-tools libqt3-mt-dev libartsc0-dev
 
 echo "*** Done: $0"
