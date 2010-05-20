cb43803195d0281a5f8290f4e896f080debee310
posde <posde@theinternet.de>
2010-05-20 18:26:44 +0000

    Refs #687 - Due to malformed version numbering in the old mythtv packages, we need to create some pinning for our repo. This ensures, that the version that we have in our repo is used, if other pinning is not against this

--- src/Update/mkr_postinst.sh
+++ src/Update/mkr_postinst.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 update-rc.d LMCEUpdate start 98 2 . > /dev/null
+
+# Check if we have a pinning to origin deb.linuxmce.org, and if not add it.
+
+EXIST=0
+grep -q "Pin: origin deb.linuxmce.org" /etc/apt/preferences 2>/dev/null && EXIST=1
+
+if [[ "$EXIST" == "0" ]]; then
+   echo "Package: *">> /etc/apt/preferences
+   echo "Pin: origin deb.linuxmce.org">>/etc/apt/preferences
+   echo "Pin-Priority: 1001">>/etc/apt/preferences
+   echo "Pinning setup for deb.linuxmce.org"
+else
+   echo "Pinning for deb.linuxmce.org already exists"
+fi
