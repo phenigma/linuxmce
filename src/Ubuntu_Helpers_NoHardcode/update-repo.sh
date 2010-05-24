f94c88c2b35311e96a05602e41917ee61dac2e10
posde <posde@theinternet.de>
2010-05-24 14:04:45 +0000

    Refs #164 - update-repo allows the use of username

--- src/Ubuntu_Helpers_NoHardcode/update-repo.sh
+++ src/Ubuntu_Helpers_NoHardcode/update-repo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 USER=pos
+if [[ "x$1" != "x" ]]; then
+	USER=$1
+fi
+echo "Using $USER to connect to the repo"
 scp /var/lmce-build/out/tmp/*.deb $USER@deb.linuxmce.org:/var/www/ubuntu/dists/intrepid/beta2/binary-i386
 # home/$USER/newdebs/
 # rsh $USER@deb.linuxmce.org sudo mv /home/$USER/newdebs/*.deb /var/www/ubuntu/dists/intrepid/lmce-alpha-latest-updates/binary-i386 -v
