8032f2768ae6b0fb12907cc3b5c79d545f3a4d13
posde <posde@theinternet.de>
2010-05-26 09:31:48 +0000

    Refs #687 - copy paste error. Thanks mandingo

--- src/Update/mkr_postinst.sh
+++ src/Update/mkr_postinst.sh
@@ -10,7 +10,7 @@ grep -q "^# Dont overwrite" /etc/apt/preferences 2>/dev/null && KEEPOLD=1
 if [[ "$KEEPOLD" == "0" ]]; then
 	echo '
 # We take MythTV from our repo always
-Package: mythtv mythtv-frontend mythtv-backend mythtv-common mythtv-database mythtv-transcode-utils mythweb libmythtv-perl libmyth p
+Package: mythtv mythtv-frontend mythtv-backend mythtv-common mythtv-database mythtv-transcode-utils mythweb libmythtv-perl libmyth python-myth mythtv-themes mythplugins mytharchive mytharchive-data mythbrowser mythgallery mythgame mythmovies mythmusic mythnetvison mythnetvision-data mythnews mythvideo mythweather mythzoneminder myth-doc 
 Pin: origin deb.linuxmce.org
 Pin-Priority: 1001
 
