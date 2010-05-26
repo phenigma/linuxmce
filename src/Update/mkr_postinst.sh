04c20ce4d9dfe20a61fecd1823a690c88cb58235
posde <posde@theinternet.de>
2010-05-26 09:26:57 +0000

    Refs #687 - Make sure the preferences is updated to the way we have it in the install

--- src/Update/mkr_postinst.sh
+++ src/Update/mkr_postinst.sh
@@ -5,11 +5,18 @@ update-rc.d LMCEUpdate start 98 2 . > /dev/null
 # Check if we have a pinning to origin deb.linuxmce.org, and if not add it.
 
 KEEPOLD=0
-grep -q "# Dont overwrite" /etc/apt/preferences 2>/dev/null && KEEPOLD
+grep -q "^# Dont overwrite" /etc/apt/preferences 2>/dev/null && KEEPOLD=1
 
 if [[ "$KEEPOLD" == "0" ]]; then
-   echo "Package: mythtv mythtv-frontend mythtv-backend mythtv-common mythtv-database mythtv-transcode-utils mythweb libmythtv-perl libmyth python-myth mythtv-themes mythplugins mytharchive mytharchive-data mythbrowser mythgallery mythgame mythmovies mythmusic mythnetvison mythnetvision-data mythnews mythvideo mythweather mythzoneminder myth-doc">> /etc/apt/preferences
-   echo "Pin: origin deb.linuxmce.org">>/etc/apt/preferences
-   echo "Pin-Priority: 1001">>/etc/apt/preferences
-   echo "Pinning setup for deb.linuxmce.org"
+	echo '
+# We take MythTV from our repo always
+Package: mythtv mythtv-frontend mythtv-backend mythtv-common mythtv-database mythtv-transcode-utils mythweb libmythtv-perl libmyth p
+Pin: origin deb.linuxmce.org
+Pin-Priority: 1001
+
+# If our local package uses the same version as elsewhere, we take the one we have locally.
+Package: *
+Pin: origin
+Pin-Priority: 600
+' > /etc/apt/preferences
 fi
