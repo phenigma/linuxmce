e50e8edd52426851411eb4f8bc2617413602b23f
posde <posde@theinternet.de>
2010-05-31 18:52:29 +0000

    Refs #687 - make sure the preference is correctly updated, and next time remember BASH syntax

--- src/Update/mkr_postinst.sh
+++ src/Update/mkr_postinst.sh
@@ -7,7 +7,7 @@ update-rc.d LMCEUpdate start 98 2 . > /dev/null
 KEEPOLD=0
 grep -q "^# Dont overwrite" /etc/apt/preferences 2>/dev/null && KEEPOLD=1
 
-if [[ "$KEEPOLD" == "0" ]]; then
+if [[ "$KEEPOLD" -eq "0" ]]; then
 	echo '
 # We take MythTV from our repo always
 Package: mythtv mythtv-frontend mythtv-backend mythtv-common mythtv-database mythtv-transcode-utils mythweb libmythtv-perl libmyth python-myth mythtv-themes mythplugins mytharchive mytharchive-data mythbrowser mythgallery mythgame mythmovies mythmusic mythnetvison mythnetvision-data mythnews mythvideo mythweather mythzoneminder myth-doc 
