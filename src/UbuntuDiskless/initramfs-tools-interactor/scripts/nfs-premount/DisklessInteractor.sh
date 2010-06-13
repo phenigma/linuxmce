54805de957f60fe60b52d73df90a94943a878bc4
tschak <thom.cherryhomes@gmail.com>
2010-06-13 03:30:22 +0000

    Fixes #702 - Temporarily remove amd64 check from the DisklessInteractor until we get an amd64 build going again.

--- src/UbuntuDiskless/initramfs-tools-interactor/scripts/nfs-premount/DisklessInteractor.sh
+++ src/UbuntuDiskless/initramfs-tools-interactor/scripts/nfs-premount/DisklessInteractor.sh
@@ -9,9 +9,11 @@ MyMAC=`ifconfig eth0 | grep "HWaddr "  | sed -r 's/^.* HWaddr ([a-zA-Z0-9:]*)/\1
 Gateway=`ip route | grep "default via" | cut -d " " -f3`
 
 Architecture="i386"
-if grep -q "^flags.* lm" /proc/cpuinfo ;then
-	Architecture="amd64"
-fi
+
+#Temporarily Removed until amd64 builds are re-instated. -tschak
+#if grep -q "^flags.* lm" /proc/cpuinfo ;then
+#	Architecture="amd64"
+#fi
 
 echo "IP: $MyIP; MAC: $MyMAC; Gateway: $Gateway; Architecture: $Architecture; DT: $DT"
 /scripts/interactor "$Gateway" "$MyIP" "$MyMAC" "$Architecture" "$DT"
