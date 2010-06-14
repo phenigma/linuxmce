3d3c1dd60cc18a071fc69b08d139ccc99310a1ed
los93sol <los93sol@gmail.com>
2010-06-14 04:17:42 +0000

    Cleanup the voicemenu generation script and generate the voice for invalid IVR entries

--- src/Asterisk/generate_users_voicemenu.pl
+++ src/Asterisk/generate_users_voicemenu.pl
@@ -13,7 +13,7 @@ my $DB_SQL;
 
 $DB_PL_HANDLE = DBI->connect(&read_pluto_cred()) or die "Can't connect to database: $DBI::errstr\n";
 
-#Lets generate the IVR menu voice. See below example:
+# Let's generate the IVR menu voice. See below example:
 #'Thank you for calling. To call everybody in the house, press 0. To call George, press 1. To call Mary, press 2. To leave a message, press #.';
 print "Generating speech for IVR Main Menu.\n";
 my $list = "";
@@ -24,13 +24,6 @@ $list .= "/tmp/pluto-default-voicemenu2.gsm ";
 &generate_voice("To call everybody in the house, dial 0.","/tmp/pluto-default-voicemenu3.gsm");
 $list .= "/tmp/pluto-default-voicemenu3.gsm ";
 &generate_voice("To leave a message, please press the pound sign.","/tmp/pluto-default-voicemenu4.gsm");
-#$list .= "/tmp/pluto-default-voicemenu4.gsm "; #we will add this to the list last, after all user options are generated.
-#`/usr/bin/sox $list /tmp/pluto-default-voicemenu.gsm`;
-
-
-#$list = "";
-#$list .= "/var/lib/asterisk/sounds/pluto/pluto-default-voicemenu1.gsm ";
-#$list .= "/var/lib/asterisk/sounds/pluto/pluto-default-voicemenu2.gsm ";
 
 $DB_SQL = "select if((Nickname=\"\" OR Nickname IS NULL),UserName,Nickname) AS Name, Extension from Users where `Extension` like '30%' and HasMailbox = 1"; 
 $DB_STATEMENT = $DB_PL_HANDLE->prepare($DB_SQL) or die "Couldn't prepare query '$DB_SQL': $DBI::errstr\n";
@@ -48,56 +41,32 @@ while(my $DB_ROW = $DB_STATEMENT->fetchrow_hashref())
 $DB_STATEMENT->finish();
 
 $list .= "/tmp/pluto-default-voicemenu4.gsm";
+
 `/usr/bin/sox $list /var/lib/asterisk/sounds/pluto/pluto-default-voicemenu.gsm`;
 `/bin/mkdir -p /var/spool/asterisk/voicemail/default/`;
 `/bin/chown -R asterisk:www-data /var/spool/asterisk/voicemail/*`;
 `/bin/chmod 770 -R /var/spool/asterisk/voicemail/*`;
 
-#clean up
+# Clear $list and generate voice for invalid IVR entries
+$list = "";
+
+# Let's generate the voice for invalid IVR entries.
+print "Generating speech for invalid IVR entries.\n";
+&generate_voice("Your selection is not valid, please try again.","/tmp/invalid-entry.gsm");
+$list .= "/tmp/invalid-entry.gsm ";
+
+`/usr/bin/sox $list /var/lib/asterisk/sounds/pluto/invalid-entry.gsm`;
+
+# Clean up
 unlink "/tmp/pluto-default-voicemenu1.gsm";
 unlink "/tmp/pluto-default-voicemenu2.gsm";
 unlink "/tmp/pluto-default-voicemenu3.gsm";
 unlink "/tmp/pluto-default-voicemenu4.gsm";
+unlink "/tmp/invalid-entry.gsm";
+
 sub generate_voice()
 {
 	&generate_voice_festival(@_);
-}
-
-sub generate_voice_att()
-{
-    my $SERVER = 'http://192.20.225.55';
-    my $URL_FORM = '/tts/cgi-bin/nph-talk';
-    my $URL_SOUND = "";
-    my %URL_VARS = ();
-    my $URL_DATA = "";
-    my $TEXT = shift;
-    my $FILE = shift;
-
-    `rm -f /tmp/curl.*`;
-
-    $URL_VARS{'voice'}='crystal';
-    $URL_VARS{'txt'}=$TEXT;
-    $URL_VARS{'downloadButton'}='DOWNLOAD';
-
-    foreach my $var (keys %URL_VARS)
-    {
-        my $str = $URL_VARS{$var};
-        $str =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
-        $URL_DATA .= $var."=".$str."&";
-    }
-    `curl -d '$URL_DATA' '$SERVER$URL_FORM' > /tmp/curl.log`;
-    open(PAGE,"/tmp/curl.log") or die "Bad thing happend";
-    while(<PAGE>)
-    {
-        chomp;
-        if($_ =~ /[<]A\sHREF[=]["]([^"]+)["][>]/)
-        {
-            $URL_SOUND=$1;
-        }
-    }
-    close(PAGE);
-    `curl '$SERVER$URL_SOUND' > /tmp/curl.wav`;
-    `/usr/bin/sox /tmp/curl.wav -r 8000 -c 1 $FILE resample -ql`;
 }
 
 sub generate_voice_festival()
@@ -109,7 +78,7 @@ sub generate_voice_festival()
 	my $DB_STATEMENT2;
 	my $DB_SQL2;
 
-	#TODO: make proper mysql query to get default voice for voice generation! This is a bit of an ugly hack, but should work!
+	# TODO: make proper mysql query to get default voice for voice generation! This is a bit of an ugly hack, but should work!
 	$DB_SQL2 = "SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_DeviceData=283 LIMIT 1";
 	$DB_STATEMENT2 = $DB_PL_HANDLE->prepare($DB_SQL2) or die "Couldn't prepare query '$DB_SQL2': $DBI::errstr\n";
 	$DB_STATEMENT2->execute() or die "Couldn't execute query '$DB_SQL2': $DBI::errstr\n";
@@ -117,18 +86,20 @@ sub generate_voice_festival()
 	my $defaultVoice=$DB_ROW2->{'IK_DeviceData'};
 	$DB_STATEMENT2->finish();
 
-	#create a text file for festival to use
+	# Create a text file for festival to use
 	open TF,">/tmp/festival.txt";
 	print TF "$TEXT\n";
 	close TF;
-	#Create the wave file
+
+	# Create the wave file
 	system('text2wave /tmp/festival.txt -o /tmp/festival.wav -eval "(' . $defaultVoice . ')"');
-	#resample and create permanent file
+
+	# Resample and create permanent file
 	`/usr/bin/sox /tmp/festival.wav -r 8000 -c 1 $FILE resample -ql`;
-	#clean up the temporary files
+
+	# Clean up the temporary files
 	unlink "/tmp/festival.wav";
 	unlink "/tmp/festival.txt";
 }
 
-#`/usr/pluto/bin/create_telecom_defaults.pl`;
 `/usr/pluto/bin/create_pluto_dialplan.pl`;
