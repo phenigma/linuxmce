4bb974410eb77cb7298ca8414690ae50e5c47b4c
los93sol <los93sol@gmail.com>
2010-06-23 00:46:31 +0000

    Fixes pluto-sos-security priority callers, and lots of extensions_custom.conf dialplan cleanup

--- ubuntu/asterisk/add/pluto-sos-gen.pl
+++ ubuntu/asterisk/add/pluto-sos-gen.pl
@@ -4,12 +4,6 @@ use strict;
 use diagnostics;
 use DBI;
 require "/usr/pluto/bin/config_ops.pl";
-use vars '$DCERouter';
-use vars '$PK_Device';
-use vars '$MySqlHost';
-use vars '$MySqlUser';
-use vars '$MySqlPassword';
-use vars '$MySqlDBName';
 
 #$|=1;
 
@@ -43,15 +37,56 @@ unless($row = $statement->fetchrow_hashref())
 $statement->finish();
 @data = split(/[,]/,$row->{IK_DeviceData});
 
+my $list = "";
+
 for(my $i=0;defined($data[$i]);$i+=2)
 {
 	my $j=int(($i+2)/2);
-	print STDERR "[".$data[$i]."]=".$data[$i+1]."\n";
-	`flite -t "To call the $data[$i] press $j" -o /tmp/pluto-security-sos-option$j.wav`;
-	#`/usr/bin/sox /tmp/pluto-security-sos-option$j.wav -t raw -r 8000 -s -w -c 1 /tmp/pluto-security-sos-option$j.gsm`;
-	`/usr/bin/sox /tmp/pluto-security-sos-option$j.wav -r 8000 -c 1 /tmp/pluto-security-sos-option$j.gsm resample -ql`;
-	`rm /tmp/pluto-security-sos-option$j.wav`;
+	&generate_voice("To call the $data[$i] press $j","/tmp/pluto-security-sos-option$j.gsm");
+	$list .= "/tmp/pluto-security-sos-option$j.gsm ";
 }
+
+print "Generating speech for SOS menu options.\n";
+`/usr/bin/sox $list /var/lib/asterisk/sounds/pluto/pluto-security-sos-options.gsm`;
 
 $statement->finish();
 $db_handle->disconnect();
+
+sub generate_voice()
+{
+        &generate_voice_festival(@_);
+}
+
+sub generate_voice_festival()
+{
+        my $TEXT = shift;
+        my $FILE = shift;
+        my $DB_PL_HANDLE2;$db_handle = DBI->connect(&read_pluto_cred()) or die "Can't connect to database: $DBI::errstr\n";
+
+        my $DB_STATEMENT2;
+        my $DB_SQL2;
+
+        #TODO: make proper mysql query to get default voice for voice generation! This is a bit of an ugly hack, but should work!
+        $DB_SQL2 = "SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_DeviceData=283 LIMIT 1";
+        $DB_STATEMENT2 = $db_handle->prepare($DB_SQL2) or die "Couldn't prepare query '$DB_SQL2': $DBI::errstr\n";
+        $DB_STATEMENT2->execute() or die "Couldn't execute query '$DB_SQL2': $DBI::errstr\n";
+        my $DB_ROW2 = $DB_STATEMENT2->fetchrow_hashref();
+        my $defaultVoice=$DB_ROW2->{'IK_DeviceData'};
+        $DB_STATEMENT2->finish();
+
+        # Create a text file for festival to use
+        open TF,">/tmp/festival.txt";
+        print TF "$TEXT\n";
+        close TF;
+
+        # Create the wave file
+        system('text2wave /tmp/festival.txt -o /tmp/festival.wav -eval "(' . $defaultVoice . ')"');
+
+        # Resample and create permanent file
+        `/usr/bin/sox /tmp/festival.wav -r 8000 -c 1 $FILE resample -ql`;
+
+        # Clean up the temporary files
+        unlink "/tmp/festival.wav";
+        unlink "/tmp/festival.txt";
+}
+
