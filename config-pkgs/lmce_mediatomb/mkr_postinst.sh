0fe1efb691dd85c8b930ea89b3e41a71ae5a4d58
merkur2k <merkur2k@gmail.com>
2010-09-24 00:38:27 +0000

    Refs #826 - Updates for mediatomb config file

--- config-pkgs/lmce_mediatomb/mkr_postinst.sh
+++ config-pkgs/lmce_mediatomb/mkr_postinst.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Configure Mediatomb to properly integrate with LMCE.
-sed -i "/INTERFACE/d" /etc/default/mediatomb
-# grep -v "INTERFACE" /etc/default/mediatomb>/etc/default/mediatomb
-echo "INTERFACE=\"eth1\"">>/etc/default/mediatomb
+. /usr/pluto/bin/Network_Parameters.sh
+sed -i "s/^INTERFACE=\"*\(.*\)\"*/INTERFACE=\"${IntIf}\"/" /etc/default/mediatomb
+sed -i "s/255\.0\.0\.0.*\"$/255.0.0.0 ${IntIf}\"/" /etc/default/mediatomb
 usermod -G public -a mediatomb
 mysql -e "CREATE DATABASE IF NOT EXISTS pluto_mediatomb"
+route del -net 239.0.0.0 netmask 255.0.0.0
 /etc/init.d/mediatomb restart
+route add -net 239.0.0.0 netmask 255.0.0.0 ${IntIf}
