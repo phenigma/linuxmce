ddc856c8a99425fb434351e8431b2c3bd5cdb323
tschak <thom.cherryhomes@gmail.com>
2010-10-11 00:28:59 +0000

    Fixes #841 - thanks esev. This works great. -tschak

--- config-pkgs/lmce_mediatomb/mkr_postinst.sh
+++ config-pkgs/lmce_mediatomb/mkr_postinst.sh
@@ -4,6 +4,6 @@
 sed -i "s/^INTERFACE=\"*.*\"*/INTERFACE=\"${IntIf}\"/" /etc/default/mediatomb
 usermod -G public -a mediatomb
 mysql -e "CREATE DATABASE IF NOT EXISTS pluto_mediatomb"
-route del -net 239.0.0.0 netmask 255.0.0.0
+route del -net 239.0.0.0 netmask 255.0.0.0 || /bin/true
 /etc/init.d/mediatomb stop
 /etc/init.d/mediatomb start
