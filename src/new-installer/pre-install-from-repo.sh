7e150a8aa2bbe21a14e60984e047bfb2e57727a4
posde <posde@theinternet.de>
2010-12-04 09:02:15 +0000

    Refs #878 - put some intelligence into the pre-install-from-repo to detect the host for our distro

--- src/new-installer/pre-install-from-repo.sh
+++ src/new-installer/pre-install-from-repo.sh
@@ -17,6 +17,13 @@ AddRepoToSourcesTop "file:${LOCAL_REPO_BASE} ${LOCAL_REPO_DIR}"
 AddRepoToSources "http://deb.linuxmce.org/ubuntu/ ${DISTRO}  ${COMPOS}"
 AddRepoToSources "http://debian.slimdevices.com/ stable  main"
 
+# Check where our distro is hosted
+DISTRO_HOST=
+wget http://archive.ubuntu.com/ubuntu/dists/${DISTRO}/main/binary-i386/Packages.gz && DISTRO_HOST=archive
+wget http://old-releases.ubuntu.com/ubuntu/dists/${DISTRO}/main/binary-i386/Packages.gz && DISTRO_HOST=old-releases
+AddRepoToSources "http://${DISTRO-HOST}.ubuntu.com/ubuntu ${DISTRO} main restricted universe multiverse"
+AddRepoToSources "http://${DISTRO-HOST}.ubuntu.com/ubuntu ${DISTRO}-security main restricted universe multiverse"
+
 if AddRepoToSources "http://packages.medibuntu.org/ ${DISTRO}  free non-free"
 then
 	AddGpgKeyToKeyring http://packages.medibuntu.org/medibuntu-key.gpg
