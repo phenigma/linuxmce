fbf1fd76a3359e53f25290f62595375d5a5e98a8
posde <posde@theinternet.de>
2010-12-04 08:46:17 +0000

    Refs #878 - first changes to DVD install script for distros other than intrepid

--- src/new-installer/pre-install-from-DVD.sh
+++ src/new-installer/pre-install-from-DVD.sh
@@ -17,8 +17,8 @@ CreatePackagesFiles
 AddRepoToSourcesTop "file:${LOCAL_REPO_BASE} ${LOCAL_REPO_DIR}"
 AddRepoToSources "http://deb.linuxmce.org/ubuntu/ ${DISTRO}  ${COMPOS}"
 AddRepoToSources "http://debian.slimdevices.com/ stable  main"
-AddRepoToSources "http://old-releases.ubuntu.com/ubuntu intrepid-security main restricted universe multiverse"
-AddRepoToSources "http://old-releases.ubuntu.com/ubuntu intrepid main restricted universe multiverse"
+AddRepoToSources "http://old-releases.ubuntu.com/ubuntu ${DISTRO}-security main restricted universe multiverse"
+AddRepoToSources "http://old-releases.ubuntu.com/ubuntu ${DISTRO} main restricted universe multiverse"
 
 #AddRepoToSources "http://packages.medibuntu.org/ ${DISTRO}  free non-free"
 AddGpgKeyToKeyring http://packages.medibuntu.org/medibuntu-key.gpg
