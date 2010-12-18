68e74b9cafbebe9a2b4ca44d1a0c846d9a1ec5fe
los93sol <los93sol@gmail.com>
2010-12-18 11:59:22 +0000

    Refs #921 Initial commit of Kinect DCE Device stubs

---
+++ src/Kinect/Kinect.d
@@ -0,0 +1,18 @@
+Kinect.o Kinect.d : Kinect.cpp Kinect.h ../Gen_Devices/KinectBase.h \
+  ../DCE/DeviceData_Impl.h ../DCE/Event_Impl.h ../DCE/ClientSocket.h \
+  ../DCE/Socket.h ../PlutoUtils/MultiThreadIncludes.h \
+  ../PlutoUtils/CommonIncludes.h ../DCE/Logger.h \
+  ../SerializeClass/SerializeClass.h ../PlutoUtils/FileUtils.h \
+  ../PlutoUtils/StringUtils.h ../PlutoUtils/CommonIncludes.h \
+  ../PlutoUtils/Other.h ../PlutoUtils/getch.h ../PlutoUtils/MyStl.h \
+  ../DCE/PlutoLockLogger.h ../DCE/Message.h ../DCE/Message.h \
+  ../DCE/DeviceData_Base.h ../pluto_main/Define_DeviceCategory.h \
+  ../DCE/Message.h ../DCE/Command_Impl.h ../DCE/HandleRequestSocket.h \
+  ../DCE/DeviceData_Impl.h ../DCE/MessageBuffer.h ../DCE/Logger.h \
+  ../Gen_Devices/../pluto_main/Define_Command.h \
+  ../Gen_Devices/../pluto_main/Define_CommandParameter.h \
+  ../Gen_Devices/../pluto_main/Define_DeviceTemplate.h \
+  ../Gen_Devices/../pluto_main/Define_Event.h \
+  ../Gen_Devices/../pluto_main/Define_EventParameter.h \
+  ../Gen_Devices/../pluto_main/Define_DeviceData.h \
+  ../Gen_Devices/AllCommandsRequests.h ../DCE/PreformedCommand.h
