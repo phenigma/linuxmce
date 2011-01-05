31de21d7abfd7d3067f110842a5c23043179ef08
radu <radu.cx+linuxmce@gmail.com>
2011-01-05 01:42:40 +0000

    fixed logic determining if the Orbiter is running, and beautified

--- src/BootScripts/BootMessage.sh
+++ src/BootScripts/BootMessage.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 
-if [ -a "/tmp/.X11-unix/X0" ]
-then
+export DISPLAY=:0
+
+OSD_Message()
+{
+	gnome-osd-client --full --dbus "<message id='bootmsg' osd_fake_translucent_bg='off' osd_vposition='center' hide_timeout='1000000' osd_halignment='center'><span foreground='white' font='Arial 72'>$*</span></message>"
+}
+
+if [[ -e "/tmp/.X11-unix/X0" ]]; then
 	# A little trick to see if Orbiter is Running.
 
-	if [ $(pidof OrbiterGL) ]
-	then
+	if [[ -n "$(pidof OrbiterGL)" ]]; then
 		OrbiterGLisNotRunning=0
 	else
 		OrbiterGLisNotRunning=1
 	fi
 
-	if [ $(pidof Orbiter) ]
-	then
+	if [[ -n "$(pidof Orbiter)" ]]; then
 		OrbiterIsNotRunning=0
 	else
 		OrbiterIsNotRunning=1
 	fi
 
-	NoOrbitersRunning=$OrbiterGLisNotRunning || $OrbiterIsNotRunning
+	NoOrbitersRunning=$(($OrbiterGLisNotRunning && $OrbiterIsNotRunning))
 
-
-	if [ $NoOrbitersRunning -eq 1 ] 
-	then
+	if [[ $NoOrbitersRunning -eq 1 ]]; then
 		# Orbiter is not running, feedback needed.
-
-		export DISPLAY=:0
-		gnome-osd-client --full --dbus "<message id='bootmsg' osd_fake_translucent_bg='off' osd_vposition='center' hide_timeout='1000000' osd_halignment='center'><span foreground='white' font='Arial 72'>$*</span></message>"
-
+		OSD_Message "$@"
 	else
 		# Orbiter is running, feedback not needed.
 		# clear out any messages pending.
-		export DISPLAY=:0
-		gnome-osd-client --full --dbus "<message id='bootmsg' osd_fake_translucent_bg='off' osd_vposition='center' hide_timeout='1000000' osd_halignment='center'><span foreground='white' font='Arial 72'> </span></message>"
+		OSD_Message " "
 	fi
-
 else
 	usplash_write TEXT-URGENT "$*"
 fi
