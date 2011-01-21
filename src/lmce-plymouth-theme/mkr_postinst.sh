0d86540029a34476ac14267b547c308aff5f490c
foxi352 <serge@swa.lu>
2011-01-21 12:57:06 +0000

    Refs #958 - Linked kubuntu boot messages to plymouth

--- src/lmce-plymouth-theme/mkr_postinst.sh
+++ src/lmce-plymouth-theme/mkr_postinst.sh
@@ -6,12 +6,14 @@
 exit 0
 
 # Prevent delayed loading of our splash
-if [[ ! $(grep "FRAMEBUFFER=y" /etc/initramfs-tools/splash) ]]; then
+if [[ ! $(grep "FRAMEBUFFER=y" /etc/initramfs-tools/conf.d/splash) ]]; then
 	echo "FRAMEBUFFER=y" >>/etc/initramfs-tools/conf.d/splash
 fi
 
 # register our theme with plymouth
 update-alternatives --install /lib/plymouth/themes/default.plymouth default.plymouth /lib/plymouth/themes/LinuxMCE/LinuxMCE.plymouth 900
 update-alternatives --config default.plymouth
+
+# trigger initrams update to include new theme
 update-initramfs -u
 
