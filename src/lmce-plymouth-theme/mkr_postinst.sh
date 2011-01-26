407d75541dc8420a4202e71ccb27d126319ad69c
foxi352 <serge@swa.lu>
2011-01-26 11:01:04 +0000

    Refs #958 - Suppress selection prompt, lmce theme = auto select

--- src/lmce-plymouth-theme/mkr_postinst.sh
+++ src/lmce-plymouth-theme/mkr_postinst.sh
@@ -9,7 +9,7 @@ fi
 
 # register our theme with plymouth
 update-alternatives --install /lib/plymouth/themes/default.plymouth default.plymouth /lib/plymouth/themes/LinuxMCE/LinuxMCE.plymouth 900
-update-alternatives --config default.plymouth
+update-alternatives --auto default.plymouth
 
 # trigger initrams update to include new theme
 dpkg-trigger update-initramfs
