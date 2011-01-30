781c82e1ae34a8d634040579680a65ca589c0afc
merkur2k <merkur2k@gmail.com>
2011-01-30 07:00:30 +0000

    Refs #826 - linux-igd now enabled by default

--- config-pkgs/lmce_linux-igd/mkr_postinst.sh
+++ config-pkgs/lmce_linux-igd/mkr_postinst.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Configure linux-igd to properly integrate with LMCE.
 . /usr/pluto/bin/Network_Parameters.sh
+sed -i "s/^#INTIFACE/INTIFACE/" /etc/default/linux-igd
+sed -i "s/^#EXTIFACE/EXTIFACE/" /etc/default/linux-igd
 sed -i "s/^INTIFACE=\"*\(.*\)\"*/INTIFACE=${IntIf}/" /etc/default/linux-igd
 sed -i "s/^EXTIFACE=\"*\(.*\)\"*/EXTIFACE=${ExtIf}/" /etc/default/linux-igd
 /etc/init.d/linux-igd restart
