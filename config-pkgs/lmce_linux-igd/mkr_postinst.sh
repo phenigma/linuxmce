20819de093a9fa92ee6d5bab7bbb8a593a37a4b9
merkur2k <merkur2k@gmail.com>
2011-01-31 17:56:10 +0000

    Refs #826 - Further fixes for linux=igd installation

--- config-pkgs/lmce_linux-igd/mkr_postinst.sh
+++ config-pkgs/lmce_linux-igd/mkr_postinst.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Configure linux-igd to properly integrate with LMCE.
 . /usr/pluto/bin/Network_Parameters.sh
-# Do
+# Do not configure linux-igd if single-nic system
+IntIfUp=$(ip addre | grep inet | grep $IntIf)
 if [ -n "$IntIf" ] && [ -z "$( echo $IntIf | grep : )"  ] && [ -e /etc/default/linux-igd ] ;then
 	sed -i "s/^#INTIFACE/INTIFACE/" /etc/default/linux-igd
 	sed -i "s/^#EXTIFACE/EXTIFACE/" /etc/default/linux-igd
 	sed -i "s/^INTIFACE=\"*\(.*\)\"*/INTIFACE=${IntIf}/" /etc/default/linux-igd
 	sed -i "s/^EXTIFACE=\"*\(.*\)\"*/EXTIFACE=${ExtIf}/" /etc/default/linux-igd
-	if [ -n "$( ifconfig -s | grep $IntIf )" ] ;then
+	# Do not start linux-igd if internel interface is not yet up
+	if [ -n "IntIfUp" ] ;then
 		/etc/init.d/linux-igd restart
 	fi
 fi
