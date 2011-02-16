66b3129b2c5c3dd1863d68c363c2ce27157a9fad
posde <posde@theinternet.de>
2011-02-16 22:53:33 +0000

    Refs #878 - disable xine_gui_send_vo_data as it is not being used, and has been deprecated a long time

--- src/Xine_Player/Xine_Stream.cpp
+++ src/Xine_Player/Xine_Stream.cpp
@@ -1128,7 +1128,7 @@ int Xine_Stream::XServerEventProcessor(XEvent &event )
 			rect.w = 0;
 			rect.h = 0;
 
-			xine_gui_send_vo_data ( m_pXineStream, XINE_GUI_SEND_TRANSLATE_GUI_TO_VIDEO, ( void* ) & rect );
+//			xine_gui_send_vo_data ( m_pXineStream, XINE_GUI_SEND_TRANSLATE_GUI_TO_VIDEO, ( void* ) & rect );
 			LoggerWrapper::GetInstance()->Write( LV_STATUS, "Xine player: mouse button event after translation: mx=%d my=%d", rect.x, rect.y );
 
 			xineEvent.stream = m_pXineStream;
