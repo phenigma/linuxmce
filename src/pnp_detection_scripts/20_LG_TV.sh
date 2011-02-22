222036acc7357051bced8117fcb56cc0a9da1b1c
merkur2k <merkur2k@gmail.com>
2011-02-22 23:11:09 +0000

    Refs #1006 - Update LG TV pnp script

--- src/pnp_detection_scripts/20_LG_TV.sh
+++ src/pnp_detection_scripts/20_LG_TV.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-
-echo "LG TV Detection script queue $2"
-/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "ka 01 01\r\n" -s "a 01 " -i 8
+echo "LG TV detection script queue $2"
+# make sure its on
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "ka 01 01\r\n" -s "a 01 OK" -i 2
+# double check that its on
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "ka 01 FF\r\n" -s "a 01 OK" -i 2
 
 if [[ "$?" -ne 0 ]]; then
 echo "It's not a LG TV"
 /usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 0
-else
-echo "It is an LG TV"
+exit
+fi
+# older TVs should not have a "xb" command
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "xb 01 FF\r\n" -s "b 01 OK" -i 2
+if [[ "$?" -eq 0 ]]; then
+echo "LG TV with newer command set"
+/usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 2126
+exit
+fi
+# double check that it responds correctly to the older command
+/usr/pluto/bin/TestSerialPort -p $3 -P N81 -b 9600 -t "kb 01 FF\r\n" -s "b 01 OK" -i 2
+if [[ "$?" -eq 0 ]]; then
+echo "LG TV with older command set"
 /usr/pluto/bin/MessageSend dcerouter -r 0 $1 1 806 224 $2 13 "$4" 44 1915
 fi
+
