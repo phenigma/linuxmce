8c7dad808ffe94fab97232708920131fc5a85ac0
posde <posde@theinternet.de>
2011-02-28 17:31:08 +0000

    Fixes #1004 - Start DCERouter before doing orbitergen

--- src/LMCE_Launch_Manager/StartCoreServices.sh
+++ src/LMCE_Launch_Manager/StartCoreServices.sh
@@ -5,6 +5,12 @@
 echo "$(date -R) Starting Core Services - config device changes" >> /var/log/pluto/StartCoreServices.log 2>&1
 bash -x /usr/pluto/bin/Config_Device_Changes.sh >> /var/log/pluto/StartCoreServices.log 2>&1
 
+# Start DCERouter before doing the orbitergen, otherwise LMCE Launch Manager will try starting devices which
+# end up being disabled due to DCERouter not yet running.
+echo "$(date -R) Starting Core Services - starting dcerouter" >> /var/log/pluto/StartCoreServices.log 2>&1
+bash -x /usr/pluto/bin/Start_DCERouter.sh >> /var/log/pluto/StartCoreServices.log 2>&1
+echo "$(date -R) Starting Core Services - done starting dcerouter" >> /var/log/pluto/StartCoreServices.log 2>&1
+
 echo "$(date -R) Starting Core Services - start orbitergen " >> /var/log/pluto/StartCoreServices.log 2>&1
 bash -x /usr/pluto/bin/Start_OrbiterGen.sh >> /var/log/pluto/StartCoreServices.log 2>&1
 
@@ -17,10 +23,6 @@ fi
 Q="SELECT PK_Device FROM pluto_main.Device LEFT JOIN pluto_main.Device_DeviceData ON pluto_main.Device.PK_Device = pluto_main.Device_DeviceData.FK_Device where pluto_main.Device_DeviceData.FK_DeviceData in (31,29) group by PK_Device"
 Devices=$(RunSQL "$Q")
 /usr/pluto/bin/sync_pluto2amp.pl $Devices
-
-echo "$(date -R) Starting Core Services - starting dcerouter" >> /var/log/pluto/StartCoreServices.log 2>&1
-bash -x /usr/pluto/bin/Start_DCERouter.sh >> /var/log/pluto/StartCoreServices.log 2>&1
-echo "$(date -R) Starting Core Services - done starting dcerouter" >> /var/log/pluto/StartCoreServices.log 2>&1
 
 #wake mds
 echo "$(date -R) Starting Core Services - waking up M/Ds" >> /var/log/pluto/StartCoreServices.log 2>&1
