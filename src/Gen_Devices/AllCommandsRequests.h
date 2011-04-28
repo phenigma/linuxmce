5dd0413519ba0e931d5c3657822d8bf790ccc496
tschak <thom.cherryhomes@gmail.com>
2011-04-28 06:45:38 +0000

    Refs #24 - Gen_Devices checkin for Media Plugin new command.

--- src/Gen_Devices/AllCommandsRequests.h
+++ src/Gen_Devices/AllCommandsRequests.h
@@ -29368,5 +29368,41 @@ namespace DCE
 			COMMANDPARAMETER_PK_Device_CONST, StringUtils::itos(iPK_Device).c_str(),
 			COMMANDPARAMETER_PK_Device_Related_CONST, StringUtils::itos(iPK_Device_Related).c_str()); }
 	};
+	class CMD_Update_Move_Status : public PreformedCommand {
+	public:
+		CMD_Update_Move_Status(long DeviceIDFrom, long DeviceIDTo,string sStatus,string sTask,string sJob) { m_pMessage = new Message(DeviceIDFrom, DeviceIDTo, MESSAGETYPE_COMMAND, PRIORITY_NORMAL, 
+			COMMAND_Update_Move_Status_CONST,
+			3 /* number of parameters */,
+			COMMANDPARAMETER_Status_CONST, sStatus.c_str(),
+			COMMANDPARAMETER_Task_CONST, sTask.c_str(),
+			COMMANDPARAMETER_Job_CONST, sJob.c_str()); }
+	};
+	class CMD_Update_Move_Status_DL : public PreformedCommand {
+	public:
+		CMD_Update_Move_Status_DL(long DeviceIDFrom, string DeviceIDTo,string sStatus,string sTask,string sJob) { m_pMessage = new Message(DeviceIDFrom, DeviceIDTo, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
+			COMMAND_Update_Move_Status_CONST,
+			3 /* number of parameters */,
+			COMMANDPARAMETER_Status_CONST, sStatus.c_str(),
+			COMMANDPARAMETER_Task_CONST, sTask.c_str(),
+			COMMANDPARAMETER_Job_CONST, sJob.c_str()); }
+	};
+	class CMD_Update_Move_Status_DT : public PreformedCommand {
+	public:
+		CMD_Update_Move_Status_DT(long DeviceIDFrom, long MasterDevice, eBroadcastLevel eB,string sStatus,string sTask,string sJob) { m_pMessage = new Message(DeviceIDFrom, MasterDevice, eB, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
+			COMMAND_Update_Move_Status_CONST,
+			3 /* number of parameters */,
+			COMMANDPARAMETER_Status_CONST, sStatus.c_str(),
+			COMMANDPARAMETER_Task_CONST, sTask.c_str(),
+			COMMANDPARAMETER_Job_CONST, sJob.c_str()); }
+	};
+	class CMD_Update_Move_Status_Cat : public PreformedCommand {
+	public:
+		CMD_Update_Move_Status_Cat(long DeviceIDFrom, long DeviceCategory, bool bIncludeChildren, eBroadcastLevel eB,string sStatus,string sTask,string sJob) { m_pMessage = new Message(DeviceIDFrom, DeviceCategory, bIncludeChildren, eB, MESSAGETYPE_COMMAND, PRIORITY_NORMAL,
+			COMMAND_Update_Move_Status_CONST,
+			3 /* number of parameters */,
+			COMMANDPARAMETER_Status_CONST, sStatus.c_str(),
+			COMMANDPARAMETER_Task_CONST, sTask.c_str(),
+			COMMANDPARAMETER_Job_CONST, sJob.c_str()); }
+	};
 }
 #endif
--- src/Gen_Devices/Media_PluginBase.h
+++ src/Gen_Devices/Media_PluginBase.h
@@ -287,6 +287,7 @@ public:
 	virtual void CMD_Specify_Repeat_Options(string sPK_EntertainArea,int iRepeat,string &sCMD_Result,class Message *pMessage) {};
 	virtual void CMD_Move_File(string sFilename,string sPath,string &sCMD_Result,class Message *pMessage) {};
 	virtual void CMD_Specify_Capture_Card_Audio_Port(int iPK_Device,int iPK_Device_Related,string &sCMD_Result,class Message *pMessage) {};
+	virtual void CMD_Update_Move_Status(string sStatus,string sTask,string sJob,string &sCMD_Result,class Message *pMessage) {};
 
 	//This distributes a received message to your handler.
 	virtual ReceivedMessageResult ReceivedMessage(class Message *pMessageOriginal)
@@ -1508,6 +1509,34 @@ public:
 							int iRepeat=atoi(itRepeat->second.c_str());
 							for(int i=2;i<=iRepeat;++i)
 								CMD_Specify_Capture_Card_Audio_Port(iPK_Device,iPK_Device_Related,sCMD_Result,pMessage);
+						}
+					};
+					iHandled++;
+					continue;
+				case COMMAND_Update_Move_Status_CONST:
+					{
+						string sCMD_Result="OK";
+						string sStatus=pMessage->m_mapParameters[COMMANDPARAMETER_Status_CONST];
+						string sTask=pMessage->m_mapParameters[COMMANDPARAMETER_Task_CONST];
+						string sJob=pMessage->m_mapParameters[COMMANDPARAMETER_Job_CONST];
+						CMD_Update_Move_Status(sStatus.c_str(),sTask.c_str(),sJob.c_str(),sCMD_Result,pMessage);
+						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
+							pMessageOut->m_mapParameters[0]=sCMD_Result;
+							SendMessage(pMessageOut);
+						}
+						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
+						{
+							pMessage->m_bRespondedToMessage=true;
+							SendString(sCMD_Result);
+						}
+						if( (itRepeat=pMessage->m_mapParameters.find(COMMANDPARAMETER_Repeat_Command_CONST))!=pMessage->m_mapParameters.end() )
+						{
+							int iRepeat=atoi(itRepeat->second.c_str());
+							for(int i=2;i<=iRepeat;++i)
+								CMD_Update_Move_Status(sStatus.c_str(),sTask.c_str(),sJob.c_str(),sCMD_Result,pMessage);
 						}
 					};
 					iHandled++;
