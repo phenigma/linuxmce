d125339f761f6b5d14d7da2bf85f8c63c99ff712
posde <posde@theinternet.de>
2011-05-05 12:06:48 +0000

    Refs #891 - Revive build.sh for mame building

---
+++ ubuntu/sdlmame/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARCH=`uname -m`
+
+
+. /etc/lmce-build/builder.conf
+
+# If we build for amd64 we need to set an env for mame
+if [ $arch = "amd64"  ]
+then
+	echo Adding 64-bit pointer handling.
+	MKOPTS="PTR64=1"	
+fi
+
+make $MKOPTS
+
