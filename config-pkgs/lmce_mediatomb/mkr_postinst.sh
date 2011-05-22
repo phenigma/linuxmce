8afcefa38af9617aa8e091dec2b46962f1d391b1
golgoj4 <golgoj4>
2011-05-22 21:26:04 +0000

    Ref #971: Now sets up default mediatomb configuration file in postinstall script.

--- config-pkgs/lmce_mediatomb/mkr_postinst.sh
+++ config-pkgs/lmce_mediatomb/mkr_postinst.sh
@@ -5,5 +5,6 @@ sed -i "s/^INTERFACE=\"*.*\"*/INTERFACE=\"${IntIf}\"/" /etc/default/mediatomb
 usermod -G public -a mediatomb
 mysql -e "CREATE DATABASE IF NOT EXISTS pluto_mediatomb"
 route del -net 239.0.0.0 netmask 255.0.0.0 || /bin/true
+cp /etc/mediatomb/config.xml.dpkg-dist /etc/mediatomb/config.xml
 /etc/init.d/mediatomb stop
 /etc/init.d/mediatomb start
