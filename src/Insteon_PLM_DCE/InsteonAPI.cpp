3cf712ff5d0848280fd200780ba1a6237be585ef
merkur2k <merkur2k@gmail.com>
2011-06-26 17:03:04 +0000

    Refs #1115 - add more robust command queueing, add improved logging, change command constant names to include actual byte values

--- src/Insteon_PLM_DCE/InsteonAPI.cpp
+++ src/Insteon_PLM_DCE/InsteonAPI.cpp
@@ -18,6 +18,8 @@
 #include "InsteonAPI.h"
 #include "Insteon_PLM_DCE.h"
 
+#include "Generic_Serial_Device/IOUtils.h"
+
 DCE::Insteon_PLM_DCE *DCEcallback;
 
 extern "C" void *start( void* );
@@ -35,6 +37,9 @@ void* InsteonAPI::InsteonAPI::init(const char *device)
 	
 	DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON, "Initialization...");
 
+	callbackpool = 1;
+	await_ack = 0;
+	
 	DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON, "Open serial port %s",device);
 	
 	OpenSerialPortEx(device,&serialPort);
@@ -58,13 +63,13 @@ void* InsteonAPI::InsteonAPI::init(const char *device)
 	
 	DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Get IM info");
 	//Initiate a request for IM Info
-	mybuf[0] = IM_CMD_START;
-	mybuf[1] = CMD_GET_IM_INFO;
-	sendFunction( mybuf , 2, 0, 0); 
+	//mybuf[0] = IM_CMD_START;
+	mybuf[0] = CMD_60_GET_IM_INFO;
+	sendFunction( mybuf , 1, 0, 0); 
 	DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Get IM config");
 	//Get the IM config
-	mybuf[1] = CMD_GET_IM_CONFIG;
-	sendFunction( mybuf , 2, 0, 0);
+	mybuf[0] = CMD_73_GET_IM_CONFIG;
+	sendFunction( mybuf , 1, 0, 0);
 	getDatabase();
 	return NULL;
 
@@ -79,15 +84,17 @@ InsteonAPI::InsteonAPI::InsteonAPI(void* myInsteon)
 void InsteonAPI::InsteonAPI::getDatabase()
 {
 	char mybuf[2];
-	mybuf[0] = IM_CMD_START;
-	mybuf[1] = CMD_GET_FIRST_ALL_LINK_RECORD;
+	//mybuf[0] = IM_CMD_START;
+	mybuf[0] = CMD_69_GET_FIRST_ALL_LINK_RECORD;
 	DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Start getting ALL-Link database");
-	sendFunction( mybuf , 2, 0, 0);
+	sendFunction( mybuf , 1, 0, 0);
 }
 
 void InsteonAPI::InsteonAPI::receiveFunction()
 {
 	DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"receiveFunction started");
+	long timer;
+	long idletimer;
 	char mybuf[1024]; // Buffer to hold commands to send
 	char mybuf2[1024]; // Buffer to hold received data
 	size_t len2;
@@ -98,13 +105,15 @@ void InsteonAPI::InsteonAPI::receiveFunction()
 		mybuf2[0]=0;
 		len2 = ReadSerialStringEx(serialPort,mybuf2, 1, 100);
 			
-		if ((len2 == 1) && (mybuf2[0] == IM_CMD_START))
+		if ((len2 == 1) && (mybuf2[0] == IM_02_CMD_START)) // we received a message
 		{
 			len2 += ReadSerialStringEx(serialPort,mybuf2+1, 1, 100);	
-			DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received command: %x",mybuf2[1]);
+//			DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received command: %X",mybuf2[1]);
+			// commands are varible lenth and the ACK/NAK is at the end, so we have to figure out what it is
+			// and read the whole thing to know if it was successfull or not
 			switch(mybuf2[1])
 			{
-				case CMD_GET_IM_INFO:
+				case CMD_60_GET_IM_INFO:
 				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,7,100);
 				
 				plmIDHiByte  = mybuf2[2];
@@ -115,36 +124,62 @@ void InsteonAPI::InsteonAPI::receiveFunction()
 				plmDevSubCat	= mybuf2[6];
 				plmFirmwareRev 	= mybuf2[7];
 
-				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Got IM info; Address: %x.%x.%x Cat: %x SubCat: %x Firmware: %x",plmIDHiByte,plmIDMidByte,plmIDLoByte,plmDevCat,plmDevSubCat,plmFirmwareRev);
+				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Got IM info %s; Address: %X.%X.%X Cat: %X SubCat: %X Firmware: %X",DCE::IOUtils::FormatHexBuffer(mybuf2,8).c_str(),plmIDHiByte,plmIDMidByte,plmIDLoByte,plmDevCat,plmDevSubCat,plmFirmwareRev);
+				sendNextCommand();
 				break;
 
-				case CMD_GET_IM_CONFIG:
+				case CMD_73_GET_IM_CONFIG:
 				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,4,100);
 				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Got IM config: %x",mybuf2[2]);
-				if (!(mybuf2[2] & CFG_BIT_MONITOR_MODE)) {
+				if (!(mybuf2[2] & CFG_40_BIT_MONITOR_MODE)) {
 					// IM is not in monitor mode, but needs to be
-					mybuf[0] = IM_CMD_START;
-					mybuf[1] = CMD_SET_IM_CONFIG;
-					mybuf[2] = mybuf2[2] | CFG_BIT_MONITOR_MODE;
-					DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Setting IM config: %x",mybuf2[2] | CFG_BIT_MONITOR_MODE);
-					sendFunction( mybuf , 3, 0, 0);
+					//mybuf[0] = IM_CMD_START;
+					mybuf[0] = CMD_6B_SET_IM_CONFIG;
+					mybuf[1] = mybuf2[2] | CFG_40_BIT_MONITOR_MODE;
+					DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Setting IM config: %x",mybuf2[2] | CFG_40_BIT_MONITOR_MODE);
+					sendFunction( mybuf , 2, 0, 0);
 				}
+				sendNextCommand();
 				break;
-				case CMD_GOT_ALL_LINK_RECORD:
+				case CMD_57_GOT_ALL_LINK_RECORD:
 				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,8,100);
-				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Got ALL-Link record; Address: %x.%x.%x",mybuf2[4],mybuf2[5],mybuf2[6]);
+				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Got ALL-Link record; Address: %X.%X.%X (%s)",mybuf2[4],mybuf2[5],mybuf2[6],DCE::IOUtils::FormatHexBuffer(mybuf2,10).c_str());
 				// we now need to query the device to see what it is
-				mybuf[0] = IM_CMD_START;
-				mybuf[1] = CMD_SEND_SD_ED_MSG;
-				mybuf[2] = mybuf2[4];
-				mybuf[3] = mybuf2[5];
-				mybuf[4] = mybuf2[6];
-				mybuf[5] = 0x0F;
-				mybuf[6] = CMD_SD_PROD_DATA_REQ;
-				mybuf[7] = 0x00;
-				sendFunction( mybuf , 8, 0, 0);
+				//mybuf[0] = IM_CMD_START;
+				mybuf[0] = CMD_62_SEND_SD_ED_MSG;
+				mybuf[1] = mybuf2[4];
+				mybuf[2] = mybuf2[5];
+				mybuf[3] = mybuf2[6];
+				mybuf[4] = 0x0f;
+				mybuf[5] = 0x19;
+				mybuf[6] = 0x00;
+				sendFunction( mybuf , 7, 0, 0);
+				//mybuf[4] = 0x1F;
+				//mybuf[5] = 0x03;
+				//mybuf[6] = 0x00;
+				//mybuf[7] = 0x00;
+				//mybuf[8] = 0x00;
+				//mybuf[9] = 0x00;
+				//mybuf[10] = 0x00;
+				//mybuf[11] = 0x00;
+				//mybuf[12] = 0x00;
+				//mybuf[13] = 0x00;
+				//mybuf[14] = 0x00;
+				//mybuf[15] = 0x00;
+				//mybuf[16] = 0x00;
+				//mybuf[17] = 0x00;
+				//mybuf[18] = 0x00;
+				//mybuf[19] = 0x00;
+				//mybuf[20] = 0x00;
+				//sendFunction( mybuf , 21, 0, 0);
+				//sendNextCommand();
 				break;
-				case CMD_ED_MSG_RECV:
+				case 0x69:
+					len2 += ReadSerialStringEx(serialPort,mybuf2+2, 1, 100);
+					DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received CMD %X response: %s",mybuf2[1],DCE::IOUtils::FormatHexBuffer(mybuf2,3).c_str());
+					sendNextCommand();
+				break;
+				case CMD_51_ED_MSG_RECV:
 					len2 += ReadSerialStringEx(serialPort,mybuf2+1, 1, 100);
 					DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received extended message: %x",mybuf2[2]);
 					switch(mybuf2[2]){
@@ -153,34 +188,65 @@ void InsteonAPI::InsteonAPI::receiveFunction()
 //				mybuf[1] = CMD_GET_NEXT_ALL_LINK_RECORD;
 //				sendFunction( mybuf , 2, 0, 0);
 				break;
-				case CMD_SD_MSG_RECV:
+				case CMD_50_SD_MSG_RECV:
 				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,7,100);
-				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received SD command: From: %x.%x.%x To: %x.%x.%x Flags: %x Cmd1: %x Cmd2: %x",mybuf2[2],mybuf2[3],mybuf2[4],mybuf2[5],mybuf2[6],mybuf2[7],mybuf[8],mybuf2[9],mybuf2[10]);
+				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received SD command - From: %X.%X.%X To: %X.%X.%X Flags: %X Cmd1: %X Cmd2: %X (%s)",mybuf2[2],mybuf2[3],mybuf2[4],mybuf2[5],mybuf2[6],mybuf2[7],mybuf2[8],mybuf2[9],mybuf2[10],DCE::IOUtils::FormatHexBuffer(mybuf2,11).c_str());
+				switch(mybuf2[9]){
+				case 0x3b:
 				break;
-				case CMD_SEND_SD_ED_MSG:
-				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,7,100);
-				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Previous command status: %x",mybuf2[8]);
-				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Cmd: %x Address: %x.%x.%x",mybuf2[6],mybuf2[2],mybuf2[3],mybuf2[4]);
-				if (mybuf2[8] == 0x15) {
-					DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Previous command failed");
 				}
 				break;
-				case CMD_GET_NEXT_ALL_LINK_RECORD:
+				case CMD_62_SEND_SD_ED_MSG:
+				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,7,100);
+				//DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Previous command status: %X",mybuf2[8]);
+				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received CMD %1X Response. Address: %X.%X.%X (%s)",mybuf2[6],mybuf2[2],mybuf2[3],mybuf2[4],DCE::IOUtils::FormatHexBuffer(mybuf2,9).c_str());
+				//if (mybuf2[8] == 0x15) {
+				//	DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Previous command failed");
+				//}
+				break;
+				case CMD_6A_GET_NEXT_ALL_LINK_RECORD:
 				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,1,100);
 				if (mybuf2[2] == 0x06) {
 					DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"ALL-Link database done downloading");
 				}
 				break;
+				default:
+				// unhandled command. assume its SD and read in some data to display
+				len2 +=ReadSerialStringEx(serialPort,mybuf2+2,7,100);
+				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Received unhandled command: %s (read %d bytes)",DCE::IOUtils::FormatHexBuffer(mybuf2,len2).c_str(),len2);
 			}
 		} else {
 			// nothing received, let's see if there is a job to send
 			pthread_mutex_lock (&mutexSendQueue);
 			if (InsteonSendQueue.size()>0) {
-				DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Sending command: %x", InsteonSendQueue.front()->buffer[1]);
-				//Send Job
-				WriteSerialStringEx(serialPort,InsteonSendQueue.front()->buffer, InsteonSendQueue.front()->len);
-				//Pop if we only need to send it once (add logic)
-				InsteonSendQueue.pop_front();
+				if ( await_ack != 1 && await_callback == 0) {
+					await_callback = (unsigned int) InsteonSendQueue.front()->callbackid;
+
+DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON, "Sending job %p (cb %i) - %s (%d jobs in queue)",InsteonSendQueue.front(),await_callback,DCE::IOUtils::FormatHexAsciiBuffer(InsteonSendQueue.front()->buffer, InsteonSendQueue.front()->len,"31").c_str(),InsteonSendQueue.size());
+					callback_type = (unsigned int) InsteonSendQueue.front()->callback_type;
+//					DCE::LoggerWrapper::GetInstance()->Write(LV_INSTEON,"Sending command: %x", InsteonSendQueue.front()->buffer[1]);
+					//Send Job
+					WriteSerialStringEx(serialPort,InsteonSendQueue.front()->buffer, InsteonSendQueue.front()->len);
+					//Pop if we only need to send it once (add logic)
+					InsteonSendQueue.front()->sendcount++;
+					await_ack = 1;
+					timer=0;
+				} else {
+					timer++;
+					if (timer > 30 && await_callback != 0) {
+						DCE::LoggerWrapper::GetInstance()->Write(LV_WARNING, "No callback received: await_callback: %i timer: %i",await_callback,timer);
+						timer=0;
+						// resend, we got no final callback
+						await_ack = 0;
+						await_callback = 0;
+						if (InsteonSendQueue.front()->sendcount > 2) {
+							DCE::LoggerWrapper::GetInstance()->Write(LV_CRITICAL, "ERROR: Dropping command, no callback received after three resends");
+							dropSendQueueJob();
+						}
+					}
+				}
+			} else {
+				idletimer++;
 			}
 			pthread_mutex_unlock (&mutexSendQueue);
 		}
@@ -191,15 +257,31 @@ bool InsteonAPI::InsteonAPI::sendFunction(char *buffer, size_t length, int type,
 {
 	
 	InsteonJob *newJob = new InsteonJob;
-	unsigned int i = 0;
+	unsigned int i = 0, index = 0;
 	
 	
+	newJob->await_response = response && (type == REQUEST);
+	newJob->sendcount = 0;
+
+	newJob->buffer[index++] = IM_02_CMD_START;
+//	newJob->buffer[index++] = length + 2 + (response ? 1 : 0);
+//	newJob->buffer[index++] = type == RESPONSE ? RESPONSE : REQUEST;
 	//Fill in remainder of command		
 	for (i = 0; i < length;i++) {
-		newJob->buffer[i ] = buffer[i];
+		newJob->buffer[index++] = buffer[i];
 	}
-	newJob->len = length;
+	newJob->len = length+1;// + 4 + (response ? 1 : 0);
 	pthread_mutex_lock (&mutexSendQueue);
+	if (response) {
+		if (callbackpool>255 || callbackpool==0) { callbackpool=1; }
+		newJob->buffer[index++] = callbackpool;
+		newJob->callbackid = callbackpool++;
+		newJob->callback_type = (unsigned int)newJob->buffer[3];
+	} else {
+		newJob->callbackid = 0;
+		newJob->callback_type = 0;
+	}
+	newJob->buffer[index] = checksum(newJob->buffer+1,length+2+( response ? 1 : 0) );
 	InsteonSendQueue.push_back(newJob);
 	pthread_mutex_unlock (&mutexSendQueue);
 	return true;
@@ -209,7 +291,7 @@ bool InsteonAPI::InsteonAPI::setLightOn (char* id, unsigned char level)
 {
 	char cmd[128];
 	
-	cmd[0] = CMD_SD_LIGHT_ON;
+	cmd[0] = CMD_11_SD_LIGHT_ON;
 	cmd[1] = level; 
 	sendSDMsg(id, cmd);
 	return 0;
@@ -219,7 +301,7 @@ bool InsteonAPI::InsteonAPI::setLightOff (char* id)
 {
 	char cmd[128];
 	
-	cmd[0] = CMD_SD_LIGHT_OFF;
+	cmd[0] = CMD_13_SD_LIGHT_OFF;
 	cmd[1] = 0x00; 
 	sendSDMsg(id, cmd);
 	return 0;
@@ -230,25 +312,56 @@ bool InsteonAPI::InsteonAPI::sendSDMsg(char* id, char* msg)
 	char sdBuffer[128];
 	
 	//First Byte of Command to PLM is always 0x02
-	sdBuffer[0] = IM_CMD_START;
+//	sdBuffer[0] = IM_02_CMD_START;
 	
 	//For a Standard-Length Message send, the second byte is 0x62
-	sdBuffer[1] = CMD_SEND_SD_ED_MSG;
+	sdBuffer[0] = CMD_62_SEND_SD_ED_MSG;
 	
 	//Fill in Target Address
 	for (int i = 0; i < 3; i++)
 	{
-		sdBuffer[i+2] = id[i];
+		sdBuffer[i+1] = id[i];
 	}
 	
 	//set to Direct Message, SD length, 3 resends
-	sdBuffer[5] = 0x0F;
+	sdBuffer[4] = 0x0F;
 	
-	sdBuffer[6] = msg[0];
-	sdBuffer[7] = msg[1];
+	sdBuffer[5] = msg[0];
+	sdBuffer[6] = msg[1];
 	
-	sendFunction(sdBuffer,8,0,0);
+	sendFunction(sdBuffer,7,0,0);
 	return 0;
 }
 	
+char InsteonAPI::InsteonAPI::checksum(char* buf, int len)
+{
+return 0;
+}
+
+void InsteonAPI::InsteonAPI::dropSendQueueJob()
+{
+	// pthread_mutex_lock (&mutexSendQueue);
+	InsteonSendQueue.pop_front();
+	// pthread_mutex_unlock (&mutexSendQueue);
+	// await_callback = 0;
+	dropped_jobs++;
+	if( dropped_jobs >= 6 ) {
+		// If you get this error, take a look at Trac ticket #874
+		DCE::LoggerWrapper::GetInstance()->Write(LV_CRITICAL, "ERROR: Three dropped commands in a row, soft-resetting controller");
+		pthread_mutex_unlock (&mutexSendQueue);
+		//zwSoftReset();
+		pthread_mutex_lock (&mutexSendQueue);
+		dropped_jobs = 0;
+	}
+}
+
+void InsteonAPI::InsteonAPI::sendNextCommand()
+{
+	pthread_mutex_lock (&mutexSendQueue);
+	InsteonSendQueue.pop_front();
+	pthread_mutex_unlock (&mutexSendQueue);
+	await_ack = 0;
+	await_callback = 0;
+	dropped_jobs = 0;
+}
 	
--- src/Insteon_PLM_DCE/InsteonAPI.h
+++ src/Insteon_PLM_DCE/InsteonAPI.h
@@ -2,61 +2,70 @@
 #define INSTEONAPI_H_
 
 // Misc data bytes
-#define IM_CMD_START		0x02
-#define CFG_BIT_MONITOR_MODE	0x40 // 00100000
+#define ACK                             0x06
+#define NAK                             0x15
+#define REQUEST                         0x0
+#define RESPONSE                        0x1
+#define IM_02_CMD_START		0x02
+#define CFG_40_BIT_MONITOR_MODE	0x40
+
 
 // Commands received from IM
-#define CMD_SD_MSG_RECV		0x50
-#define CMD_ED_MSG_RECV		0x51
-#define CMD_X10_RECV		0x52
-#define CMD_ALL_LINK_CMPL	0x53
-#define CMD_BUTTON_PRESSED	0x54
-#define CMD_USER_RESET_DETECT	0x55
-#define CMD_ALL_LINK_CLEAN_FAIL	0x56
-#define CMD_GOT_ALL_LINK_RECORD	0x57
-#define CMD_ALL_LINK_CLEAN_STATUS	0x58
+#define CMD_50_SD_MSG_RECV		0x50
+#define CMD_51_ED_MSG_RECV		0x51
+#define CMD_52_X10_RECV			0x52
+#define CMD_53_ALL_LINK_CMPL		0x53 
+#define CMD_54_BUTTON_PRESSED		0x54
+#define CMD_55_USER_RESET_DETECT	0x55
+#define CMD_56_ALL_LINK_CLEAN_FAIL	0x56
+#define CMD_57_GOT_ALL_LINK_RECORD	0x57
+#define CMD_58_ALL_LINK_CLEAN_STATUS	0x58
 
 // Commands sent to IM
-#define CMD_GET_IM_INFO 	0x60
-#define CMD_SEND_ALL_LINK	0x61
-#define CMD_SEND_SD_ED_MSG	0x62
-#define CMD_SEND_X10		0x63
-#define CMD_START_ALL_LINK	0x64
-#define CMD_CANCEL_ALL_LINK	0x65
-#define CMD_SET_HOST_CAT	0x66
-#define CMD_RESET_IM		0x67
-#define CMD_GET_FIRST_ALL_LINK_RECORD	0x69
-#define CMD_GET_NEXT_ALL_LINK_RECORD	0x6A
-#define CMD_SET_IM_CONFIG	0x6B
-#define CMD_GET_IM_CONFIG	0x73
+#define CMD_60_GET_IM_INFO 	0x60
+#define CMD_61_SEND_ALL_LINK	0x61
+#define CMD_62_SEND_SD_ED_MSG	0x62 
+#define CMD_63_SEND_X10		0x63
+#define CMD_64_START_ALL_LINK	0x64
+#define CMD_65_CANCEL_ALL_LINK	0x65
+#define CMD_66_SET_HOST_CAT	0x66
+#define CMD_67_RESET_IM		0x67
+#define CMD_69_GET_FIRST_ALL_LINK_RECORD	0x69
+#define CMD_6A_GET_NEXT_ALL_LINK_RECORD		0x6A
+#define CMD_6B_SET_IM_CONFIG	0x6B
+#define CMD_73_GET_IM_CONFIG	0x73
 
 // Standard messages
-#define CMD_SD_PROD_DATA_REQ	0x03
-#define CMD_SD_PING		0x10
-#define CMD_SD_ID_REQUEST	0x0F
-#define CMD_SD_LIGHT_ON 	0x11
-#define CMD_SD_LIGHT_ON_FAST 	0x12
-#define CMD_SD_LIGHT_OFF 	0x13
-#define CMD_SD_LIGHT_OFF_FAST 	0x14
-#define CMD_SD_LIGHT_BRIGHTEN	0x15
-#define CMD_SD_LIGHT_DIM	0x16
-#define CMD_SD_LIGHT_GET_STATUS	0x19
+#define CMD_03_SD_PROD_DATA_REQ	0x03
+#define CMD_0D_SD_GET_VERSION	0x0D
+#define CMD_0F_SD_PING		0x0F
+#define CMD_10_SD_ID_REQUEST	0x10
+#define CMD_11_SD_LIGHT_ON 	0x11
+#define CMD_12_SD_LIGHT_ON_FAST 	0x12
+#define CMD_13_SD_LIGHT_OFF 		0x13
+#define CMD_14_SD_LIGHT_OFF_FAST 	0x14
+#define CMD_15_SD_LIGHT_BRIGHTEN	0x15
+#define CMD_16_SD_LIGHT_DIM		0x16
+#define CMD_19_SD_LIGHT_GET_STATUS	0x19
+
+// Extended messages (set extended message flag and pad with 0x00)
+#define CMD_2F_ED_RW_ALL_LINK_DB	0x2F
 
 // Device categories
-#define CAT_LIGHT_SWITCH_DIMMABLE	0x01
-#define CAT_LIGHT_ON_OFF		0x02
-#define CAT_NEWORK_BRIDGE		0x03
-#define CAT_IRRIGATION_CONTROL		0x04
-#define CAT_CLIMATE_CONTROL		0x05
-#define CAT_POOL_SPA_CONTROL		0x06
-#define CAT_SENSORS_ACTUATORS		0x07
-#define CAT_SECURITY_HEALTH_SAFETY	0x10
-#define CAT_SURVEILLANCE		0x11
-#define CAT_AUTOMOTIVE			0x12
-#define CAT_PET_CARE			0x13
-#define CAT_TOYS			0x14
-#define CAT_TIMEKEEPING			0x15
-#define CAT_HOLIDAY			0x16
+#define CAT_01_LIGHT_SWITCH_DIMMABLE	0x01
+#define CAT_02_LIGHT_ON_OFF		0x02
+#define CAT_03_NEWORK_BRIDGE		0x03
+#define CAT_04_IRRIGATION_CONTROL	0x04
+#define CAT_05_CLIMATE_CONTROL		0x05
+#define CAT_06_POOL_SPA_CONTROL		0x06
+#define CAT_07_SENSORS_ACTUATORS	0x07
+#define CAT_10_SECURITY_HEALTH_SAFETY	0x10
+#define CAT_11_SURVEILLANCE		0x11
+#define CAT_12_AUTOMOTIVE		0x12
+#define CAT_13_PET_CARE			0x13
+#define CAT_14_TOYS			0x14
+#define CAT_15_TIMEKEEPING		0x15
+#define CAT_16_HOLIDAY			0x16
 
 #include <deque>
 
@@ -68,6 +77,9 @@ namespace InsteonAPI
 	size_t len;
 	time_t timeout;
 	int sendcount;
+	int callbackid;
+	int callback_type;
+	bool await_response;
     };
 
 	class InsteonAPI 
@@ -94,6 +106,24 @@ namespace InsteonAPI
 		// mutex to lock out command queue
 		pthread_mutex_t mutexSendQueue;
 		
+		// counter to get a unique callback id
+		int callbackpool;
+
+		// set true when we await an ACK from the dongle, influences state machine
+		bool await_ack;
+		// same for callback
+		int await_callback;
+		// callback type temp var for state handling
+		int callback_type;
+
+		// Number of consecutive dropped jobs
+		int dropped_jobs;
+		void dropSendQueueJob();
+		void sendNextCommand();
+
+		// polling state
+		bool poll_state;
+
 	public:
 		
 		InsteonAPI(void* myInsteon);
@@ -101,6 +131,9 @@ namespace InsteonAPI
 		// opens the serial port and starts the initalization of the insteon device
 		void *init(const char *device);
 		
+		// calculate a xor checksum for the zwave frame
+		char checksum(char *buf, int len);
+
 		// Initiate a download of the ALL-Link database
 		void getDatabase();
 		
