e89e372c52bbc053278513e4308a5c8b839e74e8
foxi352 <serge@swa.lu>
2011-09-07 13:50:56 +0000

    Refs # 1218 - French translation for rooms setup

---
+++ web/lmce-admin/languages/French/rooms.lang.php
@@ -0,0 +1,18 @@
+<?
+$TEXT_ROOMS_CONST='Pièces';
+$TEXT_MANUALY_CONFIGURE_EA_CONFIRMATION_CONST='Êtes vous sûre de vouloir manuellement configurer la zonde de divertissement pour la pièce';
+$TEXT_ROOM_DESCRIPTION_CONST='Description de la pièce';
+$TEXT_ROOM_TYPE_CONST='Type de la pièce';
+$TEXT_DONT_SHOW_ROOM_ON_ORBITERS_CONST='Cacher pièce des orbiters';
+$TEXT_ENTERTAIN_AREAS_CONST='Zones de divertissement';
+$TEXT_DELETE_AREA_CONST='Supprimer zone';
+$TEXT_ADD_EA_CONST='Ajouter ZD';
+$TEXT_ROOM_DESCRIPTION_REQUIRED_CONST='Veuillez entrer une description';
+$TEXT_ADD_ROOM_CONST='Ajouter pièce';
+$TEXT_PICTURES_RESTRICTION_WARNING_CONST='Photos acceptées en format JPG ou PNG; Elles seront redimensionnées à 160x160 px si pas déjà dans ce format';
+$TEXT_SAVED_CONST='Sauvegardé';
+$TEXT_DELETE_ROOM_CONST='Supprimer pièce';
+$TEXT_MANUALLY_CONFIGURE_CONST='Configurer manuellement.';
+$TEXT_DELETE_ROOM_CONFIRMATION_CONST='Voulez-vous vraiement supprimer cette pièce ? Dispositifs dans cette pièce:';
+$TEXT_FLOORPLAN_OBJECT_TYPE_CONST='Type d\'objet sur le plan d\'étage';
+?>
