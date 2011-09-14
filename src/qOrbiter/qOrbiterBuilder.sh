fa4507476e5f115bb7ec2e7bc1cd46eee77969d3
posde <posde@theinternet.de>
2011-09-14 06:55:52 +0000

    Refs #1108 - Added comments to the qOrbiterBuilder script and putting all configs at the beginning of the script.

--- src/qOrbiter/qOrbiterBuilder.sh
+++ src/qOrbiter/qOrbiterBuilder.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 #
+# buildenv must point to an existing QtSDK installation containing, among other
+# 	things the qmake bin.
 #
+# spec must contain the target spec for the binary.
+#
+# buildoutput is the directory specified in the qOrbiter_src.pro.user file for
+#	the shadow builds.
 #
 buildenv=~/QtSDK/Desktop/Qt/4.8.0/gcc/bin
 spec=linux-g++-64
 srcroot=~/lmce-1004
 projectdir=$srcroot/qOrbiter/qOrbiter_src
 buildoutput=$srcroot/qOrbiter/build-output
+config=CONFIG+=debug CONFIG+=for_desktop
+
 mkdir -p $buildoutput
-$buildenv/qmake $projectdir/qOrbiter_src.pro -r -spec $spec CONFIG+=debug CONFIG+=for_desktop
+
+echo "Starting the QMake process"
+pushd $buildenv
+$buildenv/qmake $projectdir/qOrbiter_src.pro -r -spec $spec $config
 cd $buildoutput
+echo "Starting the actual build process"
+make clean
 make -w
+popd
