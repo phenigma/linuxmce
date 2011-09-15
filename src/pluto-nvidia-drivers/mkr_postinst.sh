0f72116160545a9a6c7913023532d2b0e21f55f8
murdock <ryan@montysplace.org>
2011-09-15 00:17:56 +0000

    Refs #1226 - updated nvidia install script to leverage the pluto nvidia installer script

--- src/pluto-nvidia-drivers/mkr_postinst.sh
+++ src/pluto-nvidia-drivers/mkr_postinst.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+source /usr/pluto/bin/nvidia-install.sh
 . /usr/pluto/bin/Utils.sh
 
 /usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
 
-mkdir -p /etc/X11
-bash -x /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee-pluto /var/log/pluto/Xconfigure.log
+if [ ! -f /etc/X11 ]; then
+	mkdir -p /etc/X11
+fi
+
+# install the correct nvidia driver
+installCorrectNvidiaDriver
+
+#Reconfigure X with the updated video driver
+bash /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee-pluto /var/log/pluto/Xconfigure.log
