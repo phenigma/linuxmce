17d8cf43a8761c31208fb442e053d02594a7613a
murdock <ryan@montysplace.org>
2011-09-17 03:17:35 +0000

    Refs #1226 - move nvidia setup to fix installer

--- src/pluto-nvidia-drivers/mkr_postinst.sh
+++ src/pluto-nvidia-drivers/mkr_postinst.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source /usr/pluto/bin/nvidia-install.sh
 . /usr/pluto/bin/Utils.sh
 
 /usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
@@ -9,8 +8,6 @@ if [ ! -f /etc/X11 ]; then
 	mkdir -p /etc/X11
 fi
 
-# install the correct nvidia driver
-installCorrectNvidiaDriver
 
 #Reconfigure X with the updated video driver
 bash /usr/pluto/bin/Xconfigure.sh --update-video-driver --keep-resolution | tee-pluto /var/log/pluto/Xconfigure.log
