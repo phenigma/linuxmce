abb11a345b37e688d6233210c4f0d5ecedf3a89b
posde <posde@theinternet.de>
2011-09-17 22:10:05 +0000

    Refs #1243 - Add latest version of chan_sccp-b to our repo including debian packaging created by the original project, but not distributed with the regular tarball

---
+++ ubuntu/asterisk/Chan_SCCP-3.0.4_STABLE/src/sccp_adv_features.h
@@ -0,0 +1,20 @@
+
+/*!
+ * \file 	sccp_adv_features.h
+ * \brief 	SCCP Advanced Features Headers
+ * \author 	Sergio Chersovani <mlists [at] c-net.it>
+ * \date
+ * \note	Reworked, but based on chan_sccp code.
+ *        	The original chan_sccp driver that was made by Zozo which itself was derived from the chan_skinny driver.
+ *        	Modified by Jan Czmok and Julien Goodwin
+ * \note        This program is free software and may be modified and distributed under the terms of the GNU Public License. 
+ *		See the LICENSE file at the top of the source tree.
+ * 
+ * $Date: 2011-08-01 20:24:03 +0200 (Mon, 01 Aug 2011) $
+ * $Revision: 2679 $
+ */
+
+#define DUMMY 1
+
+void dummyproc1(void);
+void dummyproc2(void);
