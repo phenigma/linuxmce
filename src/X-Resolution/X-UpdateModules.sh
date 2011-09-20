33f126bcda96c565a5db3b697fad4742a9d7a220
murdock <ryan@montysplace.org>
2011-09-20 12:58:45 +0000

    Refs #1226 - Use the same check in X-UpdateModules.sh for drivers

--- src/X-Resolution/X-UpdateModules.sh
+++ src/X-Resolution/X-UpdateModules.sh
@@ -31,7 +31,7 @@ XModulesActive=($(awk -f/usr/pluto/bin/X-ModulesSection.awk "$ConfigFile"))
 
 # if driver is 'nvidia', disable the 'dri' module
 # nVidia implements its own DRI, and the X DRI module causes big problems with the nVidia drivers - vox populi
-if [[ "$Driver" == nvidia ]]; then
+if lshwd | grep -qi 'VGA .* (nv)'; then
 	XModulesRemove=("${XModulesRemove[@]}" dri)
 else
 	# all other cards will load the 'dri' module
