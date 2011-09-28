180fa2dcd0050c8e76054466622fe606da6a1800
merkur2k <merkur2k@gmail.com>
2011-09-28 00:18:09 +0000

    refs #1220 - reworked log follow code

--- web/lmce-admin/operations/logs/taillog.php
+++ web/lmce-admin/operations/logs/taillog.php
@@ -1,16 +1,36 @@
-<?
-$logname=$_REQUEST['log'];
-if(!ereg('/var/log/pluto/',$logname)){
-	die('<pre style="background:black;color:white;">Access denied to '.$logname.'</pre>');
-}
-?>
+<?php
+//session_start();
 
-<pre style="background:black;color:white;">
-<?
-print $logname.'<br><br>';
-if($logname!=''){
-	$cmd='tail -f '.$logname.' | /usr/pluto/bin/ansi2html';
-	system($cmd);
+if ($_REQUEST['log']) {
+	$logname=$_REQUEST['log'];
+	$_SESSION['log'] = $logname;
+} else {
+	$logname = $_SESSION['log'];
 }
+
+if(!ereg('/var/log/pluto/',$logname) || !is_readable($logname)){
+        die('<pre style="background:black;color:white;">Access denied to '.$logname.'</pre>');
+}
+
+/**
+ * Require the library
+ */
+require '../../include/PHPTail.php';
+/**
+ * Initilize a new instance of PHPTail
+ * @var PHPTail
+ */
+$tail = new PHPTail($logname);
+
+/**
+ * We're getting an AJAX call
+ */
+if(isset($_GET['ajax']))  {
+	echo $tail->getNewLines($_GET['lastsize'], $_GET['grep'], $_GET['invert']);
+	die();
+}
+/**
+ * Regular GET/POST call, print out the GUI
+ */
+$tail->generateGUI();
 ?>
-</pre>
