611f62e8e91f15d1a33233df335a2976853747c1
foxi352 <serge@swa.lu>
2011-10-19 11:05:59 +0000

    Refs #1303 - Add sqlCVS link to sqlCVS announces as requested by sambuca

--- src/sqlCVS/sqlCVS-postcommit.sh
+++ src/sqlCVS/sqlCVS-postcommit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# $1 = batchID, $2 = repo, $3 = user, $4 = comment
 echo "sqlCVS-postcommit hook running"
-MESSAGE="sqlCVS commit to repo $2 by $3 batch $1 - $4"
+URL="http://schema.linuxmce.org/sqlCVS/?site=batch/detail&repository=$2&batchid=$1"
+URL=$(perl -MURI::Escape -e 'print uri_escape("'$URL'");')
+MESSAGE="sqlCVS commit to repo $2 by $3 batch $1 - $4 ($URL)"
 echo "Send to Golem: $MESSAGE"
 wget "http://vt100.at/announce.php?text=$MESSAGE" -o /dev/null -O /dev/null
