dbb73c93fc58a0a72aa6e6af0fa994bd0408ed39
foxi352 <serge@swa.lu>
2011-12-31 08:26:18 +0000

    Refs #1381 - Adding postinst script from original EIB to EIB-eibd. Adding new upstart eibd job.

---
+++ src/EIB-eibd/mkr_postinst.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+/usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
+
+. /usr/pluto/bin/Config_Ops.sh
+. /usr/pluto/bin/SQL_Ops.sh
+
+Q='CREATE DATABASE IF NOT EXISTS `eib`'
+RunSQL "$Q"
+
+UseDB eib
+Q='CREATE TABLE IF NOT EXISTS `groupaddresses` (
+	`name` varchar(100) default NULL,
+	`address` varchar(100) NOT NULL,
+	PRIMARY KEY  (`address`)
+)'
+RunSQL "$Q"
+
+# add eib user
+adduser --system --home /var/run/eib eib || true
+
+Q="GRANT ALL PRIVILEGES ON eib.* to 'eib'@'localhost'; FLUSH PRIVILEGES"
+RunSQL "$Q"
