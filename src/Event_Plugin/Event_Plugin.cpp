1785b912625252a67a279219cf12005d6071ba50
posde <posde@theinternet.de>
2012-05-01 17:37:18 +0000

    Refs #1460 - First try with Eventhandler Reload - doesn't seem to work. Maybe sambuca has some insight

--- src/Event_Plugin/Event_Plugin.cpp
+++ src/Event_Plugin/Event_Plugin.cpp
@@ -602,6 +602,7 @@ void Event_Plugin::CMD_Toggle_Event_Handler(int iPK_EventHandler,string &sCMD_Re
 	}
 	pRow_EventHandler->Disabled_set( pRow_EventHandler->Disabled_get()!=1 );
 	m_pDatabase_pluto_main->EventHandler_get()->Commit();
+	pRow_EventHandler->Reload();
 }
 //<-dceag-createinst-b->!
 
