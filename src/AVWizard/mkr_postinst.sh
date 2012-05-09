e8f9efdf93fa2f188883bace3aa71c9fcb579827
L3mce <l3droid@gmail.com>
2012-05-09 04:57:57 +0000

    Refs #1414 ignorant mistake on installer for removal of 0start_avwizard

--- src/AVWizard/mkr_postinst.sh
+++ src/AVWizard/mkr_postinst.sh
@@ -4,7 +4,7 @@
 update-rc.d check_avwizard start 98 S . >/dev/null
 update-rc.d a0start_avwizard start 99 2 . >/dev/null 
 	if [[ -e /etc/init.d/0start_avwizard ]]; then 
-		update-rc.d -f remove 0start_avwizard 
+		update-rc.d -f 0start_avwizard remove 
 		rm /etc/init.d/0start_avwizard 
 	fi 
 #<-mkr_B_via_e->
