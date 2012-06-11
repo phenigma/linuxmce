0b955d641bfe6e66cb5dcbd17772820f8445e732
posde <posde@theinternet.de>
2012-06-11 10:03:37 +0000

    Refs #164 - Generic PC Workstation package for debian

---
+++ src/Generic_PC_Workstation/debian/lmce-generic-pc-workstation.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-generic-pc-workstation initscript
+# sourced by /etc/init.d/lmce-generic-pc-workstation
+# installed at /etc/default/lmce-generic-pc-workstation by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/Generic_PC_Workstation/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-generic-pc-workstation):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-generic-pc-workstation" command="/usr/bin/lmce-generic-pc-workstation"
