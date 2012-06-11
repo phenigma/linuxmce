ca7e7b20ad94f9dd2f4f5b3d2270c754a719c2b3
posde <posde@theinternet.de>
2012-06-11 10:15:32 +0000

    Refs #164 - Adding debian dir to VDR plugin

---
+++ src/VDRPlugin/debian/pluto-vdr-plugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pluto-vdr-plugin initscript
+# sourced by /etc/init.d/pluto-vdr-plugin
+# installed at /etc/default/pluto-vdr-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
