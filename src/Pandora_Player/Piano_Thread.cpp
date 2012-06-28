ad68ca9fef3583c8d78ea52565a3615b5ad7eb92
tschak <thom.cherryhomes@gmail.com>
2012-06-28 21:08:04 +0000

    Refs #696 - remove pianobar code, as it is no longer relevant.

--- src/Pandora_Player/Piano_Thread.cpp
+++
@@ -1,8 +0,0 @@
-/** 
- * Piano_Thread - The C++ library which deals with libpiano
- * for the Pandora_Player
- * 
- * @version 1.0
- * @author Thomas Cherryhomes <thom.cherryhomes@gmail.com>
- *
- */
