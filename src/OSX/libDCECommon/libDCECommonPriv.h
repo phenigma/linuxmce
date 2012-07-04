33d07e1dddae627dc96ef274d2515de2092edac4
foxi352 <serge@swa.lu>
2012-07-04 21:20:16 +0000

    Refs #1143 - Make libDCECommon compile under OSX as dylib

---
+++ src/OSX/libDCECommon/libDCECommonPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  libDCECommonPriv.h
+ *  libDCECommon
+ *
+ *  Created by Serge Wagener on 11/6/11.
+ *  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class libDCECommonPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
