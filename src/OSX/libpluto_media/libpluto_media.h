3b599c9eb0a4372a35bba53af868bf4d09654ca3
foxi352 <serge@swa.lu>
2012-07-10 21:21:58 +0000

    Refs #1143 - licpluto_media for OSX

---
+++ src/OSX/libpluto_media/libpluto_media.h
@@ -0,0 +1,23 @@
+/*
+ *  libpluto_media.h
+ *  libpluto_media
+ *
+ *  Created by Serge Wagener on 7/10/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef libpluto_media_
+#define libpluto_media_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class libpluto_media
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ src/OSX/libpluto_media/libpluto_mediaPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  libpluto_mediaPriv.h
+ *  libpluto_media
+ *
+ *  Created by Serge Wagener on 7/10/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class libpluto_mediaPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
