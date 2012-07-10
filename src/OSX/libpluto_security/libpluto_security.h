646bbaf190a6442ecd97a21b9923772acd22fec7
foxi352 <serge@swa.lu>
2012-07-10 21:19:45 +0000

    Refs #1143 - licpluto_security for OSX

---
+++ src/OSX/libpluto_security/libpluto_security.h
@@ -0,0 +1,23 @@
+/*
+ *  libpluto_security.h
+ *  libpluto_security
+ *
+ *  Created by Serge Wagener on 7/10/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef libpluto_security_
+#define libpluto_security_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class libpluto_security
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ src/OSX/libpluto_security/libpluto_securityPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  libpluto_securityPriv.h
+ *  libpluto_security
+ *
+ *  Created by Serge Wagener on 7/10/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class libpluto_securityPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
