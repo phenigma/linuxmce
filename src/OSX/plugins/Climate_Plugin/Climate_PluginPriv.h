b3546de182438cbe8c8800960c32888449de9f36
foxi352 <serge@swa.lu>
2012-07-12 20:12:04 +0000

    Refs #1143 - Climate Plugin for OSX

---
+++ src/OSX/plugins/Climate_Plugin/Climate_PluginPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  Climate_PluginPriv.h
+ *  Climate_Plugin
+ *
+ *  Created by Serge Wagener on 7/12/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class Climate_PluginPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
