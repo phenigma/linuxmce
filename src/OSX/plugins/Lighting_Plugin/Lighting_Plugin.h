a78d262765f7d1e0c54dad353013e200ba12a879
foxi352 <serge@swa.lu>
2012-07-15 20:45:13 +0000

    Refs #1143 - Lighting_Plugin for OSX

---
+++ src/OSX/plugins/Lighting_Plugin/Lighting_Plugin.h
@@ -0,0 +1,23 @@
+/*
+ *  Lighting_Plugin.h
+ *  Lighting_Plugin
+ *
+ *  Created by Serge Wagener on 7/15/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef Lighting_Plugin_
+#define Lighting_Plugin_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class Lighting_Plugin
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ src/OSX/plugins/Lighting_Plugin/Lighting_PluginPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  Lighting_PluginPriv.h
+ *  Lighting_Plugin
+ *
+ *  Created by Serge Wagener on 7/15/12.
+ *  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class Lighting_PluginPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
