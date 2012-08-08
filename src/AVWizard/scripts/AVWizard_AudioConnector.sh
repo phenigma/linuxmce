e6a86299a68782a09199897357401ccaa27f85ae
radu <radu.cx+linuxmce@gmail.com>
2012-08-08 02:05:07 +0000

    Refs #1535 - Xine will be setup by SetupAudioVideo.sh - that also means there's just one place to change now, instead of two, if Xine settings need updating

--- src/AVWizard/scripts/AVWizard_AudioConnector.sh
+++ src/AVWizard/scripts/AVWizard_AudioConnector.sh
@@ -3,24 +3,19 @@
 . /usr/pluto/bin/AVWizard-Common.sh
 . /usr/pluto/bin/Utils.sh
 
-Param="$1"
-case "$Param" in
+AudioOption="$1"
+SoundCard="$2"
+
+AudioSetting=
+case "$AudioOption" in
 	'Analog Stereo')
-		XineConfSet audio.output.speaker_arrangement 'Stereo 2.0' "$XineConf"
-		XineConfSet audio.device.alsa_front_device plughw:0 "$XineConf"
-		XineConfSet audio.device.alsa_default_device plughw:0 "$XineConf"
+		AudioSetting=S
 	;;
 	'SPDIF Coaxial'|'SPDIF Optical')
-		XineConfSet audio.output.speaker_arrangement 'Pass Through' "$XineConf"
-		XineConfSet audio.device.alsa_front_device asym_spdif "$XineConf"
-		XineConfSet audio.device.alsa_default_device asym_spdif "$XineConf"
-		XineConfSet audio.device.alsa_passthrough_device "iec958:AES0=0x6,AES1=0x82,AES2=0x0,AES3=0x2" "$XineConf"
+		AudioSetting=O3
 	;;
 	'HDMI')
-		XineConfSet audio.output.speaker_arrangement 'Pass Through' "$XineConf"
-		XineConfSet audio.device.alsa_front_device asym_hdmi "$XineConf"
-		XineConfSet audio.device.alsa_default_device asym_hdmi "$XineConf"
-		XineConfSet audio.device.alsa_passthrough_device "hdmi:AES0=0x6,AES1=0x82,AES2=0x0,AES3=0x2" "$XineConf"
+		AudioSetting=H3
 	;;
 esac
-/usr/pluto/bin/SetupAudioVideo.sh
+/usr/pluto/bin/SetupAudioVideo.sh "$AudioSetting" "$SoundCard" /etc/pluto/xine.conf.avwizard
