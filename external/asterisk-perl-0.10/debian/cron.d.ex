d1a10e23cc44119eb2e77a6071a6aa63e4ccd519
tschak <thom.cherryhomes@gmail.com>
2012-09-02 06:47:23 +0000

    Refs #1554 - remove ancient asterisk* from external.

--- external/asterisk-perl-0.10/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the asterisk-perl package
-#
-0 4	* * *	root	asterisk-perl_maintenance
--- external/asterisk-perl-0.10/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(asterisk-perl):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="asterisk-perl" command="/usr/bin/asterisk-perl"
