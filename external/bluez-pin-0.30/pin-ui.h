ec5603cea7edf78a8e84595477fb20d2b421d862
tschak <thom.cherryhomes@gmail.com>
2012-09-02 06:49:43 +0000

    Refs #1554 remove ancient bluez

--- external/bluez-pin-0.30/pin-ui.h
+++
@@ -1,4 +0,0 @@
-typedef struct _BluetoothPinRequest BluetoothPinRequest;
-typedef struct _BluetoothPinRequestClass BluetoothPinRequestClass;
-
-BluetoothPinRequest *bluetooth_pin_request_new (gboolean outgoing, const gchar *address, const gchar *name);
