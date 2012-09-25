38c5ae88a19a6a00208b5d3e499b2f4e51131141
L3mce <l3droid@gmail.com>
2012-09-25 21:31:41 +0000

    Refs #1520 final StorageDevices_Radar.sh, all functions now work correctly. Mounting is no longer necessary to extract size. Core drive will not be detected. Aliases handled.

--- src/PlutoStorageDevices/StorageDevices_Radar.sh
+++ src/PlutoStorageDevices/StorageDevices_Radar.sh
@@ -1,81 +1,79 @@
 #!/bin/bash
-
-. /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/SQL_Ops.sh
 
-export PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin
-
-TPL_GENERIC_INTERNAL_DRIVE=1790
-DD_UUID=267
-
+availPath=""
+mountedDevPath=""
+mountedPath=""
+Hdds=""
+Hdd_DT="DT:1790"
+DD_Uuid="267"
+export PATH="$PATH:/sbin:/usr/sbin:/usr/local/sbin"
+shopt -u nullglob
 ## Function to remove undesired paths
-function substractPaths {
-	local availPath=$1
-	local unavailPath=$2
-	local retPath=""
-
-	for aPath in $availPath; do
-		available=1
-
-		for uPath in $unavailPath; do
-			if [[ "$aPath" == "$uPath" ]]; then
-				available=0
-			fi
-		done
-
-		if [[ $available == 1 ]]; then
-			retPath="$retPath $aPath"
-		fi
-	done
-
-	echo $retPath
+diff_Funk(){
+        awk 'BEGIN{RS=ORS=" "}
+        {NR==FNR?a[$0]++:a[$0]--}
+        END{for(k in a)if(a[k])print k}' <(echo -n "${!1}") <(echo -n "${!2}")
 }
 
-function Detect {
-	## Available Paths
-	availPath=$(ls /dev/disk/by-path)
+Detect() {
 
-	## Removes drive paths that aren't storage drives
-	Hdds=""
-	for Drive in $availPath; do
-		drivePathDevName=$(udevadm info --query=all --name="/dev/disk/by-path/$Drive" | grep 'N:' | awk '{ print $2 }') 
-		confirmDrive=$(echo $drivePathDevName | grep -E 'sd|hd|xd|md' | awk '{ print $1 }')
-		if [[ "$confirmDrive" == "" ]]; then continue; fi
+        ## Available Paths
+        availPath=$(find /dev/disk/by-path -name '*:*' -exec basename {} \;)
+        for Drive in $availPath; do
+                drivePathDevName=$(udevadm info --query=all --name="/dev/disk/by-path/${Drive}" | grep 'N:' | awk '{ print $2 }')
+                confirmDrive=$(echo "$drivePathDevName" | grep -E 'sd|hd|xd|md' | awk '{ print $1 }')
+                if [[ "$confirmDrive" == "" ]]; then continue; fi
+                Hdds="$Hdds $Drive"
+        done
+        availPath="$Hdds"
 
-		Hdds="$Hdds $Drive"
-	done
-	availPath=$Hdds
+        ## Looks for standard device names mounted in mtab and learns path. Note some mounted drives show only UUID
+        mountedDevName=$(cat /etc/mtab | awk '/dev\/(sd|hd|md|xd)./ {print $1}')
+        for Drive in $mountedDevName; do
+                mountedDevGrep=$(udevadm info --query=all --name=${Drive} | grep 'S: disk/by-path/' | awk -F/ '{print $NF}')
+                mountedDevPath="$mountedDevPath $mountedDevGrep"
+        done
 
-	## Looks for standard device names mounted in mtab and learns path. Note some mounted drives show only UUID
-	mountedDevName=$(cat /etc/mtab | awk '/dev\/(sd|hd|md|xd)./ {print $1}')
-	mountedDevPath=""
-	for Drive in $mountedDevName; do
-		mountedDevPath="$mountedDevPath $(udevadm info --query=all --name="$Drive" | grep 'S: disk/by-path/' | sed 's,disk/by-path/,,g' | awk '{ print $2 }')"
-	done
-	
-	## Grabs UUID of mounted devices in mtab and learns path. Note some mounted drives only show device names.
-	mountedUuid=$(cat /etc/mtab | awk '/uuid/ { print $1 }' | sed 's,/dev/disk/by-uuid/,,g' | sort -u)
-	mountedPath=""
-	for Drive in $mountedUuid; do
-		mountedPath="$mountedPath $(udevadm info --query=all --name="/dev/disk/by-uuid/$Drive" | grep 'S: disk/by-path/' | sed 's,disk/by-path/,,g' | awk '{ print $2 }')"
-	done
+        ## Grabs UUID of mounted devices in mtab and learns path. Note some mounted drives only show device names.
+        mountedUuid=$(cat /etc/mtab | awk '/uuid/ { print $1 }' | sort -u)
+        for Drive in $mountedUuid; do
+                mountedPathFind=$(udevadm info --query=all --name=${Drive} | grep 'S: disk/by-path/' | sed 's,disk/by-path/,,g' | awk '{ print $2 }')
+                mountedPath="$mountedPath $mountedPathFind"
+        done
 
-	## Remove the mounted paths
-	availPath=$(substractPaths "$availPath" "$mountedPath")
-	availPath=$(substractPaths "$availPath" "$mountedDevPath")
-
-	## Remove partitions if they're alias is mounted
-	for Path in $availPath; do
-		for path_alias in $(udevadm info --query=symlink --name="/dev/disk/by-path/$Path" | awk '{ print $4 }' | sed 's,disk/by-uuid/,,g'); do
-			for alias_mounted in $(mount | awk '/dev\/disk\// {print $1}' | sed 's/\/dev\///g'); do
-				if [[ "$path_alias" == "$alias_mounted" ]]; then
-					availPath=$(substractPaths "$availPath" "$Path")
-				fi
-			done
-		done
+	## Finds UUID of devices that have been told to be ignored.
+	Q="
+		SELECT
+			SerialNumber
+		FROM
+			UnknownDevices
+		WHERE
+			FK_Device_PC = '$PK_Device' AND VendorModelId = '$Hdd_DT'
+	"
+	uDrives=$(RunSQL "$Q")
+	for Drive in $uDrives; do
+		unknownPathFind=$(udevadm info --query=all --name=/dev/disk/by-uuid/${Drive} | grep 'S: disk/by-path/' | sed 's,disk/by-path/,,g' | awk '{ print $2 }')
+		unknownPath="$unknownPath $unknownPathFind"
 	done
 
-	## Remove unmountable path additions (swap/extended/boot sector)
+	## Remove the mounted paths or unavailable paths
+	availPath=($(diff_Funk availPath[@] unknownPath[@]))
+        availPath=($(diff_Funk availPath[@] mountedPath[@]))
+        availPath=($(diff_Funk availPath[@] mountedDevPath[@]))
+        availPath=$(echo "${availPath[@]}" | sort -u)
+
+        for Path in $availPath; do
+                for path_alias in $(udevadm info --query=symlink --name="/dev/disk/by-path/${Path}" | awk '{ print $4 }' | sed 's,disk/by-label,,g'); do
+                        for alias_mounted in $(mount | grep -wo "$path_alias"); do
+                                if [[ "$path_alias" == "$alias_mounted" ]]; then
+                                        availPath=($(diff_Funk availPath[@] Path[@]))
+                                        availPath=$(echo "${availPath[@]}" | sort -u)
+                                fi
+                        done
+                done
+        done
+
 	auxPath=""
 	for Path in $availPath; do
 		## If is extended partition
@@ -96,16 +94,16 @@ function Detect {
 	done
 	availPath=$auxPath
 
-	## Remove paths that belong to a mounted RAID
-	if [[ -x /sbin/mdadm ]]; then
-		auxPath=""
-		for Path in $availPath; do
-			if [[ "$(mdadm --examine /dev/disk/by-path/$Path 2>&1)" == *"No md superblock"* ]]; then
-				auxPath="$auxPath $Path"
-			fi
-		done
-		availPath="$auxPath"
-	fi
+	## Remove paths that belong to a mounted RAID 
+	if [[ -x /sbin/mdadm ]]; then 
+		auxPath="" 
+		for Path in $availPath; do 
+			if [[ "$(mdadm --examine /dev/disk/by-path/${Path} 2>&1)" == *"No md superblock"* ]]; then 
+				auxPath="$auxPath $Path" 
+			fi 
+		done 
+		availPath="$auxPath" 
+	fi 
 
 	## Test to see if we found any available paths
 	if [[ "$availPath" != "" ]]; then
@@ -115,12 +113,12 @@ function Detect {
 			#/usr/pluto/bin/MessageSend $DCERouter 0 $OrbiterIDList 1 741 159 228 109 "[$pathPosition]" 156 $PK_Device 163 "$InfoMessage"
 
 			## Get info about this volume
-			partition_diskname=$(udevadm info --query=all --name="/dev/disk/by-path/$pathPosition" | grep 'ID_MODEL=' | cut -d'=' -f2)
-			partition_serial=$(udevadm info --query=all --name="/dev/disk/by-path/$pathPosition" | grep 'ID_SERIAL_SHORT=' | cut -d'=' -f2)
-			partition_label=$(udevadm info --query=all --name="/dev/disk/by-path/$pathPosition" | grep 'ID_FS_LABEL=' | cut -d'=' -f2)
-			partition_uuid=$(udevadm info --query=all --name="/dev/disk/by-path/$pathPosition" | grep 'ID_FS_UUID=' | cut -d'=' -f2)
-			partition=$(udevadm info --query=all --name="/dev/disk/by-path/$pathPosition" | grep 'N:' | awk '{ print $2 }')
-                        
+			partition_diskname=$(udevadm info --query=all --name="/dev/disk/by-path/${pathPosition}" | grep 'ID_MODEL=' | cut -d'=' -f2)
+			partition_serial=$(udevadm info --query=all --name="/dev/disk/by-path/${pathPosition}" | grep 'ID_SERIAL_SHORT=' | cut -d'=' -f2)
+			partition_label=$(udevadm info --query=all --name="/dev/disk/by-path/${pathPosition}" | grep 'ID_FS_LABEL=' | cut -d'=' -f2)
+			partition_uuid=$(udevadm info --query=all --name="/dev/disk/by-path/${pathPosition}" | grep 'ID_FS_UUID=' | cut -d'=' -f2)
+			partition=$(udevadm info --query=all --name="/dev/disk/by-path/${pathPosition}" | grep 'N:' | awk '{ print $2 }')
+			partition_size=$(expr $(udevadm info -a -n "/dev/${partition}" | grep 'ATTR{size}' | sed 's/.*=//;s/"//g') / 2 )
 			## fallback to serial number if no UUID available (for instance FAT devices)
 			if [[ "$partition_uuid" == "" && "$partition_serial" != "" ]]; then
 			        partition_uuid="$partition_serial"
@@ -134,19 +132,38 @@ function Detect {
 			if [[ "$partition_uuid" == "" ]]; then
 				continue
 			fi
+			
+			## make sure drive is bigger than 0
+			if [[ "$partition_size" == "0" ]]; then
+				continue
+			fi
 
-			## Mount the partition to get it's size
-			mountpoint_temp=$(mktemp -d)
-			mount -o ro "/dev/$partition" "$mountpoint_temp"
-			partition_size=$(df -h "/dev/$partition" | tail -n +2 | awk '{ print $2 }')
-			umount -lf "$mountpoint_temp";  rmdir "$mountpoint_temp"
+			## Convert size to human readable
+			num_size=$(echo -n "$partition_size" | wc -m)
+			if [[ "$num_size" -lt "19" ]]; then
+				unit="EB"; divisor="1125899906842624"
+			fi
+			if [[ "$num_size" -lt "16" ]]; then
+				unit="PB"; divisor="1099511627776"
+			fi
+			if [[ "$num_size" -lt "13" ]]; then
+				unit="TB"; divisor="1073741824"
+			fi
+			if [[ "$num_size" -lt "10" ]]; then
+				unit="GB"; divisor="1048576"
+			fi
+			if [[ "$num_size" -lt "7" ]]; then
+				unit="MB"; divisor="1024"
+			fi
+			kb=$(printf '%.2f' $(echo "$partition_size / $divisor" | bc -l))
+			partition_size="$kb $unit"
 
 			## Sends data to MessageSend
 			thisHost=$(hostname)
 			Sent="false"
 			Count=0
 			while [[ "$Sent" == "false" ]]; do
-				/usr/pluto/bin/MessageSend $DCERouter $PK_Device -1001 2 65 55 "$DD_UUID|$partition_uuid|277|$partition_size" 54 "$partition_uuid" 52 8 49 1790 13 "$partition_size $partition_diskname [$partition] on $thisHost"
+				/usr/pluto/bin/MessageSend "$DCERouter" "$PK_Device" -1001 2 65 55 "${DD_Uuid}|${partition_uuid}|277|${partition_size}" 54 "$partition_uuid" 52 8 49 1790 13 "$partition_size [${partition}] $partition_diskname on $thisHost"
 				err=$?
 
 				if [[ "$err" == "0" ]]; then
@@ -162,6 +179,6 @@ function Detect {
 			done
 		done
 	fi
-}
 
+}
 Detect
