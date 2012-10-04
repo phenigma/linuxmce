bb7b60830638df228b73225183484c2c5ad191bb
L3mce <l3droid@gmail.com>
2012-10-04 18:25:31 +0000

    Refs #1586 Change to Diskless_Setup.sh to softlink to buntu created vmlinuz and initrd.img softlinks at MD root.

--- src/UbuntuDiskless/Diskless_Setup.sh
+++ src/UbuntuDiskless/Diskless_Setup.sh
@@ -71,8 +71,9 @@ function setup_tftp_boot
 		fi
 
         	mkdir -p /tftpboot/${Moon_DeviceID}
-        	ln -sf "$Kernel" /tftpboot/${Moon_DeviceID}/vmlinuz
-        	ln -sf "$Initrd" /tftpboot/${Moon_DeviceID}/initrd.img
+		# Changed to point to the always updated buntu softlink at device root. 
+        	ln -sf ${Moon_RootLocation}/vmlinuz /tftpboot/${Moon_DeviceID}/vmlinuz
+        	ln -sf ${Moon_RootLocation}/initrd.img /tftpboot/${Moon_DeviceID}/initrd.img
 
 		## these are needed because by default they see the kernel running on the core,
 		## which may be different from the one installed on the MD, thus not doing what we want by default
@@ -98,9 +99,9 @@ function setup_tftp_boot
                                 # and we try to install the correct version
                                 chroot "${Moon_RootLocation}" apt-get install "linux-image-${Kernel}"
                         fi
-                                                                                                                                                
-                        ln -s "${Moon_RootLocation}/boot/vmlinuz-${Kernel}" "/tftpboot/${Moon_DeviceID}/$Name/vmlinuz"
-			ln -s "${Moon_RootLocation}/boot/initrd.img-${Kernel}" "/tftpboot/${Moon_DeviceID}/$Name/initrd.img"
+			# Changed to point to the always updated buntu softlink at device root. 
+                        ln -s "${Moon_RootLocation}/vmlinuz" "/tftpboot/${Moon_DeviceID}/$Name/vmlinuz"
+			ln -s "${Moon_RootLocation}/initrd.img" "/tftpboot/${Moon_DeviceID}/$Name/initrd.img"
 		done
 
 		BootConf="${BootConf}DEFAULT Pluto\n"
