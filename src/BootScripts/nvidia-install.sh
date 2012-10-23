7710ba5d423189b870f726422f420c8fdb85a52b
L3mce <l3droid@gmail.com>
2012-10-23 21:02:30 +0000

    Refs #1564 Change to nvidia-install.sh to remove redundant pciids and only guess at Alternate sound config once on install

--- src/BootScripts/nvidia-install.sh
+++ src/BootScripts/nvidia-install.sh
@@ -19,7 +19,7 @@ LogFile="/var/log/pluto/nvidia-install.log";
 
 # DRIVER LISTS
 # Make lists of all PCI_Id's supported under each driver version
-Driver_Current_Supported="0974 0a26 0a27 0a7a 0dc0 0dcd 0dce 0dd2 0dd6 0de0 0de2 0de5 0deb 0dec 0df4 0df5 0df6 0df7 0dfa 0dfc 0e30 0e31 0e3a 0e3b 0fce 0fd1 0fd2 0fd3 0fd4 0fd5 0fd8 0fe0 0ffb 0ffc 0fff 1040 1042 1048 104a 1050 1051 1054 1055 1056 1057 107d 1082 1088 1091 1094 1096 109a 109b 10c5 1180 1188 1189 118f 11a0 1200 1201 1203 1210 1211 1241 1244 1245 1247 1251"
+Driver_Current_Supported="0974 0a26 0a27 0a7a 0dc0 0dcd 0dce 0dd2 0dd6 0df4 0df5 0df6 0df7 0dfa 0dfc 0e30 0e31 0e3a 0e3b 0fce 0fd1 0fd2 0fd3 0fd4 0fd5 0fd8 0fe0 0ffb 0ffc 0fff 1040 1042 1048 104a 1050 1051 1054 1055 1056 1057 107d 1082 1088 1091 1094 1096 109a 109b 10c5 1180 1188 1189 118f 11a0 1200 1201 1203 1210 1211 1241 1244 1245 1247 1251"
 
 ## These pciids are new, and may need to end up in nvidia-current. For now they are in 260. 
 # 0565 0568 0621 0630 0633 067a 0680 0840 0869 08a0 08a2 08a3 0a32 0ca5 0dc5 0dc6 0dd3 0dda 0de8 0de9 0dea 0ded 0def 0df9 0f00 0f01 0fc0 0fc1 
@@ -39,13 +39,14 @@ VDPAU_Rev_A_Supported="016d 0400 0401 0402 0403 0404 0405 0407 0408 0409 040a 04
 
 VDPAU_Rev_B_Supported="06e0 06e1 06e2 06e5 06e6 06e8 06e9 06ea 06eb 06ec 06ef 06f1 06f8 06f9 06fa 06fb 06fd 06ff 0840 0844 0845 0846 0847 0848 0849 084a 084b 084c 084d 0860 0861 0862 0863 0864 0865 0866 0867 0869 086a 086c 086d 086e 086f 0870 0871 0872 0873 0874 0876 087a 087d 087e 087f 0a68 0a69 10C0"
 
-VDPAU_Rev_C_Supported="0565 0568 067a 0680 06c0 06c4 06ca 06cd 06d1 06d2 06d8 06d9 06da 06dc 06dd 06de 06df 0772 082f 0846 08a0 08a2 08a3 08a4 08a5 0914 0974 0a20 0a23 0a26 0a27 0a28 0a29 0a2a 0a2b 0a2c 0a2d 0a32 0a34 0a35 0a38 0a3c 0a60 0a62 0a63 0a64 0a65 0a66 0a6a 0a6c 0a6e 0a6f 0a70 0a71 0a72 0a73 0a74 0a75 0a76 0a78 0a7a 0a7c 0ca2 0ca3 0ca4 0ca5 0ca8 0ca9 0caf 0cb0 0cb1 0cbc 0dc0 0dc4 0dc5 0dc6 0dcd 0dce 0dd1 0dd2 0dd3 0dd6 0dd8 0dda 0de0 0de1 0de2 0de4 0de5 0de8 0de9 0dea 0deb 0dec 0ded 0dee 0def 0df0 0df1 0df2 0df3 0df4 0df5 0df6 0df7 0df8 0df9 0dfa 0dfc 0e22 0e23 0e24 0e30 0e31 0e3a 0e3b 0f00 0f01 0fc0 0fc1 0fc2 1049 1050 1052 1058 1059 105a 1080 1081 1082 1084 1086 1087 1088 1089 108b 1091 1094 1096 109a 109b 10c5 10cc 10d8 1200 1201 1203 1205 1206 1207 1208 1210 1211 1212 1213 1241 1243 1244 1245 1246 1247 1248 1249 124b 124d 1251 20dd 212a 212b 212c"
+VDPAU_Rev_C_Supported="0565 0568 067a 0680 06c0 06c4 06ca 06cd 06d1 06d2 06d8 06d9 06da 06dc 06dd 06de 06df 0772 082f 08a0 08a2 08a3 08a4 08a5 0914 0974 0a20 0a23 0a26 0a27 0a28 0a29 0a2a 0a2b 0a2c 0a2d 0a32 0a34 0a35 0a38 0a3c 0a60 0a62 0a63 0a64 0a65 0a66 0a6a 0a6c 0a6e 0a6f 0a70 0a71 0a72 0a73 0a74 0a75 0a76 0a78 0a7a 0a7c 0ca2 0ca3 0ca4 0ca5 0ca8 0ca9 0caf 0cb0 0cb1 0cbc 0dc0 0dc4 0dc5 0dc6 0dcd 0dce 0dd1 0dd2 0dd3 0dd6 0dd8 0dda 0de0 0de1 0de2 0de4 0de5 0de8 0de9 0dea 0deb 0dec 0ded 0dee 0def 0df0 0df1 0df2 0df3 0df4 0df5 0df6 0df7 0df8 0df9 0dfa 0dfc 0e22 0e23 0e24 0e30 0e31 0e3a 0e3b 0f00 0f01 0fc0 0fc1 0fc2 1049 1050 1052 1058 1059 105a 1080 1081 1082 1084 1086 1087 1088 1089 108b 1091 1094 1096 109a 109b 10c5 10cc 10d8 1200 1201 1203 1205 1206 1207 1208 1210 1211 1212 1213 1241 1243 1244 1245 1246 1247 1248 1249 124b 124d 1251 20dd 212a 212b 212c"
 
 VDPAU_Rev_D_Supported="0fce 0fd1 0fd2 0fd3 0fd4 0fd5 0fd8 0fe0 0ffb 0ffc 0fff 1042 1048 104a 1051 1054 1055 1056 1057 107d 1180 1188 1189 118f 11a0"
 
 Backports_Required="0a20 0a22 0a23 0a28 0a29 0a2a 0a2b 0a2d 0a34 0a35 0a64 0a6f 0a76 0ca2 0ca3 0ca4 0ca8 0ca9 0caf 0cb0 0cb1 0dc4 0de0 0de1 0de2 0de4 0de5 0df0 0df1 0df2 0df3 0e22 0e23 0e24 1080 1081"
 
-GT_Series="0410 05e0 05e1 05e2 05e3 05e6 05ea 05eb 0600 0601 0602 0603 0605 0607 0608 0609 060a 060b 060c 060f 0611 0612 0613 0614 0615 0617 0618 0621 0622 0626 0627 0628 062a 062c 062d 062e 0631 0632 0637 0640 0641 0643 0646 0647 0649 064a 064c 0652 0653 0654 0633 065b 06c0 06c4 06ca 06cd 0a32 0ca0 0ca5 0ca7 0cac 0dc5 0dc6 0dd1 0dd3 0de8 0de9 0ded 0dee 0f00 0f01 0fc0 0fc1 0fc2 1049 1052 1084 1086 1087 1089 108b 0565 0568 067a 0680 20dd 1205 1206 1207 1208 1212 1213 1243 1246 212a 212b 212c 1248 1249 124b 124d 10cc"
+GT_Series="0410 0565 0568 05e0 05e1 05e2 05e3 05e6 05ea 05eb 0600 0601 0602 0603 0605 0607 0608 0609 060a 060b 060c 060f 0611 0612 0613 0614 0615 0617 0618 0621 0622 0626 0627 0628 062a 062c 062d 062e 0631 0632 0633 0637 0640 0641 0643 0646 0647 0649 064a 064c 0652 0653 0654 065b 067a 0680 06c0 06c4 06ca 06cd 0a32 0ca0 0ca5 0ca7 0cac 0dc5 0dc6 0dd1 0dd3 0de8 0de9 0ded 0dee 0f00 0f01 0fc0 0fc1 0fc2 1049 1052 1084 1086 1087 1089 108b 10cc 1205 1206 1207 1208 1212 1213 1243 1246 1248 1249 124b 124d 20dd 212a 212b 212c"
+
 
 #######################################################################################################################
 # getPCI_Id()
@@ -145,8 +146,8 @@ checkAlsaBackportNeeds() {
 			Log "$LogFile" "Alsa backports being installed."
 			NotifyMessage "Installing alsa backports modules."
 			apt-get install -yf linux-backports-modules-alsa-$(uname -r)
-			/usr/pluto/bin/RestartALSA.sh ;;
-			ConfSet "AlternateSC" "2"
+			ConfSet "AlternateSC" "2" 
+			/usr/pluto/bin/RestartALSA.sh;;
 		esac
 	fi
 
@@ -162,9 +163,6 @@ checkAlsaBackportNeeds() {
 			;;
 		esac
 	fi
-	case " $GT_Series " in *" $PCI_Id "*)
-		ConfSet "AlternateSC" "1"
-	esac
 }
 
 # installCorrectNvidiaDriver()
@@ -219,11 +217,13 @@ installCorrectNvidiaDriver() {
 			apt-get install -y "$preferred_driver" 2> >(tee "$tmpfile")
 			rm /etc/apt/sources.list.d/*
 			apt-get update
+			ConfSet "AlternateSC" "1"
 			local param="reboot"
 		else
 			apt-get install -y "$preferred_driver" 2> >(tee "$tmpfile")
 			local param="reboot"
 		fi
+
 		CountErr="0"
 		if [[ "$?" > "0" ]] ; then
 			ERROR=$( cat $tmpfile )
