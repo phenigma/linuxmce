b87c57049e768b968c922aa43519b907cd4b5e54
hari <hari@vt100.at>
2012-11-16 09:45:13 +0000

    refs #44 - initial stub for the OZW based Z-Wave interface

---
+++ src/ZWave-OZW/ZWave.cpp
@@ -0,0 +1,437 @@
+/*
+     Copyright (C) 2004 Pluto, Inc., a Florida Corporation
+
+     www.plutohome.com
+
+     Phone: +1 (877) 758-8648
+ 
+
+     This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+     See the GNU General Public License for more details.
+
+*/
+//<-dceag-d-b->
+#include "ZWave.h"
+#include "DCE/Logger.h"
+#include "PlutoUtils/FileUtils.h"
+#include "PlutoUtils/StringUtils.h"
+#include "PlutoUtils/Other.h"
+
+#include <iostream>
+using namespace std;
+using namespace DCE;
+
+#include "Gen_Devices/AllCommandsRequests.h"
+//<-dceag-d-e->
+
+//<-dceag-const-b->
+// The primary constructor when the class is created as a stand-alone device
+ZWave::ZWave(int DeviceID, string ServerAddress,bool bConnectEventHandler,bool bLocalMode,class Router *pRouter)
+	: ZWave_Command(DeviceID, ServerAddress,bConnectEventHandler,bLocalMode,pRouter)
+//<-dceag-const-e->
+{
+}
+
+//<-dceag-const2-b->
+// The constructor when the class is created as an embedded instance within another stand-alone device
+ZWave::ZWave(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter)
+	: ZWave_Command(pPrimaryDeviceCommand, pData, pEvent, pRouter)
+//<-dceag-const2-e->
+{
+}
+
+//<-dceag-dest-b->
+ZWave::~ZWave()
+//<-dceag-dest-e->
+{
+	
+}
+
+//<-dceag-getconfig-b->
+bool ZWave::GetConfig()
+{
+	if( !ZWave_Command::GetConfig() )
+		return false;
+//<-dceag-getconfig-e->
+
+	// Put your code here to initialize the data in this class
+	// The configuration parameters DATA_ are now populated
+	return true;
+}
+
+//<-dceag-reg-b->
+// This function will only be used if this device is loaded into the DCE Router's memory space as a plug-in.  Otherwise Connect() will be called from the main()
+bool ZWave::Register()
+//<-dceag-reg-e->
+{
+	return Connect(PK_DeviceTemplate_get()); 
+}
+
+/*  Since several parents can share the same child class, and each has it's own implementation, the base class in Gen_Devices
+	cannot include the actual implementation.  Instead there's an extern function declared, and the actual new exists here.  You 
+	can safely remove this block (put a ! after the dceag-createinst-b block) if this device is not embedded within other devices. */
+//<-dceag-createinst-b->
+ZWave_Command *Create_ZWave(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter)
+{
+	return new ZWave(pPrimaryDeviceCommand, pData, pEvent, pRouter);
+}
+//<-dceag-createinst-e->
+
+/*
+	When you receive commands that are destined to one of your children,
+	then if that child implements DCE then there will already be a separate class
+	created for the child that will get the message.  If the child does not, then you will 
+	get all	commands for your children in ReceivedCommandForChild, where 
+	pDeviceData_Base is the child device.  If you handle the message, you 
+	should change the sCMD_Result to OK
+*/
+//<-dceag-cmdch-b->
+void ZWave::ReceivedCommandForChild(DeviceData_Impl *pDeviceData_Impl,string &sCMD_Result,Message *pMessage)
+//<-dceag-cmdch-e->
+{
+	sCMD_Result = "UNHANDLED CHILD";
+}
+
+/*
+	When you received a valid command, but it wasn't for one of your children,
+	then ReceivedUnknownCommand gets called.  If you handle the message, you 
+	should change the sCMD_Result to OK
+*/
+//<-dceag-cmduk-b->
+void ZWave::ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage)
+//<-dceag-cmduk-e->
+{
+	sCMD_Result = "UNKNOWN COMMAND";
+}
+
+//<-dceag-sample-b->
+/*		**** SAMPLE ILLUSTRATING HOW TO USE THE BASE CLASSES ****
+
+**** IF YOU DON'T WANT DCEGENERATOR TO KEEP PUTTING THIS AUTO-GENERATED SECTION ****
+**** ADD AN ! AFTER THE BEGINNING OF THE AUTO-GENERATE TAG, LIKE //<=dceag-sample-b->! ****
+Without the !, everything between <=dceag-sometag-b-> and <=dceag-sometag-e->
+will be replaced by DCEGenerator each time it is run with the normal merge selection.
+The above blocks are actually <- not <=.  We don't want a substitution here
+
+void ZWave::SomeFunction()
+{
+	// If this is going to be loaded into the router as a plug-in, you can implement: 	virtual bool Register();
+	// to do all your registration, such as creating message interceptors
+
+	// If you use an IDE with auto-complete, after you type DCE:: it should give you a list of all
+	// commands and requests, including the parameters.  See "AllCommandsRequests.h"
+
+	// Examples:
+	
+	// Send a specific the "CMD_Simulate_Mouse_Click" command, which takes an X and Y parameter.  We'll use 55,77 for X and Y.
+	DCE::CMD_Simulate_Mouse_Click CMD_Simulate_Mouse_Click(m_dwPK_Device,OrbiterID,55,77);
+	SendCommand(CMD_Simulate_Mouse_Click);
+
+	// Send the message to orbiters 32898 and 27283 (ie a device list, hence the _DL)
+	// And we want a response, which will be "OK" if the command was successfull
+	string sResponse;
+	DCE::CMD_Simulate_Mouse_Click_DL CMD_Simulate_Mouse_Click_DL(m_dwPK_Device,"32898,27283",55,77)
+	SendCommand(CMD_Simulate_Mouse_Click_DL,&sResponse);
+
+	// Send the message to all orbiters within the house, which is all devices with the category DEVICECATEGORY_Orbiter_CONST (see pluto_main/Define_DeviceCategory.h)
+	// Note the _Cat for category
+	DCE::CMD_Simulate_Mouse_Click_Cat CMD_Simulate_Mouse_Click_Cat(m_dwPK_Device,DEVICECATEGORY_Orbiter_CONST,true,BL_SameHouse,55,77)
+    SendCommand(CMD_Simulate_Mouse_Click_Cat);
+
+	// Send the message to all "DeviceTemplate_Orbiter_CONST" devices within the room (see pluto_main/Define_DeviceTemplate.h)
+	// Note the _DT.
+	DCE::CMD_Simulate_Mouse_Click_DT CMD_Simulate_Mouse_Click_DT(m_dwPK_Device,DeviceTemplate_Orbiter_CONST,true,BL_SameRoom,55,77);
+	SendCommand(CMD_Simulate_Mouse_Click_DT);
+
+	// This command has a normal string parameter, but also an int as an out parameter
+	int iValue;
+	DCE::CMD_Get_Signal_Strength CMD_Get_Signal_Strength(m_dwDeviceID, DestDevice, sMac_address,&iValue);
+	// This send command will wait for the destination device to respond since there is
+	// an out parameter
+	SendCommand(CMD_Get_Signal_Strength);  
+
+	// This time we don't care about the out parameter.  We just want the command to 
+	// get through, and don't want to wait for the round trip.  The out parameter, iValue,
+	// will not get set
+	SendCommandNoResponse(CMD_Get_Signal_Strength);  
+
+	// This command has an out parameter of a data block.  Any parameter that is a binary
+	// data block is a pair of int and char *
+	// We'll also want to see the response, so we'll pass a string for that too
+
+	int iFileSize;
+	char *pFileContents
+	string sResponse;
+	DCE::CMD_Request_File CMD_Request_File(m_dwDeviceID, DestDevice, "filename",&pFileContents,&iFileSize,&sResponse);
+	SendCommand(CMD_Request_File);
+
+	// If the device processed the command (in this case retrieved the file),
+	// sResponse will be "OK", and iFileSize will be the size of the file
+	// and pFileContents will be the file contents.  **NOTE**  We are responsible
+	// free deleting pFileContents.
+
+
+	// To access our data and events below, you can type this-> if your IDE supports auto complete to see all the data and events you can access
+
+	// Get our IP address from our data
+	string sIP = DATA_Get_IP_Address();
+
+	// Set our data "Filename" to "myfile"
+	DATA_Set_Filename("myfile");
+
+	// Fire the "Finished with file" event, which takes no parameters
+	EVENT_Finished_with_file();
+	// Fire the "Touch or click" which takes an X and Y parameter
+	EVENT_Touch_or_click(10,150);
+}
+*/
+//<-dceag-sample-e->
+
+/*
+
+	COMMANDS TO IMPLEMENT
+
+*/
+
+//<-dceag-c756-b->
+
+	/** @brief COMMAND: #756 - Report Child Devices */
+	/** Report all the child sensors this has by firing an event 'Reporting Child Devices' */
+
+void ZWave::CMD_Report_Child_Devices(string &sCMD_Result,Message *pMessage)
+//<-dceag-c756-e->
+{
+	cout << "Need to implement command #756 - Report Child Devices" << endl;
+}
+
+//<-dceag-c757-b->
+
+	/** @brief COMMAND: #757 - Download Configuration */
+	/** Download new configuration data for this device */
+		/** @param #9 Text */
+			/** Any information the device may want to do the download */
+
+void ZWave::CMD_Download_Configuration(string sText,string &sCMD_Result,Message *pMessage)
+//<-dceag-c757-e->
+{
+	cout << "Need to implement command #757 - Download Configuration" << endl;
+	cout << "Parm #9 - Text=" << sText << endl;
+}
+
+//<-dceag-c760-b->
+
+	/** @brief COMMAND: #760 - Send Command To Child */
+	/** After reporting new child devices, there may be children we want to test, but we haven't done a quick reload router and can't send them messages directly.  This way we can send 'live' messages to children */
+		/** @param #10 ID */
+			/** The internal ID used for this device--not the Pluto device ID. */
+		/** @param #154 PK_Command */
+			/** The command to send */
+		/** @param #202 Parameters */
+			/** Parameters for the command in the format:
+PK_CommandParameter|Value|... */
+
+void ZWave::CMD_Send_Command_To_Child(string sID,int iPK_Command,string sParameters,string &sCMD_Result,Message *pMessage)
+//<-dceag-c760-e->
+{
+	cout << "Need to implement command #760 - Send Command To Child" << endl;
+	cout << "Parm #10 - ID=" << sID << endl;
+	cout << "Parm #154 - PK_Command=" << iPK_Command << endl;
+	cout << "Parm #202 - Parameters=" << sParameters << endl;
+}
+
+//<-dceag-c776-b->
+
+	/** @brief COMMAND: #776 - Reset */
+	/** Reset device. */
+		/** @param #51 Arguments */
+			/** Argument string
+NOEMON or CANBUS */
+
+void ZWave::CMD_Reset(string sArguments,string &sCMD_Result,Message *pMessage)
+//<-dceag-c776-e->
+{
+	cout << "Need to implement command #776 - Reset" << endl;
+	cout << "Parm #51 - Arguments=" << sArguments << endl;
+}
+
+//<-dceag-c788-b->
+
+	/** @brief COMMAND: #788 - StatusReport */
+	/** Test comand. Asq a report */
+		/** @param #51 Arguments */
+			/** Argument string */
+
+void ZWave::CMD_StatusReport(string sArguments,string &sCMD_Result,Message *pMessage)
+//<-dceag-c788-e->
+{
+	cout << "Need to implement command #788 - StatusReport" << endl;
+	cout << "Parm #51 - Arguments=" << sArguments << endl;
+}
+
+//<-dceag-c820-b->
+
+	/** @brief COMMAND: #820 - Assign Return Route */
+	/** Assign Return Route between 2 nodes from ZWave network */
+		/** @param #239 NodeID */
+			/** Source Node ID */
+		/** @param #240 DestNodeID */
+			/** Destination Node ID */
+
+void ZWave::CMD_Assign_Return_Route(int iNodeID,int iDestNodeID,string &sCMD_Result,Message *pMessage)
+//<-dceag-c820-e->
+{
+	cout << "Need to implement command #820 - Assign Return Route" << endl;
+	cout << "Parm #239 - NodeID=" << iNodeID << endl;
+	cout << "Parm #240 - DestNodeID=" << iDestNodeID << endl;
+}
+
+//<-dceag-c840-b->
+
+	/** @brief COMMAND: #840 - SetWakeUp */
+	/** Set the WakeUp value for Z-Wave PIR devices. */
+		/** @param #48 Value */
+			/** WakeUp timeout value (1-255 min) */
+		/** @param #239 NodeID */
+			/** Z-Wave node ID of the device. */
+
+void ZWave::CMD_SetWakeUp(int iValue,int iNodeID,string &sCMD_Result,Message *pMessage)
+//<-dceag-c840-e->
+{
+	cout << "Need to implement command #840 - SetWakeUp" << endl;
+	cout << "Parm #48 - Value=" << iValue << endl;
+	cout << "Parm #239 - NodeID=" << iNodeID << endl;
+}
+
+//<-dceag-c841-b->
+
+	/** @brief COMMAND: #841 - Set Config Param */
+	/** Set the configuration parameters for Z-Wave devices (PIR) */
+		/** @param #48 Value */
+			/** The value of parameter. */
+		/** @param #222 Size */
+			/** Size of the parameter (1, 2 or 4; 0/empty==autodetect) */
+		/** @param #239 NodeID */
+			/** Z-Wave node id */
+		/** @param #248 Parameter ID */
+			/** The configuration parameter ID. */
+
+void ZWave::CMD_Set_Config_Param(int iValue,int iSize,int iNodeID,int iParameter_ID,string &sCMD_Result,Message *pMessage)
+//<-dceag-c841-e->
+{
+	cout << "Need to implement command #841 - Set Config Param" << endl;
+	cout << "Parm #48 - Value=" << iValue << endl;
+	cout << "Parm #222 - Size=" << iSize << endl;
+	cout << "Parm #239 - NodeID=" << iNodeID << endl;
+	cout << "Parm #248 - Parameter_ID=" << iParameter_ID << endl;
+}
+
+//<-dceag-c842-b->
+
+	/** @brief COMMAND: #842 - Set Association */
+	/** Set the association of Z-Wave nodes into a group. */
+		/** @param #239 NodeID */
+			/** Z-Wave node ID which associates some nodes in its group. */
+		/** @param #249 Group ID */
+			/** The group number which includes the associated nodes. */
+		/** @param #250 Nodes List */
+			/** The list of associated Z-Wave nodes (comma separated). */
+
+void ZWave::CMD_Set_Association(int iNodeID,int iGroup_ID,string sNodes_List,string &sCMD_Result,Message *pMessage)
+//<-dceag-c842-e->
+{
+	cout << "Need to implement command #842 - Set Association" << endl;
+	cout << "Parm #239 - NodeID=" << iNodeID << endl;
+	cout << "Parm #249 - Group_ID=" << iGroup_ID << endl;
+	cout << "Parm #250 - Nodes_List=" << sNodes_List << endl;
+}
+
+//<-dceag-c966-b->
+
+	/** @brief COMMAND: #966 - Set Polling State */
+	/** Set the polling State for the Z-Wave interface */
+		/** @param #2 PK_Device */
+			/** The device to set polling info for.  If both this and node id are blank, it sets global polling */
+		/** @param #5 Value To Assign */
+		/** @param #220 Report */
+			/** If true, the changes will go back to the router to update device data so they persist after a reload */
+		/** @param #225 Always */
+			/** If true, this setting will persist.  Otherwise it will only happen one time and revert to default */
+		/** @param #239 NodeID */
+			/** The node to set polling for.  If both this and PK_device are blank, it sets global polling.  This supercedes PK_Device */
+
+void ZWave::CMD_Set_Polling_State(int iPK_Device,string sValue_To_Assign,bool bReport,bool bAlways,int iNodeID,string &sCMD_Result,Message *pMessage)
+//<-dceag-c966-e->
+{
+	cout << "Need to implement command #966 - Set Polling State" << endl;
+	cout << "Parm #2 - PK_Device=" << iPK_Device << endl;
+	cout << "Parm #5 - Value_To_Assign=" << sValue_To_Assign << endl;
+	cout << "Parm #220 - Report=" << bReport << endl;
+	cout << "Parm #225 - Always=" << bAlways << endl;
+	cout << "Parm #239 - NodeID=" << iNodeID << endl;
+}
+
+//<-dceag-c967-b->
+
+	/** @brief COMMAND: #967 - Add Node */
+	/** Adds a new Z-Wave node, must be SIS or Master */
+		/** @param #39 Options */
+			/** A string of letters for each of the following options:
+H = high power */
+		/** @param #48 Value */
+			/** empty or 1 = Node Any, 2=node controller, 3=node slave, 4=node existing, 5=node stop, 6=node stop failed */
+		/** @param #182 Timeout */
+			/** Number of seconds to timeout.  If not specified default is 60 seconds.  0=no timeout; a subsequent node stop must be called. */
+		/** @param #259 Multiple */
+			/** If true, allow addition of multiple nodes until timeout occurs or add node is called with node stop. */
+
+void ZWave::CMD_Add_Node(string sOptions,int iValue,string sTimeout,bool bMultiple,string &sCMD_Result,Message *pMessage)
+//<-dceag-c967-e->
+{
+	cout << "Need to implement command #967 - Add Node" << endl;
+	cout << "Parm #39 - Options=" << sOptions << endl;
+	cout << "Parm #48 - Value=" << iValue << endl;
+	cout << "Parm #182 - Timeout=" << sTimeout << endl;
+	cout << "Parm #259 - Multiple=" << bMultiple << endl;
+}
+
+//<-dceag-c968-b->
+
+	/** @brief COMMAND: #968 - Remove Node */
+	/** Removes a Z-Wave node, must be SIS or Master */
+		/** @param #39 Options */
+			/** If true, allow deletion of multiple nodes until timeout occurs or add node is called with node stop. */
+		/** @param #48 Value */
+			/** empty or 1 = Node Any, 2=node controller, 3=node slave, 5=node stop */
+		/** @param #182 Timeout */
+			/** Number of seconds to timeout.  If not specified default is 60 seconds.  0=no timeout; a subsequent node stop must be called. */
+		/** @param #259 Multiple */
+			/** If true, allow deletion of multiple nodes until timeout occurs or add node is called with node stop. */
+
+void ZWave::CMD_Remove_Node(string sOptions,int iValue,string sTimeout,bool bMultiple,string &sCMD_Result,Message *pMessage)
+//<-dceag-c968-e->
+{
+	cout << "Need to implement command #968 - Remove Node" << endl;
+	cout << "Parm #39 - Options=" << sOptions << endl;
+	cout << "Parm #48 - Value=" << iValue << endl;
+	cout << "Parm #182 - Timeout=" << sTimeout << endl;
+	cout << "Parm #259 - Multiple=" << bMultiple << endl;
+}
+
+//<-dceag-c1085-b->
+
+	/** @brief COMMAND: #1085 - Resync node */
+	/** Resynchronize device data */
+		/** @param #239 NodeID */
+			/** Node id to resync */
+
+void ZWave::CMD_Resync_node(int iNodeID,string &sCMD_Result,Message *pMessage)
+//<-dceag-c1085-e->
+{
+	cout << "Need to implement command #1085 - Resync node" << endl;
+	cout << "Parm #239 - NodeID=" << iNodeID << endl;
+}
+
+
---
+++ src/ZWave-OZW/ZWave.h
@@ -0,0 +1,236 @@
+/*
+     Copyright (C) 2004 Pluto, Inc., a Florida Corporation
+
+     www.plutohome.com
+
+     Phone: +1 (877) 758-8648
+ 
+
+     This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+     See the GNU General Public License for more details.
+
+*/
+//<-dceag-d-b->
+#ifndef ZWave_h
+#define ZWave_h
+
+//	DCE Implemenation for #1754 ZWave
+
+#include "Gen_Devices/ZWaveBase.h"
+//<-dceag-d-e->
+
+//<-dceag-decl-b->
+namespace DCE
+{
+	class ZWave : public ZWave_Command
+	{
+//<-dceag-decl-e->
+		// Private member variables
+
+		// Private methods
+public:
+		// Public member variables
+
+//<-dceag-const-b->
+public:
+		// Constructors/Destructor
+		ZWave(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL);
+		virtual ~ZWave();
+		virtual bool GetConfig();
+		virtual bool Register();
+		virtual void ReceivedCommandForChild(DeviceData_Impl *pDeviceData_Impl,string &sCMD_Result,Message *pMessage);
+		virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage);
+//<-dceag-const-e->
+
+//<-dceag-const2-b->
+		// The following constructor is only used if this a class instance embedded within a DCE Device.  In that case, it won't create it's own connection to the router
+		// You can delete this whole section and put an ! after dceag-const2-b tag if you don't want this constructor.  Do the same in the implementation file
+		ZWave(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+//<-dceag-const2-e->
+
+//<-dceag-h-b->
+	/*
+				AUTO-GENERATED SECTION
+				Do not change the declarations
+	*/
+
+	/*
+			*****DATA***** accessors inherited from base class
+	string DATA_Get_COM_Port_on_PC();
+	bool DATA_Get_Only_One_Per_PC();
+	bool DATA_Get_Autoassign_to_parents_room();
+
+			*****EVENT***** accessors inherited from base class
+	void EVENT_Sensor_Tripped(bool bTripped);
+	void EVENT_Device_OnOff(bool bOnOff);
+	void EVENT_Reporting_Child_Devices(string sError_Message,string sText);
+	void EVENT_Download_Config_Done(string sError_Message);
+
+			*****COMMANDS***** we need to implement
+	*/
+
+
+	/** @brief COMMAND: #756 - Report Child Devices */
+	/** Report all the child sensors this has by firing an event 'Reporting Child Devices' */
+
+	virtual void CMD_Report_Child_Devices() { string sCMD_Result; CMD_Report_Child_Devices(sCMD_Result,NULL);};
+	virtual void CMD_Report_Child_Devices(string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #757 - Download Configuration */
+	/** Download new configuration data for this device */
+		/** @param #9 Text */
+			/** Any information the device may want to do the download */
+
+	virtual void CMD_Download_Configuration(string sText) { string sCMD_Result; CMD_Download_Configuration(sText.c_str(),sCMD_Result,NULL);};
+	virtual void CMD_Download_Configuration(string sText,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #760 - Send Command To Child */
+	/** After reporting new child devices, there may be children we want to test, but we haven't done a quick reload router and can't send them messages directly.  This way we can send 'live' messages to children */
+		/** @param #10 ID */
+			/** The internal ID used for this device--not the Pluto device ID. */
+		/** @param #154 PK_Command */
+			/** The command to send */
+		/** @param #202 Parameters */
+			/** Parameters for the command in the format:
+PK_CommandParameter|Value|... */
+
+	virtual void CMD_Send_Command_To_Child(string sID,int iPK_Command,string sParameters) { string sCMD_Result; CMD_Send_Command_To_Child(sID.c_str(),iPK_Command,sParameters.c_str(),sCMD_Result,NULL);};
+	virtual void CMD_Send_Command_To_Child(string sID,int iPK_Command,string sParameters,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #776 - Reset */
+	/** Reset device. */
+		/** @param #51 Arguments */
+			/** Argument string
+NOEMON or CANBUS */
+
+	virtual void CMD_Reset(string sArguments) { string sCMD_Result; CMD_Reset(sArguments.c_str(),sCMD_Result,NULL);};
+	virtual void CMD_Reset(string sArguments,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #788 - StatusReport */
+	/** Test comand. Asq a report */
+		/** @param #51 Arguments */
+			/** Argument string */
+
+	virtual void CMD_StatusReport(string sArguments) { string sCMD_Result; CMD_StatusReport(sArguments.c_str(),sCMD_Result,NULL);};
+	virtual void CMD_StatusReport(string sArguments,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #820 - Assign Return Route */
+	/** Assign Return Route between 2 nodes from ZWave network */
+		/** @param #239 NodeID */
+			/** Source Node ID */
+		/** @param #240 DestNodeID */
+			/** Destination Node ID */
+
+	virtual void CMD_Assign_Return_Route(int iNodeID,int iDestNodeID) { string sCMD_Result; CMD_Assign_Return_Route(iNodeID,iDestNodeID,sCMD_Result,NULL);};
+	virtual void CMD_Assign_Return_Route(int iNodeID,int iDestNodeID,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #840 - SetWakeUp */
+	/** Set the WakeUp value for Z-Wave PIR devices. */
+		/** @param #48 Value */
+			/** WakeUp timeout value (1-255 min) */
+		/** @param #239 NodeID */
+			/** Z-Wave node ID of the device. */
+
+	virtual void CMD_SetWakeUp(int iValue,int iNodeID) { string sCMD_Result; CMD_SetWakeUp(iValue,iNodeID,sCMD_Result,NULL);};
+	virtual void CMD_SetWakeUp(int iValue,int iNodeID,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #841 - Set Config Param */
+	/** Set the configuration parameters for Z-Wave devices (PIR) */
+		/** @param #48 Value */
+			/** The value of parameter. */
+		/** @param #222 Size */
+			/** Size of the parameter (1, 2 or 4; 0/empty==autodetect) */
+		/** @param #239 NodeID */
+			/** Z-Wave node id */
+		/** @param #248 Parameter ID */
+			/** The configuration parameter ID. */
+
+	virtual void CMD_Set_Config_Param(int iValue,int iSize,int iNodeID,int iParameter_ID) { string sCMD_Result; CMD_Set_Config_Param(iValue,iSize,iNodeID,iParameter_ID,sCMD_Result,NULL);};
+	virtual void CMD_Set_Config_Param(int iValue,int iSize,int iNodeID,int iParameter_ID,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #842 - Set Association */
+	/** Set the association of Z-Wave nodes into a group. */
+		/** @param #239 NodeID */
+			/** Z-Wave node ID which associates some nodes in its group. */
+		/** @param #249 Group ID */
+			/** The group number which includes the associated nodes. */
+		/** @param #250 Nodes List */
+			/** The list of associated Z-Wave nodes (comma separated). */
+
+	virtual void CMD_Set_Association(int iNodeID,int iGroup_ID,string sNodes_List) { string sCMD_Result; CMD_Set_Association(iNodeID,iGroup_ID,sNodes_List.c_str(),sCMD_Result,NULL);};
+	virtual void CMD_Set_Association(int iNodeID,int iGroup_ID,string sNodes_List,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #966 - Set Polling State */
+	/** Set the polling State for the Z-Wave interface */
+		/** @param #2 PK_Device */
+			/** The device to set polling info for.  If both this and node id are blank, it sets global polling */
+		/** @param #5 Value To Assign */
+		/** @param #220 Report */
+			/** If true, the changes will go back to the router to update device data so they persist after a reload */
+		/** @param #225 Always */
+			/** If true, this setting will persist.  Otherwise it will only happen one time and revert to default */
+		/** @param #239 NodeID */
+			/** The node to set polling for.  If both this and PK_device are blank, it sets global polling.  This supercedes PK_Device */
+
+	virtual void CMD_Set_Polling_State(int iPK_Device,string sValue_To_Assign,bool bReport,bool bAlways,int iNodeID) { string sCMD_Result; CMD_Set_Polling_State(iPK_Device,sValue_To_Assign.c_str(),bReport,bAlways,iNodeID,sCMD_Result,NULL);};
+	virtual void CMD_Set_Polling_State(int iPK_Device,string sValue_To_Assign,bool bReport,bool bAlways,int iNodeID,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #967 - Add Node */
+	/** Adds a new Z-Wave node, must be SIS or Master */
+		/** @param #39 Options */
+			/** A string of letters for each of the following options:
+H = high power */
+		/** @param #48 Value */
+			/** empty or 1 = Node Any, 2=node controller, 3=node slave, 4=node existing, 5=node stop, 6=node stop failed */
+		/** @param #182 Timeout */
+			/** Number of seconds to timeout.  If not specified default is 60 seconds.  0=no timeout; a subsequent node stop must be called. */
+		/** @param #259 Multiple */
+			/** If true, allow addition of multiple nodes until timeout occurs or add node is called with node stop. */
+
+	virtual void CMD_Add_Node(string sOptions,int iValue,string sTimeout,bool bMultiple) { string sCMD_Result; CMD_Add_Node(sOptions.c_str(),iValue,sTimeout.c_str(),bMultiple,sCMD_Result,NULL);};
+	virtual void CMD_Add_Node(string sOptions,int iValue,string sTimeout,bool bMultiple,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #968 - Remove Node */
+	/** Removes a Z-Wave node, must be SIS or Master */
+		/** @param #39 Options */
+			/** If true, allow deletion of multiple nodes until timeout occurs or add node is called with node stop. */
+		/** @param #48 Value */
+			/** empty or 1 = Node Any, 2=node controller, 3=node slave, 5=node stop */
+		/** @param #182 Timeout */
+			/** Number of seconds to timeout.  If not specified default is 60 seconds.  0=no timeout; a subsequent node stop must be called. */
+		/** @param #259 Multiple */
+			/** If true, allow deletion of multiple nodes until timeout occurs or add node is called with node stop. */
+
+	virtual void CMD_Remove_Node(string sOptions,int iValue,string sTimeout,bool bMultiple) { string sCMD_Result; CMD_Remove_Node(sOptions.c_str(),iValue,sTimeout.c_str(),bMultiple,sCMD_Result,NULL);};
+	virtual void CMD_Remove_Node(string sOptions,int iValue,string sTimeout,bool bMultiple,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #1085 - Resync node */
+	/** Resynchronize device data */
+		/** @param #239 NodeID */
+			/** Node id to resync */
+
+	virtual void CMD_Resync_node(int iNodeID) { string sCMD_Result; CMD_Resync_node(iNodeID,sCMD_Result,NULL);};
+	virtual void CMD_Resync_node(int iNodeID,string &sCMD_Result,Message *pMessage);
+
+//<-dceag-h-e->
+	};
+
+//<-dceag-end-b->
+}
+#endif
+//<-dceag-end-e->
