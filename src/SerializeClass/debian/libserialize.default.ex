bfa517ffa751d1c443e08f4afe8cca6ee4da14cd
posde <posde@theinternet.de>
2012-11-18 16:46:08 +0000

    Refs #164 - Add a debian dir to libserialize and modify the Makefile a bit for regular dpkg-buildpackage

---
+++ src/SerializeClass/debian/libserialize.default.ex
@@ -0,0 +1,10 @@
+# Defaults for libserialize initscript
+# sourced by /etc/init.d/libserialize
+# installed at /etc/default/libserialize by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/SerializeClass/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libserialize):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libserialize" command="/usr/bin/libserialize"
