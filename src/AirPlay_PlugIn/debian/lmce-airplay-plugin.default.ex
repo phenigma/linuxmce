de25b52dcf8bbb231543725331640f615e508b95
posde <posde@theinternet.de>
2012-11-22 20:15:52 +0000

    Refs #164 - AirPlay plugin gets a debian dir and a proper Makefile

---
+++ src/AirPlay_PlugIn/debian/lmce-airplay-plugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-airplay-plugin initscript
+# sourced by /etc/init.d/lmce-airplay-plugin
+# installed at /etc/default/lmce-airplay-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/AirPlay_PlugIn/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-airplay-plugin):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-airplay-plugin" command="/usr/bin/lmce-airplay-plugin"
