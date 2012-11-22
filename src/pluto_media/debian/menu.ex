74c6ea6dfa19f9abb06ec5fe0cc3ca10fad21089
posde <posde@theinternet.de>
2012-11-22 19:26:44 +0000

    Refs #164 - adding a debian dir to pluto_media modifying Makefile to work with dpkg-buildpackage

---
+++ src/pluto_media/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(pluto-media):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="pluto-media" command="/usr/bin/pluto-media"
---
+++ src/pluto_media/debian/pluto-media.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pluto-media initscript
+# sourced by /etc/init.d/pluto-media
+# installed at /etc/default/pluto-media by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
