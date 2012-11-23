cb3329d540e0756178ae14a9d5ecdf89d54555a8
posde <posde@theinternet.de>
2012-11-23 17:01:09 +0000

    Refs #164 - A shiny new debian dir for the Orbiter plugin and a polished Makefile

---
+++ src/Orbiter_Plugin/debian/lmce-orbiter-plugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-orbiter-plugin initscript
+# sourced by /etc/init.d/lmce-orbiter-plugin
+# installed at /etc/default/lmce-orbiter-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
