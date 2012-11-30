009508bcd2340f7cc621c81cb176ac5f734f2eb0
posde <posde@theinternet.de>
2012-11-30 16:11:33 +0000

    Refs #164 - remove existing debian dir from hulu plugin

--- src/Hulu_PlugIn/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lmce-hulu-plugin package
-#
-0 4	* * *	root	[ -x /usr/bin/lmce-hulu-plugin_maintenance ] && /usr/bin/lmce-hulu-plugin_maintenance
--- src/Hulu_PlugIn/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lmce-hulu-plugin):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="lmce-hulu-plugin" command="/usr/bin/lmce-hulu-plugin"
