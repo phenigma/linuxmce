2048ff419e5134e9c71db31ed1bee38bf2476dcc
posde <posde@theinternet.de>
2012-11-30 17:04:59 +0000

    Refs #164 - new debian dir for the rpc plugin with some love to the Makefile

---
+++ src/RPC_Plugin/debian/lmce-rpc-plugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-rpc-plugin initscript
+# sourced by /etc/init.d/lmce-rpc-plugin
+# installed at /etc/default/lmce-rpc-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/RPC_Plugin/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-rpc-plugin):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-rpc-plugin" command="/usr/bin/lmce-rpc-plugin"
