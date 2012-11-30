64628c55752ebb291c60d9da4be88a34a1528dd8
posde <posde@theinternet.de>
2012-11-30 16:01:13 +0000

    Refs #164 - Game Plugin real debian dir commit

---
+++ src/Game_PlugIn/debian/lmce-game-plugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-game-plugin initscript
+# sourced by /etc/init.d/lmce-game-plugin
+# installed at /etc/default/lmce-game-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
