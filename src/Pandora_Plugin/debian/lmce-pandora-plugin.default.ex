af1f7b58d11470719ad579004d4f8ef116af1719
posde <posde@theinternet.de>
2012-11-30 16:21:38 +0000

    Refs #164 - Pandora Plugin receives a debian dir and some love for the Makefile.

---
+++ src/Pandora_Plugin/debian/lmce-pandora-plugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-pandora-plugin initscript
+# sourced by /etc/init.d/lmce-pandora-plugin
+# installed at /etc/default/lmce-pandora-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/Pandora_Plugin/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-pandora-plugin):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-pandora-plugin" command="/usr/bin/lmce-pandora-plugin"
