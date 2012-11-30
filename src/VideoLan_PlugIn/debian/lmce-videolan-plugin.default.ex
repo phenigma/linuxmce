0c384ef362d52f76a1952e806e4183838c3c5aff
posde <posde@theinternet.de>
2012-11-30 17:12:59 +0000

    Refs #164 - VideoLAN Plugin receives a debian dir and some loving to the Makefile

---
+++ src/VideoLan_PlugIn/debian/lmce-videolan-plugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-videolan-plugin initscript
+# sourced by /etc/init.d/lmce-videolan-plugin
+# installed at /etc/default/lmce-videolan-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
