48bf8a4cb4eb7ff5fd8d0718b00e25c00c214f1e
posde <posde@theinternet.de>
2012-12-15 15:00:33 +0000

    Refs #164, #1546 - First version of a debian dir for DLNA device. Does not yet build due to missing include

---
+++ src/DLNA/debian/lmce-dlna.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-dlna initscript
+# sourced by /etc/init.d/lmce-dlna
+# installed at /etc/default/lmce-dlna by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/DLNA/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-dlna):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-dlna" command="/usr/bin/lmce-dlna"
