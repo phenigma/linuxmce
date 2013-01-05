235183b7c254f6d340b7b9033748b6dc25280e21
tschak <thom.cherryhomes@gmail.com>
2013-01-05 01:21:44 +0000

    Fixes #1650 - Add Game Bookmarks.

--- web/lmce-admin/operations/mediaBrowser/bookmarks.php
+++ web/lmce-admin/operations/mediaBrowser/bookmarks.php
@@ -22,6 +22,9 @@ function bookmarks($output,$mediadbADO,$dbADO) {
 		case 3:
 			$mediaTypes=array(2,3);
 		break;
+	        case 4:
+			$mediaTypes=array(29,30,32,33,34,35,36,37,38,39,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59);
+		break;
 	}
 	
 	if($action=='form'){
