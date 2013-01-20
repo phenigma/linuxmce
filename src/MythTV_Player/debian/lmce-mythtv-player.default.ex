752327254a1f9598cb0ed881f333afd1d79e75d7
posde <posde@theinternet.de>
2013-01-20 11:39:34 +0000

    Refs #164 - initial debian dir for MythTV player

---
+++ src/MythTV_Player/debian/lmce-mythtv-player.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-mythtv-player initscript
+# sourced by /etc/init.d/lmce-mythtv-player
+# installed at /etc/default/lmce-mythtv-player by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/MythTV_Player/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-mythtv-player):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-mythtv-player" command="/usr/bin/lmce-mythtv-player"
