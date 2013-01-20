fe8c3c52a40f4359a9a923fa360d7acebcd7fb89
posde <posde@theinternet.de>
2013-01-20 21:55:10 +0000

    Refs #164 - USB_UIRT needed love as well, and now that IRBase and MessageTrans are happy, USB_UIRT is debian-happy as well

---
+++ src/USB_UIRT_0038/debian/lmce-usbuirt.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lmce-usbuirt initscript
+# sourced by /etc/init.d/lmce-usbuirt
+# installed at /etc/default/lmce-usbuirt by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ src/USB_UIRT_0038/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-usbuirt):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-usbuirt" command="/usr/bin/lmce-usbuirt"
