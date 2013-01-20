38fb58ab6def98526ce9b39788ca5bee7cf50f12
posde <posde@theinternet.de>
2013-01-20 17:22:47 +0000

    Refs #164 - adding debian dir to lmce-windowutils

---
+++ src/WindowUtils/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(lmce-windowsutils):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="lmce-windowsutils" command="/usr/bin/lmce-windowsutils"
---
+++ src/WindowUtils/debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblmce-windowsutils 2.46 lmce-windowsutils (>> 2.46-0), lmce-windowsutils (<< 2.46-99)
