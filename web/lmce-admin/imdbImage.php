067e5d980e0ec7b3b03bf3cc3955548c544fae6e
golgoj4 <golgoj4>
2013-02-26 20:20:25 +0000

    refs #1168 because im a bastard and cant look up the other ticket...adds query to return backdrop images. should return some default image if one isnt found, but thats later.

--- web/lmce-admin/imdbImage.php
+++ web/lmce-admin/imdbImage.php
@@ -15,9 +15,13 @@ $filemarker = substr_replace($file, "", 0, 2);
 $ref="";
 
 #magic sql to determine ref go here
+$sql = "SELECT File_Attribute.FK_File, File_Attribute.FK_Attribute, Attribute.PK_Attribute, Attribute.FK_AttributeType, Picture_Attribute.FK_Attribute, Picture_Attribute.FK_Picture FROM Picture_Attribute, Attribute, File_Attribute WHERE File_Attribute.FK_File = \"$filemarker\" AND Attribute.FK_AttributeType=\"36\" AND File_Attribute.FK_Attribute = Attribute.PK_Attribute AND Attribute.PK_Attribute = Picture_Attribute.FK_Attribute";
 
-	
-	$img =file_get_contents("mediapics/".$ref.".jpg");
+$result = mysql_query($sql) or die(mysql_error()); 
+$row = mysql_fetch_assoc($result);
+$ref.=$row['FK_Picture'];
+
+$img =file_get_contents("mediapics/".$ref.".jpg");
 echo $img;
 
 ?>
