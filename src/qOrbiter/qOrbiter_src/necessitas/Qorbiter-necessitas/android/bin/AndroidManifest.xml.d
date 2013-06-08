0d456c99d8911de78dc980a0a0434ee5429255fc
golgoj4 <golgoj4>
2013-06-08 16:59:11 +0000

    refs #1170 - removing binary android files

--- src/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/bin/AndroidManifest.xml : \
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/AndroidManifest.xml \
--- src/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/bin/Qorbiter-necessitas-debug-unaligned.apk.d
+++
@@ -1,8 +0,0 @@
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/bin/Qorbiter-necessitas-debug-unaligned.apk : \
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/bin/Qorbiter-necessitas.ap_ \
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/bin/classes.dex \
-/home/golgoj4/necessitas4/android-sdk/tools/support/annotations.jar \
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/libs/armeabi-v7a/libqmlshadersplugin.so \
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/libs/armeabi-v7a/libandroidplugin_1_1.so \
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/libs/armeabi-v7a/libqorbiter-4.8.2.so \
-/src/trunk/qOrbiter/qOrbiter_src/necessitas/Qorbiter-necessitas/android/libs/armeabi-v7a/gdbserver \
