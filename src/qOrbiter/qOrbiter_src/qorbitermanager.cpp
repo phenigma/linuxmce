1a78726aff02e6851e0374d9db2ae07d3dd40782
golgoj4 <golgoj4>
2013-08-10 03:17:41 +0000

    refs #1170 - getting rid of cruft in qrc / manager.

--- src/qOrbiter/qOrbiter_src/qorbitermanager.cpp
+++ src/qOrbiter/qOrbiter_src/qorbitermanager.cpp
@@ -279,6 +279,11 @@ qorbiterManager::qorbiterManager(QDeclarativeView *view, QObject *parent) :
 void qorbiterManager::gotoQScreen(QString s)
 {
 
+    if(currentScreen ==s ){
+        emit skinMessage("You are already on"+s);
+        return;
+    }
+
     if(s.contains("Screen_1"))
     {
         qDebug() << "Clearing Models";
