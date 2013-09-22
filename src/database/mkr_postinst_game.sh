9e31d8dbd96ef0bfe5491fe289c0073ed121157d
tschak <thom.cherryhomes@gmail.com>
2013-09-22 15:18:16 +0000

    Fix for lmce_game so it will drop database before import.

--- src/database/mkr_postinst_game.sh
+++ src/database/mkr_postinst_game.sh
@@ -4,9 +4,14 @@ echo
 PROCESS="upgrade"
 # Check if we have an existing install, by verifying the RomTitles table exists. This also takes care
 # of installs where the old schema is used, because this table does not exist there.
+# in addition, if RomTitles isn't there, but the psc_ tables are there, then we need to drop the db, before import.
 mysql $MYSQL_DB_CRED lmce_game -e "SELECT * from RomTitles LIMIT 1"||PROCESS="install"
+mysql $MYSQL_DB_CRED lmce_game -e "SELECT * from psc_game_repset LIMIT 1"||DONOTDROP="donotdrop"
 if [ $PROCESS = "install" ]; then
 	(
+	if [ -z $DONOTDROP ]; then 
+			mysql $MYSQL_DB_CRED lmce_game -e "DROP DATABASE lmce_game"
+	fi
 		cd /usr/pluto/database
 		/usr/pluto/bin/sqlCVS $PLUTO_DB_CRED -n -D lmce_game -r game import
 	) || exit $?
