0287e6e6e4ae9c4f3e2ac37ce2a85df16fa76d41
tschak <thom.cherryhomes@gmail.com>
2013-10-01 18:52:37 +0000

    Refs #1820 - check in DCEGen skeleton for DIAL support.

---
+++ src/DIAL/DIAL.h
@@ -0,0 +1,72 @@
+/*
+     Copyright (C) 2013 LinuxMCE 
+
+     www.linuxmce.org
+
+
+     This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+     See the GNU General Public License for more details.
+
+*/
+//<-dceag-d-b->
+#ifndef DIAL_h
+#define DIAL_h
+
+//	DCE Implemenation for #2268 DIAL
+
+#include "Gen_Devices/DIALBase.h"
+//<-dceag-d-e->
+
+//<-dceag-decl-b->
+namespace DCE
+{
+	class DIAL : public DIAL_Command
+	{
+//<-dceag-decl-e->
+		// Private member variables
+
+		// Private methods
+public:
+		// Public member variables
+
+//<-dceag-const-b->
+public:
+		// Constructors/Destructor
+		DIAL(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL);
+		virtual ~DIAL();
+		virtual bool GetConfig();
+		virtual bool Register();
+		virtual void ReceivedCommandForChild(DeviceData_Impl *pDeviceData_Impl,string &sCMD_Result,Message *pMessage);
+		virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage);
+//<-dceag-const-e->
+
+//<-dceag-const2-b->
+		// The following constructor is only used if this a class instance embedded within a DCE Device.  In that case, it won't create it's own connection to the router
+		// You can delete this whole section and put an ! after dceag-const2-b tag if you don't want this constructor.  Do the same in the implementation file
+		DIAL(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+//<-dceag-const2-e->
+
+//<-dceag-h-b->
+	/*
+				AUTO-GENERATED SECTION
+				Do not change the declarations
+	*/
+
+	/*
+			*****DATA***** accessors inherited from base class
+
+			*****EVENT***** accessors inherited from base class
+
+			*****COMMANDS***** we need to implement
+	*/
+
+//<-dceag-h-e->
+	};
+
+//<-dceag-end-b->
+}
+#endif
+//<-dceag-end-e->
