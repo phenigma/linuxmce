aa4b90b1bc27009293e671bcb1bcc1f442d4c819
phenigma <phenigma@hotmail.com>
2013-10-27 18:52:28 +0000

    Fixes #1947 - replace the medibuntu repo with the videolan repo in mce-install.sh for 1204

--- src/new-installer/mce-install.sh
+++ src/new-installer/mce-install.sh
@@ -314,8 +314,8 @@ StatsMessage "Configuring sources.list for MCE install"
 echo>>/etc/apt/sources.conf
 
 AddRepoToSourcesTop "file:${LOCAL_REPO_BASE} ${LOCAL_REPO_DIR}"
-AddRepoToSources "http://deb.linuxmce.org/ubuntu/ ${DISTRO}  ${COMPOS}"
-AddRepoToSources "http://debian.slimdevices.com/ stable  main"
+AddRepoToSources "http://deb.linuxmce.org/ubuntu/ ${DISTRO} ${COMPOS}"
+AddRepoToSources "http://debian.slimdevices.com/ stable main"
 
 # Check where our distro is hosted and assign the proper variable
 DISTRO_HOST=
@@ -327,8 +327,8 @@ AddRepoToSources "http://${DISTRO_HOST}.ubuntu.com/ubuntu ${DISTRO} main restric
 AddRepoToSources "http://${DISTRO_HOST}.ubuntu.com/ubuntu ${DISTRO}-security main restricted universe multiverse"
 AddRepoToSources "http://archive.canonical.com/ubuntu ${DISTRO} partner"
 
-if AddRepoToSources "http://packages.medibuntu.org/ ${DISTRO}  free non-free"; then
-	AddGpgKeyToKeyring http://packages.medibuntu.org/medibuntu-key.gpg
+if AddRepoToSources "http://download.videolan.org/pub/debian/stable/ /"; then
+	wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|apt-key add -
 fi
 
 # Setup pluto's apt.conf
@@ -983,6 +983,7 @@ chmod 755 /etc/rc5.d/S90firstboot
 rm -f /tmp/firstboot
 echo >> /etc/apt/sources.list
 /usr/share/update-notifier/notify-reboot-required
+rm /etc/init/kdm.conf
 }
 
 
