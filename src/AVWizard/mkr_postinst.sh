1843a4748c3834c892ecf4243e28bcc5c938605b
phenigma <phenigma@hotmail.com>
2013-11-10 21:42:24 +0000

    Refs #1961 - init script updates for precise AVWizard

--- src/AVWizard/mkr_postinst.sh
+++ src/AVWizard/mkr_postinst.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #<-mkr_B_via_b->
-update-rc.d check_avwizard start 98 S . >/dev/null
-update-rc.d a0start_avwizard start 99 2 . >/dev/null 
-	if [[ -e /etc/init.d/0start_avwizard ]]; then 
-		update-rc.d -f 0start_avwizard remove 
-		rm /etc/init.d/0start_avwizard 
-	fi 
+update-rc.d check_avwizard start 98 2 3 4 5 . >/dev/null
+update-rc.d a0start_avwizard start 99 2 3 4 5 . >/dev/null
+
+if [[ -e /etc/init.d/0start_avwizard ]]; then
+	update-rc.d -f 0start_avwizard remove
+	rm /etc/init.d/0start_avwizard
+fi
 #<-mkr_B_via_e->
