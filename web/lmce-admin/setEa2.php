1f5a4c7c7dd247b09d466530d6e750f9b7d813f3
golgoj4 <golgoj4>
2013-11-24 18:06:31 +0000

    refs #1108 - staying current with previous commit to 1004 branch

--- web/lmce-admin/setEa2.php
+++ web/lmce-admin/setEa2.php
@@ -9,96 +9,96 @@
   } else {
     die("Please specify the device ID with d=xxxx");
   }
+/*
+Need to get children devices and do the same as the parent.
+*/
 
+ $mobileRoom=-1;
+ $mobileEa=-1;
+ $server = "localhost";
+ $mysqlUser = "root";
+ $mysqlPass = "";
+ $conn = mysql_connect($server, $mysqlUser, $mysqlPass);
+ mysql_select_db("pluto_main", $conn);
+ $dupe;
 
- 
-  $mobileRoom=-1;
-  $server = "localhost";
-  $mysqlUser = "root";
-  $mysqlPass = "";
-  $conn = mysql_connect($server, $mysqlUser, $mysqlPass);
-  mysql_select_db("pluto_main", $conn);
-  $dupe;
+ if($conn){
+  $installSql = "SELECT * FROM `Installation` ";
+  $iRes=mysql_query($installSql, $conn) or die(mysql_error($conn));
+  $inst="";
+    while($row=mysql_fetch_array($iRes)){
+    $inst=$row['PK_Installation'];
+    //print_r($row);
+    }
 
-  if($conn)
-  {
+  $installation=$inst;
+  echo "Installation is ".$installation."<br>";
+    if($installation==""){
+    return;
+  }
+  echo "Connection Found, Starting<br>";
 
-$installSql = "SELECT * FROM `Installation` ";
-$iRes=mysql_query($installSql, $conn) or die(mysql_error($conn));
-$inst="";
-while($row=mysql_fetch_array($iRes)){
-$inst=$row['PK_Installation'];
-//print_r($row);
-}
+  $roomSql = "SELECT * FROM `Room` WHERE `Description` like 'Mobile QOrbiters' LIMIT 0, 30 ";
+  echo "Checking for existing mobile orbiter room <br>";
+  $result = mysql_query($roomSql,  $conn) or die (mysql_error($conn));
+  $cnt = mysql_num_rows($result);
+  
+    if($cnt===0){
+      global $mobileRoom;
+      $iRoomSql ="INSERT INTO `pluto_main`.`Room` (`PK_Room`, `FK_Installation`, `FK_RoomType`, `Description`, `FK_Icon`, `ManuallyConfigureEA`, `HideFromOrbiter`, `FK_FloorplanObjectType`, `FloorplanInfo`, `psc_id`, `psc_batch`, `psc_user`, `psc_frozen`, `psc_mod`, `psc_restrict`) VALUES (NULL, ".$installation.", '13', 'Mobile QOrbiters', NULL, '0', '0', NULL, NULL, NULL, NULL, NULL, '0', CURRENT_TIMESTAMP, NULL);";
+      $result2=mysql_query($iRoomSql,  $conn) or die (mysql_error($conn));
+      $lastId = mysql_insert_id($conn);
 
-$installation=$inst;
-echo "Installation is ".$installation."<br>";
-if($installation==""){
-return;
-}
-      echo "Connection Found, Starting<br>";
-
-	$roomSql = "SELECT * FROM `Room` WHERE `Description` like 'Mobile QOrbiters' LIMIT 0, 30 ";
-	echo "Checking for existing mobile orbiter room <br>";
-	$result = mysql_query($roomSql,  $conn) or die (mysql_error($conn));
-	$cnt = mysql_num_rows($result);
-	
-	if($cnt===0){
-
-	$iRoomSql ="INSERT INTO `pluto_main`.`Room` (`PK_Room`, `FK_Installation`, `FK_RoomType`, `Description`, `FK_Icon`, `ManuallyConfigureEA`, `HideFromOrbiter`, `FK_FloorplanObjectType`, `FloorplanInfo`, `psc_id`, `psc_batch`, `psc_user`, `psc_frozen`, `psc_mod`, `psc_restrict`) VALUES (NULL, ".$installation.", '13', 'Mobile QOrbiters', NULL, '0', '0', NULL, NULL, NULL, NULL, NULL, '0', CURRENT_TIMESTAMP, NULL);";
-	$result2=mysql_query($iRoomSql,  $conn) or die (mysql_error($conn));
-
-	$lastId = mysql_insert_id($conn);
-
-	$chkSql=$roomSql = "SELECT * FROM 'Room' WHERE PK_Room = '".$lastId."'";
-
-	$result3 = mysql_query($chkSql, $conn) or die(mysql_error($conn));
-	
-	 while ($row = mysql_fetch_array($result3)){
-          if($row['PK_Room']){
-          $mobileRoom = $row['PK_Room'];
+      $chkSql=$roomSql = "SELECT * FROM 'Room' WHERE PK_Room = '".$lastId."'";
+      $result3 = mysql_query($chkSql, $conn) or die(mysql_error($conn));
+	while ($row = mysql_fetch_array($result3)){
+	  if($row['PK_Room']){
+	    $mobileRoom = $row['PK_Room'];
+	    echo "Mobile QOrbiters Room is ".$mobileRoom;
           }
         }
-
-echo "Not Found, so we've added it. Setting up entertain area now<br>";	
-
-}
-else {
-   while ($row = mysql_fetch_array($result)){
-          if($row['PK_Room']){
-          $mobileRoom = $row['PK_Room'];
-          }
+      echo "Not Found, so we've added it. Setting up entertain area now<br>";	
+      } else {
+	while ($row = mysql_fetch_array($result)){
+	  if($row['PK_Room']){
+	    $mobileRoom = $row['PK_Room'];
+	    echo "Mobile QOrbiters Room is ".$mobileRoom."<br>" ;
+         }
         }
-	echo "Found, Setting up entertain area now <br>";
-
-	}
-
-      $dupe = checkForDupe($conn, $deviceID);
-	if($dupe)
-	{
+	  echo " Setting up entertain area now <br>";
+      }
+    $dupe = checkForDupe($conn, $deviceID);
+      if($dupe)	{
 	echo "Duplicate, will set to ea for device<br>";
-	}
-	else
-	{
+	updateEntertainArea($conn, $deviceID, $mobileRoom);
+	} else {
 	echo "No Matching Ea found, will set to new EA<br>";
 	setEntertainArea($conn, $deviceID, $mobileRoom);
 	}
+      }
 
-}
 
-  function checkForDupe($connect, $device){
-    $status = false;
-    $sql = "SELECT * FROM `EntertainArea` WHERE `Description` LIKE '".$device."' LIMIT 0, 30 ";
-    echo "<b>Checking for duplicate Ea for device " . $device. "</b><br>";
-    $result = mysql_query($sql, $connect) or die(mysql_error($connect));
-    $i= 0;
-  while ($row = mysql_fetch_array($result))
-	{
-	  if($row['Description']){
-	   $status = true;
-	  }
-	}
+function checkForDupe($connect, $device){
+global $mobileRoom;
+global $mobileEa; 
+$status = false;
+$sql = "SELECT * FROM `EntertainArea` WHERE `Description` LIKE '".$device."' LIMIT 0, 30 ";
+echo "<b>Checking for duplicate Ea for device " . $device. "</b><br>";
+$result = mysql_query($sql, $connect) or die(mysql_error($connect));
+$i= 0;
+while ($row = mysql_fetch_array($result)){
+  if($row['Description']){
+    $status = true;
+    if($row['FK_Room'] != $mobileRoom){
+      echo "Location Mismatch. EA room is ".$row['FK_Room']." but Mobile QOrbiters room is ".$mobileRoom." fixing <BR> ";
+      $fixitSql ="";
+    } else {
+      $mobileEa = $row['PK_EntertainArea'];
+      echo "Found current ea for device ==> ".$mobileEa."<br>";
 
+      }	
+    }
+  }
 return $status;
 }
 
@@ -110,4 +110,17 @@ $id=mysql_insert_id($conn);
 echo $id;
 }
 
+function updateEntertainArea($connect, $device, $location){
+echo "Updating device to existing EA <br>";
+
+global $mobileEa;
+  if($mobileEa == -1){
+  echo "invalid ea, exiting updateEntertainArea()";
+  return;
+  }
+echo "updating...<br>";
+$updatesql = "UPDATE `pluto_main`.`Device_EntertainArea` SET `FK_EntertainArea` = ".$mobileEa. " WHERE `Device_EntertainArea`.`FK_Device` = ".$device;
+$result = mysql_query($updatesql, $connect) or die(mysql_error($conn));
+echo "updated. <br>";
+}
 ?>
