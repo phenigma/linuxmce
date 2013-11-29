ed5cfa397087f6ecfa32ac92469cbe5cc4703f37
phenigma <phenigma@hotmail.com>
2013-11-29 19:57:27 +0000

    Refs #1987 - LMCE_Launch_Manager config startup scripts correctly.

--- src/LMCE_Launch_Manager/mkr_postinst.sh
+++ src/LMCE_Launch_Manager/mkr_postinst.sh
@@ -14,9 +14,18 @@ for dir in /home/* ;do
 done
 
 # Make sure the old startup scripts no longer exists
-rm -f /etc/rc{2,3,4,5}.d/{S30Start_X.sh,S93startup-script.sh,S99lmce_launch_manager.sh,S93linuxmce,S99core,S99launch-manager}
-rm -f /etc/rc2.d/S99kdm
+#rm -f /etc/rc{2,3,4,5}.d/{S30Start_X.sh,S93startup-script.sh,S99lmce_launch_manager.sh,S93linuxmce,S99core,S99launch-manager}
+#rm -f /etc/rc2.d/S99kdm
+update-rc.d -f Start_X.sh remove
+update-rc.d -f startup-script.sh remove
+update-rc.d -f lmce_launch_manager.sh remove
+update-rc.d -f core remove
+update-rc.d -f launch-manager remove
+update-rc.d -f linuxmce remove
+
 
 # Add a single new startup script.
-ln -sfv /etc/init.d/linuxmce /etc/rc2.d/S99linuxmce
-ln -sfv /etc/init.d/linuxmce /etc/rc5.d/S99linuxmce
+#ln -sfv /etc/init.d/linuxmce /etc/rc2.d/S99linuxmce
+#ln -sfv /etc/init.d/linuxmce /etc/rc5.d/S99linuxmce
+update-rc.d -f linuxmce remove
+update-rc.d -f linuxmce defaults 99
