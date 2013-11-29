1484ef41838fe106a6aec218d1fa87c94f3a4e2b
phenigma <phenigma@hotmail.com>
2013-11-29 22:20:34 +0000

    Refs #1987 - MD startup script config fixes for mountnfs.sh

--- src/UbuntuDiskless/files.d/mountnfs.sh
+++ src/UbuntuDiskless/files.d/mountnfs.sh
@@ -5,63 +5,61 @@ set -e
 
 File="/etc/init.d/mountnfs.sh"
 
-INSSERV="/sbin/insserv"
-if ! [[ -x "${Parm_RootLocation}$INSSERV" ]]; then
-        INSSERV="/usr/lib/insserv/insserv"
-fi
-if ! [[ -x "${Parm_RootLocation}$INSSERV" ]]; then
-        echo "No insserv found.  Exiting."
-        exit 1
-fi
-
 mkdir -p "${Parm_RootLocation}/$(dirname $File)"
-cat >"${Parm_RootLocation}/$File" <<"EOF"
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides:          mountnfs.sh
-# Required-Start:    $remote_fs
-# Required-Stop:     $remote_fs
-# Should-Start:
-# Default-Start:     S 2
-# Default-Stop:
-# Short-Description: mountnfs.sh
-# Description:       This script is run on MDs to ensure all remote mounts are in place
-### END INIT INFO #
+cat <<-EOF >"${Parm_RootLocation}/$File"
+	#!/bin/sh
+	### BEGIN INIT INFO
+	# Provides:          mountnfs.sh
+	# Required-Start:    $remote_fs
+	# Required-Stop:     $remote_fs
+	# Should-Start:
+	# Default-Start:     S 2
+	# Default-Stop:
+	# Short-Description: mountnfs.sh
+	# Description:       This script is run on MDs to ensure all remote mounts are in place
+	### END INIT INFO #
 
-do_start () {
-	mount -a -t nfs,nfs4,smbfs,cifs,ncp,ncpfs,coda,ocfs2,gfs
-}
+	do_start () {
+		mount -a -t nfs,nfs4,smbfs,cifs,ncp,ncpfs,coda,ocfs2,gfs
+	}
 
-do_status () {
-	return 0
-}
+	do_status () {
+		return 0
+	}
 
-case "$1" in
-	start|"")
-		do_start
-		;;
-	restart|reload|force-reload)
-		echo "Error: argument '$1' not supported" >&2
-		exit 3
-                ;;
-	stop)
-		# No-op
-		;;
-	status)
-		do_status
-		exit $?
-		;;
-	*)
-		echo "Usage: $0 [start|stop|status]" >&2
-		exit 3
-		;;
-esac
-:
-EOF
+	case "$1" in
+		start|"")
+			do_start
+			;;
+		restart|reload|force-reload)
+			echo "Error: argument '$1' not supported" >&2
+			exit 3
+	                ;;
+		stop)
+			# No-op
+			;;
+		status)
+			do_status
+			exit $?
+			;;
+		*)
+			echo "Usage: $0 [start|stop|status]" >&2
+			exit 3
+			;;
+	esac
+	:
+	EOF
 
 chmod +x "${Parm_RootLocation}/${File}"
-#LC_ALL=C chroot "${Parm_RootLocation}" insserv -fv mountnfs.sh
-LC_ALL=C chroot "${Parm_RootLocation}" "${INSSERV}" -fv mountnfs.sh
+
+INSSERV=$(which insserv)
+if [[ -n "$INSSERV" ]]; then
+	# debian/raspbian (LSB)
+	LC_ALL=C chroot "${Parm_RootLocation}" "${INSSERV}" -fv mountnfs.sh >/dev/null
+else
+	# ubuntu (sysv)
+	LC_ALL=C chroot "${Parm_RootLocation}" update-rc.d -f mountnfs.sh start 30 2 3 4 5 . >/dev/null
+fi
 
 ## This script is broken so we better remove it
 rm -f "${Parm_RootLocation}"/etc/init.d/waitnfs.sh
