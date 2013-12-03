5641c21d9b7deddd15e52e51814468d0a099d6ce
phenigma <phenigma@hotmail.com>
2013-12-03 20:42:50 +0000

    Fixes #1994 - clean up mountnfs.sh and firstrun in files.d

--- src/UbuntuDiskless/files.d/mountnfs.sh
+++ src/UbuntuDiskless/files.d/mountnfs.sh
@@ -13,53 +13,66 @@ cat <<-EOF >"${Parm_RootLocation}/$File"
 	# Required-Start:    $remote_fs
 	# Required-Stop:     $remote_fs
 	# Should-Start:
-	# Default-Start:     S 2
+	# Default-Start:     2 3 4 5
 	# Default-Stop:
 	# Short-Description: mountnfs.sh
 	# Description:       This script is run on MDs to ensure all remote mounts are in place
 	### END INIT INFO #
 
-	do_start () {
+	# Source fuction library
+	. /lib/lsb/init-functions
+
+	start () {
+	        log_daemon_msg "Mounting remote filesystems" "mountnfs.sh"
 		mount -a -t nfs,nfs4,smbfs,cifs,ncp,ncpfs,coda,ocfs2,gfs
+        	err=$?
+	        log_end_msg $err
+		return $err
 	}
 
-	do_status () {
-		return 0
+	status () {
+	        status_of_proc mountnfs.sh mountnfs.sh
+		return $?
 	}
 
 	case "$1" in
 		start|"")
-			do_start
+			$1
+			exit $?
 			;;
 		restart|reload|force-reload)
 			echo "Error: argument '$1' not supported" >&2
 			exit 3
 	                ;;
 		stop)
-			# No-op
+			: # No-op
 			;;
 		status)
-			do_status
+			$1
 			exit $?
 			;;
 		*)
 			echo "Usage: $0 [start|stop|status]" >&2
 			exit 3
-			;;
 	esac
 	:
 	EOF
-
 chmod +x "${Parm_RootLocation}/${File}"
 
-INSSERV=$(which insserv)
-if [[ -n "$INSSERV" ]]; then
-	# debian/raspbian (LSB)
-	LC_ALL=C chroot "${Parm_RootLocation}" "${INSSERV}" -fv mountnfs.sh >/dev/null
-else
-	# ubuntu (sysv)
-	LC_ALL=C chroot "${Parm_RootLocation}" update-rc.d -f mountnfs.sh start 30 2 3 4 5 . >/dev/null
-fi
+DISTRO=$(LC_ALL=C chroot ${Parm_RootLocation} lsb_release -is)
+case "$DISTRO" in
+	Debian)
+		# debian/raspbian (LSB)
+                INSSERV=$(LC_ALL=C chroot ${Parm_RootLocation} which insserv)
+		LC_ALL=C chroot "${Parm_RootLocation}" "${INSSERV}" -fv mountnfs.sh >/dev/null
+		;;
+	Ubuntu)
+		# ubuntu (sysv)
+		LC_ALL=C chroot "${Parm_RootLocation}" update-rc.d -f mountnfs.sh start 30 2 3 4 5 . >/dev/null
+		;;
+        *)
+                echo "Unknown Distro!"
+esac
 
 ## This script is broken so we better remove it
 rm -f "${Parm_RootLocation}"/etc/init.d/waitnfs.sh
