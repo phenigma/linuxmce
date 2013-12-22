ffc361223c33bba9fcadc0727ac9702a91953ab4
golgoj4 <golgoj4>
2013-12-22 15:18:47 +0000

    refs #1988 - now iterating through skins directories

--- src/qOrbiter/qOrbiter_src/qml/skins-common/tools/makeQmlManifest.sh
+++ src/qOrbiter/qOrbiter_src/qml/skins-common/tools/makeQmlManifest.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
-
+clear
 #Work Area
 CURRENT_DIRECTORY=""
-target_paths=( "android" "desktop" "rpi" "qt5-desktop" "skins-common" )
+CURRENT_SKIN_PATH=""
+QML_ROOT=""
+target_paths=( "android" "desktop" "rpi" "qt5-desktop" )
+
+function processSkinDirectory(){
+
+echo "Processing $CURRENT_SKIN_PATH" 
+cd $CURRENT_SKIN_PATH
+#ls -lha
+echo "
+"
+cd $QML_ROOT
+
+}
+
 function lookupDir(){
 
 echo "Searching::$CURRENT_DIRECTORY"
-echo "
+#ls -lha $CURRENT_DIRECTORY
 
-"
+#now we go through and find skin directories
+
+for currentDir in $CURRENT_DIRECTORY/*
+do
+
+	if [ -d "$currentDir" ]; then
+	echo "$currentDir is to be processed"
+	CURRENT_SKIN_PATH="$currentDir"
+	processSkinDirectory
+	else
+	echo "$currentDir is a file and will be skipped"
+	fi
+
+done
+
+echo "Done with $CURRENT_DIRECTORY"
+
 }
 
 #----------
@@ -16,7 +46,8 @@ clear
 echo "LinuxMCE QML manifest export - Automated qmldir creator"
 echo "Switching to qml root"
 cd ../../
-ls -lha
+QML_ROOT=$(pwd)
+echo $QML_ROOT" is qml root dir"
 echo "Beginning"
 echo "Target Paths::" ${target_paths[@]}
 
