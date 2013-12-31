b6c747fccd588cace86c0eb440f9ce1b75d6dc71
phenigma <phenigma@hotmail.com>
2013-12-31 20:47:43 +0000

    Refs #1961 - change to xv/lzma from bzip2 for the diskless image compression.  No change in filename for now, as tar does properly identify the archive type and I need to find all the instances of the file name before changing any one.

--- src/UbuntuDiskless/Diskless_CreateTBZ.sh
+++ src/UbuntuDiskless/Diskless_CreateTBZ.sh
@@ -686,7 +686,7 @@ Create_Diskless_Tar () {
 	StatsMessage "Creating the compressed tar image file, this could take up to 1 hour depending on your system..."
 	mkdir -p "$ARCHIVE_DIR"
 	pushd "$TEMP_DIR" >/dev/null
-	tar -cjf "$ARCHIVE_DIR/$DisklessFS" *
+	tar -cJf "$ARCHIVE_DIR/$DisklessFS" *
 	VerifyExitCode "create tar file failed"
 	echo "$PlutoVersion" > "$ARCHIVE_DIR/$DisklessFS.version"
 	popd >/dev/null
