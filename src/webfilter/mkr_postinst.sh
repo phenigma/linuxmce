d79f5148bd9b4f2963d016c3fc3ee31c4962aec0
Alblasco1702 <ferry@alblas.tk>
2014-01-24 11:36:31 +0000

    Refs #2015

--- src/webfilter/mkr_postinst.sh
+++ src/webfilter/mkr_postinst.sh
@@ -2,20 +2,6 @@
 
 #DB
 
-#Create config setup file
-Create_Config='
-#!/bin/bash
-
-#. /usr/pluto/bin/SQL_Ops.sh
-. /usr/pluto/bin/Utils.sh
-
-#. /usr/pluto/bin/Network_Parameters.sh
-#. /usr/pluto/bin/LockUtils.sh
-
-#create the config file
-
-
-echo $Create_Config >/usr/pluto/bin/webfilter_proxy_conf.sh
 
 Alias='
 		Alias /dgblocknotice    /var/www/lmce-admin/operations/webfilter_proxy/webfilter/blocknotice.php
