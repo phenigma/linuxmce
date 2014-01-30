750552a622f3e1e9636b2a33c3abaeaef64b701b
sambuca <magnebra@online.no>
2014-01-30 19:38:22 +0000

    Last pieces to build an apk added

--- src/qOrbiter/AndroidBuilder.sh
+++ src/qOrbiter/AndroidBuilder.sh
@@ -9,12 +9,13 @@ LINUXMCE_SRC=~/lmce-trunk/src
 export ANDROID_NDK_PLATFORM=android-9
 NECESSITAS_ROOT=~/necessitas4
 export ANDROID_NDK_ROOT=$NECESSITAS_ROOT/android-ndk
-export SDK_BASE=$NECESSITAS_ROOT/android-sdk
+export ANDROID_SDK_ROOT=$NECESSITAS_ROOT/android-sdk
 OLDPATH=$PATH
 NEWPATH=$OLDPATH:$ANDROID_NDK_ROOT:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin
 NEWPATH=$NEWPATH:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools
 export PATH=$NEWPATH
 
+TARGET=debug
 
 # We reset the path at the end of the script.
 trap 'export PATH=$OLDPATH' EXIT
@@ -26,3 +27,7 @@ make clean || :
 $NECESSITAS_ROOT/Android/Qt/482/armeabi-v7a/bin/qmake Qorbiter-necessitas.pro -r -spec android-g++ "CONFIG+=opengl"
 make clean -j6
 make INSTALL_ROOT="android" install
+
+cd android
+android update project --path .
+$NECESSITAS_ROOT/apache-ant-1.8.4/bin/ant $TARGET
