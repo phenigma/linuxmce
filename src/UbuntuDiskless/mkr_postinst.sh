a0cec842a4d02c92193c3dc852995848c036eb5f
phenigma <phenigma@hotmail.com>
2014-02-17 01:39:28 +0000

    Refs #2071 - move existing deb-cache to a multi-arch friendly location

--- src/UbuntuDiskless/mkr_postinst.sh
+++ src/UbuntuDiskless/mkr_postinst.sh
@@ -16,3 +16,43 @@ update-rc.d -f haltMDs remove || /bin/true
 
 ## delete bad file from previous installs
 rm -f /etc/init/md_halt.conf
+
+
+
+## move deb-cache to newer multi-arch friendly location
+## update sourcs.list on core and all MDs to reflect new locations
+UPDATE_CACHE=""
+# Core
+HOST_DISTRO=$(lsb_release -i -s | tr '[:upper:]' '[:lower:]')
+HOST_RELEASE=$(lsb_release -c -s)
+HOST_ARCH=$(apt-config dump | grep 'APT::Architecture' | sed 's/.*"\(.*\)".*/\1/g' | head -1)
+DEB_CACHE=$HOST_DISTRO-$HOST_RELEASE-$HOST_ARCH
+if [[ -n "$(find /var/cache/apt/archives/ -iname '*.deb')" ]]; then
+	echo "Moving the Core's apt cache to /usr/pluto/deb-cache/$DEB_CACHE"
+	find /var/cache/apt/archives/ -iname '*.deb' -exec mv {} /usr/pluto/deb-cache/$DEB_CACHE \;
+	UPDATE_CACHE="$DEB_CACHE"
+fi
+# replace sources.list "/deb-cache " with "/deb-cache/$DEB_CACHE "
+sed -i "s:/deb-cache :/deb-cache/$DEB_CACHE :" /etc/apt/sources.list
+# MDs
+for Moon_RootLocation in /usr/pluto/diskless/*; do
+	if [[ -n "$(find ${Moon_RootLocation}/var/cache/apt/archives/ -iname '*.deb')" ]]; then
+		TARGET_DISTRO=$(LC_ALL="C" chroot ${Moon_RootLocation} lsb_release -i -s | tr '[:upper:]' '[:lower:]')
+		TARGET_RELEASE=$(LC_ALL="C" chroot ${Moon_RootLocation} lsb_release -c -s)
+		TARGET_ARCH=$(LC_ALL="C" chroot ${Moon_RootLocation} apt-config dump | grep 'APT::Architecture' | sed 's/.*"\(.*\)".*/\1/g' | head -1)
+		DEB_CACHE=$TARGET_DISTRO-$TARGET_RELEASE-$TARGET_ARCH
+
+		echo "Moving this MDs apt cache to /usr/pluto/deb-cache/$DEB_CACHE"
+		find ${Moon_RootLocation}/var/cache/apt/archives/ -iname '*.deb' -exec mv {} /usr/pluto/deb-cache/$DEB_CACHE \;
+		if [[ "$UPDATE_CACHE" != "*$DEB_CACHE*" ]]; then
+			UPDATE_CACHE="$UPDATE_CACHE $DEB_CACHE"
+		fi
+	fi
+	# replace sources.list "/deb-cache " with "/deb-cache/$DEB_CACHE "
+	sed -i "s:/deb-cache :/deb-cache/$DEB_CACHE :" ${Moon_RootLocation}/etc/apt/sources.list
+done
+# update package files
+for cache_name in "$UPDATE_CACHE"; do
+	echo "Updating packages list in /usr/pluto/deb-cache/$cache_name"
+	/usr/pluto/bin/update-debcache.sh /usr/pluto/deb-cache/$cache_name
+done
