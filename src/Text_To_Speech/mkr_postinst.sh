cb0b01b9270d9b795dee7ff5439881fd713314c4
phenigma <phenigma@hotmail.com>
2014-04-10 02:14:34 +0000

    Refs #2129 - change the tts voice from nitect to cmu so ivr generation works again.

--- src/Text_To_Speech/mkr_postinst.sh
+++ src/Text_To_Speech/mkr_postinst.sh
@@ -1,39 +1,63 @@
 #!/bin/bash
+. /usr/pluto/bin/SQL_Ops.sh
 
-#Check to see if the voices already exist...
-if [ -d "/usr/share/festival/voices/us" ] ; then
-	echo "Nitech HTS festival voices already installed, no action required."
+DT_TTS=57
+DD_DEFAULT_VOICE=283
+
+#Remove the old nitech voices and db setting.
+Q="SELECT PK_Device FROM Device WHERE FK_DeviceTemplate='$DT_TTS'"
+R=$(RunSQL "$Q")
+DEVICE_TTS=$(Field 1 "$R")
+
+Q="SELECT IK_DeviceData FROM Device_DeviceData WHERE FK_Device='$DEVICE_TTS' AND FK_DeviceData='$DD_DEFAULT_VOICE'"
+R=$(RunSQL "$Q")
+DEFAULT_VOICE=$(Field 1 "$R")
+
+case "$DEFAULT_VOICE" in
+	*nitech*)
+		# if the cmu voice is installed use it
+		if [ -d "/usr/share/festival/voices/us/cmu_us_slt_arctic_hts" ]; then
+			VOICE='voice_cmu_us_slt_arctic_hts'
+		else
+			VOICE=''
+		fi
+		Q="UPDATE Device_DeviceData SET IK_DeviceData='$VOICE' WHERE FK_Device='$DEVICE_TTS' AND FK_DeviceData='$DD_DEFAULT_VOICE'"
+		R=$(RunSQL "$Q")
+	;;
+esac
+
+#Check to see if the CMU voice is installed
+if [ -d "/usr/share/festival/voices/us/cmu_us_slt_arctic_hts" ] ; then
+	echo "CMU HTS festival voice already installed, no action required."
 	exit 0
 fi
 
-echo "Downloading and installing the Nitech HTS festival voices..."
+echo "Downloading and installing the CMU HTS festival voice..."
 cd /tmp/
 mkdir -p hts_tmp
 cd hts_tmp/
 #Download the voices
-wget -c http://download.linuxmce.org/Festival/festvox_nitech_us_awb_arctic_hts-2.1.tar.bz2
-wget -c http://download.linuxmce.org/Festival/festvox_nitech_us_bdl_arctic_hts-2.1.tar.bz2
-wget -c http://download.linuxmce.org/Festival/festvox_nitech_us_clb_arctic_hts-2.1.tar.bz2
-wget -c http://download.linuxmce.org/Festival/festvox_nitech_us_rms_arctic_hts-2.1.tar.bz2
-wget -c http://download.linuxmce.org/Festival/festvox_nitech_us_slt_arctic_hts-2.1.tar.bz2
-wget -c http://download.linuxmce.org/Festival/festvox_nitech_us_jmk_arctic_hts-2.1.tar.bz2
-wget -c http://download.linuxmce.org/Festival/cmu_us_kal_com_hts.tar.gz
-wget -c http://download.linuxmce.org/Festival/cstr_us_ked_timit_hts.tar.gz
+wget -c http://festvox.org/packed/festival/2.1/festvox_cmu_us_slt_arctic_hts.tar.gz
 
 #Extract the voices
-for t in $(ls); do 
+for t in $(ls); do
 	tar xvf $t
 done
 
 #Install the voices
-sudo mkdir -p /usr/share/festival/voices/us
-sudo mv lib/voices/us/* /usr/share/festival/voices/us
-sudo mv lib/hts.scm /usr/share/festival/hts.scm
+mkdir -p /usr/share/festival/voices/us
+mv festival/lib/voices/us/* /usr/share/festival/voices/us
 
 #Clean Up
 cd /tmp/
 rm -rf hts_tmp/
 
-echo "Finished installing Nitech HTS voices."
+Q="SELECT IK_DeviceData WHERE Device_DeviceData WHERE FK_Device='$DEVICE_TTS' AND FK_DeviceData='$DD_DEFAULT_VOICE'"
+R=$(RunSQL "$Q")
+DEFAULT_VOICE=$(Field 1 "$R")
+if [[ -z "$DEFAULT_VOICE" ]]; then
+	Q="UPDATE Device_DeviceData SET IK_DeviceData='voice_cmu_us_slt_arctic_hts' WHERE FK_Device='$DEVICE_TTS' AND FK_DeviceData='$DD_DEFAULT_VOICE'"
+fi
+echo "Finished installing CMU HTS voices."
 
-sudo mkdir -p /var/lib/asterisk/sounds/pluto/
+mkdir -p /var/lib/asterisk/sounds/pluto/
