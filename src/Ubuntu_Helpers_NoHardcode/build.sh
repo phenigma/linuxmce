8ab8ea97c808a474f33053e77b4424a93362e810
phenigma <phenigma@hotmail.com>
2014-04-28 01:04:04 +0000

    Refs #2121 - revert some local changes that should not be committed.

--- src/Ubuntu_Helpers_NoHardcode/build.sh
+++ src/Ubuntu_Helpers_NoHardcode/build.sh
@@ -17,5 +17,38 @@ fi
 "${build_scripts_dir}/build-maindebs.sh"
 "${build_scripts_dir}/get-closed-source-debs.sh"
 
-#"${build_scripts_dir}/create-repo.sh"
+"${build_scripts_dir}/create-repo.sh"
 
+#"${build_scripts_dir}/cd1-build.sh"
+#"${build_scripts_dir}/cd2-build.sh"
+
+if [ x"$do_not_build_sl_dvd" != x"yes" ] ; then
+
+# Build the SingleLayer DVD
+pushd /usr/local/lmce-build/vmware-install/
+	/usr/local/lmce-build/vmware-install/install_in_vmware.sh "single-layer"
+popd 
+pushd /usr/local/lmce-build/build-dvd
+	/usr/local/lmce-build/build-dvd/build-cd.sh "LinuxMCE-DVD-${arch}.iso"
+popd
+
+fi
+
+if [ x"$do_not_build_dl_dvd" != x"yes" ] ; then
+
+# Build the DoubleLayer DVD
+pushd /usr/local/lmce-build/vmware-install/
+	/usr/local/lmce-build/vmware-install/install_in_vmware.sh
+popd 
+pushd /usr/local/lmce-build/build-dvd
+	/usr/local/lmce-build/build-dvd/build-cd.sh "LinuxMCE-DVD-DL-${arch}.iso" big
+popd
+
+fi
+# push stuff into deb
+pushd /var/lmce-build/out/tmp
+dupload --to linuxmce *.changes
+popd
+
+
+wget --no-check-certificate  https://vt100.at/announce.php?text=precise\ fluffy\ is\ done -O /dev/null
