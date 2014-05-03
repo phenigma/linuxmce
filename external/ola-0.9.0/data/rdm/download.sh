8f137b83cf2fb17a40ac09c639cfe2aee547b1bf
posde <posde@theinternet.de>
2014-05-03 09:32:22 +0000

    Refs #2121 - svn contained partly configured sources. Removing it and adding the base sources. This will require a autoreconf -i before building

--- external/ola-0.9.0/data/rdm/download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Fetch the new PIDs definition files from the RDM PID Index
-wget -O pids.proto http://rdm.openlighting.org/download?pids=esta
-wget -O draft_pids.proto http://rdm.openlighting.org/download?pids=esta-draft
-wget -O manufacturer_pids.proto http://rdm.openlighting.org/download?pids=manufacturers
