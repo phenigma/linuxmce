d85d20f81892fc2e5f64bac2fcca32e088074b50
golgoj4 <golgoj4>
2014-06-15 01:20:21 +0000

    Refs #1108 - fixed path issue in osx build script

--- src/qOrbiter/qOrbiterOSX.sh
+++ src/qOrbiter/qOrbiterOSX.sh
@@ -5,9 +5,10 @@ ${QT_INSTALL_DIR:?"Need to set Qt Install Path"}
 echo "Starting builder script for QOrbiter-OSX v1"
 rm -rf build-output/qorbiter-core-gl.app 
 rm build-output/*.dmg
-
-$QT_INSTALL_DIR/bin/qmake qOrbiterMaster/qOrbiterMaster.pro
-make clean
+cd qOrbiterMaster
+$QT_INSTALL_DIR/bin/qmake qOrbiterMaster.pro 
+#make clean
 make
-$QT_INSTALL_DIR/bin/./macdeployqt build-output/qorbiter-core-gl.app -dmg
+cd ../build-output
+$QT_INSTALL_DIR/bin/./macdeployqt qorbiter-core-gl.app -dmg
 exit 0;
