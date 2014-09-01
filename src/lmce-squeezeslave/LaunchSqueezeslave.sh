77b404d30c4d58f0cabde99095006a352f9e5452
phenigma <phenigma@hotmail.com>
2014-09-01 23:59:37 +0000

    Refs #2206 - update LaunchSqueezeslave.sh to launch squeezelite if found, squeezeslave otherwise.

--- src/lmce-squeezeslave/LaunchSqueezeslave.sh
+++ src/lmce-squeezeslave/LaunchSqueezeslave.sh
@@ -7,7 +7,10 @@
 
 DEVICEDATA_Alsa_Output_Device=74
 
-SQUEEZESLAVE=/usr/bin/squeezeslave
+SQUEEZESLAVE=$(which squeezelite)
+[ -z "$SQUEEZESLAVE" ] && SQUEEZESLAVE=$(which squeezeslave)
+
+#SQUEEZESLAVE=/usr/bin/squeezeslave
 SERVER=dcerouter
 DEVICE=0
 
@@ -38,21 +41,38 @@ AUDIO_DEVICE=$(GetDeviceData "$DEVICE" "$DEVICEDATA_Alsa_Output_Device")
 OUTPUT=""
 # echo "Mac $MAC audio device $AUDIO_DEVICE"
 if [ -n "$AUDIO_DEVICE" ]; then
+  if [ "$SQUEEZESLAVE" = "$(which squeezeslave)" ]; then
     OUTPUT=$($SQUEEZESLAVE -L | grep "$AUDIO_DEVICE" | cut -d: -f1)
     #echo "$OUTPUT : $AUDIO_DEVICE"
     # Strip any spaces from the string
     OUTPUT=${OUTPUT//[[:space:]]}
+  else
+    OUTPUT="$AUDIO_DEVICE"
+  fi
 fi
 
 # Build command parameters
 PARAMS="";
 if [ -n "$OUTPUT" ]; then
-    PARAMS="$PARAMS -o$OUTPUT"
+    if [ "$SQUEEZESLAVE" = "$(which squeezelite)" ]; then
+      OUTPUT="-o $OUTPUT"
+    else
+      OUTPUT="-o$OUTPUT"
+    fi
+      PARAMS="$PARAMS $OUTPUT"
 fi
 if [ -n "$MAC" ]; then
-    PARAMS="$PARAMS -m$MAC"
+    if [ "$SQUEEZESLAVE" = "$(which squeezelite)" ]; then
+      MAC="-m $MAC"
+    else
+      MAC="-m$MAC"
+    fi
+    PARAMS="$PARAMS $MAC"
 fi
 if [ -n "$SERVER" ]; then
+    if [ "$SQUEEZESLAVE" = "$(which squeezelite)" ]; then
+      SERVER="-s $SERVER"
+    fi
     PARAMS="$PARAMS $SERVER"
 fi
 echo "Starting squeezeslave: $SQUEEZESLAVE $PARAMS"
