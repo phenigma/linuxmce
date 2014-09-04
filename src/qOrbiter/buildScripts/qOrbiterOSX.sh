cd1927a2398de5769c07bba76902294429c3f0a5
golgoj4 <golgoj4>
2014-09-04 01:45:04 +0000

    refs #2155 - More fixes in bash to make my life easier. this change provides strict checking for the presence the imports.

--- src/qOrbiter/buildScripts/qOrbiterOSX.sh
+++ src/qOrbiter/buildScripts/qOrbiterOSX.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-#QT Install Path
-${QT_INSTALL_DIR:?"Need to set Qt Install Path"}
+#This script builds qOrbiter for OS X. 
+#You must install the Mac Developer tools PRIOR to installing Qt5
+#You must set the path of your Qt Directory
 
+QTINSTALL=/Users/Langstonius/Qt-5.3.0/5.3/clang_64
+
+clear
 echo "Starting builder script for QOrbiter-OSX v1"
+cd ..
+START=$(pwd)
+
 rm -rf build-output/qorbiter-core-gl.app 
 rm build-output/*.dmg
+rm qOrbiter_src/imports/AudioVisual/*.so
+rm qOrbiter_src/imports/DceScreenSaver/*.so
+
 cd qOrbiterMaster
-$QT_INSTALL_DIR/bin/qmake qOrbiterMaster.pro 
+$QTINSTALL/bin/qmake qOrbiterMaster.pro 
 #make clean
 make
 cd ../build-output
-cp -r -v $QT_INSTALL_DIR/qml/ qorbiter-core-gl.app/Contents/MacOS/imports
-$QT_INSTALL_DIR/bin/./macdeployqt qorbiter-core-gl.app -dmg
+cp -r -v $QTINSTALL/qml/ qorbiter-core-gl.app/Contents/MacOS/imports
+$QTINSTALL/bin/./macdeployqt qorbiter-core-gl.app -dmg
 echo "build complete, check output."
 ls -lha
 exit 0;
