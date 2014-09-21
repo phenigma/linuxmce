b688ab956e0eb53d37095a8655485e45534fca75
golgoj4 <golgoj4>
2014-09-21 19:44:54 +0000

    refs #1170 - more cleanup on aisle 5.2

--- src/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/src/trunk/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/bin/AndroidManifest.xml : \
-/src/trunk/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/AndroidManifest.xml \
--- src/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/bin/QOrbiter-Qt5-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/src/trunk/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/bin/QOrbiter-Qt5-debug-unaligned.apk : \
-/src/trunk/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/bin/QOrbiter-Qt5.ap_ \
-/src/trunk/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/bin/classes.dex \
-/home/golgoj4/Android/sdk/tools/support/annotations.jar \
-/src/trunk/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/libs/armeabi-v7a/libqorbiter-5.1.0.so \
-/src/trunk/qOrbiter/qOrbiter_src/qt5droid/Qt5Android/android/libs/armeabi-v7a/gdbserver \
