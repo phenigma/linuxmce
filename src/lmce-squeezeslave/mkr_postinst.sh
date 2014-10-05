16b2d15885506dafeba849dad26bdbbe43ae5b0f
phenigma <phenigma@hotmail.com>
2014-10-05 02:50:29 +0000

    Fixes #2232 - use the dir that is created.

--- src/lmce-squeezeslave/mkr_postinst.sh
+++ src/lmce-squeezeslave/mkr_postinst.sh
@@ -1,3 +1,3 @@
 #!/bin/dash
 mkdir -p /usr/pluto/diversions
-dpkg-divert --divert /usr/pluto/diversions/squeezelite.distrib/etc/init.d/squeezelite --rename /etc/init.d/squeezelite
+dpkg-divert --divert /usr/pluto/diversions/squeezelite --rename /etc/init.d/squeezelite
