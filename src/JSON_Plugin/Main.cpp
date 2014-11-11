96a4d601a8f28ebdb1330eb16c96cb281bf997a2
posde <posde@theinternet.de>
2014-11-11 19:21:41 +0000

    Refs #JSON Plugin - Modify main.cpp to be in line with how phenigma designed the new versioning without search and replace

--- src/JSON_Plugin/Main.cpp
+++ src/JSON_Plugin/Main.cpp
@@ -22,11 +22,7 @@
 #include "PlutoUtils/Other.h"
 #include "DCERouter.h"
 
-// In source files stored in archives and packages, these 2 lines will have the release version (build)
-// and the svn revision as a global variable that can be inspected within a core dump
-const char *g_szCompile_Date=COMPILE_DATE;
-/*SVN_REVISION*/
-
+#include "version.h"
 
 using namespace DCE;
 
