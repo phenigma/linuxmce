27cb2c4815f260d938de4e5117e3f50d79e6122a
Alblasco1702 <ferry@alblas.tk>
2014-11-27 17:17:08 +0000

    refs #2196internetradio language file

---
+++ web/lmce-admin/languages/en/internetradio.lang.php
@@ -0,0 +1,5 @@
+<?
+$TEXT_INTERNETRADIO_STATIONS_CONST='Internet radio stations';
+$TEXT_INTERNETRADIO_STATIONS_NOT_UPDATED_STATION_AL_READY_EXIST_CONST='Internet radio stations not updated station al ready exist.';
+$TEXT_INTERNETRADIO_STATIONS_UPDATED_CONST='Internet radio stations updated.';
+?>
