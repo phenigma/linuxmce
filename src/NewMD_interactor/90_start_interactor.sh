167d205d21c0204499e318d55bcdfb82bb691607
phenigma <phenigma@hotmail.com>
2015-03-24 21:10:55 +0000

    Refs #2327 - update interactor startup script for disked mds, add interactor-disked.c

--- src/NewMD_interactor/90_start_interactor.sh
+++ src/NewMD_interactor/90_start_interactor.sh
@@ -1,16 +1,33 @@
 #!/bin/dash
 
+if [ -f /etc/Disked_DeviceID ]; then
+	echo "We have a DeviceID already, bypassing interactor."
+  exit 0
+fi
+
 MyIP=$(/sbin/ifconfig eth0 | awk 'NR==2 { print substr($2, index($2, ":") + 1) }')
 MyMAC=$(/sbin/ifconfig eth0 | awk 'NR==1 { print $5 }')
 Gateway=$(/sbin/route -n | awk '/^0\.0\.0\.0/ { print $2 }')
 
-Architecture="686"
-is2ndCpu=$(awk '/processor.*: 1/' /proc/cpuinfo)
-if [ -n "$is2ndCpu"  ] ;then
-	Architecture="686-smp"
-fi
+ARCH=$(arch)
+case $ARCH in
+	i386|i686)
+		Architecture="i386"
+		;;
+	amd64)
+		Architecture="i386"
+		;;
+	armhf)
+		Architecture="armhf"
+		;;
+	*)
+		echo "ERR: unknown arch $ARCH"
+		return 1
+esac
 
-echo "IP: $MyIP; MAC: $MyMAC; Gateway: $Gateway; Architecture: $Architecture"
+devicedata=""
+MyDeviceData="$Architecture""$devicedata"
 
+echo "IP: $MyIP; MAC: $MyMAC; Gateway: $Gateway; DeviceData: $MyDeviceData"
 
-/sbin/interactor "$Gateway" "$MyIP" "$MyMAC" "$Architecture"
+/sbin/interactor "$Gateway" "$MyIP" "$MyMAC" "$MyDeviceData"
