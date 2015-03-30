fbb8068cde26606fe8d6422bf3bc4f2e4af16f88
phenigma <phenigma@hotmail.com>
2015-03-30 06:08:35 +0000

    Refs #2328 - DCEGen for Clock Screen Saver

---
+++ src/Clock_Screen_Saver/Clock_Screen_Saver.h
@@ -0,0 +1,92 @@
+/*
+     Copyright (C) 2013 LinuxMCE 
+
+     www.linuxmce.org
+
+
+     This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License.
+     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+     of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+     See the GNU General Public License for more details.
+
+*/
+//<-dceag-d-b->
+#ifndef Clock_Screen_Saver_h
+#define Clock_Screen_Saver_h
+
+//	DCE Implemenation for #2313 Clock Screen Saver
+
+#include "Gen_Devices/Clock_Screen_SaverBase.h"
+//<-dceag-d-e->
+
+//<-dceag-decl-b->
+namespace DCE
+{
+	class Clock_Screen_Saver : public Clock_Screen_Saver_Command
+	{
+//<-dceag-decl-e->
+		// Private member variables
+
+		// Private methods
+public:
+		// Public member variables
+
+//<-dceag-const-b->
+public:
+		// Constructors/Destructor
+		Clock_Screen_Saver(int DeviceID, string ServerAddress,bool bConnectEventHandler=true,bool bLocalMode=false,class Router *pRouter=NULL);
+		virtual ~Clock_Screen_Saver();
+		virtual bool GetConfig();
+		virtual bool Register();
+		virtual void ReceivedCommandForChild(DeviceData_Impl *pDeviceData_Impl,string &sCMD_Result,Message *pMessage);
+		virtual void ReceivedUnknownCommand(string &sCMD_Result,Message *pMessage);
+//<-dceag-const-e->
+
+//<-dceag-const2-b->
+		// The following constructor is only used if this a class instance embedded within a DCE Device.  In that case, it won't create it's own connection to the router
+		// You can delete this whole section and put an ! after dceag-const2-b tag if you don't want this constructor.  Do the same in the implementation file
+		Clock_Screen_Saver(Command_Impl *pPrimaryDeviceCommand, DeviceData_Impl *pData, Event_Impl *pEvent, Router *pRouter);
+//<-dceag-const2-e->
+
+//<-dceag-h-b->
+	/*
+				AUTO-GENERATED SECTION
+				Do not change the declarations
+	*/
+
+	/*
+			*****DATA***** accessors inherited from base class
+
+			*****EVENT***** accessors inherited from base class
+
+			*****COMMANDS***** we need to implement
+	*/
+
+
+	/** @brief COMMAND: #192 - On */
+	/** Turn on the device */
+		/** @param #97 PK_Pipe */
+			/** Normally when a device is turned on all the inputs and outputs are selected automatically.  If this parameter is specified, only the settings along this pipe will be set. */
+		/** @param #98 PK_Device_Pipes */
+			/** Normally when a device is turned on the corresponding "pipes" are enabled by default. if this parameter is blank.  If this parameter is 0, no pipes will be enabled.  This can also be a comma seperated list of devices, meaning only the pipes to those devic */
+
+	virtual void CMD_On(int iPK_Pipe,string sPK_Device_Pipes) { string sCMD_Result; CMD_On(iPK_Pipe,sPK_Device_Pipes.c_str(),sCMD_Result,NULL);};
+	virtual void CMD_On(int iPK_Pipe,string sPK_Device_Pipes,string &sCMD_Result,Message *pMessage);
+
+
+	/** @brief COMMAND: #193 - Off */
+	/** Turn off the device */
+		/** @param #97 PK_Pipe */
+			/** Normally when a device is turned on all the inputs and outputs are selected automatically.  If this parameter is specified, only the settings along this pipe will be set. */
+
+	virtual void CMD_Off(int iPK_Pipe) { string sCMD_Result; CMD_Off(iPK_Pipe,sCMD_Result,NULL);};
+	virtual void CMD_Off(int iPK_Pipe,string &sCMD_Result,Message *pMessage);
+
+//<-dceag-h-e->
+	};
+
+//<-dceag-end-b->
+}
+#endif
+//<-dceag-end-e->
