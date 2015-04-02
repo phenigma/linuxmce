f3b44d934101887eef0237508720c508da03d8d4
phenigma <phenigma@hotmail.com>
2015-04-02 20:19:36 +0000

    Refs #2327 - initial files to auto-install && create devices like MID Orbiters (i.e., jogglers).

--- src/MID_Orbiter/base/mkr_postinst.sh
+++ src/MID_Orbiter/base/mkr_postinst.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # setup the interactor and the firstboot script
-update-rc.d -f 90_start_interactor.sh start 90 2 3 4 5 . >/dev/null
 update-rc.d -f firstboot start 91 2 3 4 5 . >/dev/null
 
 ###########################################################
