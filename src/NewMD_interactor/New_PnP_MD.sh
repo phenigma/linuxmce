dded3296bbce1ecc8b9eb63cdc825edd3e4b32df
phenigma <phenigma@hotmail.com>
2015-04-05 09:12:47 +0000

    Refs #2327 - update New_PnP_MD to deal with disked MD/non-MD devices as required.  Give user a short countdown to view status prior to rebooting.

--- src/NewMD_interactor/New_PnP_MD.sh
+++ src/NewMD_interactor/New_PnP_MD.sh
@@ -10,6 +10,10 @@ RemoteIP="$1"
 RemoteMAC="$2"
 MD_Device="$3"
 
+bright="[1m"
+color="[1;31m"
+normal="[0m"
+
 FeedbackMD()
 {
 	echo "msg $1" | nc -n -q 0 "$RemoteIP" "$Interactor_Port"
@@ -31,31 +35,48 @@ FeedbackMD "Assigned permanent IP '$MD_IP' to device '$MD_Device'"
 
 WaitLock "NewPnPMD" "NewPnPMD$MD_Device"
 Logging "$TYPE" "$SEVERITY_NORMAL" "Running diskless MD setup command suite (device '$MD_Device')"
+
+fail=0
 # Note: keep this in sync with /var/www/lmce-admin/operations/logs/executeLog.php, case 1
 FeedbackMD "Running Diskless_Setup.sh"
 if ! /usr/pluto/bin/Diskless_Setup.sh; then
-	bright="[1m"
-	color="[1;31m"
-	normal="[0m"
-
 	Logging "$TYPE" "$SEVERITY_CRITICAL" "Diskless Setup failed"
 	FeedbackMD "${color}${bright}Diskless Setup failed${normal}"
-	Unlock "NewPnPMD" "NewPnPMD$MD_Device"
-	exit 1
+	fail=1
 fi
 
 FeedbackMD "Running Disked_Setup.sh"
-/usr/pluto/bin/Disked_Setup.sh
+if ! /usr/pluto/bin/Disked_Setup.sh; then
+	Logging "$TYPE" "$SEVERITY_CRITICAL" "Disked Setup failed"
+	FeedbackMD "${color}${bright}Diskless Setup failed${normal}"
+	fail=2
+fi
+
+if [[ "$fail" != "0" ]] ; then
+	Unlock "NewPnPMD" "NewPnPMD$MD_Device"
+	exit $fail
+fi
 
 FeedbackMD "Running DHCP_config.sh"
 /usr/pluto/bin/DHCP_config.sh
+
 FeedbackMD "Running Diskless_ExportsNFS.sh"
 /usr/pluto/bin/Diskless_ExportsNFS.sh
+
 # End of list to be kept in sync
 Logging "$TYPE" "$SEVERITY_NORMAL" "Finished running diskless MD setup command suite (device '$MD_Device')"
 Unlock "NewPnPMD" "NewPnPMD$MD_Device"
 
 Logging "$TYPE" "$SEVERITY_NORMAL" "Rebooting new MD"
-FeedbackMD "Rebooting in 5 seconds"
-sleep 5
+FeedbackMD "Rebooting in 5 seconds..."
+sleep 1
+FeedbackMD "Rebooting in 4 seconds..."
+sleep 1
+FeedbackMD "Rebooting in 3 seconds..."
+sleep 1
+FeedbackMD "Rebooting in 2 seconds..."
+sleep 1
+FeedbackMD "Rebooting in 1 seconds..."
+sleep 1
+FeedbackMD "Rebooting"
 echo "reboot" | nc -n "$RemoteIP" "$Interactor_Port"
