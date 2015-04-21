152a9a95da13da9962977676cfaf3b19c4276ff1
phenigma <phenigma@hotmail.com>
2015-04-21 00:36:12 +0000

    Refs #2327 - fix random mac problem on the jogglers sqpos

--- src/MID/joggler/mkr_postinst.sh
+++ src/MID/joggler/mkr_postinst.sh
@@ -10,5 +10,38 @@ echo "2176" > $DTVENDOR_FILE
 echo "newmd" > $COMMAND_FILE
 echo "100|800|101|480" > $DEVDATA_FILE
 
+# ensure the mac address stays consistent, without this it randomizes at boot
+cat <<-EEOF > /etc/network/if-pre-up.d/openframe-mac
+	#!/bin/bash
 
+	# Ensure udev doesn't join the party.
+	[ -f /etc/udev/rules.d/70-persistent-net.rules ] && rm /etc/udev/rules.d/70-persistent-net.rules
+	[ -f /etc/udev/rules.d/75-persistent-net-generator.rules ] || touch /etc/udev/rules.d/75-persistent-net-generator.rules
+
+	if [ "$IFACE" = "eth0" ]; then
+		if [ -f /etc/network/mac_eth0 ]; then
+			ETHMAC=`cat /etc/network/mac_eth0`
+		else
+			ETHMAC="02:`cat /proc/interrupts | md5sum | sed -r "s/(.{2})/\1:/g; s/^(.{14}).*/\1/"`"
+			echo $ETHMAC > /etc/network/mac_eth0
+		fi
+
+		ip link set eth0 address $ETHMAC
+		echo $ETHMAC > /tmp/mac_eth0
+	fi
+
+	if [ "$IFACE" = "wlan0" ]; then
+		if [ -f /etc/network/mac_wlan0 ]; then
+			ETHMAC=`cat /etc/network/mac_wlan0`
+		else
+			ETHMAC="00:`cat /proc/interrupts | md5sum | sed -r "s/(.{2})/\1:/g; s/^(.{14}).*/\1/"`"
+			echo $ETHMAC > /etc/network/mac_wlan0
+		fi
+
+		ip link set wlan0 address $ETHMAC
+		echo $ETHMAC > /tmp/mac_wlan0
+	fi
+
+	exit 0
+	EEOF
 
