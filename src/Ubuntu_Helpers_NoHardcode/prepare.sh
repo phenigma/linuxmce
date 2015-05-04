eb9dea38259ab97e0a504d045c7eb427a07e25ac
phenigma <phenigma@hotmail.com>
2015-05-04 09:31:21 +0000

    Refs #2350 - no longer necessary to call import-external-files.sh

--- src/Ubuntu_Helpers_NoHardcode/prepare.sh
+++ src/Ubuntu_Helpers_NoHardcode/prepare.sh
@@ -8,6 +8,6 @@ prepare_scripts_dir="/usr/local/lmce-build/prepare-scripts"
 #"${prepare_scripts_dir}/cd2-prepare.sh"
 "${prepare_scripts_dir}/install-build-pkgs.sh"
 "${prepare_scripts_dir}/create-diskless-debootstrap.sh"
-"${prepare_scripts_dir}/import-external-files.sh"
+#"${prepare_scripts_dir}/import-external-files.sh"
 "${prepare_scripts_dir}/preseed-build-pkgs.sh"
 
