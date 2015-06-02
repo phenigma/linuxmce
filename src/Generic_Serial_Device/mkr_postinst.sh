9659b958c136fb7323d89e975299d9f2f9198425
phenigma <phenigma@hotmail.com>
2015-06-02 01:54:15 +0000

    Refs #2121 - add postinst and prerm scripts to manipulate common.rb for GSD with ruby 1.9.1/2.0

---
+++ src/Generic_Serial_Device/mkr_postinst.sh
@@ -0,0 +1,3 @@
+cp /usr/pluto/templates/common.rb /usr/lib/ruby/1.9.1/uri/common.rb || :
+cp /usr/pluto/templates/common.rb /usr/lib/ruby/2.0.0/uri/common.rb || :
+:
---
+++ src/Generic_Serial_Device/mkr_prerm.sh
@@ -0,0 +1,3 @@
+rm -f /usr/lib/ruby/1.9.1/uri/common.rb || :
+rm -f /usr/lib/ruby/2.0.0/uri/common.rb || :
+:
