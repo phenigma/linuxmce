c9c6a8fbefe515ca62edd5b0dad5ffb177a1f606
tschak <thom.cherryhomes@gmail.com>
2015-07-26 04:59:31 +0000

    Refs #1235 - Gen_Devices fix, command removed.

--- src/Gen_Devices/Wii_Remote_ControllerBase.h
+++ src/Gen_Devices/Wii_Remote_ControllerBase.h
@@ -386,32 +386,6 @@ public:
 					};
 					iHandled++;
 					continue;
-				case COMMAND_Connect_to_Device_CONST:
-					{
-						string sCMD_Result="OK";
-						string sMac_address=pMessage->m_mapParameters[COMMANDPARAMETER_Mac_address_CONST];
-						CMD_Connect_to_Device(sMac_address.c_str(),sCMD_Result,pMessage);
-						if( pMessage->m_eExpectedResponse==ER_ReplyMessage && !pMessage->m_bRespondedToMessage )
-						{
-							pMessage->m_bRespondedToMessage=true;
-							Message *pMessageOut=new Message(m_dwPK_Device,pMessage->m_dwPK_Device_From,PRIORITY_NORMAL,MESSAGETYPE_REPLY,0,0);
-							pMessageOut->m_mapParameters[0]=sCMD_Result;
-							SendMessage(pMessageOut);
-						}
-						else if( (pMessage->m_eExpectedResponse==ER_DeliveryConfirmation || pMessage->m_eExpectedResponse==ER_ReplyString) && !pMessage->m_bRespondedToMessage )
-						{
-							pMessage->m_bRespondedToMessage=true;
-							SendString(sCMD_Result);
-						}
-						if( (itRepeat=pMessage->m_mapParameters.find(COMMANDPARAMETER_Repeat_Command_CONST))!=pMessage->m_mapParameters.end() )
-						{
-							int iRepeat=atoi(itRepeat->second.c_str());
-							for(int i=2;i<=iRepeat;++i)
-								CMD_Connect_to_Device(sMac_address.c_str(),sCMD_Result,pMessage);
-						}
-					};
-					iHandled++;
-					continue;
 				}
 				iHandled += (Command_Impl::ReceivedMessage(pMessage)==rmr_NotProcessed ? 0 : 1);
 			}
