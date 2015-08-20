7d1187bbc98673197c291f6bab0eadf9894a1157
phenigma <phenigma@hotmail.com>
2015-08-20 20:20:58 +0000

    Refs #2428 - remove the LMCEUpdate script from init.d if found

--- src/Update/mkr_postinst.sh
+++ src/Update/mkr_postinst.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-update-rc.d -f LMCEUpdate remove >/dev/null
-update-rc.d -f LMCEUpdate start 98 2 3 4 5 . >/dev/null
+#called from init.d/linuxmce now
+#update-rc.d -f LMCEUpdate remove >/dev/null
+#update-rc.d -f LMCEUpdate start 98 2 3 4 5 . >/dev/null
+if [[ -e /etc/init.d/LMCEUpdate ]]; then
+        update-rc.d -f LMCEUpdate remove >/dev/null
+        rm -f /etc/init.d/LCMEUpdate
+fi
 
