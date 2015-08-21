655013e172d84c718f9c0da1c3ca69cc210e14dd
phenigma <phenigma@hotmail.com>
2015-08-21 04:40:01 +0000

    Refs #2325 - clean old installer scripts from pkg

--- src/ConfirmDependencies_Script_Offline/VIA_Setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# add pci=conf1 to VIA kernel in GRUB
-if ! grep -q "^# kopt=.* pci=conf1" /boot/grub/menu.lst; then
-	oldKopt=$(grep "^# kopt=.*" /boot/grub/menu.lst)
-	newKopt="$oldKopt pci=conf1"
-	sed -i "s~$oldKopt~$newKopt~g" /boot/grub/menu.lst
-	update-grub
-fi
