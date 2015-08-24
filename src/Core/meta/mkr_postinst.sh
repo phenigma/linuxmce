344a5e1e1c873026edc84ae3065000385809e6ec
phenigma <phenigma@hotmail.com>
2015-08-24 01:47:34 +0000

    Refs #2325 - core-meta postinst updates, detect networking during install and firstboot.

--- src/Core/meta/mkr_postinst.sh
+++ src/Core/meta/mkr_postinst.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . /usr/pluto/bin/Utils.sh
+. /usr/pluto/bin/SQL_Ops.sh
 . /usr/pluto/install/install-core.sh
 
 ###. /usr/pluto/install/install-core.sh ; Config_MySQL_Server
@@ -25,6 +26,45 @@ if ! BlacklistConfFiles '/etc/hostname' ;then
 	fi
 fi
 
-Setup_Pluto_Conf	# lmce-install-scripts - install-core.sh
+###. /usr/pluto/install/install-core.sh ; Setup_Pluto_Conf	# lmce-install-scripts - install-core.sh
+if ! BlacklistConfFiles '/etc/pluto.conf' ;then
+	StatsMessage "Generating default /etc/pluto.conf file"
+	touch /etc/pluto.conf &>/dev/null
+	chmod 777 /etc/pluto.conf &>/dev/null
+	. /usr/pluto/bin/Config_Ops.sh
+
+	[ -n "$MySqlHost" ] || ConfSet "MySqlHost" "localhost"
+	[ -n "$MySqlUser" ] || ConfSet "MySqlUser" "root"
+	[ -n "$MySqlPassword" ] || ConfSet "MySqlPassword" ""
+	[ -n "$MySqlDBName" ] || ConfSet "MySqlDBName" "pluto_main"
+	[ -n "$DCERouter" ] || ConfSet "DCERouter" "localhost"
+	[ -n "$MySqlPort" ] || ConfSet "MySqlPort" "3306"
+	[ -n "$DCERouterPort" ] || ConfSet "DCERouterPort" "3450"
+	[ -n "$PK_Device" ] || ConfSet "PK_Device" "1"
+	[ -n "$Activation_Code" ] || ConfSet "Activation_Code" "1111"
+	[ -n "$PK_Installation" ] || ConfSet "PK_Installation" "1"
+	[ -n "$PK_Users" ] || ConfSet "PK_Users" "1"
+	[ -n "$PK_Distro" ] || ConfSet "PK_Distro" "$TARGET_DISTRO_ID"
+	[ -n "$UseVideoWizard" ] || ConfSet "UseVideoWizard" "1"
+	[ -n "$LogLevels" ] || ConfSet "LogLevels" "1,5,7,8"
+	[ -n "$AutostartCore" ] || ConfSet "AutostartCore" "1"
+	[ -n "$AutostartMedia" ] || ConfSet "AutostartMedia" "0"
+	[ -n "$LTS_HES" ] || ConfSet "LTS_HES" "$TARGET_LTS_HES"
+fi
+
+# Do we have an installation ID already?
+Q="SELECT COUNT(*) FROM Installation"
+R=RunSQL "$Q"
+if [ "$R" -lt "1" ] ; then
+	## Update some info in the database
+	Q="INSERT INTO Installation(Description, ActivationCode) VALUES('LinuxMCE', '1111')"
+	RunSQL "$Q"
+fi
+
+# If this is initial install or firstboot then do network detection/setup
+if [ "$Firstboot" != "false" ] ; then
+	Nic_Config			# install-core.sh
+	Configure_Network_Options	# install-core.sh
+fi
 
 exit 0
