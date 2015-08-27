80f2cebc83d907ee32092aec36fe9ed388b57af8
phenigma <phenigma@hotmail.com>
2015-08-27 00:31:23 +0000

    Refs #2325 - don't generate voicemail menus if there are no users.

--- src/BootScripts/SetupUsers.sh
+++ src/BootScripts/SetupUsers.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-echo "$(date -R) SetupUsers" >> /var/log/pluto/SetupUsers.log
-cd /usr/pluto/bin
-bash -x ./SetupUsers_Homes.sh >> /var/log/pluto/SetupUsers.log  2>&1
-if [ -x /usr/pluto/bin/generate_users_voicemenu.pl ]
-then
-	./generate_users_voicemenu.pl
-fi
+. /usr/pluto/bin/SQL_Ops.sh
 
+Q="SELECT COUNT(*) FROM Users"
+R=$(RunSQL "$Q")
+
+if [[ "$R" != "0" ]] ; then
+	echo "$(date -R) SetupUsers" >> /var/log/pluto/SetupUsers.log
+	cd /usr/pluto/bin
+	bash -x ./SetupUsers_Homes.sh >> /var/log/pluto/SetupUsers.log  2>&1
+	if [ -x /usr/pluto/bin/generate_users_voicemenu.pl ]
+	then
+		./generate_users_voicemenu.pl
+	fi
+fi
