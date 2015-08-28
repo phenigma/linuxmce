86a64a36a80b8366c92e139959e334e7acc46a7a
phenigma <phenigma@hotmail.com>
2015-08-28 05:19:30 +0000

    Refs #2325 - new standalone install script that does... well... only what it needs to :D

---
+++ src/install-helpers/mce-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISTRO="$(lsb_release -c -s)"
+COMPOS="main"
+
+echo >>/etc/apt/sources.list
+echo "http://deb.linuxmce.org/ubuntu/ ${DISTRO} ${COMPOS}" >>/etc/apt/sources.list
+
+apt-get -q update
+apt-get -q --allow-unauthenticated install lmce-install-scripts
+
+. /usr/pluto/install/install-core.sh
+
+UpdateDebCache
+ConfigSources
+gpgUpdate
+TimeUpdate
+
+apt-get -q --allow-unauthenticated install lmce-hybrid
