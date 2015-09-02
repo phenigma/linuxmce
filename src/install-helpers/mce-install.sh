db5cab75dfd075b43fe55973f9db3cd484111307
phenigma <phenigma@hotmail.com>
2015-09-02 02:07:56 +0000

    Refs #2325 - error in deb entry

--- src/install-helpers/mce-install.sh
+++ src/install-helpers/mce-install.sh
@@ -4,7 +4,7 @@ DISTRO="$(lsb_release -c -s)"
 COMPOS="main"
 
 echo >>/etc/apt/sources.list
-echo "http://deb.linuxmce.org/ubuntu/ ${DISTRO} ${COMPOS}" >>/etc/apt/sources.list
+echo "deb http://deb.linuxmce.org/ubuntu/ ${DISTRO} ${COMPOS}" >>/etc/apt/sources.list
 
 apt-get -q update
 apt-get -q --allow-unauthenticated install lmce-install-scripts
