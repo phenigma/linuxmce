80aacd9b641987b2fb5b10e29a368215ee9e5861
phenigma <phenigma@hotmail.com>
2015-09-02 03:24:10 +0000

    Refs #2325 - do not ask Y/n

--- src/install-helpers/mce-install.sh
+++ src/install-helpers/mce-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DISTRO="$(lsb_release -c -s)"
+DISTRO="$(lsb_release -cs)"
 COMPOS="main"
 
 echo >>/etc/apt/sources.list
 echo "deb http://deb.linuxmce.org/ubuntu/ ${DISTRO} ${COMPOS}" >>/etc/apt/sources.list
 
 apt-get -q update
-apt-get -q --allow-unauthenticated install lmce-install-scripts
+apt-get -y -q --allow-unauthenticated install lmce-install-scripts
 
 . /usr/pluto/install/install-core.sh
 
