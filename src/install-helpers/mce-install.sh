8483a6af7292e6ccfb1c69299c9888912b8ef1a6
phenigma <phenigma@hotmail.com>
2015-09-03 02:27:36 +0000

    Refs #2325 - remove extraneous fns from install script.

--- src/install-helpers/mce-install.sh
+++ src/install-helpers/mce-install.sh
@@ -8,12 +8,4 @@ echo >>/etc/apt/sources.list
 echo "deb http://deb.linuxmce.org/${TARGET_DISTRO}/ ${TARGET_RELEASE} ${REPO}" >>/etc/apt/sources.list
 
 apt-get -q update
-apt-get -y -q --allow-unauthenticated install lmce-install-scripts
-
-. /usr/pluto/install/install-core.sh
-
-UpdateDebCache
-ConfigSources
-gpgUpdate
-
-apt-get -y -q --allow-unauthenticated install lmce-hybrid
+DEBIAN_FRONTEND="noninteractive" apt-get -y -q --allow-unauthenticated install lmce-hybrid
