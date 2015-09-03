cee801824055adb2be50abf8ca88bd73afd70e90
phenigma <phenigma@hotmail.com>
2015-09-03 03:43:25 +0000

    Refs #2325 - create a log of the installation automatically.

--- src/install-helpers/mce-install.sh
+++ src/install-helpers/mce-install.sh
@@ -8,4 +8,4 @@ echo >>/etc/apt/sources.list
 echo "deb http://deb.linuxmce.org/${TARGET_DISTRO}/ ${TARGET_RELEASE} ${REPO}" >>/etc/apt/sources.list
 
 apt-get -q update
-DEBIAN_FRONTEND="noninteractive" apt-get -y -q --allow-unauthenticated install lmce-hybrid
+DEBIAN_FRONTEND="noninteractive" apt-get -y -q --allow-unauthenticated install lmce-hybrid | tee $(basename $0 | cut -d'.' -f1).log
