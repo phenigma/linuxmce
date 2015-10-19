323b701fc306db3eba21c451c8f38995e5b3acc1
phenigma <phenigma@hotmail.com>
2015-10-19 17:19:28 +0000

    Refs #2472 - ensure our config is out of the way before trying to dpkg-divert to return the original file.  Abundance of caution, issue reported once.

--- config-pkgs/lmce_mediatomb/mkr_postrm.sh
+++ config-pkgs/lmce_mediatomb/mkr_postrm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ## Revove divertion for config.xml, as we have our own configuration file.
 
+[[ -f /etc/mediatomb/config.xml ]] && rm -f /etc/mediatomb/config.xml || :
 dpkg-divert --remove --rename --package lmce-mediatomb --divert /etc/mediatomb/config.xml.wrapped /etc/mediatomb/config.xml
