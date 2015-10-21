c276a5d53b0a2d551fc7401f846246708094aaf8
phenigma <phenigma@hotmail.com>
2015-10-21 04:47:47 +0000

    Refs #2121 - don't built game player on trusty

--- src/Ubuntu_Helpers_NoHardcode/build-scripts/build-maindebs.sh
+++ src/Ubuntu_Helpers_NoHardcode/build-scripts/build-maindebs.sh
@@ -117,6 +117,7 @@ function build_main_debs() {
 					Distro_ID="21"
 					RepositorySource=25
 					Main_Version='2.0.0.47.'
+					exclude_list=$exclude_list,673,674 # lmce game player
 					case "${arch}" in
 						"armhf")
 							exclude_list=$exclude_list,452,453 # IRTrans - no armhf .so
