4292896c77933f7cb280a7ca3c35f58a6cf4fff0
phenigma <phenigma@hotmail.com>
2015-10-25 22:40:12 +0000

    Refs #2472 - ensure the config.xml is in place to chown it, accidentally disabled that in last change.

--- config-pkgs/lmce_mediatomb/mkr_postinst.sh
+++ config-pkgs/lmce_mediatomb/mkr_postinst.sh
@@ -14,10 +14,11 @@ RunSQL "$Q"
 
 ## TODO: WHAT IS THIS DOING HERE?
 #route del -net 239.0.0.0 netmask 255.0.0.0 || /bin/true
-#cp /etc/mediatomb/config.xml.dpkg-dist /etc/mediatomb/config.xml
+
+[[ ! -f /etc/mediatomb/config.xml ]] && cp /etc/mediatomb/config.xml.dpkg-dist /etc/mediatomb/config.xml || :
 
 # ensure the config file is owned by the mediatomb user
-chown mediatomb:mediatomb /etc/mediatomb/config.xml
+chown mediatomb:mediatomb /etc/mediatomb/config.xml || :
 
 # restart the service
 service mediatomb stop || :
