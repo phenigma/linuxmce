155c0a767eabe6e4e033e5d614226a735337ac94
phenigma <phenigma@hotmail.com>
2015-10-31 03:26:17 +0000

    Refs #2470 - libcec3 for raspbian jessie from debian jessie backports

--- raspbian/libcec-3.0.1/cmake/UseMultiArch.cmake
+++ raspbian/libcec-3.0.1/cmake/UseMultiArch.cmake
@@ -14,15 +14,12 @@
 
 # Fedora uses lib64/ for 64-bit systems, Debian uses lib/x86_64-linux-gnu;
 # Fedora put module files in lib64/ too, but Debian uses lib/ for that
-if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux" AND
-    "${CMAKE_INSTALL_PREFIX}" STREQUAL "/usr")
+if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
   # Debian or Ubuntu?
   if (EXISTS "/etc/debian_version")
 	set (_libdir_def "lib/${CMAKE_LIBRARY_ARCHITECTURE}")
 	set (_libdir_noarch "lib")
-  elseif (EXISTS "/etc/fedora-release" OR
-          EXISTS "/etc/redhat-release" OR
-          EXISTS "/etc/slackware-version")
+  else (EXISTS "/etc/debian_version")
 	# 64-bit system?
 	if (CMAKE_SIZEOF_VOID_P EQUAL 8)
 	  set (_libdir_noarch "lib64")
@@ -30,14 +27,11 @@ if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux" AND
 	  set (_libdir_noarch "lib")
 	endif (CMAKE_SIZEOF_VOID_P EQUAL 8)
 	set (_libdir_def "${_libdir_noarch}")
-  else ()
-    set (_libdir_def "lib")
-    set (_libdir_noarch "lib")
-  endif ()
-else ()
+  endif (EXISTS "/etc/debian_version")
+else ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
   set (_libdir_def "lib")
   set (_libdir_noarch "lib")
-endif ()
+endif ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
 
 # let the user override if somewhere else is desirable
 set (CMAKE_INSTALL_LIBDIR "${_libdir_def}" CACHE PATH "Object code libraries")
@@ -46,4 +40,3 @@ mark_as_advanced (
   CMAKE_INSTALL_LIBDIR
   CMAKE_INSTALL_LIBDIR_NOARCH
   )
-
