85d57e9e8a0f246ddda1254499910eade5c5e628
phenigma <phenigma@hotmail.com>
2015-11-27 01:56:02 +0000

    Refs #2509 - this is not required, comment for now

--- src/Core/meta/mkr_preinst.sh
+++ src/Core/meta/mkr_preinst.sh
@@ -17,21 +17,21 @@ if [ install = "$1" ]; then
 			EOF
 	fi
 
-	# Put a temporary nis config file that will prevent ypbind to start
-	# Temporary NIS setup, disabling NIS server and client.
-	if [ ! -f /etc/default/nis ] ; then
-		cat <<-EOF >/etc/default/nis
-			NISSERVER=false
-			NISCLIENT=false
-			YPPWDDIR=/etc
-			YPCHANGEOK=chsh
-			NISMASTER=
-			YPSERVARGS=
-			YPBINDARGS=
-			YPPASSWDDARGS=
-			YPXFRDARGS=
-			EOF
-	fi
+#	# Put a temporary nis config file that will prevent ypbind to start
+#	# Temporary NIS setup, disabling NIS server and client.
+#	if [ ! -f /etc/default/nis ] ; then
+#		cat <<-EOF >/etc/default/nis
+#			NISSERVER=false
+#			NISCLIENT=false
+#			YPPWDDIR=/etc
+#			YPCHANGEOK=chsh
+#			NISMASTER=
+#			YPSERVARGS=
+#			YPBINDARGS=
+#			YPPASSWDDARGS=
+#			YPXFRDARGS=
+#			EOF
+#	fi
 
 	if [ ! -f /etc/bind/named.conf.options ] ; then
 		cat <<-EOF >/etc/bind/named.conf.options
