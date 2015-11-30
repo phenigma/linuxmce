1871af0f9a7af357c5ecafa68808551c3300affc
phenigma <phenigma@hotmail.com>
2015-11-30 23:32:00 +0000

    Refs #2489 - add vdr to build-replacements for trusty/xenial & raspbian

--- src/Ubuntu_Helpers_NoHardcode/build.sh
+++ src/Ubuntu_Helpers_NoHardcode/build.sh
@@ -18,30 +18,30 @@ rm *.changes || :
 rm *.upload || :
 popd
 
-#"${build_scripts_dir}/checkout-svn.sh"
-#"${build_scripts_dir}/import-win32bins.sh"
+"${build_scripts_dir}/checkout-svn.sh"
+"${build_scripts_dir}/import-win32bins.sh"
 
-#"${build_scripts_dir}/build-replacements.sh"
+"${build_scripts_dir}/build-replacements.sh"
 "${build_scripts_dir}/build-makerelease.sh"
 "${build_scripts_dir}/import-databases.sh"
 
 "${build_scripts_dir}/build-maindebs.sh"
-#"${build_scripts_dir}/get-closed-source-debs.sh"
+"${build_scripts_dir}/get-closed-source-debs.sh"
 
 "${build_scripts_dir}/create-repo.sh"
 
 #"${build_scripts_dir}/cd1-build.sh"
 #"${build_scripts_dir}/cd2-build.sh"
 
-## push stuff into deb
-#pushd "$local_mirror_dir"
-#dupload --to linuxmce *.changes
-#popd
+# push stuff into deb
+pushd "$local_mirror_dir"
+dupload --to linuxmce *.changes
+popd
 
 # Don't dupload twice, these files are copied into $local_mirror_dir during create-repo.sh
 #pushd /var/lmce-build/replacements
 #dupload --to linuxmce *.changes
 #popd
 
-#VERSION=`grep "int g_SvnRevision" /var/lmce-build/svn/trunk/src/version.h | grep -v extern | cut -f2 -d"=" | cut -f1 -d";"`
-#wget --no-check-certificate  https://vt100.at/announce.php?text=$build_name\ $arch\ fluffy\ is\ done\ with\ $VERSION -O /dev/null
+VERSION=`grep "int g_SvnRevision" /var/lmce-build/svn/trunk/src/version.h | grep -v extern | cut -f2 -d"=" | cut -f1 -d";"`
+wget --no-check-certificate  https://vt100.at/announce.php?text=$build_name\ $arch\ fluffy\ is\ done\ with\ $VERSION -O /dev/null
