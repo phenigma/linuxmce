063bffcbaf00d084961203f4470e83a77b942877
phenigma <phenigma@hotmail.com>
2015-12-01 19:58:19 +0000

    Fixes #2086 - hide error msg generated because the devices have not been created prior to package installation.

--- src/CreateDevice/mkr_postinst.sh
+++ src/CreateDevice/mkr_postinst.sh
@@ -4,5 +4,7 @@
 . /usr/pluto/bin/Config_Ops.sh
 . /usr/pluto/bin/SQL_Ops.sh
 
-/usr/pluto/bin/CreateDevice -h dcerouter -r || :
+# TODO: remove this?  This is a relic of database based installation
+# where the database existed prior to package installation.
+/usr/pluto/bin/CreateDevice -h dcerouter -r 2>/dev/null >/dev/null || :
 
