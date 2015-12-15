04b183518b8ded741f254f1bef2d0e8a854b0c58
phenigma <phenigma@hotmail.com>
2015-12-15 21:50:11 +0000

    Fixes #2556 - overwrite autostartmedia value in md meta postinst.  Ensures that a hybrid has autostartmedia set to 1.

--- src/MediaDirector/meta/mkr_postinst.sh
+++ src/MediaDirector/meta/mkr_postinst.sh
@@ -2,7 +2,7 @@
 
 . /usr/pluto/bin/Config_Ops.sh
 
-[[ -z "$AutostartMedia" ]] && ConfSet "AutostartMedia" "1"
+ConfSet "AutostartMedia" "1"
 [[ -z "$AVWizardDone" ]] && ConfSet "AVWizardDone" "0"
 [[ -z "AVWizardOverride" ]] && ConfSet "AVWizardOverride" "1"
 
