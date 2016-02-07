db0ed83dbffcff2155a0fd2a506273f0d5a82f29
phenigma <phenigma@hotmail.com>
2016-02-07 21:36:49 +0000

    Refs #2351 - do not run UpdateDebCache.sh in postinst, this *could* cause problems

--- src/UbuntuDiskless/mkr_postinst.sh
+++ src/UbuntuDiskless/mkr_postinst.sh
@@ -10,14 +10,4 @@ if [ -x /usr/pluto/bin/Debug_LogKernelModules.sh ]; then
 	/usr/pluto/bin/Debug_LogKernelModules.sh "$0" || :
 fi
 
-#################################################################
-## move deb-cache to newer multi-arch friendly location
-## update sourcs.list on core and all MDs to reflect new locations
-if [ ! -d /usr/pluto/deb-cache ]; then
-	exit 0
-fi
-
-LastVersion="$2"
-
-[[ -x "/usr/pluto/bin/UpdateDebCache.sh" ]] && /usr/pluto/bin/UpdateDebCache.sh
-
+exit 0
