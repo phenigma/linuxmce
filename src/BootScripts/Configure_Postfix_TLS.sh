07ac395acdef4653be996e94b4073a300aa3d9ad
phenigma <phenigma@hotmail.com>
2016-02-13 20:12:46 +0000

    Refs #2314 - update TLS expect configuration script.  Certs are generated properly again.

--- src/BootScripts/Configure_Postfix_TLS.sh
+++ src/BootScripts/Configure_Postfix_TLS.sh
@@ -1,22 +1,23 @@
-#!/usr/bin/expect 
+#!/usr/bin/expect
 
-set timeout 1 
-set email [lindex $argv 0] 
-set password [lindex $argv 1] 
-set name [lindex $argv 2] 
-set country [lindex $argv 3] 
-set state [lindex $argv 4] 
-set org [lindex $argv 5] 
-set dn [lindex $argv 6] 
+set timeout 1
+set email [lindex $argv 0]
+set password [lindex $argv 1]
+set name [lindex $argv 2]
+set country [lindex $argv 3]
+set state [lindex $argv 4]
+set org [lindex $argv 5]
+set dn [lindex $argv 6]
 
 spawn openssl genrsa -des3 -out key.pem 1024
-expect -re "Enter PEM pass phrase for key.pem:"
-send "$password\r"
-expect -re "Verifying password - Enter PEM pass phrase for key.pem:"
-send "$password\r"
+expect -re "Enter pass phrase for key.pem:"
+send "$password\n"
+expect -re "Verifying - Enter pass phrase for key.pem:"
+send "$password\n"
+send_error "\nCONFIG: initial key.pem created\n"
 
 spawn openssl req -new -key key.pem -out req.pem
-expect -re "Enter PEM pass phrase for key.pem:"
+expect -re "Enter pass phrase for key.pem:"
 send "$password\r"
 expect -re "Country Name (2 letter code)"
 send "$country\r"
@@ -32,9 +33,10 @@ expect -re "Common Name (eg, your name or your server's hostname)"
 send "dcerouter\r"
 expect -re "Please enter the following 'extra' attributes\nto be sent with your certificate request\nA challenge password"
 send "\r"
-expect -re "An optional company name"
+#expect -re "An optional company name"
 send "\r"
 send "\r"
+send_error "\nCONFIG: RSA request req.pem created\n"
 
 spawn cp key.pem key.pem.orig
 
@@ -42,10 +44,13 @@ spawn openssl rsa -in key.pem.orig -out key.pem
 expect -re "Enter pass phrase for key.pem.orig:"
 send "$password\r"
 expect -re "writing RSA key"
+send_error "\nCONFIG: RSA key.pem created\n"
 
 spawn openssl x509 -req -days 365 -in req.pem -signkey key.pem -out cert.pem
-expect -re "Enter pass phrase for key.pem:"
-send "$password\r"
-#expect -re "Signature ok"
+#expect -re "Enter pass phrase for key.pem:"
+#send "$password\r"
+expect -re "Signature ok"
 
-exit 0 
+send_error "\nCONFIG: RSA key.pem signed\n"
+
+exit 0
