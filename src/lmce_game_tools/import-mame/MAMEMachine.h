fa525bcb10d203eae604f5a9d37d8bf959148efe
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2016-02-14 01:34:28 -0600

    Refactoring, more to come.

---
+++ src/lmce_game_tools/import-mame/MAMEMachine.h
@@ -0,0 +1,86 @@
+/**
+ * MAMEMachine - a container for ROM metadata
+ *
+ * Version: 1.0
+ * Author: Thomas Cherryhomes <thom.cherryhomes@gmail.com>
+ */
+
+#ifndef MAMEMACHINE_H
+#define MAMEMACHINE_H
+
+#include <string>
+
+using namespace std; 
+
+class MAMEMachine
+{
+ private:
+  string m_sMachineName;
+  string m_sMachineDescription;
+  string m_sMachineManufacturer;
+  string m_sMachineYear;
+  string m_sMachineCloneOf;
+  string m_sMachineRomOf;
+  string m_sMachineDriverStatus;
+  string m_sMachineDriverEmulationStatus;
+  string m_sMachineDriverColorStatus;
+  string m_sMachineDriverSoundStatus;
+  string m_sMachineDriverGraphicStatus;
+  string m_sMachineDriverSaveStateStatus;
+  string m_sMachineGenre;
+
+ public:
+  MAMEMachine() 
+    {
+      m_sMachineName="";
+      m_sMachineDescription="";
+      m_sMachineManufacturer="";
+      m_sMachineYear="";
+      m_sMachineCloneOf="";
+      m_sMachineRomOf="";
+      m_sMachineDriverStatus="";
+      m_sMachineDriverEmulationStatus="";
+      m_sMachineDriverColorStatus="";
+      m_sMachineDriverSoundStatus="";
+      m_sMachineDriverGraphicStatus="";
+      m_sMachineDriverSaveStateStatus="";
+      m_sMachineGenre="";
+    }
+
+  virtual ~MAMEMachine()
+    {
+      
+    }
+
+  // Get accessors
+  string MachineName_get() {return m_sMachineName;}
+  string MachineDescription_get() {return m_sMachineDescription;}
+  string MachineManufacturer_get() {return m_sMachineManufacturer;}
+  string MachineYear_get() {return m_sMachineYear;}
+  string MachineCloneOf_get() {return m_sMachineCloneOf;}
+  string MachineRomOf_get() {return m_sMachineRomOf;}
+  string MachineDriverStatus_get() {return m_sMachineDriverStatus;}
+  string MachineDriverEmulationStatus_get() {return m_sMachineDriverEmulationStatus;}
+  string MachineDriverColorStatus_get() {return m_sMachineDriverColorStatus;}
+  string MachineDriverSoundStatus_get() {return m_sMachineDriverSoundStatus;}
+  string MachineDriverGraphicStatus_get() {return m_sMachineDriverGraphicStatus;}
+  string MachineDriverSaveStateStatus_get() {return m_sMachineDriverSaveStateStatus;}
+  string MachineGenre_get() {return m_sMachineGenre;}
+
+  // Set accessors
+  void MachineName_set(string sMachineName) {m_sMachineName=sMachineName;}
+  void MachineDescription_set(string sMachineDescription) {m_sMachineDescription=sMachineDescription;}
+  void MachineManufacturer_set(string sMachineManufacturer) {m_sMachineManufacturer=sMachineManufacturer;}
+  void MachineYear_set(string sMachineYear) {m_sMachineYear=sMachineYear;}
+  void MachineCloneOf_set(string sMachineCloneOf) {m_sMachineCloneOf=sMachineCloneOf;}
+  void MachineRomOf_set(string sMachineRomOf) {m_sMachineRomOf=sMachineRomOf;}
+  void MachineDriverStatus_set(string sMachineDriverStatus) {m_sMachineDriverStatus=sMachineDriverStatus;}
+  void MachineDriverEmulationStatus_set(string sMachineDriverEmulationStatus) {m_sMachineDriverEmulationStatus=sMachineDriverEmulationStatus;}
+  void MachineDriverColorStatus_set(string sMachineDriverColorStatus) {m_sMachineDriverColorStatus=sMachineDriverColorStatus;}
+  void MachineDriverSoundStatus_set(string sMachineDriverSoundStatus) {m_sMachineDriverSoundStatus=sMachineDriverSoundStatus;}
+  void MachineDriverGraphicStatus_set(string sMachineDriverGraphicStatus) {m_sMachineDriverGraphicStatus=sMachineDriverGraphicStatus;}
+  void MachineDriverSaveStateStatus_set(string sMachineDriverSaveStateStatus) {m_sMachineDriverSaveStateStatus=sMachineDriverSaveStateStatus;}
+  void MachineGenre_set(string sMachineGenre) {m_sMachineGenre=sMachineGenre;}
+};
+
+#endif /** MAMEMACHINE_H */
