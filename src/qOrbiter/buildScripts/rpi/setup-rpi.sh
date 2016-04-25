d64f94f450363fc3f8ae739e8a670e5649cdf57e
Langston Ball <lball@ics.com>
2016-04-24 17:10:43 -0700

    refs #1108 - update qt from qorbiter at startup, update scripts used for building.

--- src/qOrbiter/buildScripts/rpi/setup-rpi.sh
+++ src/qOrbiter/buildScripts/rpi/setup-rpi.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 source setEnv.sh
+
+#a check for the following would be nice
+#a package listing these as dependencies would be better
+#dreams
 sudo apt-get install build-essential git autoconf automake libtool
-#ROOT_FS_MOUNT="/mnt/rasp-pi-rootfs"
-#TARGET_USER="golgoj4"
-#TARGET_IP="192.168.80.2"
-#TOOLCHAN_PATH=""
-#STARTPATH=$(pwd)
 
-
+#check for this somehow? makes life easier.
 ssh-copy-id $TARGET_USER@$TARGET_IP
 
 echo "Setting up Raspberry Pi working directory"
@@ -27,6 +26,7 @@ git clone git://gitorious.org/cross-compile-tools/cross-compile-tools.git
 fi
 
 clear
+
 #echo "Installing remote things, please be prepared to enter password"
 #ssh $TARGET_USER@$TARGET_IP "sudo apt-get build-dep qt4-x11"
 #ssh $TARGET_USER@$TARGET_IP "sudo apt-get build-dep libqt5gui5"
@@ -64,21 +64,14 @@ rsync -avz $TARGET_USER@$TARGET_IP:/usr/lib sysroot/usr
 rsync -avz $TARGET_USER@$TARGET_IP:/opt/vc sysroot/opt
 ./sysroot-relativelinks.py sysroot
 fi
+clear
 
-if [ -d "qt5" ];
-then
-cd qt5
-git pull
-./configure -debug -opengl es2 -device linux-rasp-pi2-g++ -device-option CROSS_COMPILE=$STARTPATH/rpi-tools/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf- -sysroot $STARTPATH/rpi-tools/sysroot -opensource -confirm-license -make libs -prefix /usr/local/qt5pi -extprefix $STARTPATH/qt/qt5pi -hostprefix  $STARTPATH/qt/qt5 -c++11 -make examples -v 
-#make -j8
-else
-git clone git://code.qt.io/qt/qt5.git
-cd qt5
-perl init-repository
-git checkout $QT_BRANCH
-#make
-#make install
-fi
+#echo "Please login and run /tmp/pi-localSetup.sh, the exit the pi"
+#echo "command: sudo /tmp/./pi-localSetup.sh"
+#scp  $STARTPATH/pi-localSetup.sh $TARGET_USER@$TARGET_IP:/tmp/
+#ssh  $TARGET_USER@$TARGET_IP
+
+
 
 #if [ -d "$ROOT_FS_MOUNT" ];
 #then
@@ -91,7 +84,7 @@ fi
 
 #sudo apt-get install libfontconfig1-dev libdbus-1-dev libfreetype6-dev libudev-dev libicu-dev libsqlite3-dev libxslt1-dev libssl-dev libasound2-dev libavcodec-dev #libavformat-dev libswscale-dev libraspberrypi-dev libpulse-dev libx11-dev libglib2.0-dev libcups2-dev freetds-dev libsqlite0-dev libpq-dev libiodbc2-dev #libmysqlclient-dev firebird-dev libpng12-dev libjpeg62-dev libgst-dev libxext-dev libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-#dev libxcb-image0 libxcb-image0-dev libxcb-shm0 libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-sync0 libxcb-sync0-dev libxcb-render-util0 libxcb-render-#util0-dev libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-glx0-dev libxi-dev libdrm-dev
 
-#debhelper dh-apparmor freetds-common freetds-dev gir1.2-atspi-2.0 gir1.2-gst-plugins-base-0.10 gir1.2-gstreamer-0.10 gir1.2-gtk-2.0 icu-devtools libatk1.0-dev libatspi2.0-dev libcairo-script-interpreter2 libcairo2-dev libct4 libcups2-dev libdbus-glib-1-dev libgbm-dev libgdk-pixbuf2.0-dev libgstreamer-plugins-base0.10-dev libgstreamer0.10-dev libgtk2.0-dev libicu-dev libjpeg-dev libjpeg-turbo8-dev libjpeg8-dev libodbc1 libopenvg1-mesa-dev libpango1.0-dev libpixman-1-dev libpq-dev libsqlite3-dev libsybdb5 libudev-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-render-util0-dev libxcb-xkb-dev libxcomposite-dev libxcursor-dev libxft-dev libxi-dev libxinerama-dev libxkbcommon-dev  libxkbcommon-x11-dev libxml2-dev libxrandr-dev libxrender-dev libxtst-dev po-debconf unixodbc unixodbc-dev x11proto-composite-dev x11proto-randr-dev x11proto-record-dev x11proto-render-dev x11proto-xinerama-dev xvfb
+#sudo apt-get install debhelper dh-apparmor freetds-common freetds-dev gir1.2-atspi-2.0 gir1.2-gst-plugins-base-0.10 gir1.2-gstreamer-0.10 gir1.2-gtk-2.0 icu-devtools libatk1.0-dev libatspi2.0-dev libcairo-script-interpreter2 libcairo2-dev libct4 libcups2-dev libdbus-glib-1-dev libgbm-dev libgdk-pixbuf2.0-dev libgstreamer-plugins-base0.10-dev libgstreamer0.10-dev libgtk2.0-dev libicu-dev libjpeg-dev libjpeg-turbo8-dev libjpeg8-dev libodbc1 libopenvg1-mesa-dev libpango1.0-dev libpixman-1-dev libpq-dev libsqlite3-dev libsybdb5 libudev-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-render-util0-dev libxcb-xkb-dev libxcomposite-dev libxcursor-dev libxft-dev libxi-dev libxinerama-dev libxkbcommon-dev  libxkbcommon-x11-dev libxml2-dev libxrandr-dev libxrender-dev libxtst-dev po-debconf unixodbc unixodbc-dev x11proto-composite-dev x11proto-randr-dev x11proto-record-dev x11proto-render-dev x11proto-xinerama-dev xvfb
 
 
 exit
