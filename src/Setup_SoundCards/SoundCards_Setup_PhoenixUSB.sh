cab9d5c43348d09dd4e74101c0bc9c97d5318274
phenigma <phenigma@hotmail.com>
2016-05-02 02:10:11 +0000

    Refs #2258 - don't assume Phoenix is card #1, determine cardid from aplay.

--- src/Setup_SoundCards/SoundCards_Setup_PhoenixUSB.sh
+++ src/Setup_SoundCards/SoundCards_Setup_PhoenixUSB.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-## 
-## Setup Script for the Phoenix Solo USB microphone.
 ##
-## TODO: Soundcard detection needs to be refined so that
-## It doesn't just assume it's the second sound device
-## in the system.
+## Setup Script for the Phoenix Solo USB microphone && Duet USB speaker/microphone.
+##
+## TODO: Store volume values in Device's DD
 ##
 
-## AudioSettings parameter isn't used at all. This is a completely
-## separate device from the main sound in an MD.
+## MDs AudioSettings parameter isn't used at all. This is a
+## completely separate device from the main sound in an MD.
 
-amixer -c 1 sset "PCM",0 50 >/dev/null 
-amixer -c 1 sset "Speaker",0 44 >/dev/null
-amixer -c 1 sset "Mic",0 50 >/dev/null
+CARDID=$(aplay -l | grep Phnx | awk '{ print $2 }' | cut -d':' -f1)
+
+amixer -c "$CARDID" sset "PCM",0 50 >/dev/null
+amixer -c "$CARDID" sset "Speaker",0 44 >/dev/null
+amixer -c "$CARDID" sset "Mic",0 50 >/dev/null
 
