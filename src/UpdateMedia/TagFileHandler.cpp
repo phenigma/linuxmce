95418edc968d6a7d0975902c4e21d58b503a0f67
phenigma <phenigma@hotmail.com>
2016-05-04 00:45:12 -0400

    Refs #2369 - notes for implementing multiple album artist, performer, genre import/exports.

--- src/UpdateMedia/TagFileHandler.cpp
+++ src/UpdateMedia/TagFileHandler.cpp
@@ -50,6 +50,9 @@ bool TagFileHandler::LoadAttributes(PlutoMediaAttributes *pPlutoMediaAttributes,
 	{
 		int nType = it->first;
 		string sValue = it->second;
+// FIXME: Need to de-tokenize items (sValue) as attributes can hold multiple entries
+// This becomes a little more complicated as we have to de-tokenize sValue 
+// for comparasin(multiple if's?) and, if required, re-tokenize for saving(else)
 		LoggerWrapper::GetInstance()->Write(LV_WARNING, "# LoadAttributes, from file: type %d: '%s'", nType, sValue.c_str());
 
 		MapPlutoMediaAttributes::iterator itm = pPlutoMediaAttributes->m_mapAttributes.find(nType);
@@ -82,6 +85,7 @@ bool TagFileHandler::SaveAttributes(PlutoMediaAttributes *pPlutoMediaAttributes)
 		for(MapPlutoMediaAttributes::iterator it = pPlutoMediaAttributes->m_mapAttributes.begin(), 
 			end = pPlutoMediaAttributes->m_mapAttributes.end(); it != end; ++it)
 		{
+// FIXME: Need to tokenize items as attributes can hold multiple entries
 			mapAttributes[it->first] = it->second->m_sName;
 		}
 
@@ -165,21 +169,24 @@ void TagFileHandler::GetTagInfo(string sFilename, map<int,string>& mapAttributes
 		LoggerWrapper::GetInstance()->Write(LV_STATUS, "# TagFileHandler::GetTagInfo: tags present");
 #endif
 
-		// Get Album Artist, from properties
+		// Get Album Artist, from file properties
 		TagLib::PropertyMap tags = f->file()->properties();
 		for(TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i) {
 			if ( (i->first == "ALBUMARTIST") || (i->first == "ALBUM ARTIST") )
 			{
 				for(TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j) {
 					TagLib::String album_artist = *j;
+// FIXME: Need to tokenize/de-tokenize items as mapAttributes[] can hold multiple entries
 					mapAttributes[ATTRIBUTETYPE_Album_Artist_CONST] = album_artist.to8Bit(true);
 					break;
 				}
 			}
 		}
 
+// FIXME: Need to de-tokenize/tokenize items as mapAttributes[] can hold multiple entries
 		mapAttributes[ATTRIBUTETYPE_Performer_CONST] = f->tag()->artist().to8Bit(true);
 		mapAttributes[ATTRIBUTETYPE_Title_CONST] = f->tag()->title().to8Bit(true);
+// FIXME: Need to de-tokenize/tokenize items as mapAttributes[] can hold multiple entries
 		mapAttributes[ATTRIBUTETYPE_Genre_CONST] = f->tag()->genre().to8Bit(true);
 		mapAttributes[ATTRIBUTETYPE_Album_CONST] = f->tag()->album().to8Bit(true);
 
@@ -202,7 +209,7 @@ void TagFileHandler::GetTagInfo(string sFilename, map<int,string>& mapAttributes
 			if (nBinSize > 0) {
 				rewind(coverart);
 				char *pPictureData = new char[nBinSize];
-				fread(pPictureData, 1,nBinSize,coverart);	
+				fread(pPictureData, 1,nBinSize,coverart);
 				listPictures.push_back(make_pair(pPictureData, nBinSize));
 			}
 		}
@@ -216,10 +223,20 @@ void TagFileHandler::SetTagInfo(string sFilename, const map<int,string>& mapAttr
 	if(NULL != f)
 	{
 		// Set Album Artist, in properties
+// FIXME: Need to de-tokenize items as properties can be specified multiple times with single entries in this tag type
 		f->file()->properties().insert("ALBUMARTIST",String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Album_Artist_CONST), String::UTF8));
+// FIXME: Need to de-tokenize items as properties can be specified multiple times with single entries in this tag type
+		f->file()->properties().insert("ARTIST", String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Performer_CONST), String::UTF8));
+		f->file()->properties().insert("TITLE", String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Title_CONST), String::UTF8));
+// FIXME: Need to de-tokenize items as properties can be specified multiple times with single entries in this tag type
+		f->file()->properties().insert("GENRE", String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Genre_CONST), String::UTF8));
+		f->file()->properties().insert("TRACKNUMBER", String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Track_CONST), String::UTF8));
+		f->file()->properties().insert("DATE", String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Release_Date_CONST), String::UTF8));
 
+// No need to de-tokenize items as single attribute will hold currently tokenized values
 		f->tag()->setArtist(String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Performer_CONST), String::UTF8));
 		f->tag()->setTitle(String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Title_CONST), String::UTF8));
+// No need to de-tokenize items as single attribute will hold currently tokenized values
 		f->tag()->setGenre(String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Genre_CONST), String::UTF8));
 		f->tag()->setAlbum(String(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Album_CONST), String::UTF8));
 		f->tag()->setTrack(atoi(ExtractAttribute(mapAttributes, ATTRIBUTETYPE_Track_CONST).c_str()));
@@ -238,34 +255,43 @@ void TagFileHandler::RemoveTag(string sFilename, int nTagType, string sValue)
 		switch(nTagType)
 		{
 			case ATTRIBUTETYPE_Album_Artist_CONST:
+// FIXME: Need to check specific value as multiple attributes of the same type can exist
 				f->file()->properties().erase("ALBUMARTIST");
+				f->file()->properties().erase("ALBUM ARTIST");
 				break;
 			case ATTRIBUTETYPE_Performer_CONST:
+// FIXME: Need to add properties handling
+// FIXME: Need to tokenize/de-tokenize items as attribute can hold multiple entries
 				if(f->tag()->artist() == sValue)
 					f->tag()->setArtist("");
 				break;
 
 			case ATTRIBUTETYPE_Title_CONST:
+// FIXME: Need to add properties handling
 				if(f->tag()->title() == sValue)
 					f->tag()->setTitle("");
 				break;
 
 			case ATTRIBUTETYPE_Genre_CONST:
+// FIXME: Need to tokenize/de-tokenize items as attribute can hold multiple entries
 				if(f->tag()->genre() == sValue)
 					f->tag()->setGenre("");
 				break;
 
 			case ATTRIBUTETYPE_Album_CONST:
+// FIXME: Need to add properties handling
 				if(f->tag()->album() == sValue)
 					f->tag()->setAlbum("");
 				break;
 
 			case ATTRIBUTETYPE_Track_CONST:
+// FIXME: Need to add properties handling
 				if(f->tag()->track() == atoi(sValue.c_str()))
 					f->tag()->setTrack(0);
 				break;
 
 			case ATTRIBUTETYPE_Release_Date_CONST:
+// FIXME: Need to add properties handling
 				if(f->tag()->year() == atoi(sValue.c_str()))
 					f->tag()->setYear(0);
 				break;
