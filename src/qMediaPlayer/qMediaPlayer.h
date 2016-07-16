93ea080ead8a68c9295dcba542268cd17ad38fa2
Langston Ball <lball@ics.com>
2016-07-16 11:38:11 -0700

    refs #1170 #1108 - dce side of pause bug fix

--- src/qMediaPlayer/qMediaPlayer.h
+++ src/qMediaPlayer/qMediaPlayer.h
@@ -977,7 +977,7 @@ public:
 
         void stopCurrentMedia();
         void startPlayback();
-        void pausePlayback();
+        void pausePlayback(bool paused);
 
         void getScreenShot();
         void setZoomLevel(QString zoom);
