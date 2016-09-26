d34cae0820e46d4ba1ddfcbcbb9c4a12b7aad37e
phenigma <phenigma@hotmail.com>
2016-09-26 22:25:17 +0000

    Refs #1521 - update the default diskless config.txt to enable analog audio and default lcd rotation for the foundation touchscreen.

--- src/UbuntuDiskless/Diskless_Setup.sh
+++ src/UbuntuDiskless/Diskless_Setup.sh
@@ -48,8 +48,13 @@ function setup_tftp_boot_rpi
 		# uncomment if you get no picture on HDMI for a default "safe" mode
 		#hdmi_safe=1
 		disable_overscan=1
-		gpu_mem=128
+		config_hdmi_boost=4
+		hdmi_force_hotplug=1
 		lcd_rotate=2
+		gpu_mem=128
+
+		# Enable audio (loads snd_bcm2835)
+		dtparam=audio=on
 		EEOF
 }
 
